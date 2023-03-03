c68d1b28736e1f502505cca07f6a7a3174081e28
Tao Chen <taochen@microsoft.com>
2023-03-02 21:54:24 -0800

    Remove the word "web" from the error msgs

--- samples/apps/book-creator-webapp-react/src/hooks/SemanticKernel.ts
+++ samples/apps/book-creator-webapp-react/src/hooks/SemanticKernel.ts
@@ -76,7 +76,7 @@ export class SemanticKernel {
             var additional_error_msg = ''
             if (e instanceof TypeError) {
                 // fetch() will reject with a TypeError when a network error is encountered.
-                additional_error_msg = '\n\nPlease check you have the function running and that it is accessible by the web app'
+                additional_error_msg = '\n\nPlease check you have the function running and that it is accessible by the app'
             }
             throw e + additional_error_msg;
         }
