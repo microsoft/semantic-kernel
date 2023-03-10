76e3593e73aabe09b99bd295fd6c6acf72615bb2
lemiller@microsoft.com <lemiller@microsoft.com>
2023-03-09 22:47:51 -0800

    Replace $input and $output with input and output in SemanticFunctionConstants.cs
    Summary: This commit changes the way the input and output parameters are written in the SemanticFunctionConstants.cs file, which defines the available functions for creating plans. Instead of using the $ symbol to indicate the parameters, the commit uses plain words input and output. This makes the syntax more consistent and easier to read. The commit also adds a new file FunctionViewExtensions.cs, which contains some helper methods for working with FunctionView objects. These methods simplify the generation of manual strings and function names for the functions.

---
+++ dotnet/src/SemanticKernel/Planning/FunctionViewExtensions.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Linq;
+using Microsoft.SemanticKernel.SkillDefinition;
+
+namespace Microsoft.SemanticKernel.Planning;
+
+internal static class FunctionViewExtensions
+{
+    internal static string ToManualString(this FunctionView function)
+    {
+        var inputs = string.Join("\n", function.Parameters.Select(p => $"    - {p.Name}: {p.Description}"));
+        return $"  {function.SkillName}.{function.Name}:\n    description: {function.Description}\n    inputs:\n{inputs}";
+    }
+
+    internal static string ToFunctionName(this FunctionView function)
+    {
+        return $"{function.SkillName}.{function.Name}";
+    }
+}
