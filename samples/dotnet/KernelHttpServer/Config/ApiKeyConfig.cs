03cd3be4e36ccb10c9ca97acd6e0c592274282b0
Craig Presti <146438+craigomatic@users.noreply.github.com>
2023-03-09 20:05:28 -0800

    Restructure the KernelHttpServer to support distinct completion and embedding backends. Most of the HTTP headers have been changed to enable this.

--- samples/dotnet/KernelHttpServer/Config/ApiKeyConfig.cs
+++ samples/dotnet/KernelHttpServer/Config/ApiKeyConfig.cs
@@ -1,36 +1,9 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
 namespace KernelHttpServer.Config;
-
 public class ApiKeyConfig
 {
-    public CompletionService CompletionBackend { get; set; }
+    public BackendConfig CompletionConfig { get; set; } = new();
 
-    public string DeploymentOrModelId { get; set; } = string.Empty;
-
-    public string Endpoint { get; set; } = string.Empty;
-
-    public string Key { get; set; } = string.Empty;
-
-    public string Label { get; set; } = string.Empty;
-
-    public bool IsValid()
-    {
-        switch (this.CompletionBackend)
-        {
-            case CompletionService.OpenAI:
-                return
-                    !string.IsNullOrEmpty(this.Label) &&
-                    !string.IsNullOrEmpty(this.DeploymentOrModelId) &&
-                    !string.IsNullOrEmpty(this.Key);
-            case CompletionService.AzureOpenAI:
-                return
-                    !string.IsNullOrEmpty(this.Endpoint) &&
-                    !string.IsNullOrEmpty(this.Label) &&
-                    !string.IsNullOrEmpty(this.DeploymentOrModelId) &&
-                    !string.IsNullOrEmpty(this.Key);
-        }
-
-        return false;
-    }
+    public BackendConfig EmbeddingConfig { get; set; } = new();
 }
