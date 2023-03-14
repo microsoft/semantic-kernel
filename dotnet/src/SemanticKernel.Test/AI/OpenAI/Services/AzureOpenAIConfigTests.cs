45e0af11478f2a950005a2fc57c03d8e13b6bd47
Tara E. Walker <taraew@live.com>
2023-03-14 06:03:30 -0400

    Adding merged files for Test/AI/OpenAI-TEW

---
+++ dotnet/src/SemanticKernel.Test/AI/OpenAI/Services/AzureOpenAIConfigTests.cs
@@ -0,0 +1,73 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.SemanticKernel.AI.OpenAI.Services;
+using Microsoft.SemanticKernel.Diagnostics;
+using Xunit;
+
+namespace SemanticKernelTests.AI.OpenAI.Services;
+
+public class AzureOpenAIConfigTests
+{
+    [Fact]
+    public void ConstructorWithValidParametersSetsProperties()
+    {
+        // Arrange
+        string label = "testLabel";
+        string deploymentName = "testDeploymentName";
+        string endpoint = "https://testEndpoint.com";
+        string apiKey = "testApiKey";
+        string apiVersion = "testApiVersion";
+
+        // Act
+        var config = new AzureOpenAIConfig(label, deploymentName, endpoint, apiKey, apiVersion);
+
+        // Assert
+        Assert.Equal(label, config.Label);
+        Assert.Equal(deploymentName, config.DeploymentName);
+        Assert.Equal(endpoint, config.Endpoint);
+        Assert.Equal(apiKey, config.APIKey);
+        Assert.Equal(apiVersion, config.APIVersion);
+    }
+
+    [Theory]
+    [InlineData("", "testDeploymentName", "https://testEndpoint.com", "testApiKey", "testApiVersion")]
+    [InlineData("testLabel", "", "https://testEndpoint.com", "testApiKey", "testApiVersion")]
+    [InlineData("testLabel", "testDeploymentName", "", "testApiKey", "testApiVersion")]
+    [InlineData("testLabel", "testDeploymentName", "https://testEndpoint.com", "", "testApiVersion")]
+    public void ConstructorWithEmptyParametersThrowsValidationException(
+        string label, string deploymentName, string endpoint, string apiKey, string apiVersion)
+    {
+        // Act + Assert
+        var exception = Assert.Throws<ValidationException>(() => new AzureOpenAIConfig(label, deploymentName, endpoint, apiKey, apiVersion));
+        Assert.Equal(ValidationException.ErrorCodes.EmptyValue, exception?.ErrorCode);
+    }
+
+    [Theory]
+    [InlineData("testLabel", "testDeploymentName", "http://testEndpoint.com", "testApiKey", "testApiVersion")]
+    [InlineData("testLabel", "testDeploymentName", "testEndpoint.com", "testApiKey", "testApiVersion")]
+    [InlineData("testLabel", "testDeploymentName", "testEndpoint", "testApiKey", "testApiVersion")]
+    public void ConstructorWithMissingPrefixParametersThrowsValidationException(
+        string label, string deploymentName, string endpoint, string apiKey, string apiVersion)
+    {
+        // Act + Assert
+        var exception = Assert.Throws<ValidationException>(() => new AzureOpenAIConfig(label, deploymentName, endpoint, apiKey, apiVersion));
+        Assert.Equal(ValidationException.ErrorCodes.MissingPrefix, exception?.ErrorCode);
+    }
+
+    [Fact]
+    public void EndpointWithValidValueDoesNotThrow()
+    {
+        // Arrange
+        string label = "testLabel";
+        string deploymentName = "testDeploymentName";
+        string endpoint = "https://testEndpoint.com";
+        string apiKey = "testApiKey";
+        string apiVersion = "testApiVersion";
+
+        // Act
+        var config = new AzureOpenAIConfig(label, deploymentName, endpoint, apiKey, apiVersion);
+
+        // Assert
+        Assert.Equal(endpoint, config.Endpoint);
+    }
+}
---
+++ dotnet/src/SemanticKernel.Test/AI/OpenAI/Services/OpenAIConfigTests.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.SemanticKernel.AI.OpenAI.Services;
+using Microsoft.SemanticKernel.Diagnostics;
+using Xunit;
+
+namespace SemanticKernelTests.AI.OpenAI.Services;
+
+/// <summary>
+/// Unit tests of <see cref="OpenAIConfig"/>
+/// </summary>
+public class OpenAIConfigTests
+{
+    [Fact]
+    public void ConstructorWithValidParametersSetsProperties()
+    {
+        // Arrange
+        string label = "testLabel";
+        string modelId = "testModelId";
+        string apiKey = "testApiKey";
+        string orgId = "testOrgId";
+
+        // Act
+        var config = new OpenAIConfig(label, modelId, apiKey, orgId);
+
+        // Assert
+        Assert.Equal(label, config.Label);
+        Assert.Equal(modelId, config.ModelId);
+        Assert.Equal(apiKey, config.APIKey);
+        Assert.Equal(orgId, config.OrgId);
+    }
+
+    [Theory]
+    [InlineData("", "testModelId", "testApiKey", "testOrgId")]
+    [InlineData("testLabel", "", "testApiKey", "testOrgId")]
+    [InlineData("testLabel", "testModelId", "", "testOrgId")]
+    public void ConstructorWithEmptyRequiredParameterThrowsArgumentException(
+        string label, string modelId, string apiKey, string orgId)
+    {
+        // Act + Assert
+        var exception = Assert.Throws<ValidationException>(() => new OpenAIConfig(label, modelId, apiKey, orgId));
+
+        Assert.Equal(ValidationException.ErrorCodes.EmptyValue, exception?.ErrorCode);
+    }
+
+    [Fact]
+    public void OrgIdWithNullValueDoesNotThrow()
+    {
+        // Arrange
+        string label = "testLabel";
+        string modelId = "testModelId";
+        string apiKey = "testApiKey";
+
+        // Act
+        var config = new OpenAIConfig(label, modelId, apiKey, null);
+
+        // Assert
+        Assert.Null(config.OrgId);
+    }
+}
