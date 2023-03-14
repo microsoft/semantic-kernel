05476805ab4d6180edc85d9b72901c1c9d59c4ab
lemiller@microsoft.com <lemiller@microsoft.com>
2023-03-14 12:56:38 -0700

    formatting

--- dotnet/src/SemanticKernel.UnitTests/Reliability/DefaultHttpRetryHandlerTests.cs
+++ dotnet/src/SemanticKernel.UnitTests/Reliability/DefaultHttpRetryHandlerTests.cs
@@ -550,7 +550,7 @@ public class DefaultHttpRetryHandlerTests
         await Assert.ThrowsAsync<HttpRequestException>(() => httpClient.GetAsync(new Uri("https://www.microsoft.com"), CancellationToken.None));
 
         // Assert
-        mockTimeProvider.Verify(x => x.GetCurrentTime(), Times.Exactly(4));  // 1 intial, 2 retries, 1 for logging time taken.
+        mockTimeProvider.Verify(x => x.GetCurrentTime(), Times.Exactly(4)); // 1 intial, 2 retries, 1 for logging time taken.
         mockDelayProvider.Verify(x => x.DelayAsync(TimeSpan.FromMilliseconds(50), It.IsAny<CancellationToken>()), Times.Exactly(1));
         mockHandler.Protected()
             .Verify<Task<HttpResponseMessage>>("SendAsync", Times.Exactly(2), ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>());
--- dotnet/src/SemanticKernel/Reliability/DefaultHttpRetryHandler.cs
+++ dotnet/src/SemanticKernel/Reliability/DefaultHttpRetryHandler.cs
@@ -85,7 +85,8 @@ public sealed class DefaultHttpRetryHandler : DelegatingHandler
                 if (!this.HasTimeForRetry(start, retryCount, response, out waitFor))
                 {
                     this._log.LogError(
-                        "Error executing request, max total retry time reached. Reason: {0}. Time spent: {1}", reason, this._timeProvider.GetCurrentTime() - start);
+                        "Error executing request, max total retry time reached. Reason: {0}. Time spent: {1}", reason,
+                        this._timeProvider.GetCurrentTime() - start);
                     return response;
                 }
             }
@@ -101,7 +102,8 @@ public sealed class DefaultHttpRetryHandler : DelegatingHandler
                 else if (!this.HasTimeForRetry(start, retryCount, response, out waitFor))
                 {
                     this._log.LogError(e,
-                        "Error executing request, max total retry time reached. Reason: {0}. Time spent: {1}", reason, this._timeProvider.GetCurrentTime() - start);
+                        "Error executing request, max total retry time reached. Reason: {0}. Time spent: {1}", reason,
+                        this._timeProvider.GetCurrentTime() - start);
                     throw;
                 }
             }
