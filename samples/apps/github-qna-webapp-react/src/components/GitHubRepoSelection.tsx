d044cab3c77415ab38fc51be4567c3a7407982a9
markwallace-microsoft <mark.e.wallace@outlook.com>
2023-03-15 18:44:43 +0000

    Add the CodeSkill to Sample4

--- samples/apps/github-qna-webapp-react/src/components/GitHubRepoSelection.tsx
+++ samples/apps/github-qna-webapp-react/src/components/GitHubRepoSelection.tsx
@@ -2,7 +2,8 @@
 
 import { Body1, Button, Input, Label, Spinner, Subtitle2, Title3 } from '@fluentui/react-components';
 import { ArrowDownload16Regular, CheckmarkCircle20Filled } from '@fluentui/react-icons';
-import { FC, useState } from "react";
+import { FC, useState } from 'react';
+
 import { useSemanticKernel } from '../hooks/useSemanticKernel';
 import { IKeyConfig } from '../model/KeyConfig';
 
@@ -25,64 +26,99 @@ const GitHubProjectSelection: FC<IData> = ({ uri, keyConfig, onLoadProject, onBa
         let cleanProjectUri = project?.trim();
 
         if (!cleanProjectUri?.endsWith('/')) {
-            cleanProjectUri = `${cleanProjectUri}/`
+            cleanProjectUri = `${cleanProjectUri}/`;
         }
 
         const url = `${cleanProjectUri}archive/refs/heads/${branch}.zip`;
-        const path = `%temp%\\${branch}_${new Date().getTime()}.zip`;
-
         try {
-            var result = await sk.invokeAsync(keyConfig, { value: url, inputs: [{ key: 'filePath', value: path }] }, 'WebFileDownloadSkill', 'DownloadToFile');
+            var result = await sk.invokeAsync(
+                keyConfig,
+                { value: url, inputs: [] },
+                'CodeSkill',
+                'SummarizeRepository',
+            );
             setIsLoaded(true);
+            console.log(result);
         } catch {
             setIsLoadError(true);
             alert('Something went wrong. Please check that the function is running and accessible from this location.');
         }
-    }
+    };
 
     return (
-        <div style={{ paddingTop: 20, gap: 20, display: "flex", flexDirection: "column", alignItems: "left" }}>
+        <div style={{ paddingTop: 20, gap: 20, display: 'flex', flexDirection: 'column', alignItems: 'left' }}>
             <Title3 style={{ alignItems: 'left' }}>Enter in the GitHub Project URL</Title3>
-            <Subtitle2>Start by entering a GitHub Repository URL. We will pull the public repository into local memory so you can ask any questions about the repository and get help. </Subtitle2>
+            <Subtitle2>
+                Start by entering a GitHub Repository URL. We will pull the public repository into local memory so you
+                can ask any questions about the repository and get help.{' '}
+            </Subtitle2>
             <br></br>
-            <Label><strong>GitHub Repository URL</strong></Label>
+            <Label>
+                <strong>GitHub Repository URL</strong>
+            </Label>
             <div style={{ display: 'flex', flexDirection: 'row', gap: 10 }}>
-                <Input style={{ width: '100%' }} type="text" value={project} onChange={(e) => setProject(e.target.value)} placeholder="https://github.com/microsoft/semantic-kernel" />
+                <Input
+                    style={{ width: '100%' }}
+                    type="text"
+                    value={project}
+                    onChange={(e) => setProject(e.target.value)}
+                    placeholder="https://github.com/microsoft/semantic-kernel"
+                />
             </div>
-            <Label><strong>Branch Name</strong></Label>
+            <Label>
+                <strong>Branch Name</strong>
+            </Label>
             <div style={{ display: 'flex', flexDirection: 'row', gap: 10 }}>
-                <Input style={{ width: '100%' }} type="text" value={branch} onChange={(e) => setBranch(e.target.value)} placeholder="main" />
-                <Button disabled={project === undefined || branch === undefined} appearance='transparent' icon={<ArrowDownload16Regular />} onClick={() => download()} />
+                <Input
+                    style={{ width: '100%' }}
+                    type="text"
+                    value={branch}
+                    onChange={(e) => setBranch(e.target.value)}
+                    placeholder="main"
+                />
+                <Button
+                    disabled={project === undefined || branch === undefined}
+                    appearance="transparent"
+                    icon={<ArrowDownload16Regular />}
+                    onClick={() => download()}
+                />
             </div>
-            {isLoading ?
+            {isLoading ? (
                 <div>
                     <Spinner />
-                    <Body1>
-                        Downloading respository...
-                    </Body1>
+                    <Body1>Downloading respository...</Body1>
                 </div>
-                :
+            ) : (
                 <></>
-            }
-            {isLoaded ?
+            )}
+            {isLoaded ? (
                 <div>
                     <CheckmarkCircle20Filled />
-                    <Body1>
-                        Repository downloaded. You can ask questions about it on the next page.
-                    </Body1>
-                </div> : <></>}
-            {isLoadError ?
-                <div>
-                    <Body1>
-                        There was an error downloading the repository. Please try again.
-                    </Body1>
+                    <Body1>Repository downloaded. You can ask questions about it on the next page.</Body1>
                 </div>
-                :
+            ) : (
                 <></>
-            }
+            )}
+            {isLoadError ? (
+                <div>
+                    <Body1>There was an error downloading the repository. Please try again.</Body1>
+                </div>
+            ) : (
+                <></>
+            )}
             <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'left', gap: 20 }}>
-                <Button style={{ width: 54 }} appearance="secondary" onClick={() => onBack()}>Back</Button>
-                <Button disabled={!isLoaded} appearance="primary" onClick={() => { if (project !== undefined && branch !== undefined) onLoadProject(project, branch) }}>Next</Button>
+                <Button style={{ width: 54 }} appearance="secondary" onClick={() => onBack()}>
+                    Back
+                </Button>
+                <Button
+                    disabled={!isLoaded}
+                    appearance="primary"
+                    onClick={() => {
+                        if (project !== undefined && branch !== undefined) onLoadProject(project, branch);
+                    }}
+                >
+                    Next
+                </Button>
             </div>
         </div>
     );
