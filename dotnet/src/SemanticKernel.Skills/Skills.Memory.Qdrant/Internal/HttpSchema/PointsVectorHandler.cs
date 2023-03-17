a0fcce44474306583e73ef1d58ded1efbc412d60
Tara E. Walker <tara.walker@microsoft.com>
2023-03-17 13:13:48 -0400

    WIP: Adding base for Points and Search API Handlers classes-TEW

---
+++ dotnet/src/SemanticKernel.Skills/Skills.Memory.Qdrant/Internal/HttpSchema/PointsVectorHandler.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Net.Http;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Microsoft.SemanticKernel.Skills.Memory.Qdrant.DataModels;
+using Microsoft.SemanticKernel.Skills.Memory.Qdrant.Diagnostics;
+using Microsoft.SemanticKernel.Skills.Memory.Qdrant.HttpSchema;
+
+namespace Micrsoft.SemanticKernel.Skills.Memory.Qdrant.HttpSchema;
+
+internal class PointsVectorHandler : IValidatable
+{
+    public enum PointVectorHandlerType
+    {
+        Upsert,
+        GetCollectionPoints,
+        GetPoint,
+        Delete,
+    }
+
+     public void Validate()
+    {
+        Verify.NotNullOrEmpty(this._collectionName, "The collection name is empty");
+    }
+
+    #region private ================================================================================
+        private string _collectionName;
+        
+        private HttpClient _client;
+    #endregion
+    
+}
