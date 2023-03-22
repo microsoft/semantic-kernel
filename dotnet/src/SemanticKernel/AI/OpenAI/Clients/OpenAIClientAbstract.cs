b6ddd6178dda8d616bfc62c04df321fcc0270bce
Dmytro Struk <dmytrostruk@microsoft.com>
2023-03-22 15:25:57 +0000

    Removed duplicated code

--- dotnet/src/SemanticKernel/AI/OpenAI/Clients/OpenAIClientAbstract.cs
+++ dotnet/src/SemanticKernel/AI/OpenAI/Clients/OpenAIClientAbstract.cs
@@ -11,6 +11,7 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.SemanticKernel.AI.Embeddings;
+using Microsoft.SemanticKernel.AI.Extensions;
 using Microsoft.SemanticKernel.AI.OpenAI.HttpSchema;
 using Microsoft.SemanticKernel.Text;
 
@@ -57,7 +58,7 @@ public abstract class OpenAIClientAbstract : IDisposable
         {
             this.Log.LogDebug("Sending completion request to {0}: {1}", url, requestBody);
 
-            var result = await this.ExecutePostRequestAsync<CompletionResponse>(url, requestBody);
+            var result = await this.HTTPClient.ExecutePostRequestAsync<CompletionResponse>(new Uri(url), requestBody);
             if (result.Completions.Count < 1)
             {
                 throw new AIException(
@@ -86,7 +87,7 @@ public abstract class OpenAIClientAbstract : IDisposable
     {
         try
         {
-            var result = await this.ExecutePostRequestAsync<EmbeddingResponse>(url, requestBody);
+            var result = await this.HTTPClient.ExecutePostRequestAsync<EmbeddingResponse>(new Uri(url), requestBody);
             if (result.Embeddings.Count < 1)
             {
                 throw new AIException(
@@ -131,112 +132,6 @@ public abstract class OpenAIClientAbstract : IDisposable
 
     // HTTP user agent sent to remote endpoints
     private const string HTTPUseragent = "Microsoft Semantic Kernel";
-
-    private async Task<T> ExecutePostRequestAsync<T>(string url, string requestBody)
-    {
-        string responseJson;
-
-        try
-        {
-            using HttpContent content = new StringContent(requestBody, Encoding.UTF8, MediaTypeNames.Application.Json);
-            HttpResponseMessage response = await this.HTTPClient.PostAsync(url, content);
-
-            if (response == null)
-            {
-                throw new AIException(AIException.ErrorCodes.NoResponse, "Empty response");
-            }
-
-            this.Log.LogTrace("HTTP response: {0} {1}", (int)response.StatusCode, response.StatusCode.ToString("G"));
-
-            responseJson = await response.Content.ReadAsStringAsync();
-            if (!response.IsSuccessStatusCode)
-            {
-                switch (response.StatusCode)
-                {
-                    case HttpStatusCode.BadRequest:
-                    case HttpStatusCode.MethodNotAllowed:
-                    case HttpStatusCode.NotFound:
-                    case HttpStatusCode.NotAcceptable:
-                    case HttpStatusCode.Conflict:
-                    case HttpStatusCode.Gone:
-                    case HttpStatusCode.LengthRequired:
-                    case HttpStatusCode.PreconditionFailed:
-                    case HttpStatusCode.RequestEntityTooLarge:
-                    case HttpStatusCode.RequestUriTooLong:
-                    case HttpStatusCode.UnsupportedMediaType:
-                    case HttpStatusCode.RequestedRangeNotSatisfiable:
-                    case HttpStatusCode.ExpectationFailed:
-                    case HttpStatusCode.MisdirectedRequest:
-                    case HttpStatusCode.UnprocessableEntity:
-                    case HttpStatusCode.Locked:
-                    case HttpStatusCode.FailedDependency:
-                    case HttpStatusCode.UpgradeRequired:
-                    case HttpStatusCode.PreconditionRequired:
-                    case HttpStatusCode.RequestHeaderFieldsTooLarge:
-                    case HttpStatusCode.HttpVersionNotSupported:
-                        throw new AIException(
-                            AIException.ErrorCodes.InvalidRequest,
-                            $"The request is not valid, HTTP status: {response.StatusCode:G}");
-
-                    case HttpStatusCode.Unauthorized:
-                    case HttpStatusCode.Forbidden:
-                    case HttpStatusCode.ProxyAuthenticationRequired:
-                    case HttpStatusCode.UnavailableForLegalReasons:
-                    case HttpStatusCode.NetworkAuthenticationRequired:
-                        throw new AIException(
-                            AIException.ErrorCodes.AccessDenied,
-                            $"The request is not authorized, HTTP status: {response.StatusCode:G}");
-
-                    case HttpStatusCode.RequestTimeout:
-                        throw new AIException(
-                            AIException.ErrorCodes.RequestTimeout,
-                            $"The request timed out, HTTP status: {response.StatusCode:G}");
-
-                    case HttpStatusCode.TooManyRequests:
-                        throw new AIException(
-                            AIException.ErrorCodes.Throttling,
-                            $"Too many requests, HTTP status: {response.StatusCode:G}");
-
-                    case HttpStatusCode.InternalServerError:
-                    case HttpStatusCode.NotImplemented:
-                    case HttpStatusCode.BadGateway:
-                    case HttpStatusCode.ServiceUnavailable:
-                    case HttpStatusCode.GatewayTimeout:
-                    case HttpStatusCode.InsufficientStorage:
-                        throw new AIException(
-                            AIException.ErrorCodes.ServiceError,
-                            $"The service failed to process the request, HTTP status: {response.StatusCode:G}");
-
-                    default:
-                        throw new AIException(
-                            AIException.ErrorCodes.UnknownError,
-                            $"Unexpected HTTP response, status: {response.StatusCode:G}");
-                }
-            }
-        }
-        catch (Exception e) when (e is not AIException)
-        {
-            throw new AIException(
-                AIException.ErrorCodes.UnknownError,
-                $"Something went wrong: {e.Message}", e);
-        }
-
-        try
-        {
-            var result = Json.Deserialize<T>(responseJson);
-            if (result != null) { return result; }
-
-            throw new AIException(
-                AIException.ErrorCodes.InvalidResponseContent,
-                "Response JSON parse error");
-        }
-        catch (Exception e) when (e is not AIException)
-        {
-            throw new AIException(
-                AIException.ErrorCodes.UnknownError,
-                $"Something went wrong: {e.Message}", e);
-        }
-    }
 
     /// <summary>
     /// C# finalizer
