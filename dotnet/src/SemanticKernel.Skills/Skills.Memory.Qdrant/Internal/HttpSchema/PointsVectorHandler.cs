d32bf06c9473391901a6de8732359d9aed0559f5
Tara E. Walker <tara.walker@microsoft.com>
2023-03-24 07:33:34 -0400

    Work in progress: Removed duplicates - Replace with one file for clarity. Adding/Copying functionality back to that file-TEW

--- dotnet/src/SemanticKernel.Skills/Skills.Memory.Qdrant/Internal/HttpSchema/PointsVectorHandler.cs
+++ dotnet/src/SemanticKernel.Skills/Skills.Memory.Qdrant/Internal/HttpSchema/PointsVectorHandler.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.SemanticKernel.Skills.Memory.Qdrant.DataModels;
 using Microsoft.SemanticKernel.Skills.Memory.Qdrant.Diagnostics;
 using Microsoft.SemanticKernel.Skills.Memory.Qdrant.HttpSchema;
+using Micrsoft.SemanticKernel.Skills.Memory.Qdrant.DataModels;
 
 namespace Micrsoft.SemanticKernel.Skills.Memory.Qdrant.HttpSchema;
 
@@ -14,7 +15,7 @@ internal class PointsVectorHandler : IValidatable
     {
         Upsert,
         GetCollectionPoints,
-        GetPoint,
+        GetVectorPoint,
         Delete,
     }
 
@@ -23,8 +24,56 @@ internal class PointsVectorHandler : IValidatable
         Verify.NotNullOrEmpty(this._collectionName, "The collection name is empty");
     }
 
+internal async Task<IQdrantResult> ExecuteRequest(PointVectorHandlerType requestType, string collectionName, string? pointId = null)
+    {
+        IQdrantResult? qdrantResult = null;
+
+        qdrantResult = requestType switch
+        {
+            
+            PointVectorHandlerType.GetVectorPoint =>
+                await this.GetVectorPointAsync(collectionName, pointId!),
+            PointVectorHandlerType.Upsert =>
+                await this.UpsertPointsToCollectionAsync(collectionName),
+            PointVectorHandlerType.GetCollectionPoints =>
+                await this.GetPointsForCollectionAsync(),
+            PointVectorHandlerType.Delete =>
+                await this.DeletePointFromCollectionAsync(collectionName!),
+            _ => throw new ArgumentOutOfRangeException(nameof(requestType), requestType, "The request type is not invalid")
+                
+        };
+
+        return qdrantResult!;
+    }
+
+    internal async Task<IQdrantResult> GetVectorPointAsync(string collectionName, string pointId )
+    {
+        IQdrantResult? getVectorPointResult = null;
+        return getVectorPointResult!;
+
+    }
+
+    internal async Task<IQdrantResult> UpsertPointsToCollectionAsync(string collectionName, string pointId )
+    {
+        IQdrantResult? upsertPointsToCollectionResult = null;
+        return upsertPointsToCollectionResult!;
+
+    }
+
+    internal async Task<IQdrantResult> DeletePointFromCollectionAsync(string collectionName, string pointId )
+    {
+        
+        IQdrantResult? deletePointFromCollectionResult = null;
+        return deletePointFromCollectionResult!;
+
+    }
+
+
+
     #region private ================================================================================
     private string? _collectionName;
+    private HttpClient? _client;
+    private string _vectorPointId;
     #endregion
 
 }
