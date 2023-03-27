9f2f02afb0940793e3c61ce0dca0e149f8685739
Devis Lucato <devis@microsoft.com>
2023-03-27 01:21:39 -0700

    Add Chat with images sample

--- samples/dotnet/kernel-syntax-examples/Example17_ChatGPT.cs
+++ samples/dotnet/kernel-syntax-examples/Example17_ChatGPT.cs
@@ -12,6 +12,7 @@ using RepoUtils;
  */
 
 // ReSharper disable once InconsistentNaming
+// ReSharper disable CommentTypo
 public static class Example17_ChatGPT
 {
     public static async Task RunAsync()
@@ -20,7 +21,7 @@ public static class Example17_ChatGPT
 
         IKernel kernel = new KernelBuilder().WithLogger(ConsoleLogger.Log).Build();
 
-        // Add your text completion backend
+        // Add your chat completion backend
         kernel.Config.AddOpenAIChatGPT("chat", "gpt-3.5-turbo", Env.Var("OPENAI_API_KEY"));
 
         IChatCompletion backend = kernel.GetService<IChatCompletion>();
@@ -47,5 +48,33 @@ public static class Example17_ChatGPT
             Console.WriteLine($"{message.AuthorRole}: {message.Content}");
             Console.WriteLine("------------------------");
         }
+
+        /* Output:
+
+        Chat content:
+        ------------------------
+        system: You are a librarian, expert about books
+        ------------------------
+        user: Hi, I'm looking for book suggestions
+        ------------------------
+        assistant: Sure, I'd be happy to help! What kind of books are you interested in? Fiction or non-fiction? Any particular genre?
+        ------------------------
+        user: I love history and philosophy, I'd like to learn something new about Greece, any suggestion?
+        ------------------------
+        assistant: Great! For history and philosophy books about Greece, here are a few suggestions:
+
+        1. "The Greeks" by H.D.F. Kitto - This is a classic book that provides an overview of ancient Greek history and culture, including their philosophy, literature, and art.
+
+        2. "The Republic" by Plato - This is one of the most famous works of philosophy in the Western world, and it explores the nature of justice and the ideal society.
+
+        3. "The Peloponnesian War" by Thucydides - This is a detailed account of the war between Athens and Sparta in the 5th century BCE, and it provides insight into the political and military strategies of the time.
+
+        4. "The Iliad" by Homer - This epic poem tells the story of the Trojan War and is considered one of the greatest works of literature in the Western canon.
+
+        5. "The Histories" by Herodotus - This is a comprehensive account of the Persian Wars and provides a wealth of information about ancient Greek culture and society.
+
+        I hope these suggestions are helpful!
+        ------------------------
+        */
     }
 }
