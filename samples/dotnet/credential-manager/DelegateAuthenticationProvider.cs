7fefd89130dac8a7679f65a35a35676fcc85b1f7
Gina Triolo <51341242+gitri-ms@users.noreply.github.com>
2023-03-27 23:12:17 -0700

    Mockup of credential manager in console app

---
+++ samples/dotnet/credential-manager/DelegateAuthenticationProvider.cs
@@ -0,0 +1,61 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+namespace CredentialManagerExample;
+
+/// <summary>
+/// Authenticate request async delegate.
+/// </summary>
+/// <param name="request">The <see cref="HttpRequestMessage"/> to authenticate.</param>
+/// <returns></returns>
+public delegate Task AuthenticateRequestAsyncDelegate(HttpRequestMessage request);
+
+/// <summary>
+/// A default <see cref="IAuthenticationProvider"/> implementation.
+/// </summary>
+public class DelegateAuthenticationProvider : IAuthenticationProvider
+{
+    /// <summary>
+    /// Constructs an <see cref="DelegateAuthenticationProvider"/>.
+    /// </summary>
+    public DelegateAuthenticationProvider(AuthenticateRequestAsyncDelegate authenticateRequestAsyncDelegate)
+    {
+        this.AuthenticateRequestAsyncDelegate = authenticateRequestAsyncDelegate;
+    }
+
+    /// <summary>
+    /// Gets or sets the delegate for authenticating requests.
+    /// </summary>
+    public AuthenticateRequestAsyncDelegate AuthenticateRequestAsyncDelegate { get; set; }
+
+    /// <summary>
+    /// Authenticates the specified request message.
+    /// </summary>
+    /// <param name="request">The <see cref="HttpRequestMessage"/> to authenticate.</param>
+    public Task AuthenticateRequestAsync(HttpRequestMessage request)
+    {
+        if (this.AuthenticateRequestAsyncDelegate != null)
+        {
+            return this.AuthenticateRequestAsyncDelegate(request);
+        }
+
+        return Task.FromResult(0);
+    }
+}
+
+//
+// Summary:
+//     Interface for authenticating requests.
+public interface IAuthenticationProvider
+{
+    //
+    // Summary:
+    //     Authenticates the specified request message.
+    //
+    // Parameters:
+    //   request:
+    //     The System.Net.Http.HttpRequestMessage to authenticate.
+    //
+    // Returns:
+    //     The task to await.
+    Task AuthenticateRequestAsync(HttpRequestMessage request);
+}
---
+++ samples/dotnet/credential-manager/Program.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Net.Http.Headers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Identity.Client;
+
+namespace CredentialManagerExample;
+
+public sealed class Program
+{
+    public static async Task Main()
+    {
+        // Using appsettings.json for our configuration settings
+        IConfiguration configuration = new ConfigurationBuilder()
+            .AddJsonFile(path: "appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+
+        PublicClientApplicationOptions appConfiguration = configuration.Get<PublicClientApplicationOptions>()
+                                                          ?? throw new InvalidOperationException("Invalid configuration for public client application."); ;
+
+        var scopes = configuration.GetSection("Scopes").Get<string[]>() ?? throw new InvalidOperationException("Invalid scopes.");
+        
+        var authProvider = CreateAuthenticationProvider(
+            new LocalUserMSALCredentialManager(),
+            appConfiguration.ClientId,
+            appConfiguration.TenantId,
+            scopes,
+            new Uri(appConfiguration.RedirectUri)
+            );
+
+        // Create and authenticate request
+        using var httpClient = new HttpClient();
+        using var requestMessage = new HttpRequestMessage(HttpMethod.Get, "https://graph.microsoft.com/beta/me/profile/");
+        await authProvider.AuthenticateRequestAsync(requestMessage);
+
+        // Send request
+        HttpResponseMessage responseMessage = await httpClient.SendAsync(requestMessage);
+        Console.WriteLine(responseMessage.StatusCode + " " + responseMessage.ReasonPhrase);
+        Console.WriteLine(await responseMessage.Content.ReadAsStringAsync());
+    }
+
+    private static DelegateAuthenticationProvider CreateAuthenticationProvider(
+        LocalUserMSALCredentialManager credentialManager,
+        string clientId,
+        string tenantId,
+        string[] scopes,
+        Uri redirectUri)
+        => new(
+            async (requestMessage) =>
+            {
+                requestMessage.Headers.Authorization = new AuthenticationHeaderValue(
+                    scheme: "bearer",
+                    parameter: await credentialManager.GetTokenAsync(
+                        clientId,
+                        tenantId,
+                        scopes,
+                        redirectUri));
+            });
+}
