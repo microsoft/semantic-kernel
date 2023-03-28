b70d35280ae4756d2b6494c0c48bb01aa9894fbb
Jordan Henkel <jordanhenkel@microsoft.com>
2023-03-28 15:29:33 -0500

    Updating tests

--- python/tests/template_engine/blocks/test_block.py
+++ python/tests/template_engine/blocks/test_block.py
@@ -9,17 +9,18 @@ from semantic_kernel.template_engine_v2.blocks.block_types import BlockTypes
 from semantic_kernel.utils.null_logger import NullLogger
 
 
-class TestBlock:
-    def test_init(self):
-        block = Block(content="test content", log=NullLogger())
-        assert block.content == "test content"
-        assert isinstance(block.log, Logger)
+def test_init():
+    block = Block(content="test content", log=NullLogger())
+    assert block.content == "test content"
+    assert isinstance(block.log, Logger)
 
-    def test_type_property(self):
-        block = Block()
-        assert block.type == BlockTypes.UNDEFINED
 
-    def test_is_valid_not_implemented(self):
-        block = Block()
-        with raises(NotImplementedError):
-            block.is_valid()
+def test_type_property():
+    block = Block()
+    assert block.type == BlockTypes.UNDEFINED
+
+
+def test_is_valid_not_implemented():
+    block = Block()
+    with raises(NotImplementedError):
+        block.is_valid()
