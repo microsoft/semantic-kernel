78d3c9b7d9795deec5895bf658c9915240bd17e1
Jerome Tremblay <jerome_tremblay@nuance.com>
2023-03-29 08:48:08 -0400

    forgot to also add TextSkill to __all__

--- python/semantic_kernel/core_skills/__init__.py
+++ python/semantic_kernel/core_skills/__init__.py
@@ -3,5 +3,6 @@
 from semantic_kernel.core_skills.text_memory_skill import TextMemorySkill
 from semantic_kernel.core_skills.file_io_skill import FileIOSkill
 from semantic_kernel.core_skills.time_skill import TimeSkill
+from semantic_kernel.core_skills.text_skill import TextSkill
 
-__all__ = ["TextMemorySkill", "FileIOSkill", "TimeSkill"]
+__all__ = ["TextMemorySkill", "TextSkill", "FileIOSkill", "TimeSkill"]
