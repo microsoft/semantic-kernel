7054787131cf9ce6ef1413291ceb84fa2cb1cbd6
Jerome Tremblay <jerome_tremblay@nuance.com>
2023-03-29 08:39:07 -0400

    formatting

--- python/semantic_kernel/core_skills/__init__.py
+++ python/semantic_kernel/core_skills/__init__.py
@@ -4,4 +4,4 @@ from semantic_kernel.core_skills.text_memory_skill import TextMemorySkill
 from semantic_kernel.core_skills.file_io_skill import FileIOSkill
 from semantic_kernel.core_skills.time_skill import TimeSkill
 
-__all__ = ["TextMemorySkill","FileIOSkill","TimeSkill"]
+__all__ = ["TextMemorySkill", "FileIOSkill", "TimeSkill"]
