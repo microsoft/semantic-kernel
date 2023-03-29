b75972e226e8f65b305a01d3eef86e8137e86186
Dmytro Struk <dmytrostruk@microsoft.com>
2023-03-29 13:57:25 +0100

    Aligned local server schema to HuggingFace schema

--- samples/apps/local_inference_server/inference_app.py
+++ samples/apps/local_inference_server/inference_app.py
@@ -28,11 +28,33 @@ def embeddings_docs():
 def images_docs():
     return render_template('images.html')
 
-@app.route('/completions', methods=['POST'])
-def receive_completion_request():
+@app.route('/completions/<model>', methods=['POST'])
+def receive_completion_by_model(model):
+    return process_completion_request(request, model)
+
+@app.route('/completions/<organization>/<model>', methods=['POST'])
+def receive_completion_by_organization_model(organization, model):
+    return process_completion_request(request, f'{organization}/{model}')
+
+@app.route('/embeddings/<model>', methods=['POST'])
+def receive_embedding_by_model(model):
+    return process_embedding_request(request, model)
+
+@app.route('/embeddings/<organization>/<model>', methods=['POST'])
+def receive_embedding_by_organization_model(organization, model):
+    return process_embedding_request(request, f'{organization}/{model}')
+
+@app.route('/images/generations/<model>', methods=['POST'])
+def receive_image_generation_by_model(model):
+    return process_image_generation_request(request, model)
+
+@app.route('/images/generations/<organization>/<model>', methods=['POST'])
+def receive_image_generation_by_organization_model(organization, model):
+    return process_image_generation_request(request, f'{organization}/{model}')
+
+def process_completion_request(request, model):
     request_data = request.data
     json_data = json.loads(request_data)
-    requested_model = json_data["model"]
     try:
         prompt = json_data["prompt"]
         if "context" in json_data:
@@ -45,50 +67,45 @@ def receive_completion_request():
         else:
             max_tokens = 32
 
-        inference_generator = CompletionGenerator.CompletionGenerator(requested_model)
+        inference_generator = CompletionGenerator.CompletionGenerator(model)
         result, num_prompt_tokens, num_result_tokens = inference_generator.perform_inference(prompt, context, max_tokens)
         return jsonify(create_responses.create_completion_response(
                         result,
-                        requested_model,
+                        model,
                         num_prompt_tokens,
                         num_result_tokens))
     except Exception as e:
         print(e)
-        return ("Sorry, unable to perform sentence completion with model {}".format(requested_model))
+        return ("Sorry, unable to perform sentence completion with model {}".format(model))
 
-@app.route('/embeddings', methods=['POST'])
-def receive_embedding_request():
+def process_embedding_request(request, model):
     request_data = request.data
     json_data = json.loads(request_data)
-    requested_model = json_data["model"]
     try:
         sentences = json_data["input"]
-        inference_generator = EmbeddingGenerator.EmbeddingGenerator(requested_model)
+        inference_generator = EmbeddingGenerator.EmbeddingGenerator(model)
         embeddings, num_prompt_tokens = inference_generator.perform_inference(sentences)
         return jsonify(create_responses.create_embedding_response(
                         embeddings,
                         num_prompt_tokens))
     except Exception as e:
         print(e)
-        return ("Sorry, unable to generate embeddings with model {}".format(requested_model))
+        return ("Sorry, unable to generate embeddings with model {}".format(model))
 
-@app.route('/images/generations', methods=['POST'])
-def receive_img_generation_request():
+def process_image_generation_request(request, model):
     request_data = request.data
     json_data = json.loads(request_data)
-    requested_model = json_data["model"]
     num_images = json_data["n"]
     prompt = json_data["prompt"]
     image_size = json_data["size"]
     try:
-        image_generator = ImageGenerator.ImageGenerator(requested_model)
+        image_generator = ImageGenerator.ImageGenerator(model)
         image_data = image_generator.perform_inference(prompt, num_images, image_size)
         return jsonify(create_responses.create_image_gen_response(image_data))
     except Exception as e:
         print(e)
-        return ("Sorry, unable to generate images with model {}".format(requested_model))
-        
-    
+        return ("Sorry, unable to generate images with model {}".format(model))
+
 # main driver function
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
