35f2102aa4a70c4c737df9b9a035b26840656429
Gil LaHaye <gillahaye@microsoft.com>
2023-03-30 14:19:20 -0700

    Initial version of service with SK functionality (#219)
    **Motivation and Context**
    Have a server encapsulating SK functionality in a way that can support
    all future skills
    
    **Description**
    - Add config classes
    - Add request classes
    - Configure server
    - Configure DI services
    - Create SK controller
    - Contribution Checklist

---
+++ refapp/SKWebApi/FunctionLoadingExtensions.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.KernelExtensions;
+using Microsoft.SemanticKernel.TemplateEngine;
+
+namespace SemanticKernel.Service;
+
+internal static class FunctionLoadingExtensions
+{
+    internal static void RegisterSemanticSkills(
+        this IKernel kernel,
+        string skillsDirectory,
+        ILogger logger)
+    {
+        string[] subDirectories = Directory.GetDirectories(skillsDirectory);
+
+        foreach (string subDir in subDirectories)
+        {
+            try
+            {
+                kernel.ImportSemanticSkillFromDirectory(skillsDirectory, Path.GetFileName(subDir)!);
+            }
+            catch (TemplateException e)
+            {
+                logger.LogError("Could not load skill from {Directory}: {Message}", subDir, e.Message);
+            }
+        }
+    }
+
+    internal static void RegisterNativeSkills(
+        this IKernel kernel,
+        ILogger logger)
+    {
+        // Hardcode your native function registrations here
+    }
+}
