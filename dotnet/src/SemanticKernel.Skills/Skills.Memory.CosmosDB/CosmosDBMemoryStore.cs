501d9b1bc05fb094caf92c41c25780ed9cb30859
Craig Presti <146438+craigomatic@users.noreply.github.com>
2023-03-31 14:38:36 -0700

    Add CosmosDB implementation of IMemoryStore<float> (#261)
    ### Motivation and Context
    Currently there is not a 1st party persistent memorystore implementation
    for Semantic Kernel. This PR adds that support so that memories can be
    stored in CosmosDB.
    
    ### Description
    * Adds a new project to SemanticKernel.Skills called
    SemanticKernel.Skills.CosmosDB
    * This project contains a class `CosmosMemoryStore` which can be added
    to the kernel during configuration, as the backing store
    
    Example usage:
    
    ```csharp
    var client = new CosmosClient("<your connection string>");
    var db = "<an existing database in CosmosDB>";
    var container = "<an existing container in the database>";
    
    var memoryStore = new CosmosMemoryStore<float>(client, db, container);
    
    var sk = Kernel.Builder
        .WithLogger(NullLogger.Instance)
        .WithMemory(memoryStore)
        .Configure(c =>
        {
           //add your model endpoints here
        })
        .Build();
    ```

---
+++ dotnet/src/SemanticKernel.Skills/Skills.Memory.CosmosDB/CosmosDBMemoryStore.cs
@@ -0,0 +1,225 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.SemanticKernel.AI.Embeddings;
+using Microsoft.SemanticKernel.Memory;
+using Microsoft.SemanticKernel.Memory.Collections;
+using Microsoft.SemanticKernel.Memory.Storage;
+using Newtonsoft.Json;
+
+namespace Skills.Memory.CosmosDB;
+
+/// <summary>
+/// An implementation of <see cref="IMemoryStore{TEmbedding}"/> for Azure Cosmos DB.
+/// </summary>
+/// <remarks>The Embedding data is saved to the Azure Cosmos DB database container specified in the constructor.
+/// The embedding data persists between subsequent instances and has similarity search capability, handled by the client as Azure Cosmos DB is not a vector-native DB.
+/// </remarks>
+public class CosmosDBMemoryStore<TEmbedding> : IMemoryStore<TEmbedding>, IDisposable
+    where TEmbedding : unmanaged
+{
+    private bool _disposedValue;
+
+    private CosmosClient _client;
+    private string _databaseName;
+    private string _containerName;
+    private ILogger? _log;
+
+    /// <summary>
+    /// Constructor for a memory store backed by an Azure Cosmos DB instance.
+    /// </summary>
+    /// <param name="client"></param>
+    /// <param name="databaseName"></param>
+    /// <param name="containerName"></param>
+    /// <param name="log"></param>
+    public CosmosDBMemoryStore(CosmosClient client, string databaseName, string containerName, ILogger? log = null)
+    {
+        this._client = client;
+        this._databaseName = databaseName;
+        this._containerName = containerName;
+        this._log = log ?? NullLogger<CosmosDBMemoryStore<TEmbedding>>.Instance;
+    }
+
+    /// <inheritdoc />
+    public async Task<DataEntry<IEmbeddingWithMetadata<TEmbedding>>?> GetAsync(string collection, string key, CancellationToken cancel = default)
+    {
+        var container = this._client.GetContainer(this._databaseName, this._containerName);
+
+        using (var responseMessage = await container.ReadItemStreamAsync(this._toCosmosFriendlyId(key), new Microsoft.Azure.Cosmos.PartitionKey(collection), cancellationToken: cancel))
+        {
+            if (!responseMessage.IsSuccessStatusCode)
+            {
+                this._log?.LogWarning("Failed to get item {0} from collection {1} with status code {2}", key, collection, responseMessage.StatusCode);
+                return null;
+            }
+
+            using (responseMessage.Content)
+            {
+                CosmosDBMemoryRecord record;
+
+                if (typeof(Stream).IsAssignableFrom(typeof(CosmosDBMemoryRecord)))
+                {
+                    record = ((CosmosDBMemoryRecord)(object)responseMessage.Content);
+                }
+                else
+                {
+                    record = await System.Text.Json.JsonSerializer.DeserializeAsync<CosmosDBMemoryRecord>(responseMessage.Content!, cancellationToken: cancel);
+                }
+
+                var embeddingHost = JsonConvert.DeserializeAnonymousType(
+                    record!.EmbeddingString,
+                    new { Embedding = new { vector = new List<float>() } });
+
+                var rec = MemoryRecord.FromJson(
+                    record.MetadataString,
+                    new Embedding<float>(embeddingHost.Embedding.vector));
+
+                return DataEntry.Create<IEmbeddingWithMetadata<TEmbedding>>(
+                            rec.Metadata.Id,
+                            (IEmbeddingWithMetadata<TEmbedding>)rec,
+                            record.Timestamp);
+            }
+        }
+    }
+
+    /// <inheritdoc />
+    public async IAsyncEnumerable<string> GetCollectionsAsync([EnumeratorCancellation] CancellationToken cancel = default)
+    {
+        var container = this._client.GetContainer(this._databaseName, this._containerName);
+        var query = new QueryDefinition($"SELECT DISTINCT c.collectionId FROM c");
+        var iterator = container.GetItemQueryIterator<CosmosDBMemoryRecord>(query);
+
+        var items = await iterator.ReadNextAsync(cancel).ConfigureAwait(false);
+
+        foreach (var item in items)
+        {
+            yield return item.CollectionId;
+        }
+    }
+
+    /// <inheritdoc />
+    public IAsyncEnumerable<(IEmbeddingWithMetadata<TEmbedding>, double)> GetNearestMatchesAsync(string collection, Embedding<TEmbedding> embedding, int limit = 1, double minRelevanceScore = 0)
+    {
+        if (limit <= 0)
+        {
+            return AsyncEnumerable.Empty<(IEmbeddingWithMetadata<TEmbedding>, double)>();
+        }
+
+        var asyncEmbeddingCollection = this.TryGetCollectionAsync(collection);
+        var embeddingCollection = asyncEmbeddingCollection.ToEnumerable().ToArray();
+
+        if (embeddingCollection == null || !embeddingCollection.Any())
+        {
+            return AsyncEnumerable.Empty<(IEmbeddingWithMetadata<TEmbedding>, double)>();
+        }
+
+        EmbeddingReadOnlySpan<TEmbedding> embeddingSpan = new(embedding.AsReadOnlySpan());
+
+        TopNCollection<IEmbeddingWithMetadata<TEmbedding>> embeddings = new(limit);
+
+        foreach (var item in embeddingCollection)
+        {
+            if (item.Value != null)
+            {
+                EmbeddingReadOnlySpan<TEmbedding> itemSpan = new(item.Value.Embedding.AsReadOnlySpan());
+                double similarity = embeddingSpan.CosineSimilarity(itemSpan);
+                if (similarity >= minRelevanceScore)
+                {
+                    embeddings.Add(new(item.Value, similarity));
+                }
+            }
+        }
+
+        embeddings.SortByScore();
+
+        return embeddings.Select(x => (x.Value, x.Score.Value)).ToAsyncEnumerable();
+    }
+
+    protected async IAsyncEnumerable<DataEntry<IEmbeddingWithMetadata<TEmbedding>>> TryGetCollectionAsync(string collectionName, [EnumeratorCancellation] CancellationToken cancel = default)
+    {
+        var container = this._client.GetContainer(this._databaseName, this._containerName);
+        var query = new QueryDefinition($"SELECT * FROM c WHERE c.collectionId = @collectionName")
+             .WithParameter("@collectionName", collectionName);
+
+        var iterator = container.GetItemQueryIterator<CosmosDBMemoryRecord>(query);
+
+        var items = await iterator.ReadNextAsync(cancel).ConfigureAwait(false);
+
+        foreach (var item in items)
+        {
+            var embeddingHost = JsonConvert.DeserializeAnonymousType(
+                item.EmbeddingString,
+                new { Embedding = new { vector = new List<float>() } });
+
+            var rec = MemoryRecord.FromJson(
+                item.MetadataString,
+                new Embedding<float>(embeddingHost.Embedding.vector));
+
+            yield return DataEntry.Create<IEmbeddingWithMetadata<TEmbedding>>(
+                rec.Metadata.Id,
+                (IEmbeddingWithMetadata<TEmbedding>)rec,
+                item.Timestamp);
+        }
+    }
+
+    /// <inheritdoc />
+    public async Task<DataEntry<IEmbeddingWithMetadata<TEmbedding>>> PutAsync(string collection, DataEntry<IEmbeddingWithMetadata<TEmbedding>> data, CancellationToken cancel = default)
+    {
+        var entity = new CosmosDBMemoryRecord
+        {
+            CollectionId = collection,
+            Id = this._toCosmosFriendlyId(data.Key),
+            Timestamp = data.Timestamp,
+            EmbeddingString = data.ValueString!,
+            MetadataString = data.Value!.GetSerializedMetadata()
+        };
+
+        var container = this._client.GetContainer(this._databaseName, this._containerName);
+
+        await container.UpsertItemAsync(entity, cancellationToken: cancel, requestOptions: new ItemRequestOptions()
+        {
+            EnableContentResponseOnWrite = false,
+        });
+
+        return data;
+    }
+
+    /// <inheritdoc />
+    public Task RemoveAsync(string collection, string key, CancellationToken cancel = default)
+    {
+        var container = this._client.GetContainer(this._databaseName, this._containerName);
+
+        return container.DeleteItemAsync<CosmosDBMemoryRecord>(
+            this._toCosmosFriendlyId(key),
+            new Microsoft.Azure.Cosmos.PartitionKey(collection),
+            cancellationToken: cancel);
+    }
+
+    private string _toCosmosFriendlyId(string id)
+    {
+        return $"{id.Trim().Replace(' ', '-').Replace('/', '_').Replace('\\', '_').Replace('?', '_').Replace('#', '_').ToUpperInvariant()}";
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!this._disposedValue)
+        {
+            this._disposedValue = true;
+        }
+    }
+
+    public void Dispose()
+    {
+        this.Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+}
