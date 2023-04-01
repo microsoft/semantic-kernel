15d0de11a635bcb1757c854263172bee985924e0
Jordan Henkel <jjhenkel@cs.wisc.edu>
2023-04-01 17:42:51 -0500

    Run pre-commit; will be adding isort to linter workflow in separate pr

--- python/semantic_kernel/core_skills/file_io_skill.py
+++ python/semantic_kernel/core_skills/file_io_skill.py
@@ -1,12 +1,9 @@
 import os
 
 import aiofiles
-from semantic_kernel.orchestration.sk_context import SKContext
 
-from semantic_kernel.skill_definition import (
-    sk_function,
-    sk_function_context_parameter,
-)
+from semantic_kernel.orchestration.sk_context import SKContext
+from semantic_kernel.skill_definition import sk_function, sk_function_context_parameter
 
 
 class FileIOSkill:
