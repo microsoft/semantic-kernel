1c6fdda58fb1c32209fa6c2eb96912154da1fbbd
lemiller@microsoft.com <lemiller@microsoft.com>
2023-04-03 13:36:06 -0700

    Rename RunAsync methods to StepAsync in KernelPlanningExtensions
          Summary: This commit renames the RunAsync extension methods in the KernelPlanningExtensions class to StepAsync, to better reflect their functionality of running the next step in a plan asynchronously. The method signatures and documentation comments are also updated accordingly. This change improves the readability and clarity of the code.

--- dotnet/src/SemanticKernel/Planning/KernelPlanningExtensions.cs
+++ dotnet/src/SemanticKernel/Planning/KernelPlanningExtensions.cs
@@ -13,73 +13,73 @@ namespace Microsoft.SemanticKernel.Planning;
 public static class KernelPlanningExtensions
 {
     /// <summary>
-    /// Run a plan asynchronously
+    /// Run the next step in a plan asynchronously
     /// </summary>
     /// <param name="kernel">Kernel instance to use</param>
     /// <param name="plan">Plan to run</param>
     /// <returns>Result of the plan execution</returns>
-    public static Task<Plan> RunAsync(this IKernel kernel, Plan plan)
+    public static Task<Plan> StepAsync(this IKernel kernel, Plan plan)
     {
-        return kernel.RunAsync(plan.State, plan, CancellationToken.None);
+        return kernel.StepAsync(plan.State, plan, CancellationToken.None);
     }
 
     /// <summary>
-    /// Run a plan asynchronously
+    /// Run the next step in a plan asynchronously
     /// </summary>
     /// <param name="kernel">Kernel instance to use</param>
     /// <param name="plan">Plan to run</param>
     /// <param name="cancellationToken">Cancellation token</param>
     /// <returns>Result of the plan execution</returns>
-    public static Task<Plan> RunAsync(this IKernel kernel, Plan plan, CancellationToken cancellationToken)
+    public static Task<Plan> StepAsync(this IKernel kernel, Plan plan, CancellationToken cancellationToken)
     {
-        return kernel.RunAsync(plan.State, plan, cancellationToken);
+        return kernel.StepAsync(plan.State, plan, cancellationToken);
     }
 
     /// <summary>
-    /// Run a plan asynchronously
+    /// Run the next step in a plan asynchronously
     /// </summary>
     /// <param name="kernel">Kernel instance to use</param>
     /// <param name="input">Input to use</param>
     /// <param name="plan">Plan to run</param>
     /// <returns>Result of the plan execution</returns>
-    public static Task<Plan> RunAsync(this IKernel kernel, string input, Plan plan)
+    public static Task<Plan> StepAsync(this IKernel kernel, string input, Plan plan)
     {
-        return kernel.RunAsync(input, plan, CancellationToken.None);
+        return kernel.StepAsync(input, plan, CancellationToken.None);
     }
 
     /// <summary>
-    /// Run a plan asynchronously
+    /// Run the next step in a plan asynchronously
     /// </summary>
     /// <param name="kernel">Kernel instance to use</param>
     /// <param name="input">Input to use</param>
     /// <param name="plan">Plan to run</param>
     /// <param name="cancellationToken">Cancellation token</param>
-    public static Task<Plan> RunAsync(this IKernel kernel, string input, Plan plan, CancellationToken cancellationToken)
+    public static Task<Plan> StepAsync(this IKernel kernel, string input, Plan plan, CancellationToken cancellationToken)
     {
-        return kernel.RunAsync(new ContextVariables(input), plan, cancellationToken);
+        return kernel.StepAsync(new ContextVariables(input), plan, cancellationToken);
     }
 
     /// <summary>
-    /// Run a plan asynchronously
+    /// Run the next step in a plan asynchronously
     /// </summary>
     /// <param name="kernel">Kernel instance to use</param>
     /// <param name="variables">Input to process</param>
     /// <param name="plan">Plan to run</param>
     /// <returns>Result of the plan execution</returns>
-    public static Task<Plan> RunAsync(this IKernel kernel, ContextVariables variables, Plan plan)
+    public static Task<Plan> StepAsync(this IKernel kernel, ContextVariables variables, Plan plan)
     {
-        return kernel.RunAsync(variables, plan, CancellationToken.None);
+        return kernel.StepAsync(variables, plan, CancellationToken.None);
     }
 
     /// <summary>
-    /// Run a plan asynchronously
+    /// Run the next step in a plan asynchronously
     /// </summary>
     /// <param name="kernel">Kernel instance to use</param>
     /// <param name="variables">Input to process</param>
     /// <param name="plan">Plan to run</param>
     /// <param name="cancellationToken">Cancellation token</param>
     /// <returns>Result of the plan execution</returns>
-    public static Task<Plan> RunAsync(this IKernel kernel, ContextVariables variables, Plan plan, CancellationToken cancellationToken)
+    public static Task<Plan> StepAsync(this IKernel kernel, ContextVariables variables, Plan plan, CancellationToken cancellationToken)
     {
         return plan.RunNextStepAsync(kernel, variables, cancellationToken);
     }
