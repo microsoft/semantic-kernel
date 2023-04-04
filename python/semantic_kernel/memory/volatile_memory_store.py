aa85c93164ab227db14407dbc94e7baf327c60da
vivekpandit-msft <82549288+vivekpandit-msft@users.noreply.github.com>
2023-04-03 20:01:56 -0400

    added logger warning and error for cosine similarity computation for … (#203)
    Added logger warning and error for cosine similarity computation for
    zero vectors

--- python/semantic_kernel/memory/volatile_memory_store.py
+++ python/semantic_kernel/memory/volatile_memory_store.py
@@ -1,17 +1,20 @@
 # Copyright (c) Microsoft. All rights reserved.
 
-from typing import List, Tuple
+from logging import Logger
+from typing import List, Optional, Tuple
 
 from numpy import array, linalg, ndarray
 
 from semantic_kernel.memory.memory_record import MemoryRecord
 from semantic_kernel.memory.memory_store_base import MemoryStoreBase
 from semantic_kernel.memory.storage.volatile_data_store import VolatileDataStore
+from semantic_kernel.utils.null_logger import NullLogger
 
 
 class VolatileMemoryStore(VolatileDataStore, MemoryStoreBase):
-    def __init__(self) -> None:
+    def __init__(self, logger: Optional[Logger] = None) -> None:
         super().__init__()
+        self._logger = logger or NullLogger()
 
     async def get_nearest_matches_async(
         self,
@@ -32,10 +35,7 @@ class VolatileMemoryStore(VolatileDataStore, MemoryStoreBase):
 
         # Use numpy to get the cosine similarity between the query
         # embedding and all the embeddings in the collection
-        similarity_scores = (
-            embedding.dot(embedding_array.T)
-            / (linalg.norm(embedding) * linalg.norm(embedding_array, axis=1))
-        )[0]
+        similarity_scores = self.compute_similarity_scores(embedding, embedding_array)
 
         # Then, sort the results by the similarity score
         sorted_results = sorted(
@@ -50,3 +50,44 @@ class VolatileMemoryStore(VolatileDataStore, MemoryStoreBase):
         # Then, take the top N results
         top_results = filtered_results[:limit]
         return top_results
+
+    def compute_similarity_scores(
+        self, embedding: ndarray, embedding_array: ndarray
+    ) -> ndarray:
+        """Compute the similarity scores between the
+        query embedding and all the embeddings in the collection.
+        Ignore the corresponding operation if zero vectors
+        are involved (in query embedding or the embedding collection)
+
+        :param embedding: The query embedding.
+        :param embedding_array: The collection of embeddings.
+        :return: similarity_scores: The similarity scores between the query embedding
+            and all the embeddings in the collection.
+        """
+
+        query_norm = linalg.norm(embedding)
+        collection_norm = linalg.norm(embedding_array, axis=1)
+
+        # Compute indices for which the similarity scores can be computed
+        valid_indices = (query_norm != 0) & (collection_norm != 0)
+
+        # Initialize the similarity scores with -1 to distinguish the cases
+        # between zero similarity from orthogonal vectors and invalid similarity
+        similarity_scores = array([-1.0] * embedding_array.shape[0])
+
+        if valid_indices.any():
+            similarity_scores[valid_indices] = embedding.dot(
+                embedding_array[valid_indices].T
+            ) / (query_norm * collection_norm[valid_indices])
+            if not valid_indices.all():
+                self._logger.warning(
+                    "Some vectors in the embedding collection are zero vectors."
+                    "Ignoring cosine similarity score computation for those vectors."
+                )
+        else:
+            raise ValueError(
+                f"Invalid vectors, cannot compute cosine similarity scores"
+                f"for zero vectors"
+                f"{embedding_array} or {embedding}"
+            )
+        return similarity_scores
