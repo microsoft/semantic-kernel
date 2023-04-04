a06f067e27176f93da659aa507f9cb9fc2ee058a
amsacha <amsacha@microsoft.com>
2023-04-04 10:46:21 -0700

    Fixed no throw literal eslint error in the refapp

--- samples/apps/copilot-chat-app/WebApp/src/libs/semantic-kernel/SemanticKernel.ts
+++ samples/apps/copilot-chat-app/WebApp/src/libs/semantic-kernel/SemanticKernel.ts
@@ -11,13 +11,9 @@ interface ServiceRequest {
 
 export class SemanticKernel {
     // eslint-disable-next-line @typescript-eslint/space-before-function-paren
-    constructor(private readonly serviceUrl: string) { }
+    constructor(private readonly serviceUrl: string) {}
 
-    public invokeAsync = async (
-        ask: IAsk,
-        skillName: string,
-        functionName: string,
-    ): Promise<IAskResult> => {
+    public invokeAsync = async (ask: IAsk, skillName: string, functionName: string): Promise<IAskResult> => {
         const result = await this.getResponseAsync<IAskResult>({
             commandPath: `skills/${skillName}/functions/${functionName}/invoke`,
             method: 'POST',
@@ -34,21 +30,22 @@ export class SemanticKernel {
             const response = await fetch(requestUrl, {
                 method: method ?? 'GET',
                 body: JSON.stringify(body),
-                headers: { "Content-Type": "application/json" }
+                headers: { 'Content-Type': 'application/json' },
             });
 
             if (!response.ok) {
-                throw response.statusText + " => " + await response.text();
+                throw Object.assign(new Error(response.statusText + ' => ' + (await response.text())));
             }
 
             return (await response.json()) as T;
         } catch (e) {
-            var additional_error_msg = ''
+            var additional_error_msg = '';
             if (e instanceof TypeError) {
                 // fetch() will reject with a TypeError when a network error is encountered.
-                additional_error_msg = '\n\nPlease check that your backend is running and that it is accessible by the app'
+                additional_error_msg =
+                    '\n\nPlease check that your backend is running and that it is accessible by the app';
             }
-            throw e + additional_error_msg;
+            throw Object.assign(new Error(e + additional_error_msg));
         }
     };
 }
