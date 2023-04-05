854604747b7bfc84868a471d9d203be6d0915d39
Jerome Tremblay <jerome_tremblay@nuance.com>
2023-04-05 16:18:34 -0400

    Adding __init__ in semantic function and partitioning functions in __all__

---
+++ python/semantic_kernel/semantic_functions/__init__.py
@@ -0,0 +1,15 @@
+# 
+
+from semantic_kernel.semantic_functions.semantic_text_partitioner import (
+    split_plaintext_lines,
+    split_markdown_paragraph,
+    split_plaintext_paragraph,
+    split_markdown_lines,
+)
+
+__all__ = [
+    "split_plaintext_lines",
+    "split_markdown_paragraph",
+    "split_plaintext_paragraph",
+    "split_markdown_lines",
+]
