9fd62c523b601e39b27416372d3b4fab8665311c
Gina Triolo <51341242+gitri-ms@users.noreply.github.com>
2023-04-05 09:50:37 -0700

    Add passthrough token auth provider

---
+++ dotnet/src/SemanticKernel.Skills/Skills.OpenAPI/Authentication/TokenAuthenticationProvider.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Microsoft.SemanticKernel.Skills.OpenAPI.Authentication;
+
+public class TokenAuthenticationProvider
+{
+    private readonly string _accessToken;
+
+    public TokenAuthenticationProvider(string accessToken)
+    {
+        this._accessToken = accessToken;
+    }
+    
+    public Task AuthenticateRequestAsync(HttpRequestMessage request)
+    {
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", this._accessToken);
+        return Task.CompletedTask;
+    }
+}
