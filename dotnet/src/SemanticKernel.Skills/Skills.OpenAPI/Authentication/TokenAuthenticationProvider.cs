28b78d888dd62e295fdbeb4bfd37bd69cf5161f8
Gina Triolo <51341242+gitri-ms@users.noreply.github.com>
2023-04-06 00:22:23 +0000

    OpenAPI passthrough token auth provider (#341)
    ### Motivation and Context
    The OpenAPI skill accepts a callback function as a parameter, to be used
    for adding auth info to HTTP requests before sending. In the simplest
    scenario, the caller would have an access token to add to the requests.
    This change provides a class/function that callers can use to pass a
    pre-acquired token to the HTTP request's auth header, and also serves as
    a reference for future OpenAPI auth providers.
    
    ### Description
    Implement the TokenAuthenticationProvider class. The constructor accepts
    an access token, which is added to the HTTP request message provided
    when calling AuthenticateRequestAsync. The function signature matches
    the signature of the AuthenticateRequestAsyncCallback delegate type.

---
+++ dotnet/src/SemanticKernel.Skills/Skills.OpenAPI/Authentication/TokenAuthenticationProvider.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Microsoft.SemanticKernel.Skills.OpenAPI.Authentication;
+
+/// <summary>
+/// Retrieves an access token via the provided delegate and uses it to authenticate HTTP requests.
+/// This class implements the <see cref="AuthenticateRequestAsyncCallback"/> delegate.
+/// </summary>
+public class TokenAuthenticationProvider
+{
+    private readonly Func<string> _accessToken;
+
+    /// <summary>
+    /// Create an instance of the TokenAuthenticationProvider class.
+    /// </summary>
+    /// <param name="accessToken">Delegate to retrieve the access token.</param>
+    public TokenAuthenticationProvider(Func<string> accessToken)
+    {
+        this._accessToken = accessToken;
+    }
+
+    /// <summary>
+    /// Apply the token to the provided HTTP request message. The signature of this function matches
+    /// the signature of the <see cref="AuthenticateRequestAsyncCallback"/> delegate.
+    /// </summary>
+    /// <param name="request">The HTTP request message.</param>
+    /// <returns></returns>
+    public Task AuthenticateRequestAsync(HttpRequestMessage request)
+    {
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", this._accessToken());
+        return Task.CompletedTask;
+    }
+}
