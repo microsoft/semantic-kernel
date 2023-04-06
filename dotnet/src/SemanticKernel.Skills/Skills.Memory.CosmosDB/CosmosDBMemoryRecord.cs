a7984a63324ab874a2a5a7f38f499f1be978bc7d
Adrian Bonar (HE/HIM) <adribona@microsoft.com>
2023-04-06 08:57:24 -0700

    Updated CopilotChat with PR feedback.

--- dotnet/src/SemanticKernel.Skills/Skills.Memory.CosmosDB/CosmosDBMemoryRecord.cs
+++ dotnet/src/SemanticKernel.Skills/Skills.Memory.CosmosDB/CosmosDBMemoryRecord.cs
@@ -1,9 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
 using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Text;
 using Microsoft.SemanticKernel.AI.Embeddings;
 using Microsoft.SemanticKernel.Memory;
 using Newtonsoft.Json;
@@ -11,18 +8,36 @@ using Newtonsoft.Json.Serialization;
 
 namespace Skills.Memory.CosmosDB;
 
+/// <summary>
+/// A CosmosDB memory record.
+/// </summary>
 [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
 public class CosmosDBMemoryRecord : IEmbeddingWithMetadata<float>
 {
+    /// <summary>
+    /// Unique identifier of the memory record.
+    /// </summary>
     public string Id { get; set; } = string.Empty;
 
+    /// <summary>
+    /// Unique identifier of the collection.
+    /// </summary>
     public string CollectionId { get; set; } = string.Empty;
 
+    /// <summary>
+    /// Optional timestamp.
+    /// </summary>
     public DateTimeOffset? Timestamp { get; set; }
 
+    /// <summary>
+    /// The embedding data.
+    /// </summary>
     [JsonIgnore]
     public Embedding<float> Embedding { get; set; } = new();
 
+    /// <summary>
+    /// The embedding data as a string.
+    /// </summary>
     public string EmbeddingString { get; set; } = string.Empty;
 
     /// <summary>
@@ -31,8 +46,14 @@ public class CosmosDBMemoryRecord : IEmbeddingWithMetadata<float>
     [JsonIgnore]
     public MemoryRecordMetadata? Metadata { get; set; }
 
+    /// <summary>
+    /// Metadata as a string.
+    /// </summary>
     public string MetadataString { get; set; } = string.Empty;
 
+    /// <summary>
+    /// Metadata seriazed as a JSON string.
+    /// </summary>
     public string GetSerializedMetadata()
     {
         return JsonConvert.SerializeObject(this.Metadata);
