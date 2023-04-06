b0bf0408a106839ceeeec47d674741ee74711828
Jordan Henkel <jordanhenkel@microsoft.com>
2023-04-05 19:56:54 -0500

    Cleanup test

--- python/tests/unit/skill_definition/test_sk_function_decorators.py
+++ python/tests/unit/skill_definition/test_sk_function_decorators.py
@@ -1,3 +1,7 @@
+# Copyright (c) Microsoft. All rights reserved.
+
+from pytest import fixture, mark
+
 from semantic_kernel.skill_definition import sk_function
 
 
@@ -95,88 +99,40 @@ class MiscClass:
         """
 
 
-def test_description():
-    decorator_test = MiscClass()
-    my_func = getattr(decorator_test, "func_with_description")
-    assert my_func.__sk_function_description__ == "description"
+@fixture
+def decorator_test():
+    return MiscClass()
 
 
-def test_sk_function_name_not_specified():
-    decorator_test = MiscClass()
-    my_func = getattr(decorator_test, "func_no_name")
-    assert my_func.__sk_function_name__ == "func_no_name"
+@mark.parametrize(
+    "func_name, expected",
+    [
+        ("func_with_description", "description"),
+        ("doc_string_test_1", "A multi-line docstring."),
+        ("doc_string_test_2", "This is a multi-line docstring too"),
+        ("doc_string_test_3", "This is a single-line docstring."),
+        ("doc_string_test_4", "This is a single-line docstring."),
+        ("doc_string_test_5", "Forms a complex number"),
+        ("doc_string_test_6", "Forms a complex number"),
+        ("doc_string_test_7", ""),
+        ("doc_string_test_8", ""),
+        ("doc_string_test_9", ""),
+        ("doc_string_test_10", "What if I have a leading empty line?"),
+        ("doc_string_test_11", "What about this sort of construct it's like"),
+    ],
+)
+def test_description(decorator_test, func_name, expected):
+    my_func = getattr(decorator_test, func_name)
+    assert my_func.__sk_function_description__ == expected
 
 
-def test_sk_function_with_name_specified():
-    decorator_test = MiscClass()
-    my_func = getattr(decorator_test, "func_with_name")
-    assert my_func.__sk_function_name__ == "my-name"
-
-
-def test_doc_string_test_1():
-    decorator_test = MiscClass()
-    my_func = getattr(decorator_test, "doc_string_test_1")
-    assert my_func.__sk_function_description__ == "A multi-line docstring."
-
-
-def test_doc_string_test_2():
-    decorator_test = MiscClass()
-    my_func = getattr(decorator_test, "doc_string_test_2")
-    assert my_func.__sk_function_description__ == "This is a multi-line docstring too"
-
-
-def test_doc_string_test_3():
-    decorator_test = MiscClass()
-    my_func = getattr(decorator_test, "doc_string_test_3")
-    assert my_func.__sk_function_description__ == "This is a single-line docstring."
-
-
-def test_doc_string_test_4():
-    decorator_test = MiscClass()
-    my_func = getattr(decorator_test, "doc_string_test_4")
-    assert my_func.__sk_function_description__ == "This is a single-line docstring."
-
-
-def test_doc_string_test_5():
-    decorator_test = MiscClass()
-    my_func = getattr(decorator_test, "doc_string_test_5")
-    assert my_func.__sk_function_description__ == "Forms a complex number"
-
-
-def test_doc_string_test_6():
-    decorator_test = MiscClass()
-    my_func = getattr(decorator_test, "doc_string_test_6")
-    assert my_func.__sk_function_description__ == "Forms a complex number"
-
-
-def test_doc_string_test_7():
-    decorator_test = MiscClass()
-    my_func = getattr(decorator_test, "doc_string_test_7")
-    assert my_func.__sk_function_description__ == ""
-
-
-def test_doc_string_test_8():
-    decorator_test = MiscClass()
-    my_func = getattr(decorator_test, "doc_string_test_8")
-    assert my_func.__sk_function_description__ == ""
-
-
-def test_doc_string_test_9():
-    decorator_test = MiscClass()
-    my_func = getattr(decorator_test, "doc_string_test_9")
-    assert my_func.__sk_function_description__ == ""
-
-
-def test_doc_string_test_10():
-    decorator_test = MiscClass()
-    my_func = getattr(decorator_test, "doc_string_test_10")
-    assert my_func.__sk_function_description__ == "What if I have a leading empty line?"
-
-
-def test_doc_string_test_11():
-    decorator_test = MiscClass()
-    my_func = getattr(decorator_test, "doc_string_test_11")
-    assert (
-        my_func.__sk_function_description__
-        == "What about this sort of construct it's like"
-    )
+@mark.parametrize(
+    "func_name, expected",
+    [
+        ("func_no_name", "func_no_name"),
+        ("func_with_name", "my-name"),
+    ],
+)
+def test_sk_function_name(decorator_test, func_name, expected):
+    my_func = getattr(decorator_test, func_name)
+    assert my_func.__sk_function_name__ == expected
