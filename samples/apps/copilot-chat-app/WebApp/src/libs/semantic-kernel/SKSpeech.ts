7ddae1574c7bfe4c7362d3b45770c5f665724625
amsacha <amsacha@microsoft.com>
2023-04-07 16:46:23 -0700

    Speech works if you use a subscription key

--- samples/apps/copilot-chat-app/WebApp/src/libs/semantic-kernel/SKSpeech.ts
+++ samples/apps/copilot-chat-app/WebApp/src/libs/semantic-kernel/SKSpeech.ts
@@ -2,7 +2,6 @@
 
 import * as speechSdk from 'microsoft-cognitiveservices-speech-sdk';
 // import speechkeys from './../../../webapp.development.json';
-import fs from 'fs';
 
 //fetchRequestAsync is a function in sk-vnext\apps\InfiniteChat\apps\chatbot-v3\src\libs\semantic-kernel\service\SKService.ts, that i need to map here somehow
 // const getAzureSpeechTokenAsync = async () => {
@@ -14,6 +13,14 @@ import fs from 'fs';
 //     return (await response.json()) as { token: string; region: string };
 // };
 
+// async function getKeysFromJSON() {
+//     return fetch('/website/MyJsonFile.json')
+//         .then((response) => response.json())
+//         .then((responseJson) => {
+//             return responseJson;
+//         });
+// }
+
 const getSpeechRecognizerAsync = async () => {
     //
     // const response = await getAzureSpeechTokenAsync();
@@ -21,12 +28,21 @@ const getSpeechRecognizerAsync = async () => {
     // const speechConfig = speechSdk.SpeechConfig.fromAuthorizationToken(token, region);
 
     // resource id: subscriptions/5b742c40-bc2b-4a4f-902f-ee9f644d8844/resourceGroups/lightspeed-team/providers/Microsoft.CognitiveServices/accounts/lightspeed-speech-recognition
+    // const config = JSON.parse(fs.readFileSync('./../../../webapp.development.json', 'utf-8')); // Read and Parse the JSON content
+    // var subscriptionKey = config[0].AzureSpeechSubscriptionKey; //'YourSubscriptionKey';
+    // var serviceRegion = config[0].AzureSpeechRegion; // e.g., "westus"
 
     //TODO: figure out a way to not need the key directly
 
-    const config = JSON.parse(fs.readFileSync('./../../../webapp.development.json', 'utf-8')); // Read and Parse the JSON content
-    var subscriptionKey = config[0].AzureSpeechSubscriptionKey; //'YourSubscriptionKey';
-    var serviceRegion = config[0].AzureSpeechRegion; // e.g., "westus"
+    // Fetch the JSON file
+
+    // var subscriptionKey, serviceRegion;
+    // // const config = await getKeysFromJSON();
+    // subscriptionKey = config[0].AzureSpeechSubscriptionKey; //'YourSubscriptionKey';
+    // serviceRegion = config[0].AzureSpeechRegion; // e.g., "westus"
+
+    //var subscriptionKey = ''; //'YourSubscriptionKey';
+    var serviceRegion = 'westus2'; // e.g., "westus"
 
     const speechConfig = speechSdk.SpeechConfig.fromSubscription(subscriptionKey, serviceRegion);
     speechConfig.speechRecognitionLanguage = 'en-US';
