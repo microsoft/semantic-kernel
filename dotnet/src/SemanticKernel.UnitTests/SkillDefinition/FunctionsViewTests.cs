39cc75d3e91a152d44fa999ee6457c4afc4f1c9c
Devis Lucato <devis@microsoft.com>
2023-04-11 16:22:45 -0700

    Code style fixes, bump version

--- dotnet/src/SemanticKernel.UnitTests/SkillDefinition/FunctionsViewTests.cs
+++ dotnet/src/SemanticKernel.UnitTests/SkillDefinition/FunctionsViewTests.cs
@@ -3,7 +3,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Runtime;
 using Microsoft.SemanticKernel.SkillDefinition;
 using Xunit;
 
--- dotnet/src/SemanticKernel/SkillDefinition/FunctionsView.cs
+++ dotnet/src/SemanticKernel/SkillDefinition/FunctionsView.cs
@@ -5,7 +5,6 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Runtime;
 
 namespace Microsoft.SemanticKernel.SkillDefinition;
 
