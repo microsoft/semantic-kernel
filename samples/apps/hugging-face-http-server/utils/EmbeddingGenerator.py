a11ab6366a8c48edc12358f30a1b909628ae66f0
Devis Lucato <dluc@users.noreply.github.com>
2023-04-12 10:43:01 -0700

    Code style and docs

--- samples/apps/hugging-face-http-server/utils/EmbeddingGenerator.py
+++ samples/apps/hugging-face-http-server/utils/EmbeddingGenerator.py
@@ -1,8 +1,10 @@
 # Copyright (c) Microsoft. All rights reserved.
 
 import torch
-from . import InferenceGenerator
 from transformers import AutoModel, AutoTokenizer
+
+from . import InferenceGenerator
+
 
 class EmbeddingGenerator(InferenceGenerator.InferenceGenerator):
     def __init__(self, model_name):
@@ -11,22 +13,22 @@ class EmbeddingGenerator(InferenceGenerator.InferenceGenerator):
         self.tokenizer.pad_token = self.tokenizer.eos_token
 
     def _mean_pooling(self, model_output, attention_mask):
-        token_embeddings = model_output[0] #First element of model_output contains all token embeddings
+        token_embeddings = model_output[
+            0
+        ]  # First element of model_output contains all token embeddings
         input_mask_expanded = attention_mask.unsqueeze(-1).float()
         x = torch.sum(token_embeddings * input_mask_expanded, 1)
         y = torch.clamp(input_mask_expanded.sum(1), min=1e-9)
-        return (x / y)
+        return x / y
 
     def perform_inference(self, sentences):
         model = AutoModel.from_pretrained(self.model_name)
         model.to(self.device)
 
         encodings = self.tokenizer(
-            sentences,
-            padding = True,
-            truncation = True,
-            return_tensors= 'pt')
+            sentences, padding=True, truncation=True, return_tensors="pt"
+        )
 
         model_output = model(**encodings)
-        embeddings = self._mean_pooling(model_output, encodings['attention_mask'])
+        embeddings = self._mean_pooling(model_output, encodings["attention_mask"])
         return embeddings, encodings.input_ids.numel()
