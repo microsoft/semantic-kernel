f275dd5494dae3647637ff26cfa09eedf839c34f
Vivien Hung <vivihung@microsoft.com>
2023-04-11 17:47:54 -0700

    Update folder naming convention

--- samples/apps/copilot-chat-app/WebApp/src/components/chat/ChatInput.tsx
+++ samples/apps/copilot-chat-app/WebApp/src/components/chat/ChatInput.tsx
@@ -8,7 +8,7 @@ import { Constants } from '../../Constants';
 import { AlertType } from '../../libs/models/AlertType';
 import { useAppDispatch } from '../../redux/app/hooks';
 import { setAlert } from '../../redux/features/app/appSlice';
-import { TypingIndicatorRenderer } from './TypingIndicator/TypingIndicatorRenderer';
+import { TypingIndicatorRenderer } from './typing-indicator/TypingIndicatorRenderer';
 
 const log = debug(Constants.debug.root).extend('chat-input');
 
