ccb9c26039f5562be22242354a5639116ff5ada5
Teresa Hoang <125500434+teresaqhoang@users.noreply.github.com>
2023-04-12 18:33:17 -0700

    Acquire token for connectors and server (#413)
    ### Motivation and Context
    This PR adds support to acquire an access token using MSAL. It passes
    this under a new header when making SK server requests, and the user ID
    token is also added under the 'Authorization' header.
    
    ### Description
    This implementation follows the 'incremental consent' concept, where the
    User will only be prompted for 'openid', 'offline_access', 'profile',
    and 'User.Read' consent on login. Further permissions will require
    additional consent, in which the user will be prompted when a request
    for the token is made.
    
    If consent is granted and user is authorized, the access token will be
    passed to the server under this header:
    `sk-copilot-connector-access-token`. The `Authorization` header is
    reserved for the SK Access token.
    
    To invoke skills with tokens, do the following (see comments in useChat
    for example):
    
    1. `import { useConnectors } from '{path}/connectors/useConnectors';`
    2. In component: `const connectors = useConnectors();`
    3. Invoke Skill with scopes required for connector as an array, i.e.
    `const scopes = ['User.Read']`
    To use Graph token specifically, comment out the scopes you need under
    `msGraphScopes` in `Constants.ts`, then call the i.e.,
    `invokeSkillWithGraphToken` function. Be default, the ones already
    uncommented map to Graph APIs used in existing connectors.
    `var result = await connectors.invokeSkillWithGraphToken(ask,
    'ChatSkill', 'Chat');`
    To use ADO token specifically, comment out the scopes you need under
    `adoScopes` in `Constants.ts`, then call the `invokeSkillWithAdoToken`
    function.
    i.e., `var result = await connectors.invokeSkillWithAdoToken(ask,
    'ChatSkill', 'Chat');`
    
    
    + Error Handling

--- samples/apps/copilot-chat-app/WebApp/src/redux/features/app/AppState.ts
+++ samples/apps/copilot-chat-app/WebApp/src/redux/features/app/AppState.ts
@@ -2,8 +2,14 @@ import { AlertType } from '../../../libs/models/AlertType';
 
 // Copyright (c) Microsoft. All rights reserved.
 export interface AppState {
-    alert?: {
-        message: string;
-        type: AlertType;
-    };
+    alerts?: Alerts;
+    unclaimed?: boolean;
+    documentId?: string;
 }
+
+export type Alert = {
+    message: string;
+    type: AlertType;
+};
+
+export type Alerts = { [key: string]: Alert };
