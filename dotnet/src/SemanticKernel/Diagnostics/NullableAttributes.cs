4cc73f2b22b62a3c6e166b8c0af8cf6f6d4cc914
Abby Harrison <abby.harrison@microsoft.com>
2023-04-12 20:05:36 -0700

    copied NullableAttributes from main

--- dotnet/src/SemanticKernel/Diagnostics/NullableAttributes.cs
+++ dotnet/src/SemanticKernel/Diagnostics/NullableAttributes.cs
@@ -1,4 +1,6 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
 
 // This was copied from https://github.com/dotnet/runtime/blob/39b9607807f29e48cae4652cd74735182b31182e/src/libraries/System.Private.CoreLib/src/System/Diagnostics/CodeAnalysis/NullableAttributes.cs
 // and updated to have the scope of the attributes be internal.
