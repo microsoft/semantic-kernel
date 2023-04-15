c326f008977e60fb1a36459392ed3eb27f6c3a27
Adrian Bonar (HE/HIM) <adribona@microsoft.com>
2023-04-15 13:26:50 -0700

    wip

---
+++ samples/apps/copilot-chat-app/webapi/Config/ServiceConfig.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.ComponentModel.DataAnnotations;
+
+namespace SemanticKernel.Service.Config;
+
+public class ServiceConfig
+{
+    [Range(0, 65535, ErrorMessage="Service port out of range.")]
+    public int Port { get; set; }
+    public bool UseHttp { get; set; }
+    public string KeyVaultUri { get; set; } = string.Empty;
+    public string SemanticSkillsDirectory { get; set; } = string.Empty;
+
+    public string AllowedHosts { get; set; } = string.Empty;
+    public IEnumerable<string> AllowedOrigins { get; set; } = Array.Empty<string>();
+
+    public void Validate()
+    {
+        if (this.Port == default(int))
+        {
+            throw new ArgumentException(nameof(this.Port), $"{nameof(this.Port)} is not set.");
+        }
+
+        if (string.IsNullOrWhiteSpace(this.KeyVaultUri))
+        {
+            throw new ArgumentException(nameof(this.KeyVaultUri), $"{nameof(this.KeyVaultUri)} is not set.");
+        }
+
+        if (string.IsNullOrWhiteSpace(this.SemanticSkillsDirectory))
+        {
+            throw new ArgumentException(nameof(this.SemanticSkillsDirectory), $"{nameof(this.KeyVaultUri)} is not set.");
+        }
+
+        if (string.IsNullOrWhiteSpace(this.AllowedHosts))
+        {
+            throw new ArgumentException(nameof(this.AllowedHosts), $"{nameof(this.AllowedHosts)} is not set.");
+        }
+    }
+}
