3b8dc18223c3f3cf271fc9cbd617f87693677b2d
Shawn Callegari <36091529+shawncal@users.noreply.github.com>
2023-04-16 23:54:00 -0700

    Azure Functions AI Plugins Server (#475)
    Initial commit.

---
+++ samples/dotnet/AIPluginsServer/AIPlugins.AzureFunctions.Extensions/IAIPluginRunner.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace AIPlugins.AzureFunctions.Extensions;
+public interface IAIPluginRunner
+{
+    public Task<HttpResponseData> RunAIPluginOperationAsync(HttpRequestData req, string operationId);
+}
