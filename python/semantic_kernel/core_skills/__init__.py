bbd13c7091be9bbfc1d13d26903beea20e73424e
Jerome Tremblay <jerome.tremblay@nuance.com>
2023-04-17 21:22:09 -0400

    Adding HttpSkill to __init__ __all__

--- python/semantic_kernel/core_skills/__init__.py
+++ python/semantic_kernel/core_skills/__init__.py
@@ -4,5 +4,6 @@ from semantic_kernel.core_skills.file_io_skill import FileIOSkill
 from semantic_kernel.core_skills.text_memory_skill import TextMemorySkill
 from semantic_kernel.core_skills.text_skill import TextSkill
 from semantic_kernel.core_skills.time_skill import TimeSkill
+from semantic_kernel.core_skills.http_skill import HttpSkill
 
-__all__ = ["TextMemorySkill", "TextSkill", "FileIOSkill", "TimeSkill"]
+__all__ = ["TextMemorySkill", "TextSkill", "FileIOSkill", "TimeSkill", "HttpSkill"]
