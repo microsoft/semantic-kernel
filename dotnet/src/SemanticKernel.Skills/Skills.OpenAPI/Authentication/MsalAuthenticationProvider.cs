72a87028c81aef55f7b0e4aa1d810d340ec7d87c
Gina Triolo <51341242+gitri-ms@users.noreply.github.com>
2023-04-19 13:46:51 -0700

    Documentation comments

--- dotnet/src/SemanticKernel.Skills/Skills.OpenAPI/Authentication/MsalAuthenticationProvider.cs
+++ dotnet/src/SemanticKernel.Skills/Skills.OpenAPI/Authentication/MsalAuthenticationProvider.cs
@@ -9,24 +9,33 @@ using Microsoft.Identity.Client;
 namespace Microsoft.SemanticKernel.Skills.OpenAPI.Authentication;
 
 /// <summary>
-/// 
+/// Uses the Microsoft Authentication Library (MSAL) to authenticate HTTP requests.
 /// </summary>
 public class MsalAuthenticationProvider : BearerAuthenticationProvider
 {
 
     /// <summary>
-    /// 
+    /// Creates an instance of the <see cref="MsalAuthenticationProvider"/> class.
     /// </summary>
-    /// <param name="clientId"></param>
-    /// <param name="tenantId"></param>
-    /// <param name="scopes"></param>
-    /// <param name="resourceUri"></param>
-    /// <param name="redirectUri"></param>
+    /// <param name="clientId">Client ID of the caller.</param>
+    /// <param name="tenantId">Tenant ID of the target resource.</param>
+    /// <param name="scopes">Requested scopes.</param>
+    /// <param name="resourceUri">URI for the target resource.</param>
+    /// <param name="redirectUri">Redirect URI.</param>
     public MsalAuthenticationProvider(string clientId, string tenantId, string[] scopes, Uri resourceUri, Uri redirectUri)
         : base(async () => { return await GetTokenAsync(clientId, tenantId, scopes, resourceUri, redirectUri); })
     {
     }
 
+    /// <summary>
+    /// Gets an access token using the Microsoft Authentication Library (MSAL).
+    /// </summary>
+    /// <param name="clientId">Client ID of the caller.</param>
+    /// <param name="tenantId">Tenant ID of the target resource.</param>
+    /// <param name="scopes">Requested scopes.</param>
+    /// <param name="resourceUri">URI for the target resource.</param>
+    /// <param name="redirectUri">Redirect URI.</param>
+    /// <returns>Access token.</returns>
     private static async Task<string> GetTokenAsync(string clientId, string tenantId, string[] scopes, Uri resourceUri, Uri redirectUri)
     {
         // Prefix scopes with the resource URI
