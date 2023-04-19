506c37f3b2d58ce95d6faad921ae6cb20306d4a8
Adrian Bonar (HE/HIM) <adribona@microsoft.com>
2023-04-18 18:18:32 -0700

    Add error propagation for invalid completion configuration

--- samples/apps/copilot-chat-app/webapi/Controllers/SemanticKernelController.cs
+++ samples/apps/copilot-chat-app/webapi/Controllers/SemanticKernelController.cs
@@ -85,15 +85,9 @@ public class SemanticKernelController : ControllerBase
         SKContext result = await kernel.RunAsync(contextVariables, function!);
         if (result.ErrorOccurred)
         {
-            // TODO latest NuGets don't have the Detail property on AIException
-            //if (result.LastException is AIException aiException && aiException.Detail is not null)
-            //{
-            //    return this.BadRequest(string.Concat(aiException.Message, " - Detail: " + aiException.Detail));
-            //}
-
-            if (result.LastException is AIException aiException)
+            if (result.LastException is AIException aiException && aiException.Detail is not null)
             {
-                return this.BadRequest(aiException.Message);
+                return this.BadRequest(string.Concat(aiException.Message, " - Detail: " + aiException.Detail));
             }
 
             return this.BadRequest(result.LastErrorDescription);
