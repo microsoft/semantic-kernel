5125349c67acb8b873acad8da311b0a066ba5557
Stephen Toub <stoub@microsoft.com>
2023-04-20 07:00:34 -0400

    Update WebFileDownloadSkill.cs

--- dotnet/src/SemanticKernel.Skills/Skills.Web/WebFileDownloadSkill.cs
+++ dotnet/src/SemanticKernel.Skills/Skills.Web/WebFileDownloadSkill.cs
@@ -76,7 +76,7 @@ public class WebFileDownloadSkill : IDisposable
         using Stream webStream = await response.Content.ReadAsStreamAsync();
         using FileStream outputFileStream = new(Environment.ExpandEnvironmentVariables(filePath), FileMode.Create);
 
-        await webStream.CopyToAsync(outputFileStream, 81920, cancellationToken: context.CancellationToken);
+        await webStream.CopyToAsync(outputFileStream, 81920 /*same value used by default*/, cancellationToken: context.CancellationToken);
     }
 
     /// <summary>
