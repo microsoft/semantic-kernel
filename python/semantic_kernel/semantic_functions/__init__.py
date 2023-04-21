9d1387881d9912975e2c5d38b7495d15c2133f1e
Jerome Tremblay <jerome.tremblay@nuance.com>
2023-04-20 20:04:12 -0400

    isort

--- python/semantic_kernel/semantic_functions/__init__.py
+++ python/semantic_kernel/semantic_functions/__init__.py
@@ -1,13 +1,13 @@
 # Copyright (c) Microsoft. All rights reserved.
 
+from semantic_kernel.semantic_functions.function_extension import (
+    aggregate_chunked_results_async,
+)
 from semantic_kernel.semantic_functions.text_chunker import (
     split_markdown_lines,
     split_markdown_paragraph,
     split_plaintext_lines,
     split_plaintext_paragraph,
-)
-from semantic_kernel.semantic_functions.function_extension import (
-    aggregate_chunked_results_async,
 )
 
 __all__ = [
