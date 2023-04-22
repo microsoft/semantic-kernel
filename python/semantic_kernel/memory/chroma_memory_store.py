8b2e2316bf3a096f8ae69875715f582f5a894042
joowon-dm-snu <joowon.kim@dm.snu.ac.kr>
2023-04-22 20:40:00 +0900

    feat: update chroma

--- python/semantic_kernel/memory/chroma_memory_store.py
+++ python/semantic_kernel/memory/chroma_memory_store.py
@@ -1,9 +1,20 @@
 # Copyright (c) Microsoft. All rights reserved.
 
-from logging import Logger
-from typing import TYPE_CHECKING, Callable, List, Optional, Tuple
+"""
+3 cases
 
-from numpy import ndarray
+Case 1) use semantic kernel's default compute similarity function
+Case 2) use chroma's nearest neighbor search
+Case 3) use custom similarity function
+
+
+"""
+
+import inspect
+from logging import Logger
+from typing import TYPE_CHECKING, Callable, List, Optional, Tuple, Union
+
+from numpy import array, linalg, ndarray
 from semantic_kernel.memory.memory_record import MemoryRecord
 from semantic_kernel.memory.memory_store_base import MemoryStoreBase
 from semantic_kernel.memory.storage.chroma_data_store import ChromaDataStore
@@ -12,24 +23,47 @@ from semantic_kernel.utils.null_logger import NullLogger
 if TYPE_CHECKING:
     import chromadb.config
 
-ComputeSimilarityFuncType = Callable[[ndarray, ndarray], ndarray]
+AvailableComputeSimilarityFunction = Union[str, Callable[[ndarray, ndarray], ndarray]]
+DEFAULT_COMPUTE_SIMILARITY_FUNCTIONS = ["sk-default", "chroma"]
+
+
+def validate_similarity_function(func) -> bool:
+    if func in DEFAULT_COMPUTE_SIMILARITY_FUNCTIONS:
+        return True
+    else:
+        # validate typing for custom compute_similarity function
+        # Callable[[ndarray, ndarray], ndarray]
+        param_a = inspect.Parameter(
+            "a", inspect.Parameter.POSITIONAL_OR_KEYWORD, annotation=ndarray
+        )
+        param_b = inspect.Parameter(
+            "b", inspect.Parameter.POSITIONAL_OR_KEYWORD, annotation=ndarray
+        )
+        expected_signature = inspect.Signature(
+            [param_a, param_b], return_annotation=ndarray
+        )
+        function_signature = inspect.signature(func)
+
+        return function_signature == expected_signature
 
 
 class ChromaMemoryStore(ChromaDataStore, MemoryStoreBase):
     def __init__(
         self,
         logger: Optional[Logger] = None,
-        compute_similarity_fetch_limit: int = 10,
-        compute_similarity_scores_func: ComputeSimilarityFuncType = None,
+        similarity_fetch_limit: int = 5,
+        similarity_compute_func: AvailableComputeSimilarityFunction = "sk-default",
         persist_directory: Optional[str] = None,
         client_settings: Optional["chromadb.config.Settings"] = None,
     ) -> None:
+        assert validate_similarity_function(similarity_compute_func)
+        self._similarity_compute_func = similarity_compute_func
+        self._similarity_fetch_limit = similarity_fetch_limit
+
         super().__init__(
             persist_directory=persist_directory, client_settings=client_settings
         )
         self._logger = logger or NullLogger()
-        self._compute_similarity_fetch_limit = compute_similarity_fetch_limit
-        self._compute_similarity_scores_func = compute_similarity_scores_func
 
     async def get_nearest_matches_async(
         self,
@@ -42,38 +76,50 @@ class ChromaMemoryStore(ChromaDataStore, MemoryStoreBase):
         if collection is None:
             return []
 
-        # use Chroma nearest neighbor search if no similarity function is provided
-        if self._compute_similarity_scores_func is None:
-            query_results = collection.query(
-                query_embeddings=embedding.tolist(),
-                n_results=limit,
-                include=["embeddings", "metadatas", "documents", "distances"],
+        query_results = collection.query(
+            query_embeddings=embedding.tolist(),
+            n_results=self._similarity_fetch_limit,
+            include=["embeddings", "metadatas", "documents", "distances"],
+        )
+
+        # Convert the collection of embeddings into a numpy array (stacked)
+        embedding_array = array(query_results["embeddings"][0])
+        embedding_array = embedding_array.reshape(embedding_array.shape[0], -1)
+
+        # If the query embedding has shape (1, embedding_size),
+        # reshape it to (embedding_size,)
+        if len(embedding.shape) == 2:
+            embedding = embedding.reshape(
+                embedding.shape[1],
             )
+
+        # Compute similarity scores
+        if self._similarity_compute_func == "sk-default":
+            # Case 1) use semantic kernel's default compute similarity function
+            similarity_score = self.compute_similarity_scores(
+                embedding, embedding_array
+            )
+        elif self._similarity_compute_func == "chroma":
+            # Case 2) use chroma's default distance
+            similarity_score = query_results["distances"][0]
         else:
-            query_results = collection.get(
-                include=["embeddings", "metadatas", "documents"],
-            )
-            # dimension compatibility with Chroma's query results
-            query_results["ids"] = [query_results["ids"]]
-            query_results["documents"] = [query_results["documents"]]
-            query_results["metadatas"] = [query_results["metadatas"]]
+            # Case 3) use custom similarity function
+            similarity_score = self._similarity_compute_func(embedding, embedding_array)
 
-            # compute similarity scores using the custom function
-            query_results["distances"] = self._compute_similarity_scores_func(
-                embedding, query_results["embeddings"]
-            )
-
-        # Convert Chroma query results into a list of MemoryRecords
+        # Convert query results into memory records
         record_list = [
             (record, distance)
             for record, distance in zip(
                 self.query_results_to_memory_records(query_results),
-                query_results["distances"][0],
+                similarity_score,
             )
         ]
 
-        if self._compute_similarity_scores_func is not None:
-            # Then, sort the results by the similarity score
+        if self._similarity_compute_func == "chroma":
+            # default chroma uses distance as similarity score (lower is better)
+            filtered_results = [x for x in record_list if x[1] <= min_relevance_score]
+            top_results = filtered_results[:limit]
+        else:
             sorted_results = sorted(
                 record_list,
                 key=lambda x: x[1],
@@ -84,7 +130,50 @@ class ChromaMemoryStore(ChromaDataStore, MemoryStoreBase):
                 x for x in sorted_results if x[1] >= min_relevance_score
             ]
             top_results = filtered_results[:limit]
-        else:
-            top_results = record_list[:limit]
 
         return top_results
+
+    def compute_similarity_scores(
+        self, embedding: ndarray, embedding_array: ndarray
+    ) -> ndarray:
+        """
+        Semantic kernel's default compute similarity function.
+        (Code from VolatileMemoryStore)
+
+        Compute the similarity scores between the
+        query embedding and all the embeddings in the collection.
+        Ignore the corresponding operation if zero vectors
+        are involved (in query embedding or the embedding collection)
+
+        :param embedding: The query embedding.
+        :param embedding_array: The collection of embeddings.
+        :return: similarity_scores: The similarity scores between the query embedding
+            and all the embeddings in the collection.
+        """
+
+        query_norm = linalg.norm(embedding)
+        collection_norm = linalg.norm(embedding_array, axis=1)
+
+        # Compute indices for which the similarity scores can be computed
+        valid_indices = (query_norm != 0) & (collection_norm != 0)
+
+        # Initialize the similarity scores with -1 to distinguish the cases
+        # between zero similarity from orthogonal vectors and invalid similarity
+        similarity_scores = array([-1.0] * embedding_array.shape[0])
+
+        if valid_indices.any():
+            similarity_scores[valid_indices] = embedding.dot(
+                embedding_array[valid_indices].T
+            ) / (query_norm * collection_norm[valid_indices])
+            if not valid_indices.all():
+                self._logger.warning(
+                    "Some vectors in the embedding collection are zero vectors."
+                    "Ignoring cosine similarity score computation for those vectors."
+                )
+        else:
+            raise ValueError(
+                f"Invalid vectors, cannot compute cosine similarity scores"
+                f"for zero vectors"
+                f"{embedding_array} or {embedding}"
+            )
+        return similarity_scores
