bf0526d96e63012b55ee838d3613ecf36444796e
Vivien Hung <vivihung@microsoft.com>
2023-04-24 16:39:25 -0700

    Copilot Chat - Bot portability (#619)
    ### Motivation and Context
    This change enables download/upload a bot in the Copilot Chat app. It is
    the first step to demonstrate the idea of "bots are documents". With
    Semantic Kernel memory and chat history, users can
    authorize/update/share bots with others, like write a document. We would
    like to inspire SK developers with this example and learn from their
    real-world use cases in order to take the idea further.
    
    ### Description
    * UX controls to trigger download/upload a bot file. Now we only support
    download/upload files from the user's computer.
    * API endpoints to expose the upload/download functionality.
    * Download: Export 1) chat message history from the given chat session
    (id: chat id), 2) embeddings from SK memory store.
    * Upload: Import 1) chat message history to a new chat session, 3)
    embeddings to SK memory store. (Note: Copilot Chat messages are not
    linked to SK memory id.)
    * Bot schema v1.0. It defines what is stored in a bot file - 1) schema
    version info (for compatibility), 2) chat history, 3) embeddings from
    the conversation, 4) the AI service information that generated the
    embeddings.
    * Error handlings:
    1. If schema in the file and in the app config mismatch, the API throws
    exception. For embedding mismatches, further enhancement could be done
    to generate new embeddings.
    2. If import failed in any step, we throw an exception. A further work
    item here to revert changes in each store to make it transactional.
    * Chat history: Hallucination is possible if the model missed context.
    Therefore, we store and render the chat history in the experience to
    allow users to tell if hallucination is presented for a given prompt.

---
+++ samples/apps/copilot-chat-app/webapi/Config/BotSchemaConfig.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+namespace SemanticKernel.Service.Config;
+
+/// <summary>
+/// Configuration settings for the bot file schema that is supported by this application.
+/// </summary>
+public class BotSchemaConfig
+{
+    /// <summary>
+    /// The name of the schema.
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The version of the schema.
+    /// </summary>
+    public int Version { get; set; } = -1;
+}
