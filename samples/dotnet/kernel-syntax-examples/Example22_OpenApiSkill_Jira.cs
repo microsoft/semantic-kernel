0eb897cc57589c1c881b6f564f17f696674ccd59
amsacha <amsacha@microsoft.com>
2023-04-24 16:54:22 -0700

    remove functions that cant be called because of incomplete jira open api schema

--- samples/dotnet/kernel-syntax-examples/Example22_OpenApiSkill_Jira.cs
+++ samples/dotnet/kernel-syntax-examples/Example22_OpenApiSkill_Jira.cs
@@ -17,15 +17,12 @@ using RepoUtils;
 
 public static class Example22_OpenApiSkill_Jira
 {
-    private static string s_email = "aman.sachan@microsoft.com";
-    private static string s_apiToken = Env.Var("JiraAPIToken");
-    private static string s_serverURL = "https://skjiratest.atlassian.net/rest/api/latest/";
     public static async Task RunAsync()
     {
         var kernel = new KernelBuilder().WithLogger(ConsoleLogger.Log).Build();
         var contextVariables = new ContextVariables();
 
-        //string s_serverURL = "https://skjiratest.atlassian.net/rest/api/latest/"; // "https://<jiraProject>.atlassian.net/rest/api/latest/"
+        string s_serverURL = "https://skjiratest.atlassian.net/rest/api/latest/"; // "https://<jiraProject>.atlassian.net/rest/api/latest/"
         contextVariables.Set("server-url", s_serverURL);
 
         IDictionary<string, ISKFunction> jiraSkills;
@@ -34,7 +31,7 @@ public static class Example22_OpenApiSkill_Jira
         bool bUseLocalFile = true;
         if (bUseLocalFile)
         {
-            var apiSkillFile = "./Jira/apiDefinitionSimplified.json";
+            var apiSkillFile = "./../../../Jira/apiDefinitionSimplified.json";
             jiraSkills = await kernel.ImportOpenApiSkillFromFileAsync("jiraSkills", apiSkillFile, tokenProvider.AuthenticateRequestAsync);
         }
         else
@@ -43,13 +40,9 @@ public static class Example22_OpenApiSkill_Jira
             jiraSkills = await kernel.ImportOpenApiSkillFromUrlAsync("jiraSkills", apiSkillRawFileURL, null, tokenProvider.AuthenticateRequestAsync);
         }
 
-        await RunJiraSkillsAsync(kernel, contextVariables, jiraSkills);
-    }
 
-    private static async Task RunJiraSkillsAsync(IKernel kernel, ContextVariables contextVars, IDictionary<string, ISKFunction> jiraSkills)
-    {
-        await GetIssueSkillAsync(kernel, jiraSkills, contextVars);
-        await AddCommentSkillAsync(kernel, jiraSkills, contextVars);
+        await GetIssueSkillAsync(kernel, jiraSkills, contextVariables);
+        await AddCommentSkillAsync(kernel, jiraSkills, contextVariables);
     }
 
     private static async Task GetIssueSkillAsync(IKernel kernel, IDictionary<string, ISKFunction> jiraSkills, ContextVariables contextVariables)
@@ -60,7 +53,9 @@ public static class Example22_OpenApiSkill_Jira
         //Run operation via the semantic kernel
         var result = await kernel.RunAsync(contextVariables, jiraSkills["GetIssue"]);
 
-        Console.WriteLine("\n\n\n GetIssue jiraSkills response: \n{0}", result);
+        Console.WriteLine("\n\n\n");
+        var formattedContent = JsonConvert.SerializeObject(JsonConvert.DeserializeObject(result.Result), Formatting.Indented);
+        Console.WriteLine("GetIssue jiraSkills response: \n{0}", formattedContent);
     }
 
     private static async Task AddCommentSkillAsync(IKernel kernel, IDictionary<string, ISKFunction> jiraSkills, ContextVariables contextVariables)
@@ -72,71 +67,15 @@ public static class Example22_OpenApiSkill_Jira
         //Run operation via the semantic kernel
         var result = await kernel.RunAsync(contextVariables, jiraSkills["AddComment"]);
 
-        Console.WriteLine("\n\n\n AddComment jiraSkills response: \n{0}", result);
-    }
-
-    // WARNING: we cant use the CreateIssueV2 Operation because the jira openAPI schema doesnt list properties under that operation name.
-    // It seems like its dynamically creating that definition but we dont yet support the dynamic schema in SK
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="kernel"></param>
-    /// <param name="jiraSkills"></param>
-    /// <param name="contextVariables"></param>
-    /// <returns></returns>
-    private static async Task CreateIssueSkillAsync(IKernel kernel, IDictionary<string, ISKFunction> jiraSkills, ContextVariables contextVariables)
-    {
-        //Set Properties for the Create Issue path in the openAPI.swagger.json 
-        contextVariables.Set("projectKey", "SKTES");
-        contextVariables.Set("issueTypeIds", "Task");
-
-        contextVariables.Set("item", "make the planet eco friendly");
-        //Run Create Issue 
-        var result = await kernel.RunAsync(contextVariables, jiraSkills["CreateIssueV2"]);
-
         Console.WriteLine("\n\n\n");
-        Console.WriteLine("CreateIssueV2 jiraSkills response: \n{0}", result);
-    }
-
-    private static async Task ListIssuesSkillAsync(IKernel kernel, IDictionary<string, ISKFunction> jiraSkills, ContextVariables contextVariables)
-    {
-        //TODO this may need a custom implementation
-        string serverURL;
-        contextVariables.Get("server-url", serverURL);
-        serverURL = s_serverURL + "search?jql="; // change search string
-
-        contextVariables.Set("server-url", serverURL);
-        //Set Properties for the Create Issue path in the openAPI.swagger.json 
-        contextVariables.Set("projectKey", "SKTES");
-        contextVariables.Set("issueTypeIds", "Task");
-        contextVariables.Set("item", "make the planet eco friendly");
-        //Run Create Issue 
-        var result = await kernel.RunAsync(contextVariables, jiraSkills["CreateIssueV2"]);
-
-        Console.WriteLine("\n\n\n CreateIssueV2 jiraSkills response: \n{0}", result);
+        var formattedContent = JsonConvert.SerializeObject(JsonConvert.DeserializeObject(result.Result), Formatting.Indented);
+        Console.WriteLine("AddComment jiraSkills response: \n{0}", formattedContent);
     }
 
     private static Task<string> AuthenticateWithBasicAPITokenAsync()
     {
-        string s = s_email + ":" + s_apiToken;
+        string s = "aman.sachan@microsoft.com" + ":" + Env.Var("JiraAPIToken");
+        //string s = {email} + ":" + Env.Var("JiraAPIToken");
         return Task.FromResult(s);
-    }
-
-    /// <summary>
-    /// Unit Test: Check if you can access jira via basic authentication; Does not use the Semantic Kernel.
-    /// </summary>
-    private static async Task Test_AccessJiraWithAPITokenAsync()
-    {
-        string serverURL = s_serverURL + "issue/SKTES-1";
-        string s_token = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{s_email}:{s_apiToken}"));
-
-        var client = new HttpClient();
-        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", s_token);
-        var response = await client.GetAsync(new Uri(serverURL));
-        var content = await response.Content.ReadAsStringAsync();
-        var formattedContent = JsonConvert.SerializeObject(JsonConvert.DeserializeObject(content), Formatting.Indented);
-
-        Console.WriteLine(formattedContent + "\n\n\n");
-        client.Dispose();
     }
 }
