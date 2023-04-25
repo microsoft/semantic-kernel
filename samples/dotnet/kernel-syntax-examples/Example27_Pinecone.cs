8939e3232e5abd0bbe7a304d6102d612303b4e6e
tech <tech@chancetheapp.com>
2023-04-24 22:15:49 -0700

    feat(dotnet): add support for metadata in Pinecone memory store example refactor(dotnet): change MemoryCollectionName from "test" to "pinecone-test" in Pinecone example

--- samples/dotnet/kernel-syntax-examples/Example27_Pinecone.cs
+++ samples/dotnet/kernel-syntax-examples/Example27_Pinecone.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Connectors.Memory.Pinecone;
@@ -11,7 +12,7 @@ using RepoUtils;
 // ReSharper disable once InconsistentNaming
 public static class Example27_Pinecone
 {
-    private const string MemoryCollectionName = "test";
+    private const string MemoryCollectionName = "pinecone-test";
 
     public static async Task RunAsync()
     {
@@ -38,22 +39,28 @@ public static class Example27_Pinecone
         }
 
         Console.WriteLine("== Adding Memories ==");
-
-        var key1 = await kernel.Memory.SaveInformationAsync(MemoryCollectionName, id: "cat1", text: "british short hair");
-        var key2 = await kernel.Memory.SaveInformationAsync(MemoryCollectionName, id: "cat2", text: "orange tabby");
-        var key3 = await kernel.Memory.SaveInformationAsync(MemoryCollectionName, id: "cat3", text: "norwegian forest cat");
+        var metadata = new Dictionary<string, object>()
+        {
+            { "type", "text" },
+            { "tags", new List<string>() { "memory", "cats" } }
+        };
+        
+        string additionalMetadata = System.Text.Json.JsonSerializer.Serialize(metadata);
+        var key1 = await kernel.Memory.SaveInformationAsync(MemoryCollectionName,   "british short hair", "cat1",null, additionalMetadata);
+        var key2 = await kernel.Memory.SaveInformationAsync(MemoryCollectionName,  "orange tabby", "cat2",null, additionalMetadata);
+        var key3 = await kernel.Memory.SaveInformationAsync(MemoryCollectionName,   "norwegian forest cat","cat3", null, additionalMetadata);
 
         Console.WriteLine("== Printing Collections in DB ==");
         collections = memoryStore.GetCollectionsAsync();
         await foreach (var collection in collections)
         {
-            Console.WriteLine(collection);
+             Console.WriteLine(collection);
         }
-
+        
         Console.WriteLine("== Retrieving Memories Through the Kernel ==");
         MemoryQueryResult? lookup = await kernel.Memory.GetAsync(MemoryCollectionName, "cat1");
         Console.WriteLine(lookup != null ? lookup.Metadata.Text : "ERROR: memory not found");
-
+        
         Console.WriteLine("== Retrieving Memories Directly From the Store ==");
         var memory1 = await memoryStore.GetWithPointIdAsync(MemoryCollectionName, key1);
         var memory2 = await memoryStore.GetWithPointIdAsync(MemoryCollectionName, key2);
@@ -61,10 +68,10 @@ public static class Example27_Pinecone
         Console.WriteLine(memory1 != null ? memory1.Metadata.Text : "ERROR: memory not found");
         Console.WriteLine(memory2 != null ? memory2.Metadata.Text : "ERROR: memory not found");
         Console.WriteLine(memory3 != null ? memory3.Metadata.Text : "ERROR: memory not found");
-
+        
         Console.WriteLine("== Similarity Searching Memories: My favorite color is orange ==");
-        var searchResults = kernel.Memory.SearchAsync(MemoryCollectionName, "My favorite color is orange", limit: 3, minRelevanceScore: 0.8);
-
+        var searchResults = kernel.Memory.SearchAsync(MemoryCollectionName, "My favorite color is orange", limit: 1, minRelevanceScore: 0.8);
+        
         await foreach (var item in searchResults)
         {
             Console.WriteLine(item.Metadata.Text + " : " + item.Relevance);
@@ -72,7 +79,7 @@ public static class Example27_Pinecone
 
         Console.WriteLine("== Removing Collection {0} ==", MemoryCollectionName);
         await memoryStore.DeleteCollectionAsync(MemoryCollectionName);
-
+        //
         Console.WriteLine("== Printing Collections in DB ==");
         await foreach (var collection in collections)
         {
