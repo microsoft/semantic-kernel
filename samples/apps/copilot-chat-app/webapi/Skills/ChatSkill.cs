5e8d9e9f8895376abbda4020ddad142129cd212a
Adrian Bonar (HE/HIM) <adribona@microsoft.com>
2023-04-26 12:04:39 -0700

    Renamed PlannerFactory to include Async, updated plan invoke to remove workaround and Kernel accessor.

--- samples/apps/copilot-chat-app/webapi/Skills/ChatSkill.cs
+++ samples/apps/copilot-chat-app/webapi/Skills/ChatSkill.cs
@@ -7,7 +7,6 @@ using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.AI.TextCompletion;
 using Microsoft.SemanticKernel.Memory;
 using Microsoft.SemanticKernel.Orchestration;
-using Microsoft.SemanticKernel.Planning.Planners;
 using Microsoft.SemanticKernel.SkillDefinition;
 using SemanticKernel.Service.Config;
 using SemanticKernel.Service.Storage;
