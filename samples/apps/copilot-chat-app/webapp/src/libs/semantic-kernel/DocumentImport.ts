08956e49fd4123d36f3bda29c8deb6060bb7e59b
Tao Chen <TaoChenOSU@users.noreply.github.com>
2023-04-25 17:16:39 -0700

    Copilot chat: fix typescript encoding format (#654)
    ### Motivation and Context
    Two typescript files are saved as UTF-8 with BOM, which will cause
    eslint to generate warnings.
    
    ### Description
    Resave the two files to UTF-8 format.

--- samples/apps/copilot-chat-app/webapp/src/libs/semantic-kernel/DocumentImport.ts
+++ samples/apps/copilot-chat-app/webapp/src/libs/semantic-kernel/DocumentImport.ts
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
+// Copyright (c) Microsoft. All rights reserved.
 
 export class DocumentImportService {
     constructor(private readonly serviceUrl: string) { }
