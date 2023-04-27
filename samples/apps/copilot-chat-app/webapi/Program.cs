4682befffcabf710ee193c5f92e0774dd87b39fc
Gil LaHaye <gillahaye@microsoft.com>
2023-04-27 15:09:08 -0700

    Add App Insights

--- samples/apps/copilot-chat-app/webapi/Program.cs
+++ samples/apps/copilot-chat-app/webapi/Program.cs
@@ -32,6 +32,7 @@ public sealed class Program
 
         // Add in the rest of the services.
         builder.Services
+            .AddApplicationInsightsTelemetry()
             .AddAuthorization(builder.Configuration)
             .AddEndpointsApiExplorer()
             .AddSwaggerGen()
