02ac970981556c18d5e7b777f1fedf4b509f118b
Adrian Bonar (HE/HIM) <adribona@microsoft.com>
2023-04-27 16:40:17 -0700

    wip

---
+++ samples/apps/copilot-chat-app/webapi/CopilotChatPlanner.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.SemanticKernel.Planning;
+
+namespace SemanticKernel.Service;
+
+public class CopilotChatPlanner
+{
+    private readonly SequentialPlanner _sequentialPlanner;
+
+    public CopilotChatPlanner(
+        SequentialPlanner  sequentialPlanner
+        )
+    {
+        this._sequentialPlanner = sequentialPlanner;
+    }
+}
