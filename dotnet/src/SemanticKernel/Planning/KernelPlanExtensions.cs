d1cfd24f1d01afb578f720b9a707cb966f216371
Stephen Toub <stoub@microsoft.com>
2023-05-01 12:58:29 -0400

    Remove duplicative extension methods on KernelPlanExtensions (#750)
    These are all handled by the corresponding overload that takes an
    optional CancellationToken. Their presence also means the optionality of
    the tokens in the other overloads would never actually be used.

--- dotnet/src/SemanticKernel/Planning/KernelPlanExtensions.cs
+++ dotnet/src/SemanticKernel/Planning/KernelPlanExtensions.cs
@@ -20,17 +20,6 @@ public static class KernelPlanExtensions
     /// </summary>
     /// <param name="kernel">Kernel instance to use</param>
     /// <param name="plan">Plan to run</param>
-    /// <returns>Result of the plan execution</returns>
-    public static Task<Plan> StepAsync(this IKernel kernel, Plan plan)
-    {
-        return kernel.StepAsync(plan.State, plan, CancellationToken.None);
-    }
-
-    /// <summary>
-    /// Run the next step in a plan asynchronously
-    /// </summary>
-    /// <param name="kernel">Kernel instance to use</param>
-    /// <param name="plan">Plan to run</param>
     /// <param name="cancellationToken">The <see cref="CancellationToken"/> to monitor for cancellation requests. The default is <see cref="CancellationToken.None"/>.</param>
     /// <returns>Result of the plan execution</returns>
     public static Task<Plan> StepAsync(this IKernel kernel, Plan plan, CancellationToken cancellationToken = default)
@@ -44,34 +33,10 @@ public static class KernelPlanExtensions
     /// <param name="kernel">Kernel instance to use</param>
     /// <param name="input">Input to use</param>
     /// <param name="plan">Plan to run</param>
-    /// <returns>Result of the plan execution</returns>
-    public static Task<Plan> StepAsync(this IKernel kernel, string input, Plan plan)
-    {
-        return kernel.StepAsync(input, plan, CancellationToken.None);
-    }
-
-    /// <summary>
-    /// Run the next step in a plan asynchronously
-    /// </summary>
-    /// <param name="kernel">Kernel instance to use</param>
-    /// <param name="input">Input to use</param>
-    /// <param name="plan">Plan to run</param>
     /// <param name="cancellationToken">The <see cref="CancellationToken"/> to monitor for cancellation requests. The default is <see cref="CancellationToken.None"/>.</param>
     public static Task<Plan> StepAsync(this IKernel kernel, string input, Plan plan, CancellationToken cancellationToken = default)
     {
         return kernel.StepAsync(new ContextVariables(input), plan, cancellationToken);
-    }
-
-    /// <summary>
-    /// Run the next step in a plan asynchronously
-    /// </summary>
-    /// <param name="kernel">Kernel instance to use</param>
-    /// <param name="variables">Input to process</param>
-    /// <param name="plan">Plan to run</param>
-    /// <returns>Result of the plan execution</returns>
-    public static Task<Plan> StepAsync(this IKernel kernel, ContextVariables variables, Plan plan)
-    {
-        return kernel.StepAsync(variables, plan, CancellationToken.None);
     }
 
     /// <summary>
