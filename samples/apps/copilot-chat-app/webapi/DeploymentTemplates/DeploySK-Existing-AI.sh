06c57b65dd6aa5507a3e8aa282be08f1a0e53adb
Gil LaHaye <gillahaye@microsoft.com>
2023-05-02 17:29:01 -0700

    Support distinct model for planner in deployment templates (#778)
    ### Motivation and Context
    We can now have a distinct completion model for the planner. This aligns
    the deployment templates to that fact.
    
    ### Description
    Add a new plannerModel parameter in the deployment templates and scripts

--- samples/apps/copilot-chat-app/webapi/DeploymentTemplates/DeploySK-Existing-AI.sh
+++ samples/apps/copilot-chat-app/webapi/DeploymentTemplates/DeploySK-Existing-AI.sh
@@ -12,11 +12,12 @@ Endpoint="${4:-}"
 AIService="${5:-AzureOpenAI}"
 CompletionModel="${6:-gpt-35-turbo}"
 EmbeddingModel="${7:-text-embedding-ada-002}"
-ResourceGroup="${8:-}"
-Region="${9:-South Central US}"
-PackageUri="${10:-https://skaasdeploy.blob.core.windows.net/api/skaas.zip}"
-AppServiceSku="${11:-B1}"
-DebugDeployment="${12:-false}"
+PlannerModel="${8:-gpt-35-turbo}"
+ResourceGroup="${9:-}"
+Region="${10:-South Central US}"
+PackageUri="${11:-https://skaasdeploy.blob.core.windows.net/api/skaas.zip}"
+AppServiceSku="${12:-B1}"
+DebugDeployment="${13:-false}"
 
 if [ -z "$ResourceGroup" ]; then
     ResourceGroup="${DeploymentName}-rg"
@@ -32,11 +33,11 @@ echo "Creating resource group $ResourceGroup if it doesn't exist..."
 az group create --location "$Region" --name "$ResourceGroup" --tags Creator="$USER"
 
 echo "Validating template file..."
-az deployment group validate --name "$DeploymentName" --resource-group "$ResourceGroup" --template-file "$templateFile" --parameters name="$DeploymentName" packageUri="$PackageUri" aiService="$AIService" completionModel="$CompletionModel" embeddingModel="$EmbeddingModel" endpoint="$Endpoint" apiKey="$ApiKey" appServiceSku="$AppServiceSku"
+az deployment group validate --name "$DeploymentName" --resource-group "$ResourceGroup" --template-file "$templateFile" --parameters name="$DeploymentName" packageUri="$PackageUri" aiService="$AIService" completionModel="$CompletionModel" embeddingModel="$EmbeddingModel" plannerModel="$PlannerModel" endpoint="$Endpoint" apiKey="$ApiKey" appServiceSku="$AppServiceSku"
 
 echo "Deploying..."
 if [ "$DebugDeployment" = "true" ]; then
-    az deployment group create --name "$DeploymentName" --resource-group "$ResourceGroup" --template-file "$templateFile" --debug --parameters name="$DeploymentName" packageUri="$PackageUri" aiService="$AIService" completionModel="$CompletionModel" embeddingModel="$EmbeddingModel" endpoint="$Endpoint" apiKey="$ApiKey" appServiceSku="$AppServiceSku"
+    az deployment group create --name "$DeploymentName" --resource-group "$ResourceGroup" --template-file "$templateFile" --debug --parameters name="$DeploymentName" packageUri="$PackageUri" aiService="$AIService" completionModel="$CompletionModel" embeddingModel="$EmbeddingModel" plannerModel="$PlannerModel" endpoint="$Endpoint" apiKey="$ApiKey" appServiceSku="$AppServiceSku"
 else
-    az deployment group create --name "$DeploymentName" --resource-group "$ResourceGroup" --template-file "$templateFile" --parameters name="$DeploymentName" packageUri="$PackageUri" aiService="$AIService" completionModel="$CompletionModel" embeddingModel="$EmbeddingModel" endpoint="$Endpoint" apiKey="$ApiKey" appServiceSku="$AppServiceSku"
+    az deployment group create --name "$DeploymentName" --resource-group "$ResourceGroup" --template-file "$templateFile" --parameters name="$DeploymentName" packageUri="$PackageUri" aiService="$AIService" completionModel="$CompletionModel" embeddingModel="$EmbeddingModel" plannerModel="$PlannerModel" endpoint="$Endpoint" apiKey="$ApiKey" appServiceSku="$AppServiceSku"
 fi
