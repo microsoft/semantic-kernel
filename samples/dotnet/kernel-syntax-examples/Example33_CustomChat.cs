0de719ede3948ab0e982fae5c90f03a138d7f281
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2023-05-05 20:28:50 +0100

    Chaning description and pattern to match existing for TextCompletion Samples

--- samples/dotnet/kernel-syntax-examples/Example33_CustomChat.cs
+++ samples/dotnet/kernel-syntax-examples/Example33_CustomChat.cs
@@ -11,8 +11,48 @@ using Microsoft.SemanticKernel.AI.ChatCompletion;
 using RepoUtils;
 
 /**
- * The following example shows how to use Semantic Kernel with OpenAI ChatGPT API
+ * The following example shows how to plug use a custom chat completion model.
+ *
+ * This might be useful in a few scenarios, for example:
+ * - You are not using OpenAI or Azure OpenAI models
+ * - You are using OpenAI/Azure OpenAI models but the models are behind a web service with a different API schema
+ * - You want to use a local model
  */
+public sealed class MyChatCompletionService : IChatCompletion
+{
+    private const string OutputAssistantResult = "Hi I'm your SK Custom Assistant and I'm here to help you to create custom chats like this. :)";
+
+    public ChatHistory CreateNewChat(string instructions = "")
+    {
+        var chatHistory = new ChatHistory();
+
+        if (!string.IsNullOrWhiteSpace(instructions))
+        {
+            chatHistory.AddMessage(ChatHistory.AuthorRoles.System, instructions);
+        }
+
+        return chatHistory;
+    }
+
+    public Task<string> GenerateMessageAsync(ChatHistory chat, ChatRequestSettings? requestSettings = null, CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult(OutputAssistantResult);
+    }
+
+    public async IAsyncEnumerable<string> GenerateMessageStreamAsync(
+        ChatHistory chat,
+        ChatRequestSettings? requestSettings = null,
+        [EnumeratorCancellation] CancellationToken cancellationToken = default)
+    {
+        var streamedOutput = OutputAssistantResult.Split(' ');
+        foreach (string word in streamedOutput)
+        {
+            await Task.Delay(200, cancellationToken);
+            yield return $"{word} ";
+        }
+    }
+}
+
 // ReSharper disable once InconsistentNaming
 public static class Example33_CustomChat
 {
@@ -50,7 +90,7 @@ public static class Example33_CustomChat
 
     private static async Task CustomChatSampleAsync()
     {
-        Console.WriteLine("======== Custom Chat ========");
+        Console.WriteLine("======== Custom LLM - Chat Completion ========");
 
         IKernel kernel = new KernelBuilder().WithLogger(ConsoleLogger.Log).Build();
 
@@ -86,7 +126,7 @@ public static class Example33_CustomChat
 
     private static async Task CustomChatStreamSampleAsync()
     {
-        Console.WriteLine("======== Custom Chat - Stream ========");
+        Console.WriteLine("======== Custom LLM - Chat Completion Streaming ========");
 
         IKernel kernel = new KernelBuilder().WithLogger(ConsoleLogger.Log).Build();
 
@@ -122,41 +162,6 @@ public static class Example33_CustomChat
         string MessageToString(ChatHistory.Message message)
         {
             return $"{message.AuthorRole}: {message.Content}\n------------------------\n";
-        }
-    }
-
-    private sealed class MyChatCompletionService : IChatCompletion
-    {
-        private readonly string _outputAssistantResult = "Hi I'm your SK Custom Assistant and I'm here to help you to create custom chats like this. :)";
-
-        public ChatHistory CreateNewChat(string instructions = "")
-        {
-            var chatHistory = new ChatHistory();
-
-            if (!string.IsNullOrWhiteSpace(instructions))
-            {
-                chatHistory.AddMessage(ChatHistory.AuthorRoles.System, instructions);
-            }
-
-            return chatHistory;
-        }
-
-        public Task<string> GenerateMessageAsync(ChatHistory chat, ChatRequestSettings? requestSettings = null, CancellationToken cancellationToken = default)
-        {
-            return Task.FromResult(this._outputAssistantResult);
-        }
-
-        public async IAsyncEnumerable<string> GenerateMessageStreamAsync(
-            ChatHistory chat,
-            ChatRequestSettings? requestSettings = null,
-            [EnumeratorCancellation] CancellationToken cancellationToken = default)
-        {
-            var streamedOutput = this._outputAssistantResult.Split(' ');
-            foreach (string word in streamedOutput)
-            {
-                await Task.Delay(200, cancellationToken);
-                yield return $"{word} ";
-            }
         }
     }
 }
