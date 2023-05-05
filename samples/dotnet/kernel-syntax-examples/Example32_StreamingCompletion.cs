59468619e426b830ddd221ccff5fa1e6fe44cad5
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2023-05-05 12:12:26 +0100

    Adding a Custom TextCompletion with Streaming as Sample

--- samples/dotnet/kernel-syntax-examples/Example32_StreamingCompletion.cs
+++ samples/dotnet/kernel-syntax-examples/Example32_StreamingCompletion.cs
@@ -1,6 +1,9 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
 using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.AI.TextCompletion;
@@ -14,11 +17,12 @@ public static class Example32_StreamingCompletion
 {
     public static async Task RunAsync()
     {
-        await AzureOpenAITextCompletionSampleAsync();
-        await OpenAITextCompletionSampleAsync();
+        await AzureOpenAITextCompletionStreamAsync();
+        await OpenAITextCompletionStreamAsync();
+        await CustomTextCompletionStreamAsync();
     }
 
-    private static async Task AzureOpenAITextCompletionSampleAsync()
+    private static async Task AzureOpenAITextCompletionStreamAsync()
     {
         Console.WriteLine("======== Azure OpenAI - Streaming TextCompletion ========");
 
@@ -30,10 +34,10 @@ public static class Example32_StreamingCompletion
 
         ITextCompletion textCompletion = kernel.GetService<ITextCompletion>();
 
-        await TextCompletionSampleAsync(textCompletion);
+        await TextCompletionStreamAsync(textCompletion);
     }
 
-    private static async Task OpenAITextCompletionSampleAsync()
+    private static async Task OpenAITextCompletionStreamAsync()
     {
         Console.WriteLine("======== Open AI - Streaming TextCompletion ========");
 
@@ -42,10 +46,25 @@ public static class Example32_StreamingCompletion
 
         ITextCompletion textCompletion = kernel.GetService<ITextCompletion>();
 
-        await TextCompletionSampleAsync(textCompletion);
+        await TextCompletionStreamAsync(textCompletion);
     }
 
-    private static async Task TextCompletionSampleAsync(ITextCompletion textCompletion)
+    private static async Task CustomTextCompletionStreamAsync()
+    {
+        Console.WriteLine("======== Custom - Streaming TextCompletion ========");
+
+        IKernel kernel = new KernelBuilder().WithLogger(ConsoleLogger.Log).Build();
+        ITextCompletion Factory(IKernel k) => new MyTextCompletionService();
+
+        // Add your text completion service
+        kernel.Config.AddTextCompletionService(Factory);
+
+        ITextCompletion textCompletion = kernel.GetService<ITextCompletion>();
+
+        await TextCompletionStreamAsync(textCompletion);
+    }
+
+    private static async Task TextCompletionStreamAsync(ITextCompletion textCompletion)
     {
         var requestSettings = new CompleteRequestSettings()
         {
@@ -76,5 +95,42 @@ public static class Example32_StreamingCompletion
         recommendations, entertainment, and assistance. AI is awesome
        
         */
+    }
+
+    private class MyTextCompletionService : ITextCompletion
+    {
+        private readonly string _outputResult =
+            @"AI is awesome because it can help us solve complex problems, enhance our creativity,
+and improve our lives in many ways. AI can perform tasks that are too difficult, tedious,
+or dangerous for humans, such as diagnosing diseases, detecting fraud, or exploring space.
+AI can also augment our abilities and inspire us to create new forms of art, music, or
+literature. AI can also improve our well-being and happiness by providing personalized
+recommendations, entertainment, and assistance. AI is awesome";
+
+        public Task<string> CompleteAsync(
+            string text,
+            CompleteRequestSettings requestSettings,
+            CancellationToken cancellationToken = default)
+        {
+            // Your model logic here
+            var result = this._outputResult;
+
+            return Task.FromResult(result);
+        }
+
+        public async IAsyncEnumerable<string> CompleteStreamAsync(
+            string text,
+            CompleteRequestSettings requestSettings,
+            [EnumeratorCancellation] CancellationToken cancellationToken = default)
+        {
+            yield return Environment.NewLine;
+
+            var streamedOutput = this._outputResult.Split(' ');
+            foreach (string word in streamedOutput)
+            {
+                await Task.Delay(50, cancellationToken);
+                yield return $"{word} ";
+            }
+        }
     }
 }
