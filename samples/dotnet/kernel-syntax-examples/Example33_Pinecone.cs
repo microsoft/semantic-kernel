0db59b8d9ba0c697176a14c32f810449831180ab
Dmytro Struk <13853051+dmytrostruk@users.noreply.github.com>
2023-05-08 15:25:40 +0100

    Updated Pinecone example

--- samples/dotnet/kernel-syntax-examples/Example33_Pinecone.cs
+++ samples/dotnet/kernel-syntax-examples/Example33_Pinecone.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Connectors.Memory.Pinecone;
+using Microsoft.SemanticKernel.Connectors.Memory.Pinecone.Model;
 using Microsoft.SemanticKernel.Memory;
 using RepoUtils;
 
@@ -12,8 +13,8 @@ using RepoUtils;
 /// <summary>
 ///  This example shows how to use the PineconeMemoryStore to store and retrieve memories with Pinecone assuming
 ///  you have a Pinecone account and have created an index. You can create an index using the Pinecone UI or
-///  instead initialize the index using PineconeClient.InitializeIndexAsync. But note that it can take a few minutes
-///  for the index to be ready for use and PineconeClient will block until the index is ready.
+///  instead initialize the index using <see cref="IPineconeClient.CreateIndexAsync(IndexDefinition, System.Threading.CancellationToken)"/>.
+///  But note that it can take a few minutes for the index to be ready for use.
 /// </summary>
 public static class Example33_Pinecone
 {
@@ -37,13 +38,16 @@ public static class Example33_Pinecone
             .Build();
 
         Console.WriteLine("== Printing Collections in DB ==");
+
         IAsyncEnumerable<string> collections = memoryStore.GetCollectionsAsync();
+
         await foreach (string collection in collections)
         {
             Console.WriteLine(collection);
         }
 
         Console.WriteLine("== Adding Memories ==");
+
         Dictionary<string, object> metadata = new Dictionary<string, object>()
         {
             { "type", "text" },
@@ -51,16 +55,10 @@ public static class Example33_Pinecone
         };
 
         string additionalMetadata = System.Text.Json.JsonSerializer.Serialize(metadata);
+
         string key1 = await kernel.Memory.SaveInformationAsync(MemoryCollectionName, "british short hair", "cat1", null, additionalMetadata);
         string key2 = await kernel.Memory.SaveInformationAsync(MemoryCollectionName, "orange tabby", "cat2", null, additionalMetadata);
         string key3 = await kernel.Memory.SaveInformationAsync(MemoryCollectionName, "norwegian forest cat", "cat3", null, additionalMetadata);
-
-        Console.WriteLine("== Printing Collections in DB ==");
-        collections = memoryStore.GetCollectionsAsync();
-        await foreach (string collection in collections)
-        {
-            Console.WriteLine(collection);
-        }
 
         Console.WriteLine("== Retrieving Memories Through the Kernel ==");
         MemoryQueryResult? lookup = await kernel.Memory.GetAsync(MemoryCollectionName, "cat1");
@@ -70,6 +68,7 @@ public static class Example33_Pinecone
         var memory1 = await memoryStore.GetAsync(MemoryCollectionName, key1);
         var memory2 = await memoryStore.GetAsync(MemoryCollectionName, key2);
         var memory3 = await memoryStore.GetAsync(MemoryCollectionName, key3);
+
         Console.WriteLine(memory1 != null ? memory1.Metadata.Text : "ERROR: memory not found");
         Console.WriteLine(memory2 != null ? memory2.Metadata.Text : "ERROR: memory not found");
         Console.WriteLine(memory3 != null ? memory3.Metadata.Text : "ERROR: memory not found");
@@ -80,15 +79,6 @@ public static class Example33_Pinecone
         await foreach (MemoryQueryResult item in searchResults)
         {
             Console.WriteLine(item.Metadata.Text + " : " + item.Relevance);
-        }
-
-        Console.WriteLine("== Removing Collection {0} ==", MemoryCollectionName);
-        await memoryStore.DeleteCollectionAsync(MemoryCollectionName);
-        //
-        Console.WriteLine("== Printing Collections in DB ==");
-        await foreach (string collection in collections)
-        {
-            Console.WriteLine(collection);
         }
     }
 }
