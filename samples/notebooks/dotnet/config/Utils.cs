c32f9124dfa2d633dcfd4b1d23e2d16aef8ae644
Almir Kazazic <kazazic@gmail.com>
2023-05-09 09:30:16 +0200

    Fixing issues in the Jupyter notebook for the planner

--- samples/notebooks/dotnet/config/Utils.cs
+++ samples/notebooks/dotnet/config/Utils.cs
@@ -22,4 +22,39 @@ public static class Utils
 
         return result.ToString();
     }
+
+    public static string ToPlanString(Microsoft.SemanticKernel.Planning.Plan originalPlan, string indent = " ")
+    {
+        string goalHeader = $"{indent}Goal: {originalPlan.Description}\n\n{indent}Steps:\n";
+
+        string stepItems = string.Join("\n", originalPlan.Steps.Select(step =>
+        {
+            if (step.Steps.Count == 0)
+            {
+                string skillName = step.SkillName;
+                string stepName = step.Name;
+
+                string parameters = string.Join(" ", step.Parameters.Select(param => $"{param.Key}='{param.Value}'"));
+                if (!string.IsNullOrEmpty(parameters))
+                {
+                    parameters = $" {parameters}";
+                }
+
+                string? outputs = step.Outputs.FirstOrDefault();
+                if (!string.IsNullOrEmpty(outputs))
+                {
+                    outputs = $" => {outputs}";
+                }
+
+                return $"{indent}{indent}- {string.Join(".", skillName, stepName)}{parameters}{outputs}";
+            }
+            else
+            {
+                string nestedSteps = ToPlanString(step, indent + indent);
+                return nestedSteps;
+            }
+        }));
+
+        return goalHeader + stepItems;
+    }
 }
