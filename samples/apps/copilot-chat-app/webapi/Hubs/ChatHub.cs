40e89254c83440b4a35ee5699699816a66a82e91
amsacha <amsacha@microsoft.com>
2023-05-09 14:59:07 -0700

    Have reconnecting behaviour established; Hub reacts to user connected op; Hub sends a message from inside the chat skill; Hub funciton can be invoked from front end

--- samples/apps/copilot-chat-app/webapi/Hubs/ChatHub.cs
+++ samples/apps/copilot-chat-app/webapi/Hubs/ChatHub.cs
@@ -6,14 +6,19 @@ namespace SemanticKernel.Service.SignalR.Hubs;
 
 public class ChatHub : Hub
 {
-#pragma warning disable IDE1006 // Naming Styles
-    public async Task SendMessage(string user, string message)
+    public async Task SendMessageAsync(string user, string message)
     {
         await this.Clients.All.SendAsync("SendMessage", user, message);
         Console.WriteLine("Log SendMessage: {0}", message);
     }
 
-    public async Task ReceiveMessage(string user, string message)
+    public async Task Func2Async(string user, string message)
+    {
+        await this.Clients.AllExcept(this.Context.ConnectionId).SendAsync("ReceiveMessage", user, message);
+        Console.WriteLine("Log ReceiveMessage: {0}", message);
+    }
+
+    public async Task ReceiveMessageAsync(string user, string message)
     {
         await this.Clients.All.SendAsync("ReceiveMessage", user, message);
         Console.WriteLine("Log ReceiveMessage: {0}", message);
@@ -25,11 +30,10 @@ public class ChatHub : Hub
         Console.WriteLine("Log UserConnected");
     }
 
-    public async Task NewMessage(long username, string message)
-#pragma warning restore IDE1006 // Naming Styles
+    public async Task NewMessageAsync(long username, string message)
     {
         await this.Clients.All.SendAsync("messageReceived", username, message);
         Console.WriteLine("Log messageReceived: {0}", message);
     }
-        
+
 }
