6195c855361bc3426cc132a6a7b18f1236f78847
amsacha <amsacha@microsoft.com>
2023-05-11 15:30:02 -0700

    Update singalR integration to broadcast from chatskill directly and have front end respond + cleanup + yarn and package.json update
    # Conflicts:
    #	samples/apps/copilot-chat-app/webapi/CopilotChat/Controllers/ChatController.cs

--- samples/apps/copilot-chat-app/webapp/src/redux/app/store.ts
+++ samples/apps/copilot-chat-app/webapp/src/redux/app/store.ts
@@ -3,7 +3,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import appReducer from '../features/app/appSlice';
 import conversationsReducer from '../features/conversations/conversationsSlice';
-import { signalRMiddleware, signalRStart } from '../features/message-relay/signalRMiddleware';
+import { signalRMiddleware, signalRStart, signalRRegisterEvents } from '../features/message-relay/signalRMiddleware';
 import pluginsReducer from '../features/plugins/pluginsSlice';
 
 export const store = configureStore({
@@ -17,7 +17,8 @@ export const store = configureStore({
 
 // Start the signalR connection to make sure messages are
 // sent to all clients and received by all clients
-signalRStart(store);
+signalRStart();
+signalRRegisterEvents(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
--- samples/apps/copilot-chat-app/webapp/src/redux/features/message-relay/signalRMiddleware.ts
+++ samples/apps/copilot-chat-app/webapp/src/redux/features/message-relay/signalRMiddleware.ts
@@ -1,4 +1,21 @@
 import * as signalR from "@microsoft/signalr";
+import { AuthorRoles } from './../../../libs/models/ChatMessage';
+import { IChatMessage } from './../../../libs/models/ChatMessage';
+
+export interface UserAsk {
+    input: string;
+    variables: KeyValuePair<string, string>[];
+}
+  
+  export interface UserAskResult {
+    value: string;
+    variables?: KeyValuePair<string, string>[];
+}
+  
+  export interface KeyValuePair<K, V> {
+    key: K;
+    value: V;
+}
 
 const url = new URL("/messageRelayHub", process.env.REACT_APP_BACKEND_URI as string);
 const signalRConnectionOptions = {
@@ -13,24 +30,41 @@ const connection = new signalR.HubConnectionBuilder()
     .configureLogging(signalR.LogLevel.Information)
     .build();
 
+const respondToServerCallbackName = "ReceiveMessageFromServer" as string;
+
 export const signalRMiddleware = (store: any) => {
     return (next: any) => async (action: any) => {
         console.log("signalRMiddleware action", action);
         console.log("signalRMiddleware store", store);
 
         if (action.type === "conversations/updateConversationFromUser") {
-            await connection.invoke("SendMessage", action.payload.message);
+            await connection.invoke("SendMessageToAllUsersExceptSelfAsync", respondToServerCallbackName, action.payload.message);
         }
 
         return next(action);
     }
 }
 
-export const signalRStart = async (store: any) => {
-    connection.on("ReceiveMessage", (message: any) => {
+export const signalRStart = async () => {
+    await connection.start().catch(err => console.error(err.toString()));
+}
+
+export const signalRRegisterEvents = async (store: any) => {
+    connection.on(respondToServerCallbackName, (message: UserAsk) => {
         store.dispatch({ type: "conversations/updateConversationFromServer", payload: { message } });
         console.log("ReceiveMessage", message);
     });
 
-    await connection.start().catch(err => console.error(err.toString()));
+    connection.on("receiveChatSkillAskResult", (message: UserAskResult) => {
+        const messageResult = {
+            timestamp: new Date().getTime(),
+            userName: 'bot',
+            userId: 'bot',
+            content: message.value,
+            authorRole: AuthorRoles.Bot,
+        } as IChatMessage;
+
+        store.dispatch({ type: "conversations/updateConversationFromServer", payload: { messageResult } });
+        console.log("ReceiveMessage", message);
+    });
 }
