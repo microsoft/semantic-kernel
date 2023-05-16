86f60fec2aca2eb143afe376c2dc93c3347a2c3c
Teresa Hoang <tehoang@microsoft.com>
2023-05-16 13:34:06 -0700

    spell check

--- samples/apps/copilot-chat-app/webapp/src/components/utils/TextUtils.ts
+++ samples/apps/copilot-chat-app/webapp/src/components/utils/TextUtils.ts
@@ -3,8 +3,8 @@
  * It wraps each link matched with anchor tags and applies safe href attributes.
  */
 export function convertToAnchorTags(htmlString: string) {
-    // Regular expression to match links, exluding any HTML tags at the end
-    // Since response from bot is plain text, sometimes line breaks and other html tags are included in the response for readibility.
+    // Regular expression to match links, excluding any HTML tags at the end
+    // Since response from bot is plain text, sometimes line breaks and other html tags are included in the response for readability.
     var linkRegex = /(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-/]))?(?=(<br|<p|<div|<span)\s*\/>|$)/g;
 
     var result = htmlString.replace(linkRegex, function (link) {
