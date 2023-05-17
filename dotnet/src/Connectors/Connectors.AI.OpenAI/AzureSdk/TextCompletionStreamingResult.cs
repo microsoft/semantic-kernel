9f1b5cb9314e68339e637ee317cf8a2cd235d5de
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2023-05-17 06:21:56 +0100

    ITextCompletion Multiple Results (Incl. Streaming) (#952)
    ### Motivation and Context
    
    Potential to get multiple results for the same prompt with and without
    streaming support.
    
    Resolves #526 
    Closes #526 
    
    
    ![MultiChatCompletionStream2](https://github.com/microsoft/semantic-kernel/assets/19890735/fb37d37c-e915-4a2e-92c8-662cb26f1e89)
    
    
    ### Description
    
    This PR introduces abstractions and the capability to get multiple
    results for the same prompt using the same request including streaming
    support.
    
    Proposed:
    
    - Added 2 result abstractions `ITextCompletionResult` and
    `ITextCompletionStreamingResult`
    - Changed current base `ITextCompletion` interface with the multiple
    results abstraction capability
    - Added 2 extensions methods to `ITextCompletion` to keep it retro
    compatible / avoid major breaking changes
    - Added concrete implementation to: Azure OpenAI / Open AI / Hugging
    Face completion impl.
    - Added Samples (with and without streaming) how to use multiple results
    
    Breaking potential:
    - Any custom completion implementation will need to implement the new
    methods in `ITextCompletion` interface with the
    `ITextCompletionResult`/`ITextCompletionStreamingResult` abstractions.
    Pretty straightforward and example was added in `CustomLLM` sample.
    
    
    Co-authored-by: Lee Miller <lemiller@microsoft.com>
    Co-authored-by: Shawn Callegari <36091529+shawncal@users.noreply.github.com>
    Co-authored-by: Stephen Toub <stoub@microsoft.com>

---
+++ dotnet/src/Connectors/Connectors.AI.OpenAI/AzureSdk/TextCompletionStreamingResult.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.AI.OpenAI;
+using Microsoft.SemanticKernel.AI.TextCompletion;
+
+namespace Microsoft.SemanticKernel.Connectors.AI.OpenAI.AzureSdk;
+
+internal sealed class TextCompletionStreamingResult : ITextCompletionStreamingResult
+{
+    private readonly StreamingChoice _choice;
+
+    public TextCompletionStreamingResult(StreamingChoice choice)
+    {
+        this._choice = choice;
+    }
+
+    public async Task<string> GetCompletionAsync(CancellationToken cancellationToken = default)
+    {
+        var fullMessage = new StringBuilder();
+        await foreach (var message in this._choice.GetTextStreaming(cancellationToken).ConfigureAwait(false))
+        {
+            fullMessage.Append(message);
+        }
+
+        return fullMessage.ToString();
+    }
+
+    public IAsyncEnumerable<string> GetCompletionStreamingAsync(CancellationToken cancellationToken = default)
+    {
+        return this._choice.GetTextStreaming(cancellationToken);
+    }
+}
