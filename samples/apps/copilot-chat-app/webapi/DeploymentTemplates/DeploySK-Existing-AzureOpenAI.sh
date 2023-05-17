bb4726c02e4a6666a01e5e4154b0449f33955079
Gil LaHaye <gillahaye@microsoft.com>
2023-05-16 19:34:12 -0500

    Bring bash deployment scripts back up to date (#1024)
    ### Motivation and Context
    The current bash deployment scripts are out of synch with the latest
    templates.
    
    
    ### Description
    - Update the bash deployment scripts
    - Provide usage information
    
    
    ### Contribution Checklist
    - [ ] The code builds clean without any errors or warnings
    - [ ] The PR follows SK Contribution Guidelines
    (https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    - [ ] The code follows the .NET coding conventions
    (https://learn.microsoft.com/dotnet/csharp/fundamentals/coding-style/coding-conventions)
    verified with `dotnet format`
    - [ ] All unit tests pass, and I have added new tests where possible
    - [ ] I didn't break anyone :smile:
    
    Co-authored-by: Teresa Hoang <125500434+teresaqhoang@users.noreply.github.com>
    Co-authored-by: Harleen Thind <39630244+hathind-ms@users.noreply.github.com>

--- samples/apps/copilot-chat-app/webapi/DeploymentTemplates/DeploySK-Existing-AzureOpenAI.sh
+++ samples/apps/copilot-chat-app/webapi/DeploymentTemplates/DeploySK-Existing-AzureOpenAI.sh
@@ -4,37 +4,171 @@
 
 set -e
 
-if [ $# -lt 4 ]; then
-    echo "Usage: $0 DeploymentName Endpoint ApiKey Subscription [ResourceGroup] [Region] [PackageUri] [AppServiceSku] [DebugDeployment]"
+usage() {
+    echo "Usage: $0 -d DEPLOYMENT_NAME -s SUBSCRIPTION -e ENDPOINT -o AZURE_OPENAI_API_KEY [OPTIONS]"
+    echo ""
+    echo "Arguments:"
+    echo "  -d, --deployment-name DEPLOYMENT_NAME      Name for the deployment (mandatory)"
+    echo "  -s, --subscription SUBSCRIPTION            Subscription to which to make the deployment (mandatory)"
+    echo "  -e, --endpoint ENDPOINT                    Endpoint to access Azure OpenAI (mandatory)"
+    echo "  -o, --aoai-key AZURE_OPENAI_API_KEY        Azure OpenAI API key (mandatory)"
+    echo "  -rg, --resource-group RESOURCE_GROUP       Resource group to which to make the deployment (default: \"rg-\$DEPLOYMENT_NAME\")"
+    echo "  -r, --region REGION                        Region to which to make the deployment (default: \"South Central US\")"
+    echo "  -p, --package-uri PACKAGE_URI              Package to deploy to web service (default: 'https://skaasdeploy.blob.core.windows.net/api/skaas.zip')"
+    echo "  -a, --app-service-sku APP_SERVICE_SKU      SKU for the Azure App Service plan (default: \"B1\")"
+    echo "  -k, --sk-server-api-key SK_SERVER_API_KEY  API key to access Semantic Kernel server's endpoints (default: random UUID)"
+    echo "  -cm, --completion-model COMPLETION_MODEL   Completion model to use (default: \"gpt-35-turbo\")"
+    echo "  -em, --embedding-model EMBEDDING_MODEL     Embedding model to use (default: \"text-embedding-ada-002\")"
+    echo "  -pm, --planner-model PLANNER_MODEL         Planner model to use (default: \"gpt-35-turbo\")"
+    echo "  -nq, --no-qdrant                           Don't deploy Qdrant for memory storage - Use volatile memory instead"
+    echo "  -nc, --no-cosmos-db                        Don't deploy Cosmos DB for chat storage - Use volatile memory instead"
+    echo "  -ns, --no-speech-services                  Don't deploy Speech Services to enable speech as chat input"
+    echo "  -dd, --debug-deployment                    Switches on verbose template deployment output"
+}
+
+# Parse arguments
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        -d|--deployment-name)
+        DEPLOYMENT_NAME="$2"
+        shift
+        shift
+        ;;
+        -s|--subscription)
+        SUBSCRIPTION="$2"
+        shift
+        shift
+        ;;
+        -e|--endpoint)
+        ENDPOINT="$2"
+        shift
+        shift
+        ;;
+        -o|--aoai-key)
+        AZURE_OPENAI_API_KEY="$2"
+        shift
+        shift
+        ;;
+        -rg|--resource-group)
+        RESOURCE_GROUP="$2"
+        shift
+        shift
+        ;;
+        -r|--region)
+        REGION="$2"
+        shift
+        shift
+        ;;
+        -p|--package-uri)
+        PACKAGE_URI="$2"
+        shift
+        shift
+        ;;
+        -a|--app-service-sku)
+        APP_SERVICE_SKU="$2"
+        shift
+        shift
+        ;;
+        -k|--sk-server-api-key)
+        SK_SERVER_API_KEY="$2"
+        shift
+        shift
+        ;;
+        -cm|--completion-model)
+        COMPLETION_MODEL="$2"
+        shift
+        shift
+        ;;
+        -em|--embedding-model)
+        EMBEDDING_MODEL="$2"
+        shift
+        shift
+        ;;
+        -pm|--planner-model)
+        PLANNER_MODEL="$2"
+        shift
+        shift
+        ;;
+        -nq|--no-qdrant)
+        NO_QDRANT=true
+        shift
+        ;;
+        -nc|--no-cosmos-db)
+        NO_COSMOS_DB=true
+        shift
+        ;;
+        -ns|--no-speech-services)
+        NO_SPEECH_SERVICES=true
+        shift
+        ;;
+        -dd|--debug-deployment)
+        DEBUG_DEPLOYMENT=true
+        shift
+        ;;
+        *)
+        usage
+        exit 1
+        ;;
+    esac
+done
+
+if [[ -z "$DEPLOYMENT_NAME" ]] || [[ -z "$SUBSCRIPTION" ]] || [[ -z "$ENDPOINT" ]] || [[ -z "$AZURE_OPENAI_API_KEY" ]]; then
+    usage
     exit 1
 fi
 
-DeploymentName="$1"
-Endpoint="$2"
-ApiKey="$3"
-Subscription="$4"
-ResourceGroup="${5:-rg-$DeploymentName}"
-Region="${6:-South Central US}"
-PackageUri="${7:-https://skaasdeploy.blob.core.windows.net/api/skaas.zip}"
-AppServiceSku="${8:-B1}"
-DebugDeployment="${9:-false}"
+if [ -z "$RESOURCE_GROUP" ]; then
+    RESOURCE_GROUP="$rg-{RESOURCE_GROUP}"
+fi
 
-templateFile="$(dirname "$0")/sk-existing-azureopenai.bicep"
+TEMPLATE_FILE="$(dirname "$0")/sk-existing-azureopenai.bicep"
 
 echo "Log into your Azure account"
 az login --use-device-code
 
-az account set -s "$Subscription"
+az account set -s "$SUBSCRIPTION"
 
-echo "Creating resource group $ResourceGroup if it doesn't exist..."
-az group create --location "$Region" --name "$ResourceGroup" --tags Creator="$USER"
+# Set defaults
+: "${REGION:="South Central US"}"
+: "${PACKAGE_URI:="https://skaasdeploy.blob.core.windows.net/api/skaas.zip"}"
+: "${APP_SERVICE_SKU:="B1"}"
+: "${SK_SERVER_API_KEY:="$(uuidgen)"}"
+: "${NO_QDRANT:=false}"
+: "${NO_COSMOS_DB:=false}"
+: "${NO_SPEECH_SERVICES:=false}"
+: "${COMPLETION_MODEL:="gpt-35-turbo"}"
+: "${EMBEDDING_MODEL:="text-embedding-ada-002"}"
+: "${PLANNER_MODEL:="gpt-35-turbo"}"
+
+# Create JSON config
+JSON_CONFIG=$(cat << EOF
+{
+    "name": { "value": "$DEPLOYMENT_NAME" },
+    "endpoint": { "value": "$ENDPOINT" },
+    "apiKey": { "value": "$AZURE_OPENAI_API_KEY" },
+    "completionModel": { "value": "$COMPLETION_MODEL" },
+    "embeddingModel": { "value": "$EMBEDDING_MODEL" },
+    "plannerModel": { "value": "$PLANNER_MODEL" },
+    "packageUri": { "value": "$PACKAGE_URI" },
+    "appServiceSku": { "value": "$APP_SERVICE_SKU" },
+    "skServerApiKey": { "value": "$SK_SERVER_API_KEY" },
+    "deployQdrant": { "value": $([ "$NO_QDRANT" = true ] && echo "false" || echo "true") },
+    "deployCosmosDB": { "value": $([ "$NO_COSMOS_DB" = true ] && echo "false" || echo "true") },
+    "deploySpeechServices": { "value": $([ "$NO_SPEECH_SERVICES" = true ] && echo "false" || echo "true") }
+}
+EOF
+)
+
+echo "Creating resource group $RESOURCE_GROUP if it doesn't exist..."
+az group create --location "$REGION" --name "$RESOURCE_GROUP" --tags Creator="$USER"
 
 echo "Validating template file..."
-az deployment group validate --name "$DeploymentName" --resource-group "$ResourceGroup" --template-file "$templateFile" --parameters name="$DeploymentName" packageUri="$PackageUri" completionModel="$CompletionModel" embeddingModel="$EmbeddingModel" plannerModel="$PlannerModel" endpoint="$Endpoint" apiKey="$ApiKey" appServiceSku="$AppServiceSku"
+az deployment group validate --name "$DEPLOYMENT_NAME" --resource-group "$RESOURCE_GROUP" --template-file "$TEMPLATE_FILE" --parameters "$JSON_CONFIG"
 
 echo "Deploying..."
-if [ "$DebugDeployment" = "true" ]; then
-    az deployment group create --name "$DeploymentName" --resource-group "$ResourceGroup" --template-file "$templateFile" --debug --parameters name="$DeploymentName" packageUri="$PackageUri" completionModel="$CompletionModel" embeddingModel="$EmbeddingModel" plannerModel="$PlannerModel" endpoint="$Endpoint" apiKey="$ApiKey" appServiceSku="$AppServiceSku"
+if [ "$DEBUG_DEPLOYMENT" = true ]; then
+    az deployment group create --name "$DEPLOYMENT_NAME" --resource-group "$RESOURCE_GROUP" --template-file "$TEMPLATE_FILE" --debug --parameters "$JSON_CONFIG"
 else
-    az deployment group create --name "$DeploymentName" --resource-group "$ResourceGroup" --template-file "$templateFile" --parameters name="$DeploymentName" packageUri="$PackageUri" completionModel="$CompletionModel" embeddingModel="$EmbeddingModel" plannerModel="$PlannerModel" endpoint="$Endpoint" apiKey="$ApiKey" appServiceSku="$AppServiceSku"
+    az deployment group create --name "$DEPLOYMENT_NAME" --resource-group "$RESOURCE_GROUP" --template-file "$TEMPLATE_FILE" --parameters "$JSON_CONFIG"
 fi
