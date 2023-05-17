240c3088a876966d3610c21a6a46d4e52dab0701
Maho Pacheco <mapach@microsoft.com>
2023-05-17 12:46:33 -0700

    Removing unused headers

--- dotnet/src/Skills/Skills.Web/WebFileDownloadSkill.cs
+++ dotnet/src/Skills/Skills.Web/WebFileDownloadSkill.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
