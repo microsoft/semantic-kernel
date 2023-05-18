cc67c6e646d9e9d40f4a9207a9e54ba5d6213649
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2023-05-18 14:18:05 -0400

    Improve the notebook

---
+++ samples/skills/GroundingSkill/native_function.py
@@ -0,0 +1,18 @@
+from semantic_kernel.skill_definition import sk_function
+
+
+class PythonGroundingSkill:
+    @sk_function(
+        description="Extract contents of <json_block> tags",
+        name="ExtractJsonBlock",
+    )
+    def extract_json_block_from_text(self, text: str):
+        start_tag = "<json_block>"
+        end_tag = "</json_block>"
+        i_0 = text.find(start_tag) + len(start_tag)
+        i_1 = text.find(end_tag)
+
+        extracted_text = text[i_0:i_1]
+        print(f"=~=~=~=\n{extracted_text}\n=~=~=~=\n")
+
+        return extracted_text
