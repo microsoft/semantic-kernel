39f766fd7bfb47f38d322163e0f9944971fd2885
Diego Colombo <dicolomb@microsoft.com>
2023-05-18 17:01:49 +0100

    experimental support for duck db as memory storage

---
+++ dotnet/src/Connectors/Connectors.Memory.DuckDB/DuckDBExtensions.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+using System.Data.Common;
+
+namespace Microsoft.SemanticKernel.Connectors.Memory.DuckDB;
+
+internal static class DuckDBExtensions
+{
+    public static T GetFieldValue<T>(this DbDataReader reader, string fieldName)
+    {
+        int ordinal = reader.GetOrdinal(fieldName);
+        return reader.GetFieldValue<T>(ordinal);
+    }
+
+    public static string GetString(this DbDataReader reader, string fieldName)
+    {
+        int ordinal = reader.GetOrdinal(fieldName);
+        return reader.GetString(ordinal);
+    }
+}
