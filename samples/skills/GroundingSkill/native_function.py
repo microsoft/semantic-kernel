4dece5b102092275233015503fbf2263793b3c03
Richard Edgar (Microsoft) <riedgar@microsoft.com>
2023-05-18 15:32:09 -0400

    So, we need a return type?

--- samples/skills/GroundingSkill/native_function.py
+++ samples/skills/GroundingSkill/native_function.py
@@ -6,13 +6,12 @@ class PythonGroundingSkill:
         description="Extract contents of <json_block> tags",
         name="ExtractJsonBlock",
     )
-    def extract_json_block_from_text(self, text: str):
+    def extract_json_block_from_text(self, text: str) -> str:
         start_tag = "<json_block>"
         end_tag = "</json_block>"
         i_0 = text.find(start_tag) + len(start_tag)
         i_1 = text.find(end_tag)
 
         extracted_text = text[i_0:i_1]
-        print(f"=~=~=~=\n{extracted_text}\n=~=~=~=\n")
 
         return extracted_text
