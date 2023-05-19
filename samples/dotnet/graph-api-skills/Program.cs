8588081a6aaee823514960eec0a74c36ec6a7d7d
Gudge <mgudgin@microsoft.com>
2023-05-18 17:51:08 -0700

    Fix dotnet graph-api-skills sample (#920)
    The skillParentDirectory local used to find the SummarizeSkill in the
    graph-api-skills sample had one too few levels of directory navigation,
    resulting in a DirectoryNotFoundException.
    
    Initialize variable using RepoFiles.SampleSkillsPath from
    kernel-syntax-examples project.
    
    Reference kernel-syntax-examples project.
    
    Make RepoFiles class and SampleSkillsPath method public in
    kernel-syntax-examples.
    
    Add namespace to graph-api-skills/Program.cs to avoid type collision
    with Program class in kernel-syntax-examples project.
    
    Co-authored-by: Adrian Bonar <56417140+adrianwyatt@users.noreply.github.com>

--- samples/dotnet/graph-api-skills/Program.cs
+++ samples/dotnet/graph-api-skills/Program.cs
@@ -20,6 +20,8 @@ using Microsoft.SemanticKernel.Skills.MsGraph.Connectors.Client;
 using Microsoft.SemanticKernel.Skills.MsGraph.Connectors.CredentialManagers;
 using DayOfWeek = System.DayOfWeek;
 
+namespace GraphApiSkills;
+
 /// <summary>
 /// The static plan below is meant to emulate a plan generated from the following request:
 ///   "Summarize the content of cheese.txt and send me an email with the summary and a link to the file.
@@ -137,7 +139,7 @@ public sealed class Program
             throw new InvalidOperationException("Unable to determine current assembly directory.");
         }
 
-        string skillParentDirectory = Path.GetFullPath(Path.Combine(currentAssemblyDirectory, "../../../../skills"));
+        string skillParentDirectory = RepoUtils.RepoFiles.SampleSkillsPath();
 
         IDictionary<string, ISKFunction> summarizeSkills =
             sk.ImportSemanticSkillFromDirectory(skillParentDirectory, "SummarizeSkill");
