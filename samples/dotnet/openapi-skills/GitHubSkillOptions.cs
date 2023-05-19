d436887eeb2d9b98221477694ac61c98ca6019bd
Adrian Bonar <56417140+adrianwyatt@users.noreply.github.com>
2023-05-18 17:31:15 -0700

    wip

---
+++ samples/dotnet/openapi-skills/GitHubSkillOptions.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.ComponentModel.DataAnnotations;
+
+/// <summary>
+/// Configuration options for GitHub skill.
+/// </summary>
+public sealed class GitHubSkillOptions
+{
+    public const string PropertyName = "GitHubSkill";
+
+    /// <summary>
+    /// GitHub API key.
+    /// </summary>
+    [Required, NotEmptyOrWhitespace]
+    public string Key { get; set; } = string.Empty;
+}
--- samples/dotnet/openapi-skills/Program.cs
+++ samples/dotnet/openapi-skills/Program.cs
@@ -1,7 +1,19 @@
-﻿using System;
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.AI.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.AI.OpenAI.ChatCompletion;
+using Microsoft.SemanticKernel.Orchestration;
+using Microsoft.SemanticKernel.Planning;
+using Microsoft.SemanticKernel.Skills.OpenAPI.Authentication;
+
+Console.WriteLine("Welcome to Semantic Kernel OpenAPI Skills Example!");
 
 // Load configuration
 IConfigurationRoot configuration = new ConfigurationBuilder()
@@ -13,18 +25,76 @@ IConfigurationRoot configuration = new ConfigurationBuilder()
 
 // Initialize logger
 using ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
-{
     builder.AddConfiguration(configuration.GetSection("Logging"))
         .AddConsole()
-        .AddDebug();
-});
+        .AddDebug());
 
 ILogger<Program> logger = loggerFactory.CreateLogger<Program>();
 
-using ILoggerFactory loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());
+// Initialize semantic kernel
+AIServiceOptions aiOptions = configuration.GetRequiredSection(AIServiceOptions.PropertyName).Get<AIServiceOptions>()
+    ?? throw new InvalidOperationException($"Missing configuration for {AIServiceOptions.PropertyName}.");
 
 IKernel kernel = Kernel.Builder
-    .WithLogger(loggerFactory.CreateLogger<IKernel>())
+    .WithLogger(logger)
+    .Configure(c =>
+    {
+        switch (aiOptions.Type)
+        {
+            case AIServiceOptions.AIServiceType.AzureOpenAI:
+                c.AddAzureChatCompletionService(aiOptions.Models.Completion, aiOptions.Endpoint, aiOptions.Key);
+                break;
+            case AIServiceOptions.AIServiceType.OpenAI:
+                c.AddOpenAIChatCompletionService(aiOptions.Models.Completion, aiOptions.Key);
+                break;
+            default:
+                throw new InvalidOperationException($"Unhandled AI service type {aiOptions.Type}");
+        }
+    })
     .Build();
 
-kernel.Config.AddAzureChatCompletionService(
+// Register GitHub skill
+GitHubSkillOptions gitHubOptions = configuration.GetRequiredSection(GitHubSkillOptions.PropertyName).Get<GitHubSkillOptions>()
+    ?? throw new InvalidOperationException($"Missing configuration for {GitHubSkillOptions.PropertyName}.");
+
+BearerAuthenticationProvider authenticationProvider = new(() => Task.FromResult(gitHubOptions.Key));
+await kernel.ImportOpenApiSkillFromFileAsync(
+    skillName: "GitHubSkill",
+    filePath: Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "GitHubSkill/openapi.json"),
+    authCallback: authenticationProvider.AuthenticateRequestAsync);
+
+ActionPlanner planner = new ActionPlanner(kernel, logger: logger);
+
+// Chat
+IChatCompletion chatGPT = kernel.GetService<IChatCompletion>();
+OpenAIChatHistory chatHistory = (OpenAIChatHistory)chatGPT.CreateNewChat("You are a helpful, friendly, intelligent assistant that is good at conversation.");
+while (true)
+{
+    Console.WriteLine("----------------");
+    Console.Write("Input: ");
+    string? input = Console.ReadLine();
+
+    if (string.IsNullOrWhiteSpace(input))
+    {
+        continue;
+    }
+
+    chatHistory.AddUserMessage(input);
+
+    Plan plan = await planner.CreatePlanAsync(input);
+    if (plan.Steps.Count > 0)
+    {
+        SKContext planContext = await plan.InvokeAsync(logger: logger);
+        if (planContext.ErrorOccurred)
+        {
+            logger.LogError("{0}", planContext.LastErrorDescription);
+        }
+        Console.WriteLine(planContext.Result);
+    }
+
+    string reply = await chatGPT.GenerateMessageAsync(chatHistory);
+    chatHistory.AddAssistantMessage(reply);
+
+    Console.WriteLine("----------------");
+    Console.WriteLine($"Assistant: {reply}");
+}
