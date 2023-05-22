a44d665dc13c7e1f050ed3e508417555bf6ce684
Thomas Gobin <thomgo@microsoft.com>
2023-05-22 22:11:17 +0200

    Add SequentialPlanner support to Copilot ChatApp

---
+++ samples/apps/copilot-chat-app/webapi/CopilotChat/Options/PlannerOptions.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+namespace SemanticKernel.Service.CopilotChat.Options;
+
+/// <summary>
+/// Configuration options for the planner.
+/// </summary>
+public class PlannerOptions
+{
+    public const string PropertyName = "Planner";
+
+    /// <summary>
+    /// Define if the planner must be Sequential or not.
+    /// </summary>
+    public bool IsSequential { get; set; } = false;
+}
