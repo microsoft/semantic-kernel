170f75f5fed49dd09974e5bed5f0cd6857ec32e7
amsacha <amsacha@microsoft.com>
2023-05-22 13:22:07 -0700

    clean up speech service

--- samples/apps/copilot-chat-app/webapp/src/libs/services/SpeechService.ts
+++ samples/apps/copilot-chat-app/webapp/src/libs/services/SpeechService.ts
@@ -10,35 +10,7 @@ interface TokenResponse {
 }
 
 export class SpeechService extends BaseService {
-    validateSpeechKeyAsync = async (accessToken: string) => {
-        const response = await this.invokeTokenAsync(accessToken);
-        return response;
-    };
-
-    getSpeechRecognizerAsyncWithValidKey = async (response: TokenResponse) => {
-        const { token, region, isSuccess } = response;
-
-        if(!isSuccess)
-        {
-            return;
-        }
-
-        return this.generateSpeechRecognizer(token, region);
-    };
-
-    getSpeechRecognizerAsync = async (accessToken: string) => {
-        const response = await this.invokeTokenAsync(accessToken);
-        return await this.getSpeechRecognizerAsyncWithValidKey(response);
-    };
-
-    private generateSpeechRecognizer ( token: string, region: string) {
-        const speechConfig = speechSdk.SpeechConfig.fromAuthorizationToken(token, region);
-        speechConfig.speechRecognitionLanguage = 'en-US';
-        const audioConfig = speechSdk.AudioConfig.fromDefaultMicrophoneInput();
-        return new speechSdk.SpeechRecognizer(speechConfig, audioConfig);
-    }
-
-    private invokeTokenAsync = async (accessToken: string): Promise<TokenResponse> => {
+    getSpeechTokenAsync = async (accessToken: string): Promise<TokenResponse> => {
         const result = await this.getResponseAsync<TokenResponse>(
             {
                 commandPath: `speechToken`,
@@ -49,4 +21,21 @@ export class SpeechService extends BaseService {
 
         return result;
     };
+
+    getSpeechRecognizerAsyncWithValidKey = async (response: TokenResponse) => {
+        const { token, region, isSuccess } = response;
+        
+        if(isSuccess) {
+            return this.generateSpeechRecognizer(token, region);
+        }
+
+        return;
+    };
+
+    private generateSpeechRecognizer ( token: string, region: string) {
+        const speechConfig = speechSdk.SpeechConfig.fromAuthorizationToken(token, region);
+        speechConfig.speechRecognitionLanguage = 'en-US';
+        const audioConfig = speechSdk.AudioConfig.fromDefaultMicrophoneInput();
+        return new speechSdk.SpeechRecognizer(speechConfig, audioConfig);
+    }
 }
