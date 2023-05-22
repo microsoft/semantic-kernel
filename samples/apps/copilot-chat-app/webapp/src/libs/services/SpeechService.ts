b7b31edc11aa60c854e5045e97563928bcdf294e
Aman Sachan <51973971+amsacha@users.noreply.github.com>
2023-05-22 14:15:17 -0700

    make seech service extend base service and use the auth helper (#1156)
    ### Motivation and Context
    Make SpeechService extend the BaseService class and use the AuthHelper
    class. This also fixes a bug where the speech service did not support
    the ApiKey auth.
    
    ### Contribution Checklist
    <!-- Before submitting this PR, please make sure: -->
    - [x] The code builds clean without any errors or warnings
    - [x] The PR follows SK Contribution Guidelines
    (https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    - [x] The code follows the .NET coding conventions
    (https://learn.microsoft.com/dotnet/csharp/fundamentals/coding-style/coding-conventions)
    verified with `dotnet format`
    - [x] All unit tests pass, and I have added new tests where possible
    - [x] I didn't break anyone :smile:
    
    ---------
    
    Co-authored-by: Adrian Bonar <56417140+adrianwyatt@users.noreply.github.com>
    Co-authored-by: Harleen Thind <39630244+hathind-ms@users.noreply.github.com>

--- samples/apps/copilot-chat-app/webapp/src/libs/services/SpeechService.ts
+++ samples/apps/copilot-chat-app/webapp/src/libs/services/SpeechService.ts
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft. All rights reserved.
 
 import * as speechSdk from 'microsoft-cognitiveservices-speech-sdk';
+import { BaseService } from './BaseService';
 
 interface TokenResponse {
     token: string;
@@ -8,33 +9,27 @@ interface TokenResponse {
     isSuccess: boolean;
 }
 
-interface SpeechServiceRequest {
-    commandPath: string;
-    method?: string;
-}
+export class SpeechService extends BaseService {
+    getSpeechTokenAsync = async (accessToken: string): Promise<TokenResponse> => {
+        const result = await this.getResponseAsync<TokenResponse>(
+            {
+                commandPath: `speechToken`,
+                method: 'GET',
+            },
+            accessToken,
+        );
 
-export class SpeechService {
-    constructor(private readonly serviceUrl: string) {}
-
-    validSpeechKeyAsync = async () => {
-        const response = await this.invokeTokenAsync();
-        return response;
+        return result;
     };
 
     getSpeechRecognizerAsyncWithValidKey = async (response: TokenResponse) => {
         const { token, region, isSuccess } = response;
-
-        if(!isSuccess)
-        {
-            return;
+        
+        if(isSuccess) {
+            return this.generateSpeechRecognizer(token, region);
         }
 
-        return this.generateSpeechRecognizer(token, region);
-    };
-
-    getSpeechRecognizerAsync = async () => {
-        const response = await this.invokeTokenAsync();
-        return await this.getSpeechRecognizerAsyncWithValidKey(response);
+        return;
     };
 
     private generateSpeechRecognizer ( token: string, region: string) {
@@ -43,40 +38,4 @@ export class SpeechService {
         const audioConfig = speechSdk.AudioConfig.fromDefaultMicrophoneInput();
         return new speechSdk.SpeechRecognizer(speechConfig, audioConfig);
     }
-
-    private invokeTokenAsync = async (): Promise<TokenResponse> => {
-        const result = await this.getAzureSpeechTokenAsync<TokenResponse>({
-            commandPath: `speechToken`,
-            method: 'GET',
-        });
-        return result;
-    };
-
-    private readonly getAzureSpeechTokenAsync = async <TokenResponse>(
-        request: SpeechServiceRequest,
-    ): Promise<TokenResponse> => {
-        const { commandPath, method } = request;
-
-        try {
-            const requestUrl = new URL(commandPath, this.serviceUrl);
-            const response = await fetch(requestUrl, {
-                method: method ?? 'GET',
-                headers: { 'Content-Type': 'application/json' },
-            });
-
-            if (!response.ok) {
-                throw Object.assign(new Error(response.statusText + ' => ' + (await response.text())));
-            }
-
-            return (await response.json()) as TokenResponse;
-        } catch (e) {
-            var additional_error_msg = '';
-            if (e instanceof TypeError) {
-                // fetch() will reject with a TypeError when a network error is encountered.
-                additional_error_msg =
-                    '\n\nPlease check that your backend is running and that it is accessible by the app';
-            }
-            throw Object.assign(new Error(e + additional_error_msg));
-        }
-    };
 }
