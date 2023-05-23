0580c4eed42fde057a0aab42ba3298bdefa96d02
Teresa Hoang <125500434+teresaqhoang@users.noreply.github.com>
2023-05-22 18:10:54 -0700

    Having chatlist timestamp reflect time of last change (#1173)
    …tname, upload bot, etc.)
    
    ### Motivation and Context
    <!-- Thank you for your contribution to the semantic-kernel repo!
    Please help reviewers and future users, providing the following
    information:
      1. Why is this change required?
      2. What problem does it solve?
      3. What scenario does it contribute to?
      4. If it fixes an open issue, please link to the issue here.
    -->
    Having chatlist timestamp reflect time of last change affecting chat
    (i.e., time bot was uploaded, chat name change, etc.).
    
    This value is computed in session only. If session is refreshed, chat
    list will be ordered timestamp of last message in each chat, with most
    recent message on top.
    
    
    ![image](https://github.com/microsoft/semantic-kernel/assets/125500434/63477ce5-d77f-4c6f-af6c-9d5ad7c2fb8b)
    Notice difference in timestamp of last message vs timestamp in chatlist
    (reflects time bot was uploaded)

--- samples/apps/copilot-chat-app/webapp/src/libs/useChat.ts
+++ samples/apps/copilot-chat-app/webapp/src/libs/useChat.ts
@@ -228,6 +228,7 @@ export const useChat = () => {
                     users: [loggedInUser],
                     messages: chatMessages,
                     botProfilePicture: getBotProfilePicture(Object.keys(conversations).length),
+                    lastUpdatedTimestamp: new Date().getTime(),
                 };
 
                 dispatch(addConversation(newChat));
