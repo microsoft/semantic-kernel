54bc180060bee82ad05e45b4e0f7a9d386b1e5f1
Stuart Cam <slc@fastmail.fm>
2023-05-30 18:09:44 +1000

    Fix pragma

--- dotnet/src/Connectors/Connectors.Memory.Weaviate/Http/ApiSchema/GraphGetRequest.cs
+++ dotnet/src/Connectors/Connectors.Memory.Weaviate/Http/ApiSchema/GraphGetRequest.cs
@@ -8,14 +8,13 @@ namespace Microsoft.SemanticKernel.Connectors.Memory.Weaviate.Http.ApiSchema;
 // ReSharper disable once ClassCannotBeInstantiated
 internal sealed class GraphGetRequest
 {
+#pragma warning disable CS8618
     public string Class { get; set; }
     public IEnumerable<float> Vector { get; set; }
+#pragma warning restore CS8618
     public int Limit { get; set; }
     public bool WithVector { get; set; }
     public double Distance { get; set; }
-#pragma warning disable CS8618
-    private GraphGetRequest() { }
-#pragma warning restore CS8618
 
     public HttpRequestMessage Build()
     {
