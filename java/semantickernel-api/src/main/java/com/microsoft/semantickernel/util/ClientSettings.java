51b450aa02f5e0ecc2768a39eb6ccc3a61313b33
Milder Hernandez Cagua <milderhc@microsoft.com>
2023-05-31 02:22:43 -0700

    Add comments

--- java/semantickernel-api/src/main/java/com/microsoft/semantickernel/util/ClientSettings.java
+++ java/semantickernel-api/src/main/java/com/microsoft/semantickernel/util/ClientSettings.java
@@ -1,89 +1,16 @@
 // Copyright (c) Microsoft. All rights reserved.
 package com.microsoft.semantickernel.util;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Properties;
 
 public class ClientSettings {
-    private static final Logger LOGGER = LoggerFactory.getLogger(ClientSettings.class);
-
-    public static class OpenAISettings {
-        private final String key;
-        private final String organizationId;
-
-        OpenAISettings(String key, String organizationId) {
-            this.key = key;
-            this.organizationId = organizationId;
-        }
-
-        public String getKey() {
-            return key;
-        }
-
-        public String getOrganizationId() {
-            return organizationId;
-        }
-    }
-
-    public static class AzureOpenAISettings {
-        private final String key;
-        private final String endpoint;
-        private final String deploymentName;
-
-        public AzureOpenAISettings(String key, String endpoint, String deploymentName) {
-            this.key = key;
-            this.endpoint = endpoint;
-            this.deploymentName = deploymentName;
-        }
-
-        public String getKey() {
-            return key;
-        }
-
-        public String getEndpoint() {
-            return endpoint;
-        }
-
-        public String getDeploymentName() {
-            return deploymentName;
-        }
-    }
-
-    private static final String DEFAULT_OPEN_AI_CLIENT_ID = "openai";
-    private static final String DEFAULT_AZURE_OPEN_AI_CLIENT_ID = "azureopenai";
-
-    public enum Property {
-        OPEN_AI_KEY(".key"),
-        OPEN_AI_ORGANIZATION_ID(".organizationid"),
-        AZURE_OPEN_AI_KEY(".key"),
-        AZURE_OPEN_AI_ENDPOINT(".endpoint"),
-        AZURE_OPEN_AI_DEPLOYMENT_NAME(".deploymentname");
-
-        public final String labelSuffix;
-
-        Property(String labelSuffix) {
-            this.labelSuffix = labelSuffix;
-        }
-
-        String getPropertyNameForClientId(String clientSettingsId) {
-            return "client." + clientSettingsId + labelSuffix;
-        }
-    }
-
     /**
      * Returns an instance of OpenAISettings with key and organizationId from the environment
      *
      * @return OpenAISettings
      */
     public static OpenAISettings getOpenAISettingsFromEnv() {
-        return new OpenAISettings(
-                ClientSettings.getSettingsValueFromEnv(Property.OPEN_AI_KEY),
-                ClientSettings.getSettingsValueFromEnv(Property.OPEN_AI_ORGANIZATION_ID));
+        return new OpenAISettings().fromEnv();
     }
 
     /**
@@ -93,10 +20,7 @@ public class ClientSettings {
      * @return AzureOpenAISettings
      */
     public static AzureOpenAISettings getAzureOpenAISettingsFromEnv() {
-        return new AzureOpenAISettings(
-                ClientSettings.getSettingsValueFromEnv(Property.AZURE_OPEN_AI_KEY),
-                ClientSettings.getSettingsValueFromEnv(Property.AZURE_OPEN_AI_ENDPOINT),
-                ClientSettings.getSettingsValueFromEnv(Property.AZURE_OPEN_AI_DEPLOYMENT_NAME));
+        return new AzureOpenAISettings().fromEnv();
     }
 
     /**
@@ -106,7 +30,7 @@ public class ClientSettings {
      * @return OpenAISettings
      */
     public static OpenAISettings getOpenAISettingsFromFile(String path) throws IOException {
-        return getOpenAISettingsFromFile(path, DEFAULT_OPEN_AI_CLIENT_ID);
+        return new OpenAISettings().fromFile(path);
     }
 
     /**
@@ -118,10 +42,7 @@ public class ClientSettings {
      */
     public static OpenAISettings getOpenAISettingsFromFile(String path, String clientSettingsId)
             throws IOException {
-        return new OpenAISettings(
-                ClientSettings.getSettingsValueFromFile(path, Property.OPEN_AI_KEY, clientSettingsId),
-                ClientSettings.getSettingsValueFromFile(
-                        path, Property.OPEN_AI_ORGANIZATION_ID, clientSettingsId));
+        return new OpenAISettings().fromFile(path, clientSettingsId);
     }
 
     /**
@@ -133,7 +54,7 @@ public class ClientSettings {
      */
     public static AzureOpenAISettings getAzureOpenAISettingsFromFile(String path)
             throws IOException {
-        return getAzureOpenAISettingsFromFile(path, DEFAULT_AZURE_OPEN_AI_CLIENT_ID);
+        return new AzureOpenAISettings().fromFile(path);
     }
 
     /**
@@ -146,62 +67,6 @@ public class ClientSettings {
      */
     public static AzureOpenAISettings getAzureOpenAISettingsFromFile(
             String path, String clientSettingsId) throws IOException {
-        return new AzureOpenAISettings(
-                ClientSettings.getSettingsValueFromFile(
-                        path, Property.AZURE_OPEN_AI_KEY, clientSettingsId),
-                ClientSettings.getSettingsValueFromFile(
-                        path, Property.AZURE_OPEN_AI_ENDPOINT, clientSettingsId),
-                ClientSettings.getSettingsValueFromFile(
-                        path, Property.AZURE_OPEN_AI_DEPLOYMENT_NAME, clientSettingsId));
-    }
-
-    /**
-     * Returns the value associated to the env var with the name of the Property
-     *
-     * @param property Property to get the env var value from
-     * @return String
-     */
-    public static String getSettingsValueFromEnv(Property property) {
-        return System.getenv(property.name());
-    }
-
-    /**
-     * Returns the Property value in the settingsFile
-     *
-     * @param settingsFile Properties file
-     * @param property Property to retrieve from file
-     * @return String with the value
-     * @throws IOException
-     */
-    public static String getSettingsValueFromFile(String settingsFile, Property property)
-            throws IOException {
-        return getSettingsValueFromFile(settingsFile, property, null);
-    }
-
-    /**
-     * Returns the Property value in the settingsFile
-     *
-     * @param settingsFile Properties file
-     * @param property Property to retrieve from file
-     * @param clientSettingsId ID of the client settings in the properties file schema
-     * @throws IOException
-     */
-    public static String getSettingsValueFromFile(
-            String settingsFile, Property property, String clientSettingsId) throws IOException {
-        File Settings = new File(settingsFile);
-
-        try (FileInputStream fis = new FileInputStream(Settings.getAbsolutePath())) {
-            Properties props = new Properties();
-            props.load(fis);
-
-            return props.getProperty(property.getPropertyNameForClientId(clientSettingsId));
-        } catch (IOException e) {
-            LOGGER.error(
-                    "Unable to load config value "
-                            + property.getPropertyNameForClientId(clientSettingsId)
-                            + " from properties file",
-                    e);
-            throw new IOException(settingsFile + " not configured properly");
-        }
+        return new AzureOpenAISettings().fromFile(path, clientSettingsId);
     }
 }
