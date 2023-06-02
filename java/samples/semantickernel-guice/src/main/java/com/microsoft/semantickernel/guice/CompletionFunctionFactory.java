676df774608fd3f839b3c639614a257cba40efb8
John Oliver <1615532+johnoliver@users.noreply.github.com>
2023-06-02 08:29:59 +0100

    Java - Add module to allow dependency injection via guice (#1199)
    ### Motivation and Context
    <!-- Thank you for your contribution to the semantic-kernel repo!
    Please help reviewers and future users, providing the following
    information:
      1. Why is this change required?
      2. What problem does it solve?
      3. What scenario does it contribute to?
      4. If it fixes an open issue, please link to the issue here.
    -->
    
    
    ### Description
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    
    ### Contribution Checklist
    <!-- Before submitting this PR, please make sure: -->
    - [ ] The code builds clean without any errors or warnings
    - [ ] The PR follows SK Contribution Guidelines
    (https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    - [ ] The code follows the .NET coding conventions
    (https://learn.microsoft.com/dotnet/csharp/fundamentals/coding-style/coding-conventions)
    verified with `dotnet format`
    - [ ] All unit tests pass, and I have added new tests where possible
    - [ ] I didn't break anyone :smile:
    
    Co-authored-by: Mark Wallace <127216156+markwallace-microsoft@users.noreply.github.com>

---
+++ java/samples/semantickernel-guice/src/main/java/com/microsoft/semantickernel/guice/CompletionFunctionFactory.java
@@ -0,0 +1,27 @@
+// Copyright (c) Microsoft. All rights reserved.
+package com.microsoft.semantickernel.guice;
+
+import com.microsoft.semantickernel.Kernel;
+import com.microsoft.semantickernel.semanticfunctions.PromptTemplateConfig;
+import com.microsoft.semantickernel.textcompletion.CompletionSKFunction;
+
+import jakarta.inject.Inject;
+
+public interface CompletionFunctionFactory {
+
+    CompletionSKFunction createFunction(
+            String prompt, String name, PromptTemplateConfig.CompletionConfig completionConfig);
+
+    class CompletionFunctionFactoryImpl implements CompletionFunctionFactory {
+        @Inject private Kernel kernel;
+
+        @Override
+        public CompletionSKFunction createFunction(
+                String prompt,
+                String name,
+                PromptTemplateConfig.CompletionConfig completionConfig) {
+            return kernel.getSemanticFunctionBuilder()
+                    .createFunction(prompt, name, null, null, completionConfig);
+        }
+    }
+}
---
+++ java/samples/semantickernel-guice/src/main/java/com/microsoft/semantickernel/guice/SemanticKernelModule.java
@@ -0,0 +1,131 @@
+// Copyright (c) Microsoft. All rights reserved.
+package com.microsoft.semantickernel.guice;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.TypeLiteral;
+import com.google.inject.util.Providers;
+import com.microsoft.openai.OpenAIAsyncClient;
+import com.microsoft.semantickernel.DefaultKernel;
+import com.microsoft.semantickernel.Kernel;
+import com.microsoft.semantickernel.KernelConfig;
+import com.microsoft.semantickernel.ai.embeddings.EmbeddingGeneration;
+import com.microsoft.semantickernel.builders.SKBuilders;
+import com.microsoft.semantickernel.connectors.ai.openai.textcompletion.OpenAITextCompletion;
+import com.microsoft.semantickernel.connectors.ai.openai.textembeddings.OpenAITextEmbeddingGeneration;
+import com.microsoft.semantickernel.memory.NullMemory;
+import com.microsoft.semantickernel.memory.SemanticTextMemory;
+import com.microsoft.semantickernel.skilldefinition.DefaultSkillCollection;
+import com.microsoft.semantickernel.skilldefinition.ReadOnlySkillCollection;
+import com.microsoft.semantickernel.templateengine.DefaultPromptTemplateEngine;
+import com.microsoft.semantickernel.templateengine.PromptTemplateEngine;
+
+import javax.annotation.Nullable;
+
+public class SemanticKernelModule extends AbstractModule {
+
+    private final SemanticTextMemory memory;
+    private final KernelConfig.Builder kernelConfig;
+    private final ReadOnlySkillCollection skillCollection;
+    private final PromptTemplateEngine promptTemplateEngine;
+    private String textCompletionModelId;
+    private boolean withMemory;
+    private String embeddingsGenerationServiceId;
+    private OpenAIAsyncClient client;
+
+    public SemanticKernelModule(
+            KernelConfig.Builder kernelConfig,
+            @Nullable SemanticTextMemory memory,
+            @Nullable ReadOnlySkillCollection skillCollection,
+            @Nullable PromptTemplateEngine promptTemplateEngine) {
+        this.kernelConfig = kernelConfig;
+
+        if (memory != null) {
+            this.memory = memory.copy();
+        } else {
+            this.memory = null;
+        }
+
+        if (skillCollection == null) {
+            skillCollection = new DefaultSkillCollection();
+        }
+        this.skillCollection = skillCollection;
+
+        if (promptTemplateEngine == null) {
+            promptTemplateEngine = new DefaultPromptTemplateEngine();
+        }
+        this.promptTemplateEngine = promptTemplateEngine;
+    }
+
+    public SemanticKernelModule(
+            KernelConfig.Builder kernelConfig, @Nullable SemanticTextMemory memory) {
+        this(kernelConfig, memory, null, null);
+    }
+
+    public SemanticKernelModule(KernelConfig.Builder kernelConfig) {
+        this(kernelConfig, null);
+    }
+
+    public SemanticKernelModule() {
+        this(SKBuilders.kernelConfig(), null);
+    }
+
+    public SemanticKernelModule withTextCompletionService(String model) {
+        this.textCompletionModelId = model;
+        return this;
+    }
+
+    public SemanticKernelModule withClient(OpenAIAsyncClient client) {
+        this.client = client;
+        return this;
+    }
+
+    @Override
+    protected void configure() {
+        super.configure();
+
+        if (client == null) {
+            client = Config.getClient(true);
+        }
+
+        bind(OpenAIAsyncClient.class).toInstance(client);
+
+        if (textCompletionModelId != null) {
+            kernelConfig.addTextCompletionService(
+                    textCompletionModelId,
+                    kernel -> new OpenAITextCompletion(client, textCompletionModelId));
+        }
+
+        if (embeddingsGenerationServiceId != null) {
+            OpenAITextEmbeddingGeneration embeddings =
+                    new OpenAITextEmbeddingGeneration(client, embeddingsGenerationServiceId);
+            kernelConfig.addTextEmbeddingsGenerationService(
+                    embeddingsGenerationServiceId, kernel -> embeddings);
+            bind(new TypeLiteral<EmbeddingGeneration<String, Float>>() {}).toInstance(embeddings);
+        }
+
+        bind(KernelConfig.class).toInstance(kernelConfig.build());
+        bind(PromptTemplateEngine.class).toInstance(promptTemplateEngine);
+        bind(ReadOnlySkillCollection.class).toProvider(Providers.of(skillCollection));
+
+        if (withMemory) {
+            bind(SemanticTextMemory.class).toProvider(Providers.of(memory));
+        } else {
+            bind(SemanticTextMemory.class).toInstance(NullMemory.getInstance());
+        }
+
+        bind(Kernel.class).to(DefaultKernel.class);
+        bind(CompletionFunctionFactory.class)
+                .to(CompletionFunctionFactory.CompletionFunctionFactoryImpl.class);
+    }
+
+    public SemanticKernelModule withMemory() {
+        this.withMemory = true;
+        return this;
+    }
+
+    public SemanticKernelModule withTextEmbeddingsGenerationService(
+            String embeddingsGenerationServiceId) {
+        this.embeddingsGenerationServiceId = embeddingsGenerationServiceId;
+        return this;
+    }
+}
