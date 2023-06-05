7d1f2ddd14126c2d98be80c2bb2a2b7d8772c1df
Abby Harrison <abby.harrison@microsoft.com>
2023-06-05 13:08:38 -0700

    update poetry.lock, add setuptools to build-system, and skip weaviate tests if python version is 3.8

--- python/tests/integration/connectors/memory/test_weaviate_memory_store.py
+++ python/tests/integration/connectors/memory/test_weaviate_memory_store.py
@@ -9,12 +9,12 @@ import pytest
 from semantic_kernel.connectors.memory import weaviate_memory_store
 from semantic_kernel.memory.memory_record import MemoryRecord
 
-if not sys.platform.startswith("linux"):
+if not sys.platform.startswith("linux") or sys.version_info < (3, 9):
     pytest.skip(
-        "test_weaviate_memory_store uses embedded weaviate which only runs on Linux at the moment",
+        "test_weaviate_memory_store uses embedded weaviate which only runs on Linux with Python 3.9+ at the moment",
         allow_module_level=True,
     )
-
+    
 
 @pytest.fixture
 def documents():
