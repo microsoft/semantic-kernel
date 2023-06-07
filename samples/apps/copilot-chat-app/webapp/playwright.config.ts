572ffc43c12b65d2c2b7aad4e6a9784f7c577aa5
Tao Chen <taochen@microsoft.com>
2023-06-07 10:22:40 -0700

    Disable headless mode

--- samples/apps/copilot-chat-app/webapp/playwright.config.ts
+++ samples/apps/copilot-chat-app/webapp/playwright.config.ts
@@ -35,6 +35,8 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+
+    headless: false,
   },
 
   /* Configure projects for major browsers */
