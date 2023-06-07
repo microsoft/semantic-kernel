f40b7844ef70bd7020e0e759868872224eb2a88e
Bruno Borges <brborges@microsoft.com>
2023-06-07 12:13:56 -0700

    Adjust to the correct openai builder and clients

--- java/api-test/integration-tests/src/test/java/com/microsoft/semantickernel/e2e/AbstractKernelTest.java
+++ java/api-test/integration-tests/src/test/java/com/microsoft/semantickernel/e2e/AbstractKernelTest.java
@@ -1,22 +1,23 @@
 // Copyright (c) Microsoft. All rights reserved.
 package com.microsoft.semantickernel.e2e;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.junit.jupiter.api.condition.EnabledIf;
+
+import com.azure.ai.openai.OpenAIAsyncClient;
+import com.azure.ai.openai.OpenAIClientBuilder;
+import com.azure.ai.openai.models.NonAzureOpenAIKeyCredential;
 import com.azure.core.credential.AzureKeyCredential;
 import com.microsoft.semantickernel.Kernel;
 import com.microsoft.semantickernel.KernelConfig;
 import com.microsoft.semantickernel.builders.SKBuilders;
 import com.microsoft.semantickernel.connectors.ai.openai.textcompletion.OpenAITextCompletion;
 import com.microsoft.semantickernel.memory.VolatileMemoryStore;
-import com.microsoft.semantickernel.openai.client.AzureOpenAIClient;
-import com.microsoft.semantickernel.openai.client.OpenAIAsyncClient;
 import com.microsoft.semantickernel.textcompletion.TextCompletion;
-
-import org.junit.jupiter.api.condition.EnabledIf;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
 
 @EnabledIf("isAzureTestEnabled")
 public class AbstractKernelTest {
@@ -28,10 +29,9 @@ public class AbstractKernelTest {
         String model = "text-davinci-003";
         TextCompletion textCompletion = new OpenAITextCompletion(getAzureOpenAIClient(), model);
 
-        KernelConfig kernelConfig =
-                SKBuilders.kernelConfig()
-                        .addTextCompletionService(model, kernel -> textCompletion)
-                        .build();
+        KernelConfig kernelConfig = SKBuilders.kernelConfig()
+                .addTextCompletionService(model, kernel -> textCompletion)
+                .build();
 
         return SKBuilders.kernel()
                 .setKernelConfig(kernelConfig)
@@ -41,21 +41,21 @@ public class AbstractKernelTest {
 
     public static OpenAIAsyncClient getOpenAIClient() throws IOException {
         String apiKey = getToken(CONF_OPENAI_PROPERTIES);
-        return new com.microsoft.semantickernel.openai.client.OpenAIClientBuilder()
-                .setApiKey(apiKey)
-                .build();
+        NonAzureOpenAIKeyCredential credential = new NonAzureOpenAIKeyCredential(apiKey);
+        return new OpenAIClientBuilder()
+                .credential(credential)
+                .buildAsyncClient();
     }
 
     public static OpenAIAsyncClient getAzureOpenAIClient() throws IOException {
         String apiKey = getToken(AZURE_CONF_PROPERTIES);
 
-        com.azure.ai.openai.OpenAIAsyncClient client =
-                new com.azure.ai.openai.OpenAIClientBuilder()
-                        .endpoint(getEndpoint(AZURE_CONF_PROPERTIES))
-                        .credential(new AzureKeyCredential(apiKey))
-                        .buildAsyncClient();
+        OpenAIAsyncClient client = new OpenAIClientBuilder()
+                .endpoint(getEndpoint(AZURE_CONF_PROPERTIES))
+                .credential(new AzureKeyCredential(apiKey))
+                .buildAsyncClient();
 
-        return new AzureOpenAIClient(client);
+        return client;
     }
 
     public static String getAzureModel() throws IOException {
