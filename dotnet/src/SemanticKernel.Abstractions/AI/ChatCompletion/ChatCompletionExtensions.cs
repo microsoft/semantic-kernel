c6ffa30107b838d6cad70e115d698f9b77ed1e7a
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2023-06-09 14:51:27 +0100

    Removing multiple appended chat messages from the extension method

--- dotnet/src/SemanticKernel.Abstractions/AI/ChatCompletion/ChatCompletionExtensions.cs
+++ dotnet/src/SemanticKernel.Abstractions/AI/ChatCompletion/ChatCompletionExtensions.cs
@@ -59,6 +59,7 @@ public static class ChatCompletionExtensions
         {
             var chatMessage = await chatResult.GetChatMessageAsync(cancellationToken).ConfigureAwait(false);
             messageContent.Append(chatMessage.Content);
+            break;
         }
 
         return messageContent.ToString();
