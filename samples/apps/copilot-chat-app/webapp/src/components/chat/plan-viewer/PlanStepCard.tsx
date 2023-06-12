5eaf92ff558c5c93bedf3a8eeb959fa734e786aa
Teresa Hoang <125500434+teresaqhoang@users.noreply.github.com>
2023-06-12 12:03:25 -0700

    Enabling Plan Edits in Copilot (#1386)
    ### Motivation and Context
    This PR enables user to edit a plan before approving or rejecting it:
    - User can edit any of the input value fields
    - User can delete step(s) (If SequentialPlanner is enabled). Deletion is
    disabled when there is only one step.
    
    When plan is returned to the Kernel, the chat message will be updated in
    the history repository to reflect the new plan state.
    
    ### Description
    This PR also contains optimizations for methods handling user actions on
    proposed plan and plan schema changes to enable the above.
    
    Chat History will also now reflect any actions the user took on the
    plan.
    
    Input Edit Mode:
    <img width="665" alt="image"
    src="https://github.com/microsoft/semantic-kernel/assets/125500434/31b7a5b5-9ca0-432a-bde1-3141e0e8e76f">
    
    Editing Action Plan (inputs only):
    
    ![UserPlanEdits](https://github.com/microsoft/semantic-kernel/assets/125500434/66b8c022-dcaf-469f-9426-928fafac4742)
    
    Editing SequentialPlanner (inputs and deleting step):
    
    ![SequentialPlannerEditsExample](https://github.com/microsoft/semantic-kernel/assets/125500434/0ae757b5-7767-46b0-977c-2f69dae133ba)
    
    Added 'NoOp' Plan state, where proposedPlan becomes unactionable if app
    state changes before user approves or rejects the plan. Potential
    reasons for unstable state: required plug-in no longer enabled, access
    permissions changed, etc.
    
    ![image](https://github.com/microsoft/semantic-kernel/assets/125500434/2af07534-4560-4e11-a8fc-4804d85a4717)
    
    
    > Note: there's some hallucinations with SequentialPlanner, but planner
    default is Action. SequentialPlanner stabilizations will continue in
    another PR

--- samples/apps/copilot-chat-app/webapp/src/components/chat/plan-viewer/PlanStepCard.tsx
+++ samples/apps/copilot-chat-app/webapp/src/components/chat/plan-viewer/PlanStepCard.tsx
@@ -1,6 +1,27 @@
-import { Badge, Body1, Card, CardHeader, makeStyles, shorthands, Text, tokens } from '@fluentui/react-components';
-import { IPlanInput, IPlanStep } from '../../../libs/models/Plan';
+import {
+    Badge,
+    Body1,
+    Button,
+    Card,
+    CardHeader,
+    Dialog,
+    DialogActions,
+    DialogBody,
+    DialogContent,
+    DialogSurface,
+    DialogTitle,
+    DialogTrigger,
+    makeStyles,
+    shorthands,
+    Text,
+    tokens,
+} from '@fluentui/react-components';
+import { Dismiss12Regular } from '@fluentui/react-icons';
+import { useState } from 'react';
+import { Constants } from '../../../Constants';
+import { IPlanInput } from '../../../libs/models/Plan';
 import { CopilotChatTokens } from '../../../styles';
+import { PlanStepInput } from './PlanStepInput';
 
 const useClasses = makeStyles({
     card: {
@@ -11,9 +32,9 @@ const useClasses = makeStyles({
     header: {
         color: CopilotChatTokens.titleColor,
     },
-    inputs: {
+    parameters: {
+        ...shorthands.gap(tokens.spacingHorizontalS),
         display: 'flex',
-        ...shorthands.gap('8px'),
         flexWrap: 'wrap',
     },
     bar: {
@@ -28,10 +49,11 @@ const useClasses = makeStyles({
     flexColumn: {
         display: 'flex',
         flexDirection: 'column',
-        marginLeft: '8px',
-        marginTop: '4px',
-        marginBottom: '4px',
-        ...shorthands.gap('8px'),
+        width: '100%',
+        marginLeft: tokens.spacingHorizontalS,
+        marginTop: tokens.spacingVerticalXS,
+        marginBottom: tokens.spacingVerticalXS,
+        ...shorthands.gap(tokens.spacingHorizontalS),
     },
     singleLine: {
         ...shorthands.overflow('hidden'),
@@ -42,18 +64,34 @@ const useClasses = makeStyles({
         width: '650px',
         fontSize: '12px',
     },
+    dialog: {
+        width: '398px',
+        '& button': {
+            marginTop: tokens.spacingVerticalL,
+            width: 'max-content',
+        },
+    },
 });
 
 interface PlanStepCardProps {
-    index: number;
-    step: IPlanStep;
+    // See Semantic Kernel's Plan object for field definitions (step === Plan).
+    // Plan.cs: https://github.com/microsoft/semantic-kernel/blob/df07fc6f28853a481dd6f47e60d39a52fc6c9967/dotnet/src/SemanticKernel/Planning/Plan.cs#
+    // Not explicitly defining the type here to avoid additional overhead of property maintenance.
+    step: any;
+    enableEdits: boolean;
+    enableStepDelete: boolean;
+    onDeleteStep: (index: number) => void;
 }
 
-export const PlanStepCard: React.FC<PlanStepCardProps> = ({ index, step }) => {
+export const PlanStepCard: React.FC<PlanStepCardProps> = ({ step, enableEdits, enableStepDelete, onDeleteStep }) => {
     const classes = useClasses();
+    const [openDialog, setOpenDialog] = useState(false);
 
-    const numbersAsStrings = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
-    const stepNumber = numbersAsStrings[index];
+    // Omit reserved context variable names from displayed inputs
+    const inputs = step.parameters.filter(
+        (parameter: IPlanInput) =>
+            !Constants.sk.reservedWords.includes(parameter.Key.trim()) && parameter.Value.trim() !== '',
+    );
 
     return (
         <Card className={classes.card}>
@@ -63,9 +101,56 @@ export const PlanStepCard: React.FC<PlanStepCardProps> = ({ index, step }) => {
                     <CardHeader
                         header={
                             <Body1>
-                                <b className={classes.header}>Step {stepNumber} •</b> {step.skill}.{step.function}
+                                <b className={classes.header}>Step {step.index + 1} •</b> {step.skill_name}.{step.name}
                                 <br />
                             </Body1>
+                        }
+                        action={
+                            enableEdits && enableStepDelete ? (
+                                <Dialog open={openDialog}>
+                                    <DialogTrigger disableButtonEnhancement>
+                                        <Button
+                                            appearance="transparent"
+                                            icon={<Dismiss12Regular />}
+                                            aria-label="Delete step"
+                                            onClick={() => {
+                                                setOpenDialog(true);
+                                            }}
+                                        />
+                                    </DialogTrigger>
+                                    <DialogSurface className={classes.dialog}>
+                                        <DialogBody>
+                                            <DialogTitle>Are you sure you want to delete this step?</DialogTitle>
+                                            <DialogContent>
+                                                Deleting this step could disrupt the plan's initial logic and cause
+                                                errors in subsequent steps. Make sure the next steps don't depend on
+                                                this step's outputs.
+                                            </DialogContent>
+                                            <DialogActions>
+                                                <DialogTrigger disableButtonEnhancement>
+                                                    <Button
+                                                        appearance="secondary"
+                                                        onClick={() => {
+                                                            setOpenDialog(false);
+                                                        }}
+                                                    >
+                                                        Cancel
+                                                    </Button>
+                                                </DialogTrigger>
+                                                <Button
+                                                    appearance="primary"
+                                                    onClick={() => {
+                                                        setOpenDialog(false);
+                                                        onDeleteStep(step.index);
+                                                    }}
+                                                >
+                                                    Yes, Delete Step
+                                                </Button>
+                                            </DialogActions>
+                                        </DialogBody>
+                                    </DialogSurface>
+                                </Dialog>
+                            ) : undefined
                         }
                     />
                     {step.description && (
@@ -73,13 +158,37 @@ export const PlanStepCard: React.FC<PlanStepCardProps> = ({ index, step }) => {
                             <Text weight="semibold">About: </Text> <Text>{step.description}</Text>
                         </div>
                     )}
-                    {step.stepInputs.length > 0 && (
-                        <div className={classes.inputs}>
+                    {inputs.length > 0 && (
+                        <div className={classes.parameters}>
                             <Text weight="semibold">Inputs: </Text>
-                            {step.stepInputs.map((input: IPlanInput) => {
+                            {inputs.map((input: IPlanInput) => {
+                                const onEditInput = (newValue: string) => {
+                                    const inputIndex = step.parameters.findIndex(
+                                        (element: IPlanInput) => element.Key === input.Key,
+                                    );
+                                    step.parameters[inputIndex] = {
+                                        Key: input.Key,
+                                        Value: newValue,
+                                    };
+                                };
                                 return (
-                                    <Badge color="informative" shape="rounded" appearance="tint" key={input.Key}>
-                                        {`${input.Key}: ${input.Value}`}
+                                    <PlanStepInput
+                                        input={input}
+                                        key={input.Key}
+                                        onEdit={onEditInput}
+                                        enableEdits={enableEdits}
+                                    />
+                                );
+                            })}
+                        </div>
+                    )}
+                    {step.outputs.length > 0 && (
+                        <div className={classes.parameters}>
+                            <Text weight="semibold">Outputs: </Text>
+                            {step.outputs.map((output: string) => {
+                                return (
+                                    <Badge color="informative" shape="rounded" appearance="tint" key={output}>
+                                        {output}
                                     </Badge>
                                 );
                             })}
--- samples/apps/copilot-chat-app/webapp/src/libs/utils/PlanUtils.ts
+++ samples/apps/copilot-chat-app/webapp/src/libs/utils/PlanUtils.ts
@@ -1,64 +1,8 @@
 // Copyright (c) Microsoft. All rights reserved.
 
-import { Constants } from '../../Constants';
-import { IPlan, IPlanInput } from '../models/Plan';
-
 export const isPlan = (object: string) => {
     // backslash has to be escaped since it's a JSON string
     // eslint-disable-next-line
     const planPrefix = `proposedPlan\":`;
     return object.indexOf(planPrefix) !== -1;
-};
-
-export const parsePlan = (response: string): IPlan | null => {
-    if (isPlan(response)) {
-        try {
-            const parsedResponse = JSON.parse(response);
-            const plan = parsedResponse.proposedPlan;
-            const userIntentPrefix = 'User intent: ';
-            const index = plan.description.indexOf(userIntentPrefix);
-
-            return {
-                userIntent: plan.description,
-                description: index !== -1 ? plan.description.substring(index + userIntentPrefix.length).trim() : '',
-                steps: extractPlanSteps(plan),
-            };
-        } catch (e: any) {
-            console.error('Error parsing plan: ' + response);
-        }
-    }
-    return null;
-};
-
-const extractPlanSteps = (plan: any) => {
-    // If plan came from Actio Planner, extract step parameters from top-level plan state
-    const planInputs = extractParameters(plan.state);
-
-    const planSteps = plan.steps;
-    return planSteps.map((step: any) => {
-        // If plan came from SequentialPlanner, extract step parameters from respective step object
-        const stepParameters = extractParameters(step.parameters);
-
-        return {
-            skill: step['skill_name'],
-            function: step['name'],
-            description: step['description'],
-            stepInputs: planSteps.length === 1 && planInputs.length > 0 ? planInputs : stepParameters,
-        };
-    });
-};
-
-const extractParameters = (parametersArray: IPlanInput[]) => {
-    const parameters: IPlanInput[] = [];
-    for (var param in parametersArray) {
-        if (
-            // Omit reserved context variable names
-            !Constants.sk.reservedWords.includes(parametersArray[param].Key.trim()) &&
-            parametersArray[param].Value.trim() !== ''
-        ) {
-            parameters.push(parametersArray[param]);
-        }
-    }
-
-    return parameters;
 };
