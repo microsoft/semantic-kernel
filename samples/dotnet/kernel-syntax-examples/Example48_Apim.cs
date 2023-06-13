86984d955a4150483d4e91e8e9d0104af0b5131f
markwallace-microsoft <mark.e.wallace@outlook.com>
2023-06-13 16:46:14 +0100

    Fix format issues

--- samples/dotnet/kernel-syntax-examples/Example48_Apim.cs
+++ samples/dotnet/kernel-syntax-examples/Example48_Apim.cs
@@ -85,16 +85,16 @@ public class BearerTokenCredential : TokenCredential
     // Constructor that takes a Bearer token string and its expiration date
     public BearerTokenCredential(AccessToken accessToken)
     {
-        _accessToken = accessToken;
+        this._accessToken = accessToken;
     }
 
     public override AccessToken GetToken(TokenRequestContext requestContext, CancellationToken cancellationToken)
     {
-        return _accessToken;
+        return this._accessToken;
     }
 
     public override ValueTask<AccessToken> GetTokenAsync(TokenRequestContext requestContext, CancellationToken cancellationToken)
     {
-        return new ValueTask<AccessToken>(_accessToken);
+        return new ValueTask<AccessToken>(this._accessToken);
     }
 }
