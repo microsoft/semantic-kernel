3d561e6f76989c66cb16b095c67c8187edfa6fe9
Tara W <tawalke@users.noreply.github.com>
2023-06-13 20:55:45 -0400

    Add Azure Cognitive Search vector connector (#1103)
    Azure Cognitive Search integration with Python SK

---
+++ python/semantic_kernel/connectors/memory/azure_cog_search/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) Microsoft. All rights reserved.
+
+from semantic_kernel.connectors.memory.azure_cog_search.acs_memory_store import (
+    CognitiveSearchMemoryStore,
+)
+
+__all__ = ["CognitiveSearchMemoryStore"]
