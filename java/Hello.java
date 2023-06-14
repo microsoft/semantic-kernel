27a3eadec5df64a4304df321b191c825515fd257
Bruno Borges <brborges@microsoft.com>
2023-06-14 16:05:49 -0700

    Auto: fix Java formatting

--- java/Hello.java
+++ java/Hello.java
@@ -1,1 +1,5 @@
-public class Hello {public static void main(String[] args) {System.out.println("Hello, World!");}}
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
