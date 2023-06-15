eb33e37ac5b71148788c5261b1ec293df389ed75
Lee Miller <lemiller@microsoft.com>
2023-06-15 12:59:41 -0700

    Fix day format in TimeSkill (#1466)
    The TimeSkill class was using the wrong format specifier for the day of
    the month, resulting in a two-digit year instead. This commit changes
    the format from "DD" to "dd", which correctly outputs the day as a
    number from 01 to 31. This fixes a potential confusion and inconsistency
    in the output of the TimeSkill methods.
    
    Co-authored-by: Lee Miller <lemillermicrosoft@users.noreply.github.com>

--- dotnet/src/SemanticKernel.UnitTests/CoreSkills/TimeSkillTests.cs
+++ dotnet/src/SemanticKernel.UnitTests/CoreSkills/TimeSkillTests.cs
@@ -43,6 +43,16 @@ public class TimeSkillTests
     }
 
     [Fact]
+    public void Day()
+    {
+        string expected = DateTime.Now.ToString("dd", CultureInfo.CurrentCulture);
+        var skill = new TimeSkill();
+        string result = skill.Day();
+        Assert.Equal(expected, result);
+        Assert.True(int.TryParse(result, out _));
+    }
+
+    [Fact]
     public void LastMatchingDayBadInput()
     {
         var skill = new TimeSkill();
