930c6337a9840794b0dbae9e225fa7a063e9399b
Lee Miller <lemiller@microsoft.com>
2023-06-15 12:10:06 -0700

    Update dotnet/src/Skills/Skills.MsGraph/OrganizationHierarchySkill.cs

--- dotnet/src/Skills/Skills.MsGraph/OrganizationHierarchySkill.cs
+++ dotnet/src/Skills/Skills.MsGraph/OrganizationHierarchySkill.cs
@@ -1,6 +1,5 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
-using System.Collections.Generic;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.SemanticKernel.Orchestration;
