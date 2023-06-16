b8db473356bbf0d9c385ec512e705127f23ee8fc
Teresa Hoang <tehoang@microsoft.com>
2023-06-16 14:58:35 -0700

    specifying range for threshold

--- samples/apps/copilot-chat-app/webapi/CopilotChat/Options/PlannerOptions.cs
+++ samples/apps/copilot-chat-app/webapi/CopilotChat/Options/PlannerOptions.cs
@@ -21,5 +21,6 @@ public class PlannerOptions
     /// The minimum relevancy score for a function to be considered during plan creation
     /// when using SequentialPlanner
     /// </summary>
+    [Range(0, 1.0)]
     public double? RelevancyThreshold { get; set; } = null;
 }
