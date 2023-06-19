51b4e2f50091ae54e7270a407dc600cedab7d5b2
Jerome Tremblay <jerome.tremblay@nuance.com>
2023-06-19 08:25:58 -0400

    Creating GuidanceBlock as a separete template block type

--- python/semantic_kernel/orchestration/context_variables.py
+++ python/semantic_kernel/orchestration/context_variables.py
@@ -1,24 +1,30 @@
 # Copyright (c) Microsoft. All rights reserved.
 
 
-from typing import Dict, Tuple
+from typing import Dict, List, Tuple, Union
 
 
 class ContextVariables:
-    def __init__(self, content: str = "", variables: Dict[str, str] = None) -> None:
+    def __init__(
+        self, content: str = "", variables: Dict[str, Union[str, Dict, List]] = None
+    ) -> None:
         """
         Initialize the ContextVariables instance with an optional content string.
 
         :param content: The content string to be stored as the main variable,
             defaults to an empty string.
         """
-        self._variables: Dict[str, str] = variables or {}
+        self._variables: Dict[str, Union[str, Dict, List]] = variables or {}
         self._main_key: str = "input"
         self._variables[self._main_key] = content
 
     @property
     def input(self) -> str:
-        return self._variables[self._main_key]
+        return str(self._variables[self._main_key])
+
+    @property
+    def dict(self) -> Dict[str, Union[str, Dict, List]]:
+        return self._variables
 
     def update(self, content: str) -> "ContextVariables":
         self._variables[self._main_key] = content
@@ -33,7 +39,7 @@ class ContextVariables:
             self._variables.update(new_vars._variables)
         return self
 
-    def set(self, name: str, value: str) -> "ContextVariables":
+    def set(self, name: str, value: Union[str, Dict, List]) -> "ContextVariables":
         if not name:
             raise ValueError("The variable name cannot be `None` or empty")
         name = name.lower()
@@ -45,14 +51,14 @@ class ContextVariables:
 
         return self
 
-    def get(self, name: str) -> Tuple[bool, str]:
+    def get(self, name: str) -> Tuple[bool, Union[str, Dict, List]]:
         name = name.lower()
         if name in self._variables:
             return True, self._variables[name]
 
         return False, ""
 
-    def __getitem__(self, name: str) -> str:
+    def __getitem__(self, name: str) -> Union[str, Dict, List]:
         name = name.lower()
         return self._variables[name]
 
@@ -67,7 +73,7 @@ class ContextVariables:
         return name in self._variables
 
     def __str__(self) -> str:
-        return self._variables[self._main_key]
+        return str(self._variables[self._main_key])
 
     def clone(self) -> "ContextVariables":
         main_content = self._variables.get(self._main_key, "")
--- python/semantic_kernel/template_engine/blocks/symbols.py
+++ python/semantic_kernel/template_engine/blocks/symbols.py
@@ -15,3 +15,12 @@ class Symbols:
     TAB = "\t"
     NEW_LINE = "\n"
     CARRIAGE_RETURN = "\r"
+
+    GUIDANCE_BLOCK_PREFIX_GEN = "gen "
+    GUIDANCE_BLOCK_PREFIX_SELECT = "select"
+    GUIDANCE_BLOCK_HB_START = "#"
+    GUIDANCE_BLOCK_HB_TILD = "~"
+    GUIDANCE_BLOCK_HB_END = "/"
+    GUIDANCE_BLOCK_HB_VAR = "%"
+    GUIDANCE_BLOCK_OR_KW = "or"
+    GUIDANCE_BLOCK_AND_KW = "and"
