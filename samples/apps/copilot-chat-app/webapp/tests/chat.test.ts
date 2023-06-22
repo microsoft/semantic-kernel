04e5403d7cb7909b518b24fe522453f2875a1485
amsacha <amsacha@microsoft.com>
2023-06-22 14:18:26 -0700

    separate out into tests; add helper util; still testing self eval

--- samples/apps/copilot-chat-app/webapp/tests/chat.test.ts
+++ samples/apps/copilot-chat-app/webapp/tests/chat.test.ts
@@ -1,21 +1,18 @@
 /* eslint-disable testing-library/prefer-screen-queries */
 import { expect, test } from '@playwright/test';
-
-const LLMresponsetimeout = 120000; // LLM can take a while to respond, wait upto 2 mins
-const ChatStateChangeWait = 500;
+import * as util from './testUtilCommon'
 
 /*
 This is a test suite to test the functionality of the copilot sample app.
-The Test suite goes through a bunch of sub-tests in a single long running test; 
-Running the tests individually and asynchronously can confuse the LLM when multiple 
-tests ask the LLM to respond to them.
-This incidentally also helps to simulate a longer running chat experience. 
-There are a number of smaller sub-tests in this unit test:
+The Test suite goes through a bunch of tests, opening a new chat session
+for each test so that chat history is not polluted and the LLM isn't confused; 
+Here is a list of things we test right now, either standalone or as part of another test:
 - Server is running
 - User login
 - Page has correct title
 - Send a message to the bot, and expect a response
 - Chat History check
+- Start New Chat
 - SK Skill test: joke and fun fact
 - Change chat title
 - Document upload
@@ -25,9 +22,26 @@ There are a number of smaller sub-tests in this unit test:
 */
 test.describe('Copilot Chat App Test Suite', () => {
     test('Server Health', async ({ page }) => { await ServerHealth(page) });
-    test('Long Running Chat Test', async ({ page }) => { 
+
+    test('Basic Bot Responses', async ({ page }) => { 
         test.setTimeout(300000);
-        await LongRunningChatTest(page) });
+        await BasicBotResponses(page) });
+    test('Chat Title Change', async ({ page }) => { 
+        test.setTimeout(300000);
+        await ChatTitleChange(page) });
+    test('Chat Document Upload', async ({ page }) => { 
+        test.setTimeout(300000);
+        await DocumentUpload(page) });
+
+    test('Planner Test: Klarna', async ({ page }) => { 
+        test.setTimeout(300000);
+        await KlarnaTest(page) });
+    test('Planner Test: Jira', async ({ page }) => { 
+        test.setTimeout(300000);
+        await JiraTest(page) });
+    test('Planner Test: Github', async ({ page }) => { 
+        test.setTimeout(300000);
+        await GithubTest(page) });
 });
 
 async function ServerHealth( page ) {
@@ -36,45 +50,14 @@ async function ServerHealth( page ) {
     await expect(page.getByText('Healthy')).toBeDefined();
 }
 
-async function LoginHelper(page) {
-    await page.goto('/');
-    // Expect the page to contain a "Login" button.
-    await page.getByRole('button').click();
-    // Clicking the login button should redirect to the login page.
-    await expect(page).toHaveURL(new RegExp('^' + process.env.REACT_APP_AAD_AUTHORITY));
-    // Login with the test user.
-    await page.getByPlaceholder('Email, phone, or Skype').click();
-    await page.getByPlaceholder('Email, phone, or Skype').fill(process.env.REACT_APP_TEST_USER_ACCOUNT as string);
-    await page.getByRole('button', { name: 'Next' }).click();
-    await page.getByPlaceholder('Password').click();
-    await page.getByPlaceholder('Password').fill(process.env.REACT_APP_TEST_USER_PASSWORD as string);
-    await page.getByRole('button', { name: 'Sign in' }).click();
-
-    // Select No if asked to stay signed in.
-    const isAskingStaySignedIn = await page.$$("text='Stay signed in?'");
-    if (isAskingStaySignedIn) {
-        await page.getByRole('button', { name: 'No' }).click();
-    }
-
-    // After login, the page should redirect back to the app.
-    await expect(page).toHaveTitle('Copilot Chat');
-}
-
 async function BasicBotResponses( page ) {    
-    // Send a message to the bot and wait for the response.
-    const responsePromise1 = page.waitForResponse('**/chat',  {timeout : LLMresponsetimeout});
-    await page.locator('#chat-input').click();
-    await page.locator('#chat-input').fill('Can you tell me a funny joke about penguins?');
-    await page.locator('#chat-input').press('Enter');
-    await responsePromise1;
+    await util.LoginAndCreateNewChat(page);
+    
+    const joke = "Can you tell me a funny joke about penguins?";
+    await util.SendChatMessageAndWaitForResponse(page, joke);
 
-    // Send a message to the bot and wait for the response.
-    await page.waitForTimeout(ChatStateChangeWait);
-    const responsePromise2 = page.waitForResponse('**/chat', {timeout : LLMresponsetimeout});
-    await page.locator('#chat-input').click();
-    await page.locator('#chat-input').fill('Tell me a fun fact about the cosmos!');
-    await page.locator('#chat-input').press('Enter');
-    await responsePromise2;
+    const funfact = "Tell me a fun fact about the cosmos!";
+    await util.SendChatMessageAndWaitForResponse(page, funfact);
 
     // Expect the chat history to contain 7 messages (both user messages and bot responses).
     const chatHistoryItems = page.getByTestId(new RegExp('chat-history-item-*'));
@@ -82,15 +65,23 @@ async function BasicBotResponses( page ) {
 
     // Expect the last message to be the bot's response.
     await expect(chatHistoryItems.last()).toHaveAttribute('data-username', 'Copilot');
+
+    await util.PostUnitTest(page);
 }
 
 async function ChatTitleChange(page) {
+    await util.LoginAndCreateNewChat(page);
+    
     await page.getByRole('button', { name: 'Edit conversation name' }).click();
     await page.locator('input[type="text"]').fill('Copilot Unit Tests');
     await page.locator('input[type="text"]').press('Enter');
+
+    await util.PostUnitTest(page);
 }
 
 async function DocumentUpload(page) {    
+    await util.LoginAndCreateNewChat(page);
+    
     const testFilename = 'Lorem_ipsum.pdf';
     const testFilepath = './../importdocument/sample-docs/' + testFilename;    
     await page.setInputFiles("input[type='file']", testFilepath)
@@ -98,120 +89,88 @@ async function DocumentUpload(page) {
     await page.getByRole('cell', { name: testFilename }).locator('path') // Check if corresponding cell for the file exists after upload
     await page.getByRole('tab', { name: 'Chat' }).click(); // Go back to the chat page
 
-    // Wait 3 seconds for document upload; Once document uploads the chat state changes
-    // and this can introduce non deterministic behavior depending on when the state change happens.
-    // So we wait to skew towards more determinism
-    await page.waitForTimeout(3000);
+    await util.PostUnitTest(page);
 }
 
 async function KlarnaTest(page) {
+    await util.LoginAndCreateNewChat(page);
+    
     // Plan generation and execution using Klarna (doesn't require auth)
 
     // Enable Klarna
-    await page.locator('div').filter({ hasText: /^DB$/ }).getByRole('button').click();
-    await page.getByRole('group').filter({ hasText: 'Klarna ShoppingKlarnaEnableSearch' }).getByRole('button', { name: 'Enable plugin' }).click();
-    await page.getByRole('button', { name: 'Enable', exact: true }).click();
-    await page.locator('.fui-DialogTitle__action > .fui-Button').click();
-
-    // Try using Klarna by sending a request to the bot and wait for the response.
-    await page.waitForTimeout(ChatStateChangeWait);
-    const responsePromiseKlarnaQuery1 = page.waitForResponse('**/chat', {timeout : LLMresponsetimeout}); 
-    await page.locator('#chat-input').click();
-    await page.locator('#chat-input').fill('Can you get me a list of prices of surface notebooks?');
-    await page.locator('#chat-input').press('Enter');    
-    await responsePromiseKlarnaQuery1;
-
-    // Try executing the plan that is returned
-    var buttonLocator = page.getByRole('button', { name: 'Yes, proceed' });
-    buttonLocator.click();
+    await util.OpenPluginEnablePopUp(page, 'Klarna ShoppingKlarnaEnableSearch');
+    await util.EnablePluginAndClosePopUp(page);
     
-    // Wait for LLM to respond to request by executing the plan
-    await page.waitForResponse('**/chat', {timeout : LLMresponsetimeout}); 
+    // Try using Klarna by sending a request to the bot and wait for the response.
+    const klarnaQuery = "Can you get me a list of prices of surface notebooks?";
+    await util.SendChatMessageAndWaitForResponse(page, klarnaQuery);
+    await util.ExecutePlanAndWaitForResponse(page);
 
     // Expect the last message to be the bot's response.
     const chatHistoryItems = page.getByTestId(new RegExp('chat-history-item-*'));
     await expect(chatHistoryItems.last()).toHaveAttribute('data-username', 'Copilot');
+    
     // Specifically accessing the us site of klarna so any results should have a dollar sign
     await expect(chatHistoryItems.last()).toContainText('$');
+
+    var chatbotResponse = await util.GetLastChatMessageContentsAsStringWHistory(page, chatHistoryItems);
+
+    await util.ChatBotSelfEval(page, klarnaQuery, chatbotResponse);
+
+    await util.PostUnitTest(page);
 }
 
 async function JiraTest(page) {
+    await util.LoginAndCreateNewChat(page);
+
     // Plan generation and execution using Jira (requires auth)
 
     // Enable Jira
-    await page.locator('div').filter({ hasText: /^DB$/ }).getByRole('button').click();
-    await page.getByRole('group').filter({ hasText: 'JiraAtlassianEnableAuthorize' }).getByRole('button', { name: 'Enable plugin' }).click();
+    await util.OpenPluginEnablePopUp(page, 'JiraAtlassianEnableAuthorize');
     
     // Enter Auth Credentials and server url
     await page.getByPlaceholder('Enter your Jira email').fill(process.env.REACT_APP_TEST_JIRA_EMAIL as string);
     await page.getByPlaceholder('Enter your Jira Personal Access Token').fill(process.env.REACT_APP_TEST_JIRA_ACCESS_TOKEN as string);
     await page.getByPlaceholder('Enter the server url').fill(process.env.REACT_APP_TEST_JIRA_SERVER_URL as string);
     
-    await page.getByRole('button', { name: 'Enable', exact: true }).click();
-    await page.locator('.fui-DialogTitle__action > .fui-Button').click();
+    await util.EnablePluginAndClosePopUp(page);
     
     // Try using Jira by sending a request to the bot and wait for it to respond.
-    await page.waitForTimeout(ChatStateChangeWait);
-    const responsePromiseJiraQuery1 = page.waitForResponse('**/chat', {timeout : LLMresponsetimeout}); 
-    await page.locator('#chat-input').click();
-    await page.locator('#chat-input').fill('Can you Get Issue details about SKTES-1 from jira ?');
-    await page.locator('#chat-input').press('Enter');    
-    await responsePromiseJiraQuery1;
-
-    // Try executing the plan that is returned
-    var buttonLocator = page.getByRole('button', { name: 'Yes, proceed' });
-    buttonLocator.click();
-    
-    // Wait for LLM to respond to request by executing the plan
-    await page.waitForResponse('**/chat', {timeout : LLMresponsetimeout}); 
+    const jiraQuery = "Can you Get Issue details about SKTES-1 from jira ?";
+    await util.SendChatMessageAndWaitForResponse(page, jiraQuery);
+    await util.ExecutePlanAndWaitForResponse(page);
 
     // Expect the last message to be the bot's response.
     const chatHistoryItems = page.getByTestId(new RegExp('chat-history-item-*'));
     await expect(chatHistoryItems.last()).toHaveAttribute('data-username', 'Copilot');
     await expect(chatHistoryItems.last()).toContainText('SKTES');
+
+    await util.PostUnitTest(page);
 }
 
 async function GithubTest(page) {
-    // Plan generation and execution using Jira (requires auth)
+    await util.LoginAndCreateNewChat(page);
+    
+    // Plan generation and execution using Github (requires auth)
 
-    // Enable Jira
-    await page.locator('div').filter({ hasText: /^DB$/ }).getByRole('button').click();
-    await page.getByRole('group').filter({ hasText: 'GitHubMicrosoftEnableIntegrate' }).getByRole('button', { name: 'Enable plugin' }).click();
+    // Enable Github
+    await util.OpenPluginEnablePopUp(page, 'GitHubMicrosoftEnableIntegrate');
     
     // Enter Auth Credentials and server url
     await page.getByPlaceholder('Enter your GitHub Personal Access Token').fill(process.env.REACT_APP_TEST_GITHUB_ACCESS_TOKEN as string);
     await page.getByPlaceholder('Enter the account owner of repository').fill(process.env.REACT_APP_TEST_GITHUB_ACCOUNT_OWNER as string);
     await page.getByPlaceholder('Enter the name of repository').fill(process.env.REACT_APP_TEST_GITHUB_REPOSITORY_NAME as string);
     
-    await page.getByRole('button', { name: 'Enable', exact: true }).click();
-    await page.locator('.fui-DialogTitle__action > .fui-Button').click();
+    await util.EnablePluginAndClosePopUp(page);
     
     // Try using Github by sending a request to the bot and wait for it to respond.
-    await page.waitForTimeout(ChatStateChangeWait);
-    const responsePromiseGithubQuery1 = page.waitForResponse('**/chat', {timeout : LLMresponsetimeout}); 
-    await page.locator('#chat-input').click();
-    await page.locator('#chat-input').fill('List the 5 most recent open pull requests');
-    await page.locator('#chat-input').press('Enter');    
-    await responsePromiseGithubQuery1;
-
-    // Try executing the plan that is returned
-    var buttonLocator = page.getByRole('button', { name: 'Yes, proceed' });
-    buttonLocator.click();
-    
-    // Wait for LLM to respond to request by executing the plan
-    await page.waitForResponse('**/chat', {timeout : LLMresponsetimeout}); 
+    const githubQuery = "List the 5 most recent open pull requests";
+    await util.SendChatMessageAndWaitForResponse(page, githubQuery);
+    await util.ExecutePlanAndWaitForResponse(page);
 
     // Expect the last message to be the bot's response.
     const chatHistoryItems = page.getByTestId(new RegExp('chat-history-item-*'));
     await expect(chatHistoryItems.last()).toHaveAttribute('data-username', 'Copilot');
-}
 
-async function LongRunningChatTest(page) {
-    await LoginHelper(page);
-    await BasicBotResponses(page);
-    await ChatTitleChange(page);
-    await DocumentUpload(page);
-    await KlarnaTest(page);
-    await JiraTest(page);
-    await GithubTest(page);
+    await util.PostUnitTest(page);
 }
---
+++ samples/apps/copilot-chat-app/webapp/tests/testUtilCommon.ts
@@ -0,0 +1,137 @@
+import { expect } from '@playwright/test';
+
+export const LLMresponsetimeout = 300000; // LLM can take a while to respond, wait upto 5 mins
+export const ChatStateChangeWait = 500;
+export const ChatResponseGreenThreshold = 0.7;
+
+export async function LoginHelper(page) {
+    await page.goto('/');
+    // Expect the page to contain a "Login" button.
+    await page.getByRole('button').click();
+    // Clicking the login button should redirect to the login page.
+    await expect(page).toHaveURL(new RegExp('^' + process.env.REACT_APP_AAD_AUTHORITY));
+    // Login with the test user.
+    await page.getByPlaceholder('Email, phone, or Skype').click();
+    await page.getByPlaceholder('Email, phone, or Skype').fill(process.env.REACT_APP_TEST_USER_ACCOUNT as string);
+    await page.getByRole('button', { name: 'Next' }).click();
+    await page.getByPlaceholder('Password').click();
+    await page.getByPlaceholder('Password').fill(process.env.REACT_APP_TEST_USER_PASSWORD as string);
+    await page.getByRole('button', { name: 'Sign in' }).click();
+
+    // Select No if asked to stay signed in.
+    const isAskingStaySignedIn = await page.$$("text='Stay signed in?'");
+    if (isAskingStaySignedIn) {
+        await page.getByRole('button', { name: 'No' }).click();
+    }
+
+    // After login, the page should redirect back to the app.
+    await expect(page).toHaveTitle('Copilot Chat');
+
+    await page.getByRole('button', { name: 'Create new conversation' }).click();
+    await page.getByRole('menuitem', { name: 'Add a new Bot' }).click();
+}
+
+export async function PostUnitTest(page)
+{
+    // Change focus to somewhere else on the page so that the trace shows the result of the previous action
+    await page.locator('#chat-input').click();
+}
+
+export async function LoginAndCreateNewChat(page)
+{
+    await LoginHelper(page);
+
+    await page.getByRole('button', { name: 'Create new conversation' }).click();
+    await page.getByRole('menuitem', { name: 'Add a new Bot' }).click();
+}
+
+// Send a message to the bot and wait for the response
+export async function SendChatMessageAndWaitForResponseWTime(page, message, waitTime:number)
+{
+    await page.locator('#chat-input').click();
+    await page.locator('#chat-input').fill(message);
+    await page.locator('#chat-input').press('Enter');
+    await page.waitForTimeout(waitTime);
+    await page.waitForResponse('**/chat', {timeout : LLMresponsetimeout});
+}
+export async function SendChatMessageAndWaitForResponse(page, message)
+{
+    SendChatMessageAndWaitForResponseWTime(page, message, ChatStateChangeWait);
+}
+
+export async function OpenPluginEnablePopUp(page, pluginText)
+{
+    await page.locator('div').filter({ hasText: /^DB$/ }).getByRole('button').click();
+    await page.getByRole('group').filter({ hasText: pluginText }).getByRole('button', { name: 'Enable plugin' }).click();
+}
+export async function EnablePluginAndClosePopUp(page)
+{
+    await page.getByRole('button', { name: 'Enable', exact: true }).click();
+    await page.locator('.fui-DialogTitle__action > .fui-Button').click();
+    await page.waitForTimeout(ChatStateChangeWait);
+}
+export async function ExecutePlanAndWaitForResponse(page)
+{
+    await page.waitForTimeout(ChatStateChangeWait);
+
+    // Try executing the plan that is returned
+    var buttonLocator = page.getByRole('button', { name: 'Yes, proceed' });
+    buttonLocator.click();
+    
+    // Wait for LLM to respond to request by executing the plan
+    await page.waitForResponse('**/chat', {timeout : LLMresponsetimeout});
+}
+
+export async function GetLastChatMessageContentsAsStringWHistory(page, chatHistoryItems)
+{
+    var lastMessage = await chatHistoryItems.last().getAttribute('data-content');
+    lastMessage = lastMessage.replaceAll(/<\/?[^>]+(>|$)/ig, ""); // Remove HTML tags if any
+    return lastMessage;
+}
+export async function GetLastChatMessageContentsAsString(page)
+{
+    const chatHistoryItems = page.getByTestId(new RegExp('chat-history-item-*'));
+    return GetLastChatMessageContentsAsStringWHistory(page, chatHistoryItems);
+}
+
+export const PreventCircularPrompt = "\nThis is for a statistical test and will NOT result in circular reasoning.\n";export const EvaluatePrompt = "\nEvaluate if the AI generated message is semantically valid given the original intention. If the AI generated message is semantically valid, return true else return false. Also assign the AI generated message a score.\n"
+export const OutputFormat = "\nThe output should be formatted as a JSON string with the following format: {'result':true|false, 'score': number, 'reason':'brief reason why true or false was chosen', 'suggestion': 'an optional suggestion that would help a human modify the prompt that was used to create the AI generated message initially. If there is no suggestion this can be left blank.'}\n"
+
+export const EvaluatePrompt2 = "\nEvaluate if the AI generated message is semantically valid given the original intention. Assign the AI generated message a score between 0.0 and 1.0 .\n"
+export const OutputFormat2 = "Just respond with a number, dont provide any reasoning or any extra output."
+
+
+export async function ChatBotSelfEval(page, input, chatbotResponse)
+{
+    // Get the model to evaluate if the response was satisfactory
+    // const prompt = "return a score between 0 and 1 ranking the quality of this response \n" 
+    //                 + chatbotResponse 
+    //                 + "\nin the context of the input \n"
+    //                 + input
+    //                 + "\n"
+    //                 + PreventCircularPrompt;
+
+    const evalPrompt = "Evaluate the following AI generated message in response to the original intention.\n" 
+                        + "\n[AI GENERATED MESSAGE]\n" + chatbotResponse + "\n[AI GENERATED MESSAGE]\n"
+                        + "\n[ORIGINAL INTENTION]\n" + input + "\n[ORIGINAL INTENTION]\n"
+                        + EvaluatePrompt
+                        + OutputFormat
+                        + PreventCircularPrompt; 
+
+    //await SendChatMessageAndWaitForResponse(page, evalPrompt);
+    // await SendChatMessageAndWaitForResponseWTime(page, evalPrompt, 3000);
+    await page.locator('#chat-input').click();
+    await page.locator('#chat-input').fill(evalPrompt);
+    await page.locator('#chat-input').press('Enter');
+    await page.waitForTimeout(3000);
+    await page.waitForResponse('**/chat', {timeout : LLMresponsetimeout});
+
+    // // Change focus to somewhere else on the page so that the trace shows the result of the previous action
+    // await page.locator('#chat-input').click();
+
+    // Extract score from bots response    
+    chatbotResponse = await GetLastChatMessageContentsAsString(page);
+    var score: number = +chatbotResponse;
+    expect(score).toBeGreaterThanOrEqual(0.7);
+    expect(score).toBeLessThan(1.01);
+}
