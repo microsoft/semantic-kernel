f6a8aff8e2660e2e58c0e3e9add6d60554253cd7
Lee Miller <lemiller@microsoft.com>
2023-06-26 10:06:18 -0700

    update example comment

--- dotnet/samples/KernelSyntaxExamples/Example51_StepwisePlanner.cs
+++ dotnet/samples/KernelSyntaxExamples/Example51_StepwisePlanner.cs
@@ -167,50 +167,67 @@ public static class Example51_StepwisePlanner
     }
 }
 
+// RunTextCompletion
 // *****************************************************
-// Goal :Who is Leo DiCaprio's girlfriend? What is her current age raised to the 0.43 power?
+// Question: Who is the current president of the United States? What is his current age divided by 2
+// Result: The current president of the United States is Joe Biden. His current age divided by 2 is 40.
+// Steps Taken: 10
+// Skills Used: 4 (WebSearch.Search(2), time.Date(1), advancedCalculator.Calculator(1))
+// Time Taken: 00:00:53.6331324
 // *****************************************************
-// 01:54:27 Observation : Sarah Stier // Getty Images March 2021: They enjoy a beachside getaway. DiCaprio and Morrone headed to Malibu with friends for brief holiday. The actress shared photos from their trip to...
-// 01:54:32 Observation : Michele Morrone (born 3 October 1990) is an Italian actor, model, singer, and fashion designer appearing in both Italian and Polish films. He gained international recognition after portraying the role of Massimo Torricelli in the 2020 erotic romantic drama 365 Days . Early life
-// 01:54:37 Observation : Camila Rebeca Morrone Polak [3] (born June 16, 1997) [4] is an American model and actress. She made her acting debut in the James Franco film Bukowski and subsequently appeared in the films Death Wish and Never Going Back, both premiering at the Sundance Film Festival in January 2018. Early life and education
-// 01:54:44 Observation : Answer:3.9218486893172186
+// RunChatCompletion
 // *****************************************************
-// Result :Leo DiCaprio's girlfriend is Camila Morrone and her current age raised to the 0.43 power is 3.92.
-// Steps Taken: 5
+// Question: Who is the current president of the United States? What is his current age divided by 2
+// Result: The current president of the United States is Joe Biden. His current age divided by 2 is 40.5.
+// Steps Taken: 9
+// Skills Used: 7 (WebSearch.Search(4), time.Year(1), time.Date(1), advancedCalculator.Calculator(1))
+// Time Taken: 00:01:13.3766860
 // *****************************************************
+// RunTextCompletion
 // *****************************************************
-// Goal :Who is the current president of the United States? What is his current age divided by 2
+// Question: Who is Leo DiCaprio's girlfriend? What is her current age raised to the (his current age)/100 power?
+// Result: Leo DiCaprio's girlfriend is Camila Morrone. Her current age raised to the (his current age)/100 power is 4.935565735151678.
+// Steps Taken: 6
+// Skills Used: 5 (WebSearch.Search(3), time.Year(1), advancedCalculator.Calculator(1))
+// Time Taken: 00:00:37.8941510
 // *****************************************************
-// 01:54:52 Observation : U.S. facts and figures Presidents, vice presidents, and first ladies Presidents, vice presidents, and first ladies Learn about the duties of president, vice president, and first lady of the United States. Find out how to contact and learn more about current and past leaders. President of the United States Vice president of the United States
-// 01:55:00 Observation : On January 20, 2021, at the age of 78, Biden became the oldest president in U.S. history, the first to have a female vice president, and the first from Delaware. ... : 411-414, 419 Obama campaign staffers called Biden's blunders "Joe bombs" and kept Biden uninformed about strategy discussions, which in turn irked Biden. Relations between the ...
-// 01:55:04 Observation : Answer:39
+// RunChatCompletion
 // *****************************************************
-// Result :The current president of the United States is Joe Biden, and his current age divided by 2 is 39.
-// Steps Taken: 4
+// Question: Who is Leo DiCaprio's girlfriend? What is her current age raised to the (his current age)/100 power?
+// Result: Leo DiCaprio's girlfriend is Camila Morrone. Her current age raised to the power of (his current age)/100 is approximately 4.94.
+// Steps Taken: 9
+// Skills Used: 5 (WebSearch.Search(3), time.Year(1), advancedCalculator.Calculator(1))
+// Time Taken: 00:01:17.6742136
 // *****************************************************
+// RunTextCompletion
 // *****************************************************
-// Goal :What is the capital of France? Who is that cities current mayor? What percentage of their life has been in the 21st century as of today?
+// Question: What is the capital of France? Who is that cities current mayor? What percentage of their life has been in the 21st century as of today?
+// Result: The capital of France is Paris. The current mayor of Paris is Anne Hidalgo. She has spent 36.51% of her life in the 21st century as of 2023.
+// Steps Taken: 7
+// Skills Used: 4 (WebSearch.Search(3), advancedCalculator.Calculator(1))
+// Time Taken: 00:00:41.6837628
 // *****************************************************
-// 01:55:13 Observation : Paris is the capital of what country? Paris; Eiffel Tower
-// 01:55:24 Observation : Early life and education [ edit] Family background and youth [ edit] Hidalgo was born in San Fernando, province of Cádiz, Spain. [1] Her paternal grandfather was a Spanish Socialist who became a refugee in France after the end of the Spanish Civil War along with his wife and his four children.
-// 01:55:28 Observation : Friday, June 2, 2023 1:55 PM
-// 01:55:41 Observation : Answer:35.9375
+// RunChatCompletion
 // *****************************************************
-// Result :The capital of France is Paris. The current mayor of Paris is Anne Hidalgo. As of 2023, 35.9375 percent of her life has been in the 21st century.
-// Steps Taken: 8
+// Question: What is the capital of France? Who is that cities current mayor? What percentage of their life has been in the 21st century as of today?
+// Result: The capital of France is Paris. The current mayor of Paris is Anne Hidalgo, who was born on June 19, 1959. As of today, she has lived for 64 years, with 23 of those years in the 21st century. Therefore, 35.94% of her life has been spent in the 21st century.
+// Steps Taken: 14
+// Skills Used: 12 (WebSearch.Search(8), time.Year(1), advancedCalculator.Calculator(3))
+// Time Taken: 00:02:06.6682909
 // *****************************************************
+// RunTextCompletion
 // *****************************************************
-// Goal :What is the current day of the calendar year? Using that as an angle in degrees, what is the area of a unit circle with that angle?
+// Question: What is the current day of the calendar year? Using that as an angle in degrees, what is the area of a unit circle with that angle?
+// Result: The current day of the calendar year is 177. The angle in degrees corresponding to this day is 174.6. The area of a unit circle with that angle is 0.764 * pi.
+// Steps Taken: 16
+// Skills Used: 2 (time.DayOfYear(1), time.Date(1))
+// Time Taken: 00:01:29.9931039
 // *****************************************************
-// 10:51:27 warn: object[0] Observation : Error invoking action time.DayOfYear : Unknown error (UnknownError): The function 'time.DayOfYear' was not found.
-// 10:51:36 warn: object[0] Observation : 05
-// 10:51:38 warn: object[0] Observation : 06
-// 10:51:50 warn: object[0] Observation : 2023
-// 10:52:02 warn: object[0] Observation : Answer:3
-// 10:53:06 warn: object[0] Observation : Answer:1.361356816555577
+// RunChatCompletion
 // *****************************************************
-// Result :The area of the sector of the unit circle with the angle of 156 degrees is 1.36 square units.
-// Steps Taken: 24
-// Skills Used: Total Skills Called: 6 (time.DayOfYear(1), time.Day(1), time.MonthNumber(1), time.Year(1), calculator.Calculator(2))
+// Question: What is the current day of the calendar year? Using that as an angle in degrees, what is the area of a unit circle with that angle?
+// Result: The current day of the year is 177. Using that as an angle in degrees (approximately 174.58), the area of a unit circle with that angle is approximately 1.523 square units.
+// Steps Taken: 11
+// Skills Used: 9 (time.Now(1), time.DayOfYear(1), time.DaysBetween(1), time.MonthNumber(1), time.Day(1), advancedCalculator.Calculator(4))
+// Time Taken: 00:01:41.5585861
 // *****************************************************
-// == DONE ==
