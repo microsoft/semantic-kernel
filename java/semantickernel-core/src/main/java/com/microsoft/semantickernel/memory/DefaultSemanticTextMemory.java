4af78a41fa9e1be14aba6fddf058b660c94951bb
David Grieve <dagrieve@microsoft.com>
2023-06-27 09:42:01 -0400

    Remove SemanticTextMemory#merge(MemoryQueryResult)

--- java/semantickernel-core/src/main/java/com/microsoft/semantickernel/memory/DefaultSemanticTextMemory.java
+++ java/semantickernel-core/src/main/java/com/microsoft/semantickernel/memory/DefaultSemanticTextMemory.java
@@ -156,11 +156,6 @@ public class DefaultSemanticTextMemory implements SemanticTextMemory {
                 .flatMap(record -> this._storage.upsertAsync(collection, record));
     }
 
-    @Override
-    public SemanticTextMemory merge(MemoryQueryResult b) {
-        throw new NotSupportedException("Pending implementation");
-    }
-
     public static class Builder implements SemanticTextMemory.Builder {
 
         @Nullable MemoryStore storage = null;
