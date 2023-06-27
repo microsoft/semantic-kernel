aec848d4b4ce0506bf85554b3f3fbb45ea76fa4d
Lee Miller <lemiller@microsoft.com>
2023-06-27 09:31:31 -0700

    🔌 Add extension methods for importing plugin manifest
    This commit adds two extension methods for the IKernel interface that
    allow importing a plugin manifest document from a URL or a string. The
    plugin manifest is a JSON document that lists the names and URLs of
    remote plugins that provide chat GPT skills. The extension methods use
    the existing ImportChatGptPluginSkillFromUrlAsync method to import each
    plugin and its functions, and return a dictionary of the imported
    functions. The commit also adds a model class for the plugin manifest
    and its contents.

---
+++ dotnet/src/Skills/Skills.OpenAPI/Extensions/KernelPluginManifestExtensions.cs
@@ -0,0 +1,89 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.SemanticKernel.SkillDefinition;
+using Microsoft.SemanticKernel.Skills.OpenAPI.Model;
+
+#pragma warning disable IDE0130
+// ReSharper disable once CheckNamespace
+namespace Microsoft.SemanticKernel;
+#pragma warning restore IDE0130
+
+/// <summary>
+/// Class for extensions methods for IKernel interface.
+/// </summary>
+public static class KernelPluginManifestExtensions
+{
+    /// <summary>
+    /// Imports plugin manifest document from a URL.
+    /// </summary>
+    /// <param name="kernel">Semantic Kernel instance.</param>
+    /// <param name="url">Url to in which to retrieve the plugin manifest.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>A dictionary of all the plugins and their functions that were imported.</returns>
+    public static async Task<IDictionary<string, ISKFunction>?> ImportPluginManifestFromUrlAsync(
+        this IKernel kernel,
+        Uri url,
+        CancellationToken cancellationToken = default)
+    {
+        using var internalHttpClient = HttpClientProvider.GetHttpClient(kernel.Config, null, kernel.Log);
+
+        using HttpResponseMessage response = await internalHttpClient.GetAsync(url, cancellationToken).ConfigureAwait(false);
+        response.EnsureSuccessStatusCode();
+
+        string pluginJson = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+        var pluginManifest = ParsePluginJson(pluginJson);
+
+        Dictionary<string, ISKFunction> functions = new();
+
+        foreach (var plugin in pluginManifest.Plugins)
+        {
+            var result = await kernel.ImportChatGptPluginSkillFromUrlAsync(plugin.Value.Functions.FirstOrDefault().SkillName, plugin.Value.Url, null, cancellationToken).ConfigureAwait(false);
+            foreach (var function in result)
+            {
+                functions.Add(function.Key, function.Value);
+            }
+        }
+
+        return functions;
+    }
+
+    /// <summary>
+    /// Imports plugin manifest document from a string.
+    /// </summary>
+    /// <param name="kernel">Semantic Kernel instance.</param>
+    /// <param name="pluginManifestJson">Plugin manifest json.</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <returns>A dictionary of all the plugins and their functions that were imported.</returns>
+    public static async Task<IDictionary<string, ISKFunction>?> ImportPluginManifestAsync(
+        this IKernel kernel,
+        string pluginManifestJson,
+        CancellationToken cancellationToken = default)
+    {
+        var pluginManifest = ParsePluginJson(pluginManifestJson);
+
+        Dictionary<string, ISKFunction> functions = new();
+
+        foreach (var plugin in pluginManifest.Plugins)
+        {
+            var result = await kernel.ImportChatGptPluginSkillFromUrlAsync(plugin.Value.Functions.FirstOrDefault().SkillName, plugin.Value.Url, null, cancellationToken).ConfigureAwait(false);
+            foreach (var function in result)
+            {
+                functions.Add(function.Key, function.Value);
+            }
+        }
+
+        return functions;
+    }
+
+    private static PluginManifest ParsePluginJson(string pluginJson)
+    {
+        return JsonSerializer.Deserialize<PluginManifest>(pluginJson) ?? throw new InvalidOperationException("Invalid plugin manifest");
+    }
+}
---
+++ dotnet/src/Skills/Skills.OpenAPI/Model/PluginManifest.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using Microsoft.SemanticKernel.SkillDefinition;
+
+namespace Microsoft.SemanticKernel.Skills.OpenAPI.Model;
+
+/// <summary>
+/// A plugin manifest that lists plugins and their names.
+/// </summary>
+public sealed class PluginManifest
+{
+    /// <summary>
+    /// The list of plugins.
+    /// </summary>
+    public Dictionary<string, RemotePlugin> Plugins { get; set; } = new Dictionary<string, RemotePlugin>();
+}
+
+/// <summary>
+/// A remote plugin definition.
+/// </summary>
+public sealed class RemotePlugin
+{
+    /// <summary>
+    /// The functions available in the remote plugin.
+    /// </summary>
+    public List<FunctionView> Functions { get; set; } = new List<FunctionView>();
+
+    /// <summary>
+    /// The URL of the remote plugin.
+    /// </summary>
+    public Uri Url { get; set; } = null!;
+}
