fd7bfc06f171cf291f51f0928a9626d7bf2f9a2a
Jean-Sylvain Boige <jsboige@gmail.com>
2023-06-30 02:22:04 +0200

    Adding port defaults

--- dotnet/src/Connectors/Connectors.AI.Oobabooga/TextCompletion/OobaboogaTextCompletion.cs
+++ dotnet/src/Connectors/Connectors.AI.Oobabooga/TextCompletion/OobaboogaTextCompletion.cs
@@ -50,16 +50,16 @@ public sealed class OobaboogaTextCompletion : ITextCompletion
     /// Initializes a new instance of the <see cref="OobaboogaTextCompletion"/> class.
     /// </summary>
     /// <param name="endpoint">The service API endpoint to which requests should be sent.</param>
-    /// <param name="blockingPort">The port used for handling blocking requests.</param>
-    /// <param name="streamingPort">The port used for handling streaming requests.</param>
+    /// <param name="blockingPort">The port used for handling blocking requests. Default value is 5000</param>
+    /// <param name="streamingPort">The port used for handling streaming requests. Default value is 5005</param>
     /// <param name="httpClient">Optional. The HTTP client used for making blocking API requests. If not specified, a default client will be used.</param>
     /// <param name="useWebSocketsPooling">If true, websocket clients will be recycled in a reusable pool as long as concurrent calls are detected</param>
     /// <param name="keepAliveWebSocketsDuration">When pooling is enabled, pooled websockets are flushed on a regular basis when no more connections are made. This is the time to keep them in pool before flushing</param>
     /// <param name="webSocketFactory">Optional. The WebSocket factory used for making streaming API requests. Note that only when pooling is enabled will websocket be recycled and reused for the specified duration. Otherwise, a new websocket is created for each call and closed and disposed afterwards, to prevent data corruption from concurrent calls.</param>
     /// <param name="enforcedConcurrentCallSemaphore">You can optionally set a hard limit on the max number of concurrent calls to the streaming completion method by providing a <see cref="SemaphoreSlim"/> instance that will be used to control concurrent access. Calls in excess will wait for existing consumers to release the semaphore</param>
     public OobaboogaTextCompletion(Uri endpoint,
-        int blockingPort,
-        int streamingPort,
+        int blockingPort = 5000,
+        int streamingPort = 5005,
         HttpClient? httpClient = null,
         bool useWebSocketsPooling = true,
         int keepAliveWebSocketsDuration = 100,
