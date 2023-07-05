b1f6215200961f785096398f3b74c3f7b702bc7a
Dmytro Struk <13853051+dmytrostruk@users.noreply.github.com>
2023-07-05 20:06:28 +0100

    Added separate project for App Insights examples

---
+++ dotnet/samples/ApplicationInsightsExample/Program.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+
+namespace ApplicationInsightsExample;
+
+public static class Program
+{
+    public static void Main()
+    {
+        Console.Write("Test");
+        Console.ReadKey();
+    }
+}
