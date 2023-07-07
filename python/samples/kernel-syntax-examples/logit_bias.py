5ccc7635e6f813297a22eab4b9a2d7f0e4540287
Alisha Maddy <alisha831maddy@gmail.com>
2023-07-07 15:46:58 -0700

    logit_bias example in progress

--- python/samples/kernel-syntax-examples/logit_bias.py
+++ python/samples/kernel-syntax-examples/logit_bias.py
@@ -13,23 +13,27 @@ import semantic_kernel.connectors.ai.open_ai as sk_oai
 
 
 async def chat_request_example():
+    # To use Logit Bias you will need to know the token ids of the words you want to use.
+    # Getting the token ids using the GPT Tokenizer: https: // platform.openai.com/tokenizer
     kernel = sk.Kernel()
     api_key, org_id = sk.openai_settings_from_dot_env()
     kernel.add_chat_service(
         "chat-gpt", sk_oai.OpenAIChatCompletion("gpt-3.5-turbo", api_key, org_id)
     )
-
+    # The following text is the tokenized version of the book related tokens
+    # novel literature reading author library story chapter paperback hardcover
+    # ebook publishing fiction nonfiction manuscript textbook bestseller bookstore
+    # reading list bookworm"
     keys = [
         3919, 626, 17201, 1300, 25782, 9800, 32016, 13571, 43582, 20189,
         1891, 10424, 9631, 16497, 12984, 20020, 24046, 13159, 805, 15817,
         5239, 2070, 13466, 32932, 8095, 1351, 25323
     ]
-
+    # This will make the model try its best to avoid any of the above related words.
     settings = ChatRequestSettings()
-
     # Map each token in the keys list to a bias value from -100 (a potential ban) to 100 (exclusive selection)
     for key in keys:
-        # For this example, each key is mapped to a value of -100
+        # -100 to potentially ban all the tokens from the list.
         settings.token_selection_biases[key] = -100
 
     print("Chat content:")
@@ -47,15 +51,16 @@ async def chat_request_example():
     prompt_template.add_user_message("Hi, I'm looking some suggestions")
     function_config = sk.SemanticFunctionConfig(prompt_config, prompt_template)
     chat_function = kernel.register_semantic_function("ChatBot", "Chat", function_config)
-
     answer = await kernel.run_async(chat_function, input_vars=None)
-    print(answer)
-
+    print(f"Assistant:> {answer}")
+    prompt_template.add_user_message(
+        "I love history and philosophy, I'd like to learn something new about Greece, any suggestion?")
+    answer = await kernel.run_async(chat_function, input_vars=None)
+    print(f"Assistant:> {answer}")
     return
 
 
 async def main() -> None:
-
     await chat_request_example()
 
 if __name__ == "__main__":
