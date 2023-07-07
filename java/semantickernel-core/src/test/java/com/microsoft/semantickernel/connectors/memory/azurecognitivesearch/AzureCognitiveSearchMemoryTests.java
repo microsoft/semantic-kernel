e7f803678ad2f4b753cc73aa25af133a6079c061
John Oliver <1615532+johnoliver@users.noreply.github.com>
2023-07-07 22:32:55 +0100

    Java: Apply formatting rules (#1878)
    No code changes, just apply formatting rules

--- java/semantickernel-core/src/test/java/com/microsoft/semantickernel/connectors/memory/azurecognitivesearch/AzureCognitiveSearchMemoryTests.java
+++ java/semantickernel-core/src/test/java/com/microsoft/semantickernel/connectors/memory/azurecognitivesearch/AzureCognitiveSearchMemoryTests.java
@@ -1,6 +1,9 @@
 // Copyright (c) Microsoft. All rights reserved.
 package com.microsoft.semantickernel.connectors.memory.azurecognitivesearch;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import com.azure.core.credential.AzureKeyCredential;
 import com.azure.core.http.HttpHeaders;
 import com.azure.core.http.HttpPipeline;
@@ -15,10 +18,12 @@ import com.azure.search.documents.indexes.SearchIndexClientBuilder;
 import com.microsoft.semantickernel.Kernel;
 import com.microsoft.semantickernel.builders.SKBuilders;
 import com.microsoft.semantickernel.memory.MemoryQueryResult;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.mockito.invocation.InvocationOnMock;
+
 import reactor.core.publisher.Mono;
 
 import java.nio.charset.StandardCharsets;
@@ -26,13 +31,9 @@ import java.util.Base64;
 import java.util.List;
 import java.util.function.Function;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 // TODO: This class is in the core package to avoid a
 //       circular dependency with the connectors module.
-class AzureCognitiveSearchMemoryTests
-{
+class AzureCognitiveSearchMemoryTests {
 
     // com.azure.core.implementation.http.rest.RestProxyBase.RestProxyBase
     // expects a non-null tracer. There is a TracerProvider in the Azure SDK
@@ -46,12 +47,10 @@ class AzureCognitiveSearchMemoryTests
         }
 
         @Override
-        public void end(String errorMessage, Throwable throwable, Context context) {
-        }
+        public void end(String errorMessage, Throwable throwable, Context context) {}
 
         @Override
-        public void setAttribute(String key, String value, Context context) {
-        }
+        public void setAttribute(String key, String value, Context context) {}
 
         @Override
         public boolean isEnabled() {
@@ -59,7 +58,6 @@ class AzureCognitiveSearchMemoryTests
             return false;
         }
     }
-
 
     // Kernel is configured with a real AzureCognitiveSearchMemory which is backed
     // by a mocked HttpPipeline. The HttpPipeline is mocked to return a mocked
@@ -89,18 +87,21 @@ class AzureCognitiveSearchMemoryTests
 
         HttpPipeline pipeline = setUpHttpPipeline();
 
-        SearchIndexAsyncClient searchIndexAsyncClient = new SearchIndexClientBuilder()
-                .endpoint("https://fake-random-test-host/fake-path")
-                .credential(new AzureKeyCredential("fake-key"))
-                .pipeline(pipeline)
-                .httpLogOptions(new HttpLogOptions().setLogLevel(HttpLogDetailLevel.BODY_AND_HEADERS))
-                .buildAsyncClient();
+        SearchIndexAsyncClient searchIndexAsyncClient =
+                new SearchIndexClientBuilder()
+                        .endpoint("https://fake-random-test-host/fake-path")
+                        .credential(new AzureKeyCredential("fake-key"))
+                        .pipeline(pipeline)
+                        .httpLogOptions(
+                                new HttpLogOptions()
+                                        .setLogLevel(HttpLogDetailLevel.BODY_AND_HEADERS))
+                        .buildAsyncClient();
 
-        kernel = SKBuilders.kernel()
-                .setKernelConfig(SKBuilders.kernelConfig().build())
-                .withMemory(new AzureCognitiveSearchMemory(searchIndexAsyncClient))
-                .build();
-
+        kernel =
+                SKBuilders.kernel()
+                        .setKernelConfig(SKBuilders.kernelConfig().build())
+                        .withMemory(new AzureCognitiveSearchMemory(searchIndexAsyncClient))
+                        .build();
     }
 
     private HttpPipeline setUpHttpPipeline() {
@@ -108,8 +109,7 @@ class AzureCognitiveSearchMemoryTests
         HttpPipeline pipeline = Mockito.mock(HttpPipeline.class);
 
         Tracer tracer = new NoOpTracer();
-        Mockito.when(pipeline.getTracer())
-                .thenReturn(tracer);
+        Mockito.when(pipeline.getTracer()).thenReturn(tracer);
 
         Mockito.when(pipeline.send(Mockito.any(HttpRequest.class), Mockito.any(Context.class)))
                 .thenAnswer(this::answerHttpRequest);
@@ -123,12 +123,14 @@ class AzureCognitiveSearchMemoryTests
         // HttpResponse is based on this api-version
         assertTrue(request.getUrl().getQuery().contains("api-version=2021-04-30-Preview"));
 
-//        System.err.println(request.getHttpMethod() + " " + request.getUrl() + "\n\t" + request.getHeaders());
+        //        System.err.println(request.getHttpMethod() + " " + request.getUrl() + "\n\t" +
+        // request.getHeaders());
         byte[] body = responseBody.apply(request).getBytes(StandardCharsets.UTF_8);
         int statusCode = responseStatusCode.apply(request);
-        HttpHeaders httpHeaders = new HttpHeaders().add("Content-Type", "application/json; charset=utf-8");
+        HttpHeaders httpHeaders =
+                new HttpHeaders().add("Content-Type", "application/json; charset=utf-8");
 
-        HttpResponse httpResponse= Mockito.mock(HttpResponse.class);
+        HttpResponse httpResponse = Mockito.mock(HttpResponse.class);
         Mockito.when(httpResponse.getRequest()).thenReturn(request);
         Mockito.when(httpResponse.getHeaders()).thenReturn(httpHeaders);
         Mockito.when(httpResponse.getBodyAsByteArray()).thenReturn(Mono.just(body));
@@ -138,180 +140,185 @@ class AzureCognitiveSearchMemoryTests
 
     @Test
     void azureCognitiveSearchMemoryStoreShouldBeProperlyInitialized() {
-        //Arrange
+        // Arrange
         responseBody = httpRequest -> "{\"value\": [{\"name\": \"fake-index\"}]}";
         responseStatusCode = httpResponse -> 200;
 
-        //Act
-        //This call triggers a subsequent call to Azure Cognitive Search Memory store.
+        // Act
+        // This call triggers a subsequent call to Azure Cognitive Search Memory store.
         List<String> collections = kernel.getMemory().getCollectionsAsync().block();
 
-        //Assert
+        // Assert
         assertTrue(collections.contains("fake-index"));
     }
 
     @Test
     void azureCognitiveSearchMemoryStoreCanSaveReferenceAsync() {
-        //Arrange
-        responseBody = httpRequest ->
-                "{\"value\": [" +
-                        "    {" +
-                        "      \"key\": \"fake-key\"," +
-                        "      \"status\": true," +
-                        "      \"errorMessage\": null," +
-                        "      \"statusCode\": 200" +
-                        "    }" +
-                        "  ]" +
-                        "}";
+        // Arrange
+        responseBody =
+                httpRequest ->
+                        "{\"value\": ["
+                                + "    {"
+                                + "      \"key\": \"fake-key\","
+                                + "      \"status\": true,"
+                                + "      \"errorMessage\": null,"
+                                + "      \"statusCode\": 200"
+                                + "    }"
+                                + "  ]"
+                                + "}";
 
         responseStatusCode = httpRequest -> 200;
 
-        //Act
-        String key = kernel.getMemory()
-                .saveReferenceAsync(
-                        "fake-index",
-                        "fake-text",
-                        "fake-externalId",
-                        "fake-exernalSourceName",
-                        "fake-description",
-                        "fake-additionalMetadata")
-                .block();
+        // Act
+        String key =
+                kernel.getMemory()
+                        .saveReferenceAsync(
+                                "fake-index",
+                                "fake-text",
+                                "fake-externalId",
+                                "fake-exernalSourceName",
+                                "fake-description",
+                                "fake-additionalMetadata")
+                        .block();
 
-        //Assert
+        // Assert
         assertEquals("fake-key", key);
     }
 
     @Test
     void azureCognitiveSearchMemoryStoreCanSaveInformationAsync() {
-        //Arrange
-        responseBody = httpRequest ->
-                "{\"value\": [" +
-                        "    {" +
-                        "      \"key\": \"fake-key\"," +
-                        "      \"status\": true," +
-                        "      \"errorMessage\": null," +
-                        "      \"statusCode\": 200" +
-                        "    }" +
-                        "  ]" +
-                        "}";
+        // Arrange
+        responseBody =
+                httpRequest ->
+                        "{\"value\": ["
+                                + "    {"
+                                + "      \"key\": \"fake-key\","
+                                + "      \"status\": true,"
+                                + "      \"errorMessage\": null,"
+                                + "      \"statusCode\": 200"
+                                + "    }"
+                                + "  ]"
+                                + "}";
 
         responseStatusCode = httpRequest -> 200;
 
-        //Act
-        String key = kernel.getMemory()
-                .saveInformationAsync(
-                        "fake-index",
-                        "fake-text",
-                        "fake-externalId",
-                        "fake-description",
-                        "fake-additionalMetadata")
-                .block();
+        // Act
+        String key =
+                kernel.getMemory()
+                        .saveInformationAsync(
+                                "fake-index",
+                                "fake-text",
+                                "fake-externalId",
+                                "fake-description",
+                                "fake-additionalMetadata")
+                        .block();
 
-        //Assert
+        // Assert
         assertEquals("fake-key", key);
     }
 
     @Test
     void azureCognitiveSearchMemoryStoreGetAsync() {
-        //Arrange
+        // Arrange
         String key = "fake-key";
-        String base64EncodedKey = new String(Base64.getUrlEncoder().encode("fake-key".getBytes(StandardCharsets.UTF_8)));
+        String base64EncodedKey =
+                new String(
+                        Base64.getUrlEncoder().encode("fake-key".getBytes(StandardCharsets.UTF_8)));
 
-        responseBody = httpRequest ->
-                "{" +
-                "  \"Id\": \"" + base64EncodedKey + "\"," +
-                "  \"Text\": \"fake-text\"," +
-                "  \"Description\": \"fake-description\"," +
-                "  \"AdditionalMetadata\": \"fake-additionalmetadata\"," +
-                "  \"ExternalSourceName\": \"fake-externalsourcename\"," +
-                "  \"Reference\": false" +
-                "}";
+        responseBody =
+                httpRequest ->
+                        "{"
+                                + "  \"Id\": \""
+                                + base64EncodedKey
+                                + "\","
+                                + "  \"Text\": \"fake-text\","
+                                + "  \"Description\": \"fake-description\","
+                                + "  \"AdditionalMetadata\": \"fake-additionalmetadata\","
+                                + "  \"ExternalSourceName\": \"fake-externalsourcename\","
+                                + "  \"Reference\": false"
+                                + "}";
 
         responseStatusCode = httpRequest -> 200;
 
-        //Act
-        MemoryQueryResult result = kernel.getMemory()
-                .getAsync(
-                        "fake-index",
-                        key,
-                        false)
-                .block();
+        // Act
+        MemoryQueryResult result = kernel.getMemory().getAsync("fake-index", key, false).block();
 
-        //Assert
+        // Assert
         assertEquals(key, result.getMetadata().getId());
     }
 
     @Test
     void azureCognitiveSearchMemoryStoreRemoveAsync() {
-        //Arrange
+        // Arrange
         String key = "fake-key";
-        String base64EncodedKey = new String(Base64.getUrlEncoder().encode("fake-key".getBytes(StandardCharsets.UTF_8)));
+        String base64EncodedKey =
+                new String(
+                        Base64.getUrlEncoder().encode("fake-key".getBytes(StandardCharsets.UTF_8)));
 
-        responseBody = httpRequest ->
-                "{\"value\": [" +
-                        "    {" +
-                        "      \"key\": \"" + base64EncodedKey + "\"," +
-                        "      \"status\": true," +
-                        "      \"errorMessage\": null," +
-                        "      \"statusCode\": 200" +
-                        "    }" +
-                        "  ]" +
-                        "}";;
+        responseBody =
+                httpRequest ->
+                        "{\"value\": ["
+                                + "    {"
+                                + "      \"key\": \""
+                                + base64EncodedKey
+                                + "\","
+                                + "      \"status\": true,"
+                                + "      \"errorMessage\": null,"
+                                + "      \"statusCode\": 200"
+                                + "    }"
+                                + "  ]"
+                                + "}";
+        ;
 
         responseStatusCode = httpRequest -> 200;
 
-        //Act
-        kernel.getMemory()
-                .removeAsync(
-                        "fake-index",
-                        key)
-                .block();
+        // Act
+        kernel.getMemory().removeAsync("fake-index", key).block();
 
-        //Assert
+        // Assert
     }
 
     @Test
     void azureCognitiveSearchMemoryStoreSearchAsync() {
-        //Arrange
+        // Arrange
         String key = "fake-key";
-        String base64EncodedKey = new String(Base64.getUrlEncoder().encode("fake-key".getBytes(StandardCharsets.UTF_8)));
+        String base64EncodedKey =
+                new String(
+                        Base64.getUrlEncoder().encode("fake-key".getBytes(StandardCharsets.UTF_8)));
 
-        responseBody = httpRequest ->
-                "{\"value\": [" +
-                        "{" +
-                        "  \"@search.score\": 1," +
-                        "  \"@search.highlights\": {" +
-                        "    \"Text\": [\"It was a dark and stormy night\"]" +
-                        "  }," +
-                        "  \"@search.features\": {" +
-                        "    \"Text\": {" +
-                        "      \"uniqueTokenMatches\": 1," +
-                        "      \"similarityScore\": 0.8," +
-                        "      \"termFrequency\": 0.99" +
-                        "    }"+
-                        "  }," +
-                        "  \"Id\": \"" + base64EncodedKey + "\"," +
-                        "  \"Text\": \"fake-text\"," +
-                        "  \"Description\": \"fake-description\"," +
-                        "  \"AdditionalMetadata\": \"fake-additionalmetadata\"," +
-                        "  \"ExternalSourceName\": \"fake-externalsourcename\"," +
-                        "  \"Reference\": false" +
-                        "}" +
-                        "]}";
+        responseBody =
+                httpRequest ->
+                        "{\"value\": ["
+                                + "{"
+                                + "  \"@search.score\": 1,"
+                                + "  \"@search.highlights\": {"
+                                + "    \"Text\": [\"It was a dark and stormy night\"]"
+                                + "  },"
+                                + "  \"@search.features\": {"
+                                + "    \"Text\": {"
+                                + "      \"uniqueTokenMatches\": 1,"
+                                + "      \"similarityScore\": 0.8,"
+                                + "      \"termFrequency\": 0.99"
+                                + "    }"
+                                + "  },"
+                                + "  \"Id\": \""
+                                + base64EncodedKey
+                                + "\","
+                                + "  \"Text\": \"fake-text\","
+                                + "  \"Description\": \"fake-description\","
+                                + "  \"AdditionalMetadata\": \"fake-additionalmetadata\","
+                                + "  \"ExternalSourceName\": \"fake-externalsourcename\","
+                                + "  \"Reference\": false"
+                                + "}"
+                                + "]}";
 
         responseStatusCode = httpRequest -> 200;
 
-        //Act
-        List<MemoryQueryResult> results = kernel.getMemory()
-                .searchAsync(
-                        "fake-index",
-                        "fake-query",
-                        1,
-                        0.5,
-                        false)
-                .block();
+        // Act
+        List<MemoryQueryResult> results =
+                kernel.getMemory().searchAsync("fake-index", "fake-query", 1, 0.5, false).block();
 
-        //Assert
+        // Assert
         assertEquals(1, results.size());
         assertEquals(key, results.get(0).getMetadata().getId());
     }
