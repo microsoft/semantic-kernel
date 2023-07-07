e218cb378a71dbb4bffea7336d70eb567c463787
sneha <de_sneha@outlook.com>
2023-07-07 15:59:35 -0700

    finished example file

--- python/samples/kernel-syntax-examples/logit_bias.py
+++ python/samples/kernel-syntax-examples/logit_bias.py
@@ -1,17 +1,13 @@
 # Copyright (c) Microsoft. All rights reserved.
-from semantic_kernel.connectors.ai.open_ai.services.open_ai_text_completion import (
-    OpenAITextCompletion
-)
-from semantic_kernel.connectors.ai.open_ai.services.open_ai_chat_completion import (
-    OpenAIChatCompletion
-)
-from semantic_kernel.connectors.ai.complete_request_settings import CompleteRequestSettings
-from semantic_kernel.connectors.ai.chat_request_settings import ChatRequestSettings
-import semantic_kernel as sk
+
 import asyncio
+
+import semantic_kernel as sk
 import semantic_kernel.connectors.ai.open_ai as sk_oai
+from semantic_kernel.connectors.ai.chat_request_settings import ChatRequestSettings
 
 
+# See Example49_LogitBias.cs for original example
 async def chat_request_example():
     kernel = sk.Kernel()
     api_key, org_id = sk.openai_settings_from_dot_env()
@@ -19,6 +15,8 @@ async def chat_request_example():
         "chat-gpt", sk_oai.OpenAIChatCompletion("gpt-3.5-turbo", api_key, org_id)
     )
 
+    # Tokens corresponding to:
+    # "novel literature reading author library story chapter paperback hardcover ebook publishing fiction nonfiction manuscript textbook bestseller bookstore reading list bookworm"
     keys = [
         3919, 626, 17201, 1300, 25782, 9800, 32016, 13571, 43582, 20189,
         1891, 10424, 9631, 16497, 12984, 20020, 24046, 13159, 805, 15817,
@@ -44,18 +42,29 @@ async def chat_request_example():
     )
 
     prompt_template.add_system_message("You are a librarian expert")
-    prompt_template.add_user_message("Hi, I'm looking some suggestions")
     function_config = sk.SemanticFunctionConfig(prompt_config, prompt_template)
     chat_function = kernel.register_semantic_function("ChatBot", "Chat", function_config)
 
+    first_user_msg = "Hi, I'm looking some suggestions"
+    print(f"User: {first_user_msg}")
+    print("------------------------")
+    prompt_template.add_user_message(first_user_msg)
     answer = await kernel.run_async(chat_function, input_vars=None)
-    print(answer)
+    print(f"Bot: {answer}")
+    print("------------------------")
+
+    second_user_msg = "I love history and philosophy, I'd like to learn something new about Greece, any suggestion?"
+    print(f"User: {second_user_msg}")
+    print("------------------------")
+    prompt_template.add_user_message(second_user_msg)
+    answer = await kernel.run_async(chat_function, input_vars=None)
+    print(f"Bot: {answer}")
+    print("------------------------")
 
     return
 
 
 async def main() -> None:
-
     await chat_request_example()
 
 if __name__ == "__main__":
