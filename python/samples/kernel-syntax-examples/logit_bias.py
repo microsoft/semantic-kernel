8d84a9a9b9b0c82ced6412d6ddebe06b61009267
Alisha Maddy <alisha831maddy@gmail.com>
2023-07-10 16:29:11 -0700

    fixed format

--- python/samples/kernel-syntax-examples/logit_bias.py
+++ python/samples/kernel-syntax-examples/logit_bias.py
@@ -45,6 +45,32 @@ async def chat_request_example():
     return context_vars, banned_words
 
 
+async def text_complete_request_example():
+    kernel = sk.Kernel()
+    api_key, org_id = sk.openai_settings_from_dot_env()
+    openai_text_completion = sk_oai.OpenAITextCompletion("text-davinci-002", api_key, org_id)
+    kernel.add_text_completion_service("dv", openai_text_completion)
+
+    # Spaces and capitalization affect the token ids.
+    # The following is the token ids of pie related words.
+    keys = [18040, 17180, 16108, 4196, 79, 931, 5116, 30089, 36724, 47, 931, 5116,
+            431, 5171, 613, 5171, 350, 721, 272, 47, 721, 272]
+    banned_words = ["apple", " apple", "Apple", " Apple", "pumpkin", " pumpkin",
+                    " Pumpkin", "pecan", " pecan", " Pecan", "Pecan"]
+
+    # This will make the model try its best to avoid any of the above related words.
+    settings = CompleteRequestSettings()
+
+    # Map each token in the keys list to a bias value from -100 (a potential ban) to 100 (exclusive selection)
+    for key in keys:
+        # -100 to potentially ban all the tokens from the list.
+        settings.token_selection_biases[key] = -100
+
+    user_mssg = "The best pie flavor to have in autumn is"
+    answer = await openai_text_completion.complete_async(user_mssg, settings)
+    return answer, user_mssg, banned_words
+
+
 async def main() -> None:
     chat, banned_words = await chat_request_example()
     print("Chat completion example:")
@@ -73,32 +99,6 @@ async def main() -> None:
     if passed == True:
         print("None of the banned words were found in the answer")
     return
-
-
-async def text_complete_request_example():
-    kernel = sk.Kernel()
-    api_key, org_id = sk.openai_settings_from_dot_env()
-    openai_text_completion = sk_oai.OpenAITextCompletion("text-davinci-002", api_key, org_id)
-    kernel.add_text_completion_service("dv", openai_text_completion)
-
-    # Spaces and capitalization affect the token ids.
-    # The following is the token ids of pie related words.
-    keys = [18040, 17180, 16108, 4196, 79, 931, 5116, 30089, 36724, 47, 931, 5116,
-            431, 5171, 613, 5171, 350, 721, 272, 47, 721, 272]
-    banned_words = ["apple", " apple", "Apple", " Apple", "pumpkin", " pumpkin",
-                    " Pumpkin", "pecan", " pecan", " Pecan", "Pecan"]
-
-    # This will make the model try its best to avoid any of the above related words.
-    settings = CompleteRequestSettings()
-
-    # Map each token in the keys list to a bias value from -100 (a potential ban) to 100 (exclusive selection)
-    for key in keys:
-        # -100 to potentially ban all the tokens from the list.
-        settings.token_selection_biases[key] = -100
-
-    user_mssg = "The best pie flavor to have in autumn is"
-    answer = await openai_text_completion.complete_async(user_mssg, settings)
-    return answer, user_mssg, banned_words
 
 if __name__ == "__main__":
     asyncio.run(main())
