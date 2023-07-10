f8b23b46f8fed892768221d2a1e98816e3c48d3d
sailesh <saileshr2209@gmail.com>
2023-07-10 17:42:57 +0530

    implemented functions for populating examples, edge cases and functions list in action planner prompt template

--- python/semantic_kernel/planning/action_planner/action_planner.py
+++ python/semantic_kernel/planning/action_planner/action_planner.py
@@ -1,10 +1,15 @@
 import os
 from logging import Logger
 from typing import Optional
+from textwrap import dedent
 from semantic_kernel import Kernel
 from semantic_kernel.planning import Plan
 from semantic_kernel.utils.null_logger import NullLogger
+from semantic_kernel.orchestration.sk_context import SKContext
+from semantic_kernel.skill_definition.function_view import FunctionView
+from semantic_kernel.skill_definition.parameter_view import ParameterView
 from semantic_kernel.orchestration.sk_function_base import SKFunctionBase
+from semantic_kernel.skill_definition import sk_function, sk_function_context_parameter
 
 
 class ActionPlanner:
@@ -52,5 +57,178 @@ class ActionPlanner:
         self._kernel = kernel
         self._context = kernel.create_new_context()
 
-    async def create_plan_async(self) -> Plan:
-        pass
+    async def create_plan_async(self, goal: str) -> Plan:
+        """
+        :param goal: The input to the planner based on which the plan is made
+        :return: a Plan object
+        """
+
+        if goal is None:
+            raise ValueError("Goal cannot be `None`.")
+
+        self._context.variables.update(goal)
+
+        plan = Plan()
+        return plan
+
+    @sk_function(
+        description="List a few good examples of plans to generate", name="GoodExamples"
+    )
+    @sk_function_context_parameter(
+        name="goal", description="The current goal processed by the planner"
+    )
+    def good_examples(self, goal: str, context: SKContext) -> str:
+        return dedent(
+            """
+            [EXAMPLE]
+            - List of functions:
+            // Read a file.
+            FileIOSkill.ReadAsync
+            Parameter ""path"": Source file.
+            // Write a file.
+            FileIOSkill.WriteAsync
+            Parameter ""path"": Destination file. (default value: sample.txt)
+            Parameter ""content"": File content.
+            // Get the current time.
+            TimeSkill.Time
+            No parameters.
+            // Makes a POST request to a uri.
+            HttpSkill.PostAsync
+            Parameter ""body"": The body of the request.
+            - End list of functions.
+            Goal: create a file called ""something.txt"".
+            {""plan"":{
+            ""rationale"": ""the list contains a function that allows to create files"",
+            ""function"": ""FileIOSkill.WriteAsync"",
+            ""parameters"": {
+            ""path"": ""something.txt"",
+            ""content"": null
+            }}}
+            #END-OF-PLAN
+            """
+        )
+
+    @sk_function(
+        description="List a few edge case examples of plans to handle",
+        name="EdgeCaseExamples",
+    )
+    @sk_function_context_parameter(
+        name="goal", description="The current goal processed by the planner"
+    )
+    def edge_case_examples(self, goal: str, context: SKContext) -> str:
+        return dedent(
+            '''
+            [EXAMPLE]
+            - List of functions:
+            // Get the current time.
+            TimeSkill.Time
+            No parameters.
+            // Write a file.
+            FileIOSkill.WriteAsync
+            Parameter ""path"": Destination file. (default value: sample.txt)
+            Parameter ""content"": File content.
+            // Makes a POST request to a uri.
+            HttpSkill.PostAsync
+            Parameter ""body"": The body of the request.
+            // Read a file.
+            FileIOSkill.ReadAsync
+            Parameter ""path"": Source file.
+            - End list of functions.
+            Goal: tell me a joke.
+            {""plan"":{
+            ""rationale"": ""the list does not contain functions to tell jokes or something funny"",
+            ""function"": """",
+            ""parameters"": {
+            }}}
+            #END-OF-PLAN
+            '''
+        )
+
+    @sk_function(
+        description="List all functions available in the kernel", name="ListOfFunctions"
+    )
+    @sk_function_context_parameter(
+        name="goal", description="The current goal processed by the planner"
+    )
+    def list_of_functions(self, goal: str, context: SKContext) -> str:
+        if context.skills is None:
+            raise ValueError("No plugins are available.")
+
+        functions_view = context.skills.get_functions_view()
+        available_functions = []
+
+        for functions in functions_view._native_functions.values():
+            for func in functions:
+                if func.skill_name == "this":
+                    continue
+                available_functions.append(self._create_function_string(func))
+
+        for functions in functions_view._semantic_functions.values():
+            for func in functions:
+                if func.skill_name == "this":
+                    continue
+                available_functions.append(self._create_function_string(func))
+
+        return "\n".join(available_functions)
+
+    def _create_function_string(self, function: FunctionView) -> str:
+        """
+        Takes an instance of FunctionView and returns a string that consists of
+        function name, function description and parameters in the following format
+        // <function-description>
+        <skill-name>.<function-name>
+        Parameter ""<parameter-name>"": <parameter-description> (deafult value: `default_value`)
+        ...
+
+        :param function: An instance of FunctionView for which the string representation needs to be generated
+        :return: string representation of function
+        """
+
+        description = f"// {function.description}"
+        name = f"{function.skill_name}.{function.name}"
+
+        parameters_list = [
+            result
+            for x in function.parameters
+            if (result := self._create_parameter_string(x)) is not None
+        ]
+
+        if len(parameters_list) == 0:
+            parameters = "No parameters."
+        else:
+            parameters = "\n".join(parameters_list)
+
+        func_str = f"{description}\n{name}\n{parameters}"
+
+        print(func_str)
+
+        return func_str
+
+    def _create_parameter_string(self, parameter: ParameterView) -> str:
+        """
+        Takes an instance of ParameterView and returns a string that consists of
+        parameter name, parameter description and default value for the parameter
+        in the following format
+        Parameter ""<parameter-name>"": <parameter-description> (default value: <default-value>)
+
+        :param parameter: An instance of ParameterView for which the string representation needs to be generated
+        :return: string representation of parameter
+        """
+
+        name = parameter.name
+
+        # Don't consider default parameter `input` as one of the function parameters
+        if name == "input":
+            return None
+
+        description = parameter.description
+
+        default_value = (
+            ""
+            if not parameter.default_value
+            else f"(default value: {parameter.default_value})"
+        )
+
+        param_str = f'Parameter ""{name}"": {description} {default_value}'
+
+        return param_str.strip()
