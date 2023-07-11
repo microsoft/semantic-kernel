f6b0f079bc335f90677e5d003ed77e2669ff27a7
Jean-Sylvain Boige <jsboige@gmail.com>
2023-07-11 13:52:35 +0200

    Fixed Lambda indentation

--- dotnet/src/Connectors/Connectors.AI.OpenAI/Tokenizers/Settings/GPT3Settings.cs
+++ dotnet/src/Connectors/Connectors.AI.OpenAI/Tokenizers/Settings/GPT3Settings.cs
@@ -17,9 +17,11 @@ internal static class GPT3Settings
     internal static Dictionary<(string, string), int> BpeRanks => s_bpeRanks.Value;
 
     /// <summary>Lazy load the cached encoding table (encoder.json).</summary>
-    private static readonly Lazy<Dictionary<string, int>> s_encoder = new(() => JsonSerializer.Deserialize<Dictionary<string, int>>(EmbeddedResource.ReadEncodingTable())
-                                                                                ?? throw new AIException(AIException.ErrorCodes.InvalidConfiguration,
-                                                                                    "Encoding table deserialization returned NULL"));
+    private static readonly Lazy<Dictionary<string, int>> s_encoder = new(() =>
+        JsonSerializer.Deserialize<Dictionary<string, int>>(
+            EmbeddedResource.ReadEncodingTable()) ?? throw new AIException(
+            AIException.ErrorCodes.InvalidConfiguration,
+            "Encoding table deserialization returned NULL"));
 
     /// <summary>Lazy load the cached byte pair encoding table (vocab.bpe).</summary>
     private static readonly Lazy<Dictionary<(string, string), int>> s_bpeRanks = new(() =>
