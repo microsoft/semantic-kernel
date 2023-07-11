bdfcbb98435e9cae0abf972b3d77d583151c1f11
Niko Schuessler <54370830+nschuessler@users.noreply.github.com>
2023-07-11 10:40:35 -0700

    Make regex static to improve performance.

--- dotnet/src/Extensions/Planning.ActionPlanner/ActionPlanner.cs
+++ dotnet/src/Extensions/Planning.ActionPlanner/ActionPlanner.cs
@@ -214,6 +214,11 @@ Goal: tell me a joke.
     #region private ================================================================================
 
     /// <summary>
+    /// The regular expression for extracting serialized PLAN.
+    /// </summary>
+    private static readonly Regex PlanRegex = new("^[^{}]*(((?'Open'{)[^{}]*)+((?'Close-Open'})[^{}]*)+)*(?(Open)(?!))", RegexOptions.Singleline | RegexOptions.Compiled);
+
+    /// <summary>
     /// Native function that filters out good JSON from planner result in case additional text is present
     /// using a similar regex to the balancing group regex defined here: https://learn.microsoft.com/en-us/dotnet/standard/base-types/grouping-constructs-in-regular-expressions#balancing-group-definitions
     /// </summary>
@@ -221,8 +226,7 @@ Goal: tell me a joke.
     /// <returns>Instance of <see cref="ActionPlanResponse"/> object deserialized from extracted JSON.</returns>
     private ActionPlanResponse? ParsePlannerResult(SKContext plannerResult)
     {
-        Regex planRegex = new("^[^{}]*(((?'Open'{)[^{}]*)+((?'Close-Open'})[^{}]*)+)*(?(Open)(?!))", RegexOptions.Singleline);
-        Match match = planRegex.Match(plannerResult.ToString());
+        Match match = PlanRegex.Match(plannerResult.ToString());
 
         if (match.Success && match.Groups["Close"].Length > 0)
         {
