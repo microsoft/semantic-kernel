48a6ccbb5f1d86090a3c8277d94327c4a815112f
Aman Sachan <51973971+amsacha@users.noreply.github.com>
2023-07-13 07:45:43 -0700

    Copilot Chat: fix CC Tests (#1984)
    ### Motivation and Context
    Fixing broken Copilot Chat Tests that run as part of github actions. 
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [x] The code builds clean without any errors or warnings
    - [x] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#dev-scripts)
    raises no violations
    - [x] All unit tests pass, and I have added new tests where possible
    - [x] I didn't break anyone :smile:

--- samples/apps/copilot-chat-app/webapp/tests/testsMultiuser.ts
+++ samples/apps/copilot-chat-app/webapp/tests/testsMultiuser.ts
@@ -7,29 +7,28 @@ generate a chatid for their chat session and then if another user can join that 
 */
 export async function shareAndJoinChatSessionTest(page) {
     const userAccount1 = process.env.REACT_APP_TEST_USER_ACCOUNT1 as string;
-    const userAccount1Initials = process.env.REACT_APP_TEST_USER_ACCOUNT1_INITIALS as string;
+    const password1 = process.env.REACT_APP_TEST_USER_PASSWORD1 as string;
     const userAccount2 = process.env.REACT_APP_TEST_USER_ACCOUNT2 as string;
-    const password = process.env.REACT_APP_TEST_USER_PASSWORD as string;
-    await util.loginHelper(page, userAccount1, password);
+    const password2 = process.env.REACT_APP_TEST_USER_PASSWORD2 as string;
+
+    await util.loginHelper(page, userAccount1, password1);
     await util.createNewChat(page);
 
     await page.getByTestId('shareButton').click();
     await page.getByTestId('inviteOthersMenuItem').click();
 
-    const labelByID = await page.getByTestId('copyIDLabel');
+    const labelByID = await page.getByTestId('chatIDLabel');
     const chatId = await labelByID.textContent();
-    
-    await page.getByTestId('chatIDCopyButton').click();
     await page.getByTestId('chatIDCloseButton').click();
 
-    await page.getByText(userAccount1Initials).click();    
+    await page.getByTestId('logOutMenuList').click(); 
     await page.getByTestId('logOutMenuButton').click();
 
     const usernameToLowerCase = userAccount1.toLowerCase();
     const locatorVal = ('[data-test-id="' + usernameToLowerCase + '"]') as string;
     await page.locator(locatorVal).click();
 
-    await util.loginHelperAnotherUser(page, userAccount2, password);
+    await util.loginHelperAnotherUser(page, userAccount2, password2);
     await page.getByTestId('createNewConversationButton').click();
     await page.getByTestId('joinABotMenuItem').click();
     await page.getByTestId('enterChatIDLabel').fill(chatId as string);
@@ -37,9 +36,9 @@ export async function shareAndJoinChatSessionTest(page) {
     await page.getByTestId('joinChatButton').click();
 
     await page.waitForTimeout(util.ChatStateChangeWait);
-    await page.getByRole('button', { name: 'View more people.' }).click();
-
-    const numPeople = await page.getByRole('button', { name: 'View more people.' }).textContent();
+    
+    await page.getByTestId('chatParticipantsView').click();    
+    const numPeople = await page.getByTestId('chatParticipantsView').textContent();
     await expect(numPeople).toEqual("+2");
 
     await util.postUnitTest(page);
