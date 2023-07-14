d525e35f68a802fc644b87320a91323af4eb9c8d
amsacha <amsacha@microsoft.com>
2023-07-14 10:25:01 -0700

    use data id for plugin button and switch to using new var for password

--- samples/apps/copilot-chat-app/webapp/src/components/open-api-plugins/PluginGallery.tsx
+++ samples/apps/copilot-chat-app/webapp/src/components/open-api-plugins/PluginGallery.tsx
@@ -61,7 +61,7 @@ export const PluginGallery: React.FC = () => {
     return (
         <Dialog open={open} onOpenChange={(_event, data) => { setOpen(data.open); }}>
             <DialogTrigger>
-                <Button appearance="transparent" icon={<AppsAddIn24Regular color="white" />} />
+                <Button data-testid="pluginButton" appearance="transparent" icon={<AppsAddIn24Regular color="white" />} />
             </DialogTrigger>
             <DialogSurface className={classes.root}>
                 <DialogBody>
