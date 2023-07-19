ca60deb12d573d4078a7574715bbc11a1815e8c1
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2023-07-19 11:21:41 +0100

    Fix using order

--- dotnet/samples/KernelSyntaxExamples/Example53_FunctionHooks.cs
+++ dotnet/samples/KernelSyntaxExamples/Example53_FunctionHooks.cs
@@ -1,11 +1,11 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Orchestration;
 using RepoUtils;
-using System.Linq;
 
 // ReSharper disable once InconsistentNaming
 public static class Example53_FunctionHooks
