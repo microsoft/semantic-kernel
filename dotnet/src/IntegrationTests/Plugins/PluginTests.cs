5bca24e8a9a2882580afe5fb963ef6f0671c31a5
Craig Presti <craig.presti@microsoft.com>
2023-07-19 19:24:07 +0930

    Add instacart plugin which has a schema with validation errors according to SK's underlying validator

--- dotnet/src/IntegrationTests/Plugins/PluginTests.cs
+++ dotnet/src/IntegrationTests/Plugins/PluginTests.cs
@@ -14,7 +14,7 @@ public class PluginTests
     [Theory]
     [InlineData("https://www.klarna.com/.well-known/ai-plugin.json", "Klarna", "productsUsingGET", "Laptop", 3, 200, "US")]
     [InlineData("https://www.klarna.com/us/shopping/public/openai/v0/api-docs/", "Klarna", "productsUsingGET", "Laptop", 3, 200, "US")]
-    public async Task QueryKlarnaViaChatGPTPlugin(
+    public async Task QueryKlarnaPlugin(
         string pluginEndpoint,
         string name,
         string functionName,
@@ -37,6 +37,38 @@ public class PluginTests
         contextVariables["size"] = size.ToString(System.Globalization.CultureInfo.InvariantCulture);
         contextVariables["budget"] = budget.ToString(System.Globalization.CultureInfo.InvariantCulture);
         contextVariables["countryCode"] = countryCode;
+
+        // Act
+        var result = await skill[functionName].InvokeAsync(new SKContext(contextVariables));
+
+        // Assert
+        Assert.False(result.ErrorOccurred);
+    }
+
+    [Theory]
+    [InlineData("https://raw.githubusercontent.com/sisbell/chatgpt-plugin-store/main/manifests/instacart.com.json",
+        "Instacart",
+        "create",
+        "{\"title\":\"Shopping List\", \"ingredients\": [\"Flour\"], \"question\": \"what ingredients do I need to make chocolate cookies?\", \"partnerName\": \"OpenAI\" }"
+        )]
+    public async Task QueryInstacartPlugin(
+        string pluginEndpoint,
+        string name,
+        string functionName,
+        string payload)
+    {
+        // Arrange
+        var kernel = new KernelBuilder().Build();
+        using HttpClient httpClient = new();
+
+        //note that this plugin is not compliant according to the underlying validator in SK
+        var skill = await kernel.ImportAIPluginAsync(
+            name,
+            new Uri(pluginEndpoint),
+            new OpenApiSkillExecutionParameters(httpClient) { IgnoreNonCompliantErrors = true });
+
+        var contextVariables = new ContextVariables();
+        contextVariables["payload"] = payload;
 
         // Act
         var result = await skill[functionName].InvokeAsync(new SKContext(contextVariables));
