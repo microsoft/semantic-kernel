c22ccff66838634ebf460595a9fdd4e0720e7dd1
joowon-dm-snu <joowon.kim@dm.snu.ac.kr>
2023-07-21 01:46:23 +0900

    add todo, implement skill import helper

--- python/tests/integration/fakes/summarize_skill_fake.py
+++ python/tests/integration/fakes/summarize_skill_fake.py
@@ -1,4 +1,10 @@
+# Copyright (c) Microsoft. All rights reserved.
+
 from semantic_kernel.skill_definition.sk_function_decorator import sk_function
+
+# TODO: this fake skill is temporal usage.
+# C# supports import skill from samples dir by using test helper and python should do the same
+# `semantic-kernel/dotnet/src/IntegrationTests/TestHelpers.cs`
 
 
 class SummarizeSkillFake:
--- python/tests/integration/fakes/writer_skill_fake.py
+++ python/tests/integration/fakes/writer_skill_fake.py
@@ -1,4 +1,10 @@
+# Copyright (c) Microsoft. All rights reserved.
+
 from semantic_kernel.skill_definition.sk_function_decorator import sk_function
+
+# TODO: this fake skill is temporal usage.
+# C# supports import skill from samples dir by using test helper and python should do the same
+# `semantic-kernel/dotnet/src/IntegrationTests/TestHelpers.cs`
 
 
 class WriterSkillFake:
@@ -9,6 +15,10 @@ class WriterSkillFake:
     def translate(self, language: str) -> str:
         return f"Translate: {language}"
 
-    @sk_function(description="Write an outline for a novel", name="NovelOutline")
+    @sk_function(
+        description="Write an outline for a novel",
+        name="NovelOutline",
+        input_default_value="<!--===ENDPART===-->",
+    )
     def write_novel_outline(self, input: str) -> str:
         return f"Novel outline: {input}"
