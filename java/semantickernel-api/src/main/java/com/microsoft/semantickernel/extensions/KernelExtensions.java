8957884e5d6f9de2e1ab1dfe0dc280af17de06ef
John Oliver <1615532+johnoliver@users.noreply.github.com>
2023-07-21 09:02:50 +0100

    Java: Allow functions to be loaded from the classpath (#2080)
    Allow importing skills from the classpath using `getResourceAsStream` so
    that people can embed skills inside their jar

--- java/semantickernel-api/src/main/java/com/microsoft/semantickernel/extensions/KernelExtensions.java
+++ java/semantickernel-api/src/main/java/com/microsoft/semantickernel/extensions/KernelExtensions.java
@@ -12,15 +12,23 @@ import com.microsoft.semantickernel.templateengine.PromptTemplateEngine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.annotation.Nullable;
 
 public class KernelExtensions {
     private static final Logger LOGGER = LoggerFactory.getLogger(KernelExtensions.class);
+    private static final String CONFIG_FILE = "config.json";
+    private static final String PROMPT_FILE = "skprompt.txt";
 
     private KernelExtensions() {}
 
@@ -28,9 +36,6 @@ public class KernelExtensions {
             String parentDirectory,
             String skillDirectoryName,
             PromptTemplateEngine promptTemplateEngine) {
-
-        String CONFIG_FILE = "config.json";
-        String PROMPT_FILE = "skprompt.txt";
 
         // Verify.ValidSkillName(skillDirectoryName);
         File skillDir = new File(parentDirectory, skillDirectoryName);
@@ -86,5 +91,100 @@ public class KernelExtensions {
         }
 
         return skills;
+    }
+
+    public static Map<String, SemanticFunctionConfig> importSemanticSkillFromResourcesDirectory(
+            String pluginDirectory,
+            String pluginName,
+            String functionName,
+            @Nullable Class clazz,
+            PromptTemplateEngine promptTemplateEngine) {
+
+        PromptTemplateConfig config =
+                getPromptTemplateConfig(pluginDirectory, pluginName, functionName, clazz);
+
+        if (config == null) {
+            config = new PromptTemplateConfig("", "", null);
+        }
+
+        String template = getTemplatePrompt(pluginDirectory, pluginName, functionName, clazz);
+
+        HashMap<String, SemanticFunctionConfig> skills = new HashMap<>();
+
+        PromptTemplate promptTemplate =
+                SKBuilders.promptTemplate()
+                        .withPromptTemplate(template)
+                        .withPromptTemplateConfig(config)
+                        .build(promptTemplateEngine);
+
+        skills.put(functionName, new SemanticFunctionConfig(config, promptTemplate));
+
+        return skills;
+    }
+
+    private static String getTemplatePrompt(
+            String pluginDirectory, String pluginName, String functionName, @Nullable Class clazz) {
+        String promptFileName =
+                pluginDirectory
+                        + File.separator
+                        + pluginName
+                        + File.separator
+                        + functionName
+                        + File.separator
+                        + PROMPT_FILE;
+
+        InputStream promptFileStream;
+        if (clazz == null) {
+            promptFileStream =
+                    KernelExtensions.class.getClassLoader().getResourceAsStream(promptFileName);
+        } else {
+            promptFileStream = clazz.getResourceAsStream(promptFileName);
+        }
+
+        String template;
+
+        try (BufferedReader promptFile =
+                new BufferedReader(
+                        new InputStreamReader(promptFileStream, Charset.defaultCharset()))) {
+            template = promptFile.lines().collect(Collectors.joining("\n"));
+        } catch (IOException e) {
+            LOGGER.error("Failed to read file " + promptFileName, e);
+
+            throw new KernelException(
+                    KernelException.ErrorCodes.FunctionNotAvailable,
+                    "No Skills found in directory " + promptFileName);
+        }
+        return template;
+    }
+
+    private static PromptTemplateConfig getPromptTemplateConfig(
+            String pluginDirectory, String pluginName, String functionName, @Nullable Class clazz) {
+        String configFileName =
+                pluginDirectory
+                        + File.separator
+                        + pluginName
+                        + File.separator
+                        + functionName
+                        + File.separator
+                        + CONFIG_FILE;
+
+        InputStream configFileStream;
+        if (clazz == null) {
+            configFileStream =
+                    KernelExtensions.class.getClassLoader().getResourceAsStream(configFileName);
+        } else {
+            configFileStream = clazz.getResourceAsStream(configFileName);
+        }
+
+        if (configFileStream == null) {
+            return null;
+        }
+
+        try (InputStream is = configFileStream) {
+            return new ObjectMapper().readValue(is, PromptTemplateConfig.class);
+        } catch (IOException e) {
+            LOGGER.debug("No config for " + functionName + " in " + pluginName);
+            return null;
+        }
     }
 }
