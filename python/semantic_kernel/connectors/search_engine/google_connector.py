5a7ef942c82756fcc0cec114fc9f389a12165cae
Sailesh R <saileshr2209@gmail.com>
2023-07-29 01:26:22 +0530

    Python: google connector (#2071)
    ### Motivation and Context
    
    <!-- Thank you for your contribution to the semantic-kernel repo!
    Please help reviewers and future users, providing the following
    information:
      1. Why is this change required?
      2. What problem does it solve?
      3. What scenario does it contribute to?
      4. If it fixes an open issue, please link to the issue here.
    -->
    Implementation of Google search connector for web search skill.
    
    Fixes: #1862 
    
    Sample usage can be found in
    `python/samples/kernel-syntax-examples/google_search_skill.py`
    
    ### Description
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [x] The code builds clean without any errors or warnings
    - [x] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#dev-scripts)
    raises no violations
    - [x] All unit tests pass, and I have added new tests where possible
    - [x] I didn't break anyone :smile:
    
    ---------
    
    Co-authored-by: Abby Harrison <54643756+awharrison-28@users.noreply.github.com>
    Co-authored-by: Abby Harrison <abby.harrison@microsoft.com>

--- python/semantic_kernel/connectors/search_engine/google_connector.py
+++ python/semantic_kernel/connectors/search_engine/google_connector.py
@@ -10,27 +10,33 @@ from semantic_kernel.connectors.search_engine.connector import ConnectorBase
 from semantic_kernel.utils.null_logger import NullLogger
 
 
-class BingConnector(ConnectorBase):
+class GoogleConnector(ConnectorBase):
     """
-    A search engine connector that uses the Bing Search API to perform a web search
+    A search engine connector that uses the Google Custom Search API to perform a web search.
     """
 
     _api_key: str
+    _search_engine_id: str
+    _logger: Logger
 
-    def __init__(self, api_key: str, logger: Optional[Logger] = None) -> None:
+    def __init__(
+        self, api_key: str, search_engine_id: str, logger: Optional[Logger] = None
+    ) -> None:
         self._api_key = api_key
+        self._search_engine_id = search_engine_id
         self._logger = logger if logger else NullLogger()
 
         if not self._api_key:
-            raise ValueError(
-                "Bing API key cannot be null. Please set environment variable BING_API_KEY."
-            )
+            raise ValueError("Google Custom Search API key cannot be null.")
+
+        if not self._search_engine_id:
+            raise ValueError("Google search engine ID cannot be null.")
 
     async def search_async(
         self, query: str, num_results: str, offset: str
     ) -> List[str]:
         """
-        Returns the search results of the query provided by pinging the Bing web search API.
+        Returns the search results of the query provided by pinging the Google Custom search API.
         Returns `num_results` results and ignores the first `offset`.
 
         :param query: search query
@@ -51,34 +57,37 @@ class BingConnector(ConnectorBase):
 
         if num_results <= 0:
             raise ValueError("num_results value must be greater than 0.")
-        if num_results >= 50:
-            raise ValueError("num_results value must be less than 50.")
+        if num_results > 10:
+            raise ValueError("num_results value must be less than or equal to 10.")
 
         if offset < 0:
             raise ValueError("offset must be greater than 0.")
 
         self._logger.info(
-            f"Received request for bing web search with \
+            f"Received request for google search with \
                 params:\nquery: {query}\nnum_results: {num_results}\noffset: {offset}"
         )
 
-        _base_url = "https://api.bing.microsoft.com/v7.0/search"
-        _request_url = f"{_base_url}?q={urllib.parse.quote_plus(query)}&count={num_results}&offset={offset}"
+        _base_url = "https://www.googleapis.com/customsearch/v1"
+        _request_url = (
+            f"{_base_url}?q={urllib.parse.quote_plus(query)}"
+            f"&key={self._api_key}&cx={self._search_engine_id}"
+            f"&num={num_results}&start={offset}"
+        )
 
-        self._logger.info(f"Sending GET request to {_request_url}")
-
-        headers = {"Ocp-Apim-Subscription-Key": self._api_key}
+        self._logger.info("Sending GET request to Google Search API.")
 
         async with aiohttp.ClientSession() as session:
-            async with session.get(
-                _request_url, headers=headers, raise_for_status=True
-            ) as response:
+            async with session.get(_request_url, raise_for_status=True) as response:
                 if response.status == 200:
                     data = await response.json()
-                    pages = data["webPages"]["value"]
-                    self._logger.info(pages)
-                    result = list(map(lambda x: x["snippet"], pages))
-                    self._logger.info(result)
+                    self._logger.info("Request successful.")
+                    self._logger.info(f"API Response: {data}")
+                    items = data["items"]
+                    result = [x["snippet"] for x in items]
                     return result
                 else:
+                    self._logger.error(
+                        f"Request to Google Search API failed with status code: {response.status}."
+                    )
                     return []
