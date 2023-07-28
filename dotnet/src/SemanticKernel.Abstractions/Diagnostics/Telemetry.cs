2f9ecddc1eecd6d9527f16fd2f756cfa7137384a
Dmytro Struk <13853051+dmytrostruk@users.noreply.github.com>
2023-07-28 19:26:30 +0100

    Removed unused method

--- dotnet/src/SemanticKernel.Abstractions/Diagnostics/Telemetry.cs
+++ dotnet/src/SemanticKernel.Abstractions/Diagnostics/Telemetry.cs
@@ -23,25 +23,4 @@ public static class Telemetry
     /// Azure customers setting AZURE_TELEMETRY_DISABLED=1 expect telemetry to be disabled.
     /// </summary>
     public static bool IsTelemetryEnabled => !EnvExtensions.GetBoolEnvVar(TelemetryDisabledEnvVar) ?? true;
-
-    /// <summary>
-    /// Source: https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/core/Azure.Core/src/DiagnosticsOptions.cs
-    /// Values: https://learn.microsoft.com/en-us/dotnet/api/azure.core.diagnosticsoptions.istelemetryenabled?view=azure-dotnet
-    /// </summary>
-    private static bool? EnvVarToBool(string? value)
-    {
-        if (string.Equals(bool.TrueString, value, StringComparison.OrdinalIgnoreCase) ||
-            string.Equals("1", value, StringComparison.OrdinalIgnoreCase))
-        {
-            return true;
-        }
-
-        if (string.Equals(bool.FalseString, value, StringComparison.OrdinalIgnoreCase) ||
-            string.Equals("0", value, StringComparison.OrdinalIgnoreCase))
-        {
-            return false;
-        }
-
-        return null;
-    }
 }
