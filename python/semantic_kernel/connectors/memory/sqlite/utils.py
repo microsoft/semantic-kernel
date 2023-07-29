39e30f678a9722cdacdab50fc904bbfaeb31f1fa
Yefu Wang <1yefuwang1@gmail.com>
2023-07-29 22:05:41 +0800

    add TIMESTAMP format

--- python/semantic_kernel/connectors/memory/sqlite/utils.py
+++ python/semantic_kernel/connectors/memory/sqlite/utils.py
@@ -15,10 +15,14 @@ def serialize_embedding(embedding: np.ndarray) -> str:
     return json.dumps(embedding.tolist())
 
 
+# C# implementation uses `timestamp?.ToString("u", CultureInfo.InvariantCulture);` to serialize timestamps.
+# which is the below format accoridng to https://stackoverflow.com/questions/46778141/datetime-formats-used-in-invariantculture
+TIMESTAMP_FORMAT = "yyyy-MM-dd HH:mm:ssZ"
+
+
 def deserialize_timestamp(timestamp: str) -> datetime:
-    # note: this is not the correct format
-    return datetime.strptime(timestamp, "yyyy-MM-dd HH:mm:ssZ")
+    return datetime.strptime(timestamp, TIMESTAMP_FORMAT)
 
 
 def serialize_timestamp(timestamp: datetime) -> str:
-    return timestamp.strftime("yyyy-MM-dd HH:mm:ssZ")
+    return timestamp.strftime(TIMESTAMP_FORMAT)
