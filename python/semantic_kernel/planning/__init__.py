25cb2bdd8fd2dc33fb274c84adf21d1ecac6c5d8
joowon-dm-snu <joowon.kim@dm.snu.ac.kr>
2023-07-30 21:25:56 +0900

    implement stepwise planner

--- python/semantic_kernel/planning/__init__.py
+++ python/semantic_kernel/planning/__init__.py
@@ -1,9 +1,11 @@
 from semantic_kernel.planning.basic_planner import BasicPlanner
 from semantic_kernel.planning.plan import Plan
 from semantic_kernel.planning.sequential_planner import SequentialPlanner
+from semantic_kernel.planning.stepwise_planner import StepwisePlanner
 
 __all__ = [
-    "SequentialPlanner",
     "BasicPlanner",
     "Plan",
+    "SequentialPlanner",
+    "StepwisePlanner",
 ]
