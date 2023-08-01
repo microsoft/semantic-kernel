5829f2881932d7c2011cfdb7597f8e611686828f
Andrew Hesky <andhesky@microsoft.com>
2023-08-01 09:27:15 -0700

    Add the chat stepwise planner.

---
+++ dotnet/src/PF-Extensions/Planning.ChatStepwisePlanner/FewShotExample.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Text.Json.Serialization;
+
+#pragma warning disable IDE0130
+// ReSharper disable once CheckNamespace - Using NS of Plan
+namespace Microsoft.SemanticKernel.Planning;
+
+public class FewShotExample
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    [JsonPropertyName("role")]
+    public Role Role { get; set; }
+
+    [JsonPropertyName("content")]
+    public string? Content { get; set; }
+}
---
+++ dotnet/src/PF-Extensions/Planning.ChatStepwisePlanner/Role.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Text.Json.Serialization;
+
+#pragma warning disable IDE0130
+// ReSharper disable once CheckNamespace - Using NS of Plan
+namespace Microsoft.SemanticKernel.Planning;
+#pragma warning restore IDE0130
+
+// TODO fix with serialization options not attributes
+public enum Role
+{
+    [JsonPropertyName("user")]
+    User,
+    [JsonPropertyName("assistant")]
+    Assistant
+}
