dde23659170010192d602808f51a9d44e681ac73
Alisha Maddy <alisha831maddy@gmail.com>
2023-07-31 17:36:12 -0700

    remove all text completion work from branch

--- python/semantic_kernel/connectors/ai/google_palm/__init__.py
+++ python/semantic_kernel/connectors/ai/google_palm/__init__.py
@@ -1,8 +1,5 @@
 # Copyright (c) Microsoft. All rights reserved.
 
-from semantic_kernel.connectors.ai.google_palm.services.gp_text_completion import (
-    GooglePalmTextCompletion,
-)
 from semantic_kernel.connectors.ai.google_palm.services.gp_chat_completion import (
     GooglePalmChatCompletion,
 )
@@ -11,7 +8,6 @@ from semantic_kernel.connectors.ai.google_palm.services.gp_text_embedding import
 )
 
 __all__ = [
-    "GooglePalmTextCompletion",
     "GooglePalmChatCompletion",
     "GooglePalmTextEmbedding",
     ]
