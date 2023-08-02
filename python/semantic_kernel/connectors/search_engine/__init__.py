2cfe3ea8bf538f215f3f937a1e314f91eda33fb8
Sneha <55897319+sneha-afk@users.noreply.github.com>
2023-08-02 15:49:29 -0700

    include wiki in init

--- python/semantic_kernel/connectors/search_engine/__init__.py
+++ python/semantic_kernel/connectors/search_engine/__init__.py
@@ -2,5 +2,8 @@
 
 from semantic_kernel.connectors.search_engine.bing_connector import BingConnector
 from semantic_kernel.connectors.search_engine.google_connector import GoogleConnector
+from semantic_kernel.connectors.search_engine.wikipedia_connector import (
+    WikipediaConnector,
+)
 
-__all__ = ["BingConnector", "GoogleConnector"]
+__all__ = ["BingConnector", "GoogleConnector", "WikipediaConnector"]
