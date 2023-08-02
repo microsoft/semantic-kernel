47cd46096512d80dcd49ef0d9d5870dd43809c22
Aditya Gudimella <aditya.gudimella@gmail.com>
2023-08-02 09:10:08 -0700

    Upgraded skill_collection

--- python/semantic_kernel/skill_definition/skill_collection.py
+++ python/semantic_kernel/skill_definition/skill_collection.py
@@ -1,9 +1,12 @@
 # Copyright (c) Microsoft. All rights reserved.
 
 from logging import Logger
-from typing import TYPE_CHECKING, Dict, Literal, Optional, Tuple
+from typing import TYPE_CHECKING, Dict, Generic, Literal, Optional, Tuple, TypeVar
+
+import pydantic as pdt
 
 from semantic_kernel.kernel_exception import KernelException
+from semantic_kernel.sk_pydantic import SKGenericModel
 from semantic_kernel.skill_definition.functions_view import FunctionsView
 from semantic_kernel.skill_definition.read_only_skill_collection import (
     ReadOnlySkillCollection,
@@ -19,19 +22,26 @@ if TYPE_CHECKING:
     )
 
 
-class SkillCollection(SkillCollectionBase):
-    _skill_collection: Dict[str, Dict[str, "SKFunctionBase"]]
-    _read_only_skill_collection: "ReadOnlySkillCollectionBase"
-    _log: Logger
+SKFunctionT = TypeVar("SKFunctionT", bound="SKFunctionBase")
+
+
+class SkillCollection(SKGenericModel, SkillCollectionBase, Generic[SKFunctionT]):
+    skill_collection: Dict[str, Dict[str, "SKFunctionBase"]]
+    _read_only_skill_collection: "ReadOnlySkillCollectionBase" = pdt.PrivateAttr()
+    _log: Logger = pdt.PrivateAttr()
 
     @property
     def read_only_skill_collection(self) -> "ReadOnlySkillCollectionBase":
         return self._read_only_skill_collection
 
-    def __init__(self, log: Optional[Logger] = None) -> None:
+    def __init__(
+        self,
+        log: Optional[Logger] = None,
+        skill_collection: Optional[Dict[str, Dict[str, SKFunctionT]]] = None,
+    ) -> None:
+        super().__init__(skill_collection=skill_collection or {})
         self._log = log if log is not None else NullLogger()
         self._read_only_skill_collection = ReadOnlySkillCollection(self)
-        self._skill_collection = {}
 
     def add_semantic_function(self, function: "SKFunctionBase") -> None:
         if function is None:
@@ -40,10 +50,10 @@ class SkillCollection(SkillCollectionBase):
         s_name, f_name = function.skill_name, function.name
         s_name, f_name = s_name.lower(), f_name.lower()
 
-        if s_name not in self._skill_collection:
-            self._skill_collection[s_name] = {}
+        if s_name not in self.skill_collection:
+            self.skill_collection[s_name] = {}
 
-        self._skill_collection[s_name][f_name] = function
+        self.skill_collection[s_name][f_name] = function
 
     def add_native_function(self, function: "SKFunctionBase") -> None:
         if function is None:
@@ -52,39 +62,39 @@ class SkillCollection(SkillCollectionBase):
         s_name, f_name = function.skill_name, function.name
         s_name, f_name = self._normalize_names(s_name, f_name, True)
 
-        if s_name not in self._skill_collection:
-            self._skill_collection[s_name] = {}
+        if s_name not in self.skill_collection:
+            self.skill_collection[s_name] = {}
 
-        self._skill_collection[s_name][f_name] = function
+        self.skill_collection[s_name][f_name] = function
 
     def has_function(self, skill_name: Optional[str], function_name: str) -> bool:
         s_name, f_name = self._normalize_names(skill_name, function_name, True)
-        if s_name not in self._skill_collection:
+        if s_name not in self.skill_collection:
             return False
-        return f_name in self._skill_collection[s_name]
+        return f_name in self.skill_collection[s_name]
 
     def has_semantic_function(self, skill_name: str, function_name: str) -> bool:
         s_name, f_name = self._normalize_names(skill_name, function_name)
-        if s_name not in self._skill_collection:
+        if s_name not in self.skill_collection:
             return False
-        if f_name not in self._skill_collection[s_name]:
+        if f_name not in self.skill_collection[s_name]:
             return False
-        return self._skill_collection[s_name][f_name].is_semantic
+        return self.skill_collection[s_name][f_name].is_semantic
 
     def has_native_function(self, skill_name: str, function_name: str) -> bool:
         s_name, f_name = self._normalize_names(skill_name, function_name, True)
-        if s_name not in self._skill_collection:
+        if s_name not in self.skill_collection:
             return False
-        if f_name not in self._skill_collection[s_name]:
+        if f_name not in self.skill_collection[s_name]:
             return False
-        return self._skill_collection[s_name][f_name].is_native
+        return self.skill_collection[s_name][f_name].is_native
 
     def get_semantic_function(
         self, skill_name: str, function_name: str
     ) -> "SKFunctionBase":
         s_name, f_name = self._normalize_names(skill_name, function_name)
         if self.has_semantic_function(s_name, f_name):
-            return self._skill_collection[s_name][f_name]
+            return self.skill_collection[s_name][f_name]
 
         self._log.error(f"Function not available: {s_name}.{f_name}")
         raise KernelException(
@@ -97,7 +107,7 @@ class SkillCollection(SkillCollectionBase):
     ) -> "SKFunctionBase":
         s_name, f_name = self._normalize_names(skill_name, function_name, True)
         if self.has_native_function(s_name, f_name):
-            return self._skill_collection[s_name][f_name]
+            return self.skill_collection[s_name][f_name]
 
         self._log.error(f"Function not available: {s_name}.{f_name}")
         raise KernelException(
@@ -110,7 +120,7 @@ class SkillCollection(SkillCollectionBase):
     ) -> FunctionsView:
         result = FunctionsView()
 
-        for skill in self._skill_collection.values():
+        for skill in self.skill_collection.values():
             for function in skill.values():
                 if include_semantic and function.is_semantic:
                     result.add_function(function.describe())
@@ -124,7 +134,7 @@ class SkillCollection(SkillCollectionBase):
     ) -> "SKFunctionBase":
         s_name, f_name = self._normalize_names(skill_name, function_name, True)
         if self.has_function(s_name, f_name):
-            return self._skill_collection[s_name][f_name]
+            return self.skill_collection[s_name][f_name]
 
         self._log.error(f"Function not available: {s_name}.{f_name}")
         raise KernelException(
