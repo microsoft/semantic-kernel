05ad853c83613ebfbd85d5b84fc774f95611bfdf
Sneha <55897319+sneha-afk@users.noreply.github.com>
2023-08-02 11:47:58 -0700

    restructure example file

--- python/samples/kernel-syntax-examples/wikipedia_search_skill.py
+++ python/samples/kernel-syntax-examples/wikipedia_search_skill.py
@@ -5,17 +5,16 @@ from semantic_kernel.connectors.ai.open_ai import OpenAITextCompletion
 from semantic_kernel.connectors.search_engine import WikipediaConnector
 from semantic_kernel.core_skills import WebSearchEngineSkill
 
+kernel = sk.Kernel()
+api_key, org_id = sk.openai_settings_from_dot_env()
+kernel.add_text_completion_service(
+    "dv", OpenAITextCompletion("text-davinci-003", api_key, org_id)
+)
+wiki_connector = WikipediaConnector()
+web_skill = kernel.import_skill(WebSearchEngineSkill(wiki_connector), "WebSearch")
 
-async def main():
-    kernel = sk.Kernel()
-    api_key, org_id = sk.openai_settings_from_dot_env()
-    kernel.add_text_completion_service(
-        "dv", OpenAITextCompletion("text-davinci-003", api_key, org_id)
-    )
-    connector = WikipediaConnector()
-    web_skill = kernel.import_skill(WebSearchEngineSkill(connector), "WebSearch")
 
-    
+async def simple_search():
     # Returns a list of tuples that contain the title and a small snippet from a relevent Wikipedia article to the query
     prompt = "Where is the Golden Gate Bridge?"
     search_async = web_skill["searchAsync"]
@@ -28,6 +27,8 @@ async def main():
     result = await search_async.invoke_async(prompt)
     print(result)
 
+
+async def semantic_function():
     # Combine with a semantic function to get output generated from data found on Wikipedia
     prompt = """
     Answer the question using only the data that is provided in the data section.
@@ -50,12 +51,13 @@ async def main():
     which is the study of attacks on machine learning algorithms and defenses against such attacks; Quantum machine 
     learning, which is the integration of quantum algorithms within machine learning programs; Active learning, which 
     is a special case of machine learning in which a learning algorithm can interactively query a user or other 
-    information source; Support vector machines, which are supervised learning models with associated learning algorithms; 
-    and Boosting, which is an ensemble meta-algorithm for reducing bias and variance in supervised learning.
+    information source; Support vector machines, which are supervised learning models with associated learning 
+    algorithms; and Boosting, which is an ensemble meta-algorithm for reducing bias and variance in supervised learning.
     """
 
 
 if __name__ == "__main__":
     import asyncio
 
-    asyncio.run(main())
+    asyncio.run(simple_search())
+    # asyncio.run(semantic_function())    # Note: beware of token usage
