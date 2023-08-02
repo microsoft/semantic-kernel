05e4f09f68dd3035f1fada33d0d856df1e9cd9e5
Gil LaHaye <gillahaye@microsoft.com>
2023-08-01 18:05:58 -0600

    .Net: Update samples to use SK 0.18 (#2255)
    ### Motivation and Context
    Time marches on,
    
    ### Description
    Updated samples to use SK 0.18 and modified code when necessary due to
    that upgrade.
    
    ### Contribution Checklist
    - [ ] The code builds clean without any errors or warnings
    - [ ] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [ ] All unit tests pass, and I have added new tests where possible
    - [ ] I didn't break anyone :smile:

--- dotnet/samples/KernelSyntaxExamples/Example48_GroundednessChecks.cs
+++ dotnet/samples/KernelSyntaxExamples/Example48_GroundednessChecks.cs
@@ -4,7 +4,6 @@ using System;
 using System.Threading.Tasks;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Planning;
-using Microsoft.SemanticKernel.Planning.Sequential;
 using Microsoft.SemanticKernel.Skills.Core;
 using RepoUtils;
 
@@ -138,8 +137,7 @@ which are not grounded in the original.
 
         kernel.ImportSkill(new TextSkill());
 
-        var plannerConfig = new SequentialPlannerConfig { };
-        var planner = new SequentialPlanner(kernel, plannerConfig);
+        var planner = new SequentialPlanner(kernel);
         var plan = await planner.CreatePlanAsync(ask);
         Console.WriteLine(plan.ToPlanWithGoalString());
 
