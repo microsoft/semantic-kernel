4d49e628bc6cbec8b647655675e0b74457534c10
Monika Janas <53838891+mojanas@users.noreply.github.com>
2023-08-03 11:00:57 -0700

    Using OpenAPI to load PlayFab APIs as skills (#3)
    * Using playfab swagger to load in APIs as skills
    
    * Comment

---
+++ dotnet/samples/KernelSyntaxExamples/Example00_03_OpenApiSkill_PlayFab.cs
@@ -0,0 +1,76 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Orchestration;
+using Microsoft.SemanticKernel.SkillDefinition;
+using Microsoft.SemanticKernel.Skills.OpenAPI.Authentication;
+using Microsoft.SemanticKernel.Skills.OpenAPI.Extensions;
+using Newtonsoft.Json;
+using RepoUtils;
+
+/// <summary>
+/// This example shows how to import PlayFab APIs as skills.
+/// </summary>
+// ReSharper disable once InconsistentNaming
+public static class Example_00_03_OpenApiSkill_PlayFab
+{
+    public static async Task RunAsync()
+    {
+        await SkillImportExample();
+    }
+
+    private static async Task SkillImportExample()
+    {
+        var kernel = new KernelBuilder().WithLogger(ConsoleLogger.Logger).Build();
+        var contextVariables = new ContextVariables();
+
+        contextVariables.Set("server_url", TestConfiguration.PlayFab.Endpoint);
+
+        using HttpClient httpClient = new();
+
+        var playfabApiSkills = await GetPlayFabSkill(kernel, httpClient);
+
+        // GetSegments skill
+        {
+            // Set properties for the Get Segments operation in the openAPI.swagger.json
+            contextVariables.Set("content_type", "application/json");
+            contextVariables.Set("payload", "{ \"SegmentIds\": [] }");
+
+            // Run operation via the semantic kernel
+            var result = await kernel.RunAsync(contextVariables, playfabApiSkills["GetSegments"]);
+
+            Console.WriteLine("\n\n\n");
+            var formattedContent = JsonConvert.SerializeObject(JsonConvert.DeserializeObject(result.Result), Formatting.Indented);
+            Console.WriteLine("GetSegments playfabApiSkills response: \n{0}", formattedContent);
+        }
+    }
+
+    private static async Task<IDictionary<string, ISKFunction>> GetPlayFabSkill(IKernel kernel, HttpClient httpClient)
+    {
+        IDictionary<string, ISKFunction> playfabApiSkills;
+
+        var titleSecretKeyProvider = new PlayFabAuthenticationProvider(() =>
+        {
+            string s = TestConfiguration.PlayFab.TitleSecretKey;
+            return Task.FromResult(s);
+        });
+
+        bool useLocalFile = true;
+        if (useLocalFile)
+        {
+            var playfabApiFile = "../../../Skills/PlayFabApiSkill/openapi.json";
+            playfabApiSkills = await kernel.ImportOpenApiSkillFromFileAsync("PlayFabApiSkill", playfabApiFile, new OpenApiSkillExecutionParameters(httpClient, authCallback: titleSecretKeyProvider.AuthenticateRequestAsync)); ;
+        }
+        else
+        {
+            var playfabApiRawFileUrl = new Uri(TestConfiguration.PlayFab.SwaggerEndpoint);
+            playfabApiSkills = await kernel.ImportOpenApiSkillFromUrlAsync("PlayFabApiSkill", playfabApiRawFileUrl, new OpenApiSkillExecutionParameters(httpClient, authCallback: titleSecretKeyProvider.AuthenticateRequestAsync)); ;
+        }
+
+        return playfabApiSkills;
+    }
+}
---
+++ dotnet/src/Skills/Skills.OpenAPI/Authentication/PlayFabAuthenticationProvider.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Microsoft.SemanticKernel.Skills.OpenAPI.Authentication;
+
+/// <summary>
+/// Retrieves the title secret key via the provided delegate and
+/// applies it to HTTP requests using the required authentication scheme.
+/// </summary>
+public class PlayFabAuthenticationProvider
+{
+    private readonly Func<Task<string>> _titleSecretKey;
+
+    /// <summary>
+    /// Creates an instance of the <see cref="PlayFabAuthenticationProvider"/> class.
+    /// </summary>
+    /// <param name="titleSecretKey">Delegate for retrieving the title secret key.</param>
+    public PlayFabAuthenticationProvider(Func<Task<string>> titleSecretKey)
+    {
+        this._titleSecretKey = titleSecretKey;
+    }
+
+    /// <summary>
+    /// Applies the authentication content to the provided HTTP request message.
+    /// </summary>
+    /// <param name="request">The HTTP request message.</param>
+    public async Task AuthenticateRequestAsync(HttpRequestMessage request)
+    {
+        var titleSecretKey = await this._titleSecretKey().ConfigureAwait(false);
+        request.Headers.Add("X-SecretKey", titleSecretKey);
+    }
+}
