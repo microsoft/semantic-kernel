ff992950b63c79aa5c437fc3a5526d4bc474c632
Mark Karle <mkarle@users.noreply.github.com>
2023-08-04 15:19:08 -0700

    Add optional batch size to OpenAI Embeddings calls

--- python/tests/unit/ai/open_ai/services/test_azure_text_embedding.py
+++ python/tests/unit/ai/open_ai/services/test_azure_text_embedding.py
@@ -1,7 +1,7 @@
 # Copyright (c) Microsoft. All rights reserved.
 
 from logging import Logger
-from unittest.mock import AsyncMock, patch
+from unittest.mock import AsyncMock, call, patch
 
 import pytest
 
@@ -142,4 +142,56 @@ async def test_azure_text_embedding_calls_with_parameters() -> None:
             api_version=api_version,
             organization=None,
             input=texts,
+        )
+
+
+@pytest.mark.asyncio
+async def test_azure_text_embedding_calls_with_batches() -> None:
+    mock_openai = AsyncMock()
+    with patch(
+        "semantic_kernel.connectors.ai.open_ai.services.open_ai_text_embedding.openai",
+        new=mock_openai,
+    ):
+        deployment_name = "test_deployment"
+        endpoint = "https://test-endpoint.com"
+        api_key = "test_api_key"
+        api_type = "azure"
+        api_version = "2023-03-15-preview"
+        logger = Logger("test_logger")
+        texts = [i for i in range(0, 5)]
+
+        azure_text_embedding = AzureTextEmbedding(
+            deployment_name=deployment_name,
+            endpoint=endpoint,
+            api_key=api_key,
+            api_version=api_version,
+            logger=logger,
+        )
+
+        await azure_text_embedding.generate_embeddings_async(texts, batch_size=3)
+
+        mock_openai.assert_has_calls(
+            [
+                call.Embedding.acreate(
+                    engine=deployment_name,
+                    api_key=api_key,
+                    api_type=api_type,
+                    api_base=endpoint,
+                    api_version=api_version,
+                    organization=None,
+                    input=texts[0:3],
+                ),
+                call.Embedding.acreate().__getitem__("data"),
+                call.Embedding.acreate().__getitem__().__iter__(),
+                call.Embedding.acreate(
+                    engine=deployment_name,
+                    api_key=api_key,
+                    api_type=api_type,
+                    api_base=endpoint,
+                    api_version=api_version,
+                    organization=None,
+                    input=texts[3:5],
+                ),
+            ],
+            any_order=False,
         )
