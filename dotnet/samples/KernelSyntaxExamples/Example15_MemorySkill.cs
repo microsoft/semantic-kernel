aa509edbe8c27819064802945d6d2d4a8724b951
Steven T. Cramer <Steven.Cramer@TimeWarp.Enterprises>
2023-08-06 19:29:13 +0700

    On further thought it seems like the intention was info1

--- dotnet/samples/KernelSyntaxExamples/Example15_MemorySkill.cs
+++ dotnet/samples/KernelSyntaxExamples/Example15_MemorySkill.cs
@@ -50,8 +50,8 @@ public static class Example15_MemorySkill
         // ========= Test memory remember =========
         Console.WriteLine("========= Example: Recalling a Memory =========");
 
-        var answer = await memorySkill.RetrieveAsync(MemoryCollectionName, "info5", logger: logger);
-        Console.WriteLine("Memory associated with 'info5': {0}", answer);
+        var answer = await memorySkill.RetrieveAsync(MemoryCollectionName, "info1", logger: logger);
+        Console.WriteLine("Memory associated with 'info1': {0}", answer);
         /*
         Output:
         "Memory associated with 'info1': My name is Andrea
