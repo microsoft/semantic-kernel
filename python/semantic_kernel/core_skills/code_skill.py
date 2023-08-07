044b23581e69827b9519dab4791689de7ea8f730
Sneha <55897319+sneha-afk@users.noreply.github.com>
2023-08-07 16:11:57 -0700

    implemented code_async

--- python/semantic_kernel/core_skills/code_skill.py
+++ python/semantic_kernel/core_skills/code_skill.py
@@ -1,8 +1,19 @@
 # Copyright (c) Microsoft. All rights reserved.
 
-import openai
+import re
+from typing import Union
 
+from semantic_kernel.connectors.ai.chat_completion_client_base import (
+    ChatCompletionClientBase,
+)
+from semantic_kernel.connectors.ai.chat_request_settings import ChatRequestSettings
+from semantic_kernel.connectors.ai.open_ai import (
+    AzureChatCompletion,
+    OpenAIChatCompletion,
+)
 from semantic_kernel.skill_definition import sk_function, sk_function_context_parameter
+
+CODE_BLOCK_PATTERN = r"```(?:.*\n)?([\s\S]*?)(?:```*)"
 
 
 class CodeSkill:
@@ -16,6 +27,14 @@ class CodeSkill:
         todo
     """
 
+    _service: ChatCompletionClientBase
+    _chat_settings: ChatRequestSettings
+
+    def __init__(
+        self, service: Union[AzureChatCompletion, OpenAIChatCompletion]
+    ) -> None:
+        self._service = service
+        self._chat_settings = ChatRequestSettings(temperature=0.0)
 
     @sk_function(
         description="Returns generated Python code from a query", name="codeAsync"
@@ -26,4 +45,22 @@ class CodeSkill:
         default_value="None",
     )
     async def code_async(self, query: str) -> str:
-        pass
+        """
+        Returns generated Python code from a query
+
+        :param query: code query
+        :param context: contains context
+        :return: string result of the query
+        """
+        prompt = f"Generate plain Python code with no extra explanation nor decoration: {query}"
+
+        result = await self._service.complete_chat_async(
+            [("user", prompt)], self._chat_settings
+        )
+
+        # Parse if there is Markdown syntax
+        code_block = re.findall(pattern=CODE_BLOCK_PATTERN, string=result)
+        if code_block and len(code_block[0]):
+            return code_block[0]
+        else:
+            return result
