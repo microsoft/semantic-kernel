d9729bb263213b6264571b645d5bf7be930a1045
Lee Miller <lemiller@microsoft.com>
2023-08-08 16:38:14 -0700

    ✨ Add support for function calls with input in tests
    This commit adds a new test case to the
    `InlineFunctionsDefinitionExtensionsTests` class, which checks if
    function calls with input are supported. The test ensures that the
    `GetEmailAddress` function can be called with an input parameter and
    returns the expected result.

--- dotnet/src/IntegrationTests/SkillDefinition/InlineFunctionsDefinitionExtensionsTests.cs
+++ dotnet/src/IntegrationTests/SkillDefinition/InlineFunctionsDefinitionExtensionsTests.cs
@@ -43,6 +43,27 @@ public sealed class InlineFunctionsDefinitionExtensionsTests : IDisposable
         Assert.Equal("Hey johndoe1234@example.com", actual.Result);
     }
 
+    [Fact]
+    public async Task ItSupportsFunctionCallsWithInput()
+    {
+        var builder = Kernel.Builder
+                .WithAIService<ITextCompletion>(null, new RedirectTextCompletion(), true)
+                .WithLogger(this._logger);
+        IKernel target = builder.Build();
+
+        var emailSkill = target.ImportSkill(new EmailSkillFake());
+
+        var prompt = "Hey {{_GLOBAL_FUNCTIONS_.GetEmailAddress \"a person\"}}";
+
+        // Act
+        SKContext actual = await target.InvokeSemanticFunctionAsync(prompt, maxTokens: 150);
+
+        // Assert
+        Assert.Null(actual.LastException);
+        Assert.False(actual.ErrorOccurred);
+        Assert.Equal("Hey a person@example.com", actual.Result);
+    }
+
     private readonly RedirectOutput _logger;
     private readonly PromptTemplateEngine _target;
 
