4d67a71f2c440695a44812c8c26deadf47aa42d4
Sebastian Santoyo <sebastian.santoyopro@gmail.com>
2023-08-10 17:25:00 -0700

    Cleaned Jina init

--- python/semantic_kernel/connectors/ai/jina_ai/__init__.py
+++ python/semantic_kernel/connectors/ai/jina_ai/__init__.py
@@ -3,8 +3,5 @@
 from semantic_kernel.connectors.ai.jina_ai.services.jina_ai_text_embedding import (
     JinaTextEmbedding,
 )
-# from semantic_kernel.connectors.ai.hugging_face.services.hf_text_embedding import (
-#     HuggingFaceTextEmbedding,
-# )
 
 __all__ = ["JinaTextEmbedding",]
