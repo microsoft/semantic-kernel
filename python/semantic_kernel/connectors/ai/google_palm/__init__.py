59371fb2ae027c0ed3f684ea877d80f592faee9c
Abby Harrison <abby.harrison@microsoft.com>
2023-08-21 11:17:49 -0700

    another precommit check run

--- python/semantic_kernel/connectors/ai/google_palm/__init__.py
+++ python/semantic_kernel/connectors/ai/google_palm/__init__.py
@@ -1,10 +1,10 @@
 # Copyright (c) Microsoft. All rights reserved.
 
-from semantic_kernel.connectors.ai.google_palm.services.gp_text_completion import (
-    GooglePalmTextCompletion,
-)
 from semantic_kernel.connectors.ai.google_palm.services.gp_chat_completion import (
     GooglePalmChatCompletion,
+)
+from semantic_kernel.connectors.ai.google_palm.services.gp_text_completion import (
+    GooglePalmTextCompletion,
 )
 from semantic_kernel.connectors.ai.google_palm.services.gp_text_embedding import (
     GooglePalmTextEmbedding,
