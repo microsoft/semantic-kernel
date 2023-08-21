090b7935738d2e109d48a7b6cc1dc8f2f05d09e2
Dmytro Struk <13853051+dmytrostruk@users.noreply.github.com>
2023-08-21 17:39:08 +0100

    Update dotnet/samples/KernelSyntaxExamples/Example54_AzureChatCompletionWithData.cs
    Co-authored-by: Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>

--- dotnet/samples/KernelSyntaxExamples/Example54_AzureChatCompletionWithData.cs
+++ dotnet/samples/KernelSyntaxExamples/Example54_AzureChatCompletionWithData.cs
@@ -58,8 +58,7 @@ public static class Example54_AzureChatCompletionWithData
             }
         }
 
-        Console.Write(Environment.NewLine);
-        Console.Write(Environment.NewLine);
+        Console.WriteLine(Environment.NewLine);
     }
 
     private static async Task ExampleWithKernel()
