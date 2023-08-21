438cfb2a03526b819e2567c0ce4200da0bb0f54f
Alisha Maddy <59581465+am831@users.noreply.github.com>
2023-08-21 16:56:44 -0700

    Python: Add Google PaLM connector with chat completion and text embedding (#2258)
    ### Motivation and Context
    
    Implementation of Google PaLM connector with chat completion, text
    embedding and three example files to demonstrate their functionality.
    Closes #2098
    I also opened a PR for text completion #2076
    
    <!-- Thank you for your contribution to the semantic-kernel repo!
    Please help reviewers and future users, providing the following
    information:
      1. Why is this change required?
      2. What problem does it solve?
      3. What scenario does it contribute to?
      4. If it fixes an open issue, please link to the issue here.
    
    -->
    
    ### Description
    **What's new:**
    1. Implemented Google Palm connector with chat completion and text
    embedding
    2. Added 3 example files to `python/samples/kernel-syntax-examples`
    3. The example files show PaLM chat in action with skills, memory, and
    just with user messages. The memory example uses PaLM embedding as well
    5. Added integration tests to test chat with skills and embedding with
    kernel.memory functions
    6. Added unit tests to verify successful class initialization and
    correct behavior from class functions
      
    **There are some important differences between google palm chat and open
    ai chat:**
    
    1. PaLM has two functions for chatting, `chat` and `reply`. The chat
    function in google's genai library starts a new conversation, and reply
    continues the conversation. Reply is an attribute of the response object
    returned by chat. So an instance of the `GooglePalmChatCompletion` class
    needs a way to determine which function to use, which is why I
    introduced a private attribute to store the response object. See
    https://developers.generativeai.google/api/python/google/generativeai/types/ChatResponse
    2. PaLM does not use system messages. Instead, the chat function takes a
    parameter called context. It serves the same purpose, to prime the
    assistant with certain behaviors and information. So when the user
    passes a system message to `complete_chat_async`, it is passed to `chat`
    as the context parameter.
    3. Semantic memory works with the chat service as long as the user
    creates a chat prompt template. The prompt containing the memory needs
    to be added to the chat prompt template as a system message. See
    `python\samples\kernel-syntax-examples\google_palm_chat_with_memory.py`
    for more details. If the only purpose of `complete_async` in
    `GooglePalmChatCompletion` is to send memories + user messages to the
    chat service as a text prompt, then `complete_async` is not fulfilling
    its intended purpose. A possible solution would be to send the text
    prompt as a request to the text service within `complete_async`.
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [x] The code builds clean without any errors or warnings
    - [x] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [x] All unit tests pass, and I have added new tests where possible
    - [x] I didn't break anyone :smile:
    
    Currently no warnings, there was 1 warning when first installing genai
    with `poetry add google.generativeai==v0.1.0rc2` from within poetry
    shell: "The locked version 0.1.0rc2 for google-generativeai is a yanked
    version. Reason for being yanked: Release is marked as supporting Py3.8,
    but in practice it requires 3.9". We would need to require later
    versions of python to fix it.
    
    ---------
    
    Co-authored-by: Abby Harrison <54643756+awharrison-28@users.noreply.github.com>
    Co-authored-by: Abby Harrison <abby.harrison@microsoft.com>
    Co-authored-by: Abby Harrison <abharris@microsoft.com>

---
+++ python/samples/kernel-syntax-examples/google_palm_chat.py
@@ -0,0 +1,48 @@
+# Copyright (c) Microsoft. All rights reserved.
+
+import asyncio
+
+import semantic_kernel as sk
+import semantic_kernel.connectors.ai.google_palm as sk_gp
+from semantic_kernel.connectors.ai.chat_request_settings import ChatRequestSettings
+
+
+async def chat_request_example(api_key):
+    palm_chat_completion = sk_gp.GooglePalmChatCompletion(
+        "models/chat-bison-001", api_key
+    )
+    settings = ChatRequestSettings()
+    settings.temperature = 1
+
+    chat_messages = list()
+    user_mssg = "I'm planning a vacation. Which are some must-visit places in Europe?"
+    chat_messages.append(("user", user_mssg))
+    answer = await palm_chat_completion.complete_chat_async(chat_messages, settings)
+    chat_messages.append(("assistant", str(answer)))
+    user_mssg = "Where should I go in France?"
+    chat_messages.append(("user", user_mssg))
+    answer = await palm_chat_completion.complete_chat_async(chat_messages, settings)
+    chat_messages.append(("assistant", str(answer)))
+
+    context_vars = sk.ContextVariables()
+    context_vars["chat_history"] = ""
+    context_vars["chat_bot_ans"] = ""
+    for role, mssg in chat_messages:
+        if role == "user":
+            context_vars["chat_history"] += f"User:> {mssg}\n"
+        elif role == "assistant":
+            context_vars["chat_history"] += f"ChatBot:> {mssg}\n"
+            context_vars["chat_bot_ans"] += f"{mssg}\n"
+
+    return context_vars
+
+
+async def main() -> None:
+    api_key = sk.google_palm_settings_from_dot_env()
+    chat = await chat_request_example(api_key)
+    print(chat["chat_history"])
+    return
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ python/tests/integration/embeddings/test_gp_embedding_service.py
@@ -0,0 +1,44 @@
+# Copyright (c) Microsoft. All rights reserved.
+
+import os
+import sys
+
+import pytest
+
+import semantic_kernel as sk
+
+if sys.version_info >= (3, 9):
+    import semantic_kernel.connectors.ai.google_palm as sk_gp
+
+pytestmark = [
+    pytest.mark.skipif(
+        sys.version_info < (3, 9), reason="Google Palm requires Python 3.9 or greater"
+    ),
+    pytest.mark.skipif(
+        "Python_Integration_Tests" in os.environ,
+        reason="Google Palm integration tests are only set up to run locally",
+    ),
+]
+
+
+@pytest.mark.asyncio
+async def test_gp_embedding_service(create_kernel, get_gp_config):
+    kernel = create_kernel
+
+    api_key = get_gp_config
+
+    palm_text_embed = sk_gp.GooglePalmTextEmbedding(
+        "models/embedding-gecko-001", api_key
+    )
+    kernel.add_text_embedding_generation_service("gecko", palm_text_embed)
+    kernel.register_memory_store(memory_store=sk.memory.VolatileMemoryStore())
+
+    await kernel.memory.save_information_async(
+        "test", id="info1", text="this is a test"
+    )
+    await kernel.memory.save_reference_async(
+        "test",
+        external_id="info1",
+        text="this is a test",
+        external_source_name="external source",
+    )
