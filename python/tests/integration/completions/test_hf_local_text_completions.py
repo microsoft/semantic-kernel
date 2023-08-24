836ed63779a2e93a1956bc57984f166c7524e3ca
Karol Żak <karol.zak@hotmail.com>
2023-08-24 18:55:17 +0200

    add falcon model test

--- python/tests/integration/completions/test_hf_local_text_completions.py
+++ python/tests/integration/completions/test_hf_local_text_completions.py
@@ -3,6 +3,9 @@
 import pytest
 
 import semantic_kernel as sk
+import semantic_kernel.connectors.ai.hugging_face as sk_hf
+import torch
+from transformers import AutoTokenizer
 
 
 @pytest.mark.asyncio
@@ -86,6 +89,58 @@ async def test_text2text_generation_input_context_with_vars_and_str(
     setup_hf_text_completion_function,
 ):
     kernel, text2text_function, simple_input = setup_hf_text_completion_function
+
+    # Complete input context with additional variables and string and print
+    context = kernel.create_new_context()
+    context["input"] = simple_input
+    context_vars = sk.ContextVariables(variables={"input2": "running and"})
+    summary = await kernel.run_async(
+        text2text_function,
+        input_context=context,
+        input_vars=context_vars,
+        input_str="new text",
+    )
+
+    output = str(summary).strip()
+    print(
+        f"Completion using context, additional variables, and additional string: '{output}'"
+    )
+    assert len(output) > 0
+
+
+@pytest.mark.asyncio
+async def test_text_generation_with_kwargs(
+    setup_hf_text_completion_function,
+):
+    _, _, simple_input = setup_hf_text_completion_function
+    
+    model_name = "tiiuae/falcon-7b-instruct"
+    
+    tokenizer = AutoTokenizer.from_pretrained(
+        pretrained_model_name_or_path=model_name,
+        trust_remote_code=True)
+    
+    hf_model = sk_hf.HuggingFaceTextCompletion(
+        model_name,
+        task="text-generation",
+        model_kwargs={
+            "torch_dtype": torch.bfloat16
+        },
+        pipeline_kwargs={
+            "tokenizer": tokenizer,
+            "trust_remote_code": True
+        }
+    )
+    
+    kernel = sk.Kernel()
+    
+    # Configure LLM service
+    kernel.add_text_completion_service(
+        "falcon-7b",
+        hf_model
+    )
+    
+    text2text_function = kernel.create_semantic_function(prompt, max_tokens=25, temperature=0.2, top_p=0.5)
 
     # Complete input context with additional variables and string and print
     context = kernel.create_new_context()
