2bba5be0db19febfe60abf04929d99dcf3f49dbf
Lisa Harrylock <lisaharrylock@gmail.com>
2023-08-24 17:33:49 -0700

    add sample

--- dotnet/samples/KernelSyntaxExamples/Program.cs
+++ dotnet/samples/KernelSyntaxExamples/Program.cs
@@ -75,6 +75,7 @@ public static class Program
         await Example53_Kusto.RunAsync().SafeWaitAsync(cancelToken);
         await Example54_AzureChatCompletionWithData.RunAsync().SafeWaitAsync(cancelToken);
         await Example55_TextChunker.RunAsync().SafeWaitAsync(cancelToken);
+        await Example56_TemplateNativeFunctionsWithMultipleArguments.RunAsync().SafeWaitAsync(cancelToken);
     }
 
     private static void LoadUserSecrets()
