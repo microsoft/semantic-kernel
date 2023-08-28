7bf5311cd31e35baeb80be5ee22e7bc1b3cf56a9
Lee Miller <lemiller@microsoft.com>
2023-08-28 15:35:26 -0700

    🔧 Update Example51_StepwisePlanner with new features and bug fixes
    Run stepwise plans for a given question with the ability to override max allowed tokens and modify the chat model used by the planner. Added a summary table of ExecutionResults to print out results. Excluded some functions from the planner to improve performance. Updated the LanguageCalculatorSkill to the semanticCalculator. Fixed a bug where the planner was not using the correct logger factory. Improved the suffix feature to add more context to the question.
    
    🔧 Update StepwisePlanner with execution results
    
    Added functionality to capture and display execution results for StepwisePlanner. Includes the answer to the question, steps taken, skills used, iterations, time taken, and model used. Also added the ability to choose between Azure Chat and Text Completion services and specify a model.
    
    The updates to Example51_StepwisePlanner include new features such as the ability to override max allowed tokens and modify the chat model used by the planner. A summary table of ExecutionResults has been added to print out results. Some functions have been excluded from the planner to improve performance. The LanguageCalculatorSkill has been updated to the semanticCalculator. A bug where the planner was not using the correct logger factory has been fixed. The suffix feature has been improved to add more context to the question. Additionally, the StepwisePlanner has been updated to include functionality to capture and display execution results, including the answer to the question, steps taken, skills used, iterations, time taken, and model used. The update also includes the ability to choose between Azure Chat and Text Completion services and specify a model.

--- dotnet/samples/KernelSyntaxExamples/Example51_StepwisePlanner.cs
+++ dotnet/samples/KernelSyntaxExamples/Example51_StepwisePlanner.cs
@@ -1,9 +1,12 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Orchestration;
 using Microsoft.SemanticKernel.Planning;
 using Microsoft.SemanticKernel.Skills.Core;
 using Microsoft.SemanticKernel.Skills.Web;
@@ -12,36 +15,103 @@ using NCalcSkills;
 using RepoUtils;
 
 /**
- * This example shows how to use Stepwise Planner to create a plan for a given goal.
+ * This example shows how to use Stepwise Planner to create and run a stepwise plan for a given goal.
  */
 
 // ReSharper disable once InconsistentNaming
 public static class Example51_StepwisePlanner
 {
+    // Used to override the max allowed tokens when running the plan
+    internal static int? ChatMaxTokens = null;
+    internal static int? TextMaxTokens = null;
+
+    // Used to quickly modify the chat model used by the planner
+    internal static string? ChatModelOverride = null; //"gpt-35-turbo";
+    internal static string? TextModelOverride = null; //"text-davinci-003";
+
+    internal static string? Suffix = null;
+
     public static async Task RunAsync()
     {
         string[] questions = new string[]
         {
-            "Who is the current president of the United States? What is his current age divided by 2",
-            // "Who is Leo DiCaprio's girlfriend? What is her current age raised to the (his current age)/100 power?",
-            // "What is the capital of France? Who is that city's current mayor? What percentage of their life has been in the 21st century as of today?",
-            // "What is the current day of the calendar year? Using that as an angle in degrees, what is the area of a unit circle with that angle?"
+            "What color is the sky?",
+            "What is the weather in Seattle?",
+            "What is the tallest mountain on Earth? How tall is it divided by 2?",
+            "What is the capital of France? Who is that city's current mayor? What percentage of their life has been in the 21st century as of today?",
+            "What is the current day of the calendar year? Using that as an angle in degrees, what is the area of a unit circle with that angle?",
+            "If a spacecraft travels at 0.99 the speed of light and embarks on a journey to the nearest star system, Alpha Centauri, which is approximately 4.37 light-years away, how much time would pass on Earth during the spacecraft's voyage?"
         };
 
         foreach (var question in questions)
         {
-            var kernel = GetKernel();
-            await RunWithQuestion(kernel, question);
+            for (int i = 0; i < 1; i++)
+            {
+                // await RunTextCompletion(question);
+                await RunChatCompletion(question);
+            }
+        }
+
+        PrintResults();
+    }
+
+    // print out summary table of ExecutionResults
+    private static void PrintResults()
+    {
+        Console.WriteLine("**************************");
+        Console.WriteLine("Execution Results Summary:");
+        Console.WriteLine("**************************");
+
+        foreach (var question in ExecutionResults.Select(s => s.question).Distinct())
+        {
+            Console.WriteLine("Question: " + question);
+            Console.WriteLine("Mode\tModel\tAnswer\tStepsTaken\tIterations\tTimeTaken");
+            foreach (var er in ExecutionResults.OrderByDescending(s => s.model).Where(s => s.question == question))
+            {
+                Console.WriteLine($"{er.mode}\t{er.model}\t{er.stepsTaken}\t{er.iterations}\t{er.timeTaken}\t{er.answer}");
+            }
         }
     }
 
-    private static async Task RunWithQuestion(IKernel kernel, string question)
+    private struct ExecutionResult
     {
+        public string mode;
+        public string? model;
+        public string? question;
+        public string? answer;
+        public string? stepsTaken;
+        public string? iterations;
+        public string? timeTaken;
+    }
+
+    private static List<ExecutionResult> ExecutionResults = new();
+
+    private static async Task RunTextCompletion(string question)
+    {
+        Console.WriteLine("RunTextCompletion");
+        ExecutionResult currentExecutionResult = default;
+        currentExecutionResult.mode = "RunTextCompletion";
+        var kernel = GetKernel(ref currentExecutionResult);
+        await RunWithQuestion(kernel, currentExecutionResult, question, TextMaxTokens);
+    }
+
+    private static async Task RunChatCompletion(string question, string? model = null)
+    {
+        Console.WriteLine("RunChatCompletion");
+        ExecutionResult currentExecutionResult = default;
+        currentExecutionResult.mode = "RunChatCompletion";
+        var kernel = GetKernel(ref currentExecutionResult, true, model);
+        await RunWithQuestion(kernel, currentExecutionResult, question, ChatMaxTokens);
+    }
+
+    private static async Task RunWithQuestion(IKernel kernel, ExecutionResult currentExecutionResult, string question, int? MaxTokens = null, bool useChatStep = false, bool preferText = false)
+    {
+        currentExecutionResult.question = question;
         var bingConnector = new BingConnector(TestConfiguration.Bing.ApiKey);
         var webSearchEngineSkill = new WebSearchEngineSkill(bingConnector);
 
         kernel.ImportSkill(webSearchEngineSkill, "WebSearch");
-        kernel.ImportSkill(new LanguageCalculatorSkill(kernel), "advancedCalculator");
+        kernel.ImportSkill(new LanguageCalculatorSkill(kernel), "semanticCalculator");
         kernel.ImportSkill(new TimeSkill(), "time");
 
         Console.WriteLine("*****************************************************");
@@ -50,39 +120,97 @@ public static class Example51_StepwisePlanner
 
         var plannerConfig = new Microsoft.SemanticKernel.Planning.Stepwise.StepwisePlannerConfig();
         plannerConfig.ExcludedFunctions.Add("TranslateMathProblem");
+        plannerConfig.ExcludedFunctions.Add("DaysAgo");
+        plannerConfig.ExcludedFunctions.Add("DateMatchingLastDayName");
         plannerConfig.MinIterationTimeMs = 1500;
-        plannerConfig.MaxTokens = 4000;
+        plannerConfig.LoggerFactory = ConsoleLogger.LoggerFactory;
 
-        StepwisePlanner planner = new(kernel, plannerConfig);
-        sw.Start();
-        var plan = planner.CreatePlan(question);
-
-        var result = await plan.InvokeAsync(kernel.CreateNewContext());
-        Console.WriteLine("Result: " + result);
-        if (result.Variables.TryGetValue("stepCount", out string? stepCount))
+        if (!string.IsNullOrEmpty(Suffix))
         {
-            Console.WriteLine("Steps Taken: " + stepCount);
+            plannerConfig.Suffix = $"{Suffix}\n{plannerConfig.Suffix}";
+            currentExecutionResult.question = $"[Assisted] - {question}";
         }
 
-        if (result.Variables.TryGetValue("skillCount", out string? skillCount))
+        if (MaxTokens.HasValue)
         {
-            Console.WriteLine("Skills Used: " + skillCount);
+            plannerConfig.MaxTokens = MaxTokens.Value;
+        }
+
+        SKContext result;
+        sw.Start();
+
+        try
+        {
+            StepwisePlanner planner = new(kernel: kernel, config: plannerConfig);
+            var plan = planner.CreatePlan(question);
+
+            result = await plan.InvokeAsync(kernel.CreateNewContext());
+
+            if (result.Result.Contains("Result not found, review _stepsTaken to see what", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Could not answer question in " + plannerConfig.MaxIterations + " iterations");
+                currentExecutionResult.answer = "Could not answer question in " + plannerConfig.MaxIterations + " iterations";
+            }
+            else
+            {
+                Console.WriteLine("Result: " + result.Result);
+                currentExecutionResult.answer = result.Result;
+            }
+
+            if (result.Variables.TryGetValue("stepCount", out string? stepCount))
+            {
+                Console.WriteLine("Steps Taken: " + stepCount);
+                currentExecutionResult.stepsTaken = stepCount;
+            }
+
+            if (result.Variables.TryGetValue("skillCount", out string? skillCount))
+            {
+                Console.WriteLine("Skills Used: " + skillCount);
+            }
+
+            if (result.Variables.TryGetValue("iterations", out string? iterations))
+            {
+                Console.WriteLine("Iterations: " + iterations);
+                currentExecutionResult.iterations = iterations;
+            }
+        }
+#pragma warning disable CA1031
+        catch (Exception ex)
+        {
+            Console.WriteLine("Exception: " + ex);
         }
 
         Console.WriteLine("Time Taken: " + sw.Elapsed);
+        currentExecutionResult.timeTaken = sw.Elapsed.ToString();
+        ExecutionResults.Add(currentExecutionResult);
         Console.WriteLine("*****************************************************");
     }
 
-    private static IKernel GetKernel()
+    private static IKernel GetKernel(ref ExecutionResult result, bool useChat = false, string? model = null)
     {
         var builder = new KernelBuilder();
+        if (useChat)
+        {
+            builder.WithAzureChatCompletionService(
+                model ?? ChatModelOverride ?? TestConfiguration.AzureOpenAI.ChatDeploymentName,
+                TestConfiguration.AzureOpenAI.Endpoint,
+                TestConfiguration.AzureOpenAI.ApiKey,
+                alsoAsTextCompletion: true,
+                setAsDefault: true);
 
-        builder.WithAzureChatCompletionService(
-            TestConfiguration.AzureOpenAI.ChatDeploymentName,
-            TestConfiguration.AzureOpenAI.Endpoint,
-            TestConfiguration.AzureOpenAI.ApiKey,
-            alsoAsTextCompletion: true,
-            setAsDefault: true);
+            Console.WriteLine($"Model: {model ?? ChatModelOverride ?? TestConfiguration.AzureOpenAI.ChatDeploymentName} ({ChatMaxTokens ?? (new Microsoft.SemanticKernel.Planning.Stepwise.StepwisePlannerConfig()).MaxTokens})");
+            result.model = model ?? ChatModelOverride ?? TestConfiguration.AzureOpenAI.ChatDeploymentName;
+        }
+        else
+        {
+            builder.WithAzureTextCompletionService(
+                model ?? TextModelOverride ?? TestConfiguration.AzureOpenAI.DeploymentName,
+                TestConfiguration.AzureOpenAI.Endpoint,
+                TestConfiguration.AzureOpenAI.ApiKey);
+
+            Console.WriteLine($"Model: {model ?? TextModelOverride ?? TestConfiguration.AzureOpenAI.DeploymentName} ({TextMaxTokens ?? (new Microsoft.SemanticKernel.Planning.Stepwise.StepwisePlannerConfig()).MaxTokens})");
+            result.model = model ?? TextModelOverride ?? TestConfiguration.AzureOpenAI.DeploymentName;
+        }
 
         var kernel = builder
             .WithLoggerFactory(ConsoleLogger.LoggerFactory)
@@ -97,32 +225,3 @@ public static class Example51_StepwisePlanner
         return kernel;
     }
 }
-
-// *****************************************************
-// Question: Who is the current president of the United States? What is his current age divided by 2
-// Result: The current president of the United States is Joe Biden. His current age divided by 2 is 40.5.
-// Steps Taken: 9
-// Skills Used: 7 (WebSearch.Search(4), time.Year(1), time.Date(1), advancedCalculator.Calculator(1))
-// Time Taken: 00:01:13.3766860
-// *****************************************************
-// *****************************************************
-// Question: Who is Leo DiCaprio's girlfriend? What is her current age raised to the (his current age)/100 power?
-// Result: Leo DiCaprio's girlfriend is Camila Morrone. Her current age raised to the power of (his current age)/100 is approximately 4.94.
-// Steps Taken: 9
-// Skills Used: 5 (WebSearch.Search(3), time.Year(1), advancedCalculator.Calculator(1))
-// Time Taken: 00:01:17.6742136
-// *****************************************************
-// *****************************************************
-// Question: What is the capital of France? Who is that cities current mayor? What percentage of their life has been in the 21st century as of today?
-// Result: The capital of France is Paris. The current mayor of Paris is Anne Hidalgo, who was born on June 19, 1959. As of today, she has lived for 64 years, with 23 of those years in the 21st century. Therefore, 35.94% of her life has been spent in the 21st century.
-// Steps Taken: 14
-// Skills Used: 12 (WebSearch.Search(8), time.Year(1), advancedCalculator.Calculator(3))
-// Time Taken: 00:02:06.6682909
-// *****************************************************
-// *****************************************************
-// Question: What is the current day of the calendar year? Using that as an angle in degrees, what is the area of a unit circle with that angle?
-// Result: The current day of the year is 177. Using that as an angle in degrees (approximately 174.58), the area of a unit circle with that angle is approximately 1.523 square units.
-// Steps Taken: 11
-// Skills Used: 9 (time.Now(1), time.DayOfYear(1), time.DaysBetween(1), time.MonthNumber(1), time.Day(1), advancedCalculator.Calculator(4))
-// Time Taken: 00:01:41.5585861
-// *****************************************************
