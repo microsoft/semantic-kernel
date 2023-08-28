7ab1d66501efeae23ab7f5ab0b45238a38d8ce5e
Milder Hernandez Cagua <milderhc@microsoft.com>
2023-08-28 09:49:34 -0500

    Update connector refactor draft

--- java/connectors/semantickernel-connectors-memory-postgresql/src/main/java/com/microsoft/semantickernel/connectors/memory/postgresql/PostgreSQLMemoryStore.java
+++ java/connectors/semantickernel-connectors-memory-postgresql/src/main/java/com/microsoft/semantickernel/connectors/memory/postgresql/PostgreSQLMemoryStore.java
@@ -1,36 +1,48 @@
 // Copyright (c) Microsoft. All rights reserved.
 package com.microsoft.semantickernel.connectors.memory.postgresql;
 
+import com.microsoft.semantickernel.connectors.memory.jdbc.SQLConnector;
 import com.microsoft.semantickernel.connectors.memory.jdbc.JDBCMemoryStore;
+import com.microsoft.semantickernel.connectors.memory.jdbc.SQLMemoryStoreBuilder;
 import com.microsoft.semantickernel.memory.MemoryStore;
 import reactor.core.publisher.Mono;
 
-import javax.annotation.Nonnull;
 import java.sql.Connection;
-import java.sql.SQLException;
 
 public class PostgreSQLMemoryStore extends JDBCMemoryStore {
-
-    public PostgreSQLMemoryStore() {
-        this.dbConnector = new PostgreSQLConnector();
+    public PostgreSQLMemoryStore(SQLConnector connector) {
+        super(connector);
+    }
+    public Mono<Void> connectAsync() {
+        return super.connectAsync();
     }
 
-    public PostgreSQLMemoryStore(Connection connection) {
-        this();
-        this.dbConnection = connection;
-    }
+    /**
+     * Builds an PostgreSQLMemoryStore.
+     */
+    public static class Builder implements SQLMemoryStoreBuilder {
+        private Connection connection;
 
-    public Mono<Void> connectAsync(@Nonnull String url) throws SQLException {
-        return super.connectAsync(url);
-    }
-
-    public static class Builder implements MemoryStore.Builder {
+        /**
+         * Builds and returns a PostgreSQLMemoryStore instance with the specified database connection.
+         *
+         * @return A PostgreSQLMemoryStore instance configured with the provided PostgreSQL database connection.
+         */
         @Override
         public MemoryStore build() {
-            return new PostgreSQLMemoryStore();
+            return new PostgreSQLMemoryStore(new PostgreSQLConnector(connection));
         }
-        public MemoryStore build(Connection connection) {
-            return new PostgreSQLMemoryStore(connection);
+
+        /**
+         * Sets the PostgreSQL database connection to be used by the PostgreSQL memory store being built.
+         *
+         * @param connection The Connection object representing the PostgreSQL database connection.
+         * @return The updated Builder instance to continue the building process for a PostgreSQLMemoryStore.
+         */
+        @Override
+        public Builder withConnection(Connection connection) {
+            this.connection = connection;
+            return this;
         }
     }
 }
