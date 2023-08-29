46437a07e5a221b950c01ec22bcd3b0375855d91
Diego Colombo <dicolomb@microsoft.com>
2023-08-30 00:19:52 +0200

    implement cosine similarity in duck db

--- dotnet/src/Connectors/Connectors.Memory.DuckDB/Database.cs
+++ dotnet/src/Connectors/Connectors.Memory.DuckDB/Database.cs
@@ -1,11 +1,14 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 using DuckDB.NET.Data;
+
+using Microsoft.SemanticKernel.AI.Embeddings;
 
 namespace Microsoft.SemanticKernel.Connectors.Memory.DuckDB;
 
@@ -18,6 +21,8 @@ internal struct DatabaseEntry
     public string EmbeddingString { get; set; }
 
     public string? Timestamp { get; set; }
+
+    public float Score { get; set; }
 }
 
 internal sealed class Database
@@ -25,6 +30,18 @@ internal sealed class Database
     private const string TableName = "SKMemoryTable";
 
     public Database() { }
+
+    public Task CreateFunctionsAsync(DuckDBConnection conn, CancellationToken cancellationToken)
+    {
+        using var cmd = conn.CreateCommand();
+        cmd.CommandText = @"
+                          CREATE OR REPLACE MACRO cosine_similarity(a,b) AS (select sum (xy) from (select x * y  as xy from (select  UNNEST(a) as x,  UNNEST(b) as y))) / sqrt(list_aggregate(list_transform(a, x -> x * x), 'sum') * list_aggregate(list_transform(b,  x -> x * x), 'sum'));
+                          CREATE OR REPLACE MACRO split_string_of_numbers(t) AS regexp_extract_all(regexp_replace(t,'(\[|\])', '', 'g'), '([+-]?([0-9]*[.])?[0-9]+)(\s*;\s*)?',1);
+                          CREATE OR REPLACE MACRO number_vector_decoder(t) AS list_transform(split_string_of_numbers(t), x -> cast(x AS double));
+                          CREATE OR REPLACE MACRO encode_number_vector(t) AS concat('[',list_aggregate(list_transform(t, x -> cast(x AS string)), 'string_agg', '; '),']');
+                          ";
+        return cmd.ExecuteNonQueryAsync(cancellationToken);
+    }
 
     public Task CreateTableAsync(DuckDBConnection conn, CancellationToken cancellationToken = default)
     {
@@ -34,7 +51,7 @@ internal sealed class Database
                 collection TEXT,
                 key TEXT,
                 metadata TEXT,
-                embedding TEXT,
+                embedding FLOAT[],
                 timestamp TEXT,
                 PRIMARY KEY(collection, key))";
         return cmd.ExecuteNonQueryAsync(cancellationToken);
@@ -50,9 +67,8 @@ internal sealed class Database
 
         using var cmd = conn.CreateCommand();
         cmd.CommandText = $@"
-                INSERT INTO {TableName} VALUES (?1,?2,?3,?4,?5 ); ";
+                INSERT INTO {TableName} VALUES (?1,?2,?3, [], ?4 ); ";
         cmd.Parameters.Add(new DuckDBParameter(collectionName));
-        cmd.Parameters.Add(new DuckDBParameter(string.Empty));
         cmd.Parameters.Add(new DuckDBParameter(string.Empty));
         cmd.Parameters.Add(new DuckDBParameter(string.Empty));
         cmd.Parameters.Add(new DuckDBParameter(string.Empty));
@@ -60,17 +76,23 @@ internal sealed class Database
         await cmd.ExecuteNonQueryAsync(cancellationToken).ConfigureAwait(false);
     }
 
-    public async Task UpdateOrInsertAsync(DuckDBConnection conn,
-        string collection, string key, string? metadata, string? embedding, string? timestamp, CancellationToken cancellationToken = default)
+    private static string EncodeFloatArrayToString(float[] data)
     {
+        var dataArrayString = $"[{string.Join(", ", (data ?? Array.Empty<float>()).Select(n => n.ToString("F10")))}]";
+        return dataArrayString;
+    }
+
+    public async Task UpdateOrInsertAsync(DuckDBConnection conn,
+        string collection, string key, string? metadata, float[]? embedding, string? timestamp, CancellationToken cancellationToken = default)
+    {
+        var embeddingArrayString = EncodeFloatArrayToString(embedding ?? Array.Empty<float>());
         using var cmd = conn.CreateCommand();
         cmd.CommandText = $@"
-             INSERT INTO {TableName} VALUES(?1, ?2, ?3, ?4, ?5)
-             ON CONFLICT (collection, key) DO UPDATE SET metadata=?3, embedding=?4, timestamp=?5; ";
+             INSERT INTO {TableName} VALUES(?1, ?2, ?3, {embeddingArrayString}, ?4)
+             ON CONFLICT (collection, key) DO UPDATE SET metadata=?3, embedding={embeddingArrayString}, timestamp=?4; ";
         cmd.Parameters.Add(new DuckDBParameter(collection));
         cmd.Parameters.Add(new DuckDBParameter(key));
         cmd.Parameters.Add(new DuckDBParameter(metadata ?? string.Empty));
-        cmd.Parameters.Add(new DuckDBParameter(embedding ?? string.Empty));
         cmd.Parameters.Add(new DuckDBParameter(timestamp ?? string.Empty));
         await cmd.ExecuteNonQueryAsync(cancellationToken).ConfigureAwait(false);
     }
@@ -98,13 +120,55 @@ internal sealed class Database
         }
     }
 
+    public async IAsyncEnumerable<DatabaseEntry> GetNearestMatchesAsync(
+        DuckDBConnection conn,
+        string collectionName,
+        float[] embedding,
+        int limit,
+        double minRelevanceScore = 0,
+        [EnumeratorCancellation] CancellationToken cancellationToken = default)
+    {
+        var embeddingArrayString = EncodeFloatArrayToString(embedding ?? Array.Empty<float>());
+
+        using var cmd = conn.CreateCommand();
+        cmd.CommandText = $@"
+            SELECT key, metadata, timestamp, cast(embedding as string) as embeddingAsString, cast(cosine_similarity(embedding,{embeddingArrayString}) as FLOAT) as score FROM {TableName}
+            WHERE collection=?1 AND score >= {minRelevanceScore:F12}
+            ORDER BY score DESC
+            LIMIT {limit};";
+        cmd.Parameters.Add(new DuckDBParameter(collectionName));
+
+        using var dataReader = await cmd.ExecuteReaderAsync(cancellationToken).ConfigureAwait(false);
+        while (await dataReader.ReadAsync(cancellationToken).ConfigureAwait(false))
+        {
+            string key = dataReader.GetString("key");
+            if (string.IsNullOrWhiteSpace(key))
+            {
+                continue;
+            }
+
+            string metadata = dataReader.GetString("metadata");
+            string embeddingAsString = dataReader.GetString("embeddingAsString");
+            string timestamp = dataReader.GetString("timestamp");
+            float score = dataReader.GetFloat("score");
+            yield return new DatabaseEntry
+            {
+                Key = key,
+                MetadataString = metadata,
+                EmbeddingString = embeddingAsString,
+                Timestamp = timestamp,
+                Score = score
+            };
+        }
+    }
+
     public async IAsyncEnumerable<DatabaseEntry> ReadAllAsync(DuckDBConnection conn,
         string collectionName,
         [EnumeratorCancellation] CancellationToken cancellationToken = default)
     {
         using var cmd = conn.CreateCommand();
         cmd.CommandText = $@"
-            SELECT * FROM {TableName}
+            SELECT key, metadata, timestamp, cast(embedding as string) as embeddingAsString FROM {TableName}
             WHERE collection=?1;";
         cmd.Parameters.Add(new DuckDBParameter(collectionName));
 
@@ -116,10 +180,17 @@ internal sealed class Database
             {
                 continue;
             }
+
             string metadata = dataReader.GetString("metadata");
-            string embedding = dataReader.GetString("embedding");
+            string embeddingAsString = dataReader.GetString("embeddingAsString");
             string timestamp = dataReader.GetString("timestamp");
-            yield return new DatabaseEntry() { Key = key, MetadataString = metadata, EmbeddingString = embedding, Timestamp = timestamp };
+            yield return new DatabaseEntry
+            {
+                Key = key,
+                MetadataString = metadata,
+                EmbeddingString = embeddingAsString,
+                Timestamp = timestamp
+            };
         }
     }
 
@@ -130,7 +201,7 @@ internal sealed class Database
     {
         using var cmd = conn.CreateCommand();
         cmd.CommandText = $@"
-             SELECT * FROM {TableName}
+             SELECT metadata, timestamp, cast(embedding as string) as embeddingAsString FROM {TableName}
              WHERE collection=?1
                 AND key=?2; ";
         cmd.Parameters.Add(new DuckDBParameter(collectionName));
@@ -140,13 +211,13 @@ internal sealed class Database
         if (await dataReader.ReadAsync(cancellationToken).ConfigureAwait(false))
         {
             string metadata = dataReader.GetString(dataReader.GetOrdinal("metadata"));
-            string embedding = dataReader.GetString(dataReader.GetOrdinal("embedding"));
+            string embeddingAsString = dataReader.GetString(dataReader.GetOrdinal("embeddingAsString"));
             string timestamp = dataReader.GetString(dataReader.GetOrdinal("timestamp"));
-            return new DatabaseEntry()
+            return new DatabaseEntry
             {
                 Key = key,
                 MetadataString = metadata,
-                EmbeddingString = embedding,
+                EmbeddingString = embeddingAsString,
                 Timestamp = timestamp
             };
         }
