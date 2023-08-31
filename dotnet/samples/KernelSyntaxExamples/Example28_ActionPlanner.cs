ac4e4477b51d1322a13ef8cfe7e0a1618bea7a9b
kevdome3000 <tech@chancetheapp.com>
2023-08-30 21:21:04 -0700

    Refactor ActionPlanner usage and add StructuredActionPlanner for better action selection
    This commit refactorizes Example28_ActionPlanner to separate concerns and enhance code readability. The kernel building steps are now done inside dedicated methods, UseActionPlanner and UseStructuredActionPlanner.
    Additionally, a new StructuredActionPlanner class is added to allow more specific action planning. The goal is to provide the system with the ability to select the best action for achieving a given task.
    The commit also updates the project file to include the necessary dependencies for the new functionality and a reference to the OpenAI connector is added. This is necessary for the implementation of the StructuredActionPlanner.
    Finally, some improvements to the previous code are committed for better output messages.

--- dotnet/samples/KernelSyntaxExamples/Example28_ActionPlanner.cs
+++ dotnet/samples/KernelSyntaxExamples/Example28_ActionPlanner.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Orchestration;
 using Microsoft.SemanticKernel.Planning;
+using Microsoft.SemanticKernel.Planning.Action;
 using RepoUtils;
+
 
 // ReSharper disable once InconsistentNaming
 public static class Example28_ActionPlanner
@@ -13,45 +15,80 @@ public static class Example28_ActionPlanner
     public static async Task RunAsync()
     {
         Console.WriteLine("======== Action Planner ========");
-        var kernel = new KernelBuilder()
+        var builder = new KernelBuilder()
             .WithLoggerFactory(ConsoleLogger.LoggerFactory)
             .WithAzureChatCompletionService(
                 TestConfiguration.AzureOpenAI.ChatDeploymentName,
                 TestConfiguration.AzureOpenAI.Endpoint,
-                TestConfiguration.AzureOpenAI.ApiKey)
-            .Build();
+                TestConfiguration.AzureOpenAI.ApiKey);
+
+        // var builder = new KernelBuilder()
+        //     .WithLoggerFactory(ConsoleLogger.LoggerFactory)
+        //     .WithOpenAIChatCompletionService(
+        //         TestConfiguration.OpenAI.ChatModelId,
+        //         TestConfiguration.OpenAI.ApiKey);
 
         string folder = RepoFiles.SampleSkillsPath();
+        IKernel kernel = builder.Build();
         kernel.ImportSemanticSkillFromDirectory(folder, "SummarizeSkill");
         kernel.ImportSemanticSkillFromDirectory(folder, "WriterSkill");
         kernel.ImportSemanticSkillFromDirectory(folder, "FunSkill");
-
-        // Create an instance of ActionPlanner.
-        // The ActionPlanner takes one goal and returns a single function to execute.
-        var planner = new ActionPlanner(kernel);
 
         // We're going to ask the planner to find a function to achieve this goal.
         var goal = "Write a joke about Cleopatra in the style of Hulk Hogan.";
 
         // The planner returns a plan, consisting of a single function
         // to execute and achieve the goal requested.
-        var plan = await planner.CreatePlanAsync(goal);
-
+        // var plan = await planner.CreatePlanAsync(goal);
         // Execute the full plan (which is a single function)
-        SKContext result = await plan.InvokeAsync();
+        SKContext result = await UseActionPlanner(kernel, goal).ConfigureAwait(false);
 
-        // Show the result, which should match the given goal
+        //Show the result, which should match the given goal
         Console.WriteLine(result);
 
-        /* Output:
-         *
-         * Cleopatra was a queen
-         * But she didn't act like one
-         * She was more like a teen
+        IKernel kernel2 = builder.Build();
+        kernel2.ImportSemanticSkillFromDirectory(folder, "SummarizeSkill");
+        kernel2.ImportSemanticSkillFromDirectory(folder, "WriterSkill");
+        kernel2.ImportSemanticSkillFromDirectory(folder, "FunSkill");
+        SKContext structuredResult = await UseStructuredActionPlanner(kernel2, goal).ConfigureAwait(false);
 
-         * She was always on the scene
-         * And she loved to be seen
-         * But she didn't have a queenly bone in her body
+        Console.WriteLine(structuredResult);
+        /* Output:
+         * Why did Cleopatra refuse to wrestle Hulk Hogan at the office party?
+         * Because she heard he was the king of "de-Nile" moves!
          */
+
+        /* Usage: ActionPlanner
+         * Total tokens: 1179 + 148 = 1327 (plan + execution respectively)
+         * Total prompt tokens: 1123 + 105 = 1228 (plan + execution respectively)
+         * Total completion tokens: 56 + 43 = 99 (plan + execution respectively)
+         */
+
+        /* Usage: StructuredActionPlanner
+         * Total tokens: 709 + 136 = 845 (plan + execution respectively)
+         * Total prompt tokens: 584 + 105 = 689 (plan + execution respectively)
+         * Total completion tokens: 125 + 31 = 156 (plan + execution respectively)
+         */
+    }
+
+
+    private static async Task<SKContext> UseActionPlanner(IKernel kernel, string goal)
+    {
+        // Create an instance of ActionPlanner.
+        // The ActionPlanner takes one goal and returns a single function to execute.
+        var planner = new ActionPlanner(kernel);
+
+        // The planner returns a plan, consisting of a single function
+        // to execute and achieve the goal requested.
+        var plan = await planner.CreatePlanAsync(goal);
+        return await plan.InvokeAsync();
+    }
+
+
+    private static async Task<SKContext> UseStructuredActionPlanner(IKernel kernel, string goal)
+    {
+        var planner = new StructuredActionPlanner(kernel);
+        var plan = await planner.CreatePlanAsync(goal);
+        return await plan.InvokeAsync();
     }
 }
---
+++ dotnet/src/Extensions/Planning.ActionPlanner/StructuredActionPlanner.cs
@@ -0,0 +1,169 @@
+// Copyright (c) Microsoft. All rights reserved.
+namespace Microsoft.SemanticKernel.Planning.Action;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using AI.ChatCompletion;
+using Azure.AI.OpenAI;
+using Connectors.AI.OpenAI.AzureSdk.FunctionCalling;
+using Connectors.AI.OpenAI.ChatCompletion;
+using Diagnostics;
+using Extensions.Logging;
+using Extensions.Logging.Abstractions;
+using Orchestration;
+using SkillDefinition;
+using TemplateEngine.Prompt;
+
+
+/// <summary>
+///  Action planner that uses the OpenAI chat completion function calling API to select the best action to take.
+/// </summary>
+public class StructuredActionPlanner : IActionPlanner
+{
+    private const string SkillName = "this";
+
+    // Planner semantic function
+    private readonly ISKFunction _plannerFunction;
+
+    // Context used to access the list of functions in the kernel
+    private readonly SKContext _context;
+    private readonly IKernel _kernel;
+    private readonly ILogger _logger;
+
+
+    /// <summary>
+    ///  Initializes a new instance of the <see cref="StructuredActionPlanner"/> class.
+    /// </summary>
+    /// <param name="kernel"></param>
+    /// <param name="prompt"></param>
+    /// <param name="loggerFactory"></param>
+    public StructuredActionPlanner(
+        IKernel kernel,
+        string? prompt = null,
+        ILoggerFactory? loggerFactory = null)
+    {
+        Verify.NotNull(kernel);
+
+        _logger = loggerFactory is not null ? loggerFactory.CreateLogger(nameof(ActionPlanner)) : NullLogger.Instance;
+
+        var promptTemplate = prompt ?? PromptTemplate;
+
+        _plannerFunction = kernel.CreateSemanticFunction(
+            skillName: SkillName,
+            promptTemplate: promptTemplate,
+            maxTokens: 1024);
+
+        kernel.ImportSkill(this, SkillName);
+
+        _kernel = kernel;
+        _context = kernel.CreateNewContext();
+    }
+
+
+    /// <inheritdoc />
+    public async Task<Plan> CreatePlanAsync(string goal, CancellationToken cancellationToken = default)
+    {
+        if (string.IsNullOrEmpty(goal))
+        {
+            throw new SKException("The goal specified is empty");
+        }
+
+        // temporary solution until deeper integration with the kernel
+        _context.Variables.Update(goal);
+        var templateEngine = new PromptTemplateEngine();
+        var prompt = await templateEngine.RenderAsync(PromptTemplate, _context, cancellationToken).ConfigureAwait(false);
+
+        var chatCompletion = _kernel.GetService<IChatCompletion>();
+        var openAIChatCompletion = (OpenAIChatCompletion)chatCompletion;
+
+        var chatHistory = openAIChatCompletion.CreateNewChat(prompt);
+
+        List<FunctionDefinition> functionDefinitions = _context.Skills.GetFunctionDefinitions(new[] { SkillName }).ToList();
+        // the intended functionCall must always be included in the list of function definitions
+        functionDefinitions.Add(ActionPlan);
+        var response = await openAIChatCompletion.GenerateResponseAsync<SKFunctionCall>(
+            chatHistory,
+            new ChatRequestSettings()
+                { Temperature = 0.2, MaxTokens = 512 },
+            ActionPlan, functionDefinitions.ToArray(), cancellationToken: cancellationToken).ConfigureAwait(false);
+
+        if (response is null)
+        {
+            throw new SKException("The planner failed to generate a response");
+        }
+
+        if (!_context.Skills.TryGetFunction(response, out var function))
+        {
+            throw new SKException($"The function {response.Function} is not available");
+        }
+
+        Plan plan = new(goal, function!);
+        plan.Steps[0].Parameters = response.FunctionParameters();
+
+        return plan;
+    }
+
+
+    private const string PromptTemplate = "Decide the best action to take to achieve the user's goal." +
+                                          "\nGoal: {{ $input }}";
+
+    private static FunctionDefinition ActionPlan => new()
+    {
+        Name = "takeAction",
+        Description = "decide the best action to take to achieve the user's goal",
+        Parameters = BinaryData.FromObjectAsJson(
+            new
+            {
+                Type = "object",
+                Properties = new
+                {
+                    Action = new
+                    {
+                        Type = "object",
+                        Description = "Action data structure",
+                        Properties = new
+                        {
+                            Rationale = new
+                            {
+                                Type = "string",
+                                Description = "the rationale for the action"
+                            },
+                            Function = new
+                            {
+                                Type = "string",
+                                Description = "Name of the function chosen"
+                            },
+                            Parameters = new
+                            {
+                                Type = "array",
+                                Description = "Parameter values",
+                                Items = new
+                                {
+                                    Type = "object",
+                                    Description = "Parameter value",
+                                    Properties = new
+                                    {
+                                        Name = new
+                                        {
+                                            Type = "string",
+                                            Description = "Parameter name"
+                                        },
+                                        Value = new
+                                        {
+                                            Type = "string",
+                                            Description = "Parameter value"
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase })
+    };
+}
