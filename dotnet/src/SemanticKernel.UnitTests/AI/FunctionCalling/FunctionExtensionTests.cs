296f5c0ca1e88398db7d5a060ff74af943ebf923
kevdome3000 <tech@chancetheapp.com>
2023-08-30 21:21:29 -0700

    Add FunctionExtensionTests for FunctionView conversion
    Tests for conversion of FunctionView to FunctionDefinition in SemanticKernel solution have been added. This test ensures the correct transformation and validation of FunctionView fields to FunctionDefinition. It includes validation for the presence of the parameters, their types, and their default values. This is necessary for maintaining consistency and accuracy when referencing functions across different parts of the project.

---
+++ dotnet/src/SemanticKernel.UnitTests/AI/FunctionCalling/FunctionExtensionTests.cs
@@ -0,0 +1,59 @@
+// Copyright (c) Microsoft. All rights reserved.
+namespace SemanticKernel.UnitTests.AI.FunctionCalling;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using Microsoft.SemanticKernel.Connectors.AI.OpenAI.AzureSdk.FunctionCalling;
+using Microsoft.SemanticKernel.SkillDefinition;
+using Xunit;
+
+
+public class FunctionExtensionTests
+{
+    [Fact]
+    public void ToFunctionDefinition_ShouldConvert_FunctionViewToFunctionDefinition()
+    {
+        // Arrange
+        var functionView = new FunctionView(
+            "testFunction",
+            "testSkill",
+            "testDescription",
+            new List<ParameterView>
+            {
+                new("param1", "description1", "default1", ParameterViewType.String),
+                new("param2", "description2", "default2", ParameterViewType.Number)
+            },
+            true,
+            true);
+
+        // Act
+        var result = functionView.ToFunctionDefinition();
+
+        // Assert
+        Assert.Equal($"{functionView.SkillName}.{functionView.Name}", result.Name);
+        Assert.Equal(functionView.Description, result.Description);
+
+        var parametersJson = result.Parameters.ToString();
+        using var parametersDoc = JsonDocument.Parse(parametersJson);
+        Dictionary<string, JsonElement> propertiesDict = parametersDoc.RootElement.GetProperty("properties").EnumerateObject().ToDictionary(item => item.Name, item => item.Value);
+
+        Assert.True(propertiesDict.ContainsKey(functionView.Parameters[0].Name));
+
+        if (propertiesDict.ContainsKey(functionView.Parameters[0].Name))
+        {
+            Assert.Equal(functionView.Parameters[0].Description, propertiesDict[functionView.Parameters[0].Name].GetProperty("description").GetString());
+            Assert.Equal(functionView.Parameters[0].DefaultValue, propertiesDict[functionView.Parameters[0].Name].GetProperty("defaultValue").GetString());
+            Assert.Equal(functionView.Parameters[0].Type?.Name, propertiesDict[functionView.Parameters[0].Name].GetProperty("type").GetString());
+        }
+
+        Assert.True(propertiesDict.ContainsKey(functionView.Parameters[1].Name));
+
+        if (propertiesDict.ContainsKey(functionView.Parameters[1].Name))
+        {
+            Assert.Equal(functionView.Parameters[1].Description, propertiesDict[functionView.Parameters[1].Name].GetProperty("description").GetString());
+            Assert.Equal(functionView.Parameters[1].DefaultValue, propertiesDict[functionView.Parameters[1].Name].GetProperty("defaultValue").GetString());
+            Assert.Equal(functionView.Parameters[1].Type?.Name, propertiesDict[functionView.Parameters[1].Name].GetProperty("type").GetString());
+        }
+    }
+}
