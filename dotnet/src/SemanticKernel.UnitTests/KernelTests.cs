5a80cb53be54e1a70190adc9448f05aa0bd335b6
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2023-09-01 20:28:25 +0100

    Fix warning

--- dotnet/src/SemanticKernel.UnitTests/KernelTests.cs
+++ dotnet/src/SemanticKernel.UnitTests/KernelTests.cs
@@ -3,7 +3,6 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
