b6fcc96b6089467e447126b127d8fa63cd3c8cb8
markwallace-microsoft <mark.e.wallace@outlook.com>
2023-09-10 18:52:47 +0100

    Add hello AI sample

---
+++ dotnet/samples/HelloOpenAIConsoleApp/Program.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using RepoUtils;
+
+Console.WriteLine("Hello AI, what can you do for me?");
+
+IConfigurationRoot configRoot = new ConfigurationBuilder()
+            .AddEnvironmentVariables()
+            .AddUserSecrets<Env>()
+            .Build();
+
+var section = configRoot.GetSection("AzureOpenAI");
+var deploymentName = section.GetValue<string>("DeploymentName");
+var endpoint = section.GetValue<string>("Endpoint");
+var apiKey = section.GetValue<string>("ApiKey");
+
+IKernel kernel = new KernelBuilder()
+            .WithAzureTextCompletionService(deploymentName, endpoint, apiKey)
+            .Build();
+
+var result = await kernel.InvokeSemanticFunctionAsync("Hello AI, what can you do for me?");
+
+if (result.LastException is not null)
+{
+    Console.WriteLine(result.LastException.Message);
+}
+
+Console.WriteLine(result.Result);
