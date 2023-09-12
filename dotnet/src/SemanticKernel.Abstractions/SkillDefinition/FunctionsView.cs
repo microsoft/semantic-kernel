30495838e0644b97095ef77f920f8e04d897e506
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2023-09-12 13:07:14 +0100

    Fix warnings

--- dotnet/src/SemanticKernel.Abstractions/SkillDefinition/FunctionsView.cs
+++ dotnet/src/SemanticKernel.Abstractions/SkillDefinition/FunctionsView.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
