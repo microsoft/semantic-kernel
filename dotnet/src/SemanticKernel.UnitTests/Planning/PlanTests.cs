1754bcc019729e1fd6c5615ffc8e3eccc02fb119
markwallace-microsoft <mark.e.wallace@outlook.com>
2023-09-13 16:09:38 +0100

    Fix unused usings

--- dotnet/src/SemanticKernel.UnitTests/Planning/PlanTests.cs
+++ dotnet/src/SemanticKernel.UnitTests/Planning/PlanTests.cs
@@ -6,8 +6,6 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Microsoft.SemanticKernel;
-using Microsoft.SemanticKernel.Diagnostics;
-using Microsoft.SemanticKernel.AI.TextCompletion;
 using Microsoft.SemanticKernel.Orchestration;
 using Microsoft.SemanticKernel.Planning;
 using Microsoft.SemanticKernel.SkillDefinition;
