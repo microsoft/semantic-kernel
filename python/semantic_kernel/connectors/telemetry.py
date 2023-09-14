78d5419df75c7f12f0d0c9263dbc181853150010
Abby Harrison <abby.harrison@microsoft.com>
2023-09-14 16:27:20 -0700

    add variable IS_TELEMETRY_ENABLED

--- python/semantic_kernel/connectors/telemetry.py
+++ python/semantic_kernel/connectors/telemetry.py
@@ -1,5 +1,10 @@
 # Copyright (c) Microsoft. All rights reserved.
 
+import os
+
+
 TELEMETRY_DISABLED_ENV_VAR = "AZURE_TELEMETRY_DISABLED"
 
 HTTP_USER_AGENT = "Semantic-Kernel"
+
+IS_TELEMETRY_ENABLED = os.environ.get(TELEMETRY_DISABLED_ENV_VAR, "false").lower() != "true"
