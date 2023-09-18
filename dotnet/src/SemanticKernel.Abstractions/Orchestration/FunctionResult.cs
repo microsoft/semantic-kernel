6b2240fdfb118b34fbd1e9fa956fe9b315d8db11
Dmytro Struk <13853051+dmytrostruk@users.noreply.github.com>
2023-09-18 21:26:39 +0100

    Added XML documentation

--- dotnet/src/SemanticKernel.Abstractions/Orchestration/FunctionResult.cs
+++ dotnet/src/SemanticKernel.Abstractions/Orchestration/FunctionResult.cs
@@ -5,25 +5,51 @@ using System.Collections.Generic;
 
 namespace Microsoft.SemanticKernel.Orchestration;
 
+/// <summary>
+/// Function result after execution.
+/// </summary>
 public sealed class FunctionResult
 {
+    /// <summary>
+    /// Additional data from model.
+    /// </summary>
     public IReadOnlyCollection<ModelResult> ModelResults { get; internal set; } = Array.Empty<ModelResult>();
 
+    /// <summary>
+    /// Function result object.
+    /// </summary>
     internal object? Value { get; private set; } = null;
 
+    /// <summary>
+    /// Instance of <see cref="SKContext"/> to pass in function pipeline.
+    /// </summary>
     internal SKContext Context { get; private set; }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FunctionResult"/> class.
+    /// </summary>
+    /// <param name="context">Instance of <see cref="SKContext"/> to pass in function pipeline.</param>
     public FunctionResult(SKContext context)
     {
         this.Context = context;
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="FunctionResult"/> class.
+    /// </summary>
+    /// <param name="context">Instance of <see cref="SKContext"/> to pass in function pipeline.</param>
+    /// <param name="value">Function result object.</param>
     public FunctionResult(SKContext context, object? value)
     {
         this.Value = value;
         this.Context = context;
     }
 
+    /// <summary>
+    /// Returns function result value.
+    /// </summary>
+    /// <typeparam name="T">Target type for result value casting.</typeparam>
+    /// <exception cref="InvalidCastException">Thrown when it's not possible to cast result value to <typeparamref name="T"/>.</exception>
     public T? GetValue<T>()
     {
         if (this.Value is null)
--- dotnet/src/SemanticKernel.Abstractions/Orchestration/KernelResult.cs
+++ dotnet/src/SemanticKernel.Abstractions/Orchestration/KernelResult.cs
@@ -5,12 +5,26 @@ using System.Collections.Generic;
 
 namespace Microsoft.SemanticKernel.Orchestration;
 
+/// <summary>
+/// Kernel result after execution.
+/// </summary>
 public sealed class KernelResult
 {
+    /// <summary>
+    /// Additional data from model.
+    /// </summary>
     public IReadOnlyCollection<ModelResult> ModelResults { get; internal set; } = Array.Empty<ModelResult>();
 
+    /// <summary>
+    /// Kernel result object.
+    /// </summary>
     internal object? Value { get; private set; } = null;
 
+    /// <summary>
+    /// Returns function result value.
+    /// </summary>
+    /// <typeparam name="T">Target type for result value casting.</typeparam>
+    /// <exception cref="InvalidCastException">Thrown when it's not possible to cast result value to <typeparamref name="T"/>.</exception>
     public T? GetValue<T>()
     {
         if (this.Value is null)
@@ -26,6 +40,10 @@ public sealed class KernelResult
         throw new InvalidCastException($"Cannot cast {this.Value.GetType()} to {typeof(T)}");
     }
 
+    /// <summary>
+    /// Creates instance of <see cref="KernelResult"/> from instance of <see cref="FunctionResult"/>.
+    /// </summary>
+    /// <param name="functionResult">Instance of <see cref="FunctionResult"/>.</param>
     internal static KernelResult FromFunctionResult(FunctionResult functionResult)
     {
         return new KernelResult
