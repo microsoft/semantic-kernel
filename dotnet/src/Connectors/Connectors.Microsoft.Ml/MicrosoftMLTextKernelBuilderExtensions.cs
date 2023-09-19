72265ff43ca2f981389368f3da1c680e73e888f2
Valeriy Soloviov <vsolovyov@microsoft.com>
2023-09-19 10:54:15 +0300

    fix formatting

--- dotnet/src/Connectors/Connectors.Microsoft.Ml/MicrosoftMLTextKernelBuilderExtensions.cs
+++ dotnet/src/Connectors/Connectors.Microsoft.Ml/MicrosoftMLTextKernelBuilderExtensions.cs
@@ -1,22 +1,28 @@
+using System;
+using System.Net.Http;
 using Microsoft.SemanticKernel.AI.Embeddings;
-using Microsoft.SemanticKernel;
-
 
 #pragma warning disable IDE0130
 // ReSharper disable once CheckNamespace - Using NS of KernelConfig
 namespace Microsoft.SemanticKernel;
 #pragma warning restore IDE0130
 
+/// <summary>
+/// Extensions for configuring MsMLTextKernelBuilder.
+/// </summary>
 public static class MsMLTextKernelBuilderExtensions
 {
+    /// <summary>
+    /// Configures the MsMLTextTextEmbeddingGenerationService for text embedding generation.
+    /// </summary>
+    /// <param name="serviceId">Optional service ID.</param>
     /// <param name="vocabFile">The JSON file path containing the dictionary of string keys and their ids.</param>
     /// <param name="mergesFile">The file path containing the tokens's pairs list.</param>
     public static KernelBuilder WithMsMLTextTextEmbeddingGenerationService(this KernelBuilder builder,
         string mergeFilePath = @"merges.txt",
         string vocabFilePath = @"vocab.json",
         string? serviceId = null,
-        bool setAsDefault = false,
-        HttpClient? httpClient = null)
+        bool setAsDefault = false)
     {
         builder.WithAIService<ITextEmbeddingGeneration>(serviceId, (loggerFactory) =>
             new MsTextEmbeddingGeneration(
@@ -29,5 +35,3 @@ public static class MsMLTextKernelBuilderExtensions
         return builder;
     }
 }
-
-
--- dotnet/src/Connectors/Connectors.Microsoft.Ml/TextEmbedding/MicrosoftMLTextEmbeddingGeneration.cs
+++ dotnet/src/Connectors/Connectors.Microsoft.Ml/TextEmbedding/MicrosoftMLTextEmbeddingGeneration.cs
@@ -1,32 +1,41 @@
-using Microsoft.SemanticKernel.AI.Embeddings;
-using Microsoft.ML.Tokenizers;
+using System.Collections.Generic;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
+using System.Threading.Tasks;
+using System;
+using System.Threading;
+using Microsoft.SemanticKernel.AI.Embeddings;
+using Microsoft.ML.Tokenizers;
+
 
 #pragma warning disable IDE0130
 // ReSharper disable once CheckNamespace - Using NS of KernelConfig
 namespace Microsoft.SemanticKernel;
 #pragma warning restore IDE0130
+/// <summary>
+/// Provides text embedding generation using a BPE tokenizer.
+/// </summary>
 public sealed class MsTextEmbeddingGeneration : ITextEmbeddingGeneration
 {
     private string vocabFilePath;
     private string mergeFilePath;
     private Tokenizer tokenizer;
     /// <summary>
-    /// Logger instance
+    /// Gets the logger instance.
     /// </summary>
     private ILogger Logger { get; set; }
 
     /// <summary>
-    /// Construct a new Bpe model object to use for sentence tokenization and tokenizer training.
+    /// Initializes a new instance of the MsTextEmbeddingGeneration class.
     /// </summary>
     /// <param name="vocabFile">The JSON file path containing the dictionary of string keys and their ids.</param>
     /// <param name="mergesFile">The file path containing the tokens's pairs list.</param>
+    /// <param name="loggerFactory">Optional logger factory for logging.</param>
     public MsTextEmbeddingGeneration(
         string mergeFilePath = @"merges.txt",
         string vocabFilePath = @"vocab.json",
         ILoggerFactory? loggerFactory = null
-    ) : base()
+    )
     {
         this.vocabFilePath = vocabFilePath;
         this.mergeFilePath = mergeFilePath;
@@ -34,22 +43,33 @@ public sealed class MsTextEmbeddingGeneration : ITextEmbeddingGeneration
         this.Logger = loggerFactory is not null ? loggerFactory.CreateLogger(this.GetType()) : NullLogger.Instance;
     }
 
+    /// <summary>
+    /// Generates embeddings asynchronously for a list of input texts.
+    /// </summary>
+    /// <param name="data">The list of input texts to generate embeddings for.</param>
+    /// <param name="cancellationToken">Optional cancellation token.</param>
+    /// <returns>A task that represents the asynchronous operation and returns a list of embeddings.</returns>
     public Task<IList<ReadOnlyMemory<float>>> GenerateEmbeddingsAsync(IList<string> data, CancellationToken cancellationToken = default)
     {
         var result = new List<ReadOnlyMemory<float>>(data.Count);
         foreach (string text in data)
         {
-
             var tokenizerEncodedResult = tokenizer.Encode(text);
             var embeddings = GenerateEmbeddingsFromTokens(tokenizerEncodedResult, cancellationToken);
 
             // Add the embeddings to the result list
             result.Add(embeddings);
-
         }
 
         return Task.FromResult<IList<ReadOnlyMemory<float>>>(result);
     }
+
+    /// <summary>
+    /// Generates embeddings from tokenizer tokens.
+    /// </summary>
+    /// <param name="token">The tokenizer result.</param>
+    /// <param name="cancellationToken">Optional cancellation token.</param>
+    /// <returns>A ReadOnlyMemory of float representing the embeddings.</returns>
     private static ReadOnlyMemory<float> GenerateEmbeddingsFromTokens(TokenizerResult token, CancellationToken cancellationToken)
     {
         // Convert token.Ids (int) to float
@@ -59,8 +79,6 @@ public sealed class MsTextEmbeddingGeneration : ITextEmbeddingGeneration
         var embeddings = new ReadOnlyMemory<float>(floatIds);
 
         return embeddings;
-
     }
-
 
 }
