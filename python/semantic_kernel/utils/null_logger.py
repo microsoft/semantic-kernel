144ae240d4e536eca4d866a55ce65983aa3e8f8a
Jib Adegunloye <jib.adegunloye@mongodb.com>
2023-09-19 12:20:49 -0400

    Create nuller metaclass abstraction for Logger class

--- python/semantic_kernel/utils/null_logger.py
+++ python/semantic_kernel/utils/null_logger.py
@@ -1,6 +1,27 @@
 # Copyright (c) Microsoft. All rights reserved.
 
+from functools import wraps
 from logging import Logger
+
+
+def _nullify(fn):
+    @wraps(fn)
+    def _inner_nullify(*args, **kwargs):
+        pass
+
+    return _inner_nullify
+
+
+class _NullerMeta(type):
+    def __new__(cls, classname, base_classes, class_dict):
+        nullified_dict = {
+            attr_name: _nullify(attr)
+            for attr_name, attr in class_dict.items()
+            if callable(attr)
+        }
+        return type.__new__(
+            cls, classname, base_classes, {**class_dict, **nullified_dict}
+        )
 
 
 class NullLogger(Logger):
@@ -8,17 +29,4 @@ class NullLogger(Logger):
     A logger that does nothing.
     """
 
-    def __init__(self) -> None:
-        pass
-
-    def debug(self, _: str) -> None:
-        pass
-
-    def info(self, _: str) -> None:
-        pass
-
-    def warning(self, _: str) -> None:
-        pass
-
-    def error(self, _: str) -> None:
-        pass
+    __metaclass__ = _NullerMeta
