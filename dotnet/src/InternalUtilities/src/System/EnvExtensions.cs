a6ced30e05bb889a6d73f24b52034efd62caf1bc
Lisa Harrylock <lisaharrylock@gmail.com>
2023-09-19 13:21:39 -0700

    fix suppression

--- dotnet/src/InternalUtilities/src/System/EnvExtensions.cs
+++ dotnet/src/InternalUtilities/src/System/EnvExtensions.cs
@@ -3,8 +3,7 @@
 #pragma warning disable IDE0130 // Namespace does not match folder structure
 // ReSharper disable once CheckNamespace
 namespace System;
-#pragma warning disable IDE0130 // Namespace does not match folder structure
-// ReSharper disable once CheckNamespace
+#pragma warning restore IDE0130
 
 internal static class EnvExtensions
 {
