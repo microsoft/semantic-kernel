36d659c99b579b5f1385188c2e8a2a8f316109a1
Lisa Harrylock <lisaharrylock@gmail.com>
2023-09-20 13:53:03 -0700

    remove unused import

--- dotnet/src/Extensions/Planning.ActionPlanner/ActionPlanner.cs
+++ dotnet/src/Extensions/Planning.ActionPlanner/ActionPlanner.cs
@@ -11,7 +11,6 @@ using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.SemanticKernel.AI;
 using Microsoft.SemanticKernel.Diagnostics;
 using Microsoft.SemanticKernel.Orchestration;
