f12997b7a1b5277006f1ee30e6d170fe97cceced
kshitijcode <ikshitijsharma@gmail.com>
2023-09-22 06:02:06 +0000

    fixed ruff

--- python/tests/integration/connectors/memory/test_azure_cosmosdb_mongodb.py
+++ python/tests/integration/connectors/memory/test_azure_cosmosdb_mongodb.py
@@ -3,11 +3,11 @@ import os
 
 import numpy as np
 import pytest
-
-import semantic_kernel as sk
 from python.semantic_kernel.connectors.memory.azurecosmosmongodb.azurecosmosmongodb_memory_store import (
     AzureCosmosMongoDBMemoryStore,
 )
+
+import semantic_kernel as sk
 from semantic_kernel.memory.memory_record import MemoryRecord
 
 try:
