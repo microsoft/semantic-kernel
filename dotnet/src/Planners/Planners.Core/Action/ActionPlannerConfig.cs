4ff701070603d5bb6f0a486118b7d30b7c7ae33c
Lisa Harrylock <lisaharrylock@gmail.com>
2023-09-25 10:41:50 -0700

    .Net: Merge planner packages2 (#2931)
    ### Motivation and Context
    
    Combining the planner files into one package makes it easier to provide
    a consistent interface for planners.
    Fixes #2856
    
    This introduces breaking changes since the namespaces for the planners
    changed.
    
    ### Description
    * Created Planners.Core and Planners.Core.UnitTests projects under new
    folder Planners
    * Created a folder for each type of planner within the Planners project
    * Moved planner files to their new project and folder.
    * Removed old projects
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [x] The code builds clean without any errors or warnings
    - [x] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [x] All unit tests pass, and I have added new tests where possible
    - [ ] I didn't break anyone :smile: - Namespaces for planner files
    changed
    
    ---------
    
    Co-authored-by: Mark Wallace <127216156+markwallace-microsoft@users.noreply.github.com>

---
+++ dotnet/src/Planners/Planners.Core/Action/ActionPlannerConfig.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.SemanticKernel.Planning;
+
+#pragma warning disable IDE0130
+// ReSharper disable once CheckNamespace - Using NS of Plan
+namespace Microsoft.SemanticKernel.Planners;
+#pragma warning restore IDE0130
+
+/// <summary>
+/// Configuration for Action planner instances.
+/// </summary>
+public sealed class ActionPlannerConfig : PlannerConfigBase
+{
+}
