fbd0afd6870315b50030eae62682636dc1d0bd6f
Lisa Harrylock <lisaharrylock@gmail.com>
2023-09-25 10:41:50 -0700

    .Net: Merge planner packages2 (#2931)
    ### Motivation and Context
    
    Combining the planner files into one package makes it easier to provide
    a consistent interface for planners.
    Fixes #2856
    
    This introduces breaking changes since the namespaces for the planners
    changed.
    
    ### Description
    * Created Planners.Core and Planners.Core.UnitTests projects under new
    folder Planners
    * Created a folder for each type of planner within the Planners project
    * Moved planner files to their new project and folder.
    * Removed old projects
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [x] The code builds clean without any errors or warnings
    - [x] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [x] All unit tests pass, and I have added new tests where possible
    - [ ] I didn't break anyone :smile: - Namespaces for planner files
    changed
    
    ---------
    
    Co-authored-by: Mark Wallace <127216156+markwallace-microsoft@users.noreply.github.com>

---
+++ dotnet/src/Planners/Planners.Core/Action/ActionPlannerExtensions.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.Extensions.Logging;
+
+#pragma warning disable IDE0130
+// ReSharper disable once CheckNamespace - Using NS of Plan
+namespace Microsoft.SemanticKernel.Planners;
+#pragma warning restore IDE0130
+
+/// <summary>
+/// Extension methods for <see cref="ActionPlanner"/> class.
+/// </summary>
+public static class ActionPlannerExtensions
+{
+    /// <summary>
+    /// Returns decorated instance of <see cref="IActionPlanner"/> with enabled instrumentation.
+    /// </summary>
+    /// <param name="planner">Instance of <see cref="IActionPlanner"/> to decorate.</param>
+    /// <param name="loggerFactory">The <see cref="ILoggerFactory"/> to use for logging. If null, no logging will be performed.</param>
+    public static IActionPlanner WithInstrumentation(this IActionPlanner planner, ILoggerFactory? loggerFactory = null)
+    {
+        return new InstrumentedActionPlanner(planner, loggerFactory);
+    }
+}
