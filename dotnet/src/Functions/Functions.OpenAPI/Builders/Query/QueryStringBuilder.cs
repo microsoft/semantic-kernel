8213645375686585492cf93b7e269422c8e4a923
Weihan Li <weihanli@outlook.com>
2023-09-26 22:12:39 +0800

    style: apply dotnet-format changes

--- dotnet/src/Functions/Functions.OpenAPI/Builders/Query/QueryStringBuilder.cs
+++ dotnet/src/Functions/Functions.OpenAPI/Builders/Query/QueryStringBuilder.cs
@@ -17,7 +17,7 @@ internal sealed class QueryStringBuilder : IQueryStringBuilder
     /// <summary>
     /// Query string parameter serializers.
     /// </summary>
-    private static Dictionary<RestApiOperationParameterStyle, Func<RestApiOperationParameter, string, string>> s_queryStringParameterSerializers = new()
+    private static readonly Dictionary<RestApiOperationParameterStyle, Func<RestApiOperationParameter, string, string>> s_queryStringParameterSerializers = new()
     {
         { RestApiOperationParameterStyle.Form, FormStyleParameterSerializer.Serialize },
         { RestApiOperationParameterStyle.SpaceDelimited, SpaceDelimitedStyleParameterSerializer.Serialize },
