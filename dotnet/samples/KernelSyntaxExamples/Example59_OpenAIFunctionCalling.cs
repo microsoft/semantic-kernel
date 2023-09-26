60517c46890b2ca3cc8c3c0d1da80f2654b2d344
Gina Triolo <51341242+gitri-ms@users.noreply.github.com>
2023-09-26 08:51:30 -0700

    Formatting and fix filename clash

--- dotnet/samples/KernelSyntaxExamples/Example59_OpenAIFunctionCalling.cs
+++ dotnet/samples/KernelSyntaxExamples/Example59_OpenAIFunctionCalling.cs
@@ -3,7 +3,6 @@
 using System;
 using System.Linq;
 using System.Threading.Tasks;
-using Azure;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.AI.ChatCompletion;
 using Microsoft.SemanticKernel.Connectors.AI.OpenAI;
