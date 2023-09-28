25e65ad39be7950ceb9ef61aaff4df03923b200a
feiyun0112 <feiyun0112@gmail.com>
2023-09-28 20:59:06 +0800

    .Net: remove local variable not used (#3009)
    ````
    **SKFunctionAttribute? functionAttribute = method.GetCustomAttribute<SKFunctionAttribute>(inherit: true);**
    
    string? description = method.GetCustomAttribute<DescriptionAttribute>(inherit: true)?.Description;
    
    var result = new MethodDetails
    {
        Name = functionName!,
        Description = description ?? string.Empty,
    };
    
    (result.Function, result.Parameters) = GetDelegateInfo(functionName!, pluginName, target, method);
    
    logger?.LogTrace("Method '{0}' found", result.Name);
    
    return result;
    ````

--- dotnet/src/SemanticKernel.Core/Functions/NativeFunction.cs
+++ dotnet/src/SemanticKernel.Core/Functions/NativeFunction.cs
@@ -274,8 +274,6 @@ internal sealed class NativeFunction : ISKFunction, IDisposable
             }
         }
 
-        SKFunctionAttribute? functionAttribute = method.GetCustomAttribute<SKFunctionAttribute>(inherit: true);
-
         string? description = method.GetCustomAttribute<DescriptionAttribute>(inherit: true)?.Description;
 
         var result = new MethodDetails
