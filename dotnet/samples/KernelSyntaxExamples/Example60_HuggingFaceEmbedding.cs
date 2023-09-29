884512356b32d79188803dad30460eec52ad38ad
ashishwebt <120636885+ashishwebt@users.noreply.github.com>
2023-09-29 22:12:15 +0530

    Added example for hugging face

---
+++ dotnet/samples/KernelSyntaxExamples/Example60_HuggingFaceEmbedding.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.SemanticKernel.AI.Embeddings;
+using Microsoft.SemanticKernel;
+
+public static class Example60_HuggingFaceEmbedding
+{
+    public static async Task RunAsync()
+    {
+        Console.WriteLine("======== Example59_HuggingFaceEmbedding ========");
+
+
+        IKernel kernel = Kernel.Builder
+                .WithHuggingFaceTextEmbeddingGenerationService("sentence-transformers/all-MiniLM-L6-v2",
+                "https://api-inference.huggingface.co/pipeline/feature-extraction",
+                serviceId: "all-MiniLM-L6-v2")
+                 .WithHuggingFaceTextEmbeddingGenerationService("flax-sentence-embeddings/st-codesearch-distilroberta-base",
+                "https://api-inference.huggingface.co/pipeline/feature-extraction",
+                serviceId: "distilroberta")
+      .Build();
+
+
+        var embedinGgeneratorMiniLM = kernel.GetService<ITextEmbeddingGeneration>("all-MiniLM-L6-v2");
+        var embedingMiniLM = await embedinGgeneratorMiniLM.GenerateEmbeddingAsync("british short hair");
+
+        var embedinGeneratorDistilRoberta = kernel.GetService<ITextEmbeddingGeneration>("distilroberta");
+        var embedinDistilRoberta = await embedinGeneratorDistilRoberta.GenerateEmbeddingAsync("british short hair");
+
+        Console.WriteLine("======== Embedding all-MiniLM-L6-v2 ========");
+        Console.Write("[");
+        foreach (var item in embedingMiniLM.ToArray())
+        {
+            Console.Write(item);
+        }
+        Console.Write("]");
+
+        Console.WriteLine(" ");
+        Console.WriteLine(" ");
+
+
+        Console.WriteLine("======== Embedding google_speech_command_xvector ========");
+        Console.Write("[");
+        foreach (var item in embedinDistilRoberta.ToArray())
+        {
+            Console.Write(item);
+        }
+        Console.Write("]");
+
+    }
+}
