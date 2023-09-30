a886995dadcd488af9c309ecaa7cb1592023af28
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2023-09-30 10:55:07 +0100

    Bug + Regression Fix + Added UTs

--- dotnet/src/SemanticKernel.UnitTests/KernelTests.cs
+++ dotnet/src/SemanticKernel.UnitTests/KernelTests.cs
@@ -345,16 +345,275 @@ public class KernelTests
 
         // Act
         var kernelResult = await kernel.RunAsync(function1, function2);
+        var functionResult1 = kernelResult.FunctionResults.First(l => l.FunctionName == "Function1" && l.PluginName == PluginName);
+        var functionResult2 = kernelResult.FunctionResults.First(l => l.FunctionName == "Function2" && l.PluginName == PluginName);
 
         // Assert
         Assert.NotNull(kernelResult);
         Assert.Equal("Result2", kernelResult.GetValue<string>());
-
-        var functionResult1 = kernelResult.FunctionResults.First(l => l.FunctionName == "Function1" && l.PluginName == PluginName);
-        var functionResult2 = kernelResult.FunctionResults.First(l => l.FunctionName == "Function2" && l.PluginName == PluginName);
-
         Assert.Equal("Result1", functionResult1.GetValue<string>());
         Assert.Equal("Result2", functionResult2.GetValue<string>());
+    }
+
+    [Fact]
+    public async Task ItReturnsChangedResultsFromFunctionInvokedEventsAsync()
+    {
+        var kernel = Kernel.Builder.Build();
+
+        // Arrange
+        [SKName("Function1")]
+        static string Function1() => "Result1";
+        var function1 = SKFunction.FromNativeMethod(Method(Function1), pluginName: "MyPlugin");
+        const string ExpectedValue = "new result";
+
+        kernel.FunctionInvoked += (object? sender, FunctionInvokedEventArgs args) =>
+        {
+            args.SKContext.Variables.Update(ExpectedValue);
+        };
+
+        // Act
+        var kernelResult = await kernel.RunAsync(function1);
+
+        // Assert
+        Assert.NotNull(kernelResult);
+        Assert.Equal(ExpectedValue, kernelResult.GetValue<string>());
+        Assert.Equal(ExpectedValue, kernelResult.FunctionResults.Single().GetValue<string>());
+        Assert.Equal(ExpectedValue, kernelResult.FunctionResults.Single().Context.Result);
+    }
+
+    [Fact]
+    public async Task ItReturnsChangedResultsFromFunctionInvokingEventsAsync()
+    {
+        // Arrange
+        var kernel = Kernel.Builder.Build();
+
+        [SKName("Function1")]
+        static string Function1(SKContext context) => context.Variables["injected variable"];
+        var function1 = SKFunction.FromNativeMethod(Method(Function1), pluginName: "MyPlugin");
+        const string ExpectedValue = "injected value";
+
+        kernel.FunctionInvoking += (object? sender, FunctionInvokingEventArgs args) =>
+        {
+            args.SKContext.Variables["injected variable"] = ExpectedValue;
+        };
+
+        // Act
+        var kernelResult = await kernel.RunAsync(function1);
+
+        // Assert
+        Assert.NotNull(kernelResult);
+        Assert.Equal(ExpectedValue, kernelResult.GetValue<string>());
+        Assert.Equal(ExpectedValue, kernelResult.FunctionResults.Single().GetValue<string>());
+        Assert.Equal(ExpectedValue, kernelResult.FunctionResults.Single().Context.Result);
+    }
+
+    [Theory]
+    [InlineData("Function1", 5)]
+    [InlineData("Function2", 1)]
+    public async Task ItRepeatsFunctionInvokedEventsAsync(string retryFunction, int numberOfRepeats)
+    {
+        // Arrange
+        var kernel = Kernel.Builder.Build();
+
+        [SKName("Function1")]
+        static string Function1(SKContext context) => "Result1";
+        var function1 = SKFunction.FromNativeMethod(Method(Function1), pluginName: "MyPlugin");
+
+        [SKName("Function2")]
+        static string Function2(SKContext context) => "Result2";
+        var function2 = SKFunction.FromNativeMethod(Method(Function2), pluginName: "MyPlugin");
+
+        int numberOfInvocations = 0;
+        int repeatCount = 0;
+
+        kernel.FunctionInvoked += (object? sender, FunctionInvokedEventArgs args) =>
+        {
+            if (args.FunctionView.Name == retryFunction && repeatCount < numberOfRepeats)
+            {
+                args.Repeat();
+                repeatCount++;
+            }
+
+            numberOfInvocations++;
+        };
+
+        // Act
+        var kernelResult = await kernel.RunAsync(function1, function2);
+
+        // Assert
+        Assert.NotNull(kernelResult);
+        Assert.Equal(2 + numberOfRepeats, kernelResult.FunctionResults.Count);
+        Assert.Equal(2 + numberOfRepeats, numberOfInvocations);
+    }
+
+    [Theory]
+    [InlineData("Function1", "Result2 Result3")]
+    [InlineData("Function2", "Result1 Result3")]
+    [InlineData("Function3", "Result1 Result2")]
+    public async Task ItSkipsFunctionsFromFunctionInvokingEventsAsync(string skipFunction, string expectedResult)
+    {
+        // Arrange
+        [SKName("Function1")]
+        static string Function1(string input) => input + " Result1";
+
+        [SKName("Function2")]
+        static string Function2(string input) => input + " Result2";
+
+        [SKName("Function3")]
+        static string Function3(string input) => input + " Result3";
+
+        const string PluginName = "MyPlugin";
+
+        var kernel = Kernel.Builder.Build();
+
+        var function1 = SKFunction.FromNativeMethod(Method(Function1), pluginName: PluginName);
+        var function2 = SKFunction.FromNativeMethod(Method(Function2), pluginName: PluginName);
+        var function3 = SKFunction.FromNativeMethod(Method(Function3), pluginName: PluginName);
+
+        const int ExpectedInvocations = 2;
+
+        int numberOfInvocations = 0;
+
+        kernel.FunctionInvoking += (object? sender, FunctionInvokingEventArgs args) =>
+        {
+            if (args.FunctionView.Name == skipFunction)
+            {
+                args.Skip();
+            }
+        };
+
+        kernel.FunctionInvoked += (object? sender, FunctionInvokedEventArgs args) =>
+        {
+            numberOfInvocations++;
+        };
+
+        // Act
+        var kernelResult = await kernel.RunAsync(string.Empty, function1, function2, function3);
+
+        // Assert
+        Assert.NotNull(kernelResult);
+        Assert.Equal(expectedResult, kernelResult.GetValue<string>()!.Trim());
+        Assert.Equal(ExpectedInvocations, numberOfInvocations);
+        Assert.Equal(ExpectedInvocations, kernelResult.FunctionResults.Count);
+    }
+
+    [Theory]
+    [InlineData(1, 0, 0)]
+    [InlineData(2, 0, 0)]
+    [InlineData(2, 1, 1)]
+    [InlineData(5, 2, 2)]
+    public async Task ItCancelsPipelineFromFunctionInvokingEventsAsync(int numberOfFunctions, int functionCancelIndex, int expectedInvocations)
+    {
+        List<ISKFunction> functions = new();
+        const string PluginName = "MyPlugin";
+
+        // Arrange
+        [SKName("Function1")]
+        static string Function1() => "Result1";
+        functions.Add(SKFunction.FromNativeMethod(Method(Function1), pluginName: PluginName));
+
+        [SKName("Function2")]
+        static string Function2() => "Result2";
+        functions.Add(SKFunction.FromNativeMethod(Method(Function2), pluginName: PluginName));
+
+        [SKName("Function3")]
+        static string Function3() => "Result3";
+        functions.Add(SKFunction.FromNativeMethod(Method(Function3), pluginName: PluginName));
+
+        [SKName("Function4")]
+        static string Function4() => "Result4";
+        functions.Add(SKFunction.FromNativeMethod(Method(Function4), pluginName: PluginName));
+
+        [SKName("Function5")]
+        static string Function5() => "Result5";
+        functions.Add(SKFunction.FromNativeMethod(Method(Function5), pluginName: PluginName));
+
+        var kernel = Kernel.Builder.Build();
+
+        int numberOfInvocations = 0;
+
+        kernel.FunctionInvoking += (object? sender, FunctionInvokingEventArgs args) =>
+        {
+            if (args.FunctionView.Name == functions[functionCancelIndex].Name)
+            {
+                args.Cancel();
+            }
+        };
+
+        kernel.FunctionInvoked += (object? sender, FunctionInvokedEventArgs args) =>
+        {
+            numberOfInvocations++;
+        };
+
+        // Act
+        var kernelResult = await kernel.RunAsync(functions.Take(numberOfFunctions).ToArray());
+
+        // Assert
+        Assert.NotNull(kernelResult);
+
+        // Kernel result is the same as the last invoked function
+        Assert.Equal(expectedInvocations, numberOfInvocations);
+        Assert.Equal(expectedInvocations, kernelResult.FunctionResults.Count);
+    }
+
+    [Theory]
+    [InlineData(1, 0, 1)]
+    [InlineData(2, 0, 1)]
+    [InlineData(2, 1, 2)]
+    [InlineData(5, 2, 3)]
+    public async Task ItCancelsPipelineFromFunctionInvokedEventsAsync(int numberOfFunctions, int functionCancelIndex, int expectedInvocations)
+    {
+        List<ISKFunction> functions = new();
+        const string PluginName = "MyPlugin";
+
+        // Arrange
+        [SKName("Function1")]
+        static string Function1() => "Result1";
+        functions.Add(SKFunction.FromNativeMethod(Method(Function1), pluginName: PluginName));
+
+        [SKName("Function2")]
+        static string Function2() => "Result2";
+        functions.Add(SKFunction.FromNativeMethod(Method(Function2), pluginName: PluginName));
+
+        [SKName("Function3")]
+        static string Function3() => "Result3";
+        functions.Add(SKFunction.FromNativeMethod(Method(Function3), pluginName: PluginName));
+
+        [SKName("Function4")]
+        static string Function4() => "Result4";
+        functions.Add(SKFunction.FromNativeMethod(Method(Function4), pluginName: PluginName));
+
+        [SKName("Function5")]
+        static string Function5() => "Result5";
+        functions.Add(SKFunction.FromNativeMethod(Method(Function5), pluginName: PluginName));
+
+        var kernel = Kernel.Builder.Build();
+
+        int numberOfInvocations = 0;
+
+        kernel.FunctionInvoked += (object? sender, FunctionInvokedEventArgs args) =>
+        {
+            numberOfInvocations++;
+            if (args.FunctionView.Name == functions[functionCancelIndex].Name)
+            {
+                args.Cancel();
+            }
+        };
+
+        // Act
+        var kernelResult = await kernel.RunAsync(functions.Take(numberOfFunctions).ToArray());
+
+        // Assert
+        Assert.NotNull(kernelResult);
+
+        // Kernel result is the same as the last invoked function
+        Assert.Equal($"Result{functionCancelIndex + 1}", kernelResult.GetValue<string>());
+        Assert.Equal(expectedInvocations, numberOfInvocations);
+    }
+
+    private void Kernel_FunctionInvoked(object? sender, FunctionInvokedEventArgs e)
+    {
+        throw new NotImplementedException();
     }
 
     public class MyPlugin
