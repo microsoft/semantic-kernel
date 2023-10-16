a42a0a93c7ca5cb64129e59eccb8e21e150a3d19
Bohdan C <cherchyk@gmail.com>
2023-10-16 15:32:38 -0400

    appsettings.json support in KernelSyntaxExamples

--- dotnet/samples/KernelSyntaxExamples/Program.cs
+++ dotnet/samples/KernelSyntaxExamples/Program.cs
@@ -21,7 +21,7 @@ public static class Program
         using CancellationTokenSource cancellationTokenSource = new();
         CancellationToken cancelToken = cancellationTokenSource.ConsoleCancellationToken();
 
-        string? defaultFilter = null; // Modify to filter examples
+        string? defaultFilter = "06"; // Modify to filter examples
 
         // Check if args[0] is provided
         string? filter = args.Length > 0 ? args[0] : defaultFilter;
@@ -77,6 +77,7 @@ public static class Program
         IConfigurationRoot configRoot = new ConfigurationBuilder()
             .AddEnvironmentVariables()
             .AddUserSecrets<Env>()
+            .AddJsonFile("appsettings.json", true, true)
             .Build();
         TestConfiguration.Initialize(configRoot);
     }
