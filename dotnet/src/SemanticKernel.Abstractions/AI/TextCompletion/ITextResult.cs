e1394ff3ea0baa204da23cfdf344dc7b367c96e2
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2023-10-20 09:15:05 +0100

    .Net StreamingResult derivation fix for Text and Chat Results (#3230)
    ### Motivation and Context
    
    It is easy to mistakenly call the non-streaming API of the streaming
    interface when you want streaming.
    This also resolves problem where non streaming behavior/implementation
    is enforced upon streaming objects.
    
    Callers should be driven to the "pit of success" of calling the intended
    API.
    
    Resolves #2730 
    Resolves Partially #3198
    
    ### Description
    
    Removes the inheritance from ITextResult and IChatResult from streaming
    counterparts.
    
    This will avoid Non-streaming objects to be derived as `Streaming
    capable` objects.
    
    ### Contribution Checklist
    
    - [x] The code builds clean without any errors or warnings
    - [x] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [x] All unit tests pass, and I have added new tests where possible
    - [ ] I didn't break anyone :smile:

--- dotnet/src/SemanticKernel.Abstractions/AI/TextCompletion/ITextResult.cs
+++ dotnet/src/SemanticKernel.Abstractions/AI/TextCompletion/ITextResult.cs
@@ -2,19 +2,14 @@
 
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.SemanticKernel.Orchestration;
 
 namespace Microsoft.SemanticKernel.AI.TextCompletion;
+
 /// <summary>
 /// Interface for text completion results.
 /// </summary>
-public interface ITextResult
+public interface ITextResult : IResultBase
 {
-    /// <summary>
-    /// Gets the model result data.
-    /// </summary>
-    ModelResult ModelResult { get; }
-
     /// <summary>
     /// Asynchronously retrieves the text completion result.
     /// </summary>
