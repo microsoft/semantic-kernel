7c3dc26b473f6974036a7070ef704d043829249b
Yan Li <dannis_28@hotmail.com>
2023-11-03 00:33:09 +0800

    .Net: [Feature branch] Address comments for Flow Orchestrator (#3367)
    ### Motivation and Context
    
    <!-- Thank you for your contribution to the semantic-kernel repo!
    Please help reviewers and future users, providing the following
    information:
      1. Why is this change required?
      2. What problem does it solve?
      3. What scenario does it contribute to?
      4. If it fixes an open issue, please link to the issue here.
    -->
    
    ### Description
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [x] The code builds clean without any errors or warnings
    - [x] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [x] All unit tests pass, and I have added new tests where possible
    - [x] I didn't break anyone :smile:

---
+++ dotnet/src/Experimental/Orchestration.Flow/EmbeddedResource.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.IO;
+using System.Reflection;
+using Microsoft.SemanticKernel.Diagnostics;
+
+namespace Microsoft.SemanticKernel.Experimental.Orchestration;
+
+internal static class EmbeddedResource
+{
+    private static readonly string? s_namespace = typeof(EmbeddedResource).Namespace;
+
+    internal static string? Read(string name, bool throwIfNotFound = true)
+    {
+        var assembly = typeof(EmbeddedResource).GetTypeInfo().Assembly;
+        if (assembly is null) { throw new SKException($"[{s_namespace}] {name} assembly not found"); }
+
+        using Stream? resource = assembly.GetManifestResourceStream($"{s_namespace}." + name);
+        if (resource is null)
+        {
+            if (!throwIfNotFound)
+            {
+                return null;
+            }
+
+            throw new SKException($"[{s_namespace}] {name} resource not found");
+        }
+
+        using var reader = new StreamReader(resource);
+        return reader.ReadToEnd();
+    }
+}
