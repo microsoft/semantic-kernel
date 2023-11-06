6a03d19296c85366595e4535a021e646fe900859
markwallace-microsoft <mark.e.wallace@outlook.com>
2023-11-06 15:29:34 +0000

    Initial check-in of the Handlebars support

---
+++ dotnet/src/Extensions/Extensions.UnitTests/TemplateEngine/Handlebars/HandlebarsPromptTemplateTests.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Orchestration;
+using Microsoft.SemanticKernel.Services;
+using Microsoft.SemanticKernel.TemplateEngine;
+using Microsoft.SemanticKernel.TemplateEngine.Handlebars;
+using Moq;
+using SemanticKernel.Extensions.UnitTests.XunitHelpers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SemanticKernel.Extensions.UnitTests.TemplateEngine.Handlebars;
+
+public sealed class HandlebarsPromptTemplateTests
+{
+    private readonly HandlebarsPromptTemplateFactory _factory;
+    private readonly IKernel _kernel;
+    private readonly ContextVariables _variables;
+    private readonly Mock<IReadOnlyFunctionCollection> _functions;
+    private readonly ITestOutputHelper _logger;
+    private readonly Mock<IFunctionRunner> _functionRunner;
+    private readonly Mock<IAIServiceProvider> _serviceProvider;
+    private readonly Mock<IAIServiceSelector> _serviceSelector;
+
+    public HandlebarsPromptTemplateTests(ITestOutputHelper testOutputHelper)
+    {
+        this._logger = testOutputHelper;
+        this._factory = new HandlebarsPromptTemplateFactory(TestConsoleLogger.LoggerFactory);
+        this._kernel = new KernelBuilder().Build();
+        this._variables = new ContextVariables(Guid.NewGuid().ToString("X"));
+
+        this._functions = new Mock<IReadOnlyFunctionCollection>();
+        this._functionRunner = new Mock<IFunctionRunner>();
+        this._serviceProvider = new Mock<IAIServiceProvider>();
+        this._serviceSelector = new Mock<IAIServiceSelector>();
+    }
+
+    [Fact]
+    public async Task ItRendersVariablesAsync()
+    {
+        // Arrange
+        this._variables.Set("bar", "Bar");
+        var template = "Foo {{bar}}";
+        var target = (HandlebarsPromptTemplate)this._factory.Create(template, new PromptTemplateConfig() { TemplateFormat = HandlebarsPromptTemplateFactory.HandlebarsTemplateFormat });
+        var context = this._kernel.CreateNewContext(this._variables);
+
+        // Act
+        var prompt = await target.RenderAsync(context);
+
+        // Assert   
+        Assert.Equal("Foo Bar", prompt);
+    }
+}
---
+++ dotnet/src/Extensions/TemplateEngine.Handlebars/HandlebarsPromptTemplate.cs
@@ -0,0 +1,81 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using HandlebarsDotNet;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.SemanticKernel.Orchestration;
+
+namespace Microsoft.SemanticKernel.TemplateEngine.Handlebars;
+
+internal class HandlebarsPromptTemplate : IPromptTemplate
+{
+    /// <summary>
+    /// Constructor for PromptTemplate.
+    /// </summary>
+    /// <param name="templateString">Prompt template string.</param>
+    /// <param name="promptTemplateConfig">Prompt template configuration</param>
+    /// <param name="loggerFactory">Logger factory</param>
+    public HandlebarsPromptTemplate(string templateString, PromptTemplateConfig promptTemplateConfig, ILoggerFactory? loggerFactory = null)
+    {
+        this._loggerFactory = loggerFactory ?? NullLoggerFactory.Instance;
+        this._logger = this._loggerFactory.CreateLogger(typeof(HandlebarsPromptTemplate));
+        this._templateString = templateString;
+        this._promptTemplateConfig = promptTemplateConfig;
+        this._parameters = new(() => this.InitParameters());
+    }
+
+    /// <inheritdoc/>
+    public IReadOnlyList<ParameterView> Parameters => this._parameters.Value;
+
+    /// <inheritdoc/>
+    public async Task<string> RenderAsync(SKContext executionContext, CancellationToken cancellationToken = default)
+    {
+        var handlebars = HandlebarsDotNet.Handlebars.Create();
+
+        var functionViews = executionContext.Functions.GetFunctionViews();
+
+        foreach (FunctionView functionView in functionViews)
+        {
+            var skfunction = executionContext.Functions.GetFunction(functionView.PluginName, functionView.Name);
+#pragma warning disable VSTHRD101 // Avoid unsupported async delegates
+            handlebars.RegisterHelper($"{functionView.PluginName}_{functionView.Name}", async (writer, hcontext, parameters) =>
+            {
+                var result = await skfunction.InvokeAsync(executionContext).ConfigureAwait(true);
+                writer.WriteSafeString(result.GetValue<string>());
+            });
+#pragma warning restore VSTHRD101 // Avoid unsupported async delegates
+        }
+
+        var template = handlebars.Compile(this._templateString);
+
+        var prompt = template(executionContext.Variables);
+
+        return await Task.FromResult(prompt).ConfigureAwait(true);
+    }
+
+    #region private
+    private readonly ILoggerFactory _loggerFactory;
+    private readonly ILogger _logger;
+    private readonly string _templateString;
+    private readonly PromptTemplateConfig _promptTemplateConfig;
+    private readonly Lazy<IReadOnlyList<ParameterView>> _parameters;
+
+    private List<ParameterView> InitParameters()
+    {
+        // Parameters from prompt template configuration
+        Dictionary<string, ParameterView> result = new(this._promptTemplateConfig.Input.Parameters.Count, StringComparer.OrdinalIgnoreCase);
+        foreach (var p in this._promptTemplateConfig.Input.Parameters)
+        {
+            result[p.Name] = new ParameterView(p.Name, p.Description, p.DefaultValue);
+        }
+
+        return result.Values.ToList();
+    }
+    #endregion
+
+}
