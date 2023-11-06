527b2ab685449322b24a34cba848d26030290e0e
Dmytro Struk <13853051+dmytrostruk@users.noreply.github.com>
2023-11-06 12:27:02 +0000

    Added chat prompt parser

---
+++ dotnet/src/SemanticKernel.Core/AI/ChatCompletion/ChatPromptParser.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Microsoft.SemanticKernel.AI.ChatCompletion;
+
+public static class ChatPromptParser
+{
+    private static readonly Regex s_chatHistoryRegex = new(@"<message role=[""']([^""']+)[""']>\s*([^<]+?)\s*</message>");
+
+    public static bool TryGetChatHistory(string prompt, out ChatHistory? chatHistory)
+    {
+        var matchCollection = s_chatHistoryRegex.Matches(prompt);
+
+        if (matchCollection.Count == 0)
+        {
+            chatHistory = null;
+            return false;
+        }
+
+        chatHistory = new ChatHistory();
+
+        foreach (Match match in matchCollection)
+        {
+            string role = match.Groups[1].Value;
+            string content = match.Groups[2].Value;
+
+            chatHistory.AddMessage(new AuthorRole(role), content);
+        }
+
+        return true;
+    }
+}
---
+++ dotnet/src/SemanticKernel.UnitTests/AI/ChatCompletion/ChatPromptParserTests.cs
@@ -0,0 +1,78 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.SemanticKernel.AI.ChatCompletion;
+using Xunit;
+
+namespace SemanticKernel.UnitTests.AI.ChatCompletion;
+
+public class ChatPromptParserTests
+{
+    [Theory]
+    [InlineData("This is plain prompt", false)]
+    [InlineData("<message>This is not completed chat prompt</message>", false)]
+    [InlineData("<message role=\"system\">This is completed chat prompt</message>", true)]
+    [InlineData("<message role=\"system\">System message</message><message role=\"user\">User message</message>", true)]
+    public void ChatPromptIdentificationWorksCorrectly(string prompt, bool expectedResult)
+    {
+        Assert.Equal(expectedResult, ChatPromptParser.TryGetChatHistory(prompt, out _));
+    }
+
+    [Theory]
+    [InlineData("This is plain prompt", null)]
+    [InlineData("<message>This is not completed chat prompt</message>", null)]
+    public void ItReturnsNullChatHistoryWhenPromptIsNotChatPrompt(string prompt, ChatHistory? expectedChatHistory)
+    {
+        // Act
+        var result = ChatPromptParser.TryGetChatHistory(prompt, out var chatHistory);
+
+        // Assert
+        Assert.False(result);
+        Assert.Equal(expectedChatHistory, chatHistory);
+    }
+
+    [Fact]
+    public void ItReturnsChatHistoryWhenPromptIsChatPrompt()
+    {
+        // Arrange
+        const string Prompt = @"
+<message role=""system"">
+Test with role in double quotes and content in new line.
+</message>
+
+<message role='user'>Test with role in single quotes and content in the same line.</message>
+
+<message role=""assistant"">
+Test with multiline content.
+Second line.
+</message>
+
+<message role='system'>
+    Test line with tab.
+</message>
+";
+
+        var expectedChatHistory = new ChatHistory();
+        expectedChatHistory.AddSystemMessage("Test with role in double quotes and content in new line.");
+        expectedChatHistory.AddUserMessage("Test with role in single quotes and content in the same line.");
+        expectedChatHistory.AddAssistantMessage("Test with multiline content.\nSecond line.");
+        expectedChatHistory.AddSystemMessage("Test line with tab.");
+
+        // Act
+        var result = ChatPromptParser.TryGetChatHistory(Prompt, out var actualChatHistory);
+
+        // Assert
+        Assert.True(result);
+        Assert.NotNull(actualChatHistory);
+
+        for (var i = 0; i < expectedChatHistory.Count; i++)
+        {
+            this.AssertChatMessage(expectedChatHistory[i], actualChatHistory[i]);
+        }
+    }
+
+    private void AssertChatMessage(ChatMessageBase expectedMessage, ChatMessageBase actualMessage)
+    {
+        Assert.Equal(expectedMessage.Role, actualMessage.Role);
+        Assert.Equal(expectedMessage.Content, actualMessage.Content);
+    }
+}
