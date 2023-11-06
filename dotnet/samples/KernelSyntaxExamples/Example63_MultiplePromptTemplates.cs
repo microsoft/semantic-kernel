c8a95bcc959e38ec9544ea3ff73fa7f5ef73b077
markwallace-microsoft <mark.e.wallace@outlook.com>
2023-11-06 18:49:45 +0000

    Start working on a sample

---
+++ dotnet/samples/KernelSyntaxExamples/Example63_MultiplePromptTemplates.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KernelSyntaxExamples;
+internal class Example63_MultiplePromptTemplates
+{
+}
