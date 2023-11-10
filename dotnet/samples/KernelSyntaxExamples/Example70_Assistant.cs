d6ea7ba80ecb5696330e5c0ad8d11d81c59fcdbc
Chris Rickman <crickman@microsoft.com>
2023-11-09 17:03:28 -0800

    Test stubs

---
+++ dotnet/samples/KernelSyntaxExamples/Example70_Assistant.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Threading.Tasks;
+
+// ReSharper disable once InconsistentNaming
+/// <summary>
+/// $$$
+/// </summary>
+public static class Example70_Assistant
+{
+    /// <summary>
+    /// Show how to combine multiple prompt template factories.
+    /// </summary>
+    public static async Task RunAsync()
+    {
+        Console.WriteLine("======== Example70_Assistant ========");
+
+        string apiKey = TestConfiguration.AzureOpenAI.ApiKey;
+        string chatDeploymentName = TestConfiguration.AzureOpenAI.ChatDeploymentName;
+        string endpoint = TestConfiguration.AzureOpenAI.Endpoint;
+
+        if (apiKey == null || chatDeploymentName == null || endpoint == null)
+        {
+            Console.WriteLine("Azure endpoint, apiKey, or deploymentName not found. Skipping example.");
+            return;
+        }
+
+        //IKernel kernel = new KernelBuilder()
+        //    .WithLoggerFactory(ConsoleLogger.LoggerFactory)
+        //    .WithAzureOpenAIChatCompletionService(
+        //        deploymentName: chatDeploymentName,
+        //        endpoint: endpoint,
+        //        serviceId: "AzureOpenAIChat",
+        //        apiKey: apiKey)
+        //    .Build();
+
+        await Task.Delay(0); // $$$ TBD
+    }
+}
---
+++ dotnet/samples/KernelSyntaxExamples/Example71_AssistantDelegation.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Threading.Tasks;
+
+// ReSharper disable once InconsistentNaming
+/// <summary>
+/// $$$
+/// </summary>
+public static class Example71_AssistantDelegation
+{
+    /// <summary>
+    /// Show how to combine multiple prompt template factories.
+    /// </summary>
+    public static async Task RunAsync()
+    {
+        Console.WriteLine("======== Example71_AssistantDelegation ========");
+
+        string apiKey = TestConfiguration.AzureOpenAI.ApiKey;
+        string chatDeploymentName = TestConfiguration.AzureOpenAI.ChatDeploymentName;
+        string endpoint = TestConfiguration.AzureOpenAI.Endpoint;
+
+        if (apiKey == null || chatDeploymentName == null || endpoint == null)
+        {
+            Console.WriteLine("Azure endpoint, apiKey, or deploymentName not found. Skipping example.");
+            return;
+        }
+
+        //IKernel kernel = new KernelBuilder()
+        //    .WithLoggerFactory(ConsoleLogger.LoggerFactory)
+        //    .WithAzureOpenAIChatCompletionService(
+        //        deploymentName: chatDeploymentName,
+        //        endpoint: endpoint,
+        //        serviceId: "AzureOpenAIChat",
+        //        apiKey: apiKey)
+        //    .Build();
+
+        await Task.Delay(0); // $$$ TBD
+    }
+}
