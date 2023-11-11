7ca881bf1bee03a56bba9dc9f45b45e399b3a03f
Chris Rickman <crickman@microsoft.com>
2023-11-10 21:00:24 -0800

    Forward

--- dotnet/src/Experimental/Assistants/ChatMessage.cs
+++ dotnet/src/Experimental/Assistants/ChatMessage.cs
@@ -11,24 +11,24 @@ namespace Microsoft.SemanticKernel.Experimental.Assistants;
 public class ChatMessage
 {
     /// <summary>
-    /// $$$
+    /// The role associated with the chat message.
     /// </summary>
     public string Role { get; set; }
 
     /// <summary>
-    /// $$$
+    /// The chat message content.
     /// </summary>
     public object Content { get; set; }
 
     /// <summary>
-    /// $$$
+    /// Properties associated with the message.
     /// </summary>
     public Dictionary<string, object>? Properties { get; set; }
 
     /// <summary>
-    /// $$$
+    /// Initializes a new instance of the <see cref="ChatMessage"/> class.
     /// </summary>
-    /// <param name="content"></param>
+    /// <param name="content">$$$</param>
     /// <param name="role"></param>
     /// <param name="properties"></param>
     public ChatMessage(object content, string? role = null, Dictionary<string, object>? properties = null)
