abaa097130f3ab2f278c8dc3bfb33e251f3d32f3
Chris Rickman <crickman@microsoft.com>
2023-11-11 15:06:54 -0800

    Propose assisant definition

--- dotnet/src/Experimental/Assistants/Assistant2.cs
+++ dotnet/src/Experimental/Assistants/Assistant2.cs
@@ -14,9 +14,32 @@ namespace Microsoft.SemanticKernel.Experimental.Assistants;
 public sealed class Assistant2 : IAssistant
 {
     /// <inheritdoc/>
-    public string Id { get; private set; }
+    public string Id => this._model.Id;
+
+    /// <inheritdoc/>
+#pragma warning disable CA1720 // Identifier contains type name - We don't control the schema
+#pragma warning disable CA1716 // Identifiers should not match keywords
+    public string Object => this._model.Object;
+#pragma warning restore CA1720 // Identifier contains type name - We don't control the schema
+#pragma warning restore CA1716 // Identifiers should not match keywords
+
+    /// <inheritdoc/>
+    public long CreatedAt => this._model.CreatedAt;
+
+    /// <inheritdoc/>
+    public string Name => this._model.Name;
+
+    /// <inheritdoc/>
+    public string Description => this._model.Description;
+
+    /// <inheritdoc/>
+    public string Model => this._model.Model;
+
+    /// <inheritdoc/>
+    public string Instructions => this._model.Instructions;
 
     private readonly IOpenAIRestContext _restContext;
+    private readonly AssistantModel _model;
 
     /// <summary>
     /// $$$
@@ -30,15 +53,27 @@ public sealed class Assistant2 : IAssistant
             await restContext.CreateAssistantAsync(assistantModel, cancellationToken).ConfigureAwait(false) ??
             throw new InvalidOperationException("$$$");
 
-        return new Assistant2(resultModel.Id, restContext);
+        return new Assistant2(resultModel, restContext);
     }
 
     /// <summary>
     /// $$$
     /// </summary>
-    internal Assistant2(string id, IOpenAIRestContext restContext)
+    internal static async Task<Assistant2> CreateAsync(
+        IOpenAIRestContext restContext,
+        string assistantId,
+        CancellationToken cancellationToken = default)
     {
-        this.Id = id;
+        var resultModel =
+            await restContext.GetAssistantAsync(assistantId, cancellationToken).ConfigureAwait(false) ??
+            throw new InvalidOperationException("$$$");
+
+        return new Assistant2(resultModel, restContext);
+    }
+
+    private Assistant2(AssistantModel model, IOpenAIRestContext restContext)
+    {
+        this._model = model;
         this._restContext = restContext;
     }
 }
--- dotnet/src/Experimental/Assistants/IAssistant.cs
+++ dotnet/src/Experimental/Assistants/IAssistant.cs
@@ -8,7 +8,41 @@ namespace Microsoft.SemanticKernel.Experimental.Assistants;
 public interface IAssistant
 {
     /// <summary>
-    /// $$$
+    /// Identifier, which can be referenced in API endpoints
     /// </summary>
     string Id { get; }
+
+    /// <summary>
+    /// Always "assistant"
+    /// </summary>
+#pragma warning disable CA1720 // Identifier contains type name - We don't control the schema
+#pragma warning disable CA1716 // Identifiers should not match keywords
+    string Object { get; }
+#pragma warning restore CA1716 // Identifiers should not match keywords
+#pragma warning restore CA1720 // Identifier contains type name
+
+    /// <summary>
+    /// Unix timestamp (in seconds) for when the assistant was created
+    /// </summary>
+    long CreatedAt { get; }
+
+    /// <summary>
+    /// Name of the assistant
+    /// </summary>
+    string Name { get; }
+
+    /// <summary>
+    /// The description of the assistant
+    /// </summary>
+    string Description { get; }
+
+    /// <summary>
+    /// ID of the model to use
+    /// </summary>
+    string Model { get; }
+
+    /// <summary>
+    /// The system instructions that the assistant uses
+    /// </summary>
+    string Instructions { get; }
 }
