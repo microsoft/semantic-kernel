cb97a46c960868a43b658cc0514f845eae3f6a6f
Chris Rickman <crickman@microsoft.com>
2023-11-13 09:06:17 -0800

    Using clean-up

--- dotnet/src/Experimental/Assistants/Internal/Assistant.cs
+++ dotnet/src/Experimental/Assistants/Internal/Assistant.cs
@@ -1,7 +1,5 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.SemanticKernel.Diagnostics;
