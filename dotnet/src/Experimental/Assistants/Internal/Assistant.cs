95a27eb4b513a9e50c4032bc548e1dd6f3a80d82
Gil LaHaye <gillahaye@microsoft.com>
2023-11-12 18:13:54 -0800

    Changes that go along the added Assistant REST ext methods

--- dotnet/src/Experimental/Assistants/Internal/Assistant.cs
+++ dotnet/src/Experimental/Assistants/Internal/Assistant.cs
@@ -1,5 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.SemanticKernel.Diagnostics;
@@ -44,39 +46,108 @@ internal sealed class Assistant : IAssistant
     /// <summary>
     /// Create a new assistant.
     /// </summary>
-    /// <param name="restContext">A context for accessing OpenAI REST endpoint</param>
+    /// <param name="openAiRestContext">A context for accessing OpenAI REST endpoint</param>
     /// <param name="assistantModel">The assistant definition</param>
     /// <param name="cancellationToken">A cancellation token</param>
     /// <returns>An initialized <see cref="Assistant"> instance.</see></returns>
     public static async Task<IAssistant> CreateAsync(
-        IOpenAIRestContext restContext,
+        IOpenAIRestContext openAiRestContext,
         AssistantModel assistantModel,
         CancellationToken cancellationToken = default)
     {
         var resultModel =
-            await restContext.CreateAssistantModelAsync(assistantModel, cancellationToken).ConfigureAwait(false) ??
-            throw new SKException("Unexpected failure creating assisant: no result.");
+            await openAiRestContext.CreateAssistantModelAsync(assistantModel, cancellationToken).ConfigureAwait(false) ??
+            throw new SKException("Unexpected failure creating assistant: no result.");
 
-        return new Assistant(resultModel, restContext);
+        return new Assistant(resultModel, openAiRestContext);
     }
 
     /// <summary>
-    /// Retrieve an existing assisant, by identifier.
+    /// Modify an existing Assistant
     /// </summary>
-    /// <param name="restContext">A context for accessing OpenAI REST endpoint</param>
+    /// <param name="openAiRestContext">Context to make calls to OpenAI</param>
+    /// <param name="assistantModel">New properties for our instance</param>
+    /// <param name="cancellationToken">A cancellation token</param>
+    /// <returns>The modified <see cref="Assistant"> instance.</see></returns>
+    public static async Task<IAssistant> ModifyAsync(
+        IOpenAIRestContext openAiRestContext,
+        AssistantModel assistantModel,
+        CancellationToken cancellationToken = default)
+    {
+        var resultModel =
+            await openAiRestContext.ModifyAssistantModelAsync(assistantModel, cancellationToken).ConfigureAwait(false) ??
+            throw new SKException("Unexpected failure modifying assistant: no result.");
+
+        return new Assistant(resultModel, openAiRestContext);
+    }
+
+    /// <summary>
+    /// Retrieve an existing assistant, by identifier.
+    /// </summary>
+    /// <param name="openAiRestContext">A context for accessing OpenAI REST endpoint</param>
     /// <param name="assistantId">The assistant identifier</param>
     /// <param name="cancellationToken">A cancellation token</param>
     /// <returns>An initialized <see cref="Assistant"> instance.</see></returns>
     public static async Task<IAssistant> GetAsync(
-        IOpenAIRestContext restContext,
+        IOpenAIRestContext openAiRestContext,
         string assistantId,
         CancellationToken cancellationToken = default)
     {
         var resultModel =
-            await restContext.GetAssistantModelAsync(assistantId, cancellationToken).ConfigureAwait(false) ??
-            throw new SKException($"Unexpected failure retrieving assisant: no result. ({assistantId})");
+            await openAiRestContext.GetAssistantModelAsync(assistantId, cancellationToken).ConfigureAwait(false) ??
+            throw new SKException($"Unexpected failure retrieving assistant: no result. ({assistantId})");
 
-        return new Assistant(resultModel, restContext);
+        return new Assistant(resultModel, openAiRestContext);
+    }
+
+    /// <summary>
+    /// List existing Assistant instances from OpenAI
+    /// </summary>
+    /// <param name="openAiRestContext">Context to make calls to OpenAI</param>
+    /// <param name="limit">A limit on the number of objects to be returned.
+    /// Limit can range between 1 and 100, and the default is 20.</param>
+    /// <param name="ascending">Set to true to sort by ascending created_at timestamp
+    /// instead of descending.</param>
+    /// <param name="after">A cursor for use in pagination. This is an object ID that defines
+    /// your place in the list. For instance, if you make a list request and receive 100 objects,
+    /// ending with obj_foo, your subsequent call can include after=obj_foo in order to
+    /// fetch the next page of the list.</param>
+    /// <param name="before">A cursor for use in pagination. This is an object ID that defines
+    /// your place in the list. For instance, if you make a list request and receive 100 objects,
+    /// ending with obj_foo, your subsequent call can include before=obj_foo in order to
+    /// fetch the previous page of the list.</param>
+    /// <param name="cancellationToken">A cancellation token</param>
+    /// <returns>List of retrieved Assistants</returns>
+    public static async IAsyncEnumerable<IAssistant> ListAsync(
+        IOpenAIRestContext openAiRestContext,
+        int limit = 20,
+        bool ascending = false,
+        string? after = null,
+        string? before = null,
+        [EnumeratorCancellation] CancellationToken cancellationToken = default)
+    {
+        foreach (IAssistant assistant in await openAiRestContext
+            .GetAssistantsModelsAsync(limit, ascending, after, before, cancellationToken: cancellationToken)
+            .ConfigureAwait(false))
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            yield return assistant;
+        }
+    }
+
+    /// <summary>
+    /// Delete an existing Assistant
+    /// </summary>
+    /// <param name="openAiRestContext">Context to make calls to OpenAI</param>
+    /// <param name="id">Identifier of Assistant to retrieve</param>
+    /// <param name="cancellationToken">A cancellation token</param>
+    public static async Task DeleteAsync(
+        IOpenAIRestContext openAiRestContext,
+        string id,
+        CancellationToken cancellationToken = default)
+    {
+        await openAiRestContext.DeleteAssistantsModelAsync(id, cancellationToken).ConfigureAwait(false);
     }
 
     /// <summary>
