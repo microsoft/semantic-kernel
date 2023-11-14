b7708950ece4eeab5b29533f036d33b22d570a22
Gil LaHaye <gillahaye@microsoft.com>
2023-11-14 15:26:15 -0800

    Add some unit tests

---
+++ dotnet/src/Experimental/Assistants.UnitTests/Unit/SKFunctionExtensionTests.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Moq;
+using Xunit;
+
+namespace SemanticKernel.Experimental.Assistants.UnitTests.Unit;
+
+[Trait("Category", "Unit Tests")]
+public sealed class SKFunctionExtensionTests
+{
+}
