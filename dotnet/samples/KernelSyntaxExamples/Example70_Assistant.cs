233835bba82803f8c01be09a792391d5c9c54642
Chris Rickman <crickman@microsoft.com>
2023-11-14 19:29:57 -0800

    Uncomment examples

--- dotnet/samples/KernelSyntaxExamples/Example70_Assistant.cs
+++ dotnet/samples/KernelSyntaxExamples/Example70_Assistant.cs
@@ -32,11 +32,11 @@ public static class Example70_Assistant
             return;
         }
 
-        //await RunSimpleChatAsync();
+        await RunSimpleChatAsync();
 
-        //await RunWithNativeFunctionsAsync();
+        await RunWithNativeFunctionsAsync();
 
-        //await RunWithSemanticFunctionsAsync();
+        await RunWithSemanticFunctionsAsync();
 
         await RunAsFunctionAsync();
     }
