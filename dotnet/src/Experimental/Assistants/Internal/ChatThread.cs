47b879435d0ee318d1f431d83ca10301d7612045
Chris <66376200+crickman@users.noreply.github.com>
2023-11-15 15:03:53 -0800

    .Net: Experimental Feature - Assistants API (#3446)
    ### Motivation and Context
    
    <!-- Thank you for your contribution to the semantic-kernel repo!
    Please help reviewers and future users, providing the following
    information:
      1. Why is this change required?
      2. What problem does it solve?
      3. What scenario does it contribute to?
      4. If it fixes an open issue, please link to the issue here.
    -->
    This is an experimental feature that integrates Open AI assistants into
    Semantic Kernel:
    
    
    https://devblogs.microsoft.com/semantic-kernel/assistants-the-future-of-semantic-kernel/
    
    ### Description
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    This change introduces an assistant framework that integrates with
    semantic-kernel patterns, particularly around function calling.
    
    https://platform.openai.com/docs/api-reference/assistants
    
    Assistant usage is showcased in two `KernelSyntaxExamples`.
    
    This initial version does not support:
    
    - Files
    - Retrieval
    - Code Interpreter
    
    
    
    <img width="308" alt="image"
    src="https://github.com/microsoft/semantic-kernel/assets/66376200/fc221f53-1717-4767-828c-4f90fd5d0129">
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [x] The code builds clean without any errors or warnings
    - [x] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [x] All unit tests pass, and I have added new tests where possible
    - [x] I didn't break anyone :smile:
    
    ---------
    
    Co-authored-by: Gil LaHaye <gillahaye@microsoft.com>

---
+++ dotnet/src/Experimental/Assistants/Internal/ChatThread.cs
@@ -0,0 +1,116 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.SemanticKernel.Diagnostics;
+using Microsoft.SemanticKernel.Experimental.Assistants.Extensions;
+using Microsoft.SemanticKernel.Experimental.Assistants.Models;
+
+namespace Microsoft.SemanticKernel.Experimental.Assistants.Internal;
+
+/// <summary>
+/// Represents a thread that contains messages.
+/// </summary>
+internal sealed class ChatThread : IChatThread
+{
+    /// <inheritdoc/>
+    public string Id { get; private set; }
+
+    private readonly OpenAIRestContext _restContext;
+
+    /// <summary>
+    /// Create a new thread.
+    /// </summary>
+    /// <param name="restContext">A context for accessing OpenAI REST endpoint</param>
+    /// <param name="cancellationToken">A cancellation token</param>
+    /// <returns>An initialized <see cref="ChatThread"> instance.</see></returns>
+    public static async Task<IChatThread> CreateAsync(OpenAIRestContext restContext, CancellationToken cancellationToken = default)
+    {
+        var threadModel =
+            await restContext.CreateThreadModelAsync(cancellationToken).ConfigureAwait(false) ??
+            throw new SKException("Unexpected failure creating thread: no result.");
+
+        return new ChatThread(threadModel, messageListModel: null, restContext);
+    }
+
+    /// <summary>
+    /// Retrieve an existing thread.
+    /// </summary>
+    /// <param name="restContext">A context for accessing OpenAI REST endpoint</param>
+    /// <param name="threadId">The thread identifier</param>
+    /// <param name="cancellationToken">A cancellation token</param>
+    /// <returns>An initialized <see cref="ChatThread"> instance.</see></returns>
+    public static async Task<IChatThread> GetAsync(OpenAIRestContext restContext, string threadId, CancellationToken cancellationToken = default)
+    {
+        var threadModel =
+            await restContext.GetThreadModelAsync(threadId, cancellationToken).ConfigureAwait(false) ??
+            throw new SKException($"Unexpected failure retrieving thread: no result. ({threadId})");
+
+        var messageListModel =
+            await restContext.GetMessagesAsync(threadId, cancellationToken).ConfigureAwait(false) ??
+            throw new SKException($"Unexpected failure retrieving thread: no result. ({threadId})");
+
+        return new ChatThread(threadModel, messageListModel, restContext);
+    }
+
+    /// <inheritdoc/>
+    public async Task<IChatMessage> AddUserMessageAsync(string message, CancellationToken cancellationToken = default)
+    {
+        var messageModel =
+            await this._restContext.CreateUserTextMessageAsync(
+                this.Id,
+                message,
+                cancellationToken).ConfigureAwait(false);
+
+        return new ChatMessage(messageModel);
+    }
+
+    /// <inheritdoc/>
+    public async Task<IEnumerable<IChatMessage>> InvokeAsync(IAssistant assistant, CancellationToken cancellationToken)
+    {
+        var tools = assistant.Functions.Select(f => f.ToToolModel()).ToArray();
+        var runModel = await this._restContext.CreateRunAsync(this.Id, assistant.Id, assistant.Instructions, tools, cancellationToken).ConfigureAwait(false);
+
+        var run = new ChatRun(runModel, assistant.Kernel, this._restContext);
+        var results = await run.GetResultAsync(cancellationToken).ConfigureAwait(false);
+
+        var messages = await this.GetMessagesAsync(results, cancellationToken).ToListAsync(cancellationToken).ConfigureAwait(false);
+
+        return messages;
+    }
+
+    /// <summary>
+    /// Delete an existing thread.
+    /// </summary>
+    /// <param name="cancellationToken">A cancellation token</param>
+    public Task DeleteThreadAsync(CancellationToken cancellationToken)
+    {
+        return this._restContext.DeleteThreadModelAsync(this.Id, cancellationToken);
+    }
+
+    private async IAsyncEnumerable<IChatMessage> GetMessagesAsync(
+        IList<string> messageIds,
+        [EnumeratorCancellation] CancellationToken cancellationToken)
+    {
+        var messages = await this._restContext.GetMessagesAsync(this.Id, messageIds, cancellationToken).ConfigureAwait(false);
+        foreach (var message in messages)
+        {
+            yield return new ChatMessage(message);
+        }
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ChatThread"/> class.
+    /// </summary>
+    private ChatThread(
+        ThreadModel threadModel,
+        ThreadMessageListModel? messageListModel,
+        OpenAIRestContext restContext)
+    {
+        this.Id = threadModel.Id;
+        this._restContext = restContext;
+    }
+}
