42964fe307ca74e3859ec573e8dc9f5f26522172
Lee Miller <lemiller@microsoft.com>
2023-11-16 08:48:07 -0800

    🔧 Update RestApiOperationResponse validation & tests
    This commit updates the RestApiOperationResponse validation logic to
    handle different content types, including JSON, XML, and text/html.
    It also adds tests to ensure that responses are parsed correctly for
    OpenAPI versions 2.0, 3.0, and 3.1. Additionally, RestApiOperationResponseTests
    have been updated to use Theory and InlineData for testing various content,
    contentType, and schemaJson combinations. This improves test coverage and
    makes the tests more maintainable.

--- dotnet/src/Functions/Functions.OpenAPI/Extensions/RestApiOperationResponseExtensions.cs
+++ dotnet/src/Functions/Functions.OpenAPI/Extensions/RestApiOperationResponseExtensions.cs
@@ -9,7 +9,7 @@ namespace Microsoft.SemanticKernel.Functions.OpenAPI.Model;
 #pragma warning restore IDE0130
 
 /// <summary>
-/// Class for extensions methods for the <see cref="RestApiOperation"/> class.
+/// Class for extensions methods for the <see cref="RestApiOperationResponse"/> class.
 /// </summary>
 public static class RestApiOperationResponseExtensions
 {
@@ -19,6 +19,9 @@ public static class RestApiOperationResponseExtensions
     /// <returns>True if the response is valid, false otherwise.</returns>
     /// <remarks>
     /// If the schema is not specified, the response is considered valid.
+    /// If the content type is not specified, the response is considered valid.
+    /// If the content type is not supported, the response is considered valid.
+    /// Right now, only JSON is supported.
     /// </remarks>
     public static bool IsValid(this RestApiOperationResponse response)
     {
@@ -27,11 +30,55 @@ public static class RestApiOperationResponseExtensions
             return true;
         }
 
-        var jsonSchema = JsonSchema.FromText(JsonSerializer.Serialize(response.ExpectedSchema));
+        if (string.IsNullOrEmpty(response.ContentType))
+        {
+            return true;
+        }
 
+        switch (response.ContentType)
+        {
+            case "application/json":
+                return ValidateJson(response);
+
+            case "application/xml":
+                return ValidateXml(response);
+
+            case "text/plain":
+            case "text/html":
+                return ValidateTextHtml(response);
+
+            default:
+                return true;
+        }
+    }
+
+    private static bool ValidateJson(RestApiOperationResponse response)
+    {
         try
         {
+            var jsonSchema = JsonSchema.FromText(JsonSerializer.Serialize(response.ExpectedSchema));
             var contentDoc = JsonDocument.Parse(response.Content.ToString());
+            var result = jsonSchema.Evaluate(contentDoc);
+            return result.IsValid;
+        }
+        catch (JsonException)
+        {
+            return false;
+        }
+    }
+
+    private static bool ValidateXml(RestApiOperationResponse response)
+    {
+        // todo -- implement
+        return true;
+    }
+
+    private static bool ValidateTextHtml(RestApiOperationResponse response)
+    {
+        try
+        {
+            var jsonSchema = JsonSchema.FromText(JsonSerializer.Serialize(response.ExpectedSchema));
+            var contentDoc = JsonDocument.Parse($"\"{response.Content}\"");
             var result = jsonSchema.Evaluate(contentDoc);
             return result.IsValid;
         }
--- dotnet/src/Functions/Functions.UnitTests/OpenAPI/RestApiOperationResponseTests.cs
+++ dotnet/src/Functions/Functions.UnitTests/OpenAPI/RestApiOperationResponseTests.cs
@@ -34,27 +34,31 @@ public class RestApiOperationResponseTests
         Assert.True(result);
     }
 
-    [Fact]
-    public void ItShouldFailValidationWithSchema()
+    [Theory]
+    [InlineData("fake-content", "application/json", "{\"type\": \"string\"}")]
+    [InlineData("{\"fake\": \"content\"}", "text/plain", "{\"type\": \"string\"}")]
+    [InlineData("{\"fake\": \"content\"}", "application/json", "{\"type\": \"string\"}")]
+    public void ItShouldFailValidationWithSchema(string content, string contentType, string schemaJson)
     {
         //Arrange
-        var response = new RestApiOperationResponse("fake-content", "fake-content-type", JsonDocument.Parse("{\"type\": \"string\"}"));
+        var response = new RestApiOperationResponse(content, contentType, JsonDocument.Parse(schemaJson));
 
         //Act
         var result = response.IsValid();
 
         //Assert
-        Assert.False(result); // todo -- shouldn't this validate actually?
+        Assert.False(result);
     }
 
-    [Fact]
-    public void ItShouldPassValidationWithSchema()
+    [Theory]
+    [InlineData("\"fake-content\"", "application/json", "{\"type\": \"string\"}")]
+    [InlineData("fake-content", "text/plain", "{\"type\": \"string\"}")]
+    [InlineData("fake-content", "application/xml", "{\"type\": \"string\"}")]
+    [InlineData("fake-content", "image", "{\"type\": \"string\"}")]
+    public void ItShouldPassValidationWithSchema(string content, string contentType, string schemaJson)
     {
         //Arrange
-        var response = new RestApiOperationResponse(
-            "\"fake-content\"",
-            "fake-content-type",
-            JsonDocument.Parse("{\"type\": \"string\"}"));
+        var response = new RestApiOperationResponse(content, contentType, JsonDocument.Parse(schemaJson));
 
         //Act
         var result = response.IsValid();
@@ -64,7 +68,7 @@ public class RestApiOperationResponseTests
     }
 
     [Theory]
-    [InlineData("{\"products\": [{\"id\": \"1234\", \"name\": \"Laptop\"}]}", "application/json", "{\"type\": \"object\"}")] // TODO -- Is this good enough/complete?
+    [InlineData("{\"products\": [{\"id\": \"1234\", \"name\": \"Laptop\"}]}", "application/json", "{\"type\": \"object\"}")]
     [InlineData("{\"products\": [{\"id\": \"1234\", \"name\": \"Laptop\"}]}", "application/json", "{\"title\":\"ProductResponse\",\"type\":\"object\",\"properties\":{\"products\":{\"type\":\"array\",\"items\":{\"title\":\"Product\",\"type\":\"object\",\"properties\":{\"attributes\":{\"type\":\"array\",\"items\":{\"type\":\"string\"}},\"name\":{\"type\":\"string\"},\"price\":{\"type\":\"string\"},\"url\":{\"type\":\"string\"}}}}}}")]
     public void IsValidShouldBeTrue(object content, string contentType, string schemaJson)
     {
