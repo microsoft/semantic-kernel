54254fde1cd4710ef5747b439ae7536e063a86d9
Lee Miller <lemiller@microsoft.com>
2023-11-16 11:48:36 -0800

    Add support for OpenAPI responses and schema validation (#3450)
    ## Summary
    This pull request adds support for OpenAPI responses and schema
    validation to the RestApiOperationRunner and RestApiOperationResponse
    classes. Specifically, it adds a new parameter to the SendAsync method
    in RestApiOperationRunner to allow for passing in a dictionary of
    expected responses. If a matching response is found, the response schema
    is updated with the schema from the expected response. Additionally, the
    CreateRestApiOperationResponses method was added to parse the OpenAPI
    responses and create a dictionary of expected responses.
    RestApiOperationResponse now includes a new property 'Schema' to specify
    the schema against which the response body should be validated, and a
    new method 'IsValid' to validate the response content against the
    schema. RestApiOperation now includes a new property 'Responses' to
    specify possible operation responses. The KernelOpenApiPluginExtensions
    was modified to use the correct response when multiple responses are
    defined in an operation. RestApiOperationResponseTests were added to
    test RestApiOperationResponse objects with and without schemas, and
    RestApiOperationRunnerTests were updated to use the new
    RestApiOperationResponse.IsValid() method. Additionally, test cases were
    added for RestApiOperationRunner and OpenApiPluginFunctions, and a test
    was added for the Instacart plugin.
    
    ## Changes
    - Added a new parameter to the SendAsync method in
    RestApiOperationRunner to allow for passing in a dictionary of expected
    responses
    - Added the CreateRestApiOperationResponses method to parse the OpenAPI
    responses and create a dictionary of expected responses
    - Added a new property 'Schema' to RestApiOperationResponse to specify
    the schema against which the response body should be validated
    - Added a new method 'IsValid' to RestApiOperationResponse to validate
    the response content against the schema
    - Added a new property 'Responses' to RestApiOperation to specify
    possible operation responses
    - Modified KernelOpenApiPluginExtensions to use the correct response
    when multiple responses are defined in an operation
    - Added RestApiOperationResponseTests to test RestApiOperationResponse
    objects with and without schemas
    - Updated RestApiOperationRunnerTests to use the new
    RestApiOperationResponse.IsValid() method
    - Added test cases for RestApiOperationRunner and OpenApiPluginFunctions
    - Added a test for the Instacart plugin
    
    ---
    *Powered by [Microsoft Semantic
    Kernel](https://github.com/microsoft/semantic-kernel)*
    Related:
    #3129
    #1978
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [x] The code builds clean without any errors or warnings
    - [x] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [x] All unit tests pass, and I have added new tests where possible
    - [x] I didn't break anyone :smile:

--- dotnet/src/Functions/Functions.OpenAPI/Model/RestApiOperationResponse.cs
+++ dotnet/src/Functions/Functions.OpenAPI/Model/RestApiOperationResponse.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
 using System.ComponentModel;
+using System.Text.Json;
 
 namespace Microsoft.SemanticKernel.Functions.OpenAPI.Model;
 
@@ -21,13 +22,20 @@ public sealed class RestApiOperationResponse
     public string ContentType { get; }
 
     /// <summary>
+    /// The expected schema of the response as advertised in the OpenAPI operation.
+    /// </summary>
+    public JsonDocument? ExpectedSchema { get; internal set; }
+
+    /// <summary>
     /// Initializes a new instance of the <see cref="RestApiOperationResponse"/> class.
     /// </summary>
     /// <param name="content">The content of the response.</param>
     /// <param name="contentType">The content type of the response.</param>
-    public RestApiOperationResponse(object content, string contentType)
+    /// <param name="expectedSchema">The schema against which the response body should be validated.</param>
+    public RestApiOperationResponse(object content, string contentType, JsonDocument? expectedSchema = null)
     {
         this.Content = content;
         this.ContentType = contentType;
+        this.ExpectedSchema = expectedSchema;
     }
 }
