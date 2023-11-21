9b54ab8feecade645aec800a9c9f15085937e2c9
Alessandro Federici <afederici75@users.noreply.github.com>
2023-11-21 15:02:08 -0500

    Forgot to remove the test filter and added the MS copyright notice.

--- dotnet/samples/KernelSyntaxExamples/Example72_HuggingFaceEncoding.cs
+++ dotnet/samples/KernelSyntaxExamples/Example72_HuggingFaceEncoding.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
 using System.Threading.Tasks;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.AI.Embeddings;
