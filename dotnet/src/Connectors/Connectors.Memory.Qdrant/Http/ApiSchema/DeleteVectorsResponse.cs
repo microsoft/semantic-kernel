5fdb3225bf39707107af0ae3ca2c79badf5a7aaa
akade <akade@users.noreply.github.com>
2023-11-22 14:46:12 +0100

    Suppressed warning

--- dotnet/src/Connectors/Connectors.Memory.Qdrant/Http/ApiSchema/DeleteVectorsResponse.cs
+++ dotnet/src/Connectors/Connectors.Memory.Qdrant/Http/ApiSchema/DeleteVectorsResponse.cs
@@ -5,6 +5,8 @@ namespace Microsoft.SemanticKernel.Connectors.Memory.Qdrant.Http.ApiSchema;
 /// <summary>
 /// Empty qdrant response for requests that return nothing but status / error.
 /// </summary>
+#pragma warning disable CA1812 // Avoid uninstantiated internal classes. Justification: deserialized by QdrantVectorDbClient.DeleteVectorsByIdAsync & QdrantVectorDbClient.DeleteVectorByPayloadIdAsync
 internal sealed class DeleteVectorsResponse : QdrantResponse
+#pragma warning restore CA1812 // Avoid uninstantiated internal classes
 {
 }
