694888a1eedb75fe8ecf95a7d3cdfba70e79f77d
Stephen Toub <stoub@microsoft.com>
2023-11-22 12:54:57 -0500

    .Net: Consolidate use of JsonSerializerOptions (#3589)
    An internal `Json` class was defined that contained a few
    Serialize/Deserialize methods, but all these did was delegate to
    JsonSerializer. The type was only used in a few of the many places
    JsonSerializer is used throughout the codebase. I've deleted that custom
    type, and instead replaced it with a cache for commonly-used
    configurations of JsonSerializerOptions. In some cases, the Json type
    was being used but the options it was being used with didn't matter, so
    I removed the use of those options.

--- dotnet/src/Connectors/Connectors.AI.OpenAI/ImageGeneration/AzureOpenAIImageGeneration.cs
+++ dotnet/src/Connectors/Connectors.AI.OpenAI/ImageGeneration/AzureOpenAIImageGeneration.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
@@ -139,7 +140,7 @@ public class AzureOpenAIImageGeneration : OpenAIClientBase, IImageGeneration
             throw new ArgumentOutOfRangeException(nameof(width), width, "OpenAI can generate only square images of size 256x256, 512x512, or 1024x1024.");
         }
 
-        var requestBody = Microsoft.SemanticKernel.Text.Json.Serialize(new ImageGenerationRequest
+        var requestBody = JsonSerializer.Serialize(new ImageGenerationRequest
         {
             Prompt = description,
             Size = $"{width}x{height}",
