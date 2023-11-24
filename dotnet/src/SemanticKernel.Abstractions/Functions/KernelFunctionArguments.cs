156f4bb0afc3b645b75e35d878bafc704b27ce71
SergeyMenshykh <sergemenshikh@gmail.com>
2023-11-24 21:04:58 +0000

    The KernelFunctionArguments class is added.

---
+++ dotnet/src/SemanticKernel.Abstractions/Functions/KernelFunctionArguments.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Collections.Generic;
+using Microsoft.SemanticKernel.AI;
+
+#pragma warning disable IDE0130
+namespace Microsoft.SemanticKernel;
+#pragma warning restore IDE0130
+
+/// <summary>
+/// Represents arguments of <see cref="KernelFunction.InvokeAsync"/>  method.
+/// </summary>
+public sealed class KernelFunctionArguments : Dictionary<string, string>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="KernelFunctionArguments"/> class.
+    /// </summary>
+    /// <param name="requestSettings">The AI request settings.</param>
+    public KernelFunctionArguments(AIRequestSettings? requestSettings = null)
+    {
+        this.RequestSettings = requestSettings;
+    }
+
+    /// <summary>
+    /// Gets or sets the AI request settings.
+    /// </summary>
+    public AIRequestSettings? RequestSettings { get; set; }
+}
---
+++ dotnet/src/SemanticKernel.UnitTests/Functions/KernelFunctionArgumentsTests.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.AI;
+using Xunit;
+
+namespace SemanticKernel.UnitTests.Functions;
+public class KernelFunctionArgumentsTests
+{
+    [Fact]
+    public void ItCanBeCreatedWithNoArguments()
+    {
+        KernelFunctionArguments kernelFunction = new() { };
+
+        Assert.Null(kernelFunction.RequestSettings);
+        Assert.Empty(kernelFunction);
+    }
+
+    [Fact]
+    public void ItCanBeCreatedWithRequestSettingsOnly()
+    {
+        // Arrange
+        var requestSettings = new AIRequestSettings();
+
+        // Act
+        KernelFunctionArguments kernelFunction = new(requestSettings) { };
+
+        // Assert
+        Assert.Equal(requestSettings, kernelFunction.RequestSettings);
+        Assert.Empty(kernelFunction);
+    }
+
+    [Fact]
+    public void ItCanBeCreatedWithArgumentsOnly()
+    {
+        // Arrange & Act
+        KernelFunctionArguments kernelFunction = new() { { "fake-key", "fake-value" } };
+
+        // Assert
+        Assert.Null(kernelFunction.RequestSettings);
+
+        var argument = Assert.Single(kernelFunction);
+        Assert.Equal("fake-key", argument.Key);
+        Assert.Equal("fake-value", argument.Value);
+    }
+
+    [Fact]
+    public void ItCanBeCreatedWithBothRequestSettingsAndArguments()
+    {
+        // Arrange
+        var requestSettings = new AIRequestSettings();
+
+        // Act
+        KernelFunctionArguments kernelFunction = new(requestSettings) { { "fake-key", "fake-value" } };
+
+        // Assert
+        Assert.Equal(requestSettings, kernelFunction.RequestSettings);
+
+        var argument = Assert.Single(kernelFunction);
+        Assert.Equal("fake-key", argument.Key);
+        Assert.Equal("fake-value", argument.Value);
+    }
+}
