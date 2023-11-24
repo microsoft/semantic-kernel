00cf789a248884cccc865c23d1d0e907055e4d39
markwallace-microsoft <mark.e.wallace@outlook.com>
2023-11-24 22:32:53 +0000

    Move shared planning code to internal utilities

---
+++ dotnet/src/Planners/Planners.OpenAI/PlannerConfig.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+namespace Microsoft.SemanticKernel.Planning;
+
+/// <summary>
+/// Planner config with semantic memory
+/// </summary>
+public class PlannerConfig : PlannerConfigBase
+{
+    /// <summary>
+    /// Semantic Memory configuration, used to enable function filtering during plan creation.
+    /// </summary>
+    /// <remarks>
+    /// This configuration will be ignored if GetAvailableFunctionsAsync is set.
+    /// </remarks>
+    public SemanticMemoryConfig SemanticMemoryConfig { get; set; } = new();
+}
--- dotnet/src/Planners/Planners.OpenAI/Stepwise/FunctionCallingStepwisePlannerConfig.cs
+++ dotnet/src/Planners/Planners.OpenAI/Stepwise/FunctionCallingStepwisePlannerConfig.cs
@@ -11,7 +11,7 @@ namespace Microsoft.SemanticKernel.Planning;
 /// <summary>
 /// Configuration for Stepwise planner instances.
 /// </summary>
-public sealed class FunctionCallingStepwisePlannerConfig : PlannerConfigBase
+public sealed class FunctionCallingStepwisePlannerConfig : PlannerConfig
 {
     /// <summary>
     /// Initializes a new instance of the <see cref="FunctionCallingStepwisePlannerConfig"/>
