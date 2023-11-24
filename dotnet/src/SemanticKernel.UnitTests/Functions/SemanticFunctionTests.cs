5223cfff644daac046ce7547a7c6e308437808c8
markwallace-microsoft <mark.e.wallace@outlook.com>
2023-11-24 15:39:42 +0000

    Fixes after merging latest from main

--- dotnet/src/SemanticKernel.UnitTests/Functions/SemanticFunctionTests.cs
+++ dotnet/src/SemanticKernel.UnitTests/Functions/SemanticFunctionTests.cs
@@ -9,6 +9,7 @@ using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.AI;
 using Microsoft.SemanticKernel.AI.TextCompletion;
 using Microsoft.SemanticKernel.Connectors.AI.OpenAI;
+using Microsoft.SemanticKernel.Orchestration;
 using Moq;
 using Xunit;
 
