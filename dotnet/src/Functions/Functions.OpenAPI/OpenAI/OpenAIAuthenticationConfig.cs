18a3f0dbc9c49c6ea7e158dc8648c20ada07e7be
Chris Rickman <crickman@microsoft.com>
2023-11-28 09:50:00 -0800

    Namespac

--- dotnet/src/Functions/Functions.OpenAPI/OpenAI/OpenAIAuthenticationConfig.cs
+++ dotnet/src/Functions/Functions.OpenAPI/OpenAI/OpenAIAuthenticationConfig.cs
@@ -2,7 +2,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Runtime.Serialization;
 using System.Text.Json.Serialization;
 
 namespace Microsoft.SemanticKernel.Functions.OpenAPI.OpenAI;
