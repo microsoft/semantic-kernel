c9bc3e96c20b0f4e441137ddcb2bb9ba5aa66453
Teresa Hoang <tehoang@microsoft.com>
2023-11-28 15:28:42 -0800

    dotnet format

--- dotnet/src/Planners/Planners.Handlebars/Handlebars/HandlebarsPlanner.cs
+++ dotnet/src/Planners/Planners.Handlebars/Handlebars/HandlebarsPlanner.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
 using Microsoft.SemanticKernel.AI.ChatCompletion;
 using Microsoft.SemanticKernel.Orchestration;
 
