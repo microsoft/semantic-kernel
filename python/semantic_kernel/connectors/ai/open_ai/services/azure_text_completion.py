c765cb110952b226d38bc3ba4c6b44f0b044cb02
Alex Chao <5111035+alexchaomander@users.noreply.github.com>
2023-11-28 22:44:10 -0800

    Python: Make SK compatible with OpenAI 1.0 (#3555)
    Need to update SK Python to integrate the breaking changes from
    openai-python 1.0
    
    https://github.com/microsoft/semantic-kernel/issues/3330
    
    This PR replaces the old one that was merging to main
    https://github.com/microsoft/semantic-kernel/pull/3417
    
    ---------
    
    ### Motivation and Context
    
    <!-- Thank you for your contribution to the semantic-kernel repo!
    Please help reviewers and future users, providing the following
    information:
      1. Why is this change required?
      2. What problem does it solve?
      3. What scenario does it contribute to?
      4. If it fixes an open issue, please link to the issue here.
    -->
    
    ### Description
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [ ] The code builds clean without any errors or warnings
    - [ ] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [ ] All unit tests pass, and I have added new tests where possible
    - [ ] I didn't break anyone :smile:
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Joowon <joowon.kim@dm.snu.ac.kr>
    Co-authored-by: Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
    Co-authored-by: Dmytro Struk <13853051+dmytrostruk@users.noreply.github.com>
    Co-authored-by: Mark Wallace <127216156+markwallace-microsoft@users.noreply.github.com>
    Co-authored-by: Ben Thomas <ben.thomas@microsoft.com>
    Co-authored-by: Ben Thomas <bentho@microsoft.com>
    Co-authored-by: Devis Lucato <dluc@users.noreply.github.com>
    Co-authored-by: Aayush Kataria <aayushkataria3011@gmail.com>
    Co-authored-by: Abby Harrison <abby.harrison@microsoft.com>
    Co-authored-by: Abby Harrison <54643756+awharrison-28@users.noreply.github.com>
    Co-authored-by: Stephen Toub <stoub@microsoft.com>
    Co-authored-by: Teresa Hoang <125500434+teresaqhoang@users.noreply.github.com>
    Co-authored-by: Chris <66376200+crickman@users.noreply.github.com>
    Co-authored-by: Gil LaHaye <gillahaye@microsoft.com>
    Co-authored-by: Gina Triolo <51341242+gitri-ms@users.noreply.github.com>
    Co-authored-by: Tao Chen <TaoChenOSU@users.noreply.github.com>
    Co-authored-by: Lee Miller <lemiller@microsoft.com>
    Co-authored-by: Jennifer Marsman <jennifermarsman@users.noreply.github.com>
    Co-authored-by: Weihan Li <weihanli@outlook.com>
    Co-authored-by: SergeyMenshykh <68852919+SergeyMenshykh@users.noreply.github.com>
    Co-authored-by: Diego Colombo <dicolomb@microsoft.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Alex Chao <achao@achao>
    Co-authored-by: Eduard van Valkenburg <eavanvalkenburg@users.noreply.github.com>
    Co-authored-by: Evan Mattson <35585003+moonbox3@users.noreply.github.com>
    Co-authored-by: Evan Mattson <evan.mattson@microsoft.com>

--- python/semantic_kernel/connectors/ai/open_ai/services/azure_text_completion.py
+++ python/semantic_kernel/connectors/ai/open_ai/services/azure_text_completion.py
@@ -2,67 +2,158 @@
 
 
 from logging import Logger
-from typing import Optional
+from typing import Dict, Optional, overload
 
-from semantic_kernel.connectors.ai.open_ai.services.open_ai_text_completion import (
-    OpenAITextCompletion,
+from openai.lib.azure import AsyncAzureADTokenProvider
+
+from semantic_kernel.connectors.ai.open_ai.const import DEFAULT_AZURE_API_VERSION
+from semantic_kernel.connectors.ai.open_ai.services.azure_config_base import (
+    AzureOpenAIConfigBase,
+)
+from semantic_kernel.connectors.ai.open_ai.services.open_ai_handler import (
+    OpenAIModelTypes,
+)
+from semantic_kernel.connectors.ai.open_ai.services.open_ai_text_completion_base import (
+    OpenAITextCompletionBase,
 )
 
 
-class AzureTextCompletion(OpenAITextCompletion):
-    _endpoint: str
-    _api_version: str
-    _api_type: str
+class AzureTextCompletion(AzureOpenAIConfigBase, OpenAITextCompletionBase):
+    """Azure Text Completion class."""
 
+    @overload
     def __init__(
         self,
-        deployment_name: str,
-        endpoint: Optional[str] = None,
+        base_url: str,
+        api_version: str = DEFAULT_AZURE_API_VERSION,
         api_key: Optional[str] = None,
-        api_version: str = "2022-12-01",
-        logger: Optional[Logger] = None,
-        ad_auth=False,
+        ad_token: Optional[str] = None,
+        ad_token_provider: Optional[AsyncAzureADTokenProvider] = None,
+        log: Optional[Logger] = None,
     ) -> None:
         """
         Initialize an AzureTextCompletion service.
 
-        You must provide:
-        - A deployment_name, endpoint, and api_key (plus, optionally: ad_auth)
-
-        :param deployment_name: The name of the Azure deployment. This value
-            will correspond to the custom name you chose for your deployment
-            when you deployed a model. This value can be found under
-            Resource Management > Deployments in the Azure portal or, alternatively,
-            under Management > Deployments in Azure OpenAI Studio.
-        :param endpoint: The endpoint of the Azure deployment. This value
-            can be found in the Keys & Endpoint section when examining
-            your resource from the Azure portal.
-        :param api_key: The API key for the Azure deployment. This value can be
-            found in the Keys & Endpoint section when examining your resource in
-            the Azure portal. You can use either KEY1 or KEY2.
-        :param api_version: The API version to use. (Optional)
-            The default value is "2022-12-01".
-        :param logger: The logger instance to use. (Optional)
-        :param ad_auth: Whether to use Azure Active Directory authentication.
-            (Optional) The default value is False.
+        Arguments:
+            deployment_name: The name of the Azure deployment. This value
+                will correspond to the custom name you chose for your deployment
+                when you deployed a model. This value can be found under
+                Resource Management > Deployments in the Azure portal or, alternatively,
+                under Management > Deployments in Azure OpenAI Studio.
+            endpoint: The endpoint of the Azure deployment. This value
+                can be found in the Keys & Endpoint section when examining
+                your resource from the Azure portal.
+            api_key: The API key for the Azure deployment. This value can be
+                found in the Keys & Endpoint section when examining your resource in
+                the Azure portal. You can use either KEY1 or KEY2.
+            api_version: The API version to use. (Optional)
+                The default value is "2023-05-15".
+            ad_auth: Whether to use Azure Active Directory authentication. (Optional)
+                The default value is False.
+            log: The logger instance to use. (Optional)
+            logger: deprecated, use 'log' instead.
         """
-        if not deployment_name:
-            raise ValueError("The deployment name cannot be `None` or empty")
-        if not api_key:
-            raise ValueError("The Azure API key cannot be `None` or empty`")
-        if not endpoint:
-            raise ValueError("The Azure endpoint cannot be `None` or empty")
-        if not endpoint.startswith("https://"):
-            raise ValueError("The Azure endpoint must start with https://")
 
-        self._api_type = "azure_ad" if ad_auth else "azure"
+    @overload
+    def __init__(
+        self,
+        deployment_name: str,
+        endpoint: str,
+        api_version: str = DEFAULT_AZURE_API_VERSION,
+        api_key: Optional[str] = None,
+        ad_token: Optional[str] = None,
+        ad_token_provider: Optional[AsyncAzureADTokenProvider] = None,
+        log: Optional[Logger] = None,
+    ) -> None:
+        """
+        Initialize an AzureTextCompletion service.
 
+        Arguments:
+            deployment_name: The name of the Azure deployment. This value
+                will correspond to the custom name you chose for your deployment
+                when you deployed a model. This value can be found under
+                Resource Management > Deployments in the Azure portal or, alternatively,
+                under Management > Deployments in Azure OpenAI Studio.
+            endpoint: The endpoint of the Azure deployment. This value
+                can be found in the Keys & Endpoint section when examining
+                your resource from the Azure portal.
+            api_key: The API key for the Azure deployment. This value can be
+                found in the Keys & Endpoint section when examining your resource in
+                the Azure portal. You can use either KEY1 or KEY2.
+            api_version: The API version to use. (Optional)
+                The default value is "2023-05-15".
+            ad_auth: Whether to use Azure Active Directory authentication. (Optional)
+                The default value is False.
+            log: The logger instance to use. (Optional)
+            logger: deprecated, use 'log' instead.
+        """
+
+    def __init__(
+        self,
+        deployment_name: Optional[str] = None,
+        endpoint: Optional[str] = None,
+        base_url: Optional[str] = None,
+        api_version: str = DEFAULT_AZURE_API_VERSION,
+        api_key: Optional[str] = None,
+        ad_token: Optional[str] = None,
+        ad_token_provider: Optional[AsyncAzureADTokenProvider] = None,
+        log: Optional[Logger] = None,
+        logger: Optional[Logger] = None,
+    ) -> None:
+        """
+        Initialize an AzureTextCompletion service.
+
+        Arguments:
+            deployment_name: The name of the Azure deployment. This value
+                will correspond to the custom name you chose for your deployment
+                when you deployed a model. This value can be found under
+                Resource Management > Deployments in the Azure portal or, alternatively,
+                under Management > Deployments in Azure OpenAI Studio.
+            endpoint: The endpoint of the Azure deployment. This value
+                can be found in the Keys & Endpoint section when examining
+                your resource from the Azure portal.
+            api_key: The API key for the Azure deployment. This value can be
+                found in the Keys & Endpoint section when examining your resource in
+                the Azure portal. You can use either KEY1 or KEY2.
+            api_version: The API version to use. (Optional)
+                The default value is "2023-03-15-preview".
+            ad_auth: Whether to use Azure Active Directory authentication. (Optional)
+                The default value is False.
+            log: The logger instance to use. (Optional)
+            logger: deprecated, use 'log' instead.
+        """
+        if logger:
+            logger.warning("The 'logger' argument is deprecated, use 'log' instead.")
         super().__init__(
-            deployment_name,
-            api_key,
-            api_type=self._api_type,
-            api_version=api_version,
+            deployment_name=deployment_name,
             endpoint=endpoint,
-            org_id=None,
-            log=logger,
+            base_url=base_url,
+            api_version=api_version,
+            api_key=api_key,
+            ad_token=ad_token,
+            ad_token_provider=ad_token_provider,
+            log=log or logger,
+            model_type=OpenAIModelTypes.TEXT,
+        )
+
+    @classmethod
+    def from_dict(cls, settings: Dict[str, str]) -> "AzureTextCompletion":
+        """
+        Initialize an Azure OpenAI service from a dictionary of settings.
+
+        Arguments:
+            settings: A dictionary of settings for the service.
+                should contains keys: deployment_name, endpoint, api_key
+                and optionally: api_version, ad_auth, log
+        """
+
+        return AzureTextCompletion(
+            deployment_name=settings.get("deployment_name"),
+            endpoint=settings.get("endpoint"),
+            base_url=settings.get("base_url"),
+            api_version=settings.get("api_version", DEFAULT_AZURE_API_VERSION),
+            api_key=settings["api_key"],
+            ad_token=settings.get("ad_token"),
+            ad_token_provider=settings.get("ad_token_provider"),
+            log=settings.get("log"),
         )
