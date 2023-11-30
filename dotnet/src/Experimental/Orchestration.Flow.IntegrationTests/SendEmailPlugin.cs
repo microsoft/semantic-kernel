895b45a71be07e4196fdf17698951309995ee3e9
Stephen Toub <stoub@microsoft.com>
2023-11-30 14:45:50 -0500

    Fix unnecessary usings

--- dotnet/src/Experimental/Orchestration.Flow.IntegrationTests/SendEmailPlugin.cs
+++ dotnet/src/Experimental/Orchestration.Flow.IntegrationTests/SendEmailPlugin.cs
@@ -3,7 +3,6 @@
 using System.ComponentModel;
 using System.Text.Json;
 using Microsoft.SemanticKernel;
-using Microsoft.SemanticKernel.AI;
 
 namespace SemanticKernel.Experimental.Orchestration.Flow.IntegrationTests;
 
