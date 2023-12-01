a4682a03b01a45cb09d15c067210933228993ea1
Matthew Bolanos <matthewbolanos@gmail.com>
2023-12-01 13:05:21 +0000

    Run dotnet format

--- dotnet/src/SemanticKernel.Abstractions/AI/Embeddings/ITextEmbeddingGeneration.cs
+++ dotnet/src/SemanticKernel.Abstractions/AI/Embeddings/ITextEmbeddingGeneration.cs
@@ -7,7 +7,6 @@ namespace Microsoft.SemanticKernel.AI.Embeddings;
 /// <summary>
 /// Represents a generator of text embeddings of type <c>float</c>.
 /// </summary>
-
 [Experimental("SKEXP0001")]
 public interface ITextEmbeddingGeneration : IEmbeddingGeneration<string, float>
 {
