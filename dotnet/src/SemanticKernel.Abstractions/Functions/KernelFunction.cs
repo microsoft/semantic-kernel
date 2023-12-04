de3a0bd5ca9edfb754b57ba5c63690f79d947e47
Tao Chen <taochen@microsoft.com>
2023-12-04 14:17:19 -0800

    Fix dotnet format

--- dotnet/src/SemanticKernel.Abstractions/Functions/KernelFunction.cs
+++ dotnet/src/SemanticKernel.Abstractions/Functions/KernelFunction.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.Metrics;
-using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
