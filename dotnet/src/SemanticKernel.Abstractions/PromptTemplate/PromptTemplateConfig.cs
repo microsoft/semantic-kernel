3cb769b8356d429245a7eb83028e35af46a087c8
markwallace-microsoft <mark.e.wallace@outlook.com>
2023-12-05 09:17:22 +0000

    Add output variable

--- dotnet/src/SemanticKernel.Abstractions/PromptTemplate/PromptTemplateConfig.cs
+++ dotnet/src/SemanticKernel.Abstractions/PromptTemplate/PromptTemplateConfig.cs
@@ -51,6 +51,12 @@ public sealed class PromptTemplateConfig
     public List<InputVariable> InputVariables { get; set; } = new();
 
     /// <summary>
+    /// Output variable.
+    /// </summary>
+    [JsonPropertyName("output_variable")]
+    public OutputVariable? OutputVariable { get; set; }
+
+    /// <summary>
     /// Prompt execution settings.
     /// </summary>
     [JsonPropertyName("execution_settings")]
@@ -69,37 +75,6 @@ public sealed class PromptTemplateConfig
     public PromptTemplateConfig(string template)
     {
         this.Template = template;
-    }
-
-    /// <summary>
-    /// Input variable for prompt functions.
-    /// </summary>
-    public class InputVariable
-    {
-        /// <summary>
-        /// Name of the variable to pass to the prompt function.
-        /// e.g. when using "{{$input}}" the name is "input", when using "{{$style}}" the name is "style", etc.
-        /// </summary>
-        [JsonPropertyName("name")]
-        public string Name { get; set; } = string.Empty;
-
-        /// <summary>
-        /// Variable description for UI applications and planners. Localization is not supported here.
-        /// </summary>
-        [JsonPropertyName("description")]
-        public string Description { get; set; } = string.Empty;
-
-        /// <summary>
-        /// Default value when nothing is provided.
-        /// </summary>
-        [JsonPropertyName("default_value")]
-        public string DefaultValue { get; set; } = string.Empty;
-
-        /// <summary>
-        /// True to indicate the input variable is required. True by default.
-        /// </summary>
-        [JsonPropertyName("is_required")]
-        public bool IsRequired { get; set; } = true;
     }
 
     /// <summary>
