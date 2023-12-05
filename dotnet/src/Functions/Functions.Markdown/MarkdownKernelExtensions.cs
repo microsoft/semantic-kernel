7178767ea7bf4ea791ee18e86524d2488ad72e87
markwallace-microsoft <mark.e.wallace@outlook.com>
2023-12-05 08:06:58 +0000

    Remove CreateFunctionFromPromptXXXResource methods

--- dotnet/src/Functions/Functions.Markdown/MarkdownKernelExtensions.cs
+++ dotnet/src/Functions/Functions.Markdown/MarkdownKernelExtensions.cs
@@ -1,6 +1,5 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
-using System.IO;
 using Microsoft.Extensions.Logging;
 
 namespace Microsoft.SemanticKernel;
@@ -14,32 +13,8 @@ public static class MarkdownKernelExtensions
     /// Creates a <see cref="KernelFunction"/> instance for a prompt function using the specified markdown text.
     /// </summary>
     /// <param name="kernel">The <see cref="Kernel"/> containing services, plugins, and other state for use throughout the operation.</param>
-    /// <param name="resourceName">Resource containing the YAML representation of the <see cref="PromptTemplateConfig"/> to use to create the prompt function</param>
-    /// <param name="functionName">The function name</param>
-    /// <param name="pluginName">The optional name of the plug-in associated with this method.</param>
-    /// <param name="promptTemplateFactory">>Prompt template factory.</param>
-    /// <returns>The created <see cref="KernelFunction"/>.</returns>
-    public static KernelFunction CreateFunctionFromMarkdownResource(
-        this Kernel kernel,
-        string resourceName,
-        string? functionName = null,
-        string? pluginName = null,
-        IPromptTemplateFactory? promptTemplateFactory = null)
-    {
-        Verify.NotNull(kernel);
-        Verify.NotNull(resourceName);
-
-        functionName ??= Path.GetFileNameWithoutExtension(resourceName);
-        return KernelFunctionMarkdown.FromPromptMarkdownResource(resourceName, functionName, pluginName, promptTemplateFactory, kernel.LoggerFactory);
-    }
-
-    /// <summary>
-    /// Creates a <see cref="KernelFunction"/> instance for a prompt function using the specified markdown text.
-    /// </summary>
-    /// <param name="kernel">The <see cref="Kernel"/> containing services, plugins, and other state for use throughout the operation.</param>
     /// <param name="text">YAML representation of the <see cref="PromptTemplateConfig"/> to use to create the prompt function</param>
     /// <param name="functionName">The function name</param>
-    /// <param name="pluginName">The optional name of the plug-in associated with this method.</param>
     /// <param name="promptTemplateFactory">>Prompt template factory.</param>
     /// <param name="loggerFactory"></param>
     /// <returns>The created <see cref="KernelFunction"/>.</returns>
@@ -47,7 +22,6 @@ public static class MarkdownKernelExtensions
         this Kernel kernel,
         string text,
         string functionName,
-        string? pluginName = null,
         IPromptTemplateFactory? promptTemplateFactory = null,
         ILoggerFactory? loggerFactory = null)
     {
@@ -55,6 +29,6 @@ public static class MarkdownKernelExtensions
         Verify.NotNull(text);
         Verify.NotNull(functionName);
 
-        return KernelFunctionMarkdown.FromPromptMarkdown(text, functionName, pluginName, promptTemplateFactory, kernel.LoggerFactory);
+        return KernelFunctionMarkdown.FromPromptMarkdown(text, functionName, promptTemplateFactory, kernel.LoggerFactory);
     }
 }
