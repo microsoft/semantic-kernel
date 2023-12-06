66f02f486fbbcb8135980c81648844c97d8ac0db
tehoang <tehoang@microsoft.com>
2023-12-06 12:01:14 -1000

    Stabilizing integration tests

--- dotnet/src/IntegrationTests/Planners/Handlebars/HandlebarsPlannerTests.cs
+++ dotnet/src/IntegrationTests/Planners/Handlebars/HandlebarsPlannerTests.cs
@@ -50,7 +50,7 @@ public sealed class HandlebarsPlannerTests : IDisposable
     }
 
     [RetryTheory]
-    [InlineData("Outline a novel about software development that is 3 chapters long.", "NovelOutline", "WriterPlugin")]
+    [InlineData("Write a novel about software development that is 3 chapters long.", "NovelChapter", "WriterPlugin")]
     public async Task CreatePlanWithDefaultsAsync(string prompt, string expectedFunction, string expectedPlugin)
     {
         // Arrange
