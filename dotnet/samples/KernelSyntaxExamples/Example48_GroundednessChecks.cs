42628c706d2e11850c805b629f91428dab0dbb6f
Teresa Hoang <125500434+teresaqhoang@users.noreply.github.com>
2023-12-07 08:39:11 -1000

    .Net: Fixing Example48_GroundednessChecks + Tuning Variable usage in Handlebars Planner (#4063)
    ### Motivation and Context
    
    <!-- Thank you for your contribution to the semantic-kernel repo!
    Please help reviewers and future users, providing the following
    information:
      1. Why is this change required?
      2. What problem does it solve?
      3. What scenario does it contribute to?
      4. If it fixes an open issue, please link to the issue here.
    -->
    
    This PR addresses bug #3837, where the plan didn't produce expected
    Groundedness result.
    
    Fix was to add the GroundingText to the ask (used as the Goal in the
    planner).
    
    ### Description
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    PR also contains small fixes to label parameters in relevant semantic
    functions as required + some tuning of the CreatePlan prompt to
    emphasize proper variable usage.
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [x] The code builds clean without any errors or warnings
    - [x] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [x] All unit tests pass, and I have added new tests where possible
    - [x] I didn't break anyone :smile:

--- dotnet/samples/KernelSyntaxExamples/Example48_GroundednessChecks.cs
+++ dotnet/samples/KernelSyntaxExamples/Example48_GroundednessChecks.cs
@@ -58,7 +58,7 @@ after this event Caroline became his wife.""";
 
     public static async Task GroundednessCheckingAsync()
     {
-        Console.WriteLine("======== Groundedness Checks ========");
+        Console.WriteLine("\n======== Groundedness Checks ========");
         var kernel = new KernelBuilder()
             .AddAzureOpenAIChatCompletion(
                 TestConfiguration.AzureOpenAI.ChatDeploymentName,
@@ -100,14 +100,14 @@ her a beggar. My father came to her aid and two years later they married.
 
         var groundingResult = (await kernel.InvokeAsync(reference_check, variables)).ToString();
 
-        Console.WriteLine("======== Reference Check ========");
+        Console.WriteLine("\n======== Reference Check ========");
         Console.WriteLine(groundingResult);
 
         variables[KernelArguments.InputParameterName] = summaryText;
         variables["ungrounded_entities"] = groundingResult;
         var excisionResult = await kernel.InvokeAsync(entity_excision, variables);
 
-        Console.WriteLine("======== Excise Entities ========");
+        Console.WriteLine("\n======== Excise Entities ========");
         Console.WriteLine(excisionResult.GetValue<string>());
     }
 
@@ -115,14 +115,16 @@ her a beggar. My father came to her aid and two years later they married.
     {
         var targetTopic = "people and places";
         var samples = "John, Jane, mother, brother, Paris, Rome";
-        var ask = @$"Make a summary of input text. Then make a list of entities
+        var ask = @$"Make a summary of the following text. Then make a list of entities
 related to {targetTopic} (such as {samples}) which are present in the summary.
 Take this list of entities, and from it make another list of those which are not
 grounded in the original input text. Finally, rewrite your summary to remove the entities
 which are not grounded in the original.
+
+Text:\n{GroundingText};
 ";
 
-        Console.WriteLine("======== Planning - Groundedness Checks ========");
+        Console.WriteLine("\n======== Planning - Groundedness Checks ========");
 
         var kernel = new KernelBuilder()
             .AddAzureOpenAIChatCompletion(
@@ -141,9 +143,12 @@ which are not grounded in the original.
         var planner = new HandlebarsPlanner();
         var plan = await planner.CreatePlanAsync(kernel, ask);
 
-        Console.WriteLine($" Goal: {ask} \n\n Template: {plan}");
+        Console.WriteLine($"======== Goal: ========\n{ask}");
+        Console.WriteLine($"======== Plan ========\n{plan}");
 
         var result = plan.Invoke(kernel, new KernelArguments(), CancellationToken.None);
+
+        Console.WriteLine("======== Result ========");
         Console.WriteLine(result);
     }
 }
@@ -157,18 +162,21 @@ which are not grounded in the original.
 - Zurich
 - Mary
 </entities>
+
 ======== Reference Check ========
 <ungrounded_entities>
 - Milan
 - Zurich
 - Mary
 </ungrounded_entities>
+
 ======== Excise Entities ========
 My father, a respected resident of a city, was a close friend of a merchant named Beaufort who, after a series of
 misfortunes, moved to another city in poverty. My father was upset by his friend's troubles and sought him out,
 finding him in a mean street. Beaufort had saved a small sum of money, but it was not enough to support him and
 his daughter. The daughter procured work to eek out a living, but after ten months her father died, leaving
 her a beggar. My father came to her aid and two years later they married.
+
 ======== Planning - Groundedness Checks ========
 Goal: Make a summary of input text. Then make a list of entities
 related to people and places (such as John, Jane, mother, brother, Paris, Rome) which are present in the summary.
@@ -176,26 +184,30 @@ Take this list of entities, and from it make another list of those which are not
 grounded in the original input text. Finally, rewrite your summary to remove the entities
 which are not grounded in the original.
 
+Text:
+{See GroundingText above}
 
+Plan:
+{{!-- Step 1: Set the input text --}}
+{{set "inputText" "I am by birth a Genevese, and my family is one of the most distinguished of that republic. My ancestors had been for many years counsellors and syndics, and my father had filled several public situations with honour and reputation.He was respected by all who knew him for his integrity and indefatigable attention to public business.He passed his younger days perpetually occupied by the affairs of his country; a variety of circumstances had prevented his marrying early, nor was it until the decline of life that he became a husband and the father of a family. As the circumstances of his marriage illustrate his character, I cannot refrain from relating them.One of his most intimate friends was a merchant who, from a flourishing state, fell, through numerous mischances, into poverty. This man, whose name was Beaufort, was of a proud and unbending disposition and could not bear to live in poverty and oblivion in the same country where he had formerly been distinguished for his rank and magnificence. Having paid his debts, therefore, in the most honourable manner, he retreated with his daughter to the town of Lucerne, where he lived unknown and in wretchedness.My father loved Beaufort with the truest friendship and was deeply grieved by his retreat in these unfortunate circumstances.He bitterly deplored the false pride which led his friend to a conduct so little worthy of the affection that united them.He lost no time in endeavouring to seek him out, with the hope of persuading him to begin the world again through his credit and assistance. Beaufort had taken effectual measures to conceal himself, and it was ten months before my father discovered his abode.Overjoyed at this discovery, he hastened to the house, which was situated in a mean street near the Reuss. But when he entered, misery and despair alone welcomed him. Beaufort had saved but a very small sum of money from the wreck of his fortunes, but it was sufficient to provide him with sustenance for some months, and in the meantime he hoped to procure some respectable employment in a merchant's house. The interval was, consequently, spent in inaction; his grief only became more deep and rankling when he had leisure for reflection, and at length it took so fast hold of his mind that at the end of three months he lay on a bed of sickness, incapable of any exertion. His daughter attended him with the greatest tenderness, but she saw with despair that their little fund was rapidly decreasing and that there was no other prospect of support.But Caroline Beaufort possessed a mind of an uncommon mould, and her courage rose to support her in her adversity. She procured plain work; she plaited straw and by various means contrived to earn a pittance scarcely sufficient to support life. Several months passed in this manner.Her father grew worse; her time was more entirely occupied in attending him; her means of subsistence decreased; and in the tenth month her father died in her arms, leaving her an orphan and a beggar.This last blow overcame her, and she knelt by Beaufort's coffin weeping bitterly, when my father entered the chamber. He came like a protecting spirit to the poor girl, who committed herself to his care; and after the interment of his friend he conducted her to Geneva and placed her under the protection of a relation.Two years after this event Caroline became his wife."}}
 
+{{!-- Step 2: Summarize the input text --}}
+{{set "summary" (SummarizePlugin-Summarize input=(get "inputText"))}}
 
-Steps:
-  - SummarizePlugin.Summarize INPUT='' => RESULT__SUMMARY
-  - GroundingPlugin.ExtractEntities example_entities='John;Jane;mother;brother;Paris;Rome' topic='people and places' INPUT='$RESULT__SUMMARY' => ENTITIES
-  - GroundingPlugin.ReferenceCheckEntities reference_context='$ORIGINAL_TEXT' INPUT='$ENTITIES' => RESULT__UNGROUND_ENTITIES
-  - GroundingPlugin.ExciseEntities ungrounded_entities='$RESULT__UNGROUND_ENTITIES' INPUT='$RESULT__SUMMARY' => RESULT__FINAL_SUMMARY
+{{!-- Step 3: Extract entities related to people and places --}}
+{{set "exampleEntities" (array "John" "Jane" "mother" "brother" "Paris" "Rome")}}
+{{set "extractedEntities" (GroundingPlugin-ExtractEntities input=(get "summary") topic="people and places" example_entities=(get "exampleEntities"))}}
+
+{{!-- Step 4: Check if the extracted entities are grounded in the original text --}}
+{{set "ungroundedEntities" (GroundingPlugin-ReferenceCheckEntities input=(get "extractedEntities") reference_context=(get "inputText"))}}
+
+{{!-- Step 5: Remove ungrounded entities from the summary --}}
+{{set "finalSummary" (GroundingPlugin-ExciseEntities input=(get "summary") ungrounded_entities=(get "ungroundedEntities"))}}
+
+{{!-- Step 6: Output the final summary --}}
+{{json (get "finalSummary")}}
+
 A possible summary is:
-
-
-
-The narrator's father, a respected Genevese politician, befriended Beaufort, a merchant who fell into poverty and hid in Lucerne. After a long search, he found him dying and his daughter Caroline working hard to survive. He took pity on Caroline, buried Beaufort, and married her two years later.
-<ungrounded_entities>
-- narrator
-</ungrounded_entities>
-A possible summary is:
-
-
-
-The father of the story's main character, a respected Genevese politician, befriended Beaufort, a merchant who fell into poverty and hid in Lucerne. After a long search, he found him dying and his daughter Caroline working hard to survive. He took pity on Caroline, buried Beaufort, and married her two years later.
+Born in Geneva to a distinguished family, the narrator's father married late in life. His close friend Beaufort, once wealthy, fell into poverty and moved to another city with his daughter, Caroline. The narrator's father searched for Beaufort, finding him after ten months, only to witness his decline and eventual death. Caroline, now an orphan, was taken in by the narrator's father, who married her two years later.
 == DONE ==
 */
