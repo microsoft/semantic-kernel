1c70cc6440c445b8ad66685d6d9cc55f07279ead
Gil LaHaye <gillahaye@microsoft.com>
2023-12-11 11:17:37 -0800

    .Net: Simplify and clarify example 16 (#4088)
    ### Motivation and Context
    Example is somewhat convoluted.
    
    ### Description
    Remove unneeded code and comment remaining code.
    
    ### Contribution Checklist
    - [ ] The code builds clean without any errors or warnings
    - [ ] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [ ] All unit tests pass, and I have added new tests where possible
    - [ ] I didn't break anyone :smile:

--- dotnet/samples/KernelSyntaxExamples/Example16_CustomLLM.cs
+++ dotnet/samples/KernelSyntaxExamples/Example16_CustomLLM.cs
@@ -3,110 +3,80 @@
 using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.SemanticKernel;
-using Microsoft.SemanticKernel.Connectors.OpenAI;
 using Microsoft.SemanticKernel.TextGeneration;
-using RepoUtils;
-
-#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
 
 /**
- * The following example shows how to plug into SK a custom text generation model.
+ * The following example shows how to plug a custom text generation model into SK.
  *
- * This might be useful in a few scenarios, for example:
+ * To do this, this example uses a text generation service stub (MyTextGenerationService) and
+ * no actual model.
+ *
+ * Using a custom text generation model within SK can be useful in a few scenarios, for example:
  * - You are not using OpenAI or Azure OpenAI models
  * - You are using OpenAI/Azure OpenAI models but the models are behind a web service with a different API schema
  * - You want to use a local model
  *
- * Note that all text generation models are deprecated by OpenAI and will be removed in a future release.
+ * Note that all OpenAI text generation models are deprecated and no longer available to new customers.
  *
  * Refer to example 33 for streaming chat completion.
  */
-// ReSharper disable StringLiteralTypo
 // ReSharper disable once InconsistentNaming
 public static class Example16_CustomLLM
 {
-    private const string LLMResultText = @" ..output from your custom model... Example:
-    AI is awesome because it can help us solve complex problems, enhance our creativity,
-    and improve our lives in many ways. AI can perform tasks that are too difficult,
-    tedious, or dangerous for humans, such as diagnosing diseases, detecting fraud, or
-    exploring space. AI can also augment our abilities and inspire us to create new forms
-    of art, music, or literature. AI can also improve our well-being and happiness by
-    providing personalized recommendations, entertainment, and assistance. AI is awesome";
-
     public static async Task RunAsync()
     {
         await CustomTextGenerationWithSKFunctionAsync();
-
         await CustomTextGenerationAsync();
         await CustomTextGenerationStreamAsync();
     }
 
     private static async Task CustomTextGenerationWithSKFunctionAsync()
     {
-        Console.WriteLine("======== Custom LLM - Text Completion - SKFunction ========");
+        Console.WriteLine("\n======== Custom LLM - Text Completion - SKFunction ========");
 
         IKernelBuilder builder = Kernel.CreateBuilder();
-        builder.Services.AddSingleton(ConsoleLogger.LoggerFactory);
         // Add your text generation service as a singleton instance
         builder.Services.AddKeyedSingleton<ITextGenerationService>("myService1", new MyTextGenerationService());
         // Add your text generation service as a factory method
         builder.Services.AddKeyedSingleton<ITextGenerationService>("myService2", (_, _) => new MyTextGenerationService());
         Kernel kernel = builder.Build();
 
-        const string FunctionDefinition = "Does the text contain grammar errors (Y/N)? Text: {{$input}}";
+        const string FunctionDefinition = "Write one paragraph on {{$input}}";
+        var paragraphWritingFunction = kernel.CreateFunctionFromPrompt(FunctionDefinition);
 
-        var textValidationFunction = kernel.CreateFunctionFromPrompt(FunctionDefinition);
+        const string Input = "Why AI is awesome";
+        Console.WriteLine($"Function input: {Input}\n");
+        var result = await paragraphWritingFunction.InvokeAsync(kernel, new(Input));
 
-        var result = await textValidationFunction.InvokeAsync(kernel, new("I mised the training session this morning"));
-        Console.WriteLine(result.GetValue<string>());
-
-        // Details of the my custom model response
-        Console.WriteLine(JsonSerializer.Serialize(
-            result.Metadata,
-            new JsonSerializerOptions() { WriteIndented = true }
-        ));
+        Console.WriteLine(result);
     }
 
     private static async Task CustomTextGenerationAsync()
     {
-        Console.WriteLine("======== Custom LLM  - Text Completion - Raw ========");
+        Console.WriteLine("\n======== Custom LLM  - Text Completion - Raw ========");
+
+        const string Prompt = "Write one paragraph on why AI is awesome.";
         var completionService = new MyTextGenerationService();
 
-        var result = await completionService.GetTextContentAsync("I missed the training session this morning");
+        Console.WriteLine($"Prompt: {Prompt}\n");
+        var result = await completionService.GetTextContentAsync(Prompt);
 
         Console.WriteLine(result);
     }
 
     private static async Task CustomTextGenerationStreamAsync()
     {
-        Console.WriteLine("======== Custom LLM  - Text Completion - Raw Streaming ========");
+        Console.WriteLine("\n======== Custom LLM  - Text Completion - Raw Streaming ========");
 
-        Kernel kernel = new();
-        ITextGenerationService textGeneration = new MyTextGenerationService();
+        const string Prompt = "Write one paragraph on why AI is awesome.";
+        var completionService = new MyTextGenerationService();
 
-        var prompt = "Write one paragraph why AI is awesome";
-        await TextGenerationStreamAsync(prompt, textGeneration);
-    }
-
-    private static async Task TextGenerationStreamAsync(string prompt, ITextGenerationService textGeneration)
-    {
-        var executionSettings = new OpenAIPromptExecutionSettings()
-        {
-            MaxTokens = 100,
-            FrequencyPenalty = 0,
-            PresencePenalty = 0,
-            Temperature = 1,
-            TopP = 0.5
-        };
-
-        Console.WriteLine("Prompt: " + prompt);
-        await foreach (var message in textGeneration.GetStreamingTextContentsAsync(prompt, executionSettings))
+        Console.WriteLine($"Prompt: {Prompt}\n");
+        await foreach (var message in completionService.GetStreamingTextContentsAsync(Prompt))
         {
             Console.Write(message);
         }
@@ -114,19 +84,29 @@ public static class Example16_CustomLLM
         Console.WriteLine();
     }
 
+    /// <summary>
+    /// Text generation service stub.
+    /// </summary>
     private sealed class MyTextGenerationService : ITextGenerationService
     {
-        public string? ModelId { get; private set; }
+        private const string LLMResultText = @"...output from your custom model... Example:
+AI is awesome because it can help us solve complex problems, enhance our creativity,
+and improve our lives in many ways. AI can perform tasks that are too difficult,
+tedious, or dangerous for humans, such as diagnosing diseases, detecting fraud, or
+exploring space. AI can also augment our abilities and inspire us to create new forms
+of art, music, or literature. AI can also improve our well-being and happiness by
+providing personalized recommendations, entertainment, and assistance. AI is awesome.";
 
         public IReadOnlyDictionary<string, object?> Attributes => new Dictionary<string, object?>();
 
         public async IAsyncEnumerable<StreamingTextContent> GetStreamingTextContentsAsync(string prompt, PromptExecutionSettings? executionSettings = null, Kernel? kernel = null, [EnumeratorCancellation] CancellationToken cancellationToken = default)
         {
-            foreach (string word in LLMResultText.Split(' '))
+            foreach (string word in LLMResultText.Split(' ', StringSplitOptions.RemoveEmptyEntries))
             {
                 await Task.Delay(50, cancellationToken);
                 cancellationToken.ThrowIfCancellationRequested();
-                yield return new MyStreamingContent($"{word} ");
+
+                yield return new StreamingTextContent($"{word} ");
             }
         }
 
@@ -136,23 +116,6 @@ public static class Example16_CustomLLM
             {
                 new(LLMResultText)
             });
-        }
-    }
-
-    private sealed class MyStreamingContent : StreamingTextContent
-    {
-        public MyStreamingContent(string content) : base(content)
-        {
-        }
-
-        public override byte[] ToByteArray()
-        {
-            return Encoding.UTF8.GetBytes(this.Text ?? string.Empty);
-        }
-
-        public override string ToString()
-        {
-            return this.Text ?? string.Empty;
         }
     }
 }
