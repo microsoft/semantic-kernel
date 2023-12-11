912a21a7f57d2f3a8470ee5d7dabeb6f1e79c1a6
Mark Wallace <127216156+markwallace-microsoft@users.noreply.github.com>
2023-12-11 15:30:26 +0000

    .Net: Enhance Service Selector to use ExecutionSettings from Invoke Async Calls (#4141)
    ### Motivation and Context
    
    Closes #4127
    
    ### Description
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [ ] The code builds clean without any errors or warnings
    - [ ] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [ ] All unit tests pass, and I have added new tests where possible
    - [ ] I didn't break anyone :smile:

--- dotnet/src/SemanticKernel.Abstractions/Services/OrderedAIServiceSelector.cs
+++ dotnet/src/SemanticKernel.Abstractions/Services/OrderedAIServiceSelector.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using Microsoft.Extensions.DependencyInjection;
@@ -20,7 +21,10 @@ internal sealed class OrderedAIServiceSelector : IAIServiceSelector
         [NotNullWhen(true)] out T? service,
         out PromptExecutionSettings? serviceSettings) where T : class, IAIService
     {
-        var executionSettings = function.ExecutionSettings;
+        // Allow the execution settings from the kernel arguments to take precedence
+        var executionSettings = arguments.ExecutionSettings is not null
+             ? new List<PromptExecutionSettings> { arguments.ExecutionSettings }
+             : function.ExecutionSettings;
         if (executionSettings is null || executionSettings.Count == 0)
         {
             service = GetAnyService(kernel);
