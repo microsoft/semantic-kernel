a977f0f1d5e8683ca946ecdf351a232183c50b57
Sarah Widder <sawidder@microsoft.com>
2023-12-12 13:11:47 -0800

    Skip integration test for now.

--- python/tests/integration/completions/test_azure_oai_chat_service_extensions.py
+++ python/tests/integration/completions/test_azure_oai_chat_service_extensions.py
@@ -126,6 +126,7 @@ async def create_with_data_chat_function(
 
 
 @pytest.mark.asyncio
+@pytest.mark.skip
 async def test_azure_e2e_chat_completion_with_extensions(
     create_with_data_chat_function,
 ):
