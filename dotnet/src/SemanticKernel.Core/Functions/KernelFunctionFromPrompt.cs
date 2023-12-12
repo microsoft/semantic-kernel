4d0107984d39548d24fbba319ac3755948df6f9c
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2023-12-12 09:07:48 +0000

    Warning Fix

--- dotnet/src/SemanticKernel.Core/Functions/KernelFunctionFromPrompt.cs
+++ dotnet/src/SemanticKernel.Core/Functions/KernelFunctionFromPrompt.cs
@@ -3,7 +3,6 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
 using System.Diagnostics.Metrics;
 using System.Runtime.CompilerServices;
 using System.Text.Json;
