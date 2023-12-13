29a78eb21a0176b5c5e408560e662d5b1a6914b7
markwallace-microsoft <mark.e.wallace@outlook.com>
2023-12-13 14:19:55 +0000

    Merge latest from main and all tests pass

--- dotnet/src/SemanticKernel.Core/Functions/KernelFunctionFromPrompt.cs
+++ dotnet/src/SemanticKernel.Core/Functions/KernelFunctionFromPrompt.cs
@@ -66,7 +66,7 @@ internal sealed class KernelFunctionFromPrompt : KernelFunction
 
         if (executionSettings is not null)
         {
-            promptConfig.ExecutionSettings.Add(executionSettings.ServiceId ?? PromptExecutionSettings.DefaultServiceId, executionSettings);
+            promptConfig.ExecutionSettings = executionSettings;
         }
 
         var factory = promptTemplateFactory ?? new KernelPromptTemplateFactory(loggerFactory);
