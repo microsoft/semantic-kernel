79ba1ce56af04e9cf6775c0e88718d4da66e1776
Mark Wallace <127216156+markwallace-microsoft@users.noreply.github.com>
2023-12-14 15:24:20 +0000

    .Net: Update the sample to also demonstrate loading yaml resources (#4270)
    ### Motivation and Context
    
    <!-- Thank you for your contribution to the semantic-kernel repo!
    Please help reviewers and future users, providing the following
    information:
      1. Why is this change required?
      2. What problem does it solve?
      3. What scenario does it contribute to?
      4. If it fixes an open issue, please link to the issue here.
    -->
    
    ### Description
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [ ] The code builds clean without any errors or warnings
    - [ ] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [ ] All unit tests pass, and I have added new tests where possible
    - [ ] I didn't break anyone :smile:

--- dotnet/samples/KernelSyntaxExamples/Getting_Started/Step3_Yaml_Prompt.cs
+++ dotnet/samples/KernelSyntaxExamples/Getting_Started/Step3_Yaml_Prompt.cs
@@ -4,6 +4,7 @@ using System;
 using System.Threading.Tasks;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.PromptTemplates.Handlebars;
+using Resources;
 
 // This example shows how to create a prompt <see cref="KernelFunction"/> from a YAML resource.
 public static class Step3_Yaml_Prompt
@@ -21,7 +22,8 @@ public static class Step3_Yaml_Prompt
             .Build();
 
         // Load prompt from resource
-        var function = kernel.CreateFunctionFromPromptYaml(GenerateStoryYaml);
+        var generateStoryYaml = EmbeddedResource.Read("GenerateStory.yaml");
+        var function = kernel.CreateFunctionFromPromptYaml(generateStoryYaml);
 
         // Invoke the prompt function and display the result
         Console.WriteLine(await kernel.InvokeAsync(function, arguments: new()
@@ -31,7 +33,8 @@ public static class Step3_Yaml_Prompt
             }));
 
         // Load prompt from resource
-        function = kernel.CreateFunctionFromPromptYaml(GenerateStoryHandlebarsYaml, new HandlebarsPromptTemplateFactory());
+        var generateStoryHandlebarsYaml = EmbeddedResource.Read("GenerateStoryHandlebars.yaml");
+        function = kernel.CreateFunctionFromPromptYaml(generateStoryHandlebarsYaml, new HandlebarsPromptTemplateFactory());
 
         // Invoke the prompt function and display the result
         Console.WriteLine(await kernel.InvokeAsync(function, arguments: new()
@@ -40,46 +43,4 @@ public static class Step3_Yaml_Prompt
                 { "length", "3" },
             }));
     }
-
-    private const string GenerateStoryYaml = @"
-name: GenerateStory
-template: |
-  Tell a story about {{$topic}} that is {{$length}} sentences long.
-template_format: semantic-kernel
-description: A function that generates a story about a topic.
-input_variables:
-  - name: topic
-    description: The topic of the story.
-    is_required: true
-  - name: length
-    description: The number of sentences in the story.
-    is_required: true
-output_variable:
-  description: The generated story.
-execution_settings:
-  - temperature: 0.6
-";
-
-    private const string GenerateStoryHandlebarsYaml = @"
-name: GenerateStory
-template: |
-  Tell a story about {{topic}} that is {{length}} sentences long.
-template_format: handlebars
-description: A function that generates a story about a topic.
-input_variables:
-  - name: topic
-    description: The topic of the story.
-    is_required: true
-  - name: length
-    description: The number of sentences in the story.
-    is_required: true
-output_variable:
-  description: The generated story.
-execution_settings:
-  - model_id: gpt-4
-    temperature: 0.6
-  - model_id: gpt-3.5-turbo
-    temperature: 0.4  
-  - temperature: 0.5
-";
 }
