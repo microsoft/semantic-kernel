d06143cd2ada12e96f0d15e0bf19ea99e59fc92d
Ben Thomas <bentho@microsoft.com>
2023-12-14 15:52:35 -0800

    Adding new projects for Experimental.Agents and Experimental.Agents.UnitTests.

---
+++ dotnet/src/Experimental/Agents/Models/AssistantConfigurationModel.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+#pragma warning disable CA1812
+
+using YamlDotNet.Serialization;
+
+namespace Microsoft.SemanticKernel.Experimental.Agents.Models;
+
+/// <summary>
+/// Represents a yaml configuration file for an assistant.
+/// </summary>
+internal sealed class AssistantConfigurationModel
+{
+    /// <summary>
+    /// The assistant name
+    /// </summary>
+    [YamlMember(Alias = "name")]
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The assistant description
+    /// </summary>
+    [YamlMember(Alias = "description")]
+    public string Description { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The assistant instructions template
+    /// </summary>
+    [YamlMember(Alias = "instructions")]
+    public string Instructions { get; set; } = string.Empty;
+}
