7e6e9b54b07121b5f0ede54cea78133f17e72e7a
Mark Wallace <127216156+markwallace-microsoft@users.noreply.github.com>
2023-12-15 20:18:20 +0000

    .Net: Integration tests for OpenAI Tools (#4309)
    ### Motivation and Context
    
    <!-- Thank you for your contribution to the semantic-kernel repo!
    Please help reviewers and future users, providing the following
    information:
      1. Why is this change required?
      2. What problem does it solve?
      3. What scenario does it contribute to?
      4. If it fixes an open issue, please link to the issue here.
    -->
    
    ### Description
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [ ] The code builds clean without any errors or warnings
    - [ ] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [ ] All unit tests pass, and I have added new tests where possible
    - [ ] I didn't break anyone :smile:

---
+++ dotnet/src/IntegrationTests/Connectors/OpenAI/OpenAIToolsTests.cs
@@ -0,0 +1,100 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using SemanticKernel.IntegrationTests.Planners.Stepwise;
+using SemanticKernel.IntegrationTests.TestSettings;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SemanticKernel.IntegrationTests.Connectors.OpenAI;
+
+public sealed class OpenAIToolsTests : IDisposable
+{
+    public OpenAIToolsTests(ITestOutputHelper output)
+    {
+        this._testOutputHelper = new RedirectOutput(output);
+
+        // Load configuration
+        this._configuration = new ConfigurationBuilder()
+            .AddJsonFile(path: "testsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile(path: "testsettings.development.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .AddUserSecrets<FunctionCallingStepwisePlannerTests>()
+            .Build();
+    }
+
+    [Fact]
+    public async Task CanAutoInvokeKernelFunctionsAsync()
+    {
+        // Arrange
+        Kernel kernel = this.InitializeKernel();
+        kernel.ImportPluginFromType<TimeInformation>();
+
+        var invokedFunctions = new List<string>();
+        void MyInvokingHandler(object? sender, FunctionInvokingEventArgs e)
+        {
+            invokedFunctions.Add(e.Function.Name);
+        }
+        kernel.FunctionInvoking += MyInvokingHandler;
+
+        // Act
+        OpenAIPromptExecutionSettings settings = new() { ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions };
+        var result = await kernel.InvokePromptAsync("How many days until Christmas? Explain your thinking.", new(settings));
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Contains("GetCurrentUtcTime", invokedFunctions);
+    }
+
+    private Kernel InitializeKernel()
+    {
+        AzureOpenAIConfiguration? azureOpenAIConfiguration = this._configuration.GetSection("Planners:AzureOpenAI").Get<AzureOpenAIConfiguration>();
+        Assert.NotNull(azureOpenAIConfiguration);
+
+        IKernelBuilder builder = Kernel.CreateBuilder()
+            .AddAzureOpenAIChatCompletion(
+                deploymentName: azureOpenAIConfiguration.ChatDeploymentName!,
+                endpoint: azureOpenAIConfiguration.Endpoint,
+                apiKey: azureOpenAIConfiguration.ApiKey);
+
+        var kernel = builder.Build();
+
+        return kernel;
+    }
+
+    private readonly RedirectOutput _testOutputHelper;
+    private readonly IConfigurationRoot _configuration;
+
+    public void Dispose()
+    {
+        this.Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    ~OpenAIToolsTests()
+    {
+        this.Dispose(false);
+    }
+
+    private void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            this._testOutputHelper.Dispose();
+        }
+    }
+
+    /// <summary>
+    /// A plugin that returns the current time.
+    /// </summary>
+    public class TimeInformation
+    {
+        [KernelFunction]
+        public string GetCurrentUtcTime() => DateTime.UtcNow.ToString("R");
+    }
+}
