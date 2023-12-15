71d4cf470261d945c01302662ff0d5ccf1f2d74f
Dmytro Struk <13853051+dmytrostruk@users.noreply.github.com>
2023-12-15 15:03:45 +0000

    Moved HttpOperationException to root namespace

--- dotnet/src/Connectors/Connectors.UnitTests/Memory/Qdrant/QdrantMemoryStoreTests.cs
+++ dotnet/src/Connectors/Connectors.UnitTests/Memory/Qdrant/QdrantMemoryStoreTests.cs
@@ -7,7 +7,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Microsoft.SemanticKernel.Connectors.Qdrant;
-using Microsoft.SemanticKernel.Http;
+using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Memory;
 using Moq;
 using Xunit;
