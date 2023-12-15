c991521100c1e508e87c739176030d5178ad235c
Mark Wallace <127216156+markwallace-microsoft@users.noreply.github.com>
2023-12-15 16:22:30 +0000

    .Net: Add getting started sample showing how to observe the Kernel (#4305)
    ### Motivation and Context
    
    Add getting started sample showing how to observe the Kernel
    
    ### Description
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [ ] The code builds clean without any errors or warnings
    - [ ] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [ ] All unit tests pass, and I have added new tests where possible
    - [ ] I didn't break anyone :smile:

---
+++ dotnet/samples/KernelSyntaxExamples/Getting_Started/Step7_Observability.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using RepoUtils;
+
+public static class Step7_Observability
+{
+    /// <summary>
+    /// Shows different ways observe the execution of a <see cref="KernelPlugin"/> instances.
+    /// </summary>
+    public static async Task RunAsync()
+    {
+        // Create a kernel with OpenAI chat completion
+        IKernelBuilder kernelBuilder = Kernel.CreateBuilder();
+        kernelBuilder.AddOpenAIChatCompletion(
+                modelId: TestConfiguration.OpenAI.ChatModelId,
+                apiKey: TestConfiguration.OpenAI.ApiKey);
+        kernelBuilder.Plugins.AddFromType<TimeInformation>();
+        Kernel kernel = kernelBuilder.Build();
+
+        // Handler which is called before a function is invoked
+        void MyInvokingHandler(object? sender, FunctionInvokingEventArgs e)
+        {
+            Console.WriteLine($"Invoking {e.Function.Name}");
+        }
+
+        // Handler which is called after a prompt is rendered
+        void MyRenderedHandler(object? sender, PromptRenderedEventArgs e)
+        {
+            Console.WriteLine($"Prompt sent to model: {e.RenderedPrompt}");
+        }
+
+        // Handler which is called after a function is invoked
+        void MyInvokedHandler(object? sender, FunctionInvokedEventArgs e)
+        {
+            if (e.Result.Metadata is not null && e.Result.Metadata.ContainsKey("Usage"))
+            {
+                Console.WriteLine($"Token usage: {e.Result.Metadata?["Usage"]?.AsJson()}");
+            }
+        }
+
+        // Add the handlers to the kernel
+        kernel.FunctionInvoking += MyInvokingHandler;
+        kernel.PromptRendered += MyRenderedHandler;
+        kernel.FunctionInvoked += MyInvokedHandler;
+
+        // Invoke the kernel with a prompt and allow the AI to automatically invoke functions
+        OpenAIPromptExecutionSettings settings = new() { ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions };
+        Console.WriteLine(await kernel.InvokePromptAsync("How many days until Christmas? Explain your thinking.", new(settings)));
+    }
+
+    /// <summary>
+    /// A plugin that returns the current time.
+    /// </summary>
+    public class TimeInformation
+    {
+        [KernelFunction]
+        public string GetCurrentUtcTime() => DateTime.UtcNow.ToString("R");
+    }
+}
