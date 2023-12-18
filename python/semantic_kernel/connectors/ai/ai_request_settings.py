1606d0015b2827a455d964126a5ef4d5d247a723
eavanvalkenburg <edvan@microsoft.com>
2023-12-18 15:01:11 +0100

    updated docstring of AIRequestSettings

--- python/semantic_kernel/connectors/ai/ai_request_settings.py
+++ python/semantic_kernel/connectors/ai/ai_request_settings.py
@@ -6,6 +6,21 @@ from semantic_kernel.sk_pydantic import SKBaseModel
 
 
 class AIRequestSettings(SKBaseModel):
+    """Base class for AI request settings.
+
+    Can be used by itself or as a base class for other request settings. The methods are used to create specific request settings objects based on the keys in the extension_data field, this way you can create a generic AIRequestSettings object in your application, which get's mapped into the keys of the request settings that each services returns by using the service.get_request_settings() method.
+
+    Parameters:
+        service_id (str, optional): The service ID to use for the request. Defaults to None.
+        extension_data (Dict[str, Any], optional): Any additional data to send with the request. Defaults to None.
+        kwargs (Any): Additional keyword arguments,
+            these are attempted to parse into the keys of the specific request settings.
+    Methods:
+        prepare_settings_dict: Prepares the settings as a dictionary for sending to the AI service.
+        update_from_ai_request_settings: Update the keys from another request settings object.
+        from_ai_request_settings: Create a request settings from another request settings object.
+    """  # noqa: E501
+
     service_id: Optional[str] = Field(None, min_length=1)
     extension_data: Dict[str, Any] = Field(default_factory=dict)
 
@@ -19,6 +34,11 @@ class AIRequestSettings(SKBaseModel):
             if key in self.keys:
                 setattr(self, key, value)
 
+    @property
+    def keys(self):
+        """Get the keys of the request settings."""
+        return self.model_fields.keys()
+
     def prepare_settings_dict(self, **kwargs) -> Dict[str, Any]:
         return self.model_dump(
             exclude={"service_id", "extension_data"},
@@ -26,18 +46,16 @@ class AIRequestSettings(SKBaseModel):
             by_alias=True,
         )
 
-    @property
-    def keys(self):
-        """Get the keys of the request settings."""
-        return self.model_fields.keys()
-
-    def update_from_completion_config(self, config: "AIRequestSettings") -> None:
+    def update_from_ai_request_settings(self, config: "AIRequestSettings") -> None:
         """Update the request settings from a completion config."""
         self.service_id = config.service_id
         self.extension_data.update(config.extension_data)
+        for key in self.keys:
+            if hasattr(config, key):
+                setattr(self, key, getattr(config, key))
 
     @classmethod
-    def from_completion_config(cls, config: "AIRequestSettings") -> "AIRequestSettings":
+    def from_ai_request_settings(cls, config: "AIRequestSettings") -> "AIRequestSettings":
         """Create a request settings from a completion config."""
         return cls(
             service_id=config.service_id,
