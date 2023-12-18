6b63f64ce4522b1a42e55d56966a5008d7daf9ac
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2023-12-18 21:30:33 +0000

    .Net: BugFix Serializing and Deserializing ChatHistory with ToolCalling details Update OpenAI Connector to use FunctionToolCallsProperty and add ChatHistoryTests (#4358)
    Serializing the ToolCalls was losing the details about Name and
    Arguments, and serializing this lack of content back was rendering
    ToolCalls as an invalid history message and giving the Issue Error.
    
    Resolves #4336 
    
    Next PR, moving the Integration Tests as Unit Tests
    ## Summary
    This pull request updates the OpenAI Connector to use the
    FunctionToolCallsProperty metadata key instead of the ToolCallsProperty
    metadata key. This change allows for the use of a list of
    ChatCompletionsFunctionToolCall objects instead of
    ChatCompletionsToolCall objects. Additionally, a new integration test,
    ChatHistoryTests.cs, is added to the IntegrationTests/Connectors/OpenAI
    directory. The test file contains tests for the ChatHistory feature of
    the OpenAI Connector, ensuring that the feature returns the expected
    results and handles errors correctly.
    
    ## Changes
    - Update ClientCore.cs to use FunctionToolCallsProperty instead of
    ToolCallsProperty
    - Update OpenAIChatMessageContent.cs to use FunctionToolCallsProperty
    instead of ToolCallsProperty
    - Add ChatHistoryTests.cs to IntegrationTests/Connectors/OpenAI
    directory
    - Implement tests for ChatHistory feature of OpenAI Connector
    - Dispose of logger and test output helper in ChatHistoryTests.cs
    
    ---
    *Powered by [Microsoft Semantic
    Kernel](https://github.com/microsoft/semantic-kernel)*

--- dotnet/src/Connectors/Connectors.OpenAI/AzureSdk/OpenAIChatMessageContent.cs
+++ dotnet/src/Connectors/Connectors.OpenAI/AzureSdk/OpenAIChatMessageContent.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Azure.AI.OpenAI;
 using Microsoft.SemanticKernel.ChatCompletion;
 
@@ -18,9 +19,9 @@ public sealed class OpenAIChatMessageContent : ChatMessageContent
     public static string ToolIdProperty => $"{nameof(ChatCompletionsToolCall)}.{nameof(ChatCompletionsToolCall.Id)}";
 
     /// <summary>
-    /// Gets the metadata key for the <see cref="ChatCompletionsToolCall.Id"/> name property.
+    /// Gets the metadata key for the list of <see cref="ChatCompletionsFunctionToolCall"/>.
     /// </summary>
-    public static string ToolCallsProperty => $"{nameof(ChatResponseMessage)}.{nameof(ChatResponseMessage.ToolCalls)}";
+    internal static string FunctionToolCallsProperty => $"{nameof(ChatResponseMessage)}.FunctionToolCalls";
 
     /// <summary>
     /// Initializes a new instance of the <see cref="OpenAIChatMessageContent"/> class.
@@ -110,7 +111,7 @@ public sealed class OpenAIChatMessageContent : ChatMessageContent
             }
 
             // Add the additional entry.
-            newDictionary.Add(ToolCallsProperty, toolCalls);
+            newDictionary.Add(FunctionToolCallsProperty, toolCalls.OfType<ChatCompletionsFunctionToolCall>().ToList());
 
             return newDictionary;
         }
