f06ffdb79a5f391439a1c12084a8b198b89681ce
david-polo <davidsilva90131@gmail.com>
2023-12-18 11:36:04 -0500

    loop question/answer

--- python/semantic_kernel/connectors/memory/astradb/ai_sample/sample.py
+++ python/semantic_kernel/connectors/memory/astradb/ai_sample/sample.py
@@ -4,15 +4,24 @@ from semantic_kernel.connectors.memory.astradb.ai_sample.utils import kernel, CO
 
 
 def openai_search():
-    question = "As a friendly AI Copilot, answer the question: Did Albert Einstein like coffee?"
-    prompt = kernel.create_semantic_function(question)
-    print(f"Question: {question}\nAnswer: {prompt()}")
+    while True:
+        question = input("Question (type 'exit' to quit): ")
+        if question.lower() == 'exit':
+            print("Thanks!")
+            break
+
+        prompt = kernel.create_semantic_function(question)
+        print(f"Answer: {prompt()}")
 
 
 async def memory_search():
-    question = "What happens if there a role has access to the keyspace/table level and I grant row access?"
-    result = await kernel.memory.search_async(COLLECTION_NAME, question)
-    print(f"Question: {question}")
-    print(f"Retrieved document: {result[0].text}, {result[0].relevance}")
+    while True:
+        question = input("Question (type 'exit' to quit): ")
+        if question.lower() == 'exit':
+            print("Thanks!")
+            break
+
+        result = await kernel.memory.search_async(COLLECTION_NAME, question)
+        print(f"Retrieved document: {result[0].text}, {result[0].relevance}")
 
 asyncio.run(memory_search())
