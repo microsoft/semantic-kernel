ce9ff8a54e922ae8dfd3e2885fc10844a606dcf4
Krzysztof Kasprowicz <krzysztof@impe.pl>
2023-12-23 17:20:01 +0100

    Add Gemini integration test and settings
    The integration tests for Gemini connector have been added. Additionally, Gemini setting fields have been inserted into "testsettings.json".

---
+++ dotnet/src/IntegrationTests/Connectors/Gemini/TextGeneration/GeminiTextGenerationTests.cs
@@ -0,0 +1,42 @@
+﻿#region HEADER
+// Copyright (c) Microsoft. All rights reserved.
+#endregion
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel.Connectors.Gemini;
+using Microsoft.SemanticKernel.TextGeneration;
+using Xunit;
+
+namespace SemanticKernel.IntegrationTests.Connectors.Gemini.TextGeneration;
+
+public class GeminiTextGenerationTests
+{
+    private readonly IConfigurationRoot _configuration = new ConfigurationBuilder()
+        .AddJsonFile(path: "testsettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile(path: "testsettings.development.json", optional: true, reloadOnChange: true)
+        .AddEnvironmentVariables()
+        .Build();
+
+    // Load configuration
+    //[Fact(Skip = "This test is for manual verification.")]
+    [Fact]
+    public async Task GeminiTextGenerationAsync()
+    {
+        // Arrange
+        const string Input = "This is test";
+
+        var geminiService = new GeminiTextGenerationService(this.GetModel(), this.GetApiKey());
+
+        // Act
+        var response = await geminiService.GetTextContentAsync(Input);
+
+        // Assert
+        Assert.NotNull(response.Text);
+        Assert.StartsWith(Input, response.Text, StringComparison.Ordinal);
+    }
+
+    private string GetModel() => this._configuration.GetSection("Gemini:ModelId").Get<string>()!;
+    private string GetApiKey() => this._configuration.GetSection("Gemini:ApiKey").Get<string>()!;
+}
