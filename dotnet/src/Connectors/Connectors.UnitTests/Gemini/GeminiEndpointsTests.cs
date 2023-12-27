43bec88e2f7a3f15bbec1e824a990ef3bc457dc6
Krzysztof Kasprowicz <krzysztof@impe.pl>
2023-12-27 22:23:18 +0100

    Add GeminiEndpoints unit tests

---
+++ dotnet/src/Connectors/Connectors.UnitTests/Gemini/GeminiEndpointsTests.cs
@@ -0,0 +1,55 @@
+﻿#region HEADER
+
+// Copyright (c) Microsoft. All rights reserved.
+
+#endregion
+
+using System;
+using Microsoft.SemanticKernel.Connectors.Gemini;
+using Xunit;
+
+namespace SemanticKernel.Connectors.UnitTests.Gemini;
+
+public class GeminiEndpointsTests
+{
+    [Fact]
+    public void ModelsEndpointStartsWithBaseEndpoint()
+    {
+        Assert.StartsWith(
+            GeminiEndpoints.BaseEndpoint.AbsoluteUri,
+            GeminiEndpoints.ModelsEndpoint.AbsoluteUri,
+            StringComparison.Ordinal);
+    }
+
+    [Fact]
+    public void GetGenerateContentEndpointContainsModelsBaseAndModelAndApiKey()
+    {
+        // Arrange
+        string modelId = "fake-modelId";
+        string apiKey = "fake-apiKey";
+
+        // Act
+        Uri uri = GeminiEndpoints.GetGenerateContentEndpoint(modelId, apiKey);
+
+        // Assert
+        Assert.Contains(GeminiEndpoints.ModelsEndpoint.AbsoluteUri, uri.AbsoluteUri, StringComparison.Ordinal);
+        Assert.Contains(modelId, uri.AbsoluteUri, StringComparison.Ordinal);
+        Assert.Contains(apiKey, uri.AbsoluteUri, StringComparison.Ordinal);
+    }
+
+    [Fact]
+    public void GetStreamGenerateContentEndpointContainsModelsBaseAndModelAndApiKey()
+    {
+        // Arrange
+        string modelId = "fake-modelId";
+        string apiKey = "fake-apiKey";
+
+        // Act
+        Uri uri = GeminiEndpoints.GetStreamGenerateContentEndpoint(modelId, apiKey);
+
+        // Assert
+        Assert.Contains(GeminiEndpoints.ModelsEndpoint.AbsoluteUri, uri.AbsoluteUri, StringComparison.Ordinal);
+        Assert.Contains(modelId, uri.AbsoluteUri, StringComparison.Ordinal);
+        Assert.Contains(apiKey, uri.AbsoluteUri, StringComparison.Ordinal);
+    }
+}
