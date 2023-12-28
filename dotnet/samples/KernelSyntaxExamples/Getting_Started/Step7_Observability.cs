6084e4ec655b88dc55f425fd602015cc597006c5
Dmytro Struk <13853051+dmytrostruk@users.noreply.github.com>
2023-12-28 13:09:25 +0100

    Added more examples

--- dotnet/samples/KernelSyntaxExamples/Getting_Started/Step7_Observability.cs
+++ dotnet/samples/KernelSyntaxExamples/Getting_Started/Step7_Observability.cs
@@ -3,6 +3,7 @@
 using System;
 using System.ComponentModel;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
 using RepoUtils;
@@ -14,18 +15,38 @@ public static class Step7_Observability
     /// </summary>
     public static async Task RunAsync()
     {
+        await ObservabilityWithHooksAsync();
+
+        await ObservabilityWithFiltersAsync();
+    }
+
+    /// <summary>
+    /// Shows how to observe the execution of a <see cref="KernelPlugin"/> instance with hooks.
+    /// </summary>
+    private static async Task ObservabilityWithHooksAsync()
+    {
+        Console.WriteLine("\n======== Observability with Hooks ========\n");
+
         // Create a kernel with OpenAI chat completion
         IKernelBuilder kernelBuilder = Kernel.CreateBuilder();
         kernelBuilder.AddOpenAIChatCompletion(
                 modelId: TestConfiguration.OpenAI.ChatModelId,
                 apiKey: TestConfiguration.OpenAI.ApiKey);
+
         kernelBuilder.Plugins.AddFromType<TimeInformation>();
+
         Kernel kernel = kernelBuilder.Build();
 
         // Handler which is called before a function is invoked
         void MyInvokingHandler(object? sender, FunctionInvokingEventArgs e)
         {
             Console.WriteLine($"Invoking {e.Function.Name}");
+        }
+
+        // Handler which is called before a prompt is rendered
+        void MyRenderingHandler(object? sender, PromptRenderingEventArgs e)
+        {
+            Console.WriteLine($"Rendering prompt for {e.Function.Name}");
         }
 
         // Handler which is called after a prompt is rendered
@@ -45,6 +66,7 @@ public static class Step7_Observability
 
         // Add the handlers to the kernel
         kernel.FunctionInvoking += MyInvokingHandler;
+        kernel.PromptRendering += MyRenderingHandler;
         kernel.PromptRendered += MyRenderedHandler;
         kernel.FunctionInvoked += MyInvokedHandler;
 
@@ -54,12 +76,74 @@ public static class Step7_Observability
     }
 
     /// <summary>
+    /// Shows how to observe the execution of a <see cref="KernelPlugin"/> instance with filters.
+    /// </summary>
+    private static async Task ObservabilityWithFiltersAsync()
+    {
+        Console.WriteLine("\n======== Observability with Filters ========\n");
+
+        // Create a kernel with OpenAI chat completion
+        IKernelBuilder kernelBuilder = Kernel.CreateBuilder();
+        kernelBuilder.AddOpenAIChatCompletion(
+                modelId: TestConfiguration.OpenAI.ChatModelId,
+                apiKey: TestConfiguration.OpenAI.ApiKey);
+
+        kernelBuilder.Plugins.AddFromType<TimeInformation>();
+
+        kernelBuilder.Services.AddSingleton<IFunctionFilter, MyFunctionFilter>();
+        kernelBuilder.Services.AddSingleton<IPromptFilter, MyPromptFilter>();
+
+        Kernel kernel = kernelBuilder.Build();
+
+        // Invoke the kernel with a prompt and allow the AI to automatically invoke functions
+        OpenAIPromptExecutionSettings settings = new() { ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions };
+        Console.WriteLine(await kernel.InvokePromptAsync("How many days until Christmas? Explain your thinking.", new(settings)));
+    }
+
+    /// <summary>
     /// A plugin that returns the current time.
     /// </summary>
-    public class TimeInformation
+    private class TimeInformation
     {
         [KernelFunction]
         [Description("Retrieves the current time in UTC.")]
         public string GetCurrentUtcTime() => DateTime.UtcNow.ToString("R");
+    }
+
+    /// <summary>
+    /// Function filter for observability.
+    /// </summary>
+    private class MyFunctionFilter : IFunctionFilter
+    {
+        public void OnFunctionInvoked(FunctionInvokedContext context)
+        {
+            var metadata = context.Result.Metadata;
+
+            if (metadata is not null && metadata.ContainsKey("Usage"))
+            {
+                Console.WriteLine($"Token usage: {metadata["Usage"]?.AsJson()}");
+            }
+        }
+
+        public void OnFunctionInvoking(FunctionInvokingContext context)
+        {
+            Console.WriteLine($"Invoking {context.Function.Name}");
+        }
+    }
+
+    /// <summary>
+    /// Prompt filter for observability.
+    /// </summary>
+    private class MyPromptFilter : IPromptFilter
+    {
+        public void OnPromptRendered(PromptRenderedContext context)
+        {
+            Console.WriteLine($"Prompt sent to model: {context.RenderedPrompt}");
+        }
+
+        public void OnPromptRendering(PromptRenderingContext context)
+        {
+            Console.WriteLine($"Rendering prompt for {context.Function.Name}");
+        }
     }
 }
