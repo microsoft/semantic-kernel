fe3de488ad731d193e939e5b6c42920db8c0b211
Krzysztof Kasprowicz <krzysztof@impe.pl>
2023-12-28 17:42:03 +0100

    This commit introduces extensions to add the Gemini Text Generation service to the kernel builder and the service collection.

---
+++ dotnet/src/Connectors/Connectors.Gemini/GeminiKernelBuilderExtensions.cs
@@ -0,0 +1,66 @@
+﻿#region HEADER
+
+// Copyright (c) Microsoft. All rights reserved.
+
+#endregion
+
+using System.Net.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.SemanticKernel.Connectors.Gemini;
+using Microsoft.SemanticKernel.Http;
+using Microsoft.SemanticKernel.TextGeneration;
+
+namespace Microsoft.SemanticKernel;
+
+/// <summary>
+/// Extensions for adding Gemini text generation services to the application.
+/// </summary>
+public static class GeminiKernelBuilderExtensions
+{
+    /// <summary>
+    /// Adds Gemini Text Generation service to the kernel builder.
+    /// </summary>
+    /// <param name="builder">The kernel builder.</param>
+    /// <param name="model">The model for text generation.</param>
+    /// <param name="apiKey">The API key for authentication Gemini API.</param>
+    /// <param name="serviceId">The optional service ID.</param>
+    /// <param name="httpClient">The optional custom HttpClient.</param>
+    /// <returns>The updated kernel builder.</returns>
+    public static IKernelBuilder AddGeminiTextGeneration(
+        this IKernelBuilder builder,
+        string model,
+        string apiKey,
+        string? serviceId = null,
+        HttpClient? httpClient = null)
+    {
+        Verify.NotNull(builder);
+        Verify.NotNull(model);
+        Verify.NotNull(apiKey);
+
+        builder.Services.AddKeyedSingleton<ITextGenerationService>(serviceId, (serviceProvider, _) =>
+            new GeminiTextGenerationService(model, apiKey, HttpClientProvider.GetHttpClient(httpClient, serviceProvider)));
+        return builder;
+    }
+
+    /// <summary>
+    /// Adds Gemini Text Generation service to the specified service collection.
+    /// </summary>
+    /// <param name="services">The service collection to add the Gemini Text Generation service to.</param>
+    /// <param name="model">The model for text generation.</param>
+    /// <param name="apiKey">The API key for authentication Gemini API.</param>
+    /// <param name="serviceId">Optional service ID.</param>
+    /// <returns>The updated service collection.</returns>
+    public static IServiceCollection AddGeminiTextGeneration(
+        this IServiceCollection services,
+        string model,
+        string apiKey,
+        string? serviceId = null)
+    {
+        Verify.NotNull(services);
+        Verify.NotNull(model);
+        Verify.NotNull(apiKey);
+
+        return services.AddKeyedSingleton<ITextGenerationService>(serviceId, (serviceProvider, _) =>
+            new GeminiTextGenerationService(model, apiKey, HttpClientProvider.GetHttpClient(serviceProvider)));
+    }
+}
