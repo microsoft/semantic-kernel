c89e33e0da31a10a63d97b81efdcacc96c75e8a9
Krzysztof Kasprowicz <krzysztof@impe.pl>
2023-12-31 16:47:06 +0100

    Added detailed documentation to GeminiClient's methods
    Enhanced the readability of the GeminiClient class by providing detailed xml documentation to the GenerateChatMessageAsync and StreamGenerateChatMessageAsync methods.

--- dotnet/src/Connectors/Connectors.Gemini/Core/GeminiClient.cs
+++ dotnet/src/Connectors/Connectors.Gemini/Core/GeminiClient.cs
@@ -100,6 +100,13 @@ internal sealed class GeminiClient
         }
     }
 
+    /// <summary>
+    /// Generates a chat message asynchronously.
+    /// </summary>
+    /// <param name="chatHistory">The chat history containing the conversation data.</param>
+    /// <param name="executionSettings">Optional settings for prompt execution.</param>
+    /// <param name="cancellationToken">A cancellation token to cancel the operation.</param>
+    /// <returns>Returns a list of chat message contents.</returns>
     public async Task<IReadOnlyList<ChatMessageContent>> GenerateChatMessageAsync(
         ChatHistory chatHistory,
         PromptExecutionSettings? executionSettings = null,
@@ -116,6 +123,13 @@ internal sealed class GeminiClient
         return this.DeserializeAndProcessChatResponse(body);
     }
 
+    /// <summary>
+    /// Generates a stream of chat messages asynchronously.
+    /// </summary>
+    /// <param name="chatHistory">The chat history containing the conversation data.</param>
+    /// <param name="executionSettings">Optional settings for prompt execution.</param>
+    /// <param name="cancellationToken">A cancellation token to cancel the operation.</param>
+    /// <returns>An asynchronous enumerable of <see cref="StreamingChatMessageContent"/> streaming chat contents.</returns>
     public async IAsyncEnumerable<StreamingChatMessageContent> StreamGenerateChatMessageAsync(
         ChatHistory chatHistory,
         PromptExecutionSettings? executionSettings = null,
