3d929bc1ca650e778a5617e602f0906364e811b6
Mustafa Zengin <muzengin@microsoft.com>
2024-01-09 11:26:21 -0800

    add sample calling the plugin dynamically via handlebars planner

--- dotnet/samples/KernelSyntaxExamples/Example99_ApiManifest.cs
+++ dotnet/samples/KernelSyntaxExamples/Example99_ApiManifest.cs
@@ -10,6 +10,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Planning.Handlebars;
 using Microsoft.SemanticKernel.Plugins.MsGraph.Connectors.CredentialManagers;
 using Microsoft.SemanticKernel.Plugins.OpenApi;
 
@@ -25,7 +26,12 @@ public static class Example99_ApiManifest
     {
         s_sampleIndex = 1;
 
-        await RunSampleAsync("get my first message", shouldPrintPrompt: false, "MessagesPlugin");
+        await RunSampleWithPlannerAsync("get my first message", shouldPrintPrompt: false, "MessagesPlugin").ConfigureAwait(false);
+
+        await RunSampleAsync("MessagesPlugin", "Getmemessages", new KernelArguments
+        {
+           { "_top", "1" }
+        }).ConfigureAwait(false);
     }
 
     private static void WriteSampleHeadingToConsole(string name)
@@ -33,7 +39,21 @@ public static class Example99_ApiManifest
         Console.WriteLine($"======== [ApiManifest Plugins] Sample {s_sampleIndex++} - Create and Execute \"{name}\" Plan ========");
     }
 
-    private static async Task RunSampleAsync(string goal, bool shouldPrintPrompt = false, params string[] pluginDirectoryNames)
+    private static async Task RunSampleAsync(string pluginName, string functionName, KernelArguments arguments)
+    {
+        var kernel = Kernel.CreateBuilder()
+            .Build();
+
+        await AddApiManifestPluginsAsync(kernel, pluginName).ConfigureAwait(false);
+
+        WriteSampleHeadingToConsole($"{pluginName}.{functionName}");
+        var result = await kernel.InvokeAsync(pluginName, functionName, arguments).ConfigureAwait(false);
+#pragma warning disable SKEXP0042
+        Console.WriteLine(result.GetValue<RestApiOperationResponse>()?.Content);
+#pragma warning restore SKEXP0042
+    }
+
+    private static async Task RunSampleWithPlannerAsync(string goal, bool shouldPrintPrompt = false, params string[] pluginNames)
     {
         string apiKey = TestConfiguration.AzureOpenAI.ApiKey;
         string chatDeploymentName = TestConfiguration.AzureOpenAI.ChatDeploymentName;
@@ -55,16 +75,34 @@ public static class Example99_ApiManifest
                 modelId: chatModelId)
             .Build();
 
-        await AddApiManifestPluginsAsync(kernel, pluginDirectoryNames).ConfigureAwait(false);
+        await AddApiManifestPluginsAsync(kernel, pluginNames).ConfigureAwait(false);
 
-        WriteSampleHeadingToConsole(goal);
-        var result = await kernel.InvokeAsync("MessagesPlugin", "Getmemessages", new KernelArguments
+        // Use gpt-4 or newer models if you want to test with loops. 
+        // Older models like gpt-35-turbo are less recommended. They do handle loops but are more prone to syntax errors.
+        var allowLoopsInPlan = chatDeploymentName.Contains("gpt-4", StringComparison.OrdinalIgnoreCase);
+        var planner = new HandlebarsPlanner(
+            new HandlebarsPlannerOptions()
+            {
+                // Change this if you want to test with loops regardless of model selection.
+                AllowLoops = allowLoopsInPlan
+            });
+
+        WriteSampleHeadingToConsole($"{goal}");
+
+        // Create the plan
+        var plan = await planner.CreatePlanAsync(kernel, goal);
+
+        // Print the prompt template
+        if (shouldPrintPrompt && plan.Prompt is not null)
         {
-           { "_top", "1" }
-        }).ConfigureAwait(false);
-#pragma warning disable SKEXP0042
-        Console.WriteLine(result.GetValue<RestApiOperationResponse>()?.Content);
-#pragma warning restore SKEXP0042
+            Console.WriteLine($"\nPrompt template:\n{plan.Prompt}");
+        }
+
+        Console.WriteLine($"\nOriginal plan:\n{plan}");
+
+        // Execute the plan
+        var result = await plan.InvokeAsync(kernel);
+        Console.WriteLine($"\nResult:\n{result}\n");
     }
 
     private static async Task AddApiManifestPluginsAsync(Kernel sk, params string[] pluginNames)
