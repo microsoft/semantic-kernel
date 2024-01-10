ee404b7f85adcb66130eb924f231f30dbf2f789a
Chris Rickman <crickman@microsoft.com>
2024-01-09 23:21:52 -0800

    Using

--- dotnet/samples/KernelSyntaxExamples/Example75_AgentTools.cs
+++ dotnet/samples/KernelSyntaxExamples/Example75_AgentTools.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.SemanticKernel.Experimental.Agents;
-using static Google.Protobuf.Reflection.GeneratedCodeInfo;
 
 // ReSharper disable once InconsistentNaming
 /// <summary>
