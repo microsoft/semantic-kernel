b99b780eca883b8d954b6ed3d3335f0723c36120
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2024-01-16 19:16:24 +0000

    .Net Kernel Examples as tests (#4526)
    ### Motivation and Context
    
    Convert Kernel Examples as Integration Tests.

--- dotnet/samples/KernelSyntaxExamples/Example62_CustomAIServiceSelector.cs
+++ dotnet/samples/KernelSyntaxExamples/Example62_CustomAIServiceSelector.cs
@@ -7,55 +7,40 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
 using Microsoft.SemanticKernel.Services;
+using Xunit;
+using Xunit.Abstractions;
 
-public static class Example62_CustomAIServiceSelector
+namespace Examples;
+
+public class Example62_CustomAIServiceSelector : BaseTest
 {
     /// <summary>
     /// Show how to use a custom AI service selector to select a specific model
     /// </summary>
-    public static async Task RunAsync()
+    [Fact]
+    public async Task RunAsync()
     {
-        Console.WriteLine("======== Example62_CustomAIServiceSelector ========");
-
-        string azureApiKey = TestConfiguration.AzureOpenAI.ApiKey;
-        string azureDeploymentName = TestConfiguration.AzureOpenAI.ChatDeploymentName;
-        string azureModelId = TestConfiguration.AzureOpenAI.ChatModelId;
-        string azureEndpoint = TestConfiguration.AzureOpenAI.Endpoint;
-
-        if (azureApiKey == null || azureDeploymentName == null || azureModelId == null || azureEndpoint == null)
-        {
-            Console.WriteLine("AzureOpenAI endpoint, apiKey, deploymentName or modelId not found. Skipping example.");
-            return;
-        }
-
-        string openAIModelId = TestConfiguration.OpenAI.ChatModelId;
-        string openAIApiKey = TestConfiguration.OpenAI.ApiKey;
-
-        if (openAIModelId == null || openAIApiKey == null)
-        {
-            Console.WriteLine("OpenAI credentials not found. Skipping example.");
-            return;
-        }
+        WriteLine("======== Example62_CustomAIServiceSelector ========");
 
         // Build a kernel with multiple chat completion services
         var builder = Kernel.CreateBuilder()
             .AddAzureOpenAIChatCompletion(
-                deploymentName: azureDeploymentName,
-                endpoint: azureEndpoint,
-                apiKey: azureApiKey,
+                deploymentName: TestConfiguration.AzureOpenAI.ChatDeploymentName,
+                endpoint: TestConfiguration.AzureOpenAI.Endpoint,
+                apiKey: TestConfiguration.AzureOpenAI.ApiKey,
                 serviceId: "AzureOpenAIChat",
-                modelId: azureModelId)
+                modelId: TestConfiguration.AzureOpenAI.ChatModelId)
             .AddOpenAIChatCompletion(
-                modelId: openAIModelId,
-                apiKey: openAIApiKey,
+                modelId: TestConfiguration.OpenAI.ChatModelId,
+                apiKey: TestConfiguration.OpenAI.ApiKey,
                 serviceId: "OpenAIChat");
-        builder.Services.AddSingleton<IAIServiceSelector>(new GptAIServiceSelector()); // Use the custom AI service selector to select the GPT model
+        builder.Services.AddSingleton<IAIServiceSelector>(new GptAIServiceSelector(this.Output)); // Use the custom AI service selector to select the GPT model
         Kernel kernel = builder.Build();
 
         // This invocation is done with the model selected by the custom selector
         var prompt = "Hello AI, what can you do for me?";
         var result = await kernel.InvokePromptAsync(prompt);
-        Console.WriteLine(result.GetValue<string>());
+        WriteLine(result.GetValue<string>());
     }
 
     /// <summary>
@@ -66,6 +51,13 @@ public static class Example62_CustomAIServiceSelector
     /// </summary>
     private sealed class GptAIServiceSelector : IAIServiceSelector
     {
+        private readonly ITestOutputHelper _output;
+
+        public GptAIServiceSelector(ITestOutputHelper output)
+        {
+            this._output = output;
+        }
+
         public bool TrySelectAIService<T>(
             Kernel kernel, KernelFunction function, KernelArguments arguments,
             [NotNullWhen(true)] out T? service, out PromptExecutionSettings? serviceSettings) where T : class, IAIService
@@ -77,7 +69,7 @@ public static class Example62_CustomAIServiceSelector
                 var endpoint = serviceToCheck.GetEndpoint();
                 if (!string.IsNullOrEmpty(serviceModelId) && serviceModelId.StartsWith("gpt", StringComparison.OrdinalIgnoreCase))
                 {
-                    Console.WriteLine($"Selected model: {serviceModelId} {endpoint}");
+                    this._output.WriteLine($"Selected model: {serviceModelId} {endpoint}");
                     service = serviceToCheck;
                     serviceSettings = new OpenAIPromptExecutionSettings();
                     return true;
@@ -88,5 +80,9 @@ public static class Example62_CustomAIServiceSelector
             serviceSettings = null;
             return false;
         }
+    }
+
+    public Example62_CustomAIServiceSelector(ITestOutputHelper output) : base(output)
+    {
     }
 }
