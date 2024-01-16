ed85e5fd3922f0a827fd7642b8b1246a61bad3a5
Jose Luis Latorre Millas <joslat@gmail.com>
2024-01-16 20:46:04 +0100

    Handlebars syntax example - rough and not fully working.  Needs help from the master of Handlebars ;)

---
+++ dotnet/samples/KernelSyntaxExamples/Example77_HandlebarsPromptSyntax.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Threading.Tasks;
+using Azure.AI.OpenAI;
+using Microsoft.SemanticKernel;
+using System.Text.Json;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace KernelSyntaxExamples;
+
+// ReSharper disable once InconsistentNaming
+public static class Example77_HandlebarsPromptSyntax
+{
+    public static async Task RunAsync()
+    {
+        Console.WriteLine("======== LLMPrompts ========");
+
+        string openAIModelId = TestConfiguration.OpenAI.ChatModelId;
+        string openAIApiKey = TestConfiguration.OpenAI.ApiKey;
+
+        if (openAIApiKey == null)
+        {
+            Console.WriteLine("OpenAI credentials not found. Skipping example.");
+            return;
+        }
+
+        Kernel kernel = Kernel.CreateBuilder()
+            .AddOpenAIChatCompletion(
+                modelId: openAIModelId,
+                apiKey: openAIApiKey)
+            .Build();
+
+
+
+    }
+}
