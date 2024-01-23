2bf5684b0adf88b08ea65cf63841667e194d734d
Dmytro Struk <13853051+dmytrostruk@users.noreply.github.com>
2024-01-23 18:07:51 +0000

    Updated example with adding filter without DI

--- dotnet/samples/KernelSyntaxExamples/Getting_Started/Step7_Observability.cs
+++ dotnet/samples/KernelSyntaxExamples/Getting_Started/Step7_Observability.cs
@@ -29,10 +29,14 @@ public class Step7_Observability : BaseTest
 
         kernelBuilder.Plugins.AddFromType<TimeInformation>();
 
+        // Add filter using DI
+        kernelBuilder.Services.AddSingleton<ITestOutputHelper>(this.Output);
         kernelBuilder.Services.AddSingleton<IFunctionFilter, MyFunctionFilter>();
-        kernelBuilder.Services.AddSingleton<IPromptFilter, MyPromptFilter>();
 
         Kernel kernel = kernelBuilder.Build();
+
+        // Add filter without DI
+        kernel.PromptFilters.Add(new MyPromptFilter(this.Output));
 
         // Invoke the kernel with a prompt and allow the AI to automatically invoke functions
         OpenAIPromptExecutionSettings settings = new() { ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions };
@@ -65,7 +69,7 @@ public class Step7_Observability : BaseTest
         // Handler which is called before a prompt is rendered
         void MyRenderingHandler(object? sender, PromptRenderingEventArgs e)
         {
-            Console.WriteLine($"Rendering prompt for {e.Function.Name}");
+            WriteLine($"Rendering prompt for {e.Function.Name}");
         }
 
         // Handler which is called after a prompt is rendered
@@ -109,19 +113,26 @@ public class Step7_Observability : BaseTest
     /// </summary>
     private sealed class MyFunctionFilter : IFunctionFilter
     {
+        private readonly ITestOutputHelper _output;
+
+        public MyFunctionFilter(ITestOutputHelper output)
+        {
+            this._output = output;
+        }
+
         public void OnFunctionInvoked(FunctionInvokedContext context)
         {
             var metadata = context.Result.Metadata;
 
             if (metadata is not null && metadata.ContainsKey("Usage"))
             {
-                Console.WriteLine($"Token usage: {metadata["Usage"]?.AsJson()}");
+                this._output.WriteLine($"Token usage: {metadata["Usage"]?.AsJson()}");
             }
         }
 
         public void OnFunctionInvoking(FunctionInvokingContext context)
         {
-            Console.WriteLine($"Invoking {context.Function.Name}");
+            this._output.WriteLine($"Invoking {context.Function.Name}");
         }
     }
 
@@ -130,14 +141,21 @@ public class Step7_Observability : BaseTest
     /// </summary>
     private sealed class MyPromptFilter : IPromptFilter
     {
+        private readonly ITestOutputHelper _output;
+
+        public MyPromptFilter(ITestOutputHelper output)
+        {
+            this._output = output;
+        }
+
         public void OnPromptRendered(PromptRenderedContext context)
         {
-            Console.WriteLine($"Prompt sent to model: {context.RenderedPrompt}");
+            this._output.WriteLine($"Prompt sent to model: {context.RenderedPrompt}");
         }
 
         public void OnPromptRendering(PromptRenderingContext context)
         {
-            Console.WriteLine($"Rendering prompt for {context.Function.Name}");
+            this._output.WriteLine($"Rendering prompt for {context.Function.Name}");
         }
     }
 
