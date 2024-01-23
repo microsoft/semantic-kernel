886a94813921256593e4d26c01256b36a4e2925e
Milder Hernandez <milderhc@microsoft.com>
2024-01-23 10:10:18 -0800

    Java: Add Example69_MutableKernelPlugin (#4683)
    ### Motivation and Context
    
    <!-- Thank you for your contribution to the semantic-kernel repo!
    Please help reviewers and future users, providing the following
    information:
      1. Why is this change required?
      2. What problem does it solve?
      3. What scenario does it contribute to?
      4. If it fixes an open issue, please link to the issue here.
    -->
    
    ### Description
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [ ] The code builds clean without any errors or warnings
    - [ ] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [ ] All unit tests pass, and I have added new tests where possible
    - [ ] I didn't break anyone :smile:

---
+++ java/samples/sample-code/src/main/java/com/microsoft/semantickernel/samples/syntaxexamples/Example69_MutableKernelPlugin.java
@@ -0,0 +1,37 @@
+package com.microsoft.semantickernel.samples.syntaxexamples;
+
+import com.microsoft.semantickernel.Kernel;
+import com.microsoft.semantickernel.plugin.KernelFunctionFactory;
+import com.microsoft.semantickernel.plugin.KernelPlugin;
+import com.microsoft.semantickernel.plugin.annotations.DefineKernelFunction;
+
+public class Example69_MutableKernelPlugin {
+
+    public static class Time {
+        @DefineKernelFunction(name = "date")
+        public String date() {
+            return "2021-09-01";
+        }
+    }
+
+    /**
+     * Mutable KernelPlugin example
+     * <p>
+     * KernelFunction can be added directly to the function collection of the KernelPlugin
+     * by using KernelPlugin.getFunctions and putting the corresponding KernelFunction
+     */
+    public static void main(String[] args) throws NoSuchMethodException {
+        System.out.println("======== Example69_MutableKernelPlugin ========");
+
+        KernelPlugin plugin = new KernelPlugin("Plugin", "Mutable plugin", null);
+        plugin.getFunctions().put("dateFunction", KernelFunctionFactory.createFromMethod(
+                Time.class.getMethod("date"), new Time(),"date", null, null, null));
+
+        Kernel kernel = Kernel.builder().build();
+        kernel.getPlugins().add(plugin);
+
+        var result = kernel.invokeAsync(kernel.getPlugins().getFunction("Plugin", "dateFunction"), null, String.class).block();
+
+        System.out.println("Result: " + result.getValue());
+    }
+}
