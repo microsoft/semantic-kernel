300cb5a7fea15e48b21e6c46320318a33f1762f7
SergeyMenshykh <sergemenshikh@gmail.com>
2024-01-23 12:59:34 +0000

    Added a comment explaining the reason for swallowing the exception.

--- dotnet/src/Functions/Functions.OpenApi/Extensions/OpenApiKernelExtensions.cs
+++ dotnet/src/Functions/Functions.OpenApi/Extensions/OpenApiKernelExtensions.cs
@@ -358,6 +358,9 @@ public static class OpenApiKernelExtensions
         }
         catch (ArgumentException)
         {
+            // The exception indicates that the operationId is not a valid function name.  
+            // To comply with the SK Function name requirements, it needs to be converted or sanitized.  
+            // Therefore, it should not be re-thrown, but rather swallowed to allow the conversion below.  
         }
 
         // Tokenize operation id on forward and back slashes
