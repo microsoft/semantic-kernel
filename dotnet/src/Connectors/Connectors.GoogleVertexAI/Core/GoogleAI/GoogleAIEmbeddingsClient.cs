7ed96a3e8995f66d55fa0868be40798847129650
Krzysztof Kasprowicz <60486987+Krzysztof318@users.noreply.github.com>
2024-01-26 12:22:07 +0100

    .Net GoogleAI Embeddings generation impl (#4745)
    ### Description
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    State of connector progress
    https://github.com/microsoft/semantic-kernel/issues/4680
    Spit from PR https://github.com/microsoft/semantic-kernel/pull/4407
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [x] The code builds clean without any errors or warnings
    - [x] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [x] All unit tests pass, and I have added new tests where possible
    - [x] I didn't break anyone :smile:

---
+++ dotnet/src/Connectors/Connectors.GoogleVertexAI/Core/GoogleAI/GoogleAIEmbeddingsClient.cs
@@ -0,0 +1,70 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.SemanticKernel.Connectors.GoogleVertexAI;
+
+/// <summary>
+/// Represents a client for interacting with the embeddings models by Google AI.
+/// </summary>
+internal sealed class GoogleAIEmbeddingsClient : ClientBase, IEmbeddingsClient
+{
+    private readonly string _embeddingModelId;
+
+    /// <summary>
+    /// Represents a client for interacting with the embeddings models by Google AI.
+    /// </summary>
+    /// <param name="httpClient">HttpClient instance used to send HTTP requests</param>
+    /// <param name="embeddingModelId">Embeddings generation model id</param>
+    /// <param name="httpRequestFactory">Request factory for gemini rest api or gemini vertex ai</param>
+    /// <param name="endpointProvider">Endpoints provider for gemini rest api or gemini vertex ai</param>
+    /// <param name="logger">Logger instance used for logging (optional)</param>
+    public GoogleAIEmbeddingsClient(
+        HttpClient httpClient,
+        string embeddingModelId,
+        IHttpRequestFactory httpRequestFactory,
+        IEndpointProvider endpointProvider,
+        ILogger? logger = null)
+        : base(
+            httpClient: httpClient,
+            httpRequestFactory:
+            httpRequestFactory,
+            endpointProvider: endpointProvider,
+            logger: logger)
+    {
+        Verify.NotNullOrWhiteSpace(embeddingModelId);
+        this._embeddingModelId = embeddingModelId;
+    }
+
+    /// <inheritdoc/>
+    public async Task<IList<ReadOnlyMemory<float>>> GenerateEmbeddingsAsync(
+        IList<string> data,
+        CancellationToken cancellationToken = default)
+    {
+        Verify.NotNullOrEmpty(data);
+
+        var endpoint = this.EndpointProvider.GetEmbeddingsEndpoint(this._embeddingModelId);
+        var geminiRequest = this.GetEmbeddingRequest(data);
+        using var httpRequestMessage = this.HttpRequestFactory.CreatePost(geminiRequest, endpoint);
+
+        string body = await this.SendRequestAndGetStringBodyAsync(httpRequestMessage, cancellationToken)
+            .ConfigureAwait(false);
+
+        return DeserializeAndProcessEmbeddingsResponse(body);
+    }
+
+    private GoogleAIEmbeddingRequest GetEmbeddingRequest(IEnumerable<string> data)
+        => GoogleAIEmbeddingRequest.FromData(data, this._embeddingModelId);
+
+    private static List<ReadOnlyMemory<float>> DeserializeAndProcessEmbeddingsResponse(string body)
+        => ProcessEmbeddingsResponse(DeserializeResponse<GoogleAIEmbeddingResponse>(body));
+
+    private static List<ReadOnlyMemory<float>> ProcessEmbeddingsResponse(GoogleAIEmbeddingResponse embeddingsResponse)
+        => embeddingsResponse.Embeddings.Select(embedding => embedding.Values).ToList();
+}
