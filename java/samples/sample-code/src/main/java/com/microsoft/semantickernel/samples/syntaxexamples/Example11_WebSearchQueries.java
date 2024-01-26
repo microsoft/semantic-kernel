c74f637913c26a41d9bf24bc94eefe1fa90ced1d
David Grieve <dsgrieve@yahoo.com>
2024-01-26 09:00:06 -0500

    Java: Example11_WebSearchQueries (#4750)
    Implement Example11_WebSearchQueries

---
+++ java/samples/sample-code/src/main/java/com/microsoft/semantickernel/samples/syntaxexamples/Example11_WebSearchQueries.java
@@ -0,0 +1,38 @@
+package com.microsoft.semantickernel.samples.syntaxexamples;
+
+import com.microsoft.semantickernel.Kernel;
+import com.microsoft.semantickernel.orchestration.contextvariables.KernelArguments;
+import com.microsoft.semantickernel.plugin.KernelPluginFactory;
+import com.microsoft.semantickernel.plugins.web.SearchUrlPlugin;
+
+public class Example11_WebSearchQueries {
+
+    public static void main(String[] args) {
+
+        var kernel = Kernel.builder().build();
+
+        // Load native plugins
+        var searchUrlPlugin = KernelPluginFactory.createFromObject(new SearchUrlPlugin(), "SearchUrlPlugin");
+        var bingSearchFunction = searchUrlPlugin.get("BingSearchUrl");
+
+         // Run
+        var ask = "What's the largest building in Europe?";
+        var kernelArguments = KernelArguments.builder()
+            .withVariable("query", ask)
+            .build();
+
+        var result = kernel.invokeAsync(bingSearchFunction, kernelArguments, String.class).block();
+
+        System.out.println(ask);
+        System.out.println(result.getResult());
+
+        /* Expected output: 
+        * ======== WebSearchQueries ========
+        * What's the tallest building in Europe?
+        * 
+        * https://www.bing.com/search?q=What%27s%20the%20tallest%20building%20in%20Europe%3F
+        * == DONE ==
+        */
+    }
+
+}
