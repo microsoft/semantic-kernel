31d3810f412de2f342fbc232fe741b415ac13ab8
Evan Mattson <evan.mattson@microsoft.com>
2024-01-26 21:28:20 -0500

    Fix plugin __init__.py

--- python/semantic_kernel/plugin_definition/__init__.py
+++ python/semantic_kernel/plugin_definition/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (c) Microsoft. All rights reserved.
 
-from python.semantic_kernel.plugin_definition.kernel_plugin_base import KernelPluginBase
-
+from semantic_kernel.plugin_definition.kernel_plugin_base import KernelPluginBase
+from semantic_kernel.plugin_definition.kernel_plugin_collection import KernelPluginCollection
 from semantic_kernel.plugin_definition.kernel_function_context_parameter_decorator import (
     kernel_function_context_parameter,
 )
