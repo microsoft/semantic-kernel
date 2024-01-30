835835e4ff50a11af714874ee3c2304b8ed648e1
Krzysztof Kasprowicz <krzysztof@impe.pl>
2024-01-30 15:16:20 +0100

    Added public API for embeddings

--- dotnet/src/Connectors/Connectors.GoogleVertexAI/Extensions/GoogleAIServiceCollectionExtensions.cs
+++ dotnet/src/Connectors/Connectors.GoogleVertexAI/Extensions/GoogleAIServiceCollectionExtensions.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.Connectors.GoogleVertexAI;
+using Microsoft.SemanticKernel.Embeddings;
 using Microsoft.SemanticKernel.Http;
 using Microsoft.SemanticKernel.TextGeneration;
 
@@ -136,5 +137,60 @@ public static class GoogleAIServiceCollectionExtensions
                 httpClient: HttpClientProvider.GetHttpClient(serviceProvider),
                 loggerFactory: serviceProvider.GetService<ILoggerFactory>()));
         return services;
+    }
+
+    /// <summary>
+    /// Add Google AI embeddings generation service to the kernel builder.
+    /// </summary>
+    /// <param name="builder">The kernel builder.</param>
+    /// <param name="modelId">The model for text generation.</param>
+    /// <param name="apiKey">The API key for authentication Gemini API.</param>
+    /// <param name="serviceId">The optional service ID.</param>
+    /// <param name="httpClient">The optional custom HttpClient.</param>
+    /// <returns>The updated kernel builder.</returns>
+    public static IKernelBuilder AddGoogleAIEmbeddingsGeneration(
+        this IKernelBuilder builder,
+        string modelId,
+        string apiKey,
+        string? serviceId = null,
+        HttpClient? httpClient = null)
+    {
+        Verify.NotNull(builder);
+        Verify.NotNull(modelId);
+        Verify.NotNull(apiKey);
+
+        builder.Services.AddKeyedSingleton<ITextEmbeddingGenerationService>(serviceId, (serviceProvider, _) =>
+            new GoogleAITextEmbeddingGenerationService(
+                model: modelId,
+                apiKey: apiKey,
+                httpClient: HttpClientProvider.GetHttpClient(httpClient, serviceProvider),
+                loggerFactory: serviceProvider.GetService<ILoggerFactory>()));
+        return builder;
+    }
+
+    /// <summary>
+    /// Add Google AI embeddings generation service to the specified service collection.
+    /// </summary>
+    /// <param name="services">The service collection to add the Gemini Embeddings Generation service to.</param>
+    /// <param name="modelId">The model for embeddings generation.</param>
+    /// <param name="apiKey">The API key for authentication Gemini API.</param>
+    /// <param name="serviceId">Optional service ID.</param>
+    /// <returns>The updated service collection.</returns>
+    public static IServiceCollection AddGoogleAIEmbeddingsGeneration(
+        this IServiceCollection services,
+        string modelId,
+        string apiKey,
+        string? serviceId = null)
+    {
+        Verify.NotNull(services);
+        Verify.NotNull(modelId);
+        Verify.NotNull(apiKey);
+
+        return services.AddKeyedSingleton<ITextEmbeddingGenerationService>(serviceId, (serviceProvider, _) =>
+            new GoogleAITextEmbeddingGenerationService(
+                model: modelId,
+                apiKey: apiKey,
+                httpClient: HttpClientProvider.GetHttpClient(serviceProvider),
+                loggerFactory: serviceProvider.GetService<ILoggerFactory>()));
     }
 }
