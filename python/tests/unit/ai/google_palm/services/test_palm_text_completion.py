11cf8eacd446f0c638d9767efa8ddfeca12bf7be
Eduard van Valkenburg <eavanvalkenburg@users.noreply.github.com>
2024-01-30 22:15:32 +0100

    Python: implement KernelContent and all related classes (#4491)
    ### Motivation and Context
    
    <!-- Thank you for your contribution to the semantic-kernel repo!
    Please help reviewers and future users, providing the following
    information:
      1. Why is this change required?
      2. What problem does it solve?
      3. What scenario does it contribute to?
      4. If it fixes an open issue, please link to the issue here.
    -->
    As we are preparing to move towards v1 where the context no longer is
    used, we need a better way of handling the responses from models and
    capture them in a more structured way, also in preparation of
    multi-modal models and the like.
    
    Closes #4598 
    
    ### Description
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    Added KernelContent and StreamingKernelContent base classs, consistent
    with the dotnet versions of those.
    Subclasses of this class for specific services, for instance, the OpenAI
    class has parsing for the content, function_calls, tool_calls in it, and
    the Azure OpenAI one has parsing for the context in it.
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [x] The code builds clean without any errors or warnings
    - [x] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [x] All unit tests pass, and I have added new tests where possible
    - [ ] I didn't break anyone :smile:

--- python/tests/unit/ai/google_palm/services/test_palm_text_completion.py
+++ python/tests/unit/ai/google_palm/services/test_palm_text_completion.py
@@ -1,5 +1,4 @@
 # Copyright (c) Microsoft. All rights reserved.
-import asyncio
 import sys
 from unittest.mock import MagicMock, patch
 
@@ -7,6 +6,9 @@ import pytest
 from pydantic import ValidationError
 
 if sys.version_info >= (3, 9):
+    from google.generativeai.types import Completion
+    from google.generativeai.types.text_types import TextCompletion
+
     from semantic_kernel.connectors.ai.google_palm import (
         GooglePalmTextRequestSettings,
     )
@@ -46,11 +48,12 @@ def test_google_palm_text_completion_init_with_empty_api_key() -> None:
 
 @pytest.mark.asyncio
 async def test_google_palm_text_completion_complete_call_with_parameters() -> None:
-    mock_response = MagicMock()
-    mock_response.result = asyncio.Future()
-    mock_response.result.set_result("Example response")
+    gp_completion = Completion()
+    gp_completion.candidates = [TextCompletion(output="Example response")]
+    gp_completion.filters = None
+    gp_completion.safety_feedback = None
     mock_gp = MagicMock()
-    mock_gp.generate_text.return_value = mock_response
+    mock_gp.generate_text.return_value = gp_completion
     with patch(
         "semantic_kernel.connectors.ai.google_palm.services.gp_text_completion.palm",
         new=mock_gp,
@@ -64,7 +67,7 @@ async def test_google_palm_text_completion_complete_call_with_parameters() -> No
         )
         settings = GooglePalmTextRequestSettings()
         response = await gp_text_completion.complete(prompt, settings)
-        assert isinstance(response.result(), str) and len(response.result()) > 0
+        assert isinstance(response[0].text, str) and len(response) > 0
 
         mock_gp.generate_text.assert_called_once_with(
             model=ai_model_id,
