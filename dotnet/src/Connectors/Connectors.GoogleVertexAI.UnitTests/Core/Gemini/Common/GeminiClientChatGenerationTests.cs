465f6533df3e726a2cdc1674b45863fb6eaddc77
Krzysztof Kasprowicz <60486987+Krzysztof318@users.noreply.github.com>
2024-01-30 23:43:15 +0100

    .Net Gemini refactor chat system messages handling (#4765)
    ### Motivation and Context
    
    <!-- Thank you for your contribution to the semantic-kernel repo!
    Please help reviewers and future users, providing the following
    information:
      1. Why is this change required?
      2. What problem does it solve?
      3. What scenario does it contribute to?
      4. If it fixes an open issue, please link to the issue here.
    -->
    
    ~~Gemini chat client could handle only one system message. Exception was
    thrown if there were more than one system message.~~
    Lacks of some tests, and ref to chathistory param.
    
    ### Description
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    ~~Fixes handling multiple system messages. Added revelant unit tests.~~
    Refactoring and new unit tests.
    State of connector progress
    https://github.com/microsoft/semantic-kernel/issues/4680
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [x] The code builds clean without any errors or warnings
    - [x] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [x] All unit tests pass, and I have added new tests where possible
    - [x] I didn't break anyone :smile:

--- dotnet/src/Connectors/Connectors.GoogleVertexAI.UnitTests/Core/Gemini/Common/GeminiClientChatGenerationTests.cs
+++ dotnet/src/Connectors/Connectors.GoogleVertexAI.UnitTests/Core/Gemini/Common/GeminiClientChatGenerationTests.cs
@@ -241,6 +241,61 @@ public sealed class GeminiClientChatGenerationTests : IDisposable
     }
 
     [Fact]
+    public async Task ShouldThrowKernelExceptionIfChatHistoryContainsOnlyManySystemMessagesAsync()
+    {
+        // Arrange
+        string modelId = "fake-model";
+        string apiKey = "fake-api-key";
+        var client = this.CreateChatCompletionClient(modelId, apiKey);
+        var chatHistory = new ChatHistory("System message");
+        chatHistory.AddSystemMessage("System message 2");
+        chatHistory.AddSystemMessage("System message 3");
+
+        // Act & Assert
+        await Assert.ThrowsAsync<KernelException>(
+            () => client.GenerateChatMessageAsync(chatHistory));
+    }
+
+    [Fact]
+    public async Task ShouldThrowKernelExceptionIfChatHistoryContainsMoreThanOneSystemMessageAsync()
+    {
+        string modelId = "fake-model";
+        string apiKey = "fake-api-key";
+        var client = this.CreateChatCompletionClient(modelId, apiKey);
+        var chatHistory = new ChatHistory("System message");
+        chatHistory.AddSystemMessage("System message 2");
+        chatHistory.AddSystemMessage("System message 3");
+        chatHistory.AddUserMessage("hello");
+
+        // Act & Assert
+        await Assert.ThrowsAsync<KernelException>(
+            () => client.GenerateChatMessageAsync(chatHistory));
+    }
+
+    [Fact]
+    public async Task ShouldPassConvertedSystemMessageToUserMessageToRequestAsync()
+    {
+        // Arrange
+        string modelId = "fake-model";
+        string apiKey = "fake-api-key";
+        var client = this.CreateChatCompletionClient(modelId, apiKey);
+        string message = "System message";
+        var chatHistory = new ChatHistory(message);
+        chatHistory.AddUserMessage("Hello");
+
+        // Act
+        await client.GenerateChatMessageAsync(chatHistory);
+
+        // Assert
+        GeminiRequest? request = JsonSerializer.Deserialize<GeminiRequest>(this._messageHandlerStub.RequestContent);
+        Assert.NotNull(request);
+        var systemMessage = request.Contents[0].Parts[0].Text;
+        var messageRole = request.Contents[0].Role;
+        Assert.Equal(AuthorRole.User, messageRole);
+        Assert.Equal(message, systemMessage);
+    }
+
+    [Fact]
     public async Task ShouldThrowNotSupportedIfChatHistoryHaveIncorrectOrderAsync()
     {
         // Arrange
