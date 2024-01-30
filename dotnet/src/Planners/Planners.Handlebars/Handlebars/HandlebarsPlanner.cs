bb87ea02ec6a43b1b1483e01fea1af6afb07f83d
Gina Triolo <51341242+gitri-ms@users.noreply.github.com>
2024-01-30 15:17:51 -0800

    Port planner options changes to new PR

--- dotnet/src/Planners/Planners.Handlebars/Handlebars/HandlebarsPlanner.cs
+++ dotnet/src/Planners/Planners.Handlebars/Handlebars/HandlebarsPlanner.cs
@@ -40,6 +40,7 @@ public sealed class HandlebarsPlanner
     {
         this._options = options ?? new HandlebarsPlannerOptions();
         this._templateFactory = new HandlebarsPromptTemplateFactory(options: PromptTemplateOptions);
+        this._options.ExcludedPlugins.Add("Planner_Excluded");
     }
 
     /// <summary>Creates a plan for the specified goal.</summary>
@@ -66,7 +67,18 @@ public sealed class HandlebarsPlanner
 
     private readonly HandlebarsPlannerOptions _options;
 
-    private HandlebarsPromptTemplateFactory _templateFactory { get; }
+    private readonly HandlebarsPromptTemplateFactory _templateFactory;
+
+    private static readonly PromptExecutionSettings s_defaultPromptExecutionSettings = new()
+    {
+        ExtensionData = new Dictionary<string, object>()
+        {
+            { "temperature", 0.0 },
+            { "top_p", 0.0 },
+            { "presence_penalty", 0.0 },
+            { "frequency_penalty", 0.0 },
+        }
+    };
 
     /// <summary>
     /// Error message if kernel does not contain sufficient functions to create a plan.
@@ -75,14 +87,17 @@ public sealed class HandlebarsPlanner
 
     private async Task<HandlebarsPlan> CreatePlanCoreAsync(Kernel kernel, string goal, CancellationToken cancellationToken = default)
     {
+        var executionSettings = this._options.ExecutionSettings ?? s_defaultPromptExecutionSettings;
+
         // Get CreatePlan prompt template
-        var availableFunctions = this.GetAvailableFunctionsManual(kernel, out var complexParameterTypes, out var complexParameterSchemas);
+        var functionsMetadata = await kernel.Plugins.GetFunctionsAsync(this._options, null, null, cancellationToken).ConfigureAwait(false);
+        var availableFunctions = this.GetAvailableFunctionsManual(functionsMetadata, out var complexParameterTypes, out var complexParameterSchemas);
         var createPlanPrompt = await this.GetHandlebarsTemplateAsync(kernel, goal, availableFunctions, complexParameterTypes, complexParameterSchemas, cancellationToken).ConfigureAwait(false);
         ChatHistory chatMessages = this.GetChatHistoryFromPrompt(createPlanPrompt);
 
         // Get the chat completion results
         var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
-        var completionResults = await chatCompletionService.GetChatMessageContentAsync(chatMessages, cancellationToken: cancellationToken).ConfigureAwait(false);
+        var completionResults = await chatCompletionService.GetChatMessageContentAsync(chatMessages, executionSettings: executionSettings, cancellationToken: cancellationToken).ConfigureAwait(false);
 
         // Check if plan could not be created due to insufficient functions
         if (completionResults.Content is not null && completionResults.Content.IndexOf(InsufficientFunctionsError, StringComparison.OrdinalIgnoreCase) >= 0)
@@ -104,18 +119,12 @@ public sealed class HandlebarsPlanner
     }
 
     private List<KernelFunctionMetadata> GetAvailableFunctionsManual(
-        Kernel kernel,
+        IEnumerable<KernelFunctionMetadata> availableFunctions,
         out HashSet<HandlebarsParameterTypeMetadata> complexParameterTypes,
         out Dictionary<string, string> complexParameterSchemas)
     {
         complexParameterTypes = new();
         complexParameterSchemas = new();
-
-        var availableFunctions = kernel.Plugins.GetFunctionsMetadata()
-            .Where(s => !this._options.ExcludedPlugins.Contains(s.PluginName, StringComparer.OrdinalIgnoreCase)
-                && !this._options.ExcludedFunctions.Contains(s.Name, StringComparer.OrdinalIgnoreCase)
-                && !s.Name.Contains("Planner_Excluded"))
-            .ToList();
 
         var functionsMetadata = new List<KernelFunctionMetadata>();
         foreach (var kernelFunction in availableFunctions)
