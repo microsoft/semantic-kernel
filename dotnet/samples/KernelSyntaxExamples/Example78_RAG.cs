0c05f8b0fc3bc6a69d5d7c6f0e9482fea2e2d99e
Dmytro Struk <13853051+dmytrostruk@users.noreply.github.com>
2024-01-30 21:14:20 +0000

    Added example with TextMemoryPlugin

--- dotnet/samples/KernelSyntaxExamples/Example78_RAG.cs
+++ dotnet/samples/KernelSyntaxExamples/Example78_RAG.cs
@@ -5,6 +5,10 @@ using System.Net.Http.Headers;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.Chroma;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel.Memory;
+using Microsoft.SemanticKernel.Plugins.Memory;
 using Microsoft.SemanticKernel.Plugins.OpenApi;
 using Resources;
 using Xunit;
@@ -15,9 +19,31 @@ namespace Examples;
 public class Example78_RAG : BaseTest
 {
     /// <summary>
+    /// Shows how to use RAG pattern with <see cref="TextMemoryPlugin"/>.
+    /// </summary>
+    [Fact(Skip = "Requires Chroma server up and running")]
+    public async Task RAGWithTextMemoryPluginAsync()
+    {
+        var memory = new MemoryBuilder()
+            .WithMemoryStore(new ChromaMemoryStore("http://localhost:8000"))
+            .WithOpenAITextEmbeddingGeneration(TestConfiguration.OpenAI.EmbeddingModelId, TestConfiguration.OpenAI.ApiKey)
+            .Build();
+
+        var kernel = Kernel.CreateBuilder()
+            .AddOpenAIChatCompletion(TestConfiguration.OpenAI.ChatModelId, TestConfiguration.OpenAI.ApiKey)
+            .Build();
+
+        kernel.ImportPluginFromObject(new TextMemoryPlugin(memory));
+
+        var result = await kernel.InvokePromptAsync("{{recall 'budget by year' collection='finances'}} What is my budget for 2024?");
+
+        WriteLine(result);
+    }
+
+    /// <summary>
     /// Shows how to use RAG pattern with ChatGPT Retrieval Plugin.
     /// </summary>
-    [Fact(Skip = "Requires hosted ChatGPT Retrieval Plugin and memory store")]
+    [Fact(Skip = "Requires ChatGPT Retrieval Plugin and selected vector DB server up and running")]
     public async Task RAGWithChatGPTRetrievalPluginAsync()
     {
         var openApi = EmbeddedResource.ReadStream("chat-gpt-retrieval-plugin-open-api.yaml");
