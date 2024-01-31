5303126c0d45b752c77517851a021ee7083196d4
Julio Carlos Menendez <juliome@microsoft.com>
2024-01-31 08:58:09 -0700

    Python: Renames request settings to prompt execution settings (#4782)
    ### Motivation and Context
    
    Following the C# implementation, renames all classes and variables of
    `*RequestSettings` to `*PromptExecutionSettings`.
    
    Fixes #4676 
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [X] The code builds clean without any errors or warnings
    - [X] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [X] All unit tests pass, and I have added new tests where possible
    - [X] I didn't break anyone :smile:

--- python/semantic_kernel/connectors/ai/prompt_execution_settings.py
+++ python/semantic_kernel/connectors/ai/prompt_execution_settings.py
@@ -5,20 +5,23 @@ from pydantic import Field
 from semantic_kernel.kernel_pydantic import KernelBaseModel
 
 
-class AIRequestSettings(KernelBaseModel):
-    """Base class for AI request settings.
+class PromptExecutionSettings(KernelBaseModel):
+    """Base class for prompt execution settings.
 
-    Can be used by itself or as a base class for other request settings. The methods are used to create specific request settings objects based on the keys in the extension_data field, this way you can create a generic AIRequestSettings object in your application, which get's mapped into the keys of the request settings that each services returns by using the service.get_request_settings() method.
+    Can be used by itself or as a base class for other prompt execution settings. The methods are used to create
+    specific prompt execution settings objects based on the keys in the extension_data field, this way you can
+    create a generic PromptExecutionSettings object in your application, which get's mapped into the keys of the
+    prompt execution settings that each services returns by using the service.get_prompt_execution_settings() method.
 
     Parameters:
         service_id (str, optional): The service ID to use for the request. Defaults to None.
         extension_data (Dict[str, Any], optional): Any additional data to send with the request. Defaults to None.
         kwargs (Any): Additional keyword arguments,
-            these are attempted to parse into the keys of the specific request settings.
+            these are attempted to parse into the keys of the specific prompt execution settings.
     Methods:
         prepare_settings_dict: Prepares the settings as a dictionary for sending to the AI service.
-        update_from_ai_request_settings: Update the keys from another request settings object.
-        from_ai_request_settings: Create a request settings from another request settings object.
+        update_from_prompt_execution_settings: Update the keys from another prompt execution settings object.
+        from_prompt_execution_settings: Create a prompt execution settings from another prompt execution settings object.
     """  # noqa: E501
 
     service_id: Optional[str] = Field(None, min_length=1)
@@ -32,7 +35,7 @@ class AIRequestSettings(KernelBaseModel):
 
     @property
     def keys(self):
-        """Get the keys of the request settings."""
+        """Get the keys of the prompt execution settings."""
         return self.model_fields.keys()
 
     def prepare_settings_dict(self, **kwargs) -> Dict[str, Any]:
@@ -42,8 +45,8 @@ class AIRequestSettings(KernelBaseModel):
             by_alias=True,
         )
 
-    def update_from_ai_request_settings(self, config: "AIRequestSettings") -> None:
-        """Update the request settings from a completion config."""
+    def update_from_prompt_execution_settings(self, config: "PromptExecutionSettings") -> None:
+        """Update the prompt execution settings from a completion config."""
         if config.service_id is not None:
             self.service_id = config.service_id
         config.pack_extension_data()
@@ -51,8 +54,8 @@ class AIRequestSettings(KernelBaseModel):
         self.unpack_extension_data()
 
     @classmethod
-    def from_ai_request_settings(cls, config: "AIRequestSettings") -> "AIRequestSettings":
-        """Create a request settings from a completion config."""
+    def from_prompt_execution_settings(cls, config: "PromptExecutionSettings") -> "PromptExecutionSettings":
+        """Create a prompt execution settings from a completion config."""
         config.pack_extension_data()
         return cls(
             service_id=config.service_id,
@@ -60,7 +63,7 @@ class AIRequestSettings(KernelBaseModel):
         )
 
     def unpack_extension_data(self) -> None:
-        """Update the request settings from extension data.
+        """Update the prompt execution settings from extension data.
 
         Does not overwrite existing values with None.
         """
@@ -71,7 +74,7 @@ class AIRequestSettings(KernelBaseModel):
                 setattr(self, key, value)
 
     def pack_extension_data(self) -> None:
-        """Update the extension data from the request settings."""
+        """Update the extension data from the prompt execution settings."""
         for key in self.model_fields_set:
             if key not in ["service_id", "extension_data"] and getattr(self, key) is not None:
                 self.extension_data[key] = getattr(self, key)
