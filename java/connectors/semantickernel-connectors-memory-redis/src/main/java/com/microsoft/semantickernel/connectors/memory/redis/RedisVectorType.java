4e23aea59501446b7e7638617c2546682a219a99
John Burke <john.burke@redis.com>
2024-01-31 15:27:47 -0500

    initial commit of SemanticKernel Redis Connector

---
+++ java/connectors/semantickernel-connectors-memory-redis/src/main/java/com/microsoft/semantickernel/connectors/memory/redis/RedisVectorType.java
@@ -0,0 +1,6 @@
+package com.microsoft.semantickernel.connectors.memory.redis;
+
+public class RedisVectorType {
+  public static final String FLOAT32 = "FLOAT32";
+  public static final String FLOAT64 = "FLOAT64";
+}
