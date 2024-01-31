c07c56d1a12bcafa566463c17dcd57e222f31b85
Krzysztof Kasprowicz <krzysztof@impe.pl>
2024-01-31 19:36:22 +0100

    Add unit tests for Vertex AI services

--- dotnet/src/Connectors/Connectors.GoogleVertexAI.UnitTests/TextGeneration/VertexAIGeminiTextGenerationServiceTests.cs
+++ dotnet/src/Connectors/Connectors.GoogleVertexAI.UnitTests/TextGeneration/VertexAIGeminiTextGenerationServiceTests.cs
@@ -4,16 +4,16 @@ using Microsoft.SemanticKernel.Connectors.GoogleVertexAI;
 using Microsoft.SemanticKernel.Services;
 using Xunit;
 
-namespace SemanticKernel.Connectors.GoogleVertexAI.UnitTests.ChatCompletion;
+namespace SemanticKernel.Connectors.GoogleVertexAI.UnitTests.TextGeneration;
 
-public sealed class GoogleAIGeminiChatCompletionServiceTests
+public sealed class VertexAIGeminiTextGenerationServiceTests
 {
     [Fact]
     public void AttributesShouldContainModelId()
     {
         // Arrange & Act
         string model = "fake-model";
-        var service = new GoogleAIGeminiChatCompletionService(model, "key");
+        var service = new VertexAIGeminiTextGenerationService(model, "key", "location", "project");
 
         // Assert
         Assert.Equal(model, service.Attributes[AIServiceExtensions.ModelIdKey]);
