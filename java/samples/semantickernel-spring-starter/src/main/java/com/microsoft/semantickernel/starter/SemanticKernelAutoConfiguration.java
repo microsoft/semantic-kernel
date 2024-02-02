b74843e02ed1d8a197e6f1bf8680e6dec22bc4cb
SandraAhlgrimm <sakriema@microsoft.com>
2024-02-02 12:13:39 +0100

    make Deployment Name optional and set modelid with default as requested in review

--- java/samples/semantickernel-spring-starter/src/main/java/com/microsoft/semantickernel/starter/SemanticKernelAutoConfiguration.java
+++ java/samples/semantickernel-spring-starter/src/main/java/com/microsoft/semantickernel/starter/SemanticKernelAutoConfiguration.java
@@ -5,6 +5,8 @@ import com.azure.ai.openai.OpenAIClientBuilder;
 import com.azure.core.credential.AzureKeyCredential;
 import com.microsoft.semantickernel.Kernel;
 import com.microsoft.semantickernel.SKBuilders;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
@@ -18,11 +20,11 @@ import org.springframework.util.Assert;
 @EnableConfigurationProperties(AzureOpenAIConnectionProperties.class)
 public class SemanticKernelAutoConfiguration {
 
-    String modelId;
+    private static final Logger LOGGER = LoggerFactory.getLogger(SemanticKernelAutoConfiguration.class);
 
     /**
-     * Creates a {@link OpenAIAsyncClient} with the endpoint and key specified in the {@link
-     * AzureOpenAIConnectionProperties}.
+     * Creates a {@link OpenAIAsyncClient} with the endpoint and key specified in the
+     * {@link AzureOpenAIConnectionProperties}.
      *
      * @param connectionProperties the {@link AzureOpenAIConnectionProperties} to use
      * @return the {@link OpenAIAsyncClient}
@@ -31,35 +33,46 @@ public class SemanticKernelAutoConfiguration {
     @ConditionalOnClass(OpenAIAsyncClient.class)
     @ConditionalOnMissingBean
     public OpenAIAsyncClient openAIAsyncClient(
-            AzureOpenAIConnectionProperties connectionProperties) {
+        AzureOpenAIConnectionProperties connectionProperties) {
         Assert.hasText(connectionProperties.getEndpoint(), "Azure OpenAI endpoint must be set");
         Assert.hasText(connectionProperties.getKey(), "Azure OpenAI key must be set");
-
-        modelId = connectionProperties.getDeploymentName();
-
         return new OpenAIClientBuilder()
-                .endpoint(connectionProperties.getEndpoint())
-                .credential(new AzureKeyCredential(connectionProperties.getKey()))
-                .buildAsyncClient();
+            .endpoint(connectionProperties.getEndpoint())
+            .credential(new AzureKeyCredential(connectionProperties.getKey()))
+            .buildAsyncClient();
     }
 
     /**
-     * Creates a {@link Kernel} with a default {@link
-     * com.microsoft.semantickernel.services.AIService} that uses the {@link
-     * com.microsoft.semantickernel.chatcompletion;} with the model id specified in the {@link
-     * AzureOpenAIConnectionProperties} as DeploymentName.
+     * Creates a {@link Kernel} with a default
+     * {@link com.microsoft.semantickernel.services.AIService} that uses the
+     * {@link com.microsoft.semantickernel.chatcompletion;} with the model id specified in the
+     * {@link AzureOpenAIConnectionProperties} as DeploymentName.
      *
      * @param client the {@link OpenAIAsyncClient} to use
      * @return the {@link Kernel}
      */
     @Bean
-    public Kernel semanticKernel(OpenAIAsyncClient client) {
+    public Kernel semanticKernel(OpenAIAsyncClient client,
+        AzureOpenAIConnectionProperties connectionProperties) {
         return SKBuilders.kernel()
-                .withDefaultAIService(
-                        SKBuilders.textCompletion()
-                                .withModelId(modelId)
-                                .withOpenAIClient(client)
-                                .build())
-                .build();
+            .withDefaultAIService(
+                SKBuilders.textCompletion()
+                    .withModelId(setModelID(connectionProperties))
+                    .withOpenAIClient(client)
+                    .build())
+            .build();
+    }
+
+    private static String setModelID(AzureOpenAIConnectionProperties connectionProperties) {
+        String modelId;
+        if (connectionProperties.getDeploymentName() == null) {
+            modelId = "text-davinci-003";
+            LOGGER.warn(
+                "No deployment name specified, using default model id: " + modelId);
+        } else {
+            modelId = connectionProperties.getDeploymentName();
+            LOGGER.info("Using model id: " + modelId);
+        }
+        return modelId;
     }
 }
