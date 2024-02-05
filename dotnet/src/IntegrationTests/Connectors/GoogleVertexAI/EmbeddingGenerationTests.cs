ce4669e09cedde6c8b85726a342fe43a2f6919bb
Krzysztof Kasprowicz <krzysztof@impe.pl>
2024-02-05 11:32:13 +0100

    Added integration tests

---
+++ dotnet/src/IntegrationTests/Connectors/GoogleVertexAI/EmbeddingGenerationTests.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel.Connectors.GoogleVertexAI;
+using Microsoft.SemanticKernel.Embeddings;
+using Xunit;
+
+namespace SemanticKernel.IntegrationTests.Connectors.GoogleVertexAI;
+
+public sealed class EmbeddingGenerationTests
+{
+    private readonly IConfigurationRoot _configuration = new ConfigurationBuilder()
+        .AddJsonFile(path: "testsettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile(path: "testsettings.development.json", optional: true, reloadOnChange: true)
+        .AddEnvironmentVariables()
+        .Build();
+
+    [Fact(Skip = "This test is for manual verification.")]
+    public async Task GoogleAIEmbeddingsGenerationAsync()
+    {
+        // Arrange
+        const string Input = "LLM is Large Language Model.";
+        var geminiService = new GoogleAITextEmbeddingGenerationService(this.GoogleAIGetModel(), this.GoogleAIGetApiKey());
+
+        // Act
+        var response = await geminiService.GenerateEmbeddingAsync(Input);
+
+        // Assert
+        Assert.Equal(768, response.Length);
+    }
+
+    [Fact(Skip = "This test is for manual verification.")]
+    public async Task VertexAIEmbeddingsGenerationAsync()
+    {
+        // Arrange
+        const string Input = "LLM is Large Language Model.";
+        var geminiService = new VertexAITextEmbeddingGenerationService(
+            model: this.VertexAIGetModel(),
+            apiKey: this.VertexAIGetApiKey(),
+            location: this.VertexAIGetLocation(),
+            projectId: this.VertexAIGetProjectId());
+
+        // Act
+        var response = await geminiService.GenerateEmbeddingAsync(Input);
+
+        // Assert
+        Assert.Equal(768, response.Length);
+    }
+
+    private string GoogleAIGetModel() => this._configuration.GetSection("GoogleAI:EmbeddingModelId").Get<string>()!;
+    private string GoogleAIGetApiKey() => this._configuration.GetSection("GoogleAI:ApiKey").Get<string>()!;
+
+    private string VertexAIGetModel() => this._configuration.GetSection("VertexAI:EmbeddingModelId").Get<string>()!;
+    private string VertexAIGetApiKey() => this._configuration.GetSection("VertexAI:ApiKey").Get<string>()!;
+    private string VertexAIGetLocation() => this._configuration.GetSection("VertexAI:Location").Get<string>()!;
+    private string VertexAIGetProjectId() => this._configuration.GetSection("VertexAI:ProjectId").Get<string>()!;
+}
