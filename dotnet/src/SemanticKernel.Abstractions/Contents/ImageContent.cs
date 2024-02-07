c0587790f0d86e6a7accf68caf86c83061af0d82
Sia Ghassemi <sia@sia-consulting.eu>
2024-02-07 22:10:35 +0100

    Added support for BinaryData for ImageContent. Added Tests Closes #4781

--- dotnet/src/SemanticKernel.Abstractions/Contents/ImageContent.cs
+++ dotnet/src/SemanticKernel.Abstractions/Contents/ImageContent.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Net.Mime;
 using System.Text;
 
 namespace Microsoft.SemanticKernel;
@@ -15,6 +16,11 @@ public sealed class ImageContent : KernelContent
     /// The URI of image.
     /// </summary>
     public Uri? Uri { get; set; }
+
+    /// <summary>
+    /// The Data used as DataUri for the image.
+    /// </summary>
+    public BinaryData? Data { get; }
 
     /// <summary>
     /// Initializes a new instance of the <see cref="ImageContent"/> class.
@@ -35,9 +41,43 @@ public sealed class ImageContent : KernelContent
         this.Uri = uri;
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ImageContent"/> class.
+    /// </summary>
+    /// <param name="data">The Data used as DataUri for the image.</param>
+    /// <param name="modelId">The model ID used to generate the content</param>
+    /// <param name="innerContent">Inner content</param>
+    /// <param name="encoding">Encoding of the text</param>
+    /// <param name="metadata">Additional metadata</param>
+    public ImageContent(
+        BinaryData data,
+        string? modelId = null,
+        object? innerContent = null,
+        Encoding? encoding = null,
+        IReadOnlyDictionary<string, object?>? metadata = null)
+        : base(innerContent, modelId, metadata)
+    {
+        if (data.IsEmpty || string.IsNullOrWhiteSpace(data?.MediaType))
+        {
+            throw new ArgumentNullException(nameof(data), "MediaType is needed for DataUri Images");
+        }
+
+        this.Data = data;
+    }
+
     /// <inheritdoc/>
     public override string ToString()
     {
-        return this.Uri?.ToString() ?? string.Empty;
+        return this.BuildDataUri() ?? this.Uri?.ToString() ?? string.Empty;
+    }
+
+    private string? BuildDataUri()
+    {
+        if (this.Data is null)
+        {
+            return null;
+        }
+
+        return $"data:{this.Data.MediaType};base64,{Convert.ToBase64String(this.Data.ToArray())}";
     }
 }
--- dotnet/src/SemanticKernel.UnitTests/Contents/ImageContentTests.cs
+++ dotnet/src/SemanticKernel.UnitTests/Contents/ImageContentTests.cs
@@ -1,7 +1,9 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
 using System;
+
 using Microsoft.SemanticKernel;
+
 using Xunit;
 
 namespace SemanticKernel.UnitTests.Contents;
@@ -12,10 +14,10 @@ namespace SemanticKernel.UnitTests.Contents;
 public sealed class ImageContentTests
 {
     [Fact]
-    public void ToStringReturnsString()
+    public void ToStringForUriReturnsString()
     {
         // Arrange
-        var content1 = new ImageContent(null!);
+        var content1 = new ImageContent((Uri)null!);
         var content2 = new ImageContent(new Uri("https://endpoint/"));
 
         // Act
@@ -25,5 +27,108 @@ public sealed class ImageContentTests
         // Assert
         Assert.Empty(result1);
         Assert.Equal("https://endpoint/", result2);
+    }
+
+    [Fact]
+    public void ToStringForDataUriReturnsDataUriString()
+    {
+        // Arrange
+        var data = BinaryData.FromString("this is a test", "text/plain");
+        var content1 = new ImageContent(data);
+
+        // Act
+        var result1 = content1.ToString();
+        var dataUriToExpect = $"data:{data.MediaType};base64,{Convert.ToBase64String(data.ToArray())}";
+
+        // Assert
+        Assert.Equal(dataUriToExpect, result1);
+    }
+
+    [Fact]
+    public void ToStringForUriAndDataUriReturnsDataUriString()
+    {
+        // Arrange
+        var data = BinaryData.FromString("this is a test", "text/plain");
+        var content1 = new ImageContent(data);
+        content1.Uri = new Uri("https://endpoint/");
+
+        // Act
+        var result1 = content1.ToString();
+        var dataUriToExpect = $"data:{data.MediaType};base64,{Convert.ToBase64String(data.ToArray())}";
+
+        // Assert
+        Assert.Equal(dataUriToExpect, result1);
+    }
+
+    [Fact]
+    public void CreateForNullDataUriOrWithoutMediaTypeThrows()
+    {
+        // Arrange
+        var data = BinaryData.FromString("this is a test", null);
+        var data2 = BinaryData.FromString("this is a test", string.Empty);
+        var data3 = BinaryData.FromString("this is a test", " ");
+
+        // Assert
+        Assert.Throws<ArgumentNullException>(() => new ImageContent((BinaryData)null!));
+        Assert.Throws<ArgumentNullException>(() => new ImageContent(data));
+        Assert.Throws<ArgumentNullException>(() => new ImageContent(data2));
+        Assert.Throws<ArgumentNullException>(() => new ImageContent(data3));
+    }
+
+    [Fact]
+    public void CreateForEmptyDataUriThrows()
+    {
+        // Arrange
+        var data = BinaryData.Empty;
+
+        // Assert
+        Assert.Throws<ArgumentNullException>(() => new ImageContent(data));
+    }
+
+    [Fact]
+    public void ToStringForDataUriFromBytesReturnsDataUriString()
+    {
+        // Arrange
+        var bytes = System.Text.Encoding.UTF8.GetBytes("this is a test");
+        var data = BinaryData.FromBytes(bytes, "text/plain");
+        var content1 = new ImageContent(data);
+
+        // Act
+        var result1 = content1.ToString();
+        var dataUriToExpect = $"data:{data.MediaType};base64,{Convert.ToBase64String(data.ToArray())}";
+
+        // Assert
+        Assert.Equal(dataUriToExpect, result1);
+
+        // Assert throws if mediatype is null
+        Assert.Throws<ArgumentNullException>(() => new ImageContent(BinaryData.FromBytes(bytes, null)));
+        Assert.Throws<ArgumentNullException>(() => new ImageContent(BinaryData.FromBytes(bytes, string.Empty)));
+        Assert.Throws<ArgumentNullException>(() => new ImageContent(BinaryData.FromBytes(bytes, " ")));
+    }
+
+    [Fact]
+    public void ToStringForDataUriFromStreamReturnsDataUriString()
+    {
+        // Arrange
+        var ms = new System.IO.MemoryStream(System.Text.Encoding.UTF8.GetBytes("this is a test"));
+        try
+        {
+            var data = BinaryData.FromStream(ms, "text/plain");
+            var content1 = new ImageContent(data);
+
+            // Act
+            var result1 = content1.ToString();
+            var dataUriToExpect = $"data:{data.MediaType};base64,{Convert.ToBase64String(data.ToArray())}";
+
+            // Assert
+            Assert.Equal(dataUriToExpect, result1);
+
+            // Assert throws if mediatype is null
+            Assert.Throws<ArgumentNullException>(() => new ImageContent(BinaryData.FromStream(ms, null)));
+        }
+        finally
+        {
+            ms.Dispose();
+        }
     }
 }
