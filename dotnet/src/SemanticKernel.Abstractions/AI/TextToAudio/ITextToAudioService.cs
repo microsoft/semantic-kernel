905ba18603bb3d7a43b0f1b5c48c200b9f0ca49b
Dmytro Struk <13853051+dmytrostruk@users.noreply.github.com>
2024-02-07 00:44:23 +0000

    Added initial abstractions

---
+++ dotnet/src/SemanticKernel.Abstractions/AI/TextToAudio/ITextToAudioService.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.SemanticKernel.Contents;
+
+namespace Microsoft.SemanticKernel.TextToAudio;
+
+public interface ITextToAudioService
+{
+    Task<AudioContent> GetAudioContentAsync(
+        string text,
+        PromptExecutionSettings? executionSettings = null,
+        Kernel? kernel = null,
+        CancellationToken cancellationToken = default);
+}
---
+++ dotnet/src/SemanticKernel.Abstractions/Contents/AudioContent.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.SemanticKernel.Contents;
+
+public sealed class AudioContent : KernelContent
+{
+    public BinaryData Audio { get; set; }
+
+    public AudioContent(BinaryData audio, object? innerContent, string? modelId = null, IReadOnlyDictionary<string, object?>? metadata = null) : base(innerContent, modelId, metadata)
+    {
+        this.Audio = audio;
+    }
+}
