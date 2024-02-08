936728e3c41d6fafabd5bbe9b6db27e4f935b4e3
edvan_microsoft <edvan@microsoft.com>
2024-02-08 14:25:25 +0100

    fixes for hugging face and working integration tests

--- python/tests/integration/completions/test_hf_local_text_completions.py
+++ python/tests/integration/completions/test_hf_local_text_completions.py
@@ -1,19 +1,49 @@
 # Copyright (c) Microsoft. All rights reserved.
 
 import pytest
-from transformers import AutoTokenizer
 
-import semantic_kernel as sk
 import semantic_kernel.connectors.ai.hugging_face as sk_hf
+from semantic_kernel.kernel import Kernel
 
 
 @pytest.mark.asyncio
-@pytest.mark.skip
-async def test_text2text_generation_input_str(setup_hf_text_completion_function):
-    kernel, text2text_function, simple_input = setup_hf_text_completion_function
+@pytest.mark.parametrize(
+    ("model_name", "task", "input_str"),
+    [
+        ("google-t5/t5-base", "text2text-generation", "translate English to Dutch: Hello, how are you?"),
+        (
+            "Falconsai/text_summarization",
+            "summarization",
+            """
+        Summarize: Whales are fully aquatic, open-ocean animals:
+        they can feed, mate, give birth, suckle and raise their young at sea.
+        Whales range in size from the 2.6 metres (8.5 ft) and 135 kilograms (298 lb)
+        dwarf sperm whale to the 29.9 metres (98 ft) and 190 tonnes (210 short tons) blue whale,
+        which is the largest known animal that has ever lived. The sperm whale is the largest
+        toothed predator on Earth. Several whale species exhibit sexual dimorphism,
+        in that the females are larger than males.
+    """,
+        ),
+        ("openai-community/gpt2", "text-generation", "Hello, I like sleeping and "),
+    ],
+    ids=["text2text-generation", "summarization", "text-generation"],
+)
+async def test_text_completion(model_name, task, input_str):
+    kernel = Kernel()
 
-    # Complete input string and print
-    summary = await kernel.run(text2text_function, input_str=simple_input)
+    # Configure LLM service
+    kernel.add_text_completion_service(
+        service_id=model_name,
+        service=sk_hf.HuggingFaceTextCompletion(ai_model_id=model_name, task=task),
+    )
+
+    # Define semantic function using SK prompt template language
+    sk_prompt = "{{$input}}"
+
+    # Create the semantic function
+    function = kernel.create_semantic_function(sk_prompt, max_tokens=25, temperature=0.7, top_p=0.5)
+
+    summary = await kernel.run(function, input_str=input_str)
 
     output = str(summary).strip()
     print(f"Completion using input string: '{output}'")
@@ -21,127 +51,47 @@ async def test_text2text_generation_input_str(setup_hf_text_completion_function)
 
 
 @pytest.mark.asyncio
-@pytest.mark.skip
-async def test_text2text_generation_input_vars(setup_hf_text_completion_function):
-    kernel, text2text_function, simple_input = setup_hf_text_completion_function
-
-    # Complete input as context variable and print
-    context_vars = sk.ContextVariables(simple_input)
-    summary = await kernel.run(text2text_function, input_vars=context_vars)
-
-    output = str(summary).strip()
-    print(f"Completion using context variables: '{output}'")
-    assert len(output) > 0
-
-
-@pytest.mark.asyncio
-@pytest.mark.skip
-async def test_text2text_generation_input_context(setup_hf_text_completion_function):
-    kernel, text2text_function, simple_input = setup_hf_text_completion_function
-
-    # Complete input context and print
-    context = kernel.create_new_context()
-    context["input"] = simple_input
-    summary = await kernel.run(text2text_function, input_context=context)
-
-    output = str(summary).strip()
-    print(f"Completion using input context: '{output}'")
-    assert len(output) > 0
-
-
-@pytest.mark.asyncio
-@pytest.mark.skip
-async def test_text2text_generation_input_context_with_vars(
-    setup_hf_text_completion_function,
-):
-    kernel, text2text_function, simple_input = setup_hf_text_completion_function
-
-    # Complete input context with additional variables and print
-    context = kernel.create_new_context()
-    context["input"] = simple_input
-    context_vars = sk.ContextVariables("running and")
-    summary = await kernel.run(text2text_function, input_context=context, input_vars=context_vars)
-
-    output = str(summary).strip()
-    print(f"Completion using context and additional variables: '{output}'")
-    assert len(output) > 0
-
-
-@pytest.mark.asyncio
-@pytest.mark.skip
-async def test_text2text_generation_input_context_with_str(
-    setup_hf_text_completion_function,
-):
-    kernel, text2text_function, simple_input = setup_hf_text_completion_function
-
-    # Complete input context with additional input string and print
-    context = kernel.create_new_context()
-    context["input"] = simple_input
-    summary = await kernel.run(text2text_function, input_context=context, input_str="running and")
-
-    output = str(summary).strip()
-    print(f"Completion using context and additional string: '{output}'")
-    assert len(output) > 0
-
-
-@pytest.mark.asyncio
-@pytest.mark.skip
-async def test_text2text_generation_input_context_with_vars_and_str(
-    setup_hf_text_completion_function,
-):
-    kernel, text2text_function, simple_input = setup_hf_text_completion_function
-
-    # Complete input context with additional variables and string and print
-    context = kernel.create_new_context()
-    context["input"] = simple_input
-    context_vars = sk.ContextVariables(variables={"input2": "running and"})
-    summary = await kernel.run(
-        text2text_function,
-        input_context=context,
-        input_vars=context_vars,
-        input_str="new text",
-    )
-
-    output = str(summary).strip()
-    print(f"Completion using context, additional variables, and additional string: '{output}'")
-    assert len(output) > 0
-
-
-@pytest.mark.asyncio
-@pytest.mark.skip
-async def test_text_generation_with_kwargs():
-    simple_input = "sleeping and "
-    model_name = "google/flan-t5-base"
-
-    tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path=model_name, trust_remote_code=True)
-
-    hf_model = sk_hf.HuggingFaceTextCompletion(
-        model_name,
-        task="text2text-generation",
-        model_kwargs={"repetition_penalty": 0.2},
-        pipeline_kwargs={"tokenizer": tokenizer, "trust_remote_code": True},
-    )
-
-    kernel = sk.Kernel()
+@pytest.mark.parametrize(
+    ("model_name", "task", "input_str"),
+    [
+        ("google-t5/t5-base", "text2text-generation", "translate English to Dutch: Hello, how are you?"),
+        (
+            "Falconsai/text_summarization",
+            "summarization",
+            """
+        Summarize: Whales are fully aquatic, open-ocean animals:
+        they can feed, mate, give birth, suckle and raise their young at sea.
+        Whales range in size from the 2.6 metres (8.5 ft) and 135 kilograms (298 lb)
+        dwarf sperm whale to the 29.9 metres (98 ft) and 190 tonnes (210 short tons) blue whale,
+        which is the largest known animal that has ever lived. The sperm whale is the largest
+        toothed predator on Earth. Several whale species exhibit sexual dimorphism,
+        in that the females are larger than males.
+    """,
+        ),
+        # skipped for now, as it takes too long
+        # ("openai-community/gpt2", "text-generation", "Hello, I like sleeping and "),
+    ],
+    ids=["text2text-generation", "summarization"],  # , "text-generation"],
+)
+async def test_text_completion_stream(model_name, task, input_str):
+    kernel = Kernel()
 
     # Configure LLM service
-    kernel.add_text_completion_service("hf-local", hf_model)
-
-    # Define semantic function using SK prompt template language
-    sk_prompt = "Hello, I like {{$input}}{{$input2}}"
-    text2text_function = kernel.create_semantic_function(sk_prompt, max_tokens=25, temperature=0.2, top_p=0.5)
-
-    # Complete input context with additional variables and string and print
-    context = kernel.create_new_context()
-    context["input"] = simple_input
-    context_vars = sk.ContextVariables(variables={"input2": "running and"})
-    summary = await kernel.run(
-        text2text_function,
-        input_context=context,
-        input_vars=context_vars,
-        input_str="new text",
+    kernel.add_text_completion_service(
+        service_id=model_name,
+        service=sk_hf.HuggingFaceTextCompletion(ai_model_id=model_name, task=task),
     )
 
+    # Define semantic function using SK prompt template language
+    sk_prompt = "{{$input}}"
+
+    # Create the semantic function
+    function = kernel.create_semantic_function(sk_prompt, max_tokens=25, temperature=0.7, top_p=0.5)
+
+    summary = ""
+    async for text in kernel.run_stream(function, input_str=input_str):
+        summary += str(text[0])
+
     output = str(summary).strip()
-    print(f"Completion using context, additional variables, and additional string: '{output}'")
+    print(f"Completion using input string: '{output}'")
     assert len(output) > 0
