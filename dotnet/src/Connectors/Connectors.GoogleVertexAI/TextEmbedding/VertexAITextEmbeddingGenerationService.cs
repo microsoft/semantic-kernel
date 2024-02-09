2b8ed51f7c9889af82d40c15812ed530cd5a4b51
Krzysztof Kasprowicz <60486987+Krzysztof318@users.noreply.github.com>
2024-02-09 10:39:15 +0100

    .Net GoogleAI/VertexAI Added public API for embeddings (#4786)
    ### Description
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    State of connector progress
    https://github.com/microsoft/semantic-kernel/issues/4680
    Spit from PR https://github.com/microsoft/semantic-kernel/pull/4407
    
    @RogerBarreto 
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [x] The code builds clean without any errors or warnings
    - [x] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [x] All unit tests pass, and I have added new tests where possible
    - [x] I didn't break anyone :smile:

---
+++ dotnet/src/Connectors/Connectors.GoogleVertexAI/TextEmbedding/VertexAITextEmbeddingGenerationService.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Net.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel.Http;
+using Microsoft.SemanticKernel.Services;
+
+namespace Microsoft.SemanticKernel.Connectors.GoogleVertexAI;
+
+/// <summary>
+/// Represents a service for generating text embeddings using the Vertex AI Gemini API.
+/// </summary>
+public sealed class VertexAITextEmbeddingGenerationService : TextEmbeddingGenerationServiceBase
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="GoogleAITextEmbeddingGenerationService"/> class.
+    /// </summary>
+    /// <param name="model">The model identifier.</param>
+    /// <param name="apiKey">The API key.</param>
+    /// <param name="location">The location to process the request</param>
+    /// <param name="projectId">Your project ID</param>
+    /// <param name="httpClient">The optional HTTP client.</param>
+    /// <param name="loggerFactory">Optional logger factory to be used for logging.</param>
+    public VertexAITextEmbeddingGenerationService(
+        string model,
+        string apiKey,
+        string location,
+        string projectId,
+        HttpClient? httpClient = null,
+        ILoggerFactory? loggerFactory = null)
+    {
+        Verify.NotNullOrWhiteSpace(model);
+        Verify.NotNullOrWhiteSpace(apiKey);
+
+        this.EmbeddingClient = new VertexAIEmbeddingClient(
+#pragma warning disable CA2000
+            httpClient: HttpClientProvider.GetHttpClient(httpClient),
+#pragma warning restore CA2000
+            embeddingModelId: model,
+            httpRequestFactory: new VertexAIGeminiHttpRequestFactory(apiKey),
+            endpointProvider: new VertexAIGeminiEndpointProvider(new VertexAIConfiguration(location, projectId)),
+            logger: loggerFactory?.CreateLogger(typeof(VertexAITextEmbeddingGenerationService)));
+        this.AttributesInternal.Add(AIServiceExtensions.ModelIdKey, model);
+    }
+
+    internal VertexAITextEmbeddingGenerationService(IEmbeddingClient client, string embeddingModelId)
+    {
+        this.EmbeddingClient = client;
+        this.AttributesInternal.Add(AIServiceExtensions.ModelIdKey, embeddingModelId);
+    }
+}
