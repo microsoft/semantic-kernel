c0c0fb97b1db7f241e5049b4d0570f863f4e7a99
Krzysztof Kasprowicz <60486987+Krzysztof318@users.noreply.github.com>
2024-02-09 10:39:15 +0100

    .Net GoogleAI/VertexAI Added public API for embeddings (#4786)
    ### Description
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    State of connector progress
    https://github.com/microsoft/semantic-kernel/issues/4680
    Spit from PR https://github.com/microsoft/semantic-kernel/pull/4407
    
    @RogerBarreto 
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [x] The code builds clean without any errors or warnings
    - [x] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [x] All unit tests pass, and I have added new tests where possible
    - [x] I didn't break anyone :smile:

--- dotnet/src/Connectors/Connectors.GoogleVertexAI/Core/GoogleAI/GoogleAIEmbeddingClient.cs
+++ dotnet/src/Connectors/Connectors.GoogleVertexAI/Core/GoogleAI/GoogleAIEmbeddingClient.cs
@@ -11,21 +11,21 @@ using Microsoft.Extensions.Logging;
 namespace Microsoft.SemanticKernel.Connectors.GoogleVertexAI;
 
 /// <summary>
-/// Represents a client for interacting with the embeddings models by Vertex AI.
+/// Represents a client for interacting with the embeddings models by Google AI.
 /// </summary>
-internal sealed class VertexAIEmbeddingsClient : ClientBase, IEmbeddingsClient
+internal sealed class GoogleAIEmbeddingClient : ClientBase, IEmbeddingClient
 {
     private readonly string _embeddingModelId;
 
     /// <summary>
-    /// Represents a client for interacting with the embeddings models by Vertex AI.
+    /// Represents a client for interacting with the embeddings models by Google AI.
     /// </summary>
     /// <param name="httpClient">HttpClient instance used to send HTTP requests</param>
     /// <param name="embeddingModelId">Embeddings generation model id</param>
     /// <param name="httpRequestFactory">Request factory for gemini rest api or gemini vertex ai</param>
     /// <param name="endpointProvider">Endpoints provider for gemini rest api or gemini vertex ai</param>
     /// <param name="logger">Logger instance used for logging (optional)</param>
-    public VertexAIEmbeddingsClient(
+    public GoogleAIEmbeddingClient(
         HttpClient httpClient,
         string embeddingModelId,
         IHttpRequestFactory httpRequestFactory,
@@ -50,7 +50,7 @@ internal sealed class VertexAIEmbeddingsClient : ClientBase, IEmbeddingsClient
         Verify.NotNullOrEmpty(data);
 
         var endpoint = this.EndpointProvider.GetEmbeddingsEndpoint(this._embeddingModelId);
-        var geminiRequest = GetEmbeddingRequest(data);
+        var geminiRequest = this.GetEmbeddingRequest(data);
         using var httpRequestMessage = this.HttpRequestFactory.CreatePost(geminiRequest, endpoint);
 
         string body = await this.SendRequestAndGetStringBodyAsync(httpRequestMessage, cancellationToken)
@@ -59,12 +59,12 @@ internal sealed class VertexAIEmbeddingsClient : ClientBase, IEmbeddingsClient
         return DeserializeAndProcessEmbeddingsResponse(body);
     }
 
-    private static VertexAIEmbeddingRequest GetEmbeddingRequest(IEnumerable<string> data)
-        => VertexAIEmbeddingRequest.FromData(data);
+    private GoogleAIEmbeddingRequest GetEmbeddingRequest(IEnumerable<string> data)
+        => GoogleAIEmbeddingRequest.FromData(data, this._embeddingModelId);
 
     private static List<ReadOnlyMemory<float>> DeserializeAndProcessEmbeddingsResponse(string body)
-        => ProcessEmbeddingsResponse(DeserializeResponse<VertexAIEmbeddingResponse>(body));
+        => ProcessEmbeddingsResponse(DeserializeResponse<GoogleAIEmbeddingResponse>(body));
 
-    private static List<ReadOnlyMemory<float>> ProcessEmbeddingsResponse(VertexAIEmbeddingResponse embeddingsResponse)
-        => embeddingsResponse.Predictions.Select(prediction => prediction.Embeddings.Values).ToList();
+    private static List<ReadOnlyMemory<float>> ProcessEmbeddingsResponse(GoogleAIEmbeddingResponse embeddingsResponse)
+        => embeddingsResponse.Embeddings.Select(embedding => embedding.Values).ToList();
 }
