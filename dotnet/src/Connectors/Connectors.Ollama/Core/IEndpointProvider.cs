95699084bb53a3bf1eaed5ed2220b30aff7f0d84
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2024-02-12 09:51:29 +0000

    Change

--- dotnet/src/Connectors/Connectors.Ollama/Core/IEndpointProvider.cs
+++ dotnet/src/Connectors/Connectors.Ollama/Core/IEndpointProvider.cs
@@ -11,25 +11,3 @@ internal interface IEndpointProvider
     Uri ChatCompletionEndpoint { get; }
     Uri StreamChatCompletionEndpoint { get; }
 }
-
-/// <summary>
-/// Provides a collection of endpoints for the Gemini API.
-/// </summary>
-internal sealed class OllamaEndpointProvider : IEndpointProvider
-{
-    /// <summary>
-    /// Initializes a new instance of the GeminiEndpoints class with the specified API key.
-    /// </summary>
-    /// <param name="baseUrl">Base url for Ollama API</param>
-    public OllamaEndpointProvider(string baseUrl)
-    {
-        this.TextGenerationEndpoint = new Uri($"{baseUrl}/api/generate");
-        this.ChatCompletionEndpoint = new Uri($"{baseUrl}/api/chat");
-        this.StreamChatCompletionEndpoint= new Uri($"{baseUrl}/api/chat");
-        this.StreamTextGenerationEndpoint= new Uri($"{baseUrl}/api/generate");
-    }
-    public Uri TextGenerationEndpoint { get; }
-    public Uri StreamTextGenerationEndpoint { get; }
-    public Uri ChatCompletionEndpoint { get; }
-    public Uri StreamChatCompletionEndpoint { get; }
-}
