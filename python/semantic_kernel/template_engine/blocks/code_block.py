4020bcf24ec10f55d47698ae2cc5435202d34022
edvan_microsoft <edvan@microsoft.com>
2024-02-13 20:20:02 +0100

    created enrich_function_arguments func

--- python/semantic_kernel/template_engine/blocks/code_block.py
+++ python/semantic_kernel/template_engine/blocks/code_block.py
@@ -7,6 +7,7 @@ from typing import TYPE_CHECKING, Any, ClassVar, List, Optional, Tuple
 from pydantic import Field
 
 from semantic_kernel.functions.kernel_function import KernelFunction
+from semantic_kernel.functions.kernel_function_metadata import KernelFunctionMetadata
 from semantic_kernel.functions.kernel_plugin_collection import KernelPluginCollection
 from semantic_kernel.template_engine.blocks.block import Block
 from semantic_kernel.template_engine.blocks.block_types import BlockTypes
@@ -72,31 +73,23 @@ class CodeBlock(Block):
             return self.tokens[0].render(kernel, arguments)
 
         if self.tokens[0].type == BlockTypes.FUNCTION_ID:
-            return await self._render_function_call(self.tokens[0], kernel, arguments)
+            return await self._render_function_call(kernel, arguments)
 
         raise ValueError(f"Unexpected first token type: {self.tokens[0].type}")
 
-    async def _render_function_call(self, f_block: FunctionIdBlock, kernel: "Kernel", arguments: "KernelArguments"):
+    async def _render_function_call(self, kernel: "Kernel", arguments: "KernelArguments"):
         if not kernel.plugins:
             raise ValueError("Plugin collection not set")
-
-        function = self._get_function_from_plugin_collection(kernel.plugins, f_block)
+        function_block = self.tokens[0]
+        function = self._get_function_from_plugin_collection(kernel.plugins, function_block)
         if not function:
-            error_msg = f"Function `{f_block.content}` not found"
+            error_msg = f"Function `{function_block.content}` not found"
             logger.error(error_msg)
             raise ValueError(error_msg)
 
         arguments_clone = copy(arguments)
         if len(self.tokens) > 1:
-            logger.debug(f"Passing variable/value: `{self.tokens[1].content}`")
-            rendered_value = self.tokens[1].render(kernel, arguments_clone)
-            if self.tokens[1].type == BlockTypes.VALUE:
-                arguments_clone[function.parameters[0].name] = rendered_value
-            elif self.tokens[1].type == BlockTypes.VARIABLE:
-                arg_name = self.tokens[1].content[1:]
-                arguments_clone[arg_name] = rendered_value
-            else:
-                raise ValueError("Unknown block type: %s", self.tokens[0].type)
+            arguments_clone = self._enrich_function_arguments(kernel, arguments_clone, function.metadata)
 
         result = await function.invoke(kernel, arguments_clone)
         if exc := result.metadata.get("error", None):
@@ -104,25 +97,74 @@ class CodeBlock(Block):
 
         return str(result) if result else ""
 
+    def _enrich_function_arguments(
+        self,
+        kernel: "Kernel",
+        arguments: "KernelArguments",
+        function_metadata: KernelFunctionMetadata,
+    ) -> "KernelArguments":
+        function_block: FunctionIdBlock = self.tokens[0]
+        # current templates only support 1 argument.
+        function_argument: Block = self.tokens[1]
+
+        if not function_metadata.parameters:
+            raise ValueError(
+                f"Function {function_block.plugin_name}.{function_block.function_name} does not take any arguments "
+                f"but it is being called in the template with {len(self.tokens) - 1} arguments."
+            )
+
+        # if function_argument.type != BlockTypes.NAMED_ARG:
+        logger.debug(f"Passing variable/value: `{self.tokens[1].content}`")
+        rendered_value = function_argument.render(kernel, arguments)
+        if function_argument.type == BlockTypes.VALUE:
+            arguments[function_metadata.parameters[0].name] = rendered_value
+        elif function_argument.type == BlockTypes.VARIABLE:
+            arguments[function_argument.name] = rendered_value
+        else:
+            raise ValueError("Unknown block type: %s", self.tokens[0].type)
+            # first_positional_parameter_name = function_metadata.parameters[0].name
+            # first_positional_input_value = function_argument.render(kernel, arguments)
+            # arguments[first_positional_parameter_name] = first_positional_input_value
+            # named_args_start_index += 1
+
+        # Commented out because not needed until ADR0009 is implemented
+        # for i in range(named_args_start_index, len(self.tokens)):
+        #     arg = self.tokens[i]
+        #     if arg.type != BlockTypes.NAMED_ARG:
+        #         error_msg = "Functions support up to one positional argument"
+        #         logger.error(error_msg)
+        #         raise Exception(f"Unexpected first token type: {arg.type}")
+
+        #     if first_positional_parameter_name and first_positional_parameter_name.lower() == arg.name.lower():
+        #         raise ValueError(
+        #             f"Ambiguity found as a named parameter '{arg.name}' cannot be set for the first parameter "
+        #             f"when there is also a positional value: '{first_positional_input_value}' provided. "
+        #             f"Function: {function_block.plugin_name}.{function_block.function_name}"
+        #         )
+
+        #     arguments[arg.name] = arg.get_value(arguments)
+
+        return arguments
+
     def _get_function_from_plugin_collection(
-        self, plugins: KernelPluginCollection, f_block: FunctionIdBlock
+        self, plugins: KernelPluginCollection, function_block: FunctionIdBlock
     ) -> Optional[KernelFunction]:
         """
         Get the function from the plugin collection
 
         Args:
             plugins: The plugin collection
-            f_block: The function block that contains the function name
+            function_block: The function block that contains the function name
 
         Returns:
             The function if it exists, None otherwise.
         """
-        if f_block.plugin_name is not None and len(f_block.plugin_name) > 0:
-            return plugins[f_block.plugin_name][f_block.function_name]
+        if function_block.plugin_name is not None and len(function_block.plugin_name) > 0:
+            return plugins[function_block.plugin_name][function_block.function_name]
         else:
             # We now require a plug-in name, but if one isn't set then we'll try to find the function
             for plugin in plugins:
-                if f_block.function_name in plugin:
-                    return plugin[f_block.function_name]
+                if function_block.function_name in plugin:
+                    return plugin[function_block.function_name]
 
         return None
