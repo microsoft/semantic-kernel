4c02d57caea036379a56ecf0246508192d519402
Krzysztof Kasprowicz <krzysztof@impe.pl>
2024-02-14 14:56:19 +0100

    Add mutability control in GeminiPromptExecutionSettings class

--- dotnet/src/Connectors/Connectors.GoogleVertexAI/Settings/GeminiPromptExecutionSettings.cs
+++ dotnet/src/Connectors/Connectors.GoogleVertexAI/Settings/GeminiPromptExecutionSettings.cs
@@ -2,6 +2,8 @@
 
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using Microsoft.SemanticKernel.Text;
@@ -14,6 +16,14 @@ namespace Microsoft.SemanticKernel.Connectors.GoogleVertexAI;
 [JsonNumberHandling(JsonNumberHandling.AllowReadingFromString)]
 public sealed class GeminiPromptExecutionSettings : PromptExecutionSettings
 {
+    private double? _temperature;
+    private double? _topP;
+    private int? _topK;
+    private int? _maxTokens;
+    private int? _candidateCount;
+    private IList<string>? _stopSequences;
+    private IList<GeminiSafetySetting>? _safetySettings;
+
     /// <summary>
     /// Default max tokens for a text generation.
     /// </summary>
@@ -25,46 +35,135 @@ public sealed class GeminiPromptExecutionSettings : PromptExecutionSettings
     /// Range is 0.0 to 1.0.
     /// </summary>
     [JsonPropertyName("temperature")]
-    public double? Temperature { get; set; }
+    public double? Temperature
+    {
+        get => this._temperature;
+        set
+        {
+            this.ThrowIfFrozen();
+            this._temperature = value;
+        }
+    }
 
     /// <summary>
     /// TopP controls the diversity of the completion.
     /// The higher the TopP, the more diverse the completion.
     /// </summary>
     [JsonPropertyName("top_p")]
-    public double? TopP { get; set; }
+    public double? TopP
+    {
+        get => this._topP;
+        set
+        {
+            this.ThrowIfFrozen();
+            this._topP = value;
+        }
+    }
 
     /// <summary>
     /// Gets or sets the value of the TopK property.
     /// The TopK property represents the maximum value of a collection or dataset.
     /// </summary>
     [JsonPropertyName("top_k")]
-    public int? TopK { get; set; }
+    public int? TopK
+    {
+        get => this._topK;
+        set
+        {
+            this.ThrowIfFrozen();
+            this._topK = value;
+        }
+    }
 
     /// <summary>
     /// The maximum number of tokens to generate in the completion.
     /// </summary>
     [JsonPropertyName("max_tokens")]
-    public int? MaxTokens { get; set; }
+    public int? MaxTokens
+    {
+        get => this._maxTokens;
+        set
+        {
+            this.ThrowIfFrozen();
+            this._maxTokens = value;
+        }
+    }
 
     /// <summary>
     /// The count of candidates. Possible values range from 1 to 8.
     /// </summary>
     [JsonPropertyName("candidate_count")]
-    public int? CandidateCount { get; set; }
+    public int? CandidateCount
+    {
+        get => this._candidateCount;
+        set
+        {
+            this.ThrowIfFrozen();
+            this._candidateCount = value;
+        }
+    }
 
     /// <summary>
     /// Sequences where the completion will stop generating further tokens.
     /// Maximum number of stop sequences is 5.
     /// </summary>
     [JsonPropertyName("stop_sequences")]
-    public IList<string>? StopSequences { get; set; }
+    public IList<string>? StopSequences
+    {
+        get => this._stopSequences;
+        set
+        {
+            this.ThrowIfFrozen();
+            this._stopSequences = value;
+        }
+    }
 
     /// <summary>
     /// Represents a list of safety settings.
     /// </summary>
     [JsonPropertyName("safety_settings")]
-    public IList<GeminiSafetySetting>? SafetySettings { get; set; }
+    public IList<GeminiSafetySetting>? SafetySettings
+    {
+        get => this._safetySettings;
+        set
+        {
+            this.ThrowIfFrozen();
+            this._safetySettings = value;
+        }
+    }
+
+    /// <inheritdoc />
+    public override void Freeze()
+    {
+        base.Freeze();
+
+        if (this._stopSequences is not null)
+        {
+            this._stopSequences = new ReadOnlyCollection<string>(this._stopSequences);
+        }
+
+        if (this._safetySettings is not null)
+        {
+            this._safetySettings = new ReadOnlyCollection<GeminiSafetySetting>(this._safetySettings);
+        }
+    }
+
+    /// <inheritdoc />
+    public override PromptExecutionSettings Clone()
+    {
+        return new GeminiPromptExecutionSettings()
+        {
+            ModelId = this.ModelId,
+            ExtensionData = this.ExtensionData is not null ? new Dictionary<string, object>(this.ExtensionData) : null,
+            Temperature = this.Temperature,
+            TopP = this.TopP,
+            TopK = this.TopK,
+            MaxTokens = this.MaxTokens,
+            CandidateCount = this.CandidateCount,
+            StopSequences = this.StopSequences?.ToList(),
+            SafetySettings = this.SafetySettings?.Select(setting => new GeminiSafetySetting(setting)).ToList(),
+        };
+    }
 
     /// <summary>
     /// Converts a <see cref="PromptExecutionSettings"/> object to a <see cref="GeminiPromptExecutionSettings"/> object.
