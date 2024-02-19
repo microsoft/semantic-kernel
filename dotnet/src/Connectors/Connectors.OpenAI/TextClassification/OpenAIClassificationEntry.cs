c0282cc89f80419cbf167e136947a73227418e0d
Krzysztof Kasprowicz <krzysztof@impe.pl>
2024-02-19 23:48:27 +0100

    docs

--- dotnet/src/Connectors/Connectors.OpenAI/TextClassification/OpenAIClassificationEntry.cs
+++ dotnet/src/Connectors/Connectors.OpenAI/TextClassification/OpenAIClassificationEntry.cs
@@ -20,12 +20,13 @@ public sealed class OpenAIClassificationEntry
     public OpenAIClassificationCategory Category { get; }
 
     /// <summary>
-    /// Gets a value indicating whether the category is flagged.
+    /// Gets a value indicating whether the category violates the OpenAI's policy.
     /// </summary>
     public bool Flagged { get; }
 
     /// <summary>
-    /// Represents a score for the OpenAI classification result.
+    /// Raw scores output by the model, denoting the model's confidence that the input violates the OpenAI's policy for the category.
+    /// The value is between 0 and 1, where higher values denote higher confidence. The scores should not be interpreted as probabilities.
     /// </summary>
     public double Score { get; }
 
