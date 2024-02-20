0e5ad040f72e3eee5e9639fc6a30b764d99656f0
Evan Mattson <evan.mattson@microsoft.com>
2024-02-20 15:33:03 -0500

    Update test from merge issue

--- python/tests/unit/text/test_function_extension.py
+++ python/tests/unit/text/test_function_extension.py
@@ -40,9 +40,6 @@ async def test_aggregate_results():
     func.function = lambda function, kernel, arguments, service, request_settings, chat_history: FunctionResult(
         function=function, value=arguments["input"], metadata={}
     )
-    func.function = lambda function, kernel, arguments, service, request_settings: FunctionResult(
-        function=function, value=arguments["input"], metadata={}
-    )
 
     chunked = [
         "This is a test of the emergency broadcast system.",
