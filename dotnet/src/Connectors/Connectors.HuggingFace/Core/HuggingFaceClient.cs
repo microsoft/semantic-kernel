1c378ead0ba0ee86b8af8bfe790c7fc442a03110
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2024-02-21 10:41:00 +0000

    Adding client and missing components to huggingface connectors

---
+++ dotnet/src/Connectors/Connectors.HuggingFace/Core/HuggingFaceClient.cs
@@ -0,0 +1,181 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.SemanticKernel.Http;
+
+namespace Microsoft.SemanticKernel.Connectors.HuggingFace.Core;
+
+internal sealed class HuggingFaceClient : IHuggingFaceClient
+{
+    private readonly IStreamJsonParser _streamJsonParser;
+    private readonly string _modelId;
+
+    private IHttpRequestFactory HttpRequestFactory { get; }
+    private IEndpointProvider EndpointProvider { get; }
+    private HttpClient HttpClient { get; }
+    private ILogger Logger { get; }
+
+    internal HuggingFaceClient(
+        string modelId,
+        HttpClient httpClient,
+        IHttpRequestFactory httpRequestFactory,
+        IEndpointProvider endpointProvider,
+        IStreamJsonParser? streamJsonParser = null,
+        ILogger? logger = null)
+    {
+        Verify.NotNullOrWhiteSpace(modelId);
+
+        this._modelId = modelId;
+        this.HttpClient = httpClient;
+        this.HttpRequestFactory = httpRequestFactory;
+        this.EndpointProvider = endpointProvider;
+        this.Logger = logger ?? NullLogger.Instance;
+        this._streamJsonParser = streamJsonParser ?? new HuggingFaceStreamJsonParser();
+    }
+
+    public async Task<IReadOnlyList<TextContent>> GenerateTextAsync(string prompt, PromptExecutionSettings? executionSettings, CancellationToken cancellationToken)
+    {
+        var endpoint = this.EndpointProvider.TextGenerationEndpoint;
+        var request = this.CreateTextRequest(prompt, executionSettings);
+        using var httpRequestMessage = this.HttpRequestFactory.CreatePost(request, endpoint);
+
+        string body = await this.SendRequestAndGetStringBodyAsync(httpRequestMessage, cancellationToken)
+            .ConfigureAwait(false);
+
+        var response = DeserializeResponse<TextGenerationResponse>(body);
+        var textContents = GetTextContentFromResponse(response, executionSettings.ModelId ?? this._modelId);
+
+        this.LogTextGenerationUsage(executionSettings);
+
+        return textContents;
+    }
+
+    public async IAsyncEnumerable<StreamingTextContent> StreamGenerateTextAsync(
+        string prompt,
+        PromptExecutionSettings? executionSettings = null,
+        [EnumeratorCancellation] CancellationToken cancellationToken = default)
+    {
+        var endpoint = this.EndpointProvider.TextGenerationEndpoint;
+        var request = this.CreateTextRequest(prompt, executionSettings);
+        request.Stream = true;
+
+        using var httpRequestMessage = this.HttpRequestFactory.CreatePost(request, endpoint);
+
+        using var response = await this.SendRequestAndGetResponseImmediatelyAfterHeadersReadAsync(httpRequestMessage, cancellationToken)
+            .ConfigureAwait(false);
+        using var responseStream = await response.Content.ReadAsStreamAndTranslateExceptionAsync()
+            .ConfigureAwait(false);
+
+        foreach (var streamingTextContent in this.ProcessTextResponseStream(responseStream))
+        {
+            yield return streamingTextContent;
+        }
+    }
+
+
+    private static void ValidateMaxTokens(int? maxTokens)
+    {
+        if (maxTokens is < 1)
+        {
+            throw new ArgumentException($"MaxTokens {maxTokens} is not valid, the value must be greater than zero");
+        }
+    }
+
+    private async Task<string> SendRequestAndGetStringBodyAsync(
+        HttpRequestMessage httpRequestMessage,
+        CancellationToken cancellationToken)
+    {
+        using var response = await this.HttpClient.SendWithSuccessCheckAsync(httpRequestMessage, cancellationToken)
+            .ConfigureAwait(false);
+        var body = await response.Content.ReadAsStringWithExceptionMappingAsync()
+            .ConfigureAwait(false);
+        return body;
+    }
+
+    private async Task<HttpResponseMessage> SendRequestAndGetResponseImmediatelyAfterHeadersReadAsync(
+        HttpRequestMessage httpRequestMessage,
+        CancellationToken cancellationToken)
+    {
+        var response = await this.HttpClient.SendWithSuccessCheckAsync(httpRequestMessage, HttpCompletionOption.ResponseHeadersRead, cancellationToken)
+            .ConfigureAwait(false);
+        return response;
+    }
+
+    private IEnumerable<StreamingTextContent> ProcessTextResponseStream(Stream stream)
+        => from ollamaResponse in this.ParseTextResponseStream(stream)
+           from textContent in this.GetTextStreamContentsFromResponse(ollamaResponse)
+           select GetStreamingTextContentFromTextContent(textContent);
+
+    private IEnumerable<TextGenerationStreamResponse> ParseTextResponseStream(Stream responseStream)
+        => this._streamJsonParser.Parse(responseStream).Select(DeserializeResponse<TextGenerationStreamResponse>);
+
+    private List<TextContent> GetTextStreamContentsFromResponse(TextGenerationStreamResponse response)
+    {
+        return new List<TextContent>
+        {
+            new(text: response.GeneratedText,
+                modelId: this._modelId,
+                innerContent: response,
+                metadata: new HuggingFaceMetadata(response))
+        };
+    }
+
+    private static StreamingTextContent GetStreamingTextContentFromTextContent(TextContent textContent)
+        => new(
+            text: textContent.Text,
+            modelId: textContent.ModelId,
+            innerContent: textContent.InnerContent,
+            metadata: textContent.Metadata);
+
+    private TextGenerationRequest CreateTextRequest(
+        string prompt,
+        PromptExecutionSettings? promptExecutionSettings)
+    {
+        var huggingFaceExecutionSettings = HuggingFacePromptExecutionSettings.FromExecutionSettings(promptExecutionSettings);
+        ValidateMaxTokens(huggingFaceExecutionSettings.MaxTokens);
+        var request = TextGenerationRequest.FromPromptAndExecutionSettings(prompt, huggingFaceExecutionSettings, this._modelId);
+        return request;
+    }
+
+    private static T DeserializeResponse<T>(string body)
+    {
+        try
+        {
+            T? deserializedResponse = JsonSerializer.Deserialize<T>(body);
+            if (deserializedResponse is null)
+            {
+                throw new JsonException("Response is null");
+            }
+
+            return deserializedResponse;
+        }
+        catch (JsonException exc)
+        {
+            throw new KernelException("Unexpected response from model", exc)
+            {
+                Data = { { "ResponseData", body } },
+            };
+        }
+    }
+
+    private static List<TextContent> GetTextContentFromResponse(TextGenerationResponse response, string modelId)
+        => response.Select(r => new TextContent(r.GeneratedText, modelId, r, Encoding.UTF8)).ToList();
+
+    private void LogTextGenerationUsage(PromptExecutionSettings executionSettings)
+    {
+        this.Logger.LogDebug(
+            "HuggingFace text generation usage: ModelId: {ModelId}",
+            executionSettings.ModelId ?? this._modelId);
+    }
+}
---
+++ dotnet/src/Connectors/Connectors.HuggingFace/Core/HuggingFaceMetadata.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Runtime.CompilerServices;
+
+namespace Microsoft.SemanticKernel.Connectors.HuggingFace.Core;
+
+/// <summary>
+/// Represents the metadata of the Ollama response.
+/// </summary>
+public sealed class HuggingFaceStreamMetadata : ReadOnlyDictionary<string, object?>
+{
+    internal HuggingFaceStreamMetadata(TextGenerationStreamResponse response) : base(new Dictionary<string, object?>())
+    {
+        this.Details = response.Details;
+        this.Index = response.Index;
+        this.GeneratedText = response.GeneratedText;
+        this.TokenId = response.Token.Id;
+        this.TokenLogProb = response.Token.LogProb;
+        this.TokenSpecial = response.Token.Special;
+    }
+
+    /// <summary>
+    /// The generated text.
+    /// This will only be populated in the last chunk of the response.
+    /// </summary>
+    public string? GeneratedText
+    {
+        get => this.GetValueFromDictionary() as string;
+        internal init => this.SetValueInDictionary(value);
+    }
+
+    /// <summary>
+    /// Detail of the current chunk of the response
+    /// </summary>
+    public string? Details
+    {
+        get => this.GetValueFromDictionary() as string;
+        internal init => this.SetValueInDictionary(value);
+    }
+
+    /// <summary>
+    /// Current token index of the response
+    /// </summary>
+    public int Index
+    {
+        get => (this.GetValueFromDictionary() as int?) ?? 0;
+        internal init => this.SetValueInDictionary(value);
+    }
+
+    /// <summary>
+    /// Unique token idenfitier for the model
+    /// </summary>
+    public int TokenId
+    {
+        get => (this.GetValueFromDictionary() as int?) ?? 0;
+        internal init => this.SetValueInDictionary(value);
+    }
+
+    private void SetValueInDictionary(object? value, [CallerMemberName] string propertyName = "")
+        => this.Dictionary[propertyName] = value;
+
+    private object? GetValueFromDictionary([CallerMemberName] string propertyName = "")
+        => this.Dictionary.TryGetValue(propertyName, out var value) ? value : null;
+}
