b3a28c509be6cb92e5f5b0d751643c2bce5133a1
Krzysztof Kasprowicz <krzysztof@impe.pl>
2024-02-22 16:13:53 +0100

    Add TextClassificationExtensions

---
+++ dotnet/src/SemanticKernel.Abstractions/AI/TextClassification/TextClassificationExtensions.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.SemanticKernel.TextClassification;
+
+/// <summary>
+/// Class sponsor that holds extension methods for <see cref ="ITextClassificationService" /> interface.
+/// </summary>
+[Experimental("SKEXP0006")]
+public static class TextClassificationExtensions
+{
+    /// <summary>
+    /// Classify text.
+    /// </summary>
+    /// <param name="service">The <see cref ="ITextClassificationService" /> service.</param>
+    /// <param name="text">The text to classify.</param>
+    /// <param name="executionSettings">The AI execution settings (optional).</param>
+    /// <param name="kernel">The <see cref="Kernel"/> containing services, plugins, and other state for use throughout the operation.</param>
+    /// <param name="cancellationToken">The <see cref="CancellationToken"/> to monitor for cancellation requests. The default is <see cref="CancellationToken.None"/>.</param>
+    /// <returns>Classification of text.</returns>
+    [Experimental("SKEXP0006")]
+    public static async Task<ClassificationContent> ClassifyTextAsync(
+        this ITextClassificationService service,
+        string text,
+        PromptExecutionSettings? executionSettings = null,
+        Kernel? kernel = null,
+        CancellationToken cancellationToken = default)
+    {
+        var list = await service.ClassifyTextAsync(new[] { text }, executionSettings, kernel, cancellationToken)
+            .ConfigureAwait(false);
+        return list.Single();
+    }
+}
---
+++ dotnet/src/SemanticKernel.UnitTests/AI/TextClassification/TextClassificationExtensionsTests.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.TextClassification;
+using Moq;
+using Xunit;
+
+namespace SemanticKernel.UnitTests.AI.TextClassification;
+
+public sealed class TextClassificationExtensionsTests
+{
+    [Fact]
+    public async Task ClassifyTextAsyncItReturnsClassificationContentAsync()
+    {
+        // Arrange
+        var serviceMock = new Mock<ITextClassificationService>();
+        var text = "text";
+        var classificationContent = new ClassificationContent(null, text, new Dictionary<string, object?>());
+        serviceMock.Setup(x => x.ClassifyTextAsync(new[] { text }, null, null, default))
+            .ReturnsAsync(new List<ClassificationContent> { classificationContent });
+
+        // Act
+        var result = await TextClassificationExtensions.ClassifyTextAsync(serviceMock.Object, text);
+
+        // Assert
+        Assert.Equal(classificationContent, result);
+    }
+
+    [Fact]
+    public async Task ClassifyTextAsyncItUsesTextClassificationServiceAsync()
+    {
+        // Arrange
+        var serviceMock = new Mock<ITextClassificationService>();
+        var text = "text";
+        var classificationContent = new ClassificationContent(null, text, new Dictionary<string, object?>());
+        serviceMock.Setup(x => x.ClassifyTextAsync(new[] { text }, null, null, default))
+            .ReturnsAsync(new List<ClassificationContent> { classificationContent });
+
+        // Act
+        await TextClassificationExtensions.ClassifyTextAsync(serviceMock.Object, text);
+
+        // Assert
+        serviceMock.VerifyAll();
+    }
+}
