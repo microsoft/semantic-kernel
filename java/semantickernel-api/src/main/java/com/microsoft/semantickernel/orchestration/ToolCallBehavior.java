ed04d5a8d7ec2c5f19cf740a834afe76d6472319
David Grieve <dsgrieve@yahoo.com>
2024-02-23 11:36:17 -0500

    Small edit to javadoc on requireFunction

--- java/semantickernel-api/src/main/java/com/microsoft/semantickernel/orchestration/ToolCallBehavior.java
+++ java/semantickernel-api/src/main/java/com/microsoft/semantickernel/orchestration/ToolCallBehavior.java
@@ -129,8 +129,8 @@ public class ToolCallBehavior {
     }
 
     /**
-     * Require a function. It will the only function to be passed to the model and be called.
-     * Only one function can be required.
+     * Require a function. The required function will be the only function passed to the model
+     * and forces the model to call the function. Only one function can be required.
      *
      * @param function The function to require.
      * @return A new ToolCallBehavior instance with the required function.
