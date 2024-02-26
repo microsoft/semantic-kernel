c241851cf3d49486aff8cd2c58f5aade9e3cbb01
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2024-02-26 14:51:32 -0500

    Apply Experimental attribute at AssemblyAI assembly level

--- dotnet/src/Connectors/Connectors.AssemblyAI/AudioToText/AssemblyAIAudioToTextService.cs
+++ dotnet/src/Connectors/Connectors.AssemblyAI/AudioToText/AssemblyAIAudioToTextService.cs
@@ -2,7 +2,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Net;
 using System.Net.Http;
@@ -21,7 +20,6 @@ namespace Microsoft.SemanticKernel.Connectors.AssemblyAI;
 /// <summary>
 /// AssemblyAI speech-to-text service.
 /// </summary>
-[Experimental("SKEXP0033")]
 public sealed class AssemblyAIAudioToTextService : IAudioToTextService
 {
     private const string BaseUrl = "https://api.assemblyai.com/";
