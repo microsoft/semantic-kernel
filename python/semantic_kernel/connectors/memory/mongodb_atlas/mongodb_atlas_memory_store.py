d6c92480b0c29142822467d3caa10f6d2b38326e
Jib <jib.adegunloye@mongodb.com>
2024-02-29 16:02:17 -0500

    Python: bugfix: min relevance score calculation filtering on mongodbatlas semantic search (#5224)
    ### Motivation and Context
    #4433 
    
    <!-- Thank you for your contribution to the semantic-kernel repo!
    Please help reviewers and future users, providing the following
    information:
      1. Why is this change required?
      2. What problem does it solve?
      3. What scenario does it contribute to?
      4. If it fixes an open issue, please link to the issue here.
    -->
    
    ### Description
    
    `$match` operation queries need a different structure than provided. As
    a result users receive `OperationFailure`s when trying to run queries.
    This introduces a fix to provide to correct query for filtering results
    by the min_relevance_score.
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [x] The code builds clean without any errors or warnings
    - [x] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [x] All unit tests pass, and I have added new tests where possible
    - [x] I didn't break anyone :smile:

--- python/semantic_kernel/connectors/memory/mongodb_atlas/mongodb_atlas_memory_store.py
+++ python/semantic_kernel/connectors/memory/mongodb_atlas/mongodb_atlas_memory_store.py
@@ -269,8 +269,8 @@ class MongoDBAtlasMemoryStore(MemoryStoreBase):
         # add meta search scoring
         pipeline.append({"$set": {"score": {"$meta": "vectorSearchScore"}}})
 
-        if min_relevance_score:
-            pipeline.append({"$match": {"$gte": ["$score", min_relevance_score]}})
+        if min_relevance_score is not None:
+            pipeline.append({"$match": {"score": {"$gte": min_relevance_score}}})
 
         cursor: MotorCommandCursor = self.database[collection_name].aggregate(pipeline)
 
