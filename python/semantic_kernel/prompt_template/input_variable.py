fabd1a8478506b626d81a357ccf02a022d330f64
Eduard van Valkenburg <eavanvalkenburg@users.noreply.github.com>
2024-03-01 20:21:07 +0100

    Python: Complex Type support for Method functions (#5260)
    ### Motivation and Context
    
    <!-- Thank you for your contribution to the semantic-kernel repo!
    Please help reviewers and future users, providing the following
    information:
      1. Why is this change required?
      2. What problem does it solve?
      3. What scenario does it contribute to?
      4. If it fixes an open issue, please link to the issue here.
    -->
    Added support for complex types (classes) for KernelFunctionFromMethod.
    
    Implements #4635 
    
    ### Description
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    This PR adds a field to the KernelParameterMetadata class for
    type_object
    When that field is filled and supplied by a KernelFunctionFromMethod
    decorator, it is used during invoke to cast the type.
    It checks if it is a pydantic type and calls `model_validate_json` in
    that case, otherwise invokes it directly as a init (like with a str),
    this then also casts strs to ints for example.
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [x] The code builds clean without any errors or warnings
    - [x] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [x] All unit tests pass, and I have added new tests where possible
    - [x] I didn't break anyone :smile:

--- python/semantic_kernel/prompt_template/input_variable.py
+++ python/semantic_kernel/prompt_template/input_variable.py
@@ -2,14 +2,12 @@
 
 from typing import Any, Optional
 
-from pydantic import Field
-
 from semantic_kernel.kernel_pydantic import KernelBaseModel
 
 
 class InputVariable(KernelBaseModel):
-    name: str = Field(..., alias="name")
-    description: Optional[str] = Field("", alias="description")
-    default: Optional[Any] = Field("", alias="default")
-    is_required: Optional[bool] = Field(True, alias="is_required")
-    json_schema: Optional[str] = Field("", alias="json_schema")
+    name: str
+    description: Optional[str] = ""
+    default: Optional[Any] = ""
+    is_required: Optional[bool] = True
+    json_schema: Optional[str] = ""
