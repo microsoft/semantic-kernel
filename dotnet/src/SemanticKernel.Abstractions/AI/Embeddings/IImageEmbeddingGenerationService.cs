a417f7b89505da669a279beb6f7af34d62db8609
Krzysztof Kasprowicz <krzysztof@impe.pl>
2024-03-02 23:01:10 +0100

    Minor

--- dotnet/src/SemanticKernel.Abstractions/AI/Embeddings/IImageEmbeddingGenerationService.cs
+++ dotnet/src/SemanticKernel.Abstractions/AI/Embeddings/IImageEmbeddingGenerationService.cs
@@ -5,7 +5,7 @@ using System.Diagnostics.CodeAnalysis;
 namespace Microsoft.SemanticKernel.Embeddings;
 
 /// <summary>
-/// Represents a generator of text embeddings of type <c>float</c>.
+/// Represents a generator of image embeddings of type <c>float</c>.
 /// </summary>
 [Experimental("SKEXP0001")]
 public interface IImageEmbeddingGenerationService : IEmbeddingGenerationService<ImageContent, float> { }
