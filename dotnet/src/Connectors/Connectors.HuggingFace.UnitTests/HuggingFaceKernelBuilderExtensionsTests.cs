a79fb4e24b44ee18eb9d00ec283d1dc846d32923
Mark Wallace <127216156+markwallace-microsoft@users.noreply.github.com>
2024-03-04 12:45:16 +0000

    .Net: Update classes so namespaces are consistent and match project location (#5275)
    ### Motivation and Context
    
    <!-- Thank you for your contribution to the semantic-kernel repo!
    Please help reviewers and future users, providing the following
    information:
      1. Why is this change required?
      2. What problem does it solve?
      3. What scenario does it contribute to?
      4. If it fixes an open issue, please link to the issue here.
    -->
    
    ### Description
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [ ] The code builds clean without any errors or warnings
    - [ ] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [ ] All unit tests pass, and I have added new tests where possible
    - [ ] I didn't break anyone :smile:

--- dotnet/src/Connectors/Connectors.HuggingFace.UnitTests/HuggingFaceKernelBuilderExtensionsTests.cs
+++ dotnet/src/Connectors/Connectors.HuggingFace.UnitTests/HuggingFaceKernelBuilderExtensionsTests.cs
@@ -1,6 +1,5 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Connectors.HuggingFace;
 using Microsoft.SemanticKernel.Embeddings;
@@ -26,19 +25,6 @@ public class HuggingFaceKernelBuilderExtensionsTests
     }
 
     [Fact]
-    public void AddHuggingFaceTextGenerationToServiceCollection()
-    {
-        var services = new ServiceCollection();
-        services.AddHuggingFaceTextGeneration("model");
-
-        var serviceProvider = services.BuildServiceProvider();
-        var service = serviceProvider.GetRequiredService<ITextGenerationService>();
-
-        Assert.NotNull(service);
-        Assert.IsType<HuggingFaceTextGenerationService>(service);
-    }
-
-    [Fact]
     public void AddHuggingFaceTextEmbeddingGenerationCreatesService()
     {
         var builder = Kernel.CreateBuilder();
@@ -48,19 +34,6 @@ public class HuggingFaceKernelBuilderExtensionsTests
         var service = kernel.GetRequiredService<ITextEmbeddingGenerationService>();
 
         Assert.NotNull(kernel);
-        Assert.NotNull(service);
-        Assert.IsType<HuggingFaceTextEmbeddingGenerationService>(service);
-    }
-
-    [Fact]
-    public void AddHuggingFaceTextEmbeddingsGenerationToServiceCollection()
-    {
-        var services = new ServiceCollection();
-        services.AddHuggingFaceTextEmbeddingGeneration("model");
-
-        var serviceProvider = services.BuildServiceProvider();
-        var service = serviceProvider.GetRequiredService<ITextEmbeddingGenerationService>();
-
         Assert.NotNull(service);
         Assert.IsType<HuggingFaceTextEmbeddingGenerationService>(service);
     }
