0268a1b227a6943b1b00fb62f255925a1d01286b
Krzysztof Kasprowicz <60486987+Krzysztof318@users.noreply.github.com>
2024-03-04 14:49:50 +0100

    .Net: Gemini - Add GeminiStreamResponse unit test (#5281)
    This commit includes the file GeminiStreamResponseTests.cs which tests
    the serialization and parsing functionalities of the GeminiResponse
    object using the GeminiStreamJsonParser. This ensures that all
    properties are populated correctly when serialized.
    
    @RogerBarreto

---
+++ dotnet/src/Connectors/Connectors.GoogleVertexAI.UnitTests/Core/Gemini/GeminiStreamResponseTests.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using Microsoft.SemanticKernel.Connectors.GoogleVertexAI;
+using Xunit;
+
+namespace SemanticKernel.Connectors.GoogleVertexAI.UnitTests.Core.Gemini;
+
+/// <summary>
+/// Tests for parsing <see cref="GeminiResponse"/> with <see cref="GeminiStreamJsonParser"/>.
+/// </summary>
+public sealed class GeminiStreamResponseTests
+{
+    private const string StreamTestDataFilePath = "./TestData/chat_stream_response.json";
+
+    [Fact]
+    public void SerializationShouldPopulateAllProperties()
+    {
+        // Arrange
+        var parser = new GeminiStreamJsonParser();
+        var stream = new MemoryStream();
+        var streamExample = File.ReadAllText(StreamTestDataFilePath);
+        var sampleResponses = JsonSerializer.Deserialize<List<GeminiResponse>>(streamExample)!;
+
+        WriteToStream(stream, streamExample);
+
+        // Act
+        var jsonChunks = parser.Parse(stream);
+        var responses = jsonChunks.Select(json => JsonSerializer.Deserialize<GeminiResponse>(json)).ToList();
+
+        // Assert
+        // Uses all because Equivalent ignores order
+        Assert.All(responses, (res, i) => Assert.Equivalent(sampleResponses[i], res));
+    }
+
+    private static void WriteToStream(Stream stream, string input)
+    {
+        using var writer = new StreamWriter(stream, leaveOpen: true);
+        writer.Write(input);
+        writer.Flush();
+        stream.Position = 0;
+    }
+}
