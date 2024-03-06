e498fad5c38030bd2d5b150a8d35bd816d3b529d
Evan Mattson <evan.mattson@microsoft.com>
2024-03-06 15:25:12 -0500

    Using typing.Set to support Python 3.8.

--- python/semantic_kernel/planners/planner_options.py
+++ python/semantic_kernel/planners/planner_options.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Callable, List, Optional
+from typing import Callable, List, Optional, Set
 
 from semantic_kernel.functions.kernel_function_metadata import KernelFunctionMetadata
 from semantic_kernel.kernel_pydantic import KernelBaseModel
@@ -9,7 +9,7 @@ from semantic_kernel.kernel_pydantic import KernelBaseModel
 class PlannerOptions(KernelBaseModel):
     """The default planner options that planners inherit from"""
 
-    excluded_plugins: set[str] = set()
-    excluded_functions: set[str] = set()
+    excluded_plugins: Set[str] = set()
+    excluded_functions: Set[str] = set()
     get_available_functions: Optional[Callable[["PlannerOptions", Optional[str]], List[KernelFunctionMetadata]]] = None
     # TODO semantic_memory_config
