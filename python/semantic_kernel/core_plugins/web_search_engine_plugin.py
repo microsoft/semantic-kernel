ab6df3a2d907ee71f6b94457dfba5929fbec46e3
Eduard van Valkenburg <eavanvalkenburg@users.noreply.github.com>
2024-03-06 18:04:06 +0100

    Python: Improve web search (#5317)
    ### Motivation and Context
    
    <!-- Thank you for your contribution to the semantic-kernel repo!
    Please help reviewers and future users, providing the following
    information:
      1. Why is this change required?
      2. What problem does it solve?
      3. What scenario does it contribute to?
      4. If it fixes an open issue, please link to the issue here.
    -->
    Update the web search plugin for the new structures, where types can be
    passed, instead of str everywhere.
    
    ### Description
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [x] The code builds clean without any errors or warnings
    - [x] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [x] All unit tests pass, and I have added new tests where possible
    - [x] I didn't break anyone :smile:

--- python/semantic_kernel/core_plugins/web_search_engine_plugin.py
+++ python/semantic_kernel/core_plugins/web_search_engine_plugin.py
@@ -1,10 +1,11 @@
 import sys
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING, List, Optional
 
 if sys.version_info >= (3, 9):
     from typing import Annotated
 else:
     from typing_extensions import Annotated
+
 from semantic_kernel.functions.kernel_function_decorator import kernel_function
 
 if TYPE_CHECKING:
@@ -20,10 +21,9 @@ class WebSearchEnginePlugin:
         kernel.import_plugin_from_object(WebSearchEnginePlugin(connector), plugin_name="WebSearch")
 
     Examples:
-        {{WebSearch.SearchAsync "What is semantic kernel?"}}
+        {{WebSearch.search "What is semantic kernel?"}}
         =>  Returns the first `num_results` number of results for the given search query
-            and ignores the first `offset` number of results
-            (num_results and offset are specified in KernelContext)
+            and ignores the first `offset` number of results.
     """
 
     _connector: "ConnectorBase"
@@ -31,13 +31,13 @@ class WebSearchEnginePlugin:
     def __init__(self, connector: "ConnectorBase") -> None:
         self._connector = connector
 
-    @kernel_function(description="Performs a web search for a given query", name="search")
+    @kernel_function(description="Performs a web search for a given query")
     async def search(
         self,
         query: Annotated[str, "The search query"],
         num_results: Annotated[Optional[int], "The number of search results to return"] = 1,
         offset: Annotated[Optional[int], "The number of search results to skip"] = 0,
-    ) -> str:
+    ) -> List[str]:
         """
         Returns the search results of the query provided.
         Returns `num_results` results and ignores the first `offset`.
@@ -45,7 +45,6 @@ class WebSearchEnginePlugin:
         :param query: search query
         :param num_results: number of search results to return, default is 1
         :param offset: number of search results to skip, default is 0
-        :return: stringified list of search results
+        :return: list of search results
         """
-        result = await self._connector.search(query, num_results, offset)
-        return str(result)
+        return await self._connector.search(query, num_results, offset)
