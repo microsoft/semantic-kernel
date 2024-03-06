4deb2ec53c54bfa8b87d7678130fd841ad7b6f4b
edvan_microsoft <edvan@microsoft.com>
2024-03-06 16:31:17 +0100

    revert model change

--- python/tests/unit/connectors/hugging_face/test_hf_local_text_completions.py
+++ python/tests/unit/connectors/hugging_face/test_hf_local_text_completions.py
@@ -14,7 +14,7 @@ from semantic_kernel.prompt_template.prompt_template_config import PromptTemplat
     ("model_name", "task", "input_str"),
     [
         (
-            "Narsil/tiny-random-bart",
+            "patrickvonplaten/t5-tiny-random",
             "text2text-generation",
             "translate English to Dutch: Hello, how are you?",
         ),
