dcc5961a930063f9cd02e642103c246b62f28dd3
Krzysztof Kasprowicz <krzysztof@impe.pl>
2024-03-11 13:53:20 +0100

    Add GoogleAI and VertexAI connector extensions
    Added two extension methods to the MemoryBuilder class for configuring connectors to the GoogleAI and VertexAI services. These extensions allow for the incorporation of text embedding generation services from GoogleAI and VertexAI, providing users with additional options for text generation services.

---
+++ dotnet/src/Connectors/Connectors.GoogleVertexAI/Extensions/GoogleAIMemoryBuilderExtensions.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Net.Http;
+using Microsoft.SemanticKernel.Connectors.GoogleVertexAI;
+using Microsoft.SemanticKernel.Http;
+using Microsoft.SemanticKernel.Memory;
+
+namespace Microsoft.SemanticKernel;
+
+/// <summary>
+/// Provides extension methods for the <see cref="MemoryBuilder"/> class to configure GoogleAI connector.
+/// </summary>
+public static class GoogleAIMemoryBuilderExtensions
+{
+    /// <summary>
+    /// Add GoogleAI embeddings generation service to the memory builder.
+    /// </summary>
+    /// <param name="builder">The <see cref="MemoryBuilder"/> instance</param>
+    /// <param name="modelId">The model for text generation.</param>
+    /// <param name="apiKey">The API key for authentication Gemini API.</param>
+    /// <param name="httpClient">The optional custom HttpClient.</param>
+    /// <returns>The updated memory builder.</returns>
+    public static MemoryBuilder WithGoogleAIEmbeddingGeneration(
+        this MemoryBuilder builder,
+        string modelId,
+        string apiKey,
+        HttpClient? httpClient = null)
+    {
+        Verify.NotNull(builder);
+        Verify.NotNull(modelId);
+        Verify.NotNull(apiKey);
+
+        return builder.WithTextEmbeddingGeneration((loggerFactory, builderHttpClient) =>
+            new GoogleAITextEmbeddingGenerationService(
+                model: modelId,
+                apiKey: apiKey,
+                httpClient: HttpClientProvider.GetHttpClient(httpClient ?? builderHttpClient),
+                loggerFactory: loggerFactory));
+    }
+}
---
+++ dotnet/src/Connectors/Connectors.GoogleVertexAI/Extensions/VertexAIMemoryBuilderExtensions.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Net.Http;
+using Microsoft.SemanticKernel.Connectors.GoogleVertexAI;
+using Microsoft.SemanticKernel.Http;
+using Microsoft.SemanticKernel.Memory;
+
+namespace Microsoft.SemanticKernel;
+
+/// <summary>
+/// Provides extension methods for the <see cref="MemoryBuilder"/> class to configure VertexAI connector.
+/// </summary>
+public static class VertexAIMemoryBuilderExtensions
+{
+    /// <summary>
+    /// Add VertexAI embeddings generation service to the memory builder.
+    /// </summary>
+    /// <param name="builder">The <see cref="MemoryBuilder"/> instance</param>
+    /// <param name="modelId">The model for text generation.</param>
+    /// <param name="bearerKey">The Bearer Key for authentication.</param>
+    /// <param name="location">The location to process the request</param>
+    /// <param name="projectId">Your project ID</param>
+    /// <param name="httpClient">The optional custom HttpClient.</param>
+    /// <returns>The updated memory builder.</returns>
+    public static MemoryBuilder WithVertexAIEmbeddingGeneration(
+        this MemoryBuilder builder,
+        string modelId,
+        string bearerKey,
+        string location,
+        string projectId,
+        HttpClient? httpClient = null)
+    {
+        Verify.NotNull(builder);
+        Verify.NotNull(modelId);
+        Verify.NotNull(bearerKey);
+        Verify.NotNull(location);
+        Verify.NotNull(projectId);
+
+        return builder.WithTextEmbeddingGeneration((loggerFactory, builderHttpClient) =>
+            new VertexAITextEmbeddingGenerationService(
+                model: modelId,
+                bearerKey: bearerKey,
+                location: location,
+                projectId: projectId,
+                httpClient: HttpClientProvider.GetHttpClient(httpClient ?? builderHttpClient),
+                loggerFactory: loggerFactory));
+    }
+}
