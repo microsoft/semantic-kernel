373614137421400d616fdcfc20885fa6242ec738
Krzysztof Kasprowicz <krzysztof@impe.pl>
2024-03-11 14:19:13 +0100

    Added tests

---
+++ dotnet/src/Connectors/Connectors.GoogleVertexAI.UnitTests/Extensions/VertexAIMemoryBuilderExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Memory;
+using Moq;
+using Xunit;
+
+namespace SemanticKernel.Connectors.GoogleVertexAI.UnitTests.Extensions;
+
+/// <summary>
+/// Unit tests for <see cref="VertexAIMemoryBuilderExtensions"/> class.
+/// </summary>
+public sealed class VertexAIMemoryBuilderExtensionsTests
+{
+    private readonly Mock<IMemoryStore> _mockMemoryStore = new();
+
+    [Fact]
+    public void ShouldBuildMemoryWithVertexAIEmbeddingGenerator()
+    {
+        // Arrange
+        var builder = new MemoryBuilder();
+
+        // Act
+        var memory = builder
+            .WithVertexAITextEmbeddingGeneration("fake-model", "fake-bearer-key", "fake-location", "fake-project")
+            .WithMemoryStore(this._mockMemoryStore.Object)
+            .Build();
+
+        // Assert
+        Assert.NotNull(memory);
+    }
+}
