9724c80627eb1cd597c00742f3514ae05c1d66c8
Sarfaraz Ahmed <20cs067@students.muet.edu.pk>
2024-03-12 17:50:06 +0500

    Update chat_gpt_api.py
    there are some errors in chat.add_user_message/assistant_message.
    You cannot just make chat as a object of ChatHistory, instead make it as 
    
    chat_history = ChatHistory(system_message=system_message)
    
    also define service_id and pass it into kernel.add_service: such as :
    
    service_id = "chat-gpt"
    
    kernel.add_service(
        sk_oai.OpenAIChatCompletion(
            service_id="chat-gpt", ai_model_id="gpt-3.5-turbo", api_key=api_key)
    )

--- python/samples/kernel-syntax-examples/chat_gpt_api.py
+++ python/samples/kernel-syntax-examples/chat_gpt_api.py
@@ -11,6 +11,10 @@ from semantic_kernel.contents.chat_history import ChatHistory
 from semantic_kernel.functions.kernel_arguments import KernelArguments
 from semantic_kernel.prompt_template.input_variable import InputVariable
 from semantic_kernel.prompt_template.prompt_template_config import PromptTemplateConfig
+# import os
+# from dotenv import load_dotenv
+# load_dotenv('var.env')
+# api_key = os.getenv("OPENAI_API_KEY")
 
 system_message = """
 You are a chat bot. Your name is Mosscap and
@@ -25,10 +29,12 @@ kernel = sk.Kernel()
 
 api_key, org_id = sk.openai_settings_from_dot_env()
 kernel.add_service(
-    sk_oai.OpenAIChatCompletion(service_id="chat-gpt", ai_model_id="gpt-3.5-turbo", api_key=api_key, org_id=org_id)
+    sk_oai.OpenAIChatCompletion(
+        service_id="chat-gpt", ai_model_id="gpt-3.5-turbo", api_key=api_key)
 )
-
-settings = kernel.get_prompt_execution_settings_from_service(ChatCompletionClientBase, "chat-gpt")
+service_id = "chat-gpt"
+settings = kernel.get_service(
+    service_id).get_prompt_execution_settings_class()(service_id=service_id)
 settings.max_tokens = 2000
 settings.temperature = 0.7
 settings.top_p = 0.8
@@ -53,15 +59,17 @@ prompt_template_config = PromptTemplateConfig(
     execution_settings=settings,
 )
 
-chat = ChatHistory(system_message=system_message)
-chat.add_user_message("Hi there, who are you?")
-chat.add_assistant_message("I am Mosscap, a chat bot. I'm trying to figure out what people need")
+chat_history = ChatHistory(system_message=system_message)
+chat_history.add_user_message("Hi there, who are you?")
+chat_history.add_assistant_message(
+    "I am Mosscap, a chat bot. I'm trying to figure out what people need")
 
 chat_function = kernel.create_function_from_prompt(
     plugin_name="ChatBot", function_name="Chat", prompt_template_config=prompt_template_config
 )
 
-chat.add_user_message("I want to find a hotel in Seattle with free wifi and a pool.")
+chat_history.add_user_message(
+    "I want to find a hotel in Seattle with free wifi and a pool.")
 
 
 async def chat() -> bool:
@@ -79,8 +87,8 @@ async def chat() -> bool:
         return False
 
     answer = await kernel.invoke(chat_function, KernelArguments(user_input=user_input, chat_history=chat))
-    chat.add_user_message(user_input)
-    chat.add_assistant_message(str(answer))
+    chat_history.add_user_message(user_input)
+    chat_history.add_assistant_message(str(answer))
     print(f"Mosscap:> {answer}")
     return True
 
