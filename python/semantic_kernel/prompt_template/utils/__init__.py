5694c9823b71b3e845b1346c7893dc5f283f1451
Evan Mattson <evan.mattson@microsoft.com>
2024-03-14 17:06:38 -0400

    Support jinja2 templating

--- python/semantic_kernel/prompt_template/utils/__init__.py
+++ python/semantic_kernel/prompt_template/utils/__init__.py
@@ -1,9 +1,13 @@
 # Copyright (c) Microsoft. All rights reserved.
 
-from semantic_kernel.prompt_template.utils.handlebars_function_helpers import create_helper_from_function
+from semantic_kernel.prompt_template.utils.handlebars_function_helpers import create_handlebars_helper_from_function
 from semantic_kernel.prompt_template.utils.handlebars_system_helpers import HANDLEBAR_SYSTEM_HELPERS
+from semantic_kernel.prompt_template.utils.jinja2_function_helpers import create_jinja2_helper_from_function
+from semantic_kernel.prompt_template.utils.jinja2_system_helpers import JINJA2_SYSTEM_HELPERS
 
 __all__ = [
-    "create_helper_from_function",
+    "create_handlebars_helper_from_function",
+    "create_jinja2_helper_from_function",
     "HANDLEBAR_SYSTEM_HELPERS",
+    "JINJA2_SYSTEM_HELPERS",
 ]
--- python/semantic_kernel/prompt_template/utils/jinja2_function_helpers.py
+++ python/semantic_kernel/prompt_template/utils/jinja2_function_helpers.py
@@ -15,20 +15,20 @@ if TYPE_CHECKING:
 logger: logging.Logger = logging.getLogger(__name__)
 
 
-def create_handlebars_helper_from_function(
+def create_jinja2_helper_from_function(
     function: "KernelFunction", kernel: "Kernel", base_arguments: "KernelArguments"
 ) -> Callable:
-    """Create a helper function for the Handlebars templating engine from a kernel function."""
+    """Create a helper function for the Jinja2 templating engine from a kernel function."""
     if not getattr(asyncio, "_nest_patched", False):
         nest_asyncio.apply()
 
-    def func(this, *args, **kwargs):
+    def func(*args, **kwargs):
         arguments = base_arguments.copy()
         arguments.update(kwargs)
 
         logger.debug(
-            f"Invoking function {function.metadata.fully_qualified_name} "
-            f"with args: {args} and kwargs: {kwargs} and this: {this}."
+            f"Invoking function {function.metadata.fully_qualified_name.replace('-', '_')} "
+            f"with args: {args} and kwargs: {kwargs}."
         )
         return asyncio.run(function.invoke(kernel=kernel, arguments=arguments))
 
