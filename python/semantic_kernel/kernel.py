1ff1a0cb455c107e13ea6fa5194b8f723da1398c
eavanvalkenburg <edvan@microsoft.com>
2024-03-19 09:53:24 +0100

    small update to pre-commit config to make it run only against changed files

--- python/semantic_kernel/kernel.py
+++ python/semantic_kernel/kernel.py
@@ -53,6 +53,7 @@ from semantic_kernel.utils.validation import validate_plugin_name
 T = TypeVar("T")
 
 ALL_SERVICE_TYPES = "TextCompletionClientBase | ChatCompletionClientBase | EmbeddingGeneratorBase"
+COMPLETION_TYES = "TextCompletionClientBase | ChatCompletionClientBase"
 
 logger: logging.Logger = logging.getLogger(__name__)
 
