a35293facc1ec3320a9207e3da5deb9068631863
Stephen Toub <stoub@microsoft.com>
2024-03-20 09:16:48 -0400

    Avoid duplicated logic between GetOpenAIClientOptions impls

--- dotnet/src/Connectors/Connectors.OpenAI/TextToImage/AzureOpenAITextToImageService.cs
+++ dotnet/src/Connectors/Connectors.OpenAI/TextToImage/AzureOpenAITextToImageService.cs
@@ -127,29 +127,12 @@ public sealed class AzureOpenAITextToImageService : ITextToImageService
         return imageGenerations.Value.Data[0].Url.AbsoluteUri;
     }
 
-    private static OpenAIClientOptions GetClientOptions(HttpClient? httpClient, string? apiVersion)
-    {
-        OpenAIClientOptions.ServiceVersion version = apiVersion switch
+    private static OpenAIClientOptions GetClientOptions(HttpClient? httpClient, string? apiVersion) =>
+        ClientCore.GetOpenAIClientOptions(httpClient, apiVersion switch
         {
             // DALL-E 3 is supported in the latest API releases
             _ => OpenAIClientOptions.ServiceVersion.V2024_02_15_Preview
-        };
-
-        var options = new OpenAIClientOptions(version)
-        {
-            Diagnostics = { ApplicationId = HttpHeaderConstant.Values.UserAgent }
-        };
-
-        if (httpClient != null)
-        {
-            // Disable retries when using a custom HttpClient
-            options.RetryPolicy = new RetryPolicy(maxRetries: 0);
-
-            options.Transport = new HttpClientTransport(httpClient);
-        }
-
-        return options;
-    }
+        });
 
     internal void AddAttribute(string key, string? value)
     {
