b08fa59a423e13c7fb26f433dbb5eca3f7d15ce9
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2024-03-20 16:55:43 +0000

    Fix warning

--- dotnet/samples/KernelSyntaxExamples/Example82_OllamaTextEmbeddingGeneration.cs
+++ dotnet/samples/KernelSyntaxExamples/Example82_OllamaTextEmbeddingGeneration.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.SemanticKernel;
-using Microsoft.SemanticKernel.Connectors.Ollama;
 using Microsoft.SemanticKernel.Embeddings;
 using Xunit;
 using Xunit.Abstractions;
