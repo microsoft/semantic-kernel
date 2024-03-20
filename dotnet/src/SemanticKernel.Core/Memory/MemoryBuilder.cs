cbfdcfb97c4952db90955792e57cfd24fb5b4a03
Aaron Powell <me@aaron-powell.com>
2024-03-21 10:42:53 +1100

    Removing unneeded using

--- dotnet/src/SemanticKernel.Core/Memory/MemoryBuilder.cs
+++ dotnet/src/SemanticKernel.Core/Memory/MemoryBuilder.cs
@@ -3,7 +3,6 @@
 using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Net.Http;
-using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.SemanticKernel.Embeddings;
