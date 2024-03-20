02866be210b1749e0e6eca66fee147b92ca9833e
Stephen Toub <stoub@microsoft.com>
2024-03-20 05:11:14 -0400

    .Net: Disable Azure SDK network timeout when a custom HttpClient is supplied (#5553)
    Fixes https://github.com/microsoft/semantic-kernel/issues/5310

--- dotnet/src/Connectors/Connectors.OpenAI/AzureSdk/ClientCore.cs
+++ dotnet/src/Connectors/Connectors.OpenAI/AzureSdk/ClientCore.cs
@@ -749,6 +749,7 @@ internal abstract class ClientCore
         {
             options.Transport = new HttpClientTransport(httpClient);
             options.RetryPolicy = new RetryPolicy(maxRetries: 0); // Disable Azure SDK retry policy if and only if a custom HttpClient is provided.
+            options.Retry.NetworkTimeout = Timeout.InfiniteTimeSpan; // Disable Azure SDK default timeout
         }
 
         return options;
