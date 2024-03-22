8e0bd9c5e35850e34de2232e5d4e0301513145df
Evan Mattson <evmattso@microsoft.com>
2024-03-22 19:10:54 -0400

    Allow function calling stepwise planner to use AzureOpenAI chat service. Fix some formatting issues when generating the plan.

--- python/semantic_kernel/planners/planner_extensions.py
+++ python/semantic_kernel/planners/planner_extensions.py
@@ -2,6 +2,7 @@
 
 import logging
 
+from semantic_kernel.connectors.ai.open_ai.utils import get_function_calling_object
 from semantic_kernel.functions.kernel_arguments import KernelArguments
 from semantic_kernel.functions.kernel_function_metadata import KernelFunctionMetadata
 from semantic_kernel.kernel import Kernel
@@ -68,3 +69,20 @@ class PlannerKernelExtension:
         # TODO support for semantic memory query
 
         return available_functions
+
+    @staticmethod
+    async def get_available_functions_json(
+        kernel: Kernel,
+        arguments: KernelArguments,
+        options: PlannerOptions = None,
+    ):
+        excluded_plugins = options.excluded_plugins or []
+        excluded_functions = options.excluded_functions or []
+
+        available_functions = [
+            func
+            for func in kernel.plugins.get_list_of_function_metadata()
+            if (func.plugin_name not in excluded_plugins and func.name not in excluded_functions)
+        ]
+
+        return [get_function_calling_object(func, {}) for func in available_functions]
