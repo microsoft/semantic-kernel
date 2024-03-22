dfd78d9786de5159e34786e1e0712ada4ce94c57
Evan Mattson <evmattso@microsoft.com>
2024-03-22 19:01:49 -0400

    Remove jinja2 built in helpers from custom helpers. Introduce messages custom helper func for jinja2/handlebars

--- python/semantic_kernel/prompt_template/utils/jinja2_system_helpers.py
+++ python/semantic_kernel/prompt_template/utils/jinja2_system_helpers.py
@@ -1,15 +1,20 @@
 # Copyright (c) Microsoft. All rights reserved.
 
-import json
 import logging
 import re
 from enum import Enum
 from typing import Callable, Dict
 
-from semantic_kernel.contents.chat_history import ROOT_KEY_MESSAGE
+from semantic_kernel.contents.chat_history import ROOT_KEY_MESSAGE, ChatHistory
 from semantic_kernel.contents.chat_message_content import ChatMessageContent
 
 logger: logging.Logger = logging.getLogger(__name__)
+
+
+def _messages(chat_history):
+    if not isinstance(chat_history, ChatHistory):
+        return ""
+    return str(chat_history)
 
 
 def _message_to_prompt(context):
@@ -57,65 +62,6 @@ def _array(*args, **kwargs):
     return list(args)
 
 
-def _range(*args, **kwargs):
-    args = list(args)
-    for index, arg in enumerate(args):
-        if not isinstance(arg, int):
-            try:
-                args[index] = int(arg)
-            except ValueError:
-                args.pop(index)
-    if len(args) == 1:
-        return list(range(args[0]))
-    if len(args) == 2:
-        return list(range(args[0], args[1]))
-    if len(args) == 3:
-        return list(range(args[0], args[1], args[2]))
-    return []
-
-
-def _concat(*args, **kwargs):
-    return "".join([str(value) for value in args])
-
-
-def _or(*args, **kwargs):
-    return any(args)
-
-
-def _add(*args, **kwargs):
-    return sum([float(value) for value in args])
-
-
-def _subtract(*args, **kwargs):
-    return float(args[0]) - sum([float(value) for value in args[1:]])
-
-
-def _equals(*args, **kwargs):
-    return args[0] == args[1]
-
-
-def _less_than(*args, **kwargs):
-    return float(args[0]) < float(args[1])
-
-
-def _greater_than(*args, **kwargs):
-    return float(args[0]) > float(args[1])
-
-
-def _less_than_or_equal(*args, **kwargs):
-    return float(args[0]) <= float(args[1])
-
-
-def _greater_than_or_equal(*args, **kwargs):
-    return float(args[0]) >= float(args[1])
-
-
-def _json(*args, **kwargs):
-    if not args:
-        return ""
-    return json.dumps(args[0])
-
-
 def _camel_case(*args, **kwargs):
     return "".join([word.capitalize() for word in args[0].split("_")])
 
@@ -136,23 +82,9 @@ JINJA2_SYSTEM_HELPERS: Dict[str, Callable] = {
     "doubleClose": _double_close,
     "message": _message,
     "message_to_prompt": _message_to_prompt,
+    "messages": _messages,
     "messageToPrompt": _message_to_prompt,
     "array": _array,
-    "range": _range,
-    "concat": _concat,
-    "or": _or,
-    "add": _add,
-    "subtract": _subtract,
-    "equals": _equals,
-    "less_than": _less_than,
-    "lessThan": _less_than,
-    "greater_than": _greater_than,
-    "greaterThan": _greater_than,
-    "less_than_or_equal": _less_than_or_equal,
-    "lessThanOrEqual": _less_than_or_equal,
-    "greater_than_or_equal": _greater_than_or_equal,
-    "greaterThanOrEqual": _greater_than_or_equal,
-    "json": _json,
     "camel_case": _camel_case,
     "camelCase": _camel_case,
     "snake_case": _snake_case,
