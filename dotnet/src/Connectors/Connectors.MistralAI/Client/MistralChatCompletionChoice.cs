ef43bd2836ce76bc8737572493c974d6db2e44d2
Mark Wallace <127216156+markwallace-microsoft@users.noreply.github.com>
2024-03-22 11:36:29 +0000

    .Net: Mistral Streaming Chat Completion (#5548)
    ### Motivation and Context
    
    <!-- Thank you for your contribution to the semantic-kernel repo!
    Please help reviewers and future users, providing the following
    information:
      1. Why is this change required?
      2. What problem does it solve?
      3. What scenario does it contribute to?
      4. If it fixes an open issue, please link to the issue here.
    -->
    
    Mistral connector doesn't support streaming completion.
    
    ### Description
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    Support streaming completion for mistral connector.
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [ ] The code builds clean without any errors or warnings
    - [ ] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [ ] All unit tests pass, and I have added new tests where possible
    - [ ] I didn't break anyone :smile:

---
+++ dotnet/src/Connectors/Connectors.MistralAI/Client/MistralChatCompletionChoice.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Text.Json.Serialization;
+
+namespace Microsoft.SemanticKernel.Connectors.MistralAI.Client;
+
+/// <summary>
+/// Mistral chat completion choice.
+/// </summary>
+internal class MistralChatCompletionChoice
+{
+    [JsonPropertyName("finish_reason")]
+    public string? FinishReason { get; set; }
+
+    [JsonPropertyName("index")]
+    public int? Index { get; set; }
+
+    [JsonPropertyName("delta")]
+    public MistralChatMessage? Delta { get; set; }
+
+    [JsonPropertyName("logprobs")]
+    public string? LogProbs { get; set; }
+}
--- dotnet/src/Connectors/Connectors.MistralAI/Client/MistralChatCompletionChunk.cs
+++ dotnet/src/Connectors/Connectors.MistralAI/Client/MistralChatCompletionChunk.cs
@@ -1,61 +1,77 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
-using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json.Serialization;
 using Microsoft.SemanticKernel.ChatCompletion;
 
 namespace Microsoft.SemanticKernel.Connectors.MistralAI.Client;
 
-/*
-{
-    "id": "83632e31ce19471f9163a5288cdf0bcb",
-    "object": "chat.completion.chunk",
-    "created": 1709762658,
-    "model": "mistral-tiny",
-    "choices": [
-        {
-            "index": 0,
-            "delta": {
-                "role": "assistant",
-                "content": ""
-            },
-            "finish_reason": null,
-            "logprobs": null
-        }
-    ],
-    "usage": null
-}
- */
+/// <summary>
+/// Represents a chat completion chunk from Mistral.
+/// </summary>
 internal class MistralChatCompletionChunk
 {
-    internal string? GetText()
-    {
-        throw new NotImplementedException();
-    }
+    [JsonPropertyName("id")]
+    public string? Id { get; set; }
+
+    [JsonPropertyName("object")]
+    public string? Object { get; set; }
+
+    [JsonPropertyName("created")]
+    public int Created { get; set; }
+
+    [JsonPropertyName("model")]
+    public string? Model { get; set; }
+
+    [JsonPropertyName("choices")]
+    public List<MistralChatCompletionChoice>? Choices { get; set; }
+
+    [JsonPropertyName("usage")]
+    public MistralUsage? Usage { get; set; }
 
     internal IReadOnlyDictionary<string, object?>? GetMetadata()
     {
-        throw new NotImplementedException();
+        if (this._metadata is null)
+        {
+            this._metadata = new Dictionary<string, object?>(4)
+            {
+                { nameof(MistralChatCompletionChunk.Id), this.Id },
+                { nameof(MistralChatCompletionChunk.Model), this.Model },
+                { nameof(MistralChatCompletionChunk.Created), this.Created },
+                { nameof(MistralChatCompletionChunk.Object), this.Object },
+                { nameof(MistralChatCompletionChunk.Usage), this.Usage },
+            };
+        }
+
+        return this._metadata;
     }
 
-    internal AuthorRole? GetRole()
+    internal int GetChoiceCount()
     {
-        throw new NotImplementedException();
+        return this.Choices?.Count ?? 0;
     }
 
-    internal string? GetContent()
+    internal AuthorRole? GetRole(int index)
     {
-        throw new NotImplementedException();
+        var role = this.Choices?[index]?.Delta?.Role;
+        return role is null ? null : new AuthorRole(role);
     }
 
-    internal int GetChoiceIndex()
+    internal string? GetContent(int index)
     {
-        throw new NotImplementedException();
+        return this.Choices?[index]?.Delta?.Content;
+    }
+
+    internal int GetChoiceIndex(int index)
+    {
+        return this.Choices?[index]?.Index ?? -1;
     }
 
     internal Encoding? GetEncoding()
     {
-        throw new NotImplementedException();
+        return null;
     }
+
+    private IReadOnlyDictionary<string, object?>? _metadata;
 }
