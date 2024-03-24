2d0103ee2595b9c3c7b76b539d41653dd610ab80
markwallace-microsoft <127216156+markwallace-microsoft@users.noreply.github.com>
2024-03-24 19:35:39 +0000

    More function calling models

---
+++ dotnet/src/Connectors/Connectors.MistralAI/Client/MistralParameters.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Collections.Generic;
+
+namespace Microsoft.SemanticKernel.Connectors.MistralAI.Client;
+internal class MistralParameters
+{
+    public string Type { get; set; }
+
+    public IDictionary<string, MistralPropertyType> Properties { get; set; }
+
+    public IList<string> Required { get; set; }
+}
---
+++ dotnet/src/Connectors/Connectors.MistralAI/Client/MistralPropertyType.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Collections.Generic;
+
+namespace Microsoft.SemanticKernel.Connectors.MistralAI.Client;
+
+internal class MistralPropertyType
+{
+    public string Type { get; set; }
+
+    public string? Description { get; set; }
+
+    public IList<string>? Enum { get; set; }
+
+    public MistralPropertyType(string type, string? description = null, IList<string>? @enum = null)
+    {
+        Verify.NotNull(type, nameof(type));
+
+        this.Type = type;
+        this.Description = description;
+        this.Enum = @enum;
+    }
+}
