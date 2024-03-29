78fac1b35b757d7f68bc40f9c55a32d64e8f7bbc
Chris Rickman <crickman@microsoft.com>
2024-03-29 10:15:38 -0700

    One more (StreamingChatMessageContent)

--- dotnet/src/SemanticKernel.Abstractions/Contents/StreamingChatMessageContent.cs
+++ dotnet/src/SemanticKernel.Abstractions/Contents/StreamingChatMessageContent.cs
@@ -46,9 +46,28 @@ public class StreamingChatMessageContent : StreamingKernelContent
     /// <param name="modelId">The model ID used to generate the content</param>
     /// <param name="encoding">Encoding of the chat</param>
     /// <param name="metadata">Additional metadata</param>
-    /// <param name="name">Name of the author of the message</param>
     [JsonConstructor]
-    public StreamingChatMessageContent(AuthorRole? role, string? content, object? innerContent = null, int choiceIndex = 0, string? modelId = null, Encoding? encoding = null, IReadOnlyDictionary<string, object?>? metadata = null, string? name = null)
+    public StreamingChatMessageContent(AuthorRole? role, string? content, object? innerContent = null, int choiceIndex = 0, string? modelId = null, Encoding? encoding = null, IReadOnlyDictionary<string, object?>? metadata = null)
+        : base(innerContent, choiceIndex, modelId, metadata)
+    {
+        this.Role = role;
+        this.Content = content;
+        this.Encoding = encoding ?? Encoding.UTF8;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="StreamingChatMessageContent"/> class.
+    /// </summary>
+    /// <param name="name">Name of the author of the message</param>
+    /// <param name="role">Role of the author of the message</param>
+    /// <param name="content">Content of the message</param>
+    /// <param name="innerContent">Inner content object reference</param>
+    /// <param name="choiceIndex">Choice index</param>
+    /// <param name="modelId">The model ID used to generate the content</param>
+    /// <param name="encoding">Encoding of the chat</param>
+    /// <param name="metadata">Additional metadata</param>
+    [JsonConstructor]
+    public StreamingChatMessageContent(string? name, AuthorRole? role, string? content, object? innerContent = null, int choiceIndex = 0, string? modelId = null, Encoding? encoding = null, IReadOnlyDictionary<string, object?>? metadata = null)
         : base(innerContent, choiceIndex, modelId, metadata)
     {
         this.Name = name;
