33f1c0e7308b116767de390c833c0b23f60979f9
Chris Rickman <crickman@microsoft.com>
2024-03-30 17:11:36 -0700

    Project structure

---
+++ dotnet/samples/AgentSyntaxExamples/BaseTest.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Reflection;
+using Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using RepoUtils;
+using Xunit.Abstractions;
+
+namespace Examples;
+
+public abstract class BaseTest
+{
+    protected ITestOutputHelper Output { get; }
+
+    protected ILoggerFactory LoggerFactory { get; }
+
+    protected BaseTest(ITestOutputHelper output)
+    {
+        this.Output = output;
+        this.LoggerFactory = new XunitLogger(output);
+
+        LoadUserSecrets();
+    }
+
+    private static void LoadUserSecrets()
+    {
+        IConfigurationRoot configRoot = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.Development.json", true)
+            .AddEnvironmentVariables()
+            .AddUserSecrets(Assembly.GetExecutingAssembly())
+            .Build();
+
+        TestConfiguration.Initialize(configRoot);
+    }
+
+    /// <summary>
+    /// This method can be substituted by Console.WriteLine when used in Console apps.
+    /// </summary>
+    /// <param name="target">Target object to write</param>
+    protected void WriteLine(object? target = null)
+    {
+        this.Output.WriteLine(target ?? string.Empty);
+    }
+
+    /// <summary>
+    /// Current interface ITestOutputHelper does not have a Write method. This extension method adds it to make it analogous to Console.Write when used in Console apps.
+    /// </summary>
+    /// <param name="target">Target object to write</param>
+    protected void Write(object? target = null)
+    {
+        this.Output.WriteLine(target ?? string.Empty);
+    }
+}
