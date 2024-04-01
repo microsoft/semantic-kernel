c4c7464f071b5f6f7c7d734bef988daa1c012429
Evan Mattson <35585003+moonbox3@users.noreply.github.com>
2024-04-01 08:01:08 -0700

    Python: Introduce operations to handle OpenAI plugins, improve OpenAPI plugins, and allow for auth (#5695)
    ### Motivation and Context
    
    Python had the ability to handle operations for OpenAPI plugins, but at
    a basic level. It did not have the ability to handle OpenAI plugins, nor
    did it allow the user to configure an authentication callback. This PR
    brings in that functionality.
    
    <!-- Thank you for your contribution to the semantic-kernel repo!
    Please help reviewers and future users, providing the following
    information:
      1. Why is this change required?
      2. What problem does it solve?
      3. What scenario does it contribute to?
      4. If it fixes an open issue, please link to the issue here.
    -->
    
    ### Description
    
    This PR:
    - Allows a user to handle OpenAI plugins with/without auth. Closes #5355
    - Improves the functionality around handling OpenAPI specs.
    - Introduces an auth callback mechanism to allow for REST API calls to
    use auth
    - Introduces OpenAI/OpenAPI function execution settings.
    - Provides two new kernel examples that show how to handle a simple
    OpenAI plugin (Klarna) as well as a more complex plugin (Azure Key
    Vault) that requires auth.
    - Updates unit tests.
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [X] The code builds clean without any errors or warnings
    - [X] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [X] All unit tests pass, and I have added new tests where possible
    - [X] I didn't break anyone :smile:

--- python/samples/kernel-syntax-examples/openapi_example/openapi_client.py
+++ python/samples/kernel-syntax-examples/openapi_example/openapi_client.py
@@ -1,24 +1,30 @@
 import asyncio
 
 import semantic_kernel as sk
-from semantic_kernel.connectors.openapi import register_openapi_plugin
+from semantic_kernel.functions.kernel_arguments import KernelArguments
 
-if __name__ == "__main__":
+
+async def main():
     """Client"""
     kernel = sk.Kernel()
 
-    openapi_plugin = register_openapi_plugin(kernel, "openApiPlugin", "openapi.yaml")
+    openapi_plugin = kernel.import_plugin_from_openapi(
+        plugin_name="openApiPlugin", openapi_document_path="./openapi.yaml"
+    )
 
-    context_variables = sk.ContextVariables(
-        variables={
-            "request_body": '{"input": "hello world"}',
-            "path_params": '{"name": "mark"}',
-            "query_params": '{"q": "0.7"}',
-            "headers": '{"Content-Type": "application/json", "Header": "example"}',
-        }
-    )
-    result = asyncio.run(
-        # Call the function defined in openapi.yaml
-        openapi_plugin["helloWorld"].invoke(variables=context_variables)
-    )
+    arguments = {
+        "request_body": '{"input": "hello world"}',
+        "path_params": '{"name": "mark"}',
+        "query_params": '{"q": "0.7"}',
+        "headers": '{"Content-Type": "application/json", "Header": "example"}',
+    }
+
+    kernel_arguments = KernelArguments(**arguments)
+
+    result = kernel.invoke(openapi_plugin["helloWorld"], arguments=kernel_arguments)
+
     print(result)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
