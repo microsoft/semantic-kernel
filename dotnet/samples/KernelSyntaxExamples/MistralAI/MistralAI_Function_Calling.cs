5726013c9d3963ea29b642850561fe0ec851e848
Mark Wallace <127216156+markwallace-microsoft@users.noreply.github.com>
2024-04-04 13:15:18 +0100

    .Net: Update MistralAI kernel syntax samples (#5771)
    ### Motivation and Context
    
    Adding a new function calling sample
    
    ### Description
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [ ] The code builds clean without any errors or warnings
    - [ ] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [ ] All unit tests pass, and I have added new tests where possible
    - [ ] I didn't break anyone :smile:

---
+++ dotnet/samples/KernelSyntaxExamples/MistralAI/MistralAI_Function_Calling.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.ComponentModel;
+using System.Threading.Tasks;
+using Examples;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.MistralAI;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace MistralAI;
+
+/// <summary>
+/// Demonstrates the use of function calling with MistralAI.
+/// </summary>
+public sealed class MistralAI_Function_Calling : BaseTest
+{
+    [Fact]
+    public async Task GetChatMessageContentsAsync()
+    {
+        // Create a kernel with OpenAI chat completion
+        IKernelBuilder kernelBuilder = Kernel.CreateBuilder();
+        kernelBuilder.AddMistralChatCompletion(
+                model: TestConfiguration.MistralAI.ChatModelId!,
+                apiKey: TestConfiguration.MistralAI.ApiKey!);
+        kernelBuilder.Plugins.AddFromType<WeatherPlugin>();
+        Kernel kernel = kernelBuilder.Build();
+
+        const string ChatPrompt = @"
+            <message role=""system"">What is the weather like in Paris?</message>
+        ";
+        var chatSemanticFunction = kernel.CreateFunctionFromPrompt(
+            ChatPrompt, new MistralAIPromptExecutionSettings { ToolCallBehavior = MistralAIToolCallBehavior.AutoInvokeKernelFunctions });
+        var chatPromptResult = await kernel.InvokeAsync(chatSemanticFunction);
+
+        WriteLine(chatPromptResult);
+    }
+
+    public sealed class WeatherPlugin
+    {
+        [KernelFunction]
+        [Description("Get the current weather in a given location.")]
+        public string GetWeather(
+            [Description("The city and department, e.g. Marseille, 13")] string location
+            ) => "17°C\nWind: 23 KMPH\nHumidity: 59%\nMostly cloudy";
+    }
+
+    public MistralAI_Function_Calling(ITestOutputHelper output) : base(output) { }
+}
