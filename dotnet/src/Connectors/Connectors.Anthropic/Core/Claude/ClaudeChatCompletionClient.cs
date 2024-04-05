a8ddeed64f4ed9545dc136deaf14fe082a077c71
Krzysztof Kasprowicz <krzysztof@impe.pl>
2024-04-05 17:12:52 +0200

    Add ClaudeChatCompletionClient class

---
+++ dotnet/src/Connectors/Connectors.Anthropic/Core/Claude/ClaudeChatCompletionClient.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+namespace Microsoft.SemanticKernel.Connectors.Anthropic.Core;
+
+/// <summary>
+/// Represents a client for interacting with the chat completion Claude models.
+/// </summary>
+internal sealed class ClaudeChatCompletionClient
+{
+    /// <summary>
+    /// Generates a chat message asynchronously.
+    /// </summary>
+    /// <param name="chatHistory">The chat history containing the conversation data.</param>
+    /// <param name="executionSettings">Optional settings for prompt execution.</param>
+    /// <param name="kernel">A kernel instance.</param>
+    /// <param name="cancellationToken">A cancellation token to cancel the operation.</param>
+    /// <returns>Returns a list of chat message contents.</returns>
+    public async Task<IReadOnlyList<ChatMessageContent>> GenerateChatMessageAsync(
+        ChatHistory chatHistory,
+        PromptExecutionSettings? executionSettings = null,
+        Kernel? kernel = null,
+        CancellationToken cancellationToken = default)
+    {
+        await Task.Yield();
+        throw new NotImplementedException("Implement this method in next PR.");
+    }
+
+    /// <summary>
+    /// Generates a stream of chat messages asynchronously.
+    /// </summary>
+    /// <param name="chatHistory">The chat history containing the conversation data.</param>
+    /// <param name="executionSettings">Optional settings for prompt execution.</param>
+    /// <param name="kernel">A kernel instance.</param>
+    /// <param name="cancellationToken">A cancellation token to cancel the operation.</param>
+    /// <returns>An asynchronous enumerable of <see cref="StreamingChatMessageContent"/> streaming chat contents.</returns>
+    public async IAsyncEnumerable<StreamingChatMessageContent> StreamGenerateChatMessageAsync(
+        ChatHistory chatHistory,
+        PromptExecutionSettings? executionSettings = null,
+        Kernel? kernel = null,
+        [EnumeratorCancellation] CancellationToken cancellationToken = default)
+    {
+        await Task.Yield();
+        throw new NotImplementedException("Implement this method in next PR.");
+        yield break;
+    }
+}
