b1e05b051e8a3a23e9497c0908c08ee7bd2e3058
Chris Rickman <crickman@microsoft.com>
2024-04-09 21:00:26 -0700

    Checkpoint

---
+++ dotnet/src/Agents/Abstractions/AgentPlugin.cs
@@ -0,0 +1,110 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+namespace Microsoft.SemanticKernel.Agents;
+
+/// <summary>
+/// Specialization of <see cref="KernelPlugin"/> for <see cref="Agent"/>.
+/// </summary>
+public sealed class AgentPlugin : KernelPlugin
+{
+    /// <summary>
+    /// $$$
+    /// </summary>
+    public const string FunctionName = "Ask"; // $$$
+
+    /// <inheritdoc/>
+    public override int FunctionCount => 1;
+
+    /// <summary>
+    /// $$$
+    /// </summary>
+    public IReadOnlyList<ChatMessageContent> History => this._chat.History;
+
+    /// <summary>
+    /// $$$
+    /// </summary>
+    public Agent Agent { get; }
+
+    private static readonly Regex s_removeInvalidCharsRegex = new("[^0-9A-Za-z-]");
+
+    private readonly PluginChat _chat;
+    private readonly KernelFunction _functionAsk;
+
+    /// <summary>
+    /// $$$
+    /// </summary>
+    /// <param name="history"></param>
+    public void Bind(ChatHistory history) // $$$ ROUGH
+    {
+        //this._chat.Bind(history);
+    }
+
+    /// <inheritdoc/>
+    public override IEnumerator<KernelFunction> GetEnumerator()
+    {
+        yield return this._functionAsk;
+    }
+
+    /// <inheritdoc/>
+    public override bool TryGetFunction(string name, [NotNullWhen(true)] out KernelFunction? function)
+    {
+        function =
+            FunctionName.Equals(name, StringComparison.OrdinalIgnoreCase) ?
+            this._functionAsk :
+            null;
+
+        return function != null;
+    }
+
+    internal AgentPlugin(Agent agent)
+        : base(s_removeInvalidCharsRegex.Replace(agent.Name ?? agent.Id, string.Empty), // Uniqueness ???
+               agent.Description)
+    {
+        this.Agent = agent;
+        this._chat = new PluginChat(agent);
+        this._functionAsk = KernelFunctionFactory.CreateFromMethod(this.InvokeAsync, FunctionName, description: this.Description);
+    }
+
+    /// <summary>
+    /// Invoke plugin with optional input.
+    /// </summary>
+    /// <param name="input">Optional input</param>
+    /// <param name="cancellationToken">A cancel token</param>
+    /// <returns>The agent response</returns>
+    private async Task<string?> InvokeAsync(string? input, CancellationToken cancellationToken = default)
+    {
+        this._chat.AddChatMessage(new ChatMessageContent(AuthorRole.User, input));
+
+        var message = await this._chat.InvokeAsync(cancellationToken).LastAsync(cancellationToken).ConfigureAwait(false);
+
+        return message.Content;
+    }
+
+    /// <summary>
+    /// A special nexus for managing the plug-in interaction.
+    /// </summary>
+    private sealed class PluginChat : AgentChat
+    {
+        private readonly Agent _agent;
+
+        public new ChatHistory History => base.History;
+
+        public PluginChat(Agent agent)
+        {
+            this._agent = agent;
+        }
+
+        public IAsyncEnumerable<ChatMessageContent> InvokeAsync(
+            CancellationToken cancellationToken = default) =>
+                base.InvokeAgentAsync(this._agent, cancellationToken);
+    }
+}
---
+++ dotnet/src/Agents/Core/AggregatorAgent.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.SemanticKernel.Agents;
+
+///// <summary>
+///// Delegate definition for <see cref="NexusExecutionSettings.CompletionCriteria"/>.
+///// </summary>
+///// <param name="cancellationToken">The <see cref="CancellationToken"/> to monitor for cancellation requests. The default is <see cref="CancellationToken.None"/>.</param>
+///// <returns>True when complete.</returns>
+//public delegate IAsyncEnumerable<ChatMessageContent> NexusInvocationCallback(CancellationToken cancellationToken);
+
+/// <summary>
+/// $$$
+/// </summary>
+public sealed class AggregatorAgent : Agent, IChatHistoryHandler
+{
+    private readonly AgentGroupChat _chat; // $$$ INTERFACE??? 
+
+    /// <inheritdoc/>
+    protected override IEnumerable<string> GetChannelKeys()
+    {
+        yield return typeof(ChatHistoryChannel).FullName;
+    }
+
+    /// <inheritdoc/>
+    protected override Task<AgentChannel> CreateChannelAsync(CancellationToken cancellationToken)
+    {
+        return Task.FromResult<AgentChannel>(new ChatHistoryChannel());
+    }
+
+    /// <inheritdoc/>
+    async IAsyncEnumerable<ChatMessageContent> IChatHistoryHandler.InvokeAsync(
+        IReadOnlyList<ChatMessageContent> history,
+        [EnumeratorCancellation] CancellationToken cancellationToken)
+    {
+        await foreach (var message in this._chat.InvokeAsync(cancellationToken))
+        {
+            yield return message;
+        }
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="KernelAgent"/> class.
+    /// </summary>
+    /// <param name="chat">The underlying chat.</param>
+    /// <param name="description">The agent description (optional)</param>
+    /// <param name="name">The agent name</param>
+    public AggregatorAgent(
+        AgentGroupChat chat,
+        string? description = null,
+        string? name = null)
+    {
+        this._chat = chat;
+
+        this.Id = Guid.NewGuid().ToString();
+        this.Description = description;
+        this.Name = name;
+    }
+}
