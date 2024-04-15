f02c5c4c9f0c1cfccfdc7e13596337547bd38281
Chris Rickman <crickman@microsoft.com>
2024-04-15 11:08:37 -0700

    Namespace

--- dotnet/src/Agents/UnitTests/Core/Chat/SequentialSelectionStrategyTests.cs
+++ dotnet/src/Agents/UnitTests/Core/Chat/SequentialSelectionStrategyTests.cs
@@ -1,6 +1,5 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 using System;
-using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Agents;
