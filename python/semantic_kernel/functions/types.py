59d2a5c2e900c3e2a2383336fef86dcd44fa6180
edvan_microsoft <edvan@microsoft.com>
2024-04-15 12:14:23 +0200

    moved callable to from typing

--- python/semantic_kernel/functions/types.py
+++ python/semantic_kernel/functions/types.py
@@ -1,8 +1,6 @@
 # Copyright (c) Microsoft. All rights reserved.
-from __future__ import annotations
 
-from collections.abc import Callable
-from typing import Any
+from typing import Any, Callable
 
 from semantic_kernel.functions.kernel_function import KernelFunction
 
