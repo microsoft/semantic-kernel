c72080d7da6baa539eab515fc065730d87d191e2
Chris <66376200+crickman@users.noreply.github.com>
2024-04-17 09:24:54 -0700

    .Net - Introducing AgentGroupChat (Step #2) (#5725)
    ### Motivation and Context
    
    <!-- Thank you for your contribution to the semantic-kernel repo!
    Please help reviewers and future users, providing the following
    information:
      1. Why is this change required?
      2. What problem does it solve?
      3. What scenario does it contribute to?
      4. If it fixes an open issue, please link to the issue here.
    -->
    
    Introduce `AgentGroupChat` to the `Core` project.
    
    ### Description
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    Adds `AgentGroupChat` along with associated settings, strategies,
    unit-test, and example.
    
    ![Screenshot 2024-04-03
    110654](https://github.com/microsoft/semantic-kernel/assets/66376200/378dc75c-0748-4359-a497-84dc95844374)
    
    ### Outstanding Tasks - In Order (each a future PR)
    
    - [X] AgentChat (our "GroupChat")
    - [ ] Agent-as-a-Plugin
    - [ ] OpenAIAssistantAgent
    - [ ] OpenAIAssistantAgent Citiation Content
    - [ ] Port AutoGen examples
    - [ ] Streaming
    - [ ] YAML Templates
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [x] The code builds clean without any errors or warnings
    - [x] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [x] All unit tests pass, and I have added new tests where possible
    - [x] I didn't break anyone :smile:

---
+++ dotnet/src/Agents/Core/Chat/RegExTerminationStrategy.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.SemanticKernel.Agents.Chat;
+
+/// <summary>
+/// Signals termination when the most recent message matches against the defined regular expressions
+/// for the specified agent (if provided).
+/// </summary>
+public sealed class RegExTerminationStrategy : TerminationStrategy
+{
+    private readonly string[] _expressions;
+
+    /// <inheritdoc/>
+    protected override Task<bool> ShouldAgentTerminateAsync(Agent agent, IReadOnlyList<ChatMessageContent> history, CancellationToken cancellationToken = default)
+    {
+        // Most recent message
+        var message = history[history.Count - 1];
+
+        // Evaluate expressions for match
+        foreach (var expression in this._expressions)
+        {
+            if (Regex.IsMatch(message.Content, expression))
+            {
+                return Task.FromResult(true);
+            }
+        }
+
+        return Task.FromResult(false);
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RegExTerminationStrategy"/> class.
+    /// </summary>
+    /// <param name="expressions">A list of regular expressions, that if</param>
+    public RegExTerminationStrategy(params string[] expressions)
+    {
+        this._expressions = expressions;
+    }
+}
