f1415d61751998d189e48be8abc1057ea7dd9eb7
XiaoYun Zhang <xiaoyuz@microsoft.com>
2024-04-24 12:00:11 -0700

    implement liquid template && factory

---
+++ dotnet/src/Extensions/PromptTemplates.Liquid/LiquidPromptTemplate.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Scriban;
+
+namespace Microsoft.SemanticKernel.PromptTemplates.Liquid;
+internal class LiquidPromptTemplate : IPromptTemplate
+{
+    private readonly PromptTemplateConfig _config;
+
+    public LiquidPromptTemplate(PromptTemplateConfig config)
+    {
+        if (config.TemplateFormat != LiquidPromptTemplateFactory.LiquidTemplateFormat)
+        {
+            throw new ArgumentException($"Invalid template format: {config.TemplateFormat}");
+        }
+
+        this._config = config;
+    }
+
+    public Task<string> RenderAsync(Kernel kernel, KernelArguments? arguments = null, CancellationToken cancellationToken = default)
+    {
+        Verify.NotNull(kernel);
+        var template = this._config.Template;
+        var liquidTemplate = Template.ParseLiquid(template);
+        var renderedResult = liquidTemplate.Render(arguments);
+
+        // post processing
+        // for every system: | assistant: | user: | function:
+        // replacing it with <Message role="system">, <Message role="assistant">, <Message role="user">, <Message role="function">
+        return Task.FromResult(renderedResult);
+    }
+}
---
+++ dotnet/src/Extensions/PromptTemplates.Liquid/LiquidPromptTemplateFactory.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Microsoft.SemanticKernel.PromptTemplates.Liquid;
+
+/// <summary>
+/// Porvides an <see cref="IPromptTemplateFactory"/> for liquid template format.
+/// </summary>
+public sealed class LiquidPromptTemplateFactory : IPromptTemplateFactory
+{
+    /// <summary>
+    /// Gets the name of the liquid template format.
+    /// </summary>
+    public static string LiquidTemplateFormat => "liquid";
+
+    /// <inheritdoc/>
+    public bool TryCreate(PromptTemplateConfig templateConfig, [NotNullWhen(true)] out IPromptTemplate? result)
+    {
+        if (templateConfig.TemplateFormat.Equals(LiquidTemplateFormat, StringComparison.Ordinal))
+        {
+            result = new LiquidPromptTemplate(templateConfig);
+            return true;
+        }
+
+        result = null;
+        return false;
+    }
+}
