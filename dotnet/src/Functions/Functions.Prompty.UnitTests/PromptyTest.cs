da663ad8059d9ca16657fe115be7c94e1d1105e8
XiaoYun Zhang <xiaoyuz@microsoft.com>
2024-04-24 16:08:49 -0700

    add tests for CreateFunctionFromPrompty

--- dotnet/src/Functions/Functions.Prompty.UnitTests/PromptyTest.cs
+++ dotnet/src/Functions/Functions.Prompty.UnitTests/PromptyTest.cs
@@ -3,85 +3,59 @@
 using System;
 using System.IO;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
 using Microsoft.SemanticKernel.Prompty.Extension;
 using Xunit;
+using Xunit.Abstractions;
+using YamlDotNet.Serialization.NamingConventions;
+using YamlDotNet.Serialization;
 
 namespace SemanticKernel.Functions.Prompty.UnitTests;
 public sealed class PromptyTest
 {
     [Fact]
-    public async Task ChatPromptyTemplateTestAsync()
+    public async Task ChatPromptyTestAsync()
     {
-        var modelId = "gpt-35-turbo-16k";
-        var endPoint = Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT") ?? throw new Exception("AZURE_OPENAI_ENDPOINT is not set");
-        var key = Environment.GetEnvironmentVariable("AZURE_OPENAI_API_KEY") ?? throw new Exception("AZURE_OPENAI_KEY is not set");
         var kernel = Kernel.CreateBuilder()
-            .AddAzureOpenAIChatCompletion(modelId, endPoint, key)
             .Build();
 
         var cwd = Directory.GetCurrentDirectory();
         var chatPromptyPath = Path.Combine(cwd, "TestData", "chat.prompty");
-        var function = kernel.CreateFunctionFromPrompty(chatPromptyPath);
-        // create a dynamic customer object
-        // customer contains the following properties
-        // - firstName
-        // - lastName
-        // - age
-        // - membership
-        // - orders []
-        //  - name
-        //  - description
-        var customer = new
-        {
-            firstName = "John",
-            lastName = "Doe",
-            age = 30,
-            membership = "Gold",
-            orders = new[]
-            {
-                new { name = "apple", description = "2 fuji apples", date = "2024/04/01" },
-                new { name = "banana", description = "1 free banana from amazon banana hub", date = "2024/04/03" },
-            },
-        };
+        var kernelFunction = kernel.CreateFunctionFromPrompty(chatPromptyPath);
 
-        // create a list of documents
-        // documents contains the following properties
-        // - id
-        // - title
-        // - content
-        var documents = new[]
-        {
-            new { id = "1", title = "apple", content = "2 apples"},
-            new { id = "2", title = "banana", content = "3 bananas"},
-        };
+        Assert.Equal("Contoso_Chat_Prompt", kernelFunction.Name);
+        Assert.Equal("A retail assistent for Contoso Outdoors products retailer.", kernelFunction.Description);
 
-        // create chat history
-        // each chat message contains the following properties
-        // - role (system, user, assistant)
-        // - content
+        // chat prompty doesn't contain input parameters
+        Assert.Empty(kernelFunction.Metadata.Parameters);
+    }
 
-        var chatHistory = new[]
-        {
-            new { role = "user", content = "When is the last time I bought apple? Give me specific date and year" },
-        };
+    [Fact]
+    public void ChatPromptyShouldSupportCreatingOpenAIExecutionSettings()
+    {
+        var kernel = Kernel.CreateBuilder()
+            .Build();
 
-        // create
-        var result = await kernel.InvokeAsync(function, arguments: new()
-        {
-            { "customer", customer },
-            { "documentation", documents },
-            { "history", chatHistory },
-        });
+        var cwd = Directory.GetCurrentDirectory();
+        var chatPromptyPath = Path.Combine(cwd, "TestData", "chat.prompty");
+        var kernelFunction = kernel.CreateFunctionFromPrompty(chatPromptyPath);
 
-        Assert.IsType<OpenAIChatMessageContent>(result.GetValue<OpenAIChatMessageContent>());
+        // kernel function created from chat.prompty should have a single execution setting
+        Assert.Single(kernelFunction.ExecutionSettings!);
+        Assert.True(kernelFunction.ExecutionSettings!.ContainsKey("default"));
 
-        if (result.GetValue<OpenAIChatMessageContent>() is OpenAIChatMessageContent openAIChatMessageContent)
-        {
-            Assert.Equal(AuthorRole.Assistant, openAIChatMessageContent.Role);
-            Assert.Contains("2024", openAIChatMessageContent.Content, StringComparison.InvariantCultureIgnoreCase);
-        }
+        var defaultExecutionSetting = kernelFunction.ExecutionSettings["default"];
+
+        // Act
+        var executionSettings = OpenAIPromptExecutionSettings.FromExecutionSettings(defaultExecutionSetting);
+
+        // Assert
+        Assert.NotNull(executionSettings);
+        Assert.Equal("gpt-35-turbo", executionSettings.ModelId);
+        Assert.Equal(1.0, executionSettings.Temperature);
+        Assert.Equal(1.0, executionSettings.TopP);
     }
 }
