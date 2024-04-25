d2aead256b3d6d8e2776039e166343769cdd6c65
Chris Rickman <crickman@microsoft.com>
2024-04-25 12:46:02 -0700

    Namespace

--- dotnet/src/Agents/Core/Chat/SelectionStrategy.cs
+++ dotnet/src/Agents/Core/Chat/SelectionStrategy.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 
 namespace Microsoft.SemanticKernel.Agents.Chat;
 
--- dotnet/src/Agents/Core/Chat/TerminationStrategy.cs
+++ dotnet/src/Agents/Core/Chat/TerminationStrategy.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 
 namespace Microsoft.SemanticKernel.Agents.Chat;
 
