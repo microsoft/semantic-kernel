58cd931b0f2311c9594d3f2c84ac7d09aa8577a3
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2024-04-26 15:34:54 +0100

    Fix warnings and build

--- dotnet/samples/GettingStartedWithAgents/Step2_Plugins.cs
+++ dotnet/samples/GettingStartedWithAgents/Step2_Plugins.cs
@@ -4,7 +4,6 @@ using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Agents;
 using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
-using Xunit.Abstractions;
 
 namespace GettingStarted;
 
