c545c7d774176d11964c81e173776232a2ae2f20
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2024-04-29 16:49:27 +0100

    .Net Samples Restructuring - Phase 2 (#6005)
    ## Phase 2 - Sample Restructuring
    
    - Removal of Examples numbering 
    - Decomposing `KernelSyntaxExamples` into meaningful Feature base
    folders within `Concepts`.
    - Update current sample projects to use `sample` centric
    `InternalUtilities` reducing code repetition
    - Update SDK projects type to be correctly idenfified as xUnit Test
    projects
    - Update projects to be implicit using friendly, reducing considerably
    the extra lines of code for every sample.
    - Decomposing `Concepts\AgentSyntax` into `Concepts\Agents`
    - Decomposing `Concepts\AgentSyntax\GettingStarted` into a new root
    dedicated `GettingStartedWithAgents`

---
+++ dotnet/samples/Concepts/Agents/OpenAIAssistant_CodeInterpreter.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Agents;
+using Microsoft.SemanticKernel.Agents.OpenAI;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+namespace Examples;
+/// <summary>
+/// Demonstrate using code-interpreter on <see cref="OpenAIAssistantAgent"/> .
+/// </summary>
+public class OpenAIAssistant_CodeInterpreter(ITestOutputHelper output) : BaseTest(output)
+{
+    [Fact]
+    public async Task RunAsync()
+    {
+        // Define the agent
+        OpenAIAssistantAgent agent =
+            await OpenAIAssistantAgent.CreateAsync(
+                kernel: this.CreateEmptyKernel(),
+                config: new(this.ApiKey, this.Endpoint),
+                new()
+                {
+                    EnableCodeInterpreter = true, // Enable code-interpreter
+                    ModelId = this.Model,
+                });
+
+        // Create a chat for agent interaction.
+        var chat = new AgentGroupChat();
+
+        // Respond to user input
+        try
+        {
+            await InvokeAgentAsync("What is the solution to `3x + 2 = 14`?");
+            await InvokeAgentAsync("What is the fibinacci sequence until 101?");
+        }
+        finally
+        {
+            await agent.DeleteAsync();
+        }
+
+        // Local function to invoke agent and display the conversation messages.
+        async Task InvokeAgentAsync(string input)
+        {
+            chat.AddChatMessage(new ChatMessageContent(AuthorRole.User, input));
+
+            this.WriteLine($"# {AuthorRole.User}: '{input}'");
+
+            await foreach (var content in chat.InvokeAsync(agent))
+            {
+                this.WriteLine($"# {content.Role} - {content.AuthorName ?? "*"}: '{content.Content}'");
+            }
+        }
+    }
+}
--- dotnet/samples/LearnResources/MicrosoftLearn/TestConfiguration.cs
+++ dotnet/samples/LearnResources/MicrosoftLearn/TestConfiguration.cs
@@ -1,6 +1,5 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
-using System;
 using System.Runtime.CompilerServices;
 using Microsoft.Extensions.Configuration;
 
