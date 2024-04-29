bee027fb292c3a7a07ddc87d0c2d36578c892ea1
XiaoYun Zhang <xiaoyuz@microsoft.com>
2024-04-29 16:05:14 -0700

    format

--- dotnet/src/Extensions/PromptTemplates.Liquid.UnitTests/LiquidTemplateTest.cs
+++ dotnet/src/Extensions/PromptTemplates.Liquid.UnitTests/LiquidTemplateTest.cs
@@ -1,6 +1,5 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
-using System.Globalization;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
--- dotnet/src/Extensions/PromptTemplates.Liquid/LiquidPromptTemplate.cs
+++ dotnet/src/Extensions/PromptTemplates.Liquid/LiquidPromptTemplate.cs
@@ -2,7 +2,6 @@
 
 using System;
 using System.Linq;
-using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
