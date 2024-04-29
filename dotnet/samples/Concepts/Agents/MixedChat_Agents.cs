80aa77710365ca4cc1d56cd71abb013cfcc3bddc
Chris <66376200+crickman@users.noreply.github.com>
2024-04-29 13:40:53 -0700

    .Net: Agents - Getting Started Step4 Improvement: Cross Model (#6044)
    ### Motivation and Context
    <!-- Thank you for your contribution to the semantic-kernel repo!
    Please help reviewers and future users, providing the following
    information:
      1. Why is this change required?
      2. What problem does it solve?
      3. What scenario does it contribute to?
      4. If it fixes an open issue, please link to the issue here.
    -->
    
    
    [Step4_KernelFunctionStrategies](https://github.com/microsoft/semantic-kernel/blob/main/dotnet/samples/GettingStartedWithAgents/Step4_KernelFunctionStrategies.cs)
    was either:
    1. Failing with null selection
    2. Exceeding model token limit
    3. Not performing the requested task
    
    ### Description
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    A couple things at play here:
    1. The selection prompt was copied from AutoGen. While it worked on
    `GPT-4`, it wasn't generating a reliable result for `GPT-3.5-Turbo`
    2. The agent instructions were never tuned outsidre of `GPT-4`
    3. No sensible default / fall-through (can't trust model 100%)
    4. 4k token limit may be too low for history evaluation. Tested with
    `gpt-35-turbo-16k`
    
    > Note: When formal support for history manipulation comes on line, we
    should be able to optimize around last(n) messages.
    
    ### Contribution Checklist
    <!-- Before submitting this PR, please make sure: -->
    
    - [X] The code builds clean without any errors or warnings
    - [X] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [X] All unit tests pass, and I have added new tests where possible
    - [X] I didn't break anyone :smile:

--- dotnet/samples/Concepts/Agents/MixedChat_Agents.cs
+++ dotnet/samples/Concepts/Agents/MixedChat_Agents.cs
@@ -21,12 +21,14 @@ public class MixedChat_Agents(ITestOutputHelper output) : BaseTest(output)
         If not, provide insight on how to refine suggested copy without example.
         """;
 
-    private const string CopyWriterName = "Writer";
+    private const string CopyWriterName = "CopyWriter";
     private const string CopyWriterInstructions =
         """
         You are a copywriter with ten years of experience and are known for brevity and a dry humor.
-        You're laser focused on the goal at hand. Don't waste time with chit chat.
         The goal is to refine and decide on the single best copy as an expert in the field.
+        Only provide a single proposal per response.
+        You're laser focused on the goal at hand.
+        Don't waste time with chit chat.
         Consider suggestions when refining an idea.
         """;
 
