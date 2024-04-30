b8ad85d66962945123edbfa425302037fe23517c
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2024-04-30 20:34:08 +0100

    Fix changing dictonary while enumerating

--- dotnet/src/Connectors/Connectors.OpenAI/AzureSdk/ClientCore.cs
+++ dotnet/src/Connectors/Connectors.OpenAI/AzureSdk/ClientCore.cs
@@ -1221,9 +1221,10 @@ internal abstract class ClientCore
                     arguments = JsonSerializer.Deserialize<KernelArguments>(functionToolCall.Arguments);
                     if (arguments is not null)
                     {
-                        foreach (var argument in arguments)
+                        var names = arguments.Names.ToArray();
+                        foreach (var name in names)
                         {
-                            arguments[argument.Key] = argument.Value?.ToString();
+                            arguments[name] = arguments[name]?.ToString();
                         }
                     }
                 }
