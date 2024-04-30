e1f2adfeafee3523d7ef46001ed19ad6fdd31505
XiaoYun Zhang <xiaoyuz@microsoft.com>
2024-04-30 10:29:07 -0700

    remove unused import

--- dotnet/src/Extensions/PromptTemplates.Liquid/LiquidPromptTemplate.cs
+++ dotnet/src/Extensions/PromptTemplates.Liquid/LiquidPromptTemplate.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
