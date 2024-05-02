051b4ad34a306d254545637fd0f5d06fd30cd417
Xiaoyun Zhang <bigmiao.zhang@gmail.com>
2024-05-02 01:09:21 -0700

    .Net:  fix #6033: Add more tests and comments (#6086)
    ### Motivation and Context
    
    <!-- Thank you for your contribution to the semantic-kernel repo!
    Please help reviewers and future users, providing the following
    information:
      1. Why is this change required?
      2. What problem does it solve?
      3. What scenario does it contribute to?
      4. If it fixes an open issue, please link to the issue here.
    -->
    
    ### Description
    #6033 
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [ ] The code builds clean without any errors or warnings
    - [ ] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [ ] All unit tests pass, and I have added new tests where possible
    - [ ] I didn't break anyone :smile:

--- dotnet/src/Extensions/PromptTemplates.Liquid/LiquidPromptTemplate.cs
+++ dotnet/src/Extensions/PromptTemplates.Liquid/LiquidPromptTemplate.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
 using System;
-using System.Linq;
+using System.Collections.Generic;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
@@ -10,11 +10,19 @@ using Scriban;
 
 namespace Microsoft.SemanticKernel.PromptTemplates.Liquid;
 
+/// <summary>
+/// Represents a Liquid prompt template.
+/// </summary>
 internal sealed class LiquidPromptTemplate : IPromptTemplate
 {
     private readonly PromptTemplateConfig _config;
     private static readonly Regex s_roleRegex = new(@"(?<role>system|assistant|user|function):[\s]+");
 
+    /// <summary>
+    /// Constructor for Liquid PromptTemplate.
+    /// </summary>
+    /// <param name="config">Prompt template configuration</param>
+    /// <exception cref="ArgumentException">throw if <see cref="PromptTemplateConfig.TemplateFormat"/> is not <see cref="LiquidPromptTemplateFactory.LiquidTemplateFormat"/></exception>
     public LiquidPromptTemplate(PromptTemplateConfig config)
     {
         if (config.TemplateFormat != LiquidPromptTemplateFactory.LiquidTemplateFormat)
@@ -25,13 +33,34 @@ internal sealed class LiquidPromptTemplate : IPromptTemplate
         this._config = config;
     }
 
+    /// <inheritdoc/>
     public Task<string> RenderAsync(Kernel kernel, KernelArguments? arguments = null, CancellationToken cancellationToken = default)
     {
         Verify.NotNull(kernel);
 
         var template = this._config.Template;
         var liquidTemplate = Template.ParseLiquid(template);
-        var nonEmptyArguments = arguments?.Where(x => x.Value is not null).ToDictionary(x => x.Key, x => x.Value!);
+        Dictionary<string, object> nonEmptyArguments = new();
+        foreach (var p in this._config.InputVariables)
+        {
+            if (p.Default is null || (p.Default is string s && string.IsNullOrWhiteSpace(s)))
+            {
+                continue;
+            }
+
+            nonEmptyArguments[p.Name] = p.Default;
+        }
+
+        foreach (var p in arguments ?? new KernelArguments())
+        {
+            if (p.Value is null)
+            {
+                continue;
+            }
+
+            nonEmptyArguments[p.Key] = p.Value;
+        }
+
         var renderedResult = liquidTemplate.Render(nonEmptyArguments);
 
         // parse chat history
