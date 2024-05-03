73905073206810717f6bfed6a0aaf53cb042de1a
XiaoYun Zhang <xiaoyuz@microsoft.com>
2024-05-02 17:51:00 -0700

    remove unused namespace

--- dotnet/src/Extensions/PromptTemplates.Liquid/LiquidPromptTemplate.cs
+++ dotnet/src/Extensions/PromptTemplates.Liquid/LiquidPromptTemplate.cs
@@ -3,7 +3,6 @@
 using System;
 using System.Linq;
 using System.Text;
-using System.Text.Encodings.Web;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
