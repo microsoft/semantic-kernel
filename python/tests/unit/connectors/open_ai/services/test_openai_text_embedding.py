2f4387d9204d9d1cb94ddc5c807d9042c903d4f6
sinyubonnie-ho <133104434+sinyubonnie-ho@users.noreply.github.com>
2024-05-04 14:39:29 +0200

    Python: added embedding dimensions support (#6111)
    ### Motivation and Context
    
    <!-- Thank you for your contribution to the semantic-kernel repo!
    Please help reviewers and future users, providing the following
    information:
      1. Why is this change required?
      2. What problem does it solve?
      3. What scenario does it contribute to?
      4. If it fixes an open issue, please link to the issue here.
    -->
    
    ### Description
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    added embedding dimensions support (issue:
    https://github.com/microsoft/semantic-kernel/issues/5882)
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [] The code builds clean without any errors or warnings
    - [] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [] All unit tests pass, and I have added new tests where possible
    - [] I didn't break anyone :smile:
    
    ---------
    
    Co-authored-by: Sin Yu Bonnie Ho <sinyubonnie.ho@nuance.com>
    Co-authored-by: Eduard van Valkenburg <eavanvalkenburg@users.noreply.github.com>

---
+++ python/tests/unit/connectors/open_ai/services/test_openai_text_embedding.py
@@ -0,0 +1,30 @@
+# Copyright (c) Microsoft. All rights reserved.
+
+from unittest.mock import AsyncMock, patch
+
+import pytest
+from openai.resources.embeddings import AsyncEmbeddings
+
+from semantic_kernel.connectors.ai.open_ai.services.open_ai_text_embedding import OpenAITextEmbedding
+
+
+@pytest.mark.asyncio
+@patch.object(AsyncEmbeddings, "create", new_callable=AsyncMock)
+async def test_openai_text_embedding_calls_with_parameters(mock_create) -> None:
+    ai_model_id = "test_model_id"
+    api_key = "test_api_key"
+    texts = ["hello world", "goodbye world"]
+    embedding_dimensions = 1536
+
+    openai_text_embedding = OpenAITextEmbedding(
+        ai_model_id=ai_model_id,
+        api_key=api_key,
+    )
+
+    await openai_text_embedding.generate_embeddings(texts, dimensions=embedding_dimensions)
+
+    mock_create.assert_awaited_once_with(
+        input=texts,
+        model=ai_model_id,
+        dimensions=embedding_dimensions,
+    )
