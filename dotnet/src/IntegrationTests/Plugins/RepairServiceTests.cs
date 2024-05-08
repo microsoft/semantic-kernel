431d18b93459673190d392f0e3b8faffbf417647
Mark Wallace <127216156+markwallace-microsoft@users.noreply.github.com>
2024-05-08 15:46:34 +0100

    .Net: Add request uri and payload to RestApiOperationResponse (#6082)
    ### Motivation and Context
    
    Closes #6071 
    
    ### Description
    
    Add a new `EnablePayloadInResponse` execution parameter which determines
    whether payload will be included in the `RestApiOperationResponse`. If
    true, the payload will be included in the response. Otherwise the
    payload will not be included and `RestApiOperationResponse.Payload` will
    be null. `RestApiOperationResponse.IncludesPayload` will be set to true
    if the payload is included in the response.
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [ ] The code builds clean without any errors or warnings
    - [ ] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [ ] All unit tests pass, and I have added new tests where possible
    - [ ] I didn't break anyone :smile:

--- dotnet/src/IntegrationTests/Plugins/RepairServiceTests.cs
+++ dotnet/src/IntegrationTests/Plugins/RepairServiceTests.cs
@@ -1,5 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Plugins.OpenApi;
@@ -17,7 +19,6 @@ public class RepairServiceTests
         using var stream = System.IO.File.OpenRead("Plugins/repair-service.json");
         using HttpClient httpClient = new();
 
-        //note that this plugin is not compliant according to the underlying validator in SK
         var plugin = await kernel.ImportPluginFromOpenApiAsync(
             "RepairService",
             stream,
@@ -28,35 +29,62 @@ public class RepairServiceTests
             ["payload"] = """{ "title": "Engine oil change", "description": "Need to drain the old engine oil and replace it with fresh oil.", "assignedTo": "", "date": "", "image": "" }"""
         };
 
-        // Act
+        // Create Repair
         var result = await plugin["createRepair"].InvokeAsync(kernel, arguments);
 
-        // Assert
         Assert.NotNull(result);
         Assert.Equal("New repair created", result.ToString());
 
+        // List All Repairs
+        result = await plugin["listRepairs"].InvokeAsync(kernel, arguments);
+
+        Assert.NotNull(result);
+        var repairs = JsonSerializer.Deserialize<Repair[]>(result.ToString());
+        Assert.True(repairs?.Length > 0);
+
+        var id = repairs[repairs.Length - 1].Id;
+
+        // Update Repair
         arguments = new KernelArguments
         {
-            ["payload"] = """{ "id": 1, "assignedTo": "Karin Blair", "date": "2024-04-16", "image": "https://www.howmuchisit.org/wp-content/uploads/2011/01/oil-change.jpg" }"""
+            ["payload"] = $"{{ \"id\": {id}, \"assignedTo\": \"Karin Blair\", \"date\": \"2024-04-16\", \"image\": \"https://www.howmuchisit.org/wp-content/uploads/2011/01/oil-change.jpg\" }}"
         };
 
-        // Act
         result = await plugin["updateRepair"].InvokeAsync(kernel, arguments);
 
-        // Assert
         Assert.NotNull(result);
         Assert.Equal("Repair updated", result.ToString());
 
+        // Delete Repair
         arguments = new KernelArguments
         {
-            ["payload"] = """{ "id": 1 }"""
+            ["payload"] = $"{{ \"id\": {id} }}"
         };
 
-        // Act
         result = await plugin["deleteRepair"].InvokeAsync(kernel, arguments);
 
-        // Assert
         Assert.NotNull(result);
         Assert.Equal("Repair deleted", result.ToString());
+    }
+
+    public class Repair
+    {
+        [JsonPropertyName("id")]
+        public int? Id { get; set; }
+
+        [JsonPropertyName("title")]
+        public string? Title { get; set; }
+
+        [JsonPropertyName("description")]
+        public string? description { get; set; }
+
+        [JsonPropertyName("assignedTo")]
+        public string? assignedTo { get; set; }
+
+        [JsonPropertyName("date")]
+        public string? Date { get; set; }
+
+        [JsonPropertyName("image")]
+        public string? Image { get; set; }
     }
 }
