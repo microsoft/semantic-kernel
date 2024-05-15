ea3355e5eff71921c7b6e819dee3b9ea37feac4c
Artur Kordowski <9746197+akordowski@users.noreply.github.com>
2024-05-15 22:14:28 +0200

    Add AzureOpenAIConfig, OpenAIConfig and ConfigurationException

---
+++ dotnet/src/Connectors/Connectors.OpenAI/Configuration/AzureOpenAIConfig.cs
@@ -0,0 +1,125 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using Azure.Core;
+
+namespace Microsoft.SemanticKernel;
+
+public class AzureOpenAIConfig
+{
+    private TokenCredential? _tokenCredential;
+
+    public enum AuthTypes
+    {
+        Unknown = -1,
+        AzureIdentity,
+        APIKey,
+        ManualTokenCredential,
+    }
+
+    public enum APITypes
+    {
+        Unknown = -1,
+        TextCompletion,
+        ChatCompletion,
+        ImageGeneration,
+        EmbeddingGeneration,
+    }
+
+    /// <summary>
+    /// OpenAI API type, e.g. text completion, chat completion, image generation, etc.
+    /// </summary>
+    public APITypes APIType { get; set; } = APITypes.ChatCompletion;
+
+    /// <summary>
+    /// Azure authentication type.
+    /// </summary>
+    public AuthTypes Auth { get; set; }
+
+    /// <summary>
+    /// Azure OpenAI endpoint URL.
+    /// </summary>
+    public string Endpoint { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Azure OpenAI deployment name.
+    /// </summary>
+    public string Deployment { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The max number of tokens supported by model deployed.
+    /// </summary>
+    public int MaxTokenTotal { get; set; } = 8191;
+
+    /// <summary>
+    /// API key, required if Auth == APIKey.
+    /// </summary>
+    public string APIKey { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The number of dimensions output embeddings should have.
+    /// Only supported in "text-embedding-3" and later models developed with
+    /// MRL, see https://arxiv.org/abs/2205.13147
+    /// </summary>
+    public int? EmbeddingDimensions { get; set; }
+
+    /// <summary>
+    /// Set credentials manually from code.
+    /// </summary>
+    /// <param name="credential">Token credentials</param>
+    public void SetCredential(TokenCredential credential)
+    {
+        this.Auth = AuthTypes.ManualTokenCredential;
+        this._tokenCredential = credential;
+    }
+
+    /// <summary>
+    /// Fetch the credentials passed manually from code.
+    /// </summary>
+    public TokenCredential GetTokenCredential()
+    {
+        return this._tokenCredential
+               ?? throw new ConfigurationException($"Azure OpenAI: {nameof(this._tokenCredential)} not defined");
+    }
+
+    /// <summary>
+    /// Verify that the current state is valid.
+    /// </summary>
+    public void Validate()
+    {
+        if (this.Auth == AuthTypes.Unknown)
+        {
+            throw new ConfigurationException($"Azure OpenAI: {nameof(this.Auth)} (authentication type) is not defined");
+        }
+
+        if (this.Auth == AuthTypes.APIKey && string.IsNullOrWhiteSpace(this.APIKey))
+        {
+            throw new ConfigurationException($"Azure OpenAI: {nameof(this.APIKey)} is empty");
+        }
+
+        if (string.IsNullOrWhiteSpace(this.Endpoint))
+        {
+            throw new ConfigurationException($"Azure OpenAI: {nameof(this.Endpoint)} is empty");
+        }
+
+        if (!this.Endpoint.StartsWith("https://", StringComparison.OrdinalIgnoreCase))
+        {
+            throw new ConfigurationException($"Azure OpenAI: {nameof(this.Endpoint)} must start with https://");
+        }
+
+        if (string.IsNullOrWhiteSpace(this.Deployment))
+        {
+            throw new ConfigurationException($"Azure OpenAI: {nameof(this.Deployment)} (deployment name) is empty");
+        }
+
+        if (this.MaxTokenTotal < 1)
+        {
+            throw new ConfigurationException($"Azure OpenAI: {nameof(this.MaxTokenTotal)} cannot be less than 1");
+        }
+
+        if (this.EmbeddingDimensions is < 1)
+        {
+            throw new ConfigurationException($"Azure OpenAI: {nameof(this.EmbeddingDimensions)} cannot be less than 1");
+        }
+    }
+}
---
+++ dotnet/src/Connectors/Connectors.OpenAI/Configuration/OpenAIConfig.cs
@@ -0,0 +1,75 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Text.Json.Serialization;
+
+namespace Microsoft.SemanticKernel;
+
+/// <summary>
+/// OpenAI settings.
+/// </summary>
+public class OpenAIConfig
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum TextGenerationTypes
+    {
+        Auto = 0,
+        TextCompletion,
+        Chat,
+    }
+
+    /// <summary>
+    /// Model used for text generation. Chat models can be used too.
+    /// </summary>
+    public string TextModel { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The type of OpenAI completion to use, either Text (legacy) or Chat.
+    /// When using Auto, the client uses OpenAI model names to detect the correct protocol.
+    /// Most OpenAI models use Chat. If you're using a non-OpenAI model, you might want to set this manually.
+    /// </summary>
+    public TextGenerationTypes TextGenerationType { get; set; } = TextGenerationTypes.Auto;
+
+    /// <summary>
+    /// Model used to embedding generation.
+    /// </summary>
+    public string EmbeddingModel { get; set; } = string.Empty;
+
+    /// <summary>
+    /// OpenAI HTTP endpoint. You may need to override this to work with
+    /// OpenAI compatible services like LM Studio.
+    /// </summary>
+    public string Endpoint { get; set; } = "https://api.openai.com/v1";
+
+    /// <summary>
+    /// OpenAI API key.
+    /// </summary>
+    public string APIKey { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Optional OpenAI Organization ID.
+    /// </summary>
+    public string? OrgId { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The number of dimensions output embeddings should have.
+    /// Only supported in "text-embedding-3" and later models developed with
+    /// MRL, see https://arxiv.org/abs/2205.13147
+    /// </summary>
+    public int? EmbeddingDimensions { get; set; }
+
+    /// <summary>
+    /// Verify that the current state is valid.
+    /// </summary>
+    public void Validate()
+    {
+        if (string.IsNullOrWhiteSpace(this.APIKey))
+        {
+            throw new ConfigurationException($"OpenAI: {nameof(this.APIKey)} is empty");
+        }
+
+        if (this.EmbeddingDimensions is < 1)
+        {
+            throw new ConfigurationException($"OpenAI: {nameof(this.EmbeddingDimensions)} cannot be less than 1");
+        }
+    }
+}
