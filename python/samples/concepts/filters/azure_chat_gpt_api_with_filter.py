aeae9aea795d71618c5904721e970800bcd10641
edvan_microsoft <edvan@microsoft.com>
2024-05-16 14:33:15 +0200

    updated sample

--- python/samples/concepts/filters/azure_chat_gpt_api_with_filter.py
+++ python/samples/concepts/filters/azure_chat_gpt_api_with_filter.py
@@ -7,64 +7,59 @@ import logging
 import os
 from typing import Any, Callable, Coroutine
 
-from semantic_kernel.connectors.ai.open_ai.services.azure_chat_completion import AzureChatCompletion
+from semantic_kernel.connectors.ai.open_ai.services.open_ai_chat_completion import OpenAIChatCompletion
 from semantic_kernel.contents.chat_history import ChatHistory
 from semantic_kernel.exceptions.kernel_exceptions import OperationCancelledException
 from semantic_kernel.filters.function.function_invocation_context import FunctionInvocationContext
 from semantic_kernel.kernel import Kernel
-from semantic_kernel.utils.settings import azure_openai_settings_from_dot_env_as_dict
 
 logger = logging.getLogger(__name__)
 
 
-class ChatHistoryHooked(ChatHistory):
-    async def function_filter(
-        self,
-        function_context: FunctionInvocationContext,
-        next: Callable[[FunctionInvocationContext], Coroutine[Any, Any, None]],
-    ) -> None:
-        if function_context.function.plugin_name != "chat":
-            await next(function_context)
-            return
-        try:
-            user_input = input("User:> ")
-        except KeyboardInterrupt:
-            raise OperationCancelledException("User stopped the operation")
-        except EOFError:
-            raise OperationCancelledException("User stopped the operation")
-        if user_input == "exit":
-            raise OperationCancelledException("User stopped the operation")
-        self.add_user_message(user_input)
+async def input_output_filter(
+    context: FunctionInvocationContext,
+    next: Callable[[FunctionInvocationContext], Coroutine[Any, Any, None]],
+) -> None:
+    if context.function.plugin_name != "chat":
+        await next(context)
+        return
+    try:
+        user_input = input("User:> ")
+    except KeyboardInterrupt:
+        raise OperationCancelledException("User stopped the operation")
+    except EOFError:
+        raise OperationCancelledException("User stopped the operation")
+    if user_input == "exit":
+        raise OperationCancelledException("User stopped the operation")
+    context.arguments["chat_history"].add_user_message(user_input)
 
-        await next(function_context)
+    await next(context)
 
-        if function_context.result:
-            logger.info(f'Usage: {function_context.result.metadata.get("usage")}')
-            self.add_message(function_context.result.value[0])
-            print(f"Mosscap:> {str(function_context.result)}")
+    if context.result:
+        logger.info(f'Usage: {context.result.metadata.get("usage")}')
+        context.arguments["chat_history"].add_message(context.result.value[0])
+        print(f"Mosscap:> {str(context.result)}")
 
 
 async def main() -> None:
     kernel = Kernel()
-    kernel.add_service(
-        AzureChatCompletion(
-            service_id="chat-gpt", **azure_openai_settings_from_dot_env_as_dict(include_api_version=True)
-        )
-    )
+    kernel.add_service(OpenAIChatCompletion(service_id="chat-gpt"))
     kernel.add_plugin(
         parent_directory=os.path.join(os.path.dirname(os.path.realpath(__file__)), "resources"), plugin_name="chat"
     )
-    history = ChatHistoryHooked()
+    history = ChatHistory()
 
-    @kernel.filter("function_invocation")
-    async def exception_catch_filter(context, next):
+    kernel.add_filter("function_invocation", input_output_filter)
+
+    @kernel.filter(filter_type="function_invocation")
+    async def exception_catch_filter(
+        context: FunctionInvocationContext, next: Coroutine[FunctionInvocationContext, Any, None]
+    ):
         try:
             await next(context)
         except Exception as e:
             logger.info(e)
             raise e
-
-    kernel.add_filter("function_invocation", history)
 
     chatting = True
     while chatting:
