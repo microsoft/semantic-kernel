9b0dde56287b3f17591524c1918a21169bc56b09
Mark Wallace <127216156+markwallace-microsoft@users.noreply.github.com>
2024-05-16 20:35:49 +0100

    .Net: Add MistralAI to the AppInsights sample (#6301)
    ### Motivation and Context
    
    <!-- Thank you for your contribution to the semantic-kernel repo!
    Please help reviewers and future users, providing the following
    information:
      1. Why is this change required?
      2. What problem does it solve?
      3. What scenario does it contribute to?
      4. If it fixes an open issue, please link to the issue here.
    -->
    
    ### Description
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [ ] The code builds clean without any errors or warnings
    - [ ] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [ ] All unit tests pass, and I have added new tests where possible
    - [ ] I didn't break anyone :smile:

--- dotnet/samples/Demos/TelemetryWithAppInsights/Program.cs
+++ dotnet/samples/Demos/TelemetryWithAppInsights/Program.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Connectors.Google;
 using Microsoft.SemanticKernel.Connectors.HuggingFace;
+using Microsoft.SemanticKernel.Connectors.MistralAI;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
 using Microsoft.SemanticKernel.Services;
 using OpenTelemetry;
@@ -84,6 +85,8 @@ public sealed class Program
             await RunGoogleAIChatAsync(kernel);
             Console.WriteLine();
             await RunHuggingFaceChatAsync(kernel);
+            Console.WriteLine();
+            await RunMistralAIChatAsync(kernel);
         }
 
         Console.WriteLine();
@@ -115,6 +118,7 @@ public sealed class Program
     private const string AzureOpenAIServiceKey = "AzureOpenAI";
     private const string GoogleAIGeminiServiceKey = "GoogleAIGemini";
     private const string HuggingFaceServiceKey = "HuggingFace";
+    private const string MistralAIServiceKey = "MistralAI";
 
     #region chat completion
     private static async Task RunAzureOpenAIChatAsync(Kernel kernel)
@@ -158,6 +162,24 @@ public sealed class Program
 
         using var activity = s_activitySource.StartActivity(HuggingFaceServiceKey);
         SetTargetService(kernel, HuggingFaceServiceKey);
+
+        try
+        {
+            await RunChatAsync(kernel);
+        }
+        catch (Exception ex)
+        {
+            activity?.SetStatus(ActivityStatusCode.Error, ex.Message);
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+
+    private static async Task RunMistralAIChatAsync(Kernel kernel)
+    {
+        Console.WriteLine("============= MistralAI Chat Completion =============");
+
+        using var activity = s_activitySource.StartActivity(MistralAIServiceKey);
+        SetTargetService(kernel, MistralAIServiceKey);
 
         try
         {
@@ -243,7 +265,12 @@ public sealed class Program
                 model: TestConfiguration.HuggingFace.ModelId,
                 endpoint: new Uri("https://api-inference.huggingface.co"),
                 apiKey: TestConfiguration.HuggingFace.ApiKey,
-                serviceId: HuggingFaceServiceKey);
+                serviceId: HuggingFaceServiceKey)
+            .AddMistralChatCompletion(
+                modelId: TestConfiguration.MistralAI.ChatModelId,
+                apiKey: TestConfiguration.MistralAI.ApiKey,
+                serviceId: MistralAIServiceKey
+            );
 
         builder.Services.AddSingleton<IAIServiceSelector>(new AIServiceSelector());
         builder.Plugins.AddFromPromptDirectory(Path.Combine(folder, "WriterPlugin"));
@@ -304,6 +331,11 @@ public sealed class Program
                         HuggingFaceServiceKey => new HuggingFacePromptExecutionSettings()
                         {
                             Temperature = 0,
+                        },
+                        MistralAIServiceKey => new MistralAIPromptExecutionSettings()
+                        {
+                            Temperature = 0,
+                            ToolCallBehavior = MistralAIToolCallBehavior.AutoInvokeKernelFunctions
                         },
                         _ => null,
                     };
