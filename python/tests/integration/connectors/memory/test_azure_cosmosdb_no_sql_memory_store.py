e61a22490039f9853cf9624d580d683d838145e0
Aayush Kataria <aayushkataria3011@gmail.com>
2024-05-17 15:27:29 -0700

    Fixing test case

--- python/tests/integration/connectors/memory/test_azure_cosmosdb_no_sql_memory_store.py
+++ python/tests/integration/connectors/memory/test_azure_cosmosdb_no_sql_memory_store.py
@@ -37,7 +37,7 @@ async def azure_cosmosdb_no_sql_memory_store() -> MemoryStoreBase:
     store = AzureCosmosDBNoSQLMemoryStore(
         cosmos_client=cosmos_client,
         database_name=database_name,
-        partition_key=partition_key,
+        partition_key=partition_key.path,
         vector_embedding_policy=get_vector_embedding_policy("cosine", "float32", 5),
         indexing_policy=get_vector_indexing_policy("flat"),
         cosmos_container_properties=cosmos_container_properties,
@@ -49,83 +49,83 @@ async def azure_cosmosdb_no_sql_memory_store() -> MemoryStoreBase:
 async def test_create_get_drop_exists_collection():
     store = await azure_cosmosdb_no_sql_memory_store()
 
-    await store.create_collection_async(collection_name=container_name)
+    await store.create_collection(collection_name=container_name)
 
-    collection_list = await store.get_collections_async()
+    collection_list = await store.get_collections()
     assert container_name in collection_list
 
-    await store.delete_collection_async(collection_name=container_name)
+    await store.delete_collection(collection_name=container_name)
 
-    result = await store.does_collection_exist_async(collection_name=container_name)
+    result = await store.does_collection_exist(collection_name=container_name)
     assert result is False
 
 
 @pytest.mark.asyncio
 async def test_upsert_and_get_and_remove():
     store = await azure_cosmosdb_no_sql_memory_store()
-    await store.create_collection_async(collection_name=container_name)
+    await store.create_collection(collection_name=container_name)
     record = get_vector_items()[0]
 
-    doc_id = await store.upsert_async(container_name, record)
+    doc_id = await store.upsert(container_name, record)
     assert doc_id == record.id
 
-    result = await store.get_async(container_name, record.id, with_embedding=True)
+    result = await store.get(container_name, record.id, with_embedding=True)
 
     assert result is not None
     assert result.id == record.id
     assert all(result._embedding[i] == record._embedding[i] for i in range(len(result._embedding)))
-    await store.remove_async(container_name, record.id)
+    await store.remove(container_name, record.id)
 
 
 @pytest.mark.asyncio
 async def test_upsert_batch_and_get_batch_remove_batch():
     store = await azure_cosmosdb_no_sql_memory_store()
-    await store.create_collection_async(collection_name=container_name)
+    await store.create_collection(collection_name=container_name)
     records = get_vector_items()
 
-    doc_ids = await store.upsert_batch_async(container_name, records)
+    doc_ids = await store.upsert_batch(container_name, records)
     assert len(doc_ids) == 3
     assert all(doc_id in [record.id for record in records] for doc_id in doc_ids)
 
-    results = await store.get_batch_async(container_name, [record.id for record in records], with_embeddings=True)
+    results = await store.get_batch(container_name, [record.id for record in records], with_embeddings=True)
 
     assert len(results) == 3
     assert all(result["id"] in [record.id for record in records] for result in results)
 
-    await store.remove_batch_async(container_name, [record.id for record in records])
+    await store.remove_batch(container_name, [record.id for record in records])
 
 
 @pytest.mark.asyncio
 async def test_get_nearest_match():
     store = await azure_cosmosdb_no_sql_memory_store()
-    await store.create_collection_async(collection_name=container_name)
+    await store.create_collection(collection_name=container_name)
     records = get_vector_items()
-    await store.upsert_batch_async(container_name, records)
+    await store.upsert_batch(container_name, records)
 
     test_embedding = get_vector_items()[0].embedding.copy()
     test_embedding[0] = test_embedding[0] + 0.1
 
-    result = await store.get_nearest_match_async(
+    result = await store.get_nearest_match(
         container_name, test_embedding, min_relevance_score=0.0, with_embedding=True
     )
 
     assert result is not None
     assert result[1] > 0.0
 
-    await store.remove_batch_async(container_name, [record.id for record in records])
+    await store.remove_batch(container_name, [record.id for record in records])
 
 
 @pytest.mark.asyncio
 async def test_get_nearest_matches():
     store = await azure_cosmosdb_no_sql_memory_store()
-    await store.create_collection_async(collection_name=container_name)
+    await store.create_collection(collection_name=container_name)
     records = get_vector_items()
-    await store.upsert_batch_async(container_name, records)
+    await store.upsert_batch(container_name, records)
 
     test_embedding = get_vector_items()[0].embedding.copy()
     test_embedding[0] = test_embedding[0] + 0.1
 
-    result = await store.get_nearest_matches_async(
+    result = await store.get_nearest_matches(
         container_name, test_embedding, limit=3, min_relevance_score=0.0, with_embeddings=True
     )
 
@@ -133,7 +133,7 @@ async def test_get_nearest_matches():
     assert len(result) == 3
     assert all(result[i][0].id in [record.id for record in records] for i in range(3))
 
-    await store.remove_batch_async(container_name, [record.id for record in records])
+    await store.remove_batch(container_name, [record.id for record in records])
 
 
 def get_vector_indexing_policy(embedding_type):
