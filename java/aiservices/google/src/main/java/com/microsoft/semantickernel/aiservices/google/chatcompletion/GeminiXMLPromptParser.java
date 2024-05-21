8455d5805d7959d639a3c26393eb312143a6c397
Milder Hernandez Cagua <milderhc@microsoft.com>
2024-05-20 23:49:48 -0700

    Add PR suggestions

--- java/aiservices/google/src/main/java/com/microsoft/semantickernel/aiservices/google/chatcompletion/GeminiXMLPromptParser.java
+++ java/aiservices/google/src/main/java/com/microsoft/semantickernel/aiservices/google/chatcompletion/GeminiXMLPromptParser.java
@@ -58,72 +58,77 @@ public class GeminiXMLPromptParser {
         }
     }
 
+    private static ChatPromptParseVisitor<GeminiParsedPrompt> createVisitor() {
+        return new ChatPromptParseVisitor<GeminiParsedPrompt>() {
+            private GeminiParsedPrompt parsedRaw;
+            private final List<FunctionDeclaration> functionDefinitions = new ArrayList<>();
+            private final ChatHistory chatHistory = new ChatHistory();
+
+            @Override
+            public ChatPromptParseVisitor<GeminiParsedPrompt> addMessage(
+                    String role,
+                    String content) {
+                chatHistory.addMessage(new ChatMessageContent<>(getAuthorRole(role), content));
+                return this;
+            }
+
+            @Override
+            public ChatPromptParseVisitor<GeminiParsedPrompt> addFunction(
+                    String name,
+                    @Nullable
+                    String description,
+                    @Nullable
+                    BinaryData parameters) {
+
+                String paramString = null;
+                if (parameters != null) {
+                    paramString = parameters.toString();
+                }
+
+                // TODO: Build the parameters schema
+                Schema.Builder parametersBuilder = Schema.newBuilder();
+
+                FunctionDeclaration.Builder function = FunctionDeclaration.newBuilder()
+                        .setName(name)
+                        .setDescription(description)
+                        .setParameters(parametersBuilder.build());
+
+                functionDefinitions.add(function.build());
+                return this;
+            }
+
+            @Override
+            public boolean areMessagesEmpty() {
+                return chatHistory.getMessages().isEmpty();
+            }
+
+            @Override
+            public ChatPromptParseVisitor<GeminiParsedPrompt> fromRawPrompt(
+                    String rawPrompt) {
+
+                ChatMessageContent<?> message = new ChatMessageContent<>(AuthorRole.USER, rawPrompt);
+
+                this.parsedRaw = new GeminiParsedPrompt(new ChatHistory(Collections.singletonList(message)),null);
+
+                return this;
+            }
+
+            @Override
+            public GeminiParsedPrompt get() {
+                if (parsedRaw != null) {
+                    return parsedRaw;
+                }
+
+                return new GeminiParsedPrompt(chatHistory, functionDefinitions);
+            }
+        };
+    }
+
     public static GeminiParsedPrompt parse(String rawPrompt) {
         ChatPromptParseVisitor<GeminiParsedPrompt> visitor = ChatXMLPromptParser.parse(
                 rawPrompt,
-                new ChatPromptParseVisitor<GeminiParsedPrompt>() {
-                    private GeminiParsedPrompt parsedRaw;
-                    private final List<FunctionDeclaration> functionDefinitions = new ArrayList<>();
-                    private final ChatHistory chatHistory = new ChatHistory(false);
-
-                    @Override
-                    public ChatPromptParseVisitor<GeminiParsedPrompt> addMessage(
-                            String role,
-                            String content) {
-                            chatHistory.addMessage(new ChatMessageContent<>(getAuthorRole(role), content));
-                        return this;
-                    }
-
-                    @Override
-                    public ChatPromptParseVisitor<GeminiParsedPrompt> addFunction(
-                            String name,
-                            @Nullable
-                            String description,
-                            @Nullable
-                            BinaryData parameters) {
-
-                        String paramString = null;
-                        if (parameters != null) {
-                            paramString = parameters.toString();
-                        }
-
-                        // TODO: Build the parameters schema
-                        Schema.Builder parametersBuilder = Schema.newBuilder();
-
-                        FunctionDeclaration.Builder function = FunctionDeclaration.newBuilder()
-                                .setName(name)
-                                .setDescription(description)
-                                .setParameters(parametersBuilder.build());
-
-                        functionDefinitions.add(function.build());
-                        return this;
-                    }
-
-                    @Override
-                    public boolean areMessagesEmpty() {
-                        return chatHistory.getMessages().isEmpty();
-                    }
-
-                    @Override
-                    public ChatPromptParseVisitor<GeminiParsedPrompt> fromRawPrompt(
-                            String rawPrompt) {
-
-                        ChatMessageContent<?> message = new ChatMessageContent<>(AuthorRole.USER, rawPrompt);
-
-                        this.parsedRaw = new GeminiParsedPrompt(new ChatHistory(Collections.singletonList(message)),null);
-
-                        return this;
-                    }
-
-                    @Override
-                    public GeminiParsedPrompt get() {
-                        if (parsedRaw != null) {
-                            return parsedRaw;
-                        }
-
-                        return new GeminiParsedPrompt(chatHistory, functionDefinitions);
-                    }
-                });
+                createVisitor()
+        );
 
         return visitor.get();
     }
