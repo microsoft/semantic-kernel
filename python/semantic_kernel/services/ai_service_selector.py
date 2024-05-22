bd80543e3fc9416ef29339087970da74c84f153d
edvan_microsoft <edvan@microsoft.com>
2024-05-22 16:18:42 +0200

    updated tests

--- python/semantic_kernel/services/ai_service_selector.py
+++ python/semantic_kernel/services/ai_service_selector.py
@@ -1,18 +1,14 @@
 # Copyright (c) Microsoft. All rights reserved.
 
-from typing import TYPE_CHECKING, Union
+from typing import TYPE_CHECKING
 
-from semantic_kernel.connectors.ai.prompt_execution_settings import PromptExecutionSettings
 from semantic_kernel.exceptions import KernelServiceNotFoundError
-from semantic_kernel.functions.kernel_arguments import KernelArguments
 
 if TYPE_CHECKING:
-    from semantic_kernel.connectors.ai.chat_completion_client_base import ChatCompletionClientBase
-    from semantic_kernel.connectors.ai.text_completion_client_base import TextCompletionClientBase
+    from semantic_kernel.connectors.ai.prompt_execution_settings import PromptExecutionSettings
+    from semantic_kernel.functions.kernel_arguments import KernelArguments
     from semantic_kernel.functions.kernel_function import KernelFunction
-    from semantic_kernel.kernel import Kernel
-
-    ALL_COMPLETION_SERVICE_TYPES = Union[TextCompletionClientBase, ChatCompletionClientBase]
+    from semantic_kernel.kernel import AI_SERVICE_CLIENT_TYPE, Kernel
 
 
 class AIServiceSelector:
@@ -23,15 +19,22 @@ class AIServiceSelector:
     """
 
     def select_ai_service(
-        self, kernel: "Kernel", function: "KernelFunction", arguments: KernelArguments
-    ) -> tuple["ALL_COMPLETION_SERVICE_TYPES", PromptExecutionSettings]:
+        self,
+        kernel: "Kernel",
+        function: "KernelFunction",
+        arguments: "KernelArguments",
+        type_: type["AI_SERVICE_CLIENT_TYPE"] | None = None,
+    ) -> tuple["AI_SERVICE_CLIENT_TYPE", "PromptExecutionSettings"]:
         """Select a AI Service on a first come, first served basis,
         starting with execution settings in the arguments,
         followed by the execution settings from the function.
         If the same service_id is in both, the one in the arguments will be used.
         """
-        from semantic_kernel.connectors.ai.chat_completion_client_base import ChatCompletionClientBase
-        from semantic_kernel.connectors.ai.text_completion_client_base import TextCompletionClientBase
+        if type_ is None:
+            from semantic_kernel.connectors.ai.chat_completion_client_base import ChatCompletionClientBase
+            from semantic_kernel.connectors.ai.text_completion_client_base import TextCompletionClientBase
+
+            type_ = (TextCompletionClientBase, ChatCompletionClientBase)
 
         execution_settings_dict = arguments.execution_settings or {}
         if func_exec_settings := getattr(function, "prompt_execution_settings", None):
@@ -39,10 +42,12 @@ class AIServiceSelector:
                 if id not in execution_settings_dict:
                     execution_settings_dict[id] = settings
         if not execution_settings_dict:
+            from semantic_kernel.connectors.ai.prompt_execution_settings import PromptExecutionSettings
+
             execution_settings_dict = {"default": PromptExecutionSettings()}
         for service_id, settings in execution_settings_dict.items():
             try:
-                service = kernel.get_service(service_id, type=(TextCompletionClientBase, ChatCompletionClientBase))
+                service = kernel.get_service(service_id, type=type_)
             except KernelServiceNotFoundError:
                 continue
             if service:
