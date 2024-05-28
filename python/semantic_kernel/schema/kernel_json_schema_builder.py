895a580303203121f3a1a248b13d1b1dac0c97fa
Evan Mattson <35585003+moonbox3@users.noreply.github.com>
2024-05-28 14:03:39 -0400

    Python: Fix schema building for complex types (#6394)
    ### Motivation and Context
    
    The schema building for complex types worked in some regard, but it
    didn't work for all cases. There needs to be better checks on complex
    types, if they have __args__ and if so, recurse to the base class type,
    and also grab the type that is specified in the list like
    list[CustomClass], list[str] or even Union[int, bool].
    
    <!-- Thank you for your contribution to the semantic-kernel repo!
    Please help reviewers and future users, providing the following
    information:
      1. Why is this change required?
      2. What problem does it solve?
      3. What scenario does it contribute to?
      4. If it fixes an open issue, please link to the issue here.
    -->
    
    ### Description
    
    This PR improves the parsing/building of complex types. Added
    integration tests to cover more types in the hope that this doesn't lead
    to any more issues.
    - Closes #6388 
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [X] The code builds clean without any errors or warnings
    - [X] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [X] All unit tests pass, and I have added new tests where possible
    - [X] I didn't break anyone :smile:

--- python/semantic_kernel/schema/kernel_json_schema_builder.py
+++ python/semantic_kernel/schema/kernel_json_schema_builder.py
@@ -1,6 +1,6 @@
 # Copyright (c) Microsoft. All rights reserved.
 
-from typing import Any, get_type_hints
+from typing import Any, Union, get_args, get_origin, get_type_hints
 
 from semantic_kernel.kernel_pydantic import KernelBaseModel
 
@@ -11,12 +11,16 @@ TYPE_MAPPING = {
     float: "number",
     list: "array",
     dict: "object",
+    set: "array",
+    tuple: "array",
     "int": "integer",
     "str": "string",
     "bool": "boolean",
     "float": "number",
     "list": "array",
     "dict": "object",
+    "set": "array",
+    "tuple": "array",
     "object": "object",
     "array": "array",
 }
@@ -26,13 +30,23 @@ class KernelJsonSchemaBuilder:
 
     @classmethod
     def build(cls, parameter_type: type | str, description: str | None = None) -> dict[str, Any]:
-        """Builds JSON schema for a given parameter type."""
+        """Builds the JSON schema for a given parameter type and description.
+
+        Args:
+            parameter_type (type | str): The parameter type.
+            description (str, optional): The description of the parameter. Defaults to None.
+
+        Returns:
+            dict[str, Any]: The JSON schema for the parameter type.
+        """
         if isinstance(parameter_type, str):
             return cls.build_from_type_name(parameter_type, description)
-        if issubclass(parameter_type, KernelBaseModel):
+        if isinstance(parameter_type, KernelBaseModel):
             return cls.build_model_schema(parameter_type, description)
         if hasattr(parameter_type, "__annotations__"):
             return cls.build_model_schema(parameter_type, description)
+        if hasattr(parameter_type, "__args__"):
+            return cls.handle_complex_type(parameter_type, description)
         else:
             schema = cls.get_json_schema(parameter_type)
             if description:
@@ -41,9 +55,19 @@ class KernelJsonSchemaBuilder:
 
     @classmethod
     def build_model_schema(cls, model: type, description: str | None = None) -> dict[str, Any]:
-        """Builds JSON schema for a given model."""
+        """Builds the JSON schema for a given model and description.
+
+        Args:
+            model (type): The model type.
+            description (str, optional): The description of the model. Defaults to None.
+
+        Returns:
+            dict[str, Any]: The JSON schema for the model.
+        """
         properties = {}
-        for field_name, field_type in get_type_hints(model).items():
+        # TODO: add support for handling forward references, which is not currently tested
+        hints = get_type_hints(model, globals(), locals())
+        for field_name, field_type in hints.items():
             field_description = None
             if hasattr(model, "__fields__") and field_name in model.__fields__:
                 field_info = model.__fields__[field_name]
@@ -59,7 +83,15 @@ class KernelJsonSchemaBuilder:
 
     @classmethod
     def build_from_type_name(cls, parameter_type: str, description: str | None = None) -> dict[str, Any]:
-        """Builds JSON schema for a given parameter type name."""
+        """Builds the JSON schema for a given parameter type name and description.
+
+        Args:
+            parameter_type (str): The parameter type name.
+            description (str, optional): The description of the parameter. Defaults to None.
+
+        Returns:
+            dict[str, Any]: The JSON schema for the parameter type.
+        """
         type_name = TYPE_MAPPING.get(parameter_type, "object")
         schema = {"type": type_name}
         if description:
@@ -69,7 +101,55 @@ class KernelJsonSchemaBuilder:
 
     @classmethod
     def get_json_schema(cls, parameter_type: type) -> dict[str, Any]:
-        """Gets JSON schema for a given parameter type."""
+        """Gets JSON schema for a given parameter type.
+
+        Args:
+            parameter_type (type): The parameter type.
+
+        Returns:
+            dict[str, Any]: The JSON schema for the parameter type.
+        """
         type_name = TYPE_MAPPING.get(parameter_type, "object")
         schema = {"type": type_name}
         return schema
+
+    @classmethod
+    def handle_complex_type(cls, parameter_type: type, description: str | None = None) -> dict[str, Any]:
+        """Handles building the JSON schema for complex types.
+
+        Args:
+            parameter_type (type): The parameter type.
+            description (str, optional): The description of the parameter. Defaults to None.
+
+        Returns:
+            dict[str, Any]: The JSON schema for the parameter type.
+        """
+        origin = get_origin(parameter_type)
+        args = get_args(parameter_type)
+
+        if origin is list or origin is set:
+            item_type = args[0]
+            return {"type": "array", "items": cls.build(item_type), "description": description}
+        if origin is dict:
+            _, value_type = args
+            additional_properties = cls.build(value_type)
+            if additional_properties == {"type": "object"}:
+                additional_properties["properties"] = {}  # Account for differences in Python 3.10 dict
+            return {"type": "object", "additionalProperties": additional_properties, "description": description}
+        if origin is tuple:
+            items = [cls.build(arg) for arg in args]
+            return {"type": "array", "items": items, "description": description}
+        if origin is Union:
+            # Handle Optional[T] (Union[T, None]) by making schema nullable
+            if len(args) == 2 and type(None) in args:
+                non_none_type = args[0] if args[1] is type(None) else args[1]
+                schema = cls.build(non_none_type)
+                schema["nullable"] = True
+                if description:
+                    schema["description"] = description
+                return schema
+            else:
+                schemas = [cls.build(arg) for arg in args]
+                return {"anyOf": schemas, "description": description}
+        else:
+            return cls.get_json_schema(parameter_type)
