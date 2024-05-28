3faf73120fc6608e7ce6616871eb430c15133055
Daniele Antonio Maggio <1955514+danigian@users.noreply.github.com>
2024-05-28 12:07:33 +0200

    fix: Remove wrong `response_format` override in `AzureChatPromptExecutionSettings` class

--- python/semantic_kernel/connectors/ai/open_ai/prompt_execution_settings/azure_chat_prompt_execution_settings.py
+++ python/semantic_kernel/connectors/ai/open_ai/prompt_execution_settings/azure_chat_prompt_execution_settings.py
@@ -98,5 +98,4 @@ class ExtraBody(KernelBaseModel):
 class AzureChatPromptExecutionSettings(OpenAIChatPromptExecutionSettings):
     """Specific settings for the Azure OpenAI Chat Completion endpoint."""
 
-    response_format: str | None = None
     extra_body: dict[str, Any] | ExtraBody | None = None
