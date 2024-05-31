15984aebc43bf8b0cf88127105cd8d98283486a8
edvan_microsoft <edvan@microsoft.com>
2024-05-31 16:59:37 +0200

    added tests for core_plugins

--- python/tests/unit/core_plugins/test_time_plugin.py
+++ python/tests/unit/core_plugins/test_time_plugin.py
@@ -3,8 +3,11 @@
 import datetime
 from unittest import mock
 
+from pytest import raises
+
 import semantic_kernel as sk
 from semantic_kernel.core_plugins.time_plugin import TimePlugin
+from semantic_kernel.exceptions.function_exceptions import FunctionExecutionException
 
 test_mock_now = datetime.datetime(2031, 1, 12, 12, 24, 56, tzinfo=datetime.timezone.utc)
 test_mock_today = datetime.date(2031, 1, 12)
@@ -22,12 +25,28 @@ def test_can_be_imported():
     assert kernel.get_function(plugin_name="time", function_name="now") is not None
 
 
+def test_today():
+    plugin = TimePlugin()
+
+    with mock.patch("datetime.datetime", wraps=datetime.datetime) as dt:
+        dt.now.return_value = test_mock_now
+        assert plugin.today() == "Sunday, 12 January, 2031"
+
+
 def test_date():
     plugin = TimePlugin()
 
     with mock.patch("datetime.datetime", wraps=datetime.datetime) as dt:
         dt.now.return_value = test_mock_now
         assert plugin.date() == "Sunday, 12 January, 2031"
+
+
+def test_isodate():
+    plugin = TimePlugin()
+
+    with mock.patch("datetime.date", wraps=datetime.date) as dt:
+        dt.today.return_value = test_mock_today
+        assert plugin.iso_date() == "2031-01-12"
 
 
 def test_now():
@@ -52,6 +71,15 @@ def test_date_matching_last_day_name():
     with mock.patch("datetime.date", wraps=datetime.date) as dt:
         dt.today.return_value = test_mock_today
         assert plugin.date_matching_last_day_name("Friday") == "Friday, 10 January, 2031"
+
+
+def test_date_matching_last_day_name_fail():
+    plugin = TimePlugin()
+
+    with mock.patch("datetime.date", wraps=datetime.date) as dt:
+        dt.today.return_value = test_mock_today
+        with raises(FunctionExecutionException):
+            plugin.date_matching_last_day_name("Non-existing-day")
 
 
 def test_utc_now():
@@ -116,6 +144,14 @@ def test_hour():
     with mock.patch("datetime.datetime", wraps=datetime.datetime) as dt:
         dt.now.return_value = test_mock_now
         assert plugin.hour() == "12 PM"
+
+
+def test_hour_number():
+    plugin = TimePlugin()
+
+    with mock.patch("datetime.datetime", wraps=datetime.datetime) as dt:
+        dt.now.return_value = test_mock_now
+        assert plugin.hour_number() == "12"
 
 
 def test_minute():
