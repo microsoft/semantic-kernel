748c4eeed1c2e27ea9462eff83d162ccd2af786a
feiyun0112 <feiyun0112@gmail.com>
2024-06-04 04:03:27 +0000

    Contribute to Microsoft.SemanticKernel.Connectors

---
+++ dotnet/src/Connectors/Connectors.Onnx/OnnxRuntimeGenAIChatCompletionService.cs
@@ -0,0 +1,139 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.ML.OnnxRuntimeGenAI;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Services;
+
+namespace Microsoft.SemanticKernel.Connectors.Onnx;
+
+#pragma warning disable CA2000 // Dispose objects before losing scope
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+#pragma warning disable VSTHRD002 // Avoid problematic synchronous waits
+
+/// <summary>
+/// Represents a chat completion service using OnnxRuntimeGenAI.
+/// </summary>
+public sealed class OnnxRuntimeGenAIChatCompletionService : IChatCompletionService, IDisposable
+{
+    private readonly Model _model;
+    private readonly Tokenizer _tokenizer;
+
+    private Dictionary<string, object?> AttributesInternal { get; } = new();
+
+    /// <summary>
+    /// Initializes a new instance of the OnnxRuntimeGenAIChatCompletionService class.
+    /// </summary>
+    /// <param name="modelPath">The generative AI ONNX model path for the chat completion service.</param>
+    /// <param name="loggerFactory">Optional logger factory to be used for logging.</param>
+    public OnnxRuntimeGenAIChatCompletionService(
+        string modelPath,
+        ILoggerFactory? loggerFactory = null)
+    {
+        this._model = new Model(modelPath);
+        this._tokenizer = new Tokenizer(this._model);
+
+        this.AttributesInternal.Add(AIServiceExtensions.ModelIdKey, this._tokenizer);
+    }
+
+    /// <inheritdoc />
+    public IReadOnlyDictionary<string, object?> Attributes => this.AttributesInternal;
+
+    /// <inheritdoc />
+    public async Task<IReadOnlyList<ChatMessageContent>> GetChatMessageContentsAsync(ChatHistory chatHistory, PromptExecutionSettings? executionSettings = null, Kernel? kernel = null, CancellationToken cancellationToken = default)
+    {
+        var result = new StringBuilder();
+
+        await foreach (var content in this.RunInferenceAsync(chatHistory, executionSettings, cancellationToken).ConfigureAwait(false))
+        {
+            result.Append(content);
+        }
+
+        return new List<ChatMessageContent>
+        {
+            new(
+                role: AuthorRole.Assistant,
+                content: result.ToString())
+        };
+    }
+
+    /// <inheritdoc />
+    public async IAsyncEnumerable<StreamingChatMessageContent> GetStreamingChatMessageContentsAsync(ChatHistory chatHistory, PromptExecutionSettings? executionSettings = null, Kernel? kernel = null, CancellationToken cancellationToken = default)
+    {
+        await foreach (var content in this.RunInferenceAsync(chatHistory, executionSettings, cancellationToken).ConfigureAwait(false))
+        {
+            yield return new StreamingChatMessageContent(AuthorRole.Assistant, content);
+        }
+    }
+
+    private async IAsyncEnumerable<string> RunInferenceAsync(ChatHistory chatHistory, PromptExecutionSettings? executionSettings, CancellationToken cancellationToken)
+    {
+        OnnxRuntimeGenAIPromptExecutionSettings onnxRuntimeGenAIPromptExecutionSettings = OnnxRuntimeGenAIPromptExecutionSettings.FromExecutionSettings(executionSettings);
+
+        var prompt = this.GetPrompt(chatHistory, onnxRuntimeGenAIPromptExecutionSettings);
+        var tokens = this._tokenizer.Encode(prompt);
+
+        var generatorParams = new GeneratorParams(this._model);
+        this.ApplyPromptExecutionSettings(generatorParams, onnxRuntimeGenAIPromptExecutionSettings);
+        generatorParams.SetInputSequences(tokens);
+
+        var generator = new Generator(this._model, generatorParams);
+
+        while (!generator.IsDone())
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            yield return await Task.Run(() =>
+            {
+                generator.ComputeLogits();
+                generator.GenerateNextToken();
+
+                var outputTokens = generator.GetSequence(0);
+                var newToken = outputTokens.Slice(outputTokens.Length - 1, 1);
+                var output = this._tokenizer.Decode(newToken);
+                return output;
+            }, cancellationToken).ConfigureAwait(false);
+        }
+    }
+
+    private string GetPrompt(ChatHistory chatHistory, OnnxRuntimeGenAIPromptExecutionSettings onnxRuntimeGenAIPromptExecutionSettings)
+    {
+        var promptBuilder = new StringBuilder();
+        foreach (var message in chatHistory)
+        {
+            promptBuilder.Append($"<|{message.Role}|>\n{message.Content}");
+        }
+        promptBuilder.Append("<|end|>\n<|assistant|>");
+
+        return promptBuilder.ToString();
+    }
+
+    private void ApplyPromptExecutionSettings(GeneratorParams generatorParams, OnnxRuntimeGenAIPromptExecutionSettings onnxRuntimeGenAIPromptExecutionSettings)
+    {
+        generatorParams.SetSearchOption("top_p", onnxRuntimeGenAIPromptExecutionSettings.TopP);
+        generatorParams.SetSearchOption("top_k", onnxRuntimeGenAIPromptExecutionSettings.TopK);
+        generatorParams.SetSearchOption("temperature", onnxRuntimeGenAIPromptExecutionSettings.Temperature);
+        generatorParams.SetSearchOption("repetition_penalty", onnxRuntimeGenAIPromptExecutionSettings.RepetitionPenalty);
+        generatorParams.SetSearchOption("past_present_share_buffer", onnxRuntimeGenAIPromptExecutionSettings.PastPresentShareBuffer);
+        generatorParams.SetSearchOption("num_return_sequences", onnxRuntimeGenAIPromptExecutionSettings.NumReturnSequences);
+        generatorParams.SetSearchOption("no_repeat_ngram_size", onnxRuntimeGenAIPromptExecutionSettings.NoRepeatNgramSize);
+        generatorParams.SetSearchOption("min_length", onnxRuntimeGenAIPromptExecutionSettings.MinLength);
+        generatorParams.SetSearchOption("max_length", onnxRuntimeGenAIPromptExecutionSettings.MaxLength);
+        generatorParams.SetSearchOption("length_penalty", onnxRuntimeGenAIPromptExecutionSettings.LengthPenalty);
+        generatorParams.SetSearchOption("early_stopping", onnxRuntimeGenAIPromptExecutionSettings.EarlyStopping);
+        generatorParams.SetSearchOption("do_sample", onnxRuntimeGenAIPromptExecutionSettings.DoSample);
+        generatorParams.SetSearchOption("diversity_penalty", onnxRuntimeGenAIPromptExecutionSettings.DiversityPenalty);
+    }
+
+    /// <inheritdoc/>
+    public void Dispose()
+    {
+        this._tokenizer.Dispose();
+        this._model.Dispose();
+    }
+}
---
+++ dotnet/src/Connectors/Connectors.Onnx/OnnxRuntimeGenAIPromptExecutionSettings.cs
@@ -0,0 +1,140 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Text.Json.Serialization;
+
+namespace Microsoft.SemanticKernel.Connectors.Onnx;
+
+/// <summary>
+/// OnnxRuntimeGenAI Execution Settings.
+/// </summary>
+public sealed class OnnxRuntimeGenAIPromptExecutionSettings : PromptExecutionSettings
+{
+    /// <summary>
+    /// Convert PromptExecutionSettings to OnnxRuntimeGenAIPromptExecutionSettings
+    /// </summary>
+    /// <param name="executionSettings"></param>
+    /// <returns></returns>
+    public static OnnxRuntimeGenAIPromptExecutionSettings FromExecutionSettings(PromptExecutionSettings? executionSettings)
+    {
+        switch (executionSettings)
+        {
+            case OnnxRuntimeGenAIPromptExecutionSettings settings:
+                return settings;
+            default:
+                return new OnnxRuntimeGenAIPromptExecutionSettings();
+        }
+    }
+
+    private int _topK = 50;
+    private float _topP = 0.9f;
+    private float _temperature = 1;
+    private float _repetitionPenalty = 1;
+    private bool _pastPresentShareBuffer = false;
+    private int _numReturnSequences = 1;
+    private int _numBeams = 1;
+    private int _noRepeatNgramSize = 0;
+    private int _minLength = 0;
+    private int _maxLength = 200;
+    private float _lengthPenalty = 1;
+    private bool _earlyStopping = true;
+    private bool _doSample = false;
+    private float _diversityPenalty = 0;
+
+    [JsonPropertyName("top_k")]
+    public int TopK
+    {
+        get { return this._topK; }
+        set { this._topK = value; }
+    }
+
+    [JsonPropertyName("top_p")]
+    public float TopP
+    {
+        get { return this._topP; }
+        set { this._topP = value; }
+    }
+
+    [JsonPropertyName("temperature")]
+    public float Temperature
+    {
+        get { return this._temperature; }
+        set { this._temperature = value; }
+    }
+
+    [JsonPropertyName("repetition_penalty")]
+    public float RepetitionPenalty
+    {
+        get { return this._repetitionPenalty; }
+        set { this._repetitionPenalty = value; }
+    }
+
+    [JsonPropertyName("past_present_share_buffer")]
+    public bool PastPresentShareBuffer
+    {
+        get { return this._pastPresentShareBuffer; }
+        set { this._pastPresentShareBuffer = value; }
+    }
+
+    [JsonPropertyName("num_return_sequences")]
+    public int NumReturnSequences
+    {
+        get { return this._numReturnSequences; }
+        set { this._numReturnSequences = value; }
+    }
+
+    [JsonPropertyName("num_beams")]
+    public int NumBeams
+    {
+        get { return this._numBeams; }
+        set { this._numBeams = value; }
+    }
+
+    [JsonPropertyName("no_repeat_ngram_size")]
+    public int NoRepeatNgramSize
+    {
+        get { return this._noRepeatNgramSize; }
+        set { this._noRepeatNgramSize = value; }
+    }
+
+    [JsonPropertyName("min_length")]
+    public int MinLength
+    {
+        get { return this._minLength; }
+        set { this._minLength = value; }
+    }
+
+    [JsonPropertyName("max_length")]
+    public int MaxLength
+    {
+        get { return this._maxLength; }
+        set { this._maxLength = value; }
+    }
+
+    [JsonPropertyName("length_penalty")]
+    public float LengthPenalty
+    {
+        get { return this._lengthPenalty; }
+        set { this._lengthPenalty = value; }
+    }
+
+    [JsonPropertyName("diversity_penalty")]
+    public float DiversityPenalty
+    {
+        get { return this._diversityPenalty; }
+        set { this._diversityPenalty = value; }
+    }
+
+    [JsonPropertyName("early_stopping")]
+    public bool EarlyStopping
+    {
+        get { return this._earlyStopping; }
+        set { this._earlyStopping = value; }
+    }
+
+    [JsonPropertyName("do_sample")]
+    public bool DoSample
+    {
+        get { return this._doSample; }
+        set { this._doSample = value; }
+    }
+}
