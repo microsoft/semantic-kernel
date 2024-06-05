d79a657ab628194f45414463e3656954be8d023c
westey <164392973+westey-m@users.noreply.github.com>
2024-06-05 15:16:00 +0100

    .Net: Add Updated Memory Connector ADR and IMemoryRecordService interface with related classes. (#6364)
    Adding an ADR for evolving memory connectors.
    
    Related work item:
    #5887 
    
    ### Motivation and Context
    
    See included ADR for more motivation and context.
    
    ### Description
    
    See included ADR for more more info.
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [X] The code builds clean without any errors or warnings
    - [X] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [X] All unit tests pass, and I have added new tests where possible
    - [X] I didn't break anyone :smile:

---
+++ dotnet/src/SemanticKernel.Abstractions/Memory/RecordOptions/UpsertRecordOptions.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace Microsoft.SemanticKernel.Memory;
+
+/// <summary>
+/// Optional options when calling <see cref="IMemoryRecordService{TKey, TDataModel}.UpsertAsync"/>.
+/// </summary>
+[Experimental("SKEXP0001")]
+public class UpsertRecordOptions
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="UpsertRecordOptions"/> class.
+    /// </summary>
+    public UpsertRecordOptions()
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="UpsertRecordOptions"/> class by cloning the given options.
+    /// </summary>
+    /// <param name="source">The options to clone</param>
+    public UpsertRecordOptions(UpsertRecordOptions source)
+    {
+        this.CollectionName = source.CollectionName;
+    }
+
+    /// <summary>
+    /// Get or sets an optional collection name to use for this operation that is different to the default.
+    /// </summary>
+    public string? CollectionName { get; init; }
+}
