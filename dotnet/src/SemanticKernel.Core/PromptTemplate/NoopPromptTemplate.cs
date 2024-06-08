40ebb7dccb74ac8ffb6f7cdc268480359b779504
markwallace-microsoft <127216156+markwallace-microsoft@users.noreply.github.com>
2024-06-08 15:13:39 +0100

    Implementation for NopPromptTemplateFactory

---
+++ dotnet/src/SemanticKernel.Core/PromptTemplate/NoopPromptTemplate.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.SemanticKernel;
+
+/// <summary>
+/// No operation implementation of <see cref="IPromptTemplate"/> that just returns the prompt template.
+/// </summary>
+internal sealed class NoopPromptTemplate : IPromptTemplate
+{
+    private readonly PromptTemplateConfig _promptConfig;
+
+    /// <summary>
+    /// Constructor for NoopPromptTemplate.
+    /// </summary>
+    /// <param name="promptConfig">Prompt template configuration</param>
+    internal NoopPromptTemplate(PromptTemplateConfig promptConfig)
+    {
+        Verify.NotNull(promptConfig, nameof(promptConfig));
+        Verify.NotNull(promptConfig.Template, nameof(promptConfig.Template));
+
+        this._promptConfig = promptConfig;
+    }
+
+    /// <inheritdoc/>
+    public Task<string> RenderAsync(Kernel kernel, KernelArguments? arguments = null, CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult(this._promptConfig.Template);
+    }
+}
---
+++ dotnet/src/SemanticKernel.Core/PromptTemplate/NoopPromptTemplateFactory.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace Microsoft.SemanticKernel;
+
+/// <summary>
+/// Provides an implementation of <see cref="IPromptTemplateFactory"/> which creates no operation instances of <see cref="IPromptTemplate"/>.
+/// </summary>
+public sealed class NoopPromptTemplateFactory : IPromptTemplateFactory
+{
+    /// <inheritdoc/>
+    public bool TryCreate(PromptTemplateConfig templateConfig, [NotNullWhen(true)] out IPromptTemplate? result)
+    {
+        result = new NoopPromptTemplate(templateConfig);
+
+        return true;
+    }
+}
