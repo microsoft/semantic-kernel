6b4e84c2e349fca371eb7a033c26b7c540b37b2a
NEWTON <new99mal@gmail.com>
2024-06-11 23:45:41 +0530

    Remove class `TextEmbeddingResponse` Added class `TextEmbeddingResponseType1` & `TextEmbeddingResponseType2` to parse two distinct possible JsonRequest

--- dotnet/src/Connectors/Connectors.HuggingFace/Core/Models/TextEmbeddingResponse.cs
+++ dotnet/src/Connectors/Connectors.HuggingFace/Core/Models/TextEmbeddingResponse.cs
@@ -8,4 +8,12 @@ namespace Microsoft.SemanticKernel.Connectors.HuggingFace.Core;
 /// <summary>
 /// Represents the response from the Hugging Face text embedding API.
 /// </summary>
-internal sealed class TextEmbeddingResponse : List<List<List<ReadOnlyMemory<float>>>>;
+/// <returns> List&lt;ReadOnlyMemory&lt;float&gt;&gt;</returns>
+internal sealed class TextEmbeddingResponseType1 : List<ReadOnlyMemory<float>>;
+
+/// <summary>
+/// Represents the response from the Hugging Face text embedding API.
+/// </summary>
+/// <returns>List&lt;List&lt;List&lt;ReadOnlyMemory&lt;float&gt;&gt;&gt;&gt;</returns>
+internal sealed class TextEmbeddingResponseType2 : List<List<List<ReadOnlyMemory<float>>>>;
+
