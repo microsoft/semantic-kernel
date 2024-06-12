0618ec6d634b5a4218bf11b6d269161e668dd120
NEWTON <new99mal@gmail.com>
2024-06-13 00:26:57 +0530

    Keep only one Deserilzation class  of type List<ReadOnlyMemory<float>>

--- dotnet/src/Connectors/Connectors.HuggingFace/Core/Models/TextEmbeddingResponse.cs
+++ dotnet/src/Connectors/Connectors.HuggingFace/Core/Models/TextEmbeddingResponse.cs
@@ -9,11 +9,4 @@ namespace Microsoft.SemanticKernel.Connectors.HuggingFace.Core;
 /// Represents the response from the Hugging Face text embedding API.
 /// </summary>
 /// <returns> List&lt;ReadOnlyMemory&lt;float&gt;&gt;</returns>
-internal sealed class TextEmbeddingResponseType1 : List<ReadOnlyMemory<float>>;
-
-/// <summary>
-/// Represents the response from the Hugging Face text embedding API.
-/// </summary>
-/// <returns>List&lt;List&lt;List&lt;ReadOnlyMemory&lt;float&gt;&gt;&gt;&gt;</returns>
-internal sealed class TextEmbeddingResponseType2 : List<List<List<ReadOnlyMemory<float>>>>;
-
+internal sealed class TextEmbeddingResponse : List<ReadOnlyMemory<float>>;
