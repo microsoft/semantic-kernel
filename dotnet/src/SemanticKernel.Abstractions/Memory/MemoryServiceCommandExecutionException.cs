ab5d4abcf65ae316a416c7dccda480d8503e1373
westey <164392973+westey-m@users.noreply.github.com>
2024-06-13 16:20:45 +0100

    .Net: Add new Azure AI Search memory connector implementation (#6585)
    ### Motivation and Context
    
    Adding a reference implementation using Azure AI Search for the new
    memory connector record interface.
    
    ### Description
    
    For more information see the included ADR document.
    
    Issue: #6519 
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [X] The code builds clean without any errors or warnings
    - [X] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [X] All unit tests pass, and I have added new tests where possible
    - [X] I didn't break anyone :smile:

---
+++ dotnet/src/SemanticKernel.Abstractions/Memory/MemoryServiceCommandExecutionException.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+
+namespace Microsoft.SemanticKernel.Memory;
+
+/// <summary>
+/// Exception thrown when a memory service command fails, such as upserting a record or deleting a collection.
+/// </summary>
+public class MemoryServiceCommandExecutionException : KernelException
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MemoryServiceCommandExecutionException"/> class.
+    /// </summary>
+    public MemoryServiceCommandExecutionException()
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MemoryServiceCommandExecutionException"/> class with a specified error message.
+    /// </summary>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
+    public MemoryServiceCommandExecutionException(string? message) : base(message)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MemoryServiceCommandExecutionException"/> class with a specified error message and a reference to the inner exception that is the cause of this exception.
+    /// </summary>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
+    /// <param name="innerException">The exception that is the cause of the current exception, or a null reference if no inner exception is specified.</param>
+    public MemoryServiceCommandExecutionException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+}
