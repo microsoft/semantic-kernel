76c8ddbe6a4636e9d461a30d4453e07c80b6e999
Dmytro Struk <13853051+dmytrostruk@users.noreply.github.com>
2024-06-13 12:41:12 -0700

    Added example with multiple results using Kernel

--- dotnet/samples/Concepts/ChatCompletion/OpenAI_ChatCompletionMultipleChoices.cs
+++ dotnet/samples/Concepts/ChatCompletion/OpenAI_ChatCompletionMultipleChoices.cs
@@ -1,57 +1,100 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
+using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
 
 namespace ChatCompletion;
 
-// The following example shows how to use Semantic Kernel with streaming Multiple Results Chat Completion.
+/// <summary>
+/// The following example shows how to use Semantic Kernel with multiple chat completion results.
+/// </summary>
 public class OpenAI_ChatCompletionMultipleChoices(ITestOutputHelper output) : BaseTest(output)
 {
-    [Fact]
-    public Task AzureOpenAIMultiChatCompletionAsync()
+    /// <summary>
+    /// Default prompt execution settings with configured <see cref="OpenAIPromptExecutionSettings.ResultsPerPrompt"/> property.
+    /// </summary>
+    private readonly OpenAIPromptExecutionSettings _executionSettings = new()
     {
-        Console.WriteLine("======== Azure OpenAI - Multiple Chat Completion ========");
+        MaxTokens = 200,
+        FrequencyPenalty = 0,
+        PresencePenalty = 0,
+        Temperature = 1,
+        TopP = 0.5,
+        ResultsPerPrompt = 2
+    };
 
-        var chatCompletionService = new AzureOpenAIChatCompletionService(
-            deploymentName: TestConfiguration.AzureOpenAI.ChatDeploymentName,
-            endpoint: TestConfiguration.AzureOpenAI.Endpoint,
-            apiKey: TestConfiguration.AzureOpenAI.ApiKey,
-            modelId: TestConfiguration.AzureOpenAI.ChatModelId);
+    /// <summary>
+    /// Example with multiple chat completion results using <see cref="AzureOpenAIChatCompletionService"/>.
+    /// </summary>
+    [Fact]
+    public async Task AzureOpenAIMultipleChatCompletionResultsAsync()
+    {
+        Console.WriteLine("======== Azure OpenAI - Multiple Chat Completion Results ========");
 
-        return ChatCompletionAsync(chatCompletionService);
+        var kernel = Kernel
+            .CreateBuilder()
+            .AddAzureOpenAIChatCompletion(
+                deploymentName: TestConfiguration.AzureOpenAI.ChatDeploymentName,
+                endpoint: TestConfiguration.AzureOpenAI.Endpoint,
+                apiKey: TestConfiguration.AzureOpenAI.ApiKey,
+                serviceId: TestConfiguration.AzureOpenAI.ChatModelId)
+            .Build();
+
+        await UsingKernelAsync(kernel);
+        await UsingChatCompletionServiceAsync(kernel.GetRequiredService<IChatCompletionService>());
     }
 
+    /// <summary>
+    /// Example with multiple chat completion results using <see cref="OpenAIChatCompletionService"/>.
+    /// </summary>
     [Fact]
-    public Task OpenAIMultiChatCompletionAsync()
+    public async Task OpenAIMultipleChatCompletionResultsAsync()
     {
-        Console.WriteLine("======== Open AI - Multiple Chat Completion ========");
+        Console.WriteLine("======== Open AI - Multiple Chat Completion Results ========");
 
-        var chatCompletionService = new OpenAIChatCompletionService(
-            TestConfiguration.OpenAI.ChatModelId,
-            TestConfiguration.OpenAI.ApiKey);
+        var kernel = Kernel
+            .CreateBuilder()
+            .AddOpenAIChatCompletion(
+                modelId: TestConfiguration.OpenAI.ChatModelId,
+                apiKey: TestConfiguration.OpenAI.ApiKey)
+            .Build();
 
-        return ChatCompletionAsync(chatCompletionService);
+        await UsingKernelAsync(kernel);
+        await UsingChatCompletionServiceAsync(kernel.GetRequiredService<IChatCompletionService>());
     }
 
-    private async Task ChatCompletionAsync(IChatCompletionService chatCompletionService)
+    /// <summary>
+    /// Example with multiple chat completion results using <see cref="Kernel"/>.
+    /// </summary>
+    private async Task UsingKernelAsync(Kernel kernel)
     {
-        var executionSettings = new OpenAIPromptExecutionSettings()
+        Console.WriteLine("======== Using Kernel ========");
+
+        var chatMessageContents = await kernel.InvokePromptAsync<IReadOnlyList<ChatMessageContent>>("Write one paragraph about why AI is awesome", new(this._executionSettings));
+
+        foreach (var chatMessageContent in chatMessageContents!)
         {
-            MaxTokens = 200,
-            FrequencyPenalty = 0,
-            PresencePenalty = 0,
-            Temperature = 1,
-            TopP = 0.5,
-            ResultsPerPrompt = 2,
-        };
+            Console.Write(chatMessageContent.Content ?? string.Empty);
+            Console.WriteLine("\n-------------\n");
+        }
+
+        Console.WriteLine();
+    }
+
+    /// <summary>
+    /// Example with multiple chat completion results using <see cref="IChatCompletionService"/>.
+    /// </summary>
+    private async Task UsingChatCompletionServiceAsync(IChatCompletionService chatCompletionService)
+    {
+        Console.WriteLine("======== Using IChatCompletionService ========");
 
         var chatHistory = new ChatHistory();
         chatHistory.AddUserMessage("Write one paragraph about why AI is awesome");
 
-        foreach (var chatMessageChoice in await chatCompletionService.GetChatMessageContentsAsync(chatHistory, executionSettings))
+        foreach (var chatMessageContent in await chatCompletionService.GetChatMessageContentsAsync(chatHistory, this._executionSettings))
         {
-            Console.Write(chatMessageChoice.Content ?? string.Empty);
+            Console.Write(chatMessageContent.Content ?? string.Empty);
             Console.WriteLine("\n-------------\n");
         }
 
