c176ac6103bbba3bcb5da76e3354c605afccedac
Chris Rickman <crickman@microsoft.com>
2024-06-17 08:07:01 -0700

    Sync method change

--- dotnet/samples/Concepts/Agents/OpenAIAssistant_FileManipulation.cs
+++ dotnet/samples/Concepts/Agents/OpenAIAssistant_FileManipulation.cs
@@ -62,7 +62,7 @@ public class OpenAIAssistant_FileManipulation(ITestOutputHelper output) : BaseTe
         // Local function to invoke agent and display the conversation messages.
         async Task InvokeAgentAsync(string input)
         {
-            chat.AddChatMessage(new ChatMessageContent(AuthorRole.User, input));
+            chat.Add(new ChatMessageContent(AuthorRole.User, input));
 
             Console.WriteLine($"# {AuthorRole.User}: '{input}'");
 
