c39eb292ab01b7089b103167227f033931cb0068
edvan_microsoft <edvan@microsoft.com>
2024-06-17 12:03:35 +0200

    updates to naming

--- python/semantic_kernel/vectors/vector_collection_non_schema_base.py
+++ python/semantic_kernel/vectors/vector_collection_non_schema_base.py
@@ -1,19 +1,24 @@
 # Copyright (c) Microsoft. All rights reserved.
 
 
-from typing import Protocol, runtime_checkable
+from abc import ABC, abstractmethod
+
+from semantic_kernel.utils.experimental_decorator import experimental_class
 
 
-@runtime_checkable
-class VectorCollectionNonSchemaProtocol(Protocol):
+@experimental_class
+class VectorCollectionNonSchemaBase(ABC):
+    @abstractmethod
     async def get_collection_names(self, **kwargs) -> list[str]:
         """Get the collection names."""
         ...
 
+    @abstractmethod
     async def collection_exists(self, collection_name: str, **kwargs) -> bool:
         """Check if the collection exists."""
         ...
 
+    @abstractmethod
     async def delete_collection(self, collection_name: str, **kwargs):
         """Delete a collection."""
         ...
