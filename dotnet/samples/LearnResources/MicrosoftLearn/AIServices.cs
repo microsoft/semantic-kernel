9a8dd4fb356d4b26f82b4dbaad5019cd38966fae
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2024-06-20 16:57:22 +0100

    Fix warnings

--- dotnet/samples/LearnResources/MicrosoftLearn/AIServices.cs
+++ dotnet/samples/LearnResources/MicrosoftLearn/AIServices.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
 using Microsoft.SemanticKernel;
-using Microsoft.SemanticKernel.Connectors.AzureOpenAI;
-using Microsoft.SemanticKernel.Connectors.OpenAI;
 
 namespace Examples;
 
