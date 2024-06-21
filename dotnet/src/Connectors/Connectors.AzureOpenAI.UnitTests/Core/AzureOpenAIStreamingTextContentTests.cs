351b86809ffd5a58d5b34ef48265f7a1c15cd3d7
SergeyMenshykh <sergemenshikh@gmail.com>
2024-06-21 17:42:52 +0100

    Copy all files related to azure completion service from `Connectors.OpenAI` to `Connectors.AzureOpenAI` project.

---
+++ dotnet/src/Connectors/Connectors.AzureOpenAI.UnitTests/Core/AzureOpenAIStreamingTextContentTests.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Text;
+using Microsoft.SemanticKernel.Connectors.AzureOpenAI;
+
+namespace SemanticKernel.Connectors.AzureOpenAI.UnitTests.Core;
+
+/// <summary>
+/// Unit tests for <see cref="AzureOpenAIStreamingTextContent"/> class.
+/// </summary>
+public sealed class AzureOpenAIStreamingTextContentTests
+{
+    [Fact]
+    public void ToByteArrayWorksCorrectly()
+    {
+        // Arrange
+        var expectedBytes = Encoding.UTF8.GetBytes("content");
+        var content = new AzureOpenAIStreamingTextContent("content", 0, "model-id");
+
+        // Act
+        var actualBytes = content.ToByteArray();
+
+        // Assert
+        Assert.Equal(expectedBytes, actualBytes);
+    }
+
+    [Theory]
+    [InlineData(null, "")]
+    [InlineData("content", "content")]
+    public void ToStringWorksCorrectly(string? content, string expectedString)
+    {
+        // Arrange
+        var textContent = new AzureOpenAIStreamingTextContent(content!, 0, "model-id");
+
+        // Act
+        var actualString = textContent.ToString();
+
+        // Assert
+        Assert.Equal(expectedString, actualString);
+    }
+}
