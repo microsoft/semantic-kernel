d22ee5af5555ede58eb9a5b93c4b12d55233a695
Tao Chen <taochen@microsoft.com>
2024-06-24 15:00:57 -0700

    Python: Azure Model-as-a-Service Python connector (#6742)
    ### Motivation and Context
    
    <!-- Thank you for your contribution to the semantic-kernel repo!
    Please help reviewers and future users, providing the following
    information:
      1. Why is this change required?
      2. What problem does it solve?
      3. What scenario does it contribute to?
      4. If it fixes an open issue, please link to the issue here.
    -->
    Related to: https://github.com/microsoft/semantic-kernel/pull/6693
    
    Azure Model-as-a-Service allows users to deploy certain models from the
    Azure AI Studio model catalog as an API. This option also provides
    pay-as-you-go access to the models hosted. Below are some of the models
    that are supported:
    - Microsoft Phi-3 family
    - Meta Llama family (Llama 2 chat & Llama 3 instruct)
    - Mistral-Small & Mistral-Large
    - and more
    
    We'd like to provide an AI connector for users of SK to use Azure
    Model-as-a-Service.
    
    ### Description
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    A new AI connector named `azure_ai_inference` is added to support Azure
    Model-as-a-Service. This connector takes a new dependency on the Python
    `azure.ai.inference` SDK.
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [X] The code builds clean without any errors or warnings
    - [X] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [X] All unit tests pass, and I have added new tests where possible
    - [X] I didn't break anyone :smile:
    
    ---------
    
    Co-authored-by: Evan Mattson <35585003+moonbox3@users.noreply.github.com>

--- python/tests/integration/completions/test_chat_completions.py
+++ python/tests/integration/completions/test_chat_completions.py
@@ -5,17 +5,27 @@ from functools import partial, reduce
 from typing import Any
 
 import pytest
+from azure.ai.inference.aio import ChatCompletionsClient
+from azure.core.credentials import AzureKeyCredential
 from openai import AsyncAzureOpenAI
 
 from semantic_kernel import Kernel
+from semantic_kernel.connectors.ai.azure_ai_inference.azure_ai_inference_prompt_execution_settings import (
+    AzureAIInferenceChatPromptExecutionSettings,
+)
+from semantic_kernel.connectors.ai.azure_ai_inference.services.azure_ai_inference_chat_completion import (
+    AzureAIInferenceChatCompletion,
+)
 from semantic_kernel.connectors.ai.chat_completion_client_base import ChatCompletionClientBase
 from semantic_kernel.connectors.ai.function_call_behavior import FunctionCallBehavior
-from semantic_kernel.connectors.ai.open_ai import (
-    AzureChatCompletion,
+from semantic_kernel.connectors.ai.open_ai.prompt_execution_settings.azure_chat_prompt_execution_settings import (
     AzureChatPromptExecutionSettings,
-    OpenAIChatCompletion,
+)
+from semantic_kernel.connectors.ai.open_ai.prompt_execution_settings.open_ai_prompt_execution_settings import (
     OpenAIChatPromptExecutionSettings,
 )
+from semantic_kernel.connectors.ai.open_ai.services.azure_chat_completion import AzureChatCompletion
+from semantic_kernel.connectors.ai.open_ai.services.open_ai_chat_completion import OpenAIChatCompletion
 from semantic_kernel.connectors.ai.open_ai.settings.azure_open_ai_settings import AzureOpenAISettings
 from semantic_kernel.connectors.ai.prompt_execution_settings import PromptExecutionSettings
 from semantic_kernel.contents import ChatHistory, ChatMessageContent, TextContent
@@ -65,10 +75,20 @@ def services() -> dict[str, tuple[ChatCompletionClientBase, type[PromptExecution
             default_headers={"Test-User-X-ID": "test"},
         ),
     )
+    azure_ai_inference_client = AzureAIInferenceChatCompletion(
+        ai_model_id=deployment_name,
+        client=ChatCompletionsClient(
+            endpoint=f'{str(endpoint).strip("/")}/openai/deployments/{deployment_name}',
+            credential=AzureKeyCredential(""),
+            headers={"api-key": api_key},
+        ),
+    )
+
     return {
         "openai": (OpenAIChatCompletion(), OpenAIChatPromptExecutionSettings),
         "azure": (AzureChatCompletion(), AzureChatPromptExecutionSettings),
         "azure_custom_client": (azure_custom_client, AzureChatPromptExecutionSettings),
+        "azure_ai_inference": (azure_ai_inference_client, AzureAIInferenceChatPromptExecutionSettings),
     }
 
 
@@ -281,11 +301,61 @@ pytestmark = pytest.mark.parametrize(
             ["Hello", "well"],
             id="azure_custom_client",
         ),
+        pytest.param(
+            "azure_ai_inference",
+            {},
+            [
+                ChatMessageContent(role=AuthorRole.USER, items=[TextContent(text="Hello")]),
+                ChatMessageContent(role=AuthorRole.USER, items=[TextContent(text="How are you today?")]),
+            ],
+            ["Hello", "well"],
+            id="azure_ai_inference_text_input",
+        ),
+        pytest.param(
+            "azure_ai_inference",
+            {
+                "max_tokens": 256,
+            },
+            [
+                ChatMessageContent(
+                    role=AuthorRole.USER,
+                    items=[
+                        TextContent(text="What is in this image?"),
+                        ImageContent(
+                            uri="https://upload.wikimedia.org/wikipedia/commons/d/d5/Half-timbered_mansion%2C_Zirkel%2C_East_view.jpg"
+                        ),
+                    ],
+                ),
+                ChatMessageContent(role=AuthorRole.USER, items=[TextContent(text="Where was it made?")]),
+            ],
+            ["house", "germany"],
+            id="azure_ai_inference_image_input_uri",
+        ),
+        pytest.param(
+            "azure_ai_inference",
+            {
+                "max_tokens": 256,
+            },
+            [
+                ChatMessageContent(
+                    role=AuthorRole.USER,
+                    items=[
+                        TextContent(text="What is in this image?"),
+                        ImageContent.from_image_path(
+                            image_path=os.path.join(os.path.dirname(__file__), "../../", "assets/sample_image.jpg")
+                        ),
+                    ],
+                ),
+                ChatMessageContent(role=AuthorRole.USER, items=[TextContent(text="Where was it made?")]),
+            ],
+            ["house", "germany"],
+            id="azure_ai_inference_image_input_file",
+        ),
     ],
 )
 
 
-@pytest.mark.asyncio
+@pytest.mark.asyncio(scope="module")
 async def test_chat_completion(
     kernel: Kernel,
     service: str,
@@ -309,7 +379,7 @@ async def test_chat_completion(
         history.add_message(cmc)
 
 
-@pytest.mark.asyncio
+@pytest.mark.asyncio(scope="module")
 async def test_streaming_chat_completion(
     kernel: Kernel,
     service: str,
