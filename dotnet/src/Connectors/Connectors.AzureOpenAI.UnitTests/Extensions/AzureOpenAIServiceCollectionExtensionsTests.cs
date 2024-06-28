f9162e155d59787fafb8c22d847b58dc9433c049
SergeyMenshykh <68852919+SergeyMenshykh@users.noreply.github.com>
2024-06-28 19:30:37 +0100

    Update dotnet/src/Connectors/Connectors.AzureOpenAI.UnitTests/Extensions/AzureOpenAIServiceCollectionExtensionsTests.cs
    Co-authored-by: Dmytro Struk <13853051+dmytrostruk@users.noreply.github.com>

--- dotnet/src/Connectors/Connectors.AzureOpenAI.UnitTests/Extensions/AzureOpenAIServiceCollectionExtensionsTests.cs
+++ dotnet/src/Connectors/Connectors.AzureOpenAI.UnitTests/Extensions/AzureOpenAIServiceCollectionExtensionsTests.cs
@@ -12,7 +12,7 @@ using Microsoft.SemanticKernel.TextGeneration;
 namespace SemanticKernel.Connectors.AzureOpenAI.UnitTests.Extensions;
 
 /// <summary>
-/// Unit tests for <see cref="AzureOpenAIServiceCollectionExtensionsTests"/> class.
+/// Unit tests for <see cref="AzureOpenAIServiceCollectionExtensions"/> class.
 /// </summary>
 public sealed class AzureOpenAIServiceCollectionExtensionsTests
 {
