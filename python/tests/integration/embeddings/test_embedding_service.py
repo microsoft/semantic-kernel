74ec2d0d25935e0f84dc80e787bb4dd23cd2b7b2
Nico Möller <nkm.moeller@gmail.com>
2024-06-29 11:45:04 +0200

    Parametrized Embedding Tests

--- python/tests/integration/embeddings/test_embedding_service.py
+++ python/tests/integration/embeddings/test_embedding_service.py
@@ -4,6 +4,7 @@ import pytest
 
 import semantic_kernel as sk
 from semantic_kernel.connectors.ai.embeddings.embedding_generator_base import EmbeddingGeneratorBase
+from semantic_kernel.connectors.ai.hugging_face import HuggingFaceTextEmbedding
 from semantic_kernel.connectors.ai.mistral_ai import MistralAITextEmbedding
 from semantic_kernel.connectors.ai.open_ai import OpenAITextEmbedding
 from semantic_kernel.core_plugins.text_memory_plugin import TextMemoryPlugin
@@ -19,6 +20,13 @@ pytestmark = pytest.mark.parametrize("embeddings_generator",
         pytest.param(
             OpenAITextEmbedding(ai_model_id="text-embedding-ada-002"),
             id="OpenAIEmbeddings"
+        ),
+        pytest.param(
+            HuggingFaceTextEmbedding(
+                service_id="sentence-transformers/all-MiniLM-L6-v2", 
+                ai_model_id="sentence-transformers/all-MiniLM-L6-v2"
+            ),
+            id="HuggingFaceEmbeddings"
         )
     ]
 )
@@ -37,3 +45,37 @@ async def test_embedding_service(kernel: Kernel, embeddings_generator: Embedding
         text="this is a test",
         external_source_name="external source",
     )
+
+    # Add some documents to the semantic memory
+    await memory.save_information("test", id="info1", text="Sharks are fish.")
+    await memory.save_information("test", id="info2", text="Whales are mammals.")
+    await memory.save_information("test", id="info3", text="Penguins are birds.")
+    await memory.save_information("test", id="info4", text="Dolphins are mammals.")
+    await memory.save_information("test", id="info5", text="Flies are insects.")
+
+    # Search for documents
+    query = "What are mammals?"
+    result = await memory.search("test", query, limit=2, min_relevance_score=0.0)
+    print(f"Query: {query}")
+    print(f"\tAnswer 1: {result[0].text}")
+    print(f"\tAnswer 2: {result[1].text}\n")
+    assert "mammals." in result[0].text
+    assert "mammals." in result[1].text
+
+    query = "What are fish?"
+    result = await memory.search("test", query, limit=1, min_relevance_score=0.0)
+    print(f"Query: {query}")
+    print(f"\tAnswer: {result[0].text}\n")
+    assert result[0].text == "Sharks are fish."
+
+    query = "What are insects?"
+    result = await memory.search("test", query, limit=1, min_relevance_score=0.0)
+    print(f"Query: {query}")
+    print(f"\tAnswer: {result[0].text}\n")
+    assert result[0].text == "Flies are insects."
+
+    query = "What are birds?"
+    result = await memory.search("test", query, limit=1, min_relevance_score=0.0)
+    print(f"Query: {query}")
+    print(f"\tAnswer: {result[0].text}\n")
+    assert result[0].text == "Penguins are birds."
