b48c2f2610a6fbfbefb3c09a7e9031236dece10f
SergeyMenshykh <sergemenshikh@gmail.com>
2024-07-01 20:36:56 +0100

    fix: addressing PR review comments

--- dotnet/src/Connectors/Connectors.AzureOpenAI.UnitTests/Services/AzureOpenAITextEmbeddingGenerationServiceTests.cs
+++ dotnet/src/Connectors/Connectors.AzureOpenAI.UnitTests/Services/AzureOpenAITextEmbeddingGenerationServiceTests.cs
@@ -1,179 +1,90 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
 using System;
+using System.ClientModel;
+using System.IO;
+using System.Net;
 using System.Net.Http;
-using System.Text;
-using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
 using Azure.AI.OpenAI;
-using Azure.Core;
-using Microsoft.Extensions.Logging;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Connectors.AzureOpenAI;
-using Moq;
+using Microsoft.SemanticKernel.Services;
 
 namespace SemanticKernel.Connectors.AzureOpenAI.UnitTests.Services;
 
 /// <summary>
 /// Unit tests for <see cref="AzureOpenAITextEmbeddingGenerationService"/> class.
 /// </summary>
-public sealed class AzureOpenAITextEmbeddingGenerationServiceTests : IDisposable
+public class AzureOpenAITextEmbeddingGenerationServiceTests
 {
-    private readonly HttpMessageHandlerStub _messageHandlerStub;
-    private readonly HttpClient _httpClient;
-    private readonly Mock<ILoggerFactory> _mockLoggerFactory;
-
-    public AzureOpenAITextEmbeddingGenerationServiceTests()
+    [Fact]
+    public void ItCanBeInstantiatedAndPropertiesSetAsExpected()
     {
-        this._messageHandlerStub = new HttpMessageHandlerStub();
-        this._messageHandlerStub.ResponseToReturn = this.SuccessfulResponse;
-
-        this._httpClient = new HttpClient(this._messageHandlerStub, false);
-        this._mockLoggerFactory = new Mock<ILoggerFactory>();
-    }
-
-    [Theory]
-    [InlineData(true)]
-    [InlineData(false)]
-    public void ItCanBeCreatedWithApiKey(bool includeLoggerFactory)
-    {
-        // Arrange & Act
-        var service = includeLoggerFactory ?
-            new AzureOpenAITextEmbeddingGenerationService("deployment-name", "https://endpoint", "api-key", "model-id", loggerFactory: this._mockLoggerFactory.Object) :
-            new AzureOpenAITextEmbeddingGenerationService("deployment-name", "https://endpoint", "api-key", "model-id");
+        // Arrange
+        var sut = new AzureOpenAITextEmbeddingGenerationService("deployment-name", "https://endpoint", "api-key", modelId: "model", dimensions: 2);
+        var sutWithAzureOpenAIClient = new AzureOpenAITextEmbeddingGenerationService("deployment-name", new AzureOpenAIClient(new Uri("https://endpoint"), new ApiKeyCredential("apiKey")), modelId: "model", dimensions: 2);
 
         // Assert
-        Assert.NotNull(service);
-        Assert.Equal("model-id", service.Attributes["ModelId"]);
-    }
-
-    [Theory]
-    [InlineData(true)]
-    [InlineData(false)]
-    public void ItCanBeCreatedWithTokenCredential(bool includeLoggerFactory)
-    {
-        // Arrange & Act
-        var credentials = DelegatedTokenCredential.Create((_, _) => new AccessToken());
-
-        var service = includeLoggerFactory ?
-            new AzureOpenAITextEmbeddingGenerationService("deployment", "https://endpoint", credentials, "model-id", loggerFactory: this._mockLoggerFactory.Object) :
-            new AzureOpenAITextEmbeddingGenerationService("deployment", "https://endpoint", credentials, "model-id");
-
-        // Assert
-        Assert.NotNull(service);
-        Assert.Equal("model-id", service.Attributes["ModelId"]);
-    }
-
-    [Theory]
-    [InlineData(true)]
-    [InlineData(false)]
-    public void ItCanBeCreatedWithAzureOpenAIClient(bool includeLoggerFactory)
-    {
-        // Arrange & Act
-        var client = new AzureOpenAIClient(new Uri("http://host"), "key");
-
-        var service = includeLoggerFactory ?
-            new AzureOpenAITextEmbeddingGenerationService("deployment", client, "model-id", loggerFactory: this._mockLoggerFactory.Object) :
-            new AzureOpenAITextEmbeddingGenerationService("deployment", client, "model-id");
-
-        // Assert
-        Assert.NotNull(service);
-        Assert.Equal("model-id", service.Attributes["ModelId"]);
+        Assert.NotNull(sut);
+        Assert.NotNull(sutWithAzureOpenAIClient);
+        Assert.Equal("model", sut.Attributes[AIServiceExtensions.ModelIdKey]);
+        Assert.Equal("model", sutWithAzureOpenAIClient.Attributes[AIServiceExtensions.ModelIdKey]);
     }
 
     [Fact]
-    public async Task ItCanReturnEmptyResultWhenGenerateEmbeddingsForEmptyDataAsync()
+    public async Task ItGetEmbeddingsAsyncReturnsEmptyWhenProvidedDataIsEmpty()
     {
         // Arrange
-        var service = new AzureOpenAITextEmbeddingGenerationService("deployment-name", "https://endpoint", "api-key", "model-id", this._httpClient);
+        var sut = new AzureOpenAITextEmbeddingGenerationService("deployment-name", "https://endpoint", "api-key");
 
         // Act
-        var result = await service.GenerateEmbeddingsAsync([]);
+        var result = await sut.GenerateEmbeddingsAsync([], null, CancellationToken.None);
 
         // Assert
         Assert.Empty(result);
     }
 
     [Fact]
-    public async Task ItShouldThrowExceptionWhenGenerateEmbeddingsWithEmptyResponseAsync()
+    public async Task GetEmbeddingsAsyncReturnsEmptyWhenProvidedDataIsWhitespace()
     {
         // Arrange
-        var service = new AzureOpenAITextEmbeddingGenerationService("deployment-name", "https://endpoint", "api-key", "model-id", this._httpClient);
-        this._messageHandlerStub.ResponseToReturn = new HttpResponseMessage(System.Net.HttpStatusCode.OK)
+        using HttpMessageHandlerStub handler = new()
         {
-            Content = new StringContent("""
+            ResponseToReturn = new HttpResponseMessage(HttpStatusCode.OK)
             {
-                "object": "list",
-                "data": [],
-                "model": "model-id"
+                Content = new StringContent(File.ReadAllText("./TestData/text-embeddings-response.txt"))
             }
-            """, Encoding.UTF8, "application/json")
         };
+        using HttpClient client = new(handler);
 
-        // Act & Assert
-        var exception = await Assert.ThrowsAsync<KernelException>(() => service.GenerateEmbeddingsAsync(["test"]));
-        Assert.Equal("Expected 1 text embedding(s), but received 0", exception.Message);
-    }
-
-    [Fact]
-    public async Task ItGeneratesEmbeddingsAsync()
-    {
-        // Arrange
-        var service = new AzureOpenAITextEmbeddingGenerationService("deployment-name", "https://endpoint", "api-key", "model-id", this._httpClient);
+        var sut = new AzureOpenAITextEmbeddingGenerationService("deployment-name", "https://endpoint", "api-key", httpClient: client);
 
         // Act
-        var result = await service.GenerateEmbeddingsAsync(["test"]);
+        var result = await sut.GenerateEmbeddingsAsync(["test"], null, CancellationToken.None);
 
         // Assert
         Assert.Single(result);
-
-        var memory = result[0];
-
-        Assert.Equal(0.018990106880664825, memory.Span[0]);
-        Assert.Equal(-0.0073809814639389515, memory.Span[1]);
+        Assert.Equal(4, result[0].Length);
     }
 
     [Fact]
-    public async Task ItGeneratesEmbeddingsWithDimensionsWorksAsync()
+    public async Task ItThrowsIfNumberOfResultsDiffersFromInputsAsync()
     {
         // Arrange
-        var service = new AzureOpenAITextEmbeddingGenerationService("deployment-name", "https://endpoint", "api-key", "model-id", this._httpClient, dimensions: 256);
-
-        // Act
-        await service.GenerateEmbeddingsAsync(["test"]);
-
-        var requestContent = Encoding.UTF8.GetString(this._messageHandlerStub.RequestContent!);
-        var optionsJson = JsonSerializer.Deserialize<JsonElement>(requestContent);
-
-        // Assert
-        Assert.Equal(256, optionsJson.GetProperty("dimensions").GetInt32());
-    }
-
-    public void Dispose()
-    {
-        this._httpClient.Dispose();
-        this._messageHandlerStub.Dispose();
-    }
-
-    #region private
-
-    private HttpResponseMessage SuccessfulResponse
-        => new(System.Net.HttpStatusCode.OK)
+        using HttpMessageHandlerStub handler = new()
         {
-            Content = new StringContent("""
+            ResponseToReturn = new HttpResponseMessage(HttpStatusCode.OK)
             {
-                "object": "list",
-                "data": [
-                    {
-                        "object": "embedding",
-                        "embedding": "JJGbPCnc8bs=",
-                        "index": 0
-                    }
-                ],
-                "model": "model-id"
+                Content = new StringContent(File.ReadAllText("./TestData/text-embeddings-multiple-response.txt"))
             }
-            """, Encoding.UTF8, "application/json")
         };
+        using HttpClient client = new(handler);
 
-    #endregion
+        var sut = new AzureOpenAITextEmbeddingGenerationService("deployment-name", "https://endpoint", "api-key", httpClient: client);
+
+        // Act & Assert
+        await Assert.ThrowsAsync<KernelException>(async () => await sut.GenerateEmbeddingsAsync(["test"], null, CancellationToken.None));
+    }
 }
