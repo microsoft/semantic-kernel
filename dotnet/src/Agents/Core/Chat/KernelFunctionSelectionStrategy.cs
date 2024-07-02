c225255d673bfc60fb05efc55e75559cb2055e73
Chris Rickman <crickman@microsoft.com>
2024-07-02 16:15:31 -0700

    Namespace

--- dotnet/src/Agents/Core/Chat/KernelFunctionSelectionStrategy.cs
+++ dotnet/src/Agents/Core/Chat/KernelFunctionSelectionStrategy.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
 
 namespace Microsoft.SemanticKernel.Agents.Chat;
 
