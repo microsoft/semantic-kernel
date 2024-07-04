641aa2c6088618255f922df33624f188669f2c5d
Krzysztof Kasprowicz <krzysztof@impe.pl>
2024-07-04 21:10:32 +0200

    Added integration tests for chat completion.

---
+++ dotnet/src/IntegrationTests/Connectors/Anthropic/TestsBase.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.Anthropic;
+using Xunit.Abstractions;
+
+namespace SemanticKernel.IntegrationTests.Connectors.Anthropic;
+
+public abstract class TestsBase(ITestOutputHelper output)
+{
+    private readonly IConfigurationRoot _configuration = new ConfigurationBuilder()
+        .AddJsonFile(path: "testsettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile(path: "testsettings.development.json", optional: true, reloadOnChange: true)
+        .AddUserSecrets<TestsBase>()
+        .AddEnvironmentVariables()
+        .Build();
+
+    protected ITestOutputHelper Output { get; } = output;
+
+    protected IChatCompletionService GetChatService(ServiceType serviceType) => serviceType switch
+    {
+        ServiceType.Anthropic => new AnthropicChatCompletionService(
+            modelId: this.AnthropicGetModel(),
+            apiKey: this.AnthropicGetApiKey()),
+        ServiceType.VertexAI => throw new NotImplementedException("Implement in next PR"), // TODO: Implement in next PR
+        _ => throw new ArgumentOutOfRangeException(nameof(serviceType), serviceType, null)
+    };
+
+    public enum ServiceType
+    {
+        Anthropic,
+        VertexAI
+    }
+
+    private string AnthropicGetModel() => this._configuration.GetSection("Anthropic:ModelId").Get<string>()!;
+    private string AnthropicGetApiKey() => this._configuration.GetSection("Anthropic:ApiKey").Get<string>()!;
+    private string VertexAIGetModel() => this._configuration.GetSection("VertexAI:Anthropic:ModelId").Get<string>()!;
+    private string VertexAIGetEndpoint() => this._configuration.GetSection("VertexAI:Anthropic:Endpoint").Get<string>()!;
+    private string VertexAIGetBearerKey() => this._configuration.GetSection("VertexAI:BearerKey").Get<string>()!;
+}
