2b451cc9f8f8f5bded9ac2ca40c2d3e6795b57a4
westey <164392973+westey-m@users.noreply.github.com>
2024-07-10 16:15:33 +0100

    Address build issues.

--- dotnet/src/Connectors/Connectors.Redis.UnitTests/RedisVectorRecordStoreTests.cs
+++ dotnet/src/Connectors/Connectors.Redis.UnitTests/RedisVectorRecordStoreTests.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json.Nodes;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.SemanticKernel.Data;
 using Moq;
