cf76c0e8a58d221dcc8e651eb63b2478b8b2256a
westey <164392973+westey-m@users.noreply.github.com>
2024-07-17 18:28:34 +0100

    .Net: Add IVectorStore interface and Volatile implementation (#7332)
    ### Motivation and Context
    
    As part of the memory connector redesign we have fixed on a design where
    we have a VectorStore that produces VectorStoreRecordCollection
    instances. These are tied to a collection and will expose single
    collection operations.
    
    ### Description
    
    This PR adds:
    - The new IVectorStore interface that can serve collection instances and
    list collection names.
    - An in memory (Volatile) implementation of the new interface.
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [x] The code builds clean without any errors or warnings
    - [x] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [x] All unit tests pass, and I have added new tests where possible
    - [x] I didn't break anyone :smile:

--- dotnet/src/SemanticKernel.Abstractions/Data/RecordAttributes/VectorStoreRecordDataAttribute.cs
+++ dotnet/src/SemanticKernel.Abstractions/Data/RecordAttributes/VectorStoreRecordDataAttribute.cs
@@ -6,8 +6,12 @@ using System.Diagnostics.CodeAnalysis;
 namespace Microsoft.SemanticKernel.Data;
 
 /// <summary>
-/// Attribute to mark a property on a record class as data.
+/// Attribute to mark a property on a record class as 'data'.
 /// </summary>
+/// <remarks>
+/// Marking a property as 'data' means that the property is not a key, and not a vector, but optionally
+/// this property may have an associated vector field containing an embedding for this data.
+/// </remarks>
 [Experimental("SKEXP0001")]
 [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
 public sealed class VectorStoreRecordDataAttribute : Attribute
