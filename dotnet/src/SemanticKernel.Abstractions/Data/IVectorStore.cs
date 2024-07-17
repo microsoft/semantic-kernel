9b035f04e95704e8d0b8c57e17716b0c499e433e
westey <164392973+westey-m@users.noreply.github.com>
2024-07-17 16:26:03 +0100

    Add IVectorStore interface and Volatile implementation.

---
+++ dotnet/src/SemanticKernel.Abstractions/Data/IVectorStore.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Microsoft.SemanticKernel.Data;
+
+/// <summary>
+/// Interface for managing the collections and records in a vector store.
+/// </summary>
+public interface IVectorStore
+{
+    /// <summary>
+    /// Get a collection from the vector store.
+    /// </summary>
+    /// <typeparam name="TKey">The data type of the record key.</typeparam>
+    /// <typeparam name="TRecord">The record data model to use for adding, updating and retrieving data from the collection.</typeparam>
+    /// <param name="name">The name of the collection.</param>
+    /// <param name="vectorStoreRecordDefinition">Defines the schema of the record type.</param>
+    /// <returns>A new <see cref="IVectorStoreRecordCollection{TKey, TRecord}"/> instance for managing the records in the collection.</returns>
+    IVectorStoreRecordCollection<TKey, TRecord> GetCollection<TKey, TRecord>(string name, VectorStoreRecordDefinition? vectorStoreRecordDefinition = null)
+        where TRecord : class;
+
+    /// <summary>
+    /// Retrieve the names of all the collections in the vector store.
+    /// </summary>
+    /// <param name="cancellationToken">The <see cref="CancellationToken"/> to monitor for cancellation requests. The default is <see cref="CancellationToken.None"/>.</param>
+    /// <returns>The list of names of all the collections in the vector store.</returns>
+    IAsyncEnumerable<string> ListCollectionNamesAsync(CancellationToken cancellationToken = default);
+}
---
+++ dotnet/src/SemanticKernel.Core/Data/VolatileVectorStore.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading;
+
+namespace Microsoft.SemanticKernel.Data;
+
+/// <summary>
+/// Service for storing and retrieving vector records, and managing vector record collections, that uses an in memory dictionary as the underlying storage.
+/// </summary>
+[Experimental("SKEXP0001")]
+public class VolatileVectorStore : IVectorStore
+{
+    /// <summary>Internal storage for the record collection.</summary>
+    private readonly ConcurrentDictionary<string, ConcurrentDictionary<string, object>> _internalCollection;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="VolatileVectorStore"/> class.
+    /// </summary>
+    public VolatileVectorStore()
+    {
+        this._internalCollection = new();
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="VolatileVectorStore"/> class.
+    /// </summary>
+    /// <param name="internalCollection">Allows passing in the dictionary used for storage, for testing purposes.</param>
+    internal VolatileVectorStore(ConcurrentDictionary<string, ConcurrentDictionary<string, object>> internalCollection)
+    {
+        this._internalCollection = internalCollection;
+    }
+
+    /// <inheritdoc />
+    public IVectorStoreRecordCollection<TKey, TRecord> GetCollection<TKey, TRecord>(string name, VectorStoreRecordDefinition? vectorStoreRecordDefinition = null) where TRecord : class
+    {
+        if (typeof(TKey) != typeof(string))
+        {
+            throw new NotSupportedException("Only string keys are supported.");
+        }
+
+        var typedInternalCollection = this._internalCollection as ConcurrentDictionary<string, ConcurrentDictionary<string, TRecord>>;
+        var collection = new VolatileVectorStoreRecordCollection<TRecord>(typedInternalCollection!, name, new() { VectorStoreRecordDefinition = vectorStoreRecordDefinition }) as IVectorStoreRecordCollection<TKey, TRecord>;
+        return collection!;
+    }
+
+    /// <inheritdoc />
+    public IAsyncEnumerable<string> ListCollectionNamesAsync(CancellationToken cancellationToken = default)
+    {
+        return this._internalCollection.Keys.ToAsyncEnumerable();
+    }
+}
