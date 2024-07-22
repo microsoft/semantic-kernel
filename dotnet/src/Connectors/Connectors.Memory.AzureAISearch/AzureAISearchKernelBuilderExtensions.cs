260debe2f7e5e01921843b86e3dd07fc338abf48
westey <164392973+westey-m@users.noreply.github.com>
2024-07-22 14:06:15 +0100

    Add basic KernelBuilder and ServiceCollection extensions for registering VectorStores.

---
+++ dotnet/src/Connectors/Connectors.Memory.AzureAISearch/AzureAISearchKernelBuilderExtensions.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using Azure;
+using Azure.Core;
+using Azure.Search.Documents.Indexes;
+using Microsoft.SemanticKernel.Data;
+
+namespace Microsoft.SemanticKernel.Connectors.AzureAISearch;
+
+/// <summary>
+/// Extension methods to register Azure AI Search <see cref="IVectorStore"/> instances on the <see cref="IKernelBuilder"/>.
+/// </summary>
+public static class AzureAISearchKernelBuilderExtensions
+{
+    /// <summary>
+    /// Register an Azure AI Search <see cref="IVectorStore"/> with the specified service ID and where <see cref="SearchIndexClient"/> is retrieved from the dependency injection container.
+    /// </summary>
+    /// <param name="builder">The builder to register the <see cref="IVectorStore"/> on.</param>
+    /// <param name="serviceId">An optional service id to use as the service key.</param>
+    /// <param name="options">Optoinal options to further configure the <see cref="IVectorStore"/>.</param>
+    /// <returns>The kernel builder.</returns>
+    public static IKernelBuilder AddAzureAISearchVectorStore(this IKernelBuilder builder, string? serviceId = default, AzureAISearchVectorStoreOptions? options = default)
+    {
+        builder.Services.AddAzureAISearchVectorStore(serviceId, options);
+        return builder;
+    }
+
+    /// <summary>
+    /// Register an Azure AI Search <see cref="IVectorStore"/> with the provided <see cref="Uri"/> and <see cref="TokenCredential"/> and the specified service ID.
+    /// </summary>
+    /// <param name="builder">The builder to register the <see cref="IVectorStore"/> on.</param>
+    /// <param name="endpoint">The service endpoint for Azure AI Search.</param>
+    /// <param name="tokenCredential">The credential to authenticate to Azure AI Search with.</param>
+    /// <param name="serviceId">An optional service id to use as the service key.</param>
+    /// <param name="options">Optoinal options to further configure the <see cref="IVectorStore"/>.</param>
+    /// <returns>The kernel builder.</returns>
+    public static IKernelBuilder AddAzureAISearchVectorStore(this IKernelBuilder builder, Uri endpoint, TokenCredential tokenCredential, string? serviceId = default, AzureAISearchVectorStoreOptions? options = default)
+    {
+        builder.Services.AddAzureAISearchVectorStore(endpoint, tokenCredential, serviceId, options);
+        return builder;
+    }
+
+    /// <summary>
+    /// Register an Azure AI Search <see cref="IVectorStore"/> with the provided <see cref="Uri"/> and <see cref="AzureKeyCredential"/> and the specified service ID.
+    /// </summary>
+    /// <param name="builder">The builder to register the <see cref="IVectorStore"/> on.</param>
+    /// <param name="endpoint">The service endpoint for Azure AI Search.</param>
+    /// <param name="credential">The credential to authenticate to Azure AI Search with.</param>
+    /// <param name="serviceId">An optional service id to use as the service key.</param>
+    /// <param name="options">Optoinal options to further configure the <see cref="IVectorStore"/>.</param>
+    /// <returns>The kernel builder.</returns>
+    public static IKernelBuilder AddAzureAISearchVectorStore(this IKernelBuilder builder, Uri endpoint, AzureKeyCredential credential, string? serviceId = default, AzureAISearchVectorStoreOptions? options = default)
+    {
+        builder.Services.AddAzureAISearchVectorStore(endpoint, credential, serviceId, options);
+        return builder;
+    }
+}
---
+++ dotnet/src/Connectors/Connectors.Memory.Redis/RedisKernelBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.SemanticKernel.Data;
+using StackExchange.Redis;
+
+namespace Microsoft.SemanticKernel.Connectors.Redis;
+
+/// <summary>
+/// Extension methods to register Redis <see cref="IVectorStore"/> instances on the <see cref="IKernelBuilder"/>.
+/// </summary>
+public static class RedisKernelBuilderExtensions
+{
+    /// <summary>
+    /// Register a Redis <see cref="IVectorStore"/> with the specified service ID.
+    /// </summary>
+    /// <param name="builder">The builder to register the <see cref="IVectorStore"/> on.</param>
+    /// <param name="redisConnectionConfiguration">The Redis connection configuration string. If not provided, an <see cref="IDatabase"/> instance will be requested from the dependency injection container.</param>
+    /// <param name="serviceId">An optional service id to use as the service key.</param>
+    /// <param name="options">Optoinal options to further configure the <see cref="IVectorStore"/>.</param>
+    /// <returns>The kernel builder.</returns>
+    public static IKernelBuilder AddRedisVectorStore(this IKernelBuilder builder, string? redisConnectionConfiguration = default, string? serviceId = default, RedisVectorStoreOptions? options = default)
+    {
+        builder.Services.AddRedisVectorStore(redisConnectionConfiguration, serviceId, options);
+        return builder;
+    }
+}
