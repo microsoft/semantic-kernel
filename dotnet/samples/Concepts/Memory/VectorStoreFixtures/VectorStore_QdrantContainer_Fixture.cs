326df24b7bb82255edf34be1cdb9b17efa3e124e
westey <164392973+westey-m@users.noreply.github.com>
2024-07-22 17:29:47 +0100

    Add Data ingestion sample for VectorStore

---
+++ dotnet/samples/Concepts/Memory/VectorStoreFixtures/VectorStore_QdrantContainer_Fixture.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Docker.DotNet;
+
+namespace Memory.VectorStoreFixtures;
+
+/// <summary>
+/// Fixture to use for creating a Qdrant container before tests and delete it after tests.
+/// </summary>
+public class VectorStore_QdrantContainer_Fixture : IAsyncLifetime
+{
+    private DockerClient? _dockerClient;
+    private string? _qdrantContainerId;
+
+    public async Task InitializeAsync()
+    {
+        // Connect to docker and start the docker container.
+        using var dockerClientConfiguration = new DockerClientConfiguration();
+        this._dockerClient = dockerClientConfiguration.CreateClient();
+        this._qdrantContainerId = await VectorStore_Infra.SetupQdrantContainerAsync(this._dockerClient);
+    }
+
+    public async Task DisposeAsync()
+    {
+        if (this._dockerClient != null && this._qdrantContainerId != null)
+        {
+            // Delete docker container.
+            await VectorStore_Infra.DeleteContainerAsync(this._dockerClient, this._qdrantContainerId);
+        }
+    }
+}
---
+++ dotnet/samples/Concepts/Memory/VectorStoreFixtures/VectorStore_RedisContainer_Fixture.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Docker.DotNet;
+
+namespace Memory.VectorStoreFixtures;
+
+/// <summary>
+/// Fixture to use for creating a Redis container before tests and delete it after tests.
+/// </summary>
+public class VectorStore_RedisContainer_Fixture : IAsyncLifetime
+{
+    private DockerClient? _dockerClient;
+    private string? _redisContainerId;
+
+    public async Task InitializeAsync()
+    {
+        // Connect to docker and start the docker container.
+        using var dockerClientConfiguration = new DockerClientConfiguration();
+        this._dockerClient = dockerClientConfiguration.CreateClient();
+        this._redisContainerId = await VectorStore_Infra.SetupRedisContainerAsync(this._dockerClient);
+    }
+
+    public async Task DisposeAsync()
+    {
+        if (this._dockerClient != null && this._redisContainerId != null)
+        {
+            // Delete docker container.
+            await VectorStore_Infra.DeleteContainerAsync(this._dockerClient, this._redisContainerId);
+        }
+    }
+}
