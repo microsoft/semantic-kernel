bb91cc69e907698e4391a3a45ae1372eed1ffc20
westey <164392973+westey-m@users.noreply.github.com>
2024-07-23 18:32:59 +0100

    .Net: Add Data ingestion sample for VectorStore (#7388)
    ### Motivation and Context
    
    Adding a sample that uses the new VectorStore implementations.
    
    Alternative suggestions for the best sample category for these are
    welcome. For now they are in the Memory folder.
    I have not updated the samples README.md yet, and will update, once we
    have a plan around where these should go.
    
    #7349
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [x] The code builds clean without any errors or warnings
    - [x] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [x] All unit tests pass, and I have added new tests where possible
    - [x] I didn't break anyone :smile:

---
+++ dotnet/samples/Concepts/Memory/VectorStoreFixtures/VectorStoreQdrantContainerFixture.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Docker.DotNet;
+
+namespace Memory.VectorStoreFixtures;
+
+/// <summary>
+/// Fixture to use for creating a Qdrant container before tests and delete it after tests.
+/// </summary>
+public class VectorStoreQdrantContainerFixture : IAsyncLifetime
+{
+    private DockerClient? _dockerClient;
+    private string? _qdrantContainerId;
+
+    public async Task InitializeAsync()
+    {
+        // Connect to docker and start the docker container.
+        using var dockerClientConfiguration = new DockerClientConfiguration();
+        this._dockerClient = dockerClientConfiguration.CreateClient();
+        this._qdrantContainerId = await VectorStoreInfra.SetupQdrantContainerAsync(this._dockerClient);
+    }
+
+    public async Task DisposeAsync()
+    {
+        if (this._dockerClient != null && this._qdrantContainerId != null)
+        {
+            // Delete docker container.
+            await VectorStoreInfra.DeleteContainerAsync(this._dockerClient, this._qdrantContainerId);
+        }
+    }
+}
