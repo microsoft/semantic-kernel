3ca3a34af995f953af27481201fb61513dd4980f
Charlize Yeh <charyeh@amazon.com>
2024-07-24 13:26:54 -0700

    arrange act assert kernel builder extension unit tests

--- dotnet/src/Connectors/Connectors.Amazon.UnitTests/BedrockKernelBuilderExtensionTests.cs
+++ dotnet/src/Connectors/Connectors.Amazon.UnitTests/BedrockKernelBuilderExtensionTests.cs
@@ -10,31 +10,45 @@ using Xunit;
 
 namespace Connectors.Amazon.UnitTests;
 
+/// <summary>
+/// Kernel Builder Extension Tests for Bedrock.
+/// </summary>
 public class BedrockKernelBuilderExtensionTests
 {
+    /// <summary>
+    /// Checks that AddBedrockTextGenerationService builds a proper kernel.
+    /// </summary>
     [Fact]
     public void AddBedrockTextGenerationCreatesService()
     {
+        // Arrange
         var builder = Kernel.CreateBuilder();
         builder.AddBedrockTextGenerationService("amazon.titan-text-premier-v1:0");
 
+        // Act
         var kernel = builder.Build();
         var service = kernel.GetRequiredService<ITextGenerationService>();
 
+        // Assert
         Assert.NotNull(kernel);
         Assert.NotNull(service);
         Assert.IsType<BedrockTextGenerationService>(service);
     }
-
+    /// <summary>
+    /// Checks that AddBedrockChatCompletionService builds a proper kernel.
+    /// </summary>
     [Fact]
     public void AddBedrockChatCompletionCreatesService()
     {
+        // Arrange
         var builder = Kernel.CreateBuilder();
         builder.AddBedrockChatCompletionService("amazon.titan-text-premier-v1:0");
 
+        // Act
         var kernel = builder.Build();
         var service = kernel.GetRequiredService<IChatCompletionService>();
 
+        // Assert
         Assert.NotNull(kernel);
         Assert.NotNull(service);
         Assert.IsType<BedrockChatCompletionService>(service);
