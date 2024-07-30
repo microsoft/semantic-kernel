023c91b1afad66d4a822705689b1b2bf1339469e
Caleb Kiage <747955+calebkiage@users.noreply.github.com>
2024-07-30 15:09:38 +0300

    Add OpenAPI parser tests

--- dotnet/src/Functions/Functions.OpenApi/OpenApi/OpenApiDocumentParser.cs
+++ dotnet/src/Functions/Functions.OpenApi/OpenApi/OpenApiDocumentParser.cs
@@ -123,13 +123,39 @@ internal sealed class OpenApiDocumentParser(ILoggerFactory? loggerFactory = null
     /// <returns>JSON content stream.</returns>
     private static async Task<JsonObject?> ConvertContentToJsonAsync(Stream stream, CancellationToken cancellationToken = default)
     {
+        var inputStream = stream;
+        try
+        {
+            if (stream.CanSeek)
+            {
+                // Fast path if stream is already json and seekable
+                return await JsonSerializer.DeserializeAsync<JsonObject>(inputStream, cancellationToken: cancellationToken).ConfigureAwait(false);
+            }
+
+            inputStream = new MemoryStream();
+            // Copy stream if it cannot be seeked. Needed for YAML streams which will fail JSON deserialization.
+            await stream.CopyToAsync(inputStream).ConfigureAwait(false);
+            return await JsonSerializer.DeserializeAsync<JsonObject>(inputStream, cancellationToken: cancellationToken).ConfigureAwait(false);
+        }
+        catch (JsonException)
+        { /*Not JSON. Maybe YAML*/
+        }
+        finally
+        {
+            if (inputStream != stream)
+            {
+                inputStream.Dispose();
+            }
+        }
+
+        inputStream.Seek(0, SeekOrigin.Begin);
         var serializer = new SharpYaml.Serialization.Serializer();
 
-        var obj = serializer.Deserialize(stream);
-
-        using var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(JsonSerializer.Serialize(obj)));
-
-        return await JsonSerializer.DeserializeAsync<JsonObject>(memoryStream, cancellationToken: cancellationToken).ConfigureAwait(false);
+        var obj = serializer.Deserialize(inputStream);
+        using var serializedStream = new MemoryStream();
+        await JsonSerializer.SerializeAsync(serializedStream, obj, cancellationToken: cancellationToken).ConfigureAwait(false);
+        serializedStream.Seek(0, SeekOrigin.Begin);
+        return await JsonSerializer.DeserializeAsync<JsonObject>(serializedStream, cancellationToken: cancellationToken).ConfigureAwait(false);
     }
 
     /// <summary>
---
+++ dotnet/src/Functions/Functions.UnitTests/OpenApi/OpenApiDocumentParserV30FeatureTests.cs
@@ -0,0 +1,88 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SemanticKernel.Plugins.OpenApi;
+using Xunit;
+
+namespace SemanticKernel.Functions.UnitTests.OpenApi;
+
+public static class OpenApiDocumentParserV30FeatureTests
+{
+    public class OneOfTests
+    {
+        [Fact(Skip = "OneOf is not supported yet")]
+        public async Task ParsesSimpleOneOfAsync()
+        {
+            var api = """
+                      {"openapi":"3.0.3","info":{"title":"Test OneOf0","version":"0"},
+                      "paths":{"/fooOrBar":{"get":{"responses":{"200":{"$ref":"#/components/schemas/fooOrBar"}}}}},
+                      "components":{"schemas":{"foo":{"type":"object","properties":{"name":{"type":"string"},
+                      "extra":{"type":"string"}}},"bar":{"type":"string"},
+                      "fooOrBar":{"oneOf":[{"$ref":"#/components/schemas/foo"},{"$ref":"#/components/schemas/bar"}]}}}}
+                      """.Trim();
+            var ms = new MemoryStream(Encoding.Default.GetBytes(api));
+            var parser = new OpenApiDocumentParser();
+
+            var spec = await parser.ParseAsync(ms);
+
+            Assert.NotEmpty(spec.Operations);
+            var op0 = spec.Operations[0];
+            Assert.NotEmpty(op0.Responses);
+            var res200 = op0.Responses["200"];
+            Assert.NotNull(res200.Schema);
+        }
+    }
+
+    public class AllOfTests
+    {
+        [Fact(Skip = "AllOf is not supported yet")]
+        public async Task ParsesSimpleAllOfAsync()
+        {
+            var api = """
+                      {"openapi":"3.0.3","info":{"title":"Test AllOf0","version":"0"},
+                      "paths":{"/fooOrBar":{"get":{"responses":{"200":{"$ref":"#/components/schemas/fooAndBar"}}}}},
+                      "components":{"schemas":{"foo":{"type":"object","properties":{"name":{"type":"string"},
+                      "extra":{"type":"string"}}},"bar":{"type":"object","properties":{"description":{"type":"string"}}},
+                      "fooAndBar":{"allOf":[{"$ref":"#/components/schemas/foo"},{"$ref":"#/components/schemas/bar"}]}}}}
+                      """.Trim();
+            var ms = new MemoryStream(Encoding.Default.GetBytes(api));
+            var parser = new OpenApiDocumentParser();
+
+            var spec = await parser.ParseAsync(ms);
+
+            Assert.NotEmpty(spec.Operations);
+            var op0 = spec.Operations[0];
+            Assert.NotEmpty(op0.Responses);
+            var res200 = op0.Responses["200"];
+            Assert.NotNull(res200.Schema);
+        }
+    }
+
+    public class AnyOfTests
+    {
+        [Fact(Skip = "AnyOf is not supported yet")]
+        public async Task ParsesSimpleAllOfAsync()
+        {
+            var api = """
+                      {"openapi":"3.0.3","info":{"title":"Test AnyOf0","version":"0"},
+                      "paths":{"/fooOrBar":{"get":{"responses":{"200":{"$ref":"#/components/schemas/fooOrBar"}}}}},
+                      "components":{"schemas":{"foo":{"type":"object","properties":{"name":{"type":"string"},
+                      "extra":{"type":"string"}}},"bar":{"type":"string"},
+                      "fooOrBar":{"anyOf":[{"$ref":"#/components/schemas/foo"},{"$ref":"#/components/schemas/bar"}]}}}}
+                      """.Trim();
+            var ms = new MemoryStream(Encoding.Default.GetBytes(api));
+            var parser = new OpenApiDocumentParser();
+
+            var spec = await parser.ParseAsync(ms);
+
+            Assert.NotEmpty(spec.Operations);
+            var op0 = spec.Operations[0];
+            Assert.NotEmpty(op0.Responses);
+            var res200 = op0.Responses["200"];
+            Assert.NotNull(res200.Schema);
+        }
+    }
+}
