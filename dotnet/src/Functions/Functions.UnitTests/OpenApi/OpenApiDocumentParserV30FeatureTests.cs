6ff1aba3ed3ec6e441e1670dd0f0a3fc908ccd5b
Caleb Kiage <747955+calebkiage@users.noreply.github.com>
2024-07-30 17:28:12 +0300

    Update unit tests

--- dotnet/src/Functions/Functions.UnitTests/OpenApi/OpenApiDocumentParserV30FeatureTests.cs
+++ dotnet/src/Functions/Functions.UnitTests/OpenApi/OpenApiDocumentParserV30FeatureTests.cs
@@ -12,12 +12,13 @@ public static class OpenApiDocumentParserV30FeatureTests
 {
     public class OneOfTests
     {
-        [Fact(Skip = "OneOf is not supported yet")]
-        public async Task ParsesSimpleOneOfAsync()
+        [Fact]
+        public async Task ParsesOneOfAsync()
         {
             var api = """
                       {"openapi":"3.0.3","info":{"title":"Test OneOf0","version":"0"},
-                      "paths":{"/fooOrBar":{"get":{"responses":{"200":{"$ref":"#/components/schemas/fooOrBar"}}}}},
+                      "paths":{"/fooOrBar":{"get":{"responses":{"200":{"content":{"application/json":{
+                      "schema":{"$ref":"#/components/schemas/fooOrBar"}}},"description":"response"}}}}},
                       "components":{"schemas":{"foo":{"type":"object","properties":{"name":{"type":"string"},
                       "extra":{"type":"string"}}},"bar":{"type":"string"},
                       "fooOrBar":{"oneOf":[{"$ref":"#/components/schemas/foo"},{"$ref":"#/components/schemas/bar"}]}}}}
@@ -32,17 +33,22 @@ public static class OpenApiDocumentParserV30FeatureTests
             Assert.NotEmpty(op0.Responses);
             var res200 = op0.Responses["200"];
             Assert.NotNull(res200.Schema);
+            var foo = res200.Schema.RootElement.GetProperty("oneOf")[0];
+            Assert.Equal("object", foo.GetProperty("type").GetString());
+            var bar = res200.Schema.RootElement.GetProperty("oneOf")[1];
+            Assert.Equal("string", bar.GetProperty("type").GetString());
         }
     }
 
     public class AllOfTests
     {
-        [Fact(Skip = "AllOf is not supported yet")]
-        public async Task ParsesSimpleAllOfAsync()
+        [Fact]
+        public async Task ParsesAllOfAsync()
         {
             var api = """
                       {"openapi":"3.0.3","info":{"title":"Test AllOf0","version":"0"},
-                      "paths":{"/fooOrBar":{"get":{"responses":{"200":{"$ref":"#/components/schemas/fooAndBar"}}}}},
+                      "paths":{"/fooOrBar":{"get":{"responses":{"200":{"content":{"application/json":{
+                      "schema":{"$ref":"#/components/schemas/fooAndBar"}}},"description":"response"}}}}},
                       "components":{"schemas":{"foo":{"type":"object","properties":{"name":{"type":"string"},
                       "extra":{"type":"string"}}},"bar":{"type":"object","properties":{"description":{"type":"string"}}},
                       "fooAndBar":{"allOf":[{"$ref":"#/components/schemas/foo"},{"$ref":"#/components/schemas/bar"}]}}}}
@@ -57,17 +63,22 @@ public static class OpenApiDocumentParserV30FeatureTests
             Assert.NotEmpty(op0.Responses);
             var res200 = op0.Responses["200"];
             Assert.NotNull(res200.Schema);
+            var foo = res200.Schema.RootElement.GetProperty("allOf")[0];
+            Assert.Equal("object", foo.GetProperty("type").GetString());
+            var bar = res200.Schema.RootElement.GetProperty("allOf")[1];
+            Assert.Equal("object", bar.GetProperty("type").GetString());
         }
     }
 
     public class AnyOfTests
     {
-        [Fact(Skip = "AnyOf is not supported yet")]
-        public async Task ParsesSimpleAllOfAsync()
+        [Fact]
+        public async Task ParsesAnyOfAsync()
         {
             var api = """
                       {"openapi":"3.0.3","info":{"title":"Test AnyOf0","version":"0"},
-                      "paths":{"/fooOrBar":{"get":{"responses":{"200":{"$ref":"#/components/schemas/fooOrBar"}}}}},
+                      "paths":{"/fooOrBar":{"get":{"responses":{"200":{"content":{"application/json":{
+                      "schema":{"$ref":"#/components/schemas/fooOrBar"}}},"description":"response"}}}}},
                       "components":{"schemas":{"foo":{"type":"object","properties":{"name":{"type":"string"},
                       "extra":{"type":"string"}}},"bar":{"type":"string"},
                       "fooOrBar":{"anyOf":[{"$ref":"#/components/schemas/foo"},{"$ref":"#/components/schemas/bar"}]}}}}
@@ -82,6 +93,10 @@ public static class OpenApiDocumentParserV30FeatureTests
             Assert.NotEmpty(op0.Responses);
             var res200 = op0.Responses["200"];
             Assert.NotNull(res200.Schema);
+            var foo = res200.Schema.RootElement.GetProperty("anyOf")[0];
+            Assert.Equal("object", foo.GetProperty("type").GetString());
+            var bar = res200.Schema.RootElement.GetProperty("anyOf")[1];
+            Assert.Equal("string", bar.GetProperty("type").GetString());
         }
     }
 }
