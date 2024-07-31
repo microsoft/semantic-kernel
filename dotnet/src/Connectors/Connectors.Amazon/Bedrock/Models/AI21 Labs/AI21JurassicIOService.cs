99b060aebaad455688a292a32ad7ded2cd8c2d69
Charlize Yeh <charyeh@amazon.com>
2024-07-31 15:25:41 -0700

    ran dotnet format

--- dotnet/src/Connectors/Connectors.Amazon/Bedrock/Models/AI21 Labs/AI21JurassicIOService.cs
+++ dotnet/src/Connectors/Connectors.Amazon/Bedrock/Models/AI21 Labs/AI21JurassicIOService.cs
@@ -3,7 +3,6 @@
 using System.Text.Json;
 using System.Text.Json.Nodes;
 using Amazon.BedrockRuntime.Model;
-using Amazon.Runtime.Documents;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
 
