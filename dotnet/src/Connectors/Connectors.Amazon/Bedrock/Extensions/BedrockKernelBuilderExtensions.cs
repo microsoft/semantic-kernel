d0b34ee12bf2040d954f9c6e1121aee7018e548c
Charlize Yeh <charyeh@amazon.com>
2024-08-05 10:46:59 -0700

    kernel builder extensions: one method per service

--- dotnet/src/Connectors/Connectors.Amazon/Bedrock/Extensions/BedrockKernelBuilderExtensions.cs
+++ dotnet/src/Connectors/Connectors.Amazon/Bedrock/Extensions/BedrockKernelBuilderExtensions.cs
@@ -21,107 +21,68 @@ public static class BedrockKernelBuilderExtensions
     /// </summary>
     /// <param name="builder">The kernel builder.</param>
     /// <param name="modelId">The model for chat completion.</param>
-    /// <param name="bedrockApi">The IAmazonBedrockRuntime to run inference using the respective model.</param>
+    /// <param name="bedrockRuntime">The IAmazonBedrockRuntime to run inference using the respective model.</param>
+    /// <param name="serviceId">The optional service ID.</param>
     /// <returns></returns>
     public static IKernelBuilder AddBedrockChatCompletionService(
         this IKernelBuilder builder,
         string modelId,
-        IAmazonBedrockRuntime bedrockApi)
+        IAmazonBedrockRuntime? bedrockRuntime = null,
+        string? serviceId = null)
     {
-        builder.Services.AddSingleton<IChatCompletionService>(services =>
+        if (bedrockRuntime == null)
+        {
+            // Add IAmazonBedrockRuntime service client to the DI container
+            builder.Services.TryAddAWSService<IAmazonBedrockRuntime>();
+        }
+
+        builder.Services.AddKeyedSingleton<IChatCompletionService>(serviceId, (serviceProvider, _) =>
         {
             try
             {
-                var logger = services.GetService<ILoggerFactory>();
-                return new BedrockChatCompletionService(modelId, bedrockApi, logger);
+                IAmazonBedrockRuntime runtime = bedrockRuntime ?? serviceProvider.GetRequiredService<IAmazonBedrockRuntime>();
+                var logger = serviceProvider.GetService<ILoggerFactory>();
+                return new BedrockChatCompletionService(modelId, runtime, logger);
             }
             catch (Exception ex)
             {
-                throw new KernelException($"An error occurred while initializing the BedrockChatCompletionService: {ex.Message}", ex);
+                throw new KernelException($"An error occurred while initializing the {nameof(BedrockChatCompletionService)}: {ex.Message}", ex);
             }
         });
 
         return builder;
     }
 
-    /// <summary>
-    /// Add Amazon Bedrock Chat Completion service to the kernel builder using new AmazonBedrockRuntimeClient().
-    /// </summary>
-    /// <param name="builder">The kernel builder.</param>
-    /// <param name="modelId">The model for chat completion.</param>
-    /// <returns></returns>
-    public static IKernelBuilder AddBedrockChatCompletionService(
-        this IKernelBuilder builder,
-        string modelId)
-    {
-        // Add IAmazonBedrockRuntime service client to the DI container
-        builder.Services.TryAddAWSService<IAmazonBedrockRuntime>();
-        builder.Services.AddSingleton<IChatCompletionService>(services =>
-        {
-            try
-            {
-                var bedrockRuntime = services.GetRequiredService<IAmazonBedrockRuntime>();
-                var logger = services.GetService<ILoggerFactory>();
-                return new BedrockChatCompletionService(modelId, bedrockRuntime, logger);
-            }
-            catch (Exception ex)
-            {
-                throw new KernelException($"An error occurred while initializing the BedrockChatCompletionService: {ex.Message}", ex);
-            }
-        });
-
-        return builder;
-    }
     /// <summary>
     /// Add Amazon Bedrock Text Generation service to the kernel builder using IAmazonBedrockRuntime object.
     /// </summary>
     /// <param name="builder">The kernel builder.</param>
     /// <param name="modelId">The model for text generation.</param>
-    /// <param name="bedrockApi">The IAmazonBedrockRuntime to run inference using the respective model.</param>
+    /// <param name="bedrockRuntime">The IAmazonBedrockRuntime to run inference using the respective model.</param>
+    /// <param name="serviceId">The optional service ID.</param>
     /// <returns></returns>
     public static IKernelBuilder AddBedrockTextGenerationService(
         this IKernelBuilder builder,
         string modelId,
-        IAmazonBedrockRuntime bedrockApi)
+        IAmazonBedrockRuntime? bedrockRuntime = null,
+        string? serviceId = null)
     {
-        builder.Services.AddSingleton<ITextGenerationService>(services =>
+        if (bedrockRuntime == null)
+        {
+            // Add IAmazonBedrockRuntime service client to the DI container
+            builder.Services.TryAddAWSService<IAmazonBedrockRuntime>();
+        }
+        builder.Services.AddKeyedSingleton<ITextGenerationService>(serviceId, (serviceProvider, _) =>
         {
             try
             {
-                var logger = services.GetService<ILoggerFactory>();
-                return new BedrockTextGenerationService(modelId, bedrockApi, logger);
+                IAmazonBedrockRuntime runtime = bedrockRuntime ?? serviceProvider.GetRequiredService<IAmazonBedrockRuntime>();
+                var logger = serviceProvider.GetService<ILoggerFactory>();
+                return new BedrockTextGenerationService(modelId, runtime, logger);
             }
             catch (Exception ex)
             {
-                throw new KernelException($"An error occurred while initializing the BedrockTextGenerationService: {ex.Message}", ex);
-            }
-        });
-
-        return builder;
-    }
-    /// <summary>
-    /// Add Amazon Bedrock Text Generation service to the kernel builder using new AmazonBedrockRuntimeClient().
-    /// </summary>
-    /// <param name="builder">The kernel builder.</param>
-    /// <param name="modelId">The model for text generation.</param>
-    /// <returns></returns>
-    public static IKernelBuilder AddBedrockTextGenerationService(
-        this IKernelBuilder builder,
-        string modelId)
-    {
-        // Add IAmazonBedrockRuntime service client to the DI container
-        builder.Services.TryAddAWSService<IAmazonBedrockRuntime>();
-        builder.Services.AddSingleton<ITextGenerationService>(services =>
-        {
-            try
-            {
-                var logger = services.GetService<ILoggerFactory>();
-                var bedrockRuntime = services.GetRequiredService<IAmazonBedrockRuntime>();
-                return new BedrockTextGenerationService(modelId, bedrockRuntime, logger);
-            }
-            catch (Exception ex)
-            {
-                throw new KernelException($"An error occurred while initializing the BedrockTextGenerationService: {ex.Message}", ex);
+                throw new KernelException($"An error occurred while initializing the {nameof(BedrockTextGenerationService)}: {ex.Message}", ex);
             }
         });
 
