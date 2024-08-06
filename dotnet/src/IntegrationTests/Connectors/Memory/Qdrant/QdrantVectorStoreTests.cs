1e982be09d77bc6c744775d629c166c06cbde37b
westey <164392973+westey-m@users.noreply.github.com>
2024-08-06 18:50:27 +0100

    .Net: Adding VectorStore abstractions and 5 sample implementations (#7595)
    ### Motivation and Context
    
    The current MemoryStore abstractions have limitations around consuming
    existing data stores with custom schemas.
    This PR adds a new abstraction layer that supports custom schemas and
    data models, removes opinionated behaviors from connectors, and aims to
    support more of the features available in each database.
    
    This set of abstractions do not yet contain any support for Searching,
    and this will be added to the IVectorStoreRecordCollection
     interface soon.
    
    See the included ADR document for more information.
    
    ### Description
    
    - Adding IVectorStore and IVectorStoreRecordCollection interfaces, plus
    associated options classes, attributes and definition classes.
    - Adding an AzureAISearch implementation.
    - Adding a Qdrant implementation
    - Adding a Redis implementation with both JSON and Hashes support
    - Adding a Pinecone implementation
    - Adding a Volatile (In memory) implementation
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [x] The code builds clean without any errors or warnings
    - [x] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [x] All unit tests pass, and I have added new tests where possible
    - [x] I didn't break anyone :smile:
    
    ---------
    
    Co-authored-by: Maurycy Markowski <maumar@microsoft.com>
    Co-authored-by: Dmytro Struk <13853051+dmytrostruk@users.noreply.github.com>

---
+++ dotnet/src/IntegrationTests/Connectors/Memory/Qdrant/QdrantVectorStoreTests.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.SemanticKernel.Connectors.Qdrant;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SemanticKernel.IntegrationTests.Connectors.Memory.Qdrant;
+
+[Collection("QdrantVectorStoreCollection")]
+public class QdrantVectorStoreTests(ITestOutputHelper output, QdrantVectorStoreFixture fixture)
+{
+    [Fact]
+    public async Task ItCanGetAListOfExistingCollectionNamesAsync()
+    {
+        // Arrange
+        var sut = new QdrantVectorStore(fixture.QdrantClient);
+
+        // Act
+        var collectionNames = await sut.ListCollectionNamesAsync().ToListAsync();
+
+        // Assert
+        Assert.Equal(3, collectionNames.Count);
+        Assert.Contains("namedVectorsHotels", collectionNames);
+        Assert.Contains("singleVectorHotels", collectionNames);
+        Assert.Contains("singleVectorGuidIdHotels", collectionNames);
+
+        // Output
+        output.WriteLine(string.Join(",", collectionNames));
+    }
+}
