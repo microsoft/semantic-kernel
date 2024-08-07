d029bd49e8820420567c496fd60b02ad32227aae
Chris Rickman <crickman@microsoft.com>
2024-08-06 17:09:59 -0700

    Image DataUrl support

--- dotnet/src/Agents/OpenAI/Internal/AssistantMessageFactory.cs
+++ dotnet/src/Agents/OpenAI/Internal/AssistantMessageFactory.cs
@@ -52,11 +52,7 @@ internal static class AssistantMessageFactory
                 }
                 else if (string.IsNullOrWhiteSpace(imageContent.DataUri))
                 {
-                    //SDK BUG - BAD SIGNATURE (https://github.com/openai/openai-dotnet/issues/135)
-                    //    URI does not accept the format used for `DataUri`
-                    //    Approach is inefficient anyway...
-                    //yield return MessageContent.FromImageUrl(new Uri(imageContent.DataUri!));
-                    throw new KernelException($"{nameof(ImageContent.DataUri)} not supported for assistant input.");
+                    yield return MessageContent.FromImageUrl(new(imageContent.DataUri!));
                 }
             }
             else if (content is FileReferenceContent fileContent)
