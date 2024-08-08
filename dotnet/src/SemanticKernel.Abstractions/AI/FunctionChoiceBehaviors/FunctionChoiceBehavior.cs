b1cb2a9778c125fd9c8c87108a2aa0f08246a110
SergeyMenshykh <sergemenshikh@gmail.com>
2024-08-08 12:30:59 +0100

    feat(function-calling-abstraction): add MVP model for funciton calling abstraction

---
+++ dotnet/src/SemanticKernel.Abstractions/AI/FunctionChoiceBehaviors/FunctionChoiceBehavior.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+namespace Microsoft.SemanticKernel;
+
+/// <summary>
+/// Represents the base class for different function choice behaviors.
+/// </summary>
+public abstract class FunctionChoiceBehavior
+{
+    /// <summary>Returns the configuration specified by the <see cref="FunctionChoiceBehavior"/>.</summary>
+    /// <param name="context">The function choice caller context.</param>
+    /// <returns>The configuration.</returns>
+    public abstract FunctionChoiceBehaviorConfiguration GetConfiguration(FunctionChoiceBehaviorConfigurationContext context);
+}
---
+++ dotnet/src/SemanticKernel.Abstractions/AI/FunctionChoiceBehaviors/FunctionChoiceBehaviorConfiguration.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Collections.Generic;
+
+namespace Microsoft.SemanticKernel;
+
+/// <summary>
+/// Represents function choice behavior configuration produced by a <see cref="FunctionChoiceBehavior" />.
+/// </summary>
+public sealed class FunctionChoiceBehaviorConfiguration
+{
+    /// <summary>
+    /// Represents an AI model's decision-making strategy for calling functions.
+    /// </summary>
+    public FunctionChoice Choice { get; internal set; }
+
+    /// <summary>
+    /// The functions available for AI model.
+    /// </summary>
+    public IReadOnlyList<KernelFunction>? Functions { get; internal set; }
+}
