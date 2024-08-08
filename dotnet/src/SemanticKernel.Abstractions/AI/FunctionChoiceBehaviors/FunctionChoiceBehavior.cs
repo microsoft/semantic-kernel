4bc345d68866723d21f41dbdd6be0e042d0d0a79
SergeyMenshykh <sergemenshikh@gmail.com>
2024-08-08 16:55:45 +0100

    fix: address pr comments

--- dotnet/src/SemanticKernel.Abstractions/AI/FunctionChoiceBehaviors/FunctionChoiceBehavior.cs
+++ dotnet/src/SemanticKernel.Abstractions/AI/FunctionChoiceBehaviors/FunctionChoiceBehavior.cs
@@ -4,11 +4,14 @@ namespace Microsoft.SemanticKernel;
 
 /// <summary>
 /// Represents the base class for different function choice behaviors.
+/// These behaviors define the way functions are chosen by LLM and various aspects of their invocation by AI connectors.
 /// </summary>
 public abstract class FunctionChoiceBehavior
 {
-    /// <summary>Returns the configuration specified by the <see cref="FunctionChoiceBehavior"/>.</summary>
-    /// <param name="context">The function choice caller context.</param>
+    /// <summary>
+    /// Returns the configuration used by AI connectors to determine function choice and invocation behavior.
+    /// </summary>
+    /// <param name="context">The context provided by AI connectors, used to determine the configuration.</param>
     /// <returns>The configuration.</returns>
     public abstract FunctionChoiceBehaviorConfiguration GetConfiguration(FunctionChoiceBehaviorConfigurationContext context);
 }
