e8ace92e352aebe1566d93af428f8d7482cca23a
SergeyMenshykh <68852919+SergeyMenshykh@users.noreply.github.com>
2024-08-08 09:10:39 +0100

    .Net: Allow chat history mutation from auto-function invocation filters (#7952)
    ### Motivation and Context
    Today, {Azure} OpenAI connectors don't allow auto-function invocation
    filters to update/mutate chat history. The mutation can be useful to
    reduce the number of tokens sent to LLM by removing no longer needed
    function-calling messages from the chat history.
    
    Partially closes:
    https://github.com/microsoft/semantic-kernel/issues/7590. The MistralAI
    connector will be updated in a separate PR.
    
    ### Description
    This PR updates the `ClientCore` class to generate a list of messages to
    send to the LLM based on the latest chat history for every
    function-calling loop iteration, rather than separately updating the
    list of LLM messages and chat history.
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [x] The code builds clean without any errors or warnings
    - [x] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [x] All unit tests pass, and I have added new tests where possible
    - [x] I didn't break anyone :smile:

--- dotnet/samples/Demos/StepwisePlannerMigration/Services/PlanProvider.cs
+++ dotnet/samples/Demos/StepwisePlannerMigration/Services/PlanProvider.cs
@@ -2,9 +2,12 @@
 
 using System.IO;
 using System.Text.Json;
-using Microsoft.SemanticKernel.ChatCompletion;
 
 #pragma warning disable IDE0005 // Using directive is unnecessary
+
+using Microsoft.SemanticKernel.ChatCompletion;
+
+#pragma warning restore IDE0005 // Using directive is unnecessary
 
 namespace StepwisePlannerMigration.Services;
 
@@ -19,6 +22,3 @@ public class PlanProvider : IPlanProvider
         return JsonSerializer.Deserialize<ChatHistory>(plan)!;
     }
 }
-
-#pragma warning restore IDE0005 // Using directive is unnecessary
-
