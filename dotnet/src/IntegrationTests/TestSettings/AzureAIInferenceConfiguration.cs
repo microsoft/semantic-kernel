fa3edc44817fc13ee4ee22adc6844f516960f63f
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2024-08-08 11:35:52 +0100

    Preparing First ITs

---
+++ dotnet/src/IntegrationTests/TestSettings/AzureAIInferenceConfiguration.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SemanticKernel.IntegrationTests.TestSettings;
+
+[SuppressMessage("Performance", "CA1812:Internal class that is apparently never instantiated",
+    Justification = "Configuration classes are instantiated through IConfiguration.")]
+internal sealed class AzureAIInferenceConfiguration(string? serviceId, Uri endpoint, string apiKey)
+{
+    public string? ServiceId { get; set; } = serviceId;
+    public Uri Endpoint { get; set; } = endpoint;
+    public string ApiKey { get; set; } = apiKey;
+}
