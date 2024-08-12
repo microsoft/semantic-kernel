c4c0dd231245df67f7a0c64987ed26fc27be26a4
SergeyMenshykh <sergemenshikh@gmail.com>
2024-08-12 17:41:29 +0100

    fix: address Pr comments

--- dotnet/src/SemanticKernel.Abstractions/AI/FunctionChoiceBehaviors/FunctionChoiceBehavior.cs
+++ dotnet/src/SemanticKernel.Abstractions/AI/FunctionChoiceBehaviors/FunctionChoiceBehavior.cs
@@ -9,6 +9,7 @@ namespace Microsoft.SemanticKernel;
 /// Represents the base class for different function choice behaviors.
 /// These behaviors define the way functions are chosen by LLM and various aspects of their invocation by AI connectors.
 /// </summary>
+[Experimental("SKEXP0001")]
 public abstract class FunctionChoiceBehavior
 {
     /// <summary>
