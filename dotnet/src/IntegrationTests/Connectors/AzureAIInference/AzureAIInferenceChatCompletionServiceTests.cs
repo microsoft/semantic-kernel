8bf3a0332be13b03d0a0f9ff3a385018d10962c6
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2024-08-12 18:19:22 +0100

    .Net: Azure AI Inference Connector - Chat Completion Phase 1 (#7976)
    ### Motivation and Context
    
    - Resolve Partially #7958
    - [x] Update current Mass Connector patterns following latest OpenAI V2.
       - [x] Implement ChatCompletion for MaaS Connector.

---
+++ dotnet/src/IntegrationTests/Connectors/AzureAIInference/AzureAIInferenceChatCompletionServiceTests.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.AzureAIInference;
+using SemanticKernel.IntegrationTests.TestSettings;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SemanticKernel.IntegrationTests.Connectors.AzureAIInference;
+
+#pragma warning disable xUnit1004 // Contains test methods used in manual verification. Disable warning for this file only.
+
+public sealed class AzureAIInferenceChatCompletionServiceTests(ITestOutputHelper output) : IDisposable
+{
+    private readonly XunitLogger<Kernel> _loggerFactory = new(output);
+    private readonly RedirectOutput _testOutputHelper = new(output);
+    private readonly IConfigurationRoot _configuration = new ConfigurationBuilder()
+        .AddJsonFile(path: "testsettings.json", optional: true, reloadOnChange: true)
+        .AddJsonFile(path: "testsettings.development.json", optional: true, reloadOnChange: true)
+        .AddEnvironmentVariables()
+        .AddUserSecrets<AzureAIInferenceChatCompletionServiceTests>()
+        .Build();
+
+    [Theory]
+    [InlineData("Where is the most famous fish market in Seattle, Washington, USA?", "Pike Place")]
+    public async Task InvokeGetChatMessageContentsAsync(string prompt, string expectedAnswerContains)
+    {
+        // Arrange
+        var config = this._configuration.GetSection("AzureAIInference").Get<AzureAIInferenceConfiguration>();
+        Assert.NotNull(config);
+
+        var sut = new AzureAIInferenceChatCompletionService(
+            endpoint: config.Endpoint,
+            apiKey: config.ApiKey,
+            loggerFactory: this._loggerFactory);
+
+        ChatHistory chatHistory = [
+            new ChatMessageContent(AuthorRole.User, prompt)
+        ];
+
+        // Act
+        var result = await sut.GetChatMessageContentsAsync(chatHistory);
+
+        // Assert
+        Assert.Single(result);
+        Assert.Contains(expectedAnswerContains, result[0].Content, StringComparison.OrdinalIgnoreCase);
+    }
+
+    public void Dispose()
+    {
+        this._loggerFactory.Dispose();
+        this._testOutputHelper.Dispose();
+    }
+}
