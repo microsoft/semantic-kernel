66881505291bfd32344caab750c45b828df26069
Dmytro Struk <13853051+dmytrostruk@users.noreply.github.com>
2024-08-13 07:14:48 -0700

    Update dotnet/src/Connectors/Connectors.Memory.AzureCosmosDBNoSQL/AzureCosmosDBNoSQLServiceCollectionExtensions.cs
    Co-authored-by: westey <164392973+westey-m@users.noreply.github.com>

--- dotnet/src/Connectors/Connectors.Memory.AzureCosmosDBNoSQL/AzureCosmosDBNoSQLServiceCollectionExtensions.cs
+++ dotnet/src/Connectors/Connectors.Memory.AzureCosmosDBNoSQL/AzureCosmosDBNoSQLServiceCollectionExtensions.cs
@@ -13,7 +13,7 @@ namespace Microsoft.SemanticKernel.Connectors.AzureCosmosDBNoSQL;
 public static class AzureCosmosDBNoSQLServiceCollectionExtensions
 {
     /// <summary>
-    /// Register a Azure CosmosDB NoSQL <see cref="IVectorStore"/> with the specified service ID
+    /// Register an Azure CosmosDB NoSQL <see cref="IVectorStore"/> with the specified service ID
     /// and where the Azure CosmosDB NoSQL <see cref="Database"/> is retrieved from the dependency injection container.
     /// </summary>
     /// <param name="services">The <see cref="IServiceCollection"/> to register the <see cref="IVectorStore"/> on.</param>
