5ade57e81bc6d413ff9f2f6ef4569f5e614dafe6
Charlize Yeh <charyeh@amazon.com>
2024-08-13 14:48:31 -0700

    fix: dotnet format removed unused imports

--- dotnet/src/Connectors/Connectors.Amazon/Bedrock/Settings/AmazonJurassicExecutionSettings.cs
+++ dotnet/src/Connectors/Connectors.Amazon/Bedrock/Settings/AmazonJurassicExecutionSettings.cs
@@ -2,7 +2,6 @@
 
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using Microsoft.SemanticKernel.Connectors.Amazon.Core;
 using Microsoft.SemanticKernel.Text;
 
 namespace Microsoft.SemanticKernel.Connectors.Amazon;
