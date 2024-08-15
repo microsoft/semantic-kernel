e15e1a318323ff6c6181af4171ad10f1aa1f0d4b
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2024-08-15 13:31:45 +0100

    Extensions UT added

---
+++ dotnet/src/Connectors/Connectors.AzureAIInference/Extensions/AzureAIInferenceKernelBuilderExtensions.cs
@@ -0,0 +1,75 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Net.Http;
+using Azure.AI.Inference;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.AzureAIInference;
+using Microsoft.SemanticKernel.Http;
+
+namespace Microsoft.SemanticKernel;
+
+/// <summary>
+/// Sponsor extensions class for <see cref="IKernelBuilder"/>.
+/// </summary>
+public static class AzureAIInferenceKernelBuilderExtensions
+{
+    /// <summary>
+    /// Adds the Azure AI Inference chat completion service to the list.
+    /// </summary>
+    /// <param name="builder">The <see cref="IKernelBuilder"/> instance to augment.</param>
+    /// <param name="modelId">Target Model Id for endpoints supporting more than one model</param>
+    /// <param name="apiKey">API Key</param>
+    /// <param name="endpoint">Endpoint / Target URI</param>
+    /// <param name="serviceId">A local identifier for the given AI service</param>
+    /// <param name="httpClient">Custom <see cref="HttpClient"/> for HTTP requests.</param>
+    /// <returns>The same instance as <paramref name="builder"/>.</returns>
+    public static IKernelBuilder AddAzureAIInferenceChatCompletion(
+        this IKernelBuilder builder,
+        string? modelId = null,
+        string? apiKey = null,
+        Uri? endpoint = null,
+        string? serviceId = null,
+        HttpClient? httpClient = null)
+    {
+        Verify.NotNull(builder);
+
+        AzureAIInferenceChatCompletionService Factory(IServiceProvider serviceProvider, object? _) =>
+            new(modelId,
+                apiKey,
+                endpoint,
+                HttpClientProvider.GetHttpClient(httpClient, serviceProvider),
+                serviceProvider.GetService<ILoggerFactory>());
+
+        builder.Services.AddKeyedSingleton<IChatCompletionService>(serviceId, (Func<IServiceProvider, object?, AzureAIInferenceChatCompletionService>)Factory);
+
+        return builder;
+    }
+
+    /// <summary>
+    /// Adds the Azure AI Inference chat completion service to the list.
+    /// </summary>
+    /// <param name="builder">The <see cref="IKernelBuilder"/> instance to augment.</param>
+    /// <param name="modelId">Azure AI Inference model id</param>
+    /// <param name="chatClient"><see cref="ChatCompletionsClient"/> to use for the service. If null, one must be available in the service provider when this service is resolved.</param>
+    /// <param name="serviceId">A local identifier for the given AI service</param>
+    /// <returns>The same instance as <paramref name="builder"/>.</returns>
+    public static IKernelBuilder AddAzureAIInferenceChatCompletion(
+        this IKernelBuilder builder,
+        string modelId,
+        ChatCompletionsClient? chatClient = null,
+        string? serviceId = null)
+    {
+        Verify.NotNull(builder);
+        Verify.NotNullOrWhiteSpace(modelId);
+
+        AzureAIInferenceChatCompletionService Factory(IServiceProvider serviceProvider, object? _) =>
+            new(modelId, chatClient ?? serviceProvider.GetRequiredService<ChatCompletionsClient>(), serviceProvider.GetService<ILoggerFactory>());
+
+        builder.Services.AddKeyedSingleton<IChatCompletionService>(serviceId, (Func<IServiceProvider, object?, AzureAIInferenceChatCompletionService>)Factory);
+
+        return builder;
+    }
+}
---
+++ dotnet/src/Connectors/Connectors.AzureAIInference/Extensions/AzureAIInferenceServiceCollectionExtensions.cs
@@ -0,0 +1,70 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using Azure.AI.Inference;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.AzureAIInference;
+using Microsoft.SemanticKernel.Http;
+
+namespace Microsoft.SemanticKernel;
+
+/// <summary>
+/// Sponsor extensions class for <see cref="IKernelBuilder"/>.
+/// </summary>
+public static class AzureAIInferenceServiceCollectionExtensions
+{
+    /// <summary>
+    /// Adds the Azure AI Inference chat completion service to the list.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> instance to augment.</param>
+    /// <param name="modelId">Target Model Id for endpoints supporting more than one model</param>
+    /// <param name="apiKey">API Key</param>
+    /// <param name="endpoint">Endpoint / Target URI</param>
+    /// <param name="serviceId">A local identifier for the given AI service</param>
+    /// <returns>The same instance as <paramref name="services"/>.</returns>
+    public static IServiceCollection AddAzureAIInferenceChatCompletion(
+        this IServiceCollection services,
+        string? modelId = null,
+        string? apiKey = null,
+        Uri? endpoint = null,
+        string? serviceId = null)
+    {
+        Verify.NotNull(services);
+
+        AzureAIInferenceChatCompletionService Factory(IServiceProvider serviceProvider, object? _) =>
+            new(modelId,
+                apiKey,
+                endpoint,
+                HttpClientProvider.GetHttpClient(serviceProvider),
+                serviceProvider.GetService<ILoggerFactory>());
+
+        services.AddKeyedSingleton<IChatCompletionService>(serviceId, (Func<IServiceProvider, object?, AzureAIInferenceChatCompletionService>)Factory);
+
+        return services;
+    }
+
+    /// <summary>
+    /// Adds the Azure AI Inference chat completion service to the list.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> instance to augment.</param>
+    /// <param name="modelId">Azure AI Inference model id</param>
+    /// <param name="chatClient"><see cref="ChatCompletionsClient"/> to use for the service. If null, one must be available in the service provider when this service is resolved.</param>
+    /// <param name="serviceId">A local identifier for the given AI service</param>
+    /// <returns>The same instance as <paramref name="services"/>.</returns>
+    public static IServiceCollection AddAzureAIInferenceChatCompletion(this IServiceCollection services,
+        string modelId,
+        ChatCompletionsClient? chatClient = null,
+        string? serviceId = null)
+    {
+        Verify.NotNull(services);
+
+        AzureAIInferenceChatCompletionService Factory(IServiceProvider serviceProvider, object? _) =>
+            new(modelId, chatClient ?? serviceProvider.GetRequiredService<ChatCompletionsClient>(), serviceProvider.GetService<ILoggerFactory>());
+
+        services.AddKeyedSingleton<IChatCompletionService>(serviceId, (Func<IServiceProvider, object?, AzureAIInferenceChatCompletionService>)Factory);
+
+        return services;
+    }
+}
