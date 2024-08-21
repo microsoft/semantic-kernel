18f5c1b0632e29e3f5d6e472e1d95c36b63ef26f
markwallace-microsoft <127216156+markwallace-microsoft@users.noreply.github.com>
2024-08-21 11:43:09 +0100

    Add public API to allow default header to be changed

--- dotnet/src/SemanticKernel.Abstractions/Http/HttpHeaderConstant.cs
+++ dotnet/src/SemanticKernel.Abstractions/Http/HttpHeaderConstant.cs
@@ -1,23 +1,33 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
 using System;
-using System.Diagnostics.CodeAnalysis;
 
 namespace Microsoft.SemanticKernel.Http;
 
-/// <summary>Provides HTTP header names and values for common purposes.</summary>
-[ExcludeFromCodeCoverage]
-internal static class HttpHeaderConstant
+/// <summary>
+/// Provides HTTP header names and values for common purposes.
+/// </summary>
+public static class HttpHeaderConstant
 {
+    /// <summary>
+    /// Provides HTTP header names for common purposes.
+    /// </summary>
     public static class Names
     {
-        /// <summary>HTTP header name to use to include the Semantic Kernel package version in all HTTP requests issued by Semantic Kernel.</summary>
+        /// <summary>
+        /// HTTP header name to use to include the Semantic Kernel package version in all HTTP requests issued by Semantic Kernel.
+        /// </summary>
         public static string SemanticKernelVersion => "Semantic-Kernel-Version";
     }
 
+    /// <summary>
+    /// Provides HTTP header values for common purposes.
+    /// </summary>
     public static class Values
     {
-        /// <summary>User agent string to use for all HTTP requests issued by Semantic Kernel.</summary>
+        /// <summary>
+        /// User agent string to use for all HTTP requests issued by Semantic Kernel.
+        /// </summary>
         public static string UserAgent => s_userAgent;
 
         /// <summary>
---
+++ dotnet/src/SemanticKernel.UnitTests/Http/HttpHeaderConstantTests.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.SemanticKernel.Http;
+using Xunit;
+
+namespace SemanticKernel.UnitTests.Http;
+public class HttpHeaderConstantTests
+{
+    [Fact]
+    public void ItVerifiesStandardUserAgent()
+    {
+        // Arrange & Act & Assert
+        Assert.Equal("Semantic-Kernel", HttpHeaderConstant.Values.UserAgent);
+    }
+
+    [Fact]
+    public void ItVerifiesUserAgentSupportsPrefix()
+    {
+        // Arrange & Act & Assert
+        HttpHeaderConstant.Values.SetUserAgentPrefix("Test");
+        Assert.Equal("Test-Semantic-Kernel", HttpHeaderConstant.Values.UserAgent);
+        HttpHeaderConstant.Values.SetUserAgentPrefix(null);
+        Assert.Equal("Semantic-Kernel", HttpHeaderConstant.Values.UserAgent);
+    }
+}
