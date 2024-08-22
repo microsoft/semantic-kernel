d7287bbf50dbbe6ae5667d9242f5fe3d05efc263
Mark Wallace <127216156+markwallace-microsoft@users.noreply.github.com>
2024-08-22 21:01:27 +0100

    .Net: ADR for Text Search Abstractions (#8307)
    ### Motivation and Context
    
    Closes #5799 
    
    ### Description
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [ ] The code builds clean without any errors or warnings
    - [ ] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [ ] All unit tests pass, and I have added new tests where possible
    - [ ] I didn't break anyone :smile:

---
+++ dotnet/src/SemanticKernel.Abstractions/Search/BasicFilterOptions.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Microsoft.SemanticKernel.Search;
+
+/// <summary>
+/// Used to provide basic filtering when using <see cref="ITextSearch"/>.
+/// </summary>
+/// <remarks>
+/// A basic filter has a collection of <see cref="FilterClause"/>s that can be used by
+/// an <see cref="ITextSearch"/> implementation to request that the underlying search
+/// service filter the search results.
+/// </remarks>
+[Experimental("SKEXP0001")]
+public sealed class BasicFilterOptions
+{
+    /// <summary>
+    /// The clauses to apply to the <see cref="BasicFilterOptions" />
+    /// </summary>
+    public IEnumerable<FilterClause> FilterClauses => this._filterClauses;
+
+    /// <summary>
+    /// Add a equality clause to the filter options.
+    /// </summary>
+    /// <param name="fieldName">Name of the field.</param>
+    /// <param name="value">Value of the field.</param>
+    /// <returns>FilterOptions instance to allow fluent configuration.</returns>
+    public BasicFilterOptions Equality(string fieldName, object value)
+    {
+        this._filterClauses.Add(new EqualityFilterClause(fieldName, value));
+        return this;
+    }
+
+    #region private
+    private readonly List<FilterClause> _filterClauses = [];
+    #endregion
+}
---
+++ dotnet/src/SemanticKernel.Abstractions/Search/TextSearchOptions.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+using System.Diagnostics.CodeAnalysis;
+
+namespace Microsoft.SemanticKernel.Search;
+
+/// <summary>
+/// Options which can be applied when using <see cref="ITextSearch"/>.
+/// </summary>
+[Experimental("SKEXP0001")]
+public sealed class TextSearchOptions
+{
+    /// <summary>
+    /// Default number of search results to return.
+    /// </summary>
+    public static readonly int DefaultCount = 5;
+
+    /// <summary>
+    /// Flag indicating the total count should be included in the results.
+    /// </summary>
+    /// <remarks>
+    /// Default value is false.
+    /// Not all text search implementations will support this option.
+    /// </remarks>
+    public bool IncludeTotalCount { get; set; } = false;
+
+    /// <summary>
+    /// The basic filter expression to apply to the search query.
+    /// </summary>
+    public BasicFilterOptions? BasicFilter { get; set; }
+
+    /// <summary>
+    /// Number of search results to return.
+    /// </summary>
+    public int Count { get; set; } = DefaultCount;
+
+    /// <summary>
+    /// The index of the first result to return.
+    /// </summary>
+    public int Offset { get; set; } = 0;
+}
