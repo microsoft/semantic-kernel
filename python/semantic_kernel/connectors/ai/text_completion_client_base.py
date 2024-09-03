35d58d152e6d6062205237c1820fe72e6e288779
Tao Chen <taochen@microsoft.com>
2024-09-03 15:56:04 -0700

    OpenAI text completion

--- python/semantic_kernel/connectors/ai/text_completion_client_base.py
+++ python/semantic_kernel/connectors/ai/text_completion_client_base.py
@@ -1,5 +1,6 @@
 # Copyright (c) Microsoft. All rights reserved.
 
+import copy
 from abc import ABC, abstractmethod
 from collections.abc import AsyncGenerator
 from typing import TYPE_CHECKING, Any
@@ -14,7 +15,46 @@ if TYPE_CHECKING:
 class TextCompletionClientBase(AIServiceClientBase, ABC):
     """Base class for text completion AI services."""
 
+    # region Abstract methods
+
     @abstractmethod
+    async def _send_text_request(
+        self,
+        prompt: str,
+        settings: "PromptExecutionSettings",
+    ) -> list["TextContent"]:
+        """Send a text completion request to the AI service.
+
+        Args:
+            prompt (str): The prompt to send to the LLM.
+            settings (PromptExecutionSettings): Settings for the request.
+
+        Returns:
+            list[TextContent]: A string or list of strings representing the response(s) from the LLM.
+        """
+        ...
+
+    @abstractmethod
+    async def _send_streaming_text_request(
+        self,
+        prompt: str,
+        settings: "PromptExecutionSettings",
+    ) -> AsyncGenerator[list["StreamingTextContent"], Any]:
+        """Send a streaming text request to the AI service.
+
+        Args:
+            prompt (str): The prompt to send to the LLM.
+            settings (PromptExecutionSettings): Settings for the request.
+
+        Yields:
+            list[StreamingTextContent]: A stream representing the response(s) from the LLM.
+        """
+        # Below is needed for mypy: https://mypy.readthedocs.io/en/stable/more_types.html#asynchronous-iterators
+        if False:
+            yield
+
+    # endregion
+
     async def get_text_contents(
         self,
         prompt: str,
@@ -29,6 +69,10 @@ class TextCompletionClientBase(AIServiceClientBase, ABC):
         Returns:
             list[TextContent]: A string or list of strings representing the response(s) from the LLM.
         """
+        # Create a copy of the settings to avoid modifying the original settings
+        settings = copy.deepcopy(settings)
+
+        return await self._send_text_request(prompt, settings)
 
     async def get_text_content(self, prompt: str, settings: "PromptExecutionSettings") -> "TextContent | None":
         """This is the method that is called from the kernel to get a response from a text-optimized LLM.
@@ -46,8 +90,7 @@ class TextCompletionClientBase(AIServiceClientBase, ABC):
         # this should not happen, should error out before returning an empty list
         return None  # pragma: no cover
 
-    @abstractmethod
-    def get_streaming_text_contents(
+    async def get_streaming_text_contents(
         self,
         prompt: str,
         settings: "PromptExecutionSettings",
@@ -61,7 +104,11 @@ class TextCompletionClientBase(AIServiceClientBase, ABC):
         Yields:
             list[StreamingTextContent]: A stream representing the response(s) from the LLM.
         """
-        ...
+        # Create a copy of the settings to avoid modifying the original settings
+        settings = copy.deepcopy(settings)
+
+        async for contents in self._send_streaming_text_request(prompt, settings):
+            yield contents
 
     async def get_streaming_text_content(
         self, prompt: str, settings: "PromptExecutionSettings"
