a781f325534412c6d29f39b184247ecdef6351d0
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2024-09-04 16:36:37 -0400

    Merge

---
+++ dotnet/src/Connectors/Connectors.AssemblyAI/AssemblyAIKernelBuilderExtensions.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Net.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.SemanticKernel.AudioToText;
+using Microsoft.SemanticKernel.Connectors.AssemblyAI;
+
+namespace Microsoft.SemanticKernel;
+
+/// <summary>
+/// Provides extension methods for <see cref="IKernelBuilder"/> and related classes to configure AssemblyAI connectors.
+/// </summary>
+public static class AssemblyAIKernelBuilderExtensions
+{
+    /// <summary>
+    /// Adds the AssemblyAI audio-to-text service to the kernel.
+    /// </summary>
+    /// <param name="builder">The <see cref="IKernelBuilder"/> instance to augment.</param>
+    /// <param name="apiKey">AssemblyAI API key, <a href="https://www.assemblyai.com/dashboard">get your API key from the dashboard.</a></param>
+    /// <param name="endpoint">The endpoint URL to the AssemblyAI API.</param>
+    /// <param name="serviceId">A local identifier for the given AI service.</param>
+    /// <param name="httpClient">The HttpClient to use with this service.</param>
+    /// <returns>The same instance as <paramref name="builder"/>.</returns>
+    public static IKernelBuilder AddAssemblyAIAudioToText(
+        this IKernelBuilder builder,
+        string apiKey,
+        Uri? endpoint = null,
+        string? serviceId = null,
+        HttpClient? httpClient = null
+    )
+    {
+        Verify.NotNull(builder);
+
+        builder.Services.AddKeyedSingleton<IAudioToTextService>(serviceId, (serviceProvider, _)
+            => new AssemblyAIAudioToTextService(
+                apiKey,
+                endpoint,
+                httpClient));
+
+        return builder;
+    }
+}
--- dotnet/src/SemanticKernel.Abstractions/AI/AudioToText/IAudioToTextService.cs
+++ dotnet/src/SemanticKernel.Abstractions/AI/AudioToText/IAudioToTextService.cs
@@ -27,4 +27,18 @@ public interface IAudioToTextService : IAIService
         PromptExecutionSettings? executionSettings = null,
         Kernel? kernel = null,
         CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Get text contents from audio content.
+    /// </summary>
+    /// <param name="content">Audio stream content.</param>
+    /// <param name="executionSettings">The AI execution settings (optional).</param>
+    /// <param name="kernel">The <see cref="Kernel"/> containing services, plugins, and other state for use throughout the operation.</param>
+    /// <param name="cancellationToken">The <see cref="CancellationToken"/> to monitor for cancellation requests. The default is <see cref="CancellationToken.None"/>.</param>
+    /// <returns>Text contents from audio content.</returns>
+    Task<IReadOnlyList<TextContent>> GetTextContentsAsync(
+        AudioStreamContent content,
+        PromptExecutionSettings? executionSettings = null,
+        Kernel? kernel = null,
+        CancellationToken cancellationToken = default);
 }
