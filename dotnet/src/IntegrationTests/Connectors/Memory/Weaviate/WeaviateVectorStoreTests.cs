2200e00c403cb37dd08183439f0404472cd81170
Dmytro Struk <13853051+dmytrostruk@users.noreply.github.com>
2024-09-05 12:54:16 -0700

    .Net: Added implementation of Weaviate connector for new memory design (#8403)
    ### Motivation and Context
    
    <!-- Thank you for your contribution to the semantic-kernel repo!
    Please help reviewers and future users, providing the following
    information:
      1. Why is this change required?
      2. What problem does it solve?
      3. What scenario does it contribute to?
      4. If it fixes an open issue, please link to the issue here.
    -->
    
    Related: https://github.com/microsoft/semantic-kernel/issues/6527
    
    In this PR:
    - Implemented `IVectorStore`
    - Implemented `IVectorStoreRecordCollection<TKey, TRecord>`
    - Weaviate default record mapper
    - `Options` classes
    - Extension methods for DI
    - Integration tests  
    - Unit tests
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [x] The code builds clean without any errors or warnings
    - [x] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [x] All unit tests pass, and I have added new tests where possible
    - [x] I didn't break anyone :smile:
    
    ---------
    
    Co-authored-by: Mark Wallace <127216156+markwallace-microsoft@users.noreply.github.com>

---
+++ dotnet/src/IntegrationTests/Connectors/Memory/Weaviate/WeaviateVectorStoreTests.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.SemanticKernel.Connectors.Weaviate;
+using Xunit;
+
+namespace SemanticKernel.IntegrationTests.Connectors.Memory.Weaviate;
+
+[Collection("WeaviateVectorStoreCollection")]
+public sealed class WeaviateVectorStoreTests(WeaviateVectorStoreFixture fixture)
+{
+    [Fact]
+    public async Task ItCanGetAListOfExistingCollectionNamesAsync()
+    {
+        // Arrange
+        var collection1 = new WeaviateVectorStoreRecordCollection<WeaviateHotel>(fixture.HttpClient!, "Collection1");
+        var collection2 = new WeaviateVectorStoreRecordCollection<WeaviateHotel>(fixture.HttpClient!, "Collection2");
+        var collection3 = new WeaviateVectorStoreRecordCollection<WeaviateHotel>(fixture.HttpClient!, "Collection3");
+
+        await collection1.CreateCollectionAsync();
+        await collection2.CreateCollectionAsync();
+        await collection3.CreateCollectionAsync();
+
+        var sut = new WeaviateVectorStore(fixture.HttpClient!);
+
+        // Act
+        var collectionNames = await sut.ListCollectionNamesAsync().ToListAsync();
+
+        // Assert
+        Assert.Contains("Collection1", collectionNames);
+        Assert.Contains("Collection2", collectionNames);
+        Assert.Contains("Collection3", collectionNames);
+    }
+}
