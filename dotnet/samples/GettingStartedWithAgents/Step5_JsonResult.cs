cc277b6302e55bbfadf41cf438f20dd7752027aa
Bryan <74067792+Bryan-Roe@users.noreply.github.com>
2024-09-08 16:39:20 -0700

    Fix project file conflicts and stashed changes.

--- dotnet/samples/GettingStartedWithAgents/Step5_JsonResult.cs
+++ dotnet/samples/GettingStartedWithAgents/Step5_JsonResult.cs
@@ -1,6 +1,14 @@
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 // Copyright (c) Microsoft. All rights reserved.
+=======
+﻿// Copyright (c) Microsoft. All rights reserved.
+>>>>>>> Stashed changes
+=======
+﻿// Copyright (c) Microsoft. All rights reserved.
+>>>>>>> Stashed changes
 =======
 ﻿// Copyright (c) Microsoft. All rights reserved.
 >>>>>>> Stashed changes
