acefead439153adc17c2be6b4a7e0be36b1624d5
SergeyMenshykh <sergemenshikh@gmail.com>
2024-09-13 09:28:20 +0100

    fix intermitently faileng unit tests

--- dotnet/src/IntegrationTests/Connectors/AzureOpenAI/AzureOpenAIChatCompletion_FunctionCallingTests.cs
+++ dotnet/src/IntegrationTests/Connectors/AzureOpenAI/AzureOpenAIChatCompletion_FunctionCallingTests.cs
@@ -795,7 +795,7 @@ public sealed class AzureOpenAIChatCompletionFunctionCallingTests : BaseIntegrat
 
         // Assert
         Assert.Equal("abc@domain.com", emailRecipient);
-        Assert.Contains("61\u00B0F", emailBody);
+        Assert.Contains("61", emailBody);
     }
 
     [Fact]
