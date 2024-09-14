22611cf8cbbd9d182ba08e180a97db2eb64594b7
Bryan Roe <74067792+Bryan-Roe@users.noreply.github.com>
2024-09-14 09:56:26 -0700

    Merge branch 'main' of git@github.com:Bryan-Roe/semantic-kernel.git

--- python/semantic_kernel/memory/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) Microsoft. All rights reserved.
-from semantic_kernel.memory.semantic_text_memory import SemanticTextMemory
-from semantic_kernel.memory.volatile_memory_store import VolatileMemoryStore
-
-__all__ = ["SemanticTextMemory", "VolatileMemoryStore"]
-from semantic_kernel.memory.volatile_memory_store import VolatileMemoryStore
-
-__all__ = ["VolatileMemoryStore"]
