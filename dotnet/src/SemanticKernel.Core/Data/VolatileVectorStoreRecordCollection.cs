dee753e672b689a9370c7ee9bfaf429b431d37fd
westey <164392973+westey-m@users.noreply.github.com>
2024-09-18 14:21:09 +0100

    Remove unused using clause.

--- dotnet/src/SemanticKernel.Core/Data/VolatileVectorStoreRecordCollection.cs
+++ dotnet/src/SemanticKernel.Core/Data/VolatileVectorStoreRecordCollection.cs
@@ -5,7 +5,6 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
