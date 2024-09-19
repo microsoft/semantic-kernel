e5ad2449c268a6c1ccccce57436480d40f5c748c
BR <Bryan-Roe@users.noreply.huggingface.co>
2024-09-19 13:28:17 -0700

    Add parameterized template instruction for ChatCompletionAgent.

---
+++ dotnet/samples/Concepts/Agents/ChatCompletion_Templating.cs
@@ -0,0 +1,132 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Agents;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.PromptTemplates.Handlebars;
+using Microsoft.SemanticKernel.PromptTemplates.Liquid;
+
+namespace Agents;
+
+/// <summary>
+/// Demonstrate parameterized template instruction for <see cref="ChatCompletionAgent"/>.
+/// </summary>
+public class ChatCompletion_Templating(ITestOutputHelper output) : BaseAgentsTest(output)
+{
+    private readonly static (string Input, string? Style)[] s_inputs =
+        [
+            (Input: "Home cooking is the great.", Style: null),
+            (Input: "Talk about world peace.", Style: "iambic pentameter"),
+            (Input: "Say something about doing your best.", Style: "e. e. cummings"),
+            (Input: "What do you think about having fun?", Style: "old school rap")
+        ];
+
+    [Fact]
+    public async Task InvokeAgentWithInstructionsTemplateAsync()
+    {
+        // Instruction based template always proceseed by KernelPromptTemplateFactory
+        ChatCompletionAgent agent =
+            new()
+            {
+                Kernel = this.CreateKernelWithChatCompletion(),
+                Instructions =
+                    """
+                    Write a one verse poem on the requested topic in the style of {{$style}}.
+                    Always state the requested style of the poem.
+                    """,
+                Arguments = new KernelArguments()
+                {
+                    {"style", "haiku"}
+                }
+            };
+
+        await InvokeChatCompletionAgentWithTemplateAsync(agent);
+    }
+
+    [Fact]
+    public async Task InvokeAgentWithKernelTemplateAsync()
+    {
+        // Default factory is KernelPromptTemplateFactory
+        await InvokeChatCompletionAgentWithTemplateAsync(
+            """
+            Write a one verse poem on the requested topic in the style of {{$style}}.
+            Always state the requested style of the poem.
+            """);
+    }
+
+    [Fact]
+    public async Task InvokeAgentWithHandlebarsTemplateAsync()
+    {
+        await InvokeChatCompletionAgentWithTemplateAsync(
+            """
+            Write a one verse poem on the requested topic in the style of {{style}}.
+            Always state the requested style of the poem.
+            """,
+            HandlebarsPromptTemplateFactory.HandlebarsTemplateFormat,
+            new HandlebarsPromptTemplateFactory());
+    }
+
+    [Fact]
+    public async Task InvokeAgentWithLiquidTemplateAsync()
+    {
+        await InvokeChatCompletionAgentWithTemplateAsync(
+            """
+            Write a one verse poem on the requested topic in the style of {{style}}.
+            Always state the requested style of the poem.
+            """,
+            LiquidPromptTemplateFactory.LiquidTemplateFormat,
+            new LiquidPromptTemplateFactory());
+    }
+
+    private async Task InvokeChatCompletionAgentWithTemplateAsync(
+        string instructionTemplate,
+        string? templateFormat = null,
+        IPromptTemplateFactory? templateFactory = null)
+    {
+        // Define the agent
+        PromptTemplateConfig templateConfig =
+            new()
+            {
+                Template = instructionTemplate,
+                TemplateFormat = templateFormat,
+            };
+        ChatCompletionAgent agent =
+            new(templateConfig, templateFactory)
+            {
+                Kernel = this.CreateKernelWithChatCompletion(),
+                Arguments = new KernelArguments()
+                {
+                    {"style", "haiku"}
+                }
+            };
+
+        await InvokeChatCompletionAgentWithTemplateAsync(agent);
+    }
+
+    private async Task InvokeChatCompletionAgentWithTemplateAsync(ChatCompletionAgent agent)
+    {
+        ChatHistory chat = [];
+
+        foreach ((string input, string? style) in s_inputs)
+        {
+            // Add input to chat
+            ChatMessageContent request = new(AuthorRole.User, input);
+            chat.Add(request);
+            this.WriteAgentChatMessage(request);
+
+            KernelArguments? arguments = null;
+
+            if (!string.IsNullOrWhiteSpace(style))
+            {
+                // Override style template parameter
+                arguments = new() { { "style", style } };
+            }
+
+            // Process agent response
+            await foreach (ChatMessageContent message in agent.InvokeAsync(chat, arguments))
+            {
+                chat.Add(message);
+                this.WriteAgentChatMessage(message);
+            }
+        }
+    }
+}
---
+++ dotnet/samples/Concepts/Agents/OpenAIAssistant_Templating.cs
@@ -0,0 +1,142 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Agents.OpenAI;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.PromptTemplates.Handlebars;
+using Microsoft.SemanticKernel.PromptTemplates.Liquid;
+
+namespace Agents;
+
+/// <summary>
+/// Demonstrate parameterized template instruction  for <see cref="OpenAIAssistantAgent"/>.
+/// </summary>
+public class OpenAIAssistant_Templating(ITestOutputHelper output) : BaseAgentsTest(output)
+{
+    private readonly static (string Input, string? Style)[] s_inputs =
+        [
+            (Input: "Home cooking is the great.", Style: null),
+            (Input: "Talk about world peace.", Style: "iambic pentameter"),
+            (Input: "Say something about doing your best.", Style: "e. e. cummings"),
+            (Input: "What do you think about having fun?", Style: "old school rap")
+        ];
+
+    [Fact]
+    public async Task InvokeAgentWithInstructionsAsync()
+    {
+        // Instruction based template always proceseed by KernelPromptTemplateFactory
+        OpenAIAssistantAgent agent = await OpenAIAssistantAgent.CreateAsync(
+                clientProvider: this.GetClientProvider(),
+                definition: new OpenAIAssistantDefinition(this.Model)
+                {
+                    Instructions =
+                        """
+                        Write a one verse poem on the requested topic in the styles of {{$style}}.
+                        Always state the requested style of the poem.
+                        """,
+                    Metadata = AssistantSampleMetadata
+                },
+                kernel: new Kernel(),
+                defaultArguments: new KernelArguments()
+                {
+                    {"style", "haiku"}
+                });
+
+        await InvokeAssistantAgentWithTemplateAsync(agent);
+    }
+
+    [Fact]
+    public async Task InvokeAgentWithKernelTemplateAsync()
+    {
+        // Default factory is KernelPromptTemplateFactory
+        await InvokeAssistantAgentWithTemplateAsync(
+            """
+            Write a one verse poem on the requested topic in the styles of {{$style}}.
+            Always state the requested style of the poem.
+            """);
+    }
+
+    [Fact]
+    public async Task InvokeAgentWithHandlebarsTemplateAsync()
+    {
+        await InvokeAssistantAgentWithTemplateAsync(
+            """
+            Write a one verse poem on the requested topic in the styles of {{style}}.
+            Always state the requested style of the poem.
+            """,
+            HandlebarsPromptTemplateFactory.HandlebarsTemplateFormat,
+            new HandlebarsPromptTemplateFactory());
+    }
+
+    [Fact]
+    public async Task InvokeAgentWithLiquidTemplateAsync()
+    {
+        await InvokeAssistantAgentWithTemplateAsync(
+            """
+            Write a one verse poem on the requested topic in the styles of {{style}}.
+            Always state the requested style of the poem.
+            """,
+            LiquidPromptTemplateFactory.LiquidTemplateFormat,
+            new LiquidPromptTemplateFactory());
+    }
+
+    private async Task InvokeAssistantAgentWithTemplateAsync(
+        string instructionTemplate,
+        string? templateFormat = null,
+        IPromptTemplateFactory? templateFactory = null)
+    {
+        // Define the agent
+        OpenAIAssistantAgent agent = await OpenAIAssistantAgent.CreateFromTemplateAsync(
+                clientProvider: this.GetClientProvider(),
+                capabilities: new OpenAIAssistantCapabilities(this.Model)
+                {
+                    Metadata = AssistantSampleMetadata
+                },
+                kernel: new Kernel(),
+                defaultArguments: new KernelArguments()
+                {
+                    {"style", "haiku"}
+                },
+                templateConfig: new PromptTemplateConfig
+                {
+                    Template = instructionTemplate,
+                    TemplateFormat = templateFormat,
+                },
+                templateFactory);
+
+        await InvokeAssistantAgentWithTemplateAsync(agent);
+    }
+
+    private async Task InvokeAssistantAgentWithTemplateAsync(OpenAIAssistantAgent agent)
+    {
+        // Create a thread for the agent conversation.
+        string threadId = await agent.CreateThreadAsync(new OpenAIThreadCreationOptions { Metadata = AssistantSampleMetadata });
+
+        try
+        {
+            // Respond to user input
+            foreach ((string input, string? style) in s_inputs)
+            {
+                ChatMessageContent request = new(AuthorRole.User, input);
+                await agent.AddChatMessageAsync(threadId, request);
+                this.WriteAgentChatMessage(request);
+
+                KernelArguments? arguments = null;
+
+                if (!string.IsNullOrWhiteSpace(style))
+                {
+                    arguments = new() { { "style", style } };
+                }
+
+                await foreach (ChatMessageContent message in agent.InvokeAsync(threadId, arguments))
+                {
+                    this.WriteAgentChatMessage(message);
+                }
+            }
+        }
+        finally
+        {
+            await agent.DeleteThreadAsync(threadId);
+            await agent.DeleteAsync();
+        }
+    }
+}
