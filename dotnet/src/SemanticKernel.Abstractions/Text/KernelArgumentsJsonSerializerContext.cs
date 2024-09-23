e76450bc393dce4ccfcdcb16b197f859dd9b55ea
SergeyMenshykh <sergemenshikh@gmail.com>
2024-09-23 20:57:44 +0100

    extend list of primitives for kernel arguments json serializer context

--- dotnet/src/SemanticKernel.Abstractions/Text/KernelArgumentsJsonSerializerContext.cs
+++ dotnet/src/SemanticKernel.Abstractions/Text/KernelArgumentsJsonSerializerContext.cs
@@ -6,18 +6,26 @@ using System.Text.Json.Serialization;
 namespace Microsoft.SemanticKernel.Text;
 
 [JsonSerializable(typeof(KernelArguments))]
-[JsonSerializable(typeof(uint?))]
+[JsonSerializable(typeof(byte?))]
+[JsonSerializable(typeof(sbyte?))]
+[JsonSerializable(typeof(short?))]
+[JsonSerializable(typeof(ushort?))]
 [JsonSerializable(typeof(int?))]
+[JsonSerializable(typeof(uint?))]
 [JsonSerializable(typeof(long?))]
-[JsonSerializable(typeof(string))]
-[JsonSerializable(typeof(bool?))]
-[JsonSerializable(typeof(float?))]
+[JsonSerializable(typeof(ulong?))]
+[JsonSerializable(typeof(char?))]
 [JsonSerializable(typeof(double?))]
+[JsonSerializable(typeof(float?))]
+[JsonSerializable(typeof(bool?))]
 [JsonSerializable(typeof(decimal?))]
+[JsonSerializable(typeof(string))]
 [JsonSerializable(typeof(DateTime?))]
 [JsonSerializable(typeof(DateTimeOffset?))]
 [JsonSerializable(typeof(TimeSpan?))]
 [JsonSerializable(typeof(Guid?))]
+[JsonSerializable(typeof(Uri))]
+
 internal sealed partial class KernelArgumentsJsonSerializerContext : JsonSerializerContext
 {
 }
