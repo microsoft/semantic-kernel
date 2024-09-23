1518b180516eedee7aeaa9c67c1bf0e7f6705a28
markwallace-microsoft <127216156+markwallace-microsoft@users.noreply.github.com>
2024-09-23 15:48:43 +0100

    First sample showing how to manage Chat History

---
+++ dotnet/samples/Concepts/ChatCompletion/ChatCompletionServiceExtensions.cs
@@ -0,0 +1,66 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Runtime.CompilerServices;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+namespace ChatCompletion;
+
+/// <summary>
+/// Extensions methods for <see cref="IChatCompletionService"/>
+/// </summary>
+internal static class ChatCompletionServiceExtensions
+{
+    /// <summary>
+    /// Adds an wrapper to an instance of <see cref="IChatCompletionService"/> which will invoke a delegate
+    /// to reduce the size of the <see cref="ChatHistory"/> before sending it to the model.
+    /// </summary>
+    /// <param name="service">Instance of <see cref="IChatCompletionService"/></param>
+    /// <param name="reducer">Instances of <see cref="ChatHistoryReducer"/></param>
+    public static IChatCompletionService WithChatHistoryReducer(this IChatCompletionService service, ChatHistoryReducer reducer)
+    {
+        return new ChatCompletionServiceWithReducer(service, reducer);
+    }
+}
+
+/// <summary>
+/// Delegate for reducing the chat history before sending it to the chat completion provider.
+/// </summary>
+internal delegate Task<ChatHistory?> ChatHistoryReducer(ChatHistory chatHistory, CancellationToken cancellationToken);
+
+/// <summary>
+/// Instance of <see cref="IChatCompletionService"/> which will invoke a delegate
+/// to reduce the size of the <see cref="ChatHistory"/> before sending it to the model.
+/// </summary>
+internal sealed class ChatCompletionServiceWithReducer(IChatCompletionService service, ChatHistoryReducer reducer) : IChatCompletionService
+{
+    public IReadOnlyDictionary<string, object?> Attributes => throw new NotImplementedException();
+
+    /// <inheritdoc/>
+    public async Task<IReadOnlyList<ChatMessageContent>> GetChatMessageContentsAsync(
+        ChatHistory chatHistory,
+        PromptExecutionSettings? executionSettings = null,
+        Kernel? kernel = null,
+        CancellationToken cancellationToken = default)
+    {
+        var reducedChatHistory = await reducer(chatHistory, cancellationToken).ConfigureAwait(false);
+
+        return await service.GetChatMessageContentsAsync(reducedChatHistory ?? chatHistory, executionSettings, kernel, cancellationToken).ConfigureAwait(false);
+    }
+
+    /// <inheritdoc/>
+    public async IAsyncEnumerable<StreamingChatMessageContent> GetStreamingChatMessageContentsAsync(
+        ChatHistory chatHistory,
+        PromptExecutionSettings? executionSettings = null,
+        Kernel? kernel = null,
+        [EnumeratorCancellation] CancellationToken cancellationToken = default)
+    {
+        var reducedChatHistory = await reducer(chatHistory, cancellationToken).ConfigureAwait(false);
+
+        var messages = service.GetStreamingChatMessageContentsAsync(reducedChatHistory ?? chatHistory, executionSettings, kernel, cancellationToken);
+        await foreach (var message in messages)
+        {
+            yield return message;
+        }
+    }
+}
---
+++ dotnet/samples/Concepts/ChatCompletion/OpenAI_ChatHistoryReducer.cs
@@ -0,0 +1,148 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Text;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using OpenAI.Chat;
+
+namespace ChatCompletion;
+
+/// <summary>
+/// The sample show how to add a chat history reducer which only sends the last two messages in <see cref="ChatHistory"/> to the model.
+/// </summary>
+public class OpenAI_ChatHistoryReducer(ITestOutputHelper output) : BaseTest(output)
+{
+    [Fact]
+    public async Task ShowTotalTokenCountAsync()
+    {
+        Assert.NotNull(TestConfiguration.OpenAI.ChatModelId);
+        Assert.NotNull(TestConfiguration.OpenAI.ApiKey);
+
+        OpenAIChatCompletionService openAiChatService = new(
+            modelId: TestConfiguration.OpenAI.ChatModelId,
+            apiKey: TestConfiguration.OpenAI.ApiKey);
+
+        var chatHistory = new ChatHistory("You are a librarian and expert on books about cities");
+
+        string[] userMessages = [
+            "Recommend a list of books about Seattle",
+            "Recommend a list of books about Dublin",
+            "Recommend a list of books about Amsterdam",
+            "Recommend a list of books about Paris",
+            "Recommend a list of books about London"
+        ];
+
+        int totalTokenCount = 0;
+        foreach (var userMessage in userMessages)
+        {
+            chatHistory.AddUserMessage(userMessage);
+
+            var response = await openAiChatService.GetChatMessageContentAsync(chatHistory);
+            chatHistory.AddAssistantMessage(response.Content!);
+
+            if (response.InnerContent is OpenAI.Chat.ChatCompletion chatCompletion)
+            {
+                totalTokenCount += chatCompletion.Usage?.TotalTokens ?? 0;
+            }
+        }
+
+        // Example total token usage is approximately: 10000
+        Console.WriteLine($"Total Token Count: {totalTokenCount}");
+    }
+
+    [Fact]
+    public async Task ShowHowToReduceChatHistoryAsync()
+    {
+        Assert.NotNull(TestConfiguration.OpenAI.ChatModelId);
+        Assert.NotNull(TestConfiguration.OpenAI.ApiKey);
+
+        OpenAIChatCompletionService openAiChatService = new(
+            modelId: TestConfiguration.OpenAI.ChatModelId,
+            apiKey: TestConfiguration.OpenAI.ApiKey);
+        IChatCompletionService chatService = openAiChatService.WithChatHistoryReducer(ChatHistoryReducerAsync);
+
+        var chatHistory = new ChatHistory("You are a librarian and expert on books about cities");
+
+        string[] userMessages = [
+            "Recommend a list of books about Seattle",
+            "Recommend a list of books about Dublin",
+            "Recommend a list of books about Amsterdam",
+            "Recommend a list of books about Paris",
+            "Recommend a list of books about London"
+        ];
+
+        int totalTokenCount = 0;
+        foreach (var userMessage in userMessages)
+        {
+            chatHistory.AddUserMessage(userMessage);
+
+            var response = await chatService.GetChatMessageContentAsync(chatHistory);
+            chatHistory.AddAssistantMessage(response.Content!);
+
+            if (response.InnerContent is OpenAI.Chat.ChatCompletion chatCompletion)
+            {
+                totalTokenCount += chatCompletion.Usage?.TotalTokens ?? 0;
+            }
+        }
+
+        // Example total token usage is approximately: 3000
+        Console.WriteLine($"Total Token Count: {totalTokenCount}");
+    }
+
+    [Fact]
+    public async Task ShowHowToReduceChatHistoryStreamingAsync()
+    {
+        Assert.NotNull(TestConfiguration.OpenAI.ChatModelId);
+        Assert.NotNull(TestConfiguration.OpenAI.ApiKey);
+
+        OpenAIChatCompletionService openAiChatService = new(
+            modelId: TestConfiguration.OpenAI.ChatModelId,
+            apiKey: TestConfiguration.OpenAI.ApiKey);
+        IChatCompletionService chatService = openAiChatService.WithChatHistoryReducer(ChatHistoryReducerAsync);
+
+        var chatHistory = new ChatHistory("You are a librarian and expert on books about cities");
+
+        string[] userMessages = [
+            "Recommend a list of books about Seattle",
+            "Recommend a list of books about Dublin",
+            "Recommend a list of books about Amsterdam",
+            "Recommend a list of books about Paris",
+            "Recommend a list of books about London"
+        ];
+
+        int totalTokenCount = 0;
+        foreach (var userMessage in userMessages)
+        {
+            chatHistory.AddUserMessage(userMessage);
+
+            var response = new StringBuilder();
+            var chatUpdates = chatService.GetStreamingChatMessageContentsAsync(chatHistory);
+            await foreach (var chatUpdate in chatUpdates)
+            {
+                response.Append((string?)chatUpdate.Content);
+
+                if (chatUpdate.InnerContent is StreamingChatCompletionUpdate openAiChatUpdate)
+                {
+                    totalTokenCount += openAiChatUpdate.Usage?.TotalTokens ?? 0;
+                }
+            }
+            chatHistory.AddAssistantMessage(response.ToString());
+        }
+
+        // Example total token usage is approximately: 3000
+        Console.WriteLine($"Total Token Count: {totalTokenCount}");
+    }
+
+    #region private
+    /// <summary>
+    /// ChatHistoryReducer function that just preserves the system message and the last message.
+    /// </summary>
+    private static async Task<ChatHistory?> ChatHistoryReducerAsync(ChatHistory chatHistory, CancellationToken cancellationToken)
+    {
+        var firstMessage = chatHistory.FirstOrDefault();
+        ChatHistory? reducedChatHistory = (firstMessage?.Role == AuthorRole.System) ? new(firstMessage?.Content!) : new();
+        reducedChatHistory.Add(chatHistory[^1]);
+        return reducedChatHistory ?? chatHistory;
+    }
+    #endregion
+}
