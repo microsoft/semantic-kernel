f29d16bc8dcdb071849485e5767b0983abeda1a1
SergeyMenshykh <sergemenshikh@gmail.com>
2024-09-24 21:53:31 +0100

    remove unused using

--- dotnet/src/SemanticKernel.Abstractions/Services/EmptyServiceProvider.cs
+++ dotnet/src/SemanticKernel.Abstractions/Services/EmptyServiceProvider.cs
@@ -3,7 +3,6 @@
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Microsoft.SemanticKernel;
