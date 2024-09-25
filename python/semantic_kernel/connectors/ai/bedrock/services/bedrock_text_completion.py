cc42162e4cd8ee426a44754a545c5bea3c520d96
Tao Chen <taochen@microsoft.com>
2024-09-25 10:46:29 -0700

    Add Bedrock packages

---
+++ python/semantic_kernel/connectors/ai/bedrock/services/bedrock_text_completion.py
@@ -0,0 +1,9 @@
+# Copyright (c) Microsoft. All rights reserved.
+
+from semantic_kernel.connectors.ai.text_completion_client_base import TextCompletionClientBase
+
+
+class BedrockTextCompletion(TextCompletionClientBase):
+    """Amazon Bedrock Text Completion Service."""
+
+    pass
