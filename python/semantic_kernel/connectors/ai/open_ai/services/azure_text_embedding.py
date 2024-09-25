771ac2c8753f7bf0ec1ee0c91534ea28b9324eb4
Rob Emanuele <rdemanuele@gmail.com>
2024-09-25 12:31:13 -0400

    Python: Azure OpenAI setting fix: Don't fetch AD token if API is present in environment variable. (#8985)
    ### Motivation and Context
    
    While working on #8951 I found that interacting with an Azure OpenAI
    instance I had an API key for, but my identity didn't have IAM access to
    the API for, that I was getting a permission error:
    
    ```
    semantic_kernel.exceptions.service_exceptions.ServiceResponseException: ("<class 'semantic_kernel.connectors.ai.open_ai.services.azure_text_embedding.AzureTextEmbedding'> service failed to generate embeddings", AuthenticationError("Error code: 401 - {'error': {'code': 'PermissionDenied', 'message': 'Principal does not have access to API/Operation.'}}"))
    ```
    
    I found that this was because, though my API key was set in the
    environment, the codepath only recognized an explicitly passed in API
    key when deciding whether to issue an AD token while initializing an
    AzureTextEmbedding instance.
    
    This PR fixes that by looking at the setting object explicitly, which
    may pull the API key from the environment.
    
    
    ### Contribution Checklist
    
    - [x] The code builds clean without any errors or warnings
    - [x] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [x] All unit tests pass, and I have added new tests where possible
    - [x] I didn't break anyone :smile:

--- python/semantic_kernel/connectors/ai/open_ai/services/azure_text_embedding.py
+++ python/semantic_kernel/connectors/ai/open_ai/services/azure_text_embedding.py
@@ -74,9 +74,20 @@ class AzureTextEmbedding(AzureOpenAIConfigBase, OpenAITextEmbeddingBase):
 
         # If the api_key is none, and the ad_token is none, and the ad_token_provider is none,
         # then we will attempt to get the ad_token using the default endpoint specified in the Azure OpenAI settings.
-        if api_key is None and ad_token_provider is None and azure_openai_settings.token_endpoint and ad_token is None:
+        if (
+            azure_openai_settings.api_key is None
+            and ad_token_provider is None
+            and azure_openai_settings.token_endpoint
+            and ad_token is None
+            and async_client is None
+        ):
             ad_token = azure_openai_settings.get_azure_openai_auth_token(
                 token_endpoint=azure_openai_settings.token_endpoint
+            )
+
+        if not azure_openai_settings.api_key and not ad_token and not ad_token_provider and not async_client:
+            raise ServiceInitializationError(
+                "Please provide either api_key, ad_token, ad_token_provider, or a custom client"
             )
 
         super().__init__(
