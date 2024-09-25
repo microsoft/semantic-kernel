c1f59d834f809b785585823e64ae405aa77760a6
markwallace-microsoft <127216156+markwallace-microsoft@users.noreply.github.com>
2024-09-25 20:52:51 +0100

    Integration tests for Bing and Google text search

---
+++ dotnet/src/IntegrationTests/Plugins/Web/Bing/BingTextSearchTests.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel.Data;
+using Microsoft.SemanticKernel.Plugins.Web.Bing;
+using SemanticKernel.IntegrationTests.Data;
+using SemanticKernel.IntegrationTests.TestSettings;
+using Xunit;
+
+namespace SemanticKernel.IntegrationTests.Plugins.Web.Bing;
+
+/// <summary>
+/// Integration tests for <see cref="BingTextSearch"/>.
+/// </summary>
+public class BingTextSearchTests : BaseTextSearchTests
+{
+    /// <inheritdoc/>
+    public override ITextSearch CreateTextSearch()
+    {
+        var configuration = this.Configuration.GetSection("Bing").Get<BingConfiguration>();
+        Assert.NotNull(configuration);
+        Assert.NotNull(configuration.ApiKey);
+
+        return new BingTextSearch(apiKey: configuration.ApiKey);
+    }
+
+    /// <inheritdoc/>
+    public override string GetQuery() => "What is the Semantic Kernel?";
+
+    /// <inheritdoc/>
+    public override TextSearchFilter GetTextSearchFilter() => new TextSearchFilter().Equality("site", "devblogs.microsoft.com");
+
+    /// <inheritdoc/>
+    public override bool VerifySearchResult(object result, TextSearchFilter? filter = null) => result is BingWebPage;
+}
