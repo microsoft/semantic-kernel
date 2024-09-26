66d608c104ad2ff503bf3055280083b69f980b52
Mark Wallace <127216156+markwallace-microsoft@users.noreply.github.com>
2024-09-26 19:03:10 +0100

    .Net: Add delegates for the string and result mappers (#9000)
    ### Motivation and Context
    
    Related to #6725 
    
    Implementation of `ITextSearchStringMapper` in core where we can just
    pass a lambda
    
    ### Description
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [ ] The code builds clean without any errors or warnings
    - [ ] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [ ] All unit tests pass, and I have added new tests where possible
    - [ ] I didn't break anyone :smile:

--- dotnet/src/SemanticKernel.Abstractions/Data/TextSearch/ITextSearchResultMapper.cs
+++ dotnet/src/SemanticKernel.Abstractions/Data/TextSearch/ITextSearchResultMapper.cs
@@ -18,3 +18,24 @@ public interface ITextSearchResultMapper
     /// <returns>A <see cref="TextSearchResult" /> instance.</returns>
     TextSearchResult MapFromResultToTextSearchResult(object result);
 }
+
+/// <summary>
+/// Delegate to map from an <see cref="object"/> which represents a result value associated with a <see cref="ITextSearch" /> implementation
+/// to a a <see cref="TextSearchResult" /> instance.
+/// </summary>
+/// <param name="result">The result value to map.</param>
+/// <returns>A <see cref="TextSearchResult" /> instance.</returns>
+public delegate TextSearchResult MapFromResultToTextSearchResult(object result);
+
+/// <summary>
+/// Default implementation of <see cref="ITextSearchResultMapper" /> that use the <see cref="MapFromResultToTextSearchResult" /> delegate.
+/// </summary>
+/// <param name="mapFromResultToTextSearchResult">MapFromResultToTextSearchResult delegate</param>
+public class TextSearchResultMapper(MapFromResultToTextSearchResult mapFromResultToTextSearchResult) : ITextSearchResultMapper
+{
+    /// <inheritdoc />
+    public TextSearchResult MapFromResultToTextSearchResult(object result)
+    {
+        return mapFromResultToTextSearchResult(result);
+    }
+}
