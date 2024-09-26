e78ec502774dc87217e560b10d2bf59fb8c2029c
SergeyMenshykh <sergemenshikh@gmail.com>
2024-09-26 22:00:22 +0100

    fix IL2067 and IL3051 warnings + aot compatibility test app

---
+++ dotnet/src/Aot/AotCompatibility.TestApp/Program.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+namespace AotCompatibility.TestApp;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
