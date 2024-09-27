4747e2401d9fa548782fd0150e1b9b39fdb8eef9
westey <164392973+westey-m@users.noreply.github.com>
2024-09-27 17:43:48 +0100

    Add skeleton InMemory Vector Store project and unit test project to solution.

---
+++ dotnet/src/Connectors/Connectors.Memory.InMemory/InMemoryVectorStore.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+namespace Microsoft.SemanticKernel.Connectors.InMemory;
+
+/// <summary>
+/// Placeholder for copy of VolatileVectorStore.
+/// </summary>
+public class InMemoryVectorStore
+{
+}
