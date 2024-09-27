01553e368c9b2345654c1401c926f9b86eca636c
Estefania Tenorio <8483207+esttenorio@users.noreply.github.com>
2024-09-27 09:44:04 -0700

    adding missing summary

--- dotnet/samples/GettingStartedWithProcesses/Models/AccountDetails.cs
+++ dotnet/samples/GettingStartedWithProcesses/Models/AccountDetails.cs
@@ -1,6 +1,11 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
 namespace Models;
+
+/// <summary>
+/// Represents the data structure for a form capturing details of a new customer, including personal information, contact details, account id and account type.<br/>
+/// Model used in Step02_AccountOpening.cs samples
+/// </summary>
 public class AccountDetails : NewCustomerForm
 {
     public Guid AccountId { get; set; }
