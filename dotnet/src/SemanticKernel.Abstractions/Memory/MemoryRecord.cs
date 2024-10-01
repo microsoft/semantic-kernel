4954a774cdc1c13ba2af488b13c82b76c355d7cd
SergeyMenshykh <sergemenshikh@gmail.com>
2024-10-01 12:40:40 +0100

    fix latest merge

--- dotnet/src/SemanticKernel.Abstractions/Memory/MemoryRecord.cs
+++ dotnet/src/SemanticKernel.Abstractions/Memory/MemoryRecord.cs
@@ -4,6 +4,7 @@ using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using Microsoft.SemanticKernel.Text;
 
 namespace Microsoft.SemanticKernel.Memory;
 
