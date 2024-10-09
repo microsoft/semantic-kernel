9d7c4fcf951b5f9cce97e4f790414d21eb08df4c
Stephen Toub <stoub@microsoft.com>
2024-10-09 14:34:41 -0400

    Enable bidirectional adapters between SK interfaces and Microsoft.Extensions.AI
    This is part 1 of replatforming Semantic Kernel on top of Microsoft.Extensions.AI, making it possible to use IChatCompletionService instances as IChatClient instances, and vice versa, and similarly for IEmbeddingGenerationService and IEmbeddingGenerator. The next step after this is refactoring the connectors to use M.E.AI internally where relevant.

---
+++ dotnet/src/SemanticKernel.UnitTests/AI/ServiceConversionExtensionsTests.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using Microsoft.SemanticKernel;
+using Xunit;
+
+namespace SemanticKernel.UnitTests.AI;
+
+public class ServiceConversionExtensionsTests
+{
+    [Fact]
+    public void InvalidArgumentsThrow()
+    {
+        Assert.Throws<ArgumentNullException>("service", () => ServiceConversionExtensions.AsChatClient(null!));
+        Assert.Throws<ArgumentNullException>("client", () => ServiceConversionExtensions.AsChatCompletionService(null!));
+
+        Assert.Throws<ArgumentNullException>("service", () => ServiceConversionExtensions.AsEmbeddingGenerator<string, float>(null!));
+        Assert.Throws<ArgumentNullException>("generator", () => ServiceConversionExtensions.AsEmbeddingGenerationService<string, float>(null!));
+    }
+}
