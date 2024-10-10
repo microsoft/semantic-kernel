b645bb7d4e2c096518f2804f0d9c8630095ae111
westey <164392973+westey-m@users.noreply.github.com>
2024-10-10 15:45:31 +0100

    .Net: Add skeleton InMemory Vector Store project and unit test project to solution (#9019)
    ### Motivation and Context
    
    We intend to move the volatile memory store to it's own project and
    rename it to InMemory.
    
    ### Description
    
    This is the first step of the process and involves creating the
    projects.
    Also moving all memory unit test projects into a separate solution
    folder to simply navigating the solution structure.
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [x] The code builds clean without any errors or warnings
    - [x] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [x] All unit tests pass, and I have added new tests where possible
    - [x] I didn't break anyone :smile:
    
    ---------
    
    Co-authored-by: Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>

---
+++ dotnet/src/Connectors/Connectors.InMemory.UnitTests/InMemoryVectorStoreTests.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Xunit;
+
+namespace Microsoft.SemanticKernel.Connectors.InMemory.UnitTests;
+
+public class InMemoryVectorStoreTests
+{
+    [Fact]
+    public void PlaceholderTest()
+    {
+        Assert.True(true);
+    }
+}
