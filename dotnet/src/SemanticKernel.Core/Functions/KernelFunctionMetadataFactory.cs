cdf3a756e674a5b2cd7bac667e11e73518d07475
Mark Wallace <127216156+markwallace-microsoft@users.noreply.github.com>
2024-10-14 16:17:35 +0100

    .Net: New CreateFromType and CreateMetadataFromType methods (#9232)
    ### Motivation and Context
    
    Closes #8946 
    
    ### Description
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [ ] The code builds clean without any errors or warnings
    - [ ] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [ ] All unit tests pass, and I have added new tests where possible
    - [ ] I didn't break anyone :smile:

---
+++ dotnet/src/SemanticKernel.Core/Functions/KernelFunctionMetadataFactory.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.SemanticKernel;
+
+/// <summary>
+/// Provides factory methods for creating collections of <see cref="KernelFunctionMetadata"/>, such as
+/// those backed by a prompt to be submitted to an LLM or those backed by a .NET method.
+/// </summary>
+[Experimental("SKEXP0001")]
+public static class KernelFunctionMetadataFactory
+{
+    /// <summary>
+    /// Creates a <see cref="KernelFunctionMetadata"/> enumeration for a method, specified via an <see cref="MethodInfo"/> instance.
+    /// </summary>
+    /// <param name="instanceType">Specifies the type of the object to extract <see cref="KernelFunctionMetadata"/> for.</param>
+    /// <param name="loggerFactory">The <see cref="ILoggerFactory"/> to use for logging. If null, no logging will be performed.</param>
+    /// <returns>A <see cref="KernelPlugin"/> containing <see cref="KernelFunction"/>s for all relevant members of <paramref name="instanceType"/>.</returns>
+    /// <remarks>
+    /// Methods decorated with <see cref="KernelFunctionAttribute"/> will be included in the plugin.
+    /// Attributed methods must all have different names; overloads are not supported.
+    /// </remarks>
+    public static IEnumerable<KernelFunctionMetadata> CreateFromType(Type instanceType, ILoggerFactory? loggerFactory = null)
+    {
+        Verify.NotNull(instanceType);
+
+        MethodInfo[] methods = instanceType.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+
+        // Filter out non-KernelFunctions and fail if two functions have the same name (with or without the same casing).
+        var functionMetadata = new List<KernelFunctionMetadata>();
+        KernelFunctionFromMethodOptions options = new();
+        foreach (MethodInfo method in methods)
+        {
+            if (method.GetCustomAttribute<KernelFunctionAttribute>() is not null)
+            {
+                functionMetadata.Add(KernelFunctionFromMethod.CreateMetadata(method, loggerFactory: loggerFactory));
+            }
+        }
+        if (functionMetadata.Count == 0)
+        {
+            throw new ArgumentException($"The {instanceType} instance doesn't implement any [KernelFunction]-attributed methods.");
+        }
+
+        return functionMetadata;
+    }
+}
