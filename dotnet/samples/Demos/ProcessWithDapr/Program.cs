18f139e08ed27109da748a341a38071fcf48f807
Ben Thomas <bentho@microsoft.com>
2024-10-16 16:35:27 -0700

    Cleaning up Dapr demo.

--- dotnet/samples/Demos/ProcessWithDapr/Program.cs
+++ dotnet/samples/Demos/ProcessWithDapr/Program.cs
@@ -4,35 +4,24 @@ using Microsoft.SemanticKernel;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Configure logging
 builder.Services.AddLogging((logging) =>
 {
     logging.AddConsole();
     logging.AddDebug();
 });
 
+// Configure the Kernel with DI. This is required for dependency injection to work with processes.
 builder.Services.AddKernel();
 
-builder.Services.AddHttpClient("SKClient", static (client) =>
-{
-    client.Timeout = TimeSpan.FromMinutes(5);
-})
-.ConfigurePrimaryHttpMessageHandler(static () =>
-{
-    var handler = new HttpClientHandler();
-    handler.CheckCertificateRevocationList = false;
-    return handler;
-});
-
+// Configure Dapr
 builder.Services.AddActors(static options =>
 {
-    // Register actor types and configure actor settings
+    // Register the actors required to run Processes
     options.AddProcessActors();
 });
 
 builder.Services.AddControllers();
-
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
