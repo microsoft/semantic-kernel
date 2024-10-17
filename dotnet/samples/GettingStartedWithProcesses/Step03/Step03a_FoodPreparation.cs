bd2e9442b701f26537da86a1e564df3a3eabff39
Estefania Tenorio <8483207+esttenorio@users.noreply.github.com>
2024-10-16 17:29:38 -0700

    Stateful food preparation samples

--- dotnet/samples/GettingStartedWithProcesses/Step03/Step03a_FoodPreparation.cs
+++ dotnet/samples/GettingStartedWithProcesses/Step03/Step03a_FoodPreparation.cs
@@ -15,6 +15,7 @@ public class Step03a_FoodPreparation(ITestOutputHelper output) : BaseTest(output
     // Target Open AI Services
     protected override bool ForceOpenAI => true;
 
+    #region Stateless Processes
     [Fact]
     public async Task UsePrepareFriedFishProcessAsync()
     {
@@ -42,7 +43,75 @@ public class Step03a_FoodPreparation(ITestOutputHelper output) : BaseTest(output
         var process = FishAndChipsProcess.CreateProcess();
         await UsePrepareSpecificProductAsync(process, FishAndChipsProcess.ProcessEvents.PrepareFishAndChips);
     }
+    #endregion
+    #region Stateful Processes
+    /// <summary>
+    /// Test case that showcase when the same process is build multiple times, it will have different initial states
+    /// </summary>
+    /// <returns></returns>
+    [Fact]
+    public async Task UsePrepareStatefulFriedFishProcessNoSharedStateAsync()
+    {
+        var processBuilder = FriedFishProcess.CreateProcessWithStatefulSteps();
+        var externalTriggerEvent = FriedFishProcess.ProcessEvents.PrepareFriedFish;
 
+        Kernel kernel = CreateKernelWithChatCompletion();
+
+        // Assert
+        Console.WriteLine($"=== Start SK Process '{processBuilder.Name}' ===");
+        await ExecuteProcessWithStateAsync(processBuilder.Build(), kernel, externalTriggerEvent, "Order 1");
+        await ExecuteProcessWithStateAsync(processBuilder.Build(), kernel, externalTriggerEvent, "Order 2");
+        Console.WriteLine($"=== End SK Process '{processBuilder.Name}' ===");
+    }
+
+    /// <summary>
+    /// Test case that showcase when the same process is build once and used multple times, it will have share the state
+    /// and the state of the steps will become the initial state of the next running process
+    /// </summary>
+    /// <returns></returns>
+    [Fact]
+    public async Task UsePrepareStatefulFriedFishProcessSharedStateAsync()
+    {
+        var processBuilder = FriedFishProcess.CreateProcessWithStatefulSteps();
+        var externalTriggerEvent = FriedFishProcess.ProcessEvents.PrepareFriedFish;
+
+        Kernel kernel = CreateKernelWithChatCompletion();
+        KernelProcess kernelProcess = processBuilder.Build();
+
+        Console.WriteLine($"=== Start SK Process '{processBuilder.Name}' ===");
+        await ExecuteProcessWithStateAsync(kernelProcess, kernel, externalTriggerEvent, "Order 1");
+        await ExecuteProcessWithStateAsync(kernelProcess, kernel, externalTriggerEvent, "Order 2");
+        await ExecuteProcessWithStateAsync(kernelProcess, kernel, externalTriggerEvent, "Order 3");
+        Console.WriteLine($"=== End SK Process '{processBuilder.Name}' ===");
+    }
+
+    [Fact]
+    public async Task UsePrepareStatefulPotatoFriesProcessSharedStateAsync()
+    {
+        var processBuilder = PotatoFriesProcess.CreateProcessWithStatefulSteps();
+        var externalTriggerEvent = PotatoFriesProcess.ProcessEvents.PreparePotatoFries;
+
+        Kernel kernel = CreateKernelWithChatCompletion();
+        KernelProcess kernelProcess = processBuilder.Build();
+
+        Console.WriteLine($"=== Start SK Process '{processBuilder.Name}' ===");
+        await ExecuteProcessWithStateAsync(kernelProcess, kernel, externalTriggerEvent, "Order 1");
+        await ExecuteProcessWithStateAsync(kernelProcess, kernel, externalTriggerEvent, "Order 2");
+        await ExecuteProcessWithStateAsync(kernelProcess, kernel, externalTriggerEvent, "Order 3");
+        Console.WriteLine($"=== End SK Process '{processBuilder.Name}' ===");
+    }
+
+    private async Task<KernelProcess> ExecuteProcessWithStateAsync(KernelProcess process, Kernel kernel, string externalTriggerEvent, string orderLabel)
+    {
+        Console.WriteLine($"=== {orderLabel} ===");
+        var runningProcess = await process.StartAsync(kernel, new KernelProcessEvent()
+        {
+            Id = externalTriggerEvent,
+            Data = new List<string>()
+        });
+        return await runningProcess.GetStateAsync();
+    }
+    #endregion
     protected async Task UsePrepareSpecificProductAsync(ProcessBuilder processBuilder, string externalTriggerEvent)
     {
         // Arrange
@@ -52,9 +121,11 @@ public class Step03a_FoodPreparation(ITestOutputHelper output) : BaseTest(output
         KernelProcess kernelProcess = processBuilder.Build();
 
         // Assert
+        Console.WriteLine($"=== Start SK Process '{processBuilder.Name}' ===");
         using var runningProcess = await kernelProcess.StartAsync(kernel, new KernelProcessEvent()
         {
             Id = externalTriggerEvent, Data = new List<string>()
         });
+        Console.WriteLine($"=== End SK Process '{processBuilder.Name}' ===");
     }
 }
