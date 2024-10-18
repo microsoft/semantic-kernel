400d9d2e692bbc1aeea4c3b376ba60b8228fe874
SergeyMenshykh <sergemenshikh@gmail.com>
2024-10-18 12:27:43 +0100

    update readme

--- dotnet/samples/Demos/AotCompatibility/Program.cs
+++ dotnet/samples/Demos/AotCompatibility/Program.cs
@@ -5,8 +5,7 @@ using Microsoft.Extensions.Configuration;
 namespace SemanticKernel.AotCompatibility;
 
 /// <summary>
-/// This application is created in accordance with the instructions in the blog post at https://devblogs.microsoft.com/dotnet/creating-aot-compatible-libraries to ensure that
-/// all Native-AOT-related warnings missed by the Roslyn analyzers are caught by the AOT compiler used in this application.
+/// This application demonstrates how to use the Semantic Kernel in AOT applications.
 /// </summary>
 internal sealed class Program
 {
