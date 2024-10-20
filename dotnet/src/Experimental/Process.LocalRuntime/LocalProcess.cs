3a740581bf4f5d1ed4a8c75add83b95a06c69d5f
Chris Rickman <crickman@microsoft.com>
2024-10-20 16:53:37 -0700

    namespace

--- dotnet/src/Experimental/Process.LocalRuntime/LocalProcess.cs
+++ dotnet/src/Experimental/Process.LocalRuntime/LocalProcess.cs
@@ -3,7 +3,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Threading;
 using System.Threading.Channels;
 using System.Threading.Tasks;
