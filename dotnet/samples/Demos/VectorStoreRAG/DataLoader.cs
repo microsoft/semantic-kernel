c6163ac45d6f82acfdde4ec3de71849cd62a6019
westey <164392973+westey-m@users.noreply.github.com>
2024-10-21 18:38:37 +0100

    Add image to text support to rag sample.

--- dotnet/samples/Demos/VectorStoreRAG/DataLoader.cs
+++ dotnet/samples/Demos/VectorStoreRAG/DataLoader.cs
@@ -3,6 +3,7 @@
 using System.Net;
 using Microsoft.Extensions.VectorData;
 using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.Embeddings;
 using UglyToad.PdfPig;
 using UglyToad.PdfPig.Content;
@@ -17,10 +18,12 @@ namespace VectorStoreRAG;
 /// <param name="uniqueKeyGenerator">A function to generate unique keys with.</param>
 /// <param name="vectorStoreRecordCollection">The collection to load the data into.</param>
 /// <param name="textEmbeddingGenerationService">The service to use for generating embeddings from the text.</param>
+/// <param name="chatCompletionService">The chat completion service to use for generating text from images.</param>
 internal sealed class DataLoader<TKey>(
     UniqueKeyGenerator<TKey> uniqueKeyGenerator,
     IVectorStoreRecordCollection<TKey, TextSnippet<TKey>> vectorStoreRecordCollection,
-    ITextEmbeddingGenerationService textEmbeddingGenerationService) : IDataLoader where TKey : notnull
+    ITextEmbeddingGenerationService textEmbeddingGenerationService,
+    IChatCompletionService chatCompletionService) : IDataLoader where TKey : notnull
 {
     /// <inheritdoc/>
     public async Task LoadPdf(string pdfPath, int batchSize, int betweenBatchDelayInMs, CancellationToken cancellationToken)
@@ -28,21 +31,37 @@ internal sealed class DataLoader<TKey>(
         // Create the collection if it doesn't exist.
         await vectorStoreRecordCollection.CreateCollectionIfNotExistsAsync(cancellationToken).ConfigureAwait(false);
 
-        // Load the paragraphs from the PDF file and split them into batches.
-        var sections = LoadParagraphs(pdfPath, cancellationToken);
+        // Load the text and images from the PDF file and split them into batches.
+        var sections = LoadTextAndImages(pdfPath, cancellationToken);
         var batches = sections.Chunk(batchSize);
 
-        // Process each batch of paragraphs.
+        // Process each batch of content items.
         foreach (var batch in batches)
         {
+            // Convert any images to text.
+            var textContentTasks = batch.Select(async content =>
+            {
+                if (content.Text != null)
+                {
+                    return content;
+                }
+
+                var textFromImage = await ConvertImageToTextWithRetryAsync(
+                    chatCompletionService,
+                    content.Image!.Value,
+                    cancellationToken).ConfigureAwait(false);
+                return new RawContent { Text = textFromImage, PageNumber = content.PageNumber };
+            });
+            var textContent = await Task.WhenAll(textContentTasks).ConfigureAwait(false);
+
             // Map each paragraph to a TextSnippet and generate an embedding for it.
-            var recordTasks = batch.Select(async section => new TextSnippet<TKey>
+            var recordTasks = textContent.Select(async content => new TextSnippet<TKey>
             {
                 Key = uniqueKeyGenerator.GenerateKey(),
-                Text = section.ParagraphText,
-                ReferenceDescription = $"{new FileInfo(pdfPath).Name}#page={section.PageNumber}",
-                ReferenceLink = $"{new Uri(new FileInfo(pdfPath).FullName).AbsoluteUri}#page={section.PageNumber}",
-                TextEmbedding = await GenerateEmbeddingsWithRetryAsync(textEmbeddingGenerationService, section.ParagraphText, cancellationToken: cancellationToken).ConfigureAwait(false)
+                Text = content.Text,
+                ReferenceDescription = $"{new FileInfo(pdfPath).Name}#page={content.PageNumber}",
+                ReferenceLink = $"{new Uri(new FileInfo(pdfPath).FullName).AbsoluteUri}#page={content.PageNumber}",
+                TextEmbedding = await GenerateEmbeddingsWithRetryAsync(textEmbeddingGenerationService, content.Text!, cancellationToken: cancellationToken).ConfigureAwait(false)
             });
 
             // Upsert the records into the vector store.
@@ -58,12 +77,12 @@ internal sealed class DataLoader<TKey>(
     }
 
     /// <summary>
-    /// Read the text from each paragraph in the provided PDF file.
+    /// Read the text and images from each page in the provided PDF file.
     /// </summary>
-    /// <param name="pdfPath">The pdf file to read the paragraphs from.</param>
+    /// <param name="pdfPath">The pdf file to read the text and images from.</param>
     /// <param name="cancellationToken">The <see cref="CancellationToken"/> to monitor for cancellation requests.</param>
-    /// <returns>The paragraphs from the pdf file, plus the page that they are on.</returns>
-    private static IEnumerable<(string ParagraphText, int PageNumber)> LoadParagraphs(string pdfPath, CancellationToken cancellationToken)
+    /// <returns>The text and images from the pdf file, plus the page number that each is on.</returns>
+    private static IEnumerable<RawContent> LoadTextAndImages(string pdfPath, CancellationToken cancellationToken)
     {
         using (PdfDocument document = PdfDocument.Open(pdfPath))
         {
@@ -74,6 +93,18 @@ internal sealed class DataLoader<TKey>(
                     break;
                 }
 
+                foreach (var image in page.GetImages())
+                {
+                    if (image.TryGetPng(out var png))
+                    {
+                        yield return new RawContent { Image = png, PageNumber = page.Number };
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Unsupported image format on page {page.Number}");
+                    }
+                }
+
                 var blocks = DefaultPageSegmenter.Instance.GetBlocks(page.GetWords());
                 foreach (var block in blocks)
                 {
@@ -82,7 +113,7 @@ internal sealed class DataLoader<TKey>(
                         break;
                     }
 
-                    yield return (ParagraphText: block.Text, PageNumber: page.Number);
+                    yield return new RawContent { Text = block.Text, PageNumber = page.Number };
                 }
             }
         }
@@ -121,5 +152,61 @@ internal sealed class DataLoader<TKey>(
                 }
             }
         }
+    }
+
+    /// <summary>
+    /// Add a simple retry mechanism to image to text.
+    /// </summary>
+    /// <param name="chatCompletionService">The chat completion service to use for generating text from images.</param>
+    /// <param name="imageBytes">The image to generate the text for.</param>
+    /// <param name="cancellationToken">The <see cref="CancellationToken"/> to monitor for cancellation requests.</param>
+    /// <returns>The generated text.</returns>
+    private static async Task<string> ConvertImageToTextWithRetryAsync(
+        IChatCompletionService chatCompletionService,
+        ReadOnlyMemory<byte> imageBytes,
+        CancellationToken cancellationToken)
+    {
+        var tries = 0;
+
+        while (true)
+        {
+            try
+            {
+                var chatHistory = new ChatHistory();
+                chatHistory.AddUserMessage([
+                    new TextContent("What’s in this image?"),
+                    new ImageContent(imageBytes, "image/png"),
+                ]);
+                var result = await chatCompletionService.GetChatMessageContentsAsync(chatHistory, cancellationToken: cancellationToken).ConfigureAwait(false);
+                return string.Join("\n", result.Select(x => x.Content));
+            }
+            catch (HttpOperationException ex) when (ex.StatusCode == HttpStatusCode.TooManyRequests)
+            {
+                tries++;
+
+                if (tries < 3)
+                {
+                    Console.WriteLine($"Failed to generate text from image. Error: {ex}");
+                    Console.WriteLine("Retrying text to image conversion...");
+                    await Task.Delay(10_000, cancellationToken).ConfigureAwait(false);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Private model for returning the content items from a PDF file.
+    /// </summary>
+    private class RawContent
+    {
+        public string? Text { get; init; }
+
+        public ReadOnlyMemory<byte>? Image { get; init; }
+
+        public int PageNumber { get; init; }
     }
 }
