c24fcb1a3d0571b95fdd6b3ae0c8bd89546d9fbd
Chris Rickman <crickman@microsoft.com>
2024-10-24 14:31:51 -0700

    Core test coverage

--- dotnet/src/Experimental/Process.UnitTests/Core/ProcessMapBuilderTests.cs
+++ dotnet/src/Experimental/Process.UnitTests/Core/ProcessMapBuilderTests.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -17,7 +18,7 @@ public class ProcessMapBuilderTests
     public void ProcessMapBuilderFromStep()
     {
         // Arrange
-        ProcessStepBuilder<TestStep> step = new($"One{nameof(TestStep)}");
+        ProcessStepBuilder<SimpleTestStep> step = new($"One{nameof(SimpleTestStep)}");
 
         // Act
         ProcessMapBuilder map = new(step);
@@ -25,9 +26,9 @@ public class ProcessMapBuilderTests
         // Assert
         Assert.NotNull(map.Id);
         Assert.NotNull(map.Name);
-        Assert.Contains(nameof(TestStep), map.Name);
+        Assert.Contains(nameof(SimpleTestStep), map.Name);
         Assert.NotNull(map.TargetFunction);
-        Assert.Equal(nameof(TestStep.TestFunction), map.TargetFunction.FunctionName);
+        Assert.Equal(nameof(SimpleTestStep.TestFunction), map.TargetFunction.FunctionName);
         Assert.Equal("value", map.TargetFunction.ParameterName);
     }
 
@@ -39,7 +40,7 @@ public class ProcessMapBuilderTests
     {
         // Arrange
         ProcessBuilder process = new("MapOperation");
-        ProcessStepBuilder step = process.AddStepFromType<TestStep>($"One{nameof(TestStep)}");
+        ProcessStepBuilder step = process.AddStepFromType<SimpleTestStep>($"One{nameof(SimpleTestStep)}");
         process.OnInputEvent("ComputeMapValue").SendEventTo(new ProcessFunctionTargetBuilder(step));
 
         // Act
@@ -50,82 +51,136 @@ public class ProcessMapBuilderTests
         Assert.NotNull(map.Name);
         Assert.Contains(process.Name, map.Name);
         Assert.NotNull(map.TargetFunction);
-        Assert.Equal(nameof(TestStep.TestFunction), map.TargetFunction.FunctionName);
+        Assert.Equal(nameof(SimpleTestStep.TestFunction), map.TargetFunction.FunctionName);
         Assert.Equal("value", map.TargetFunction.ParameterName);
     }
 
-    ///// <summary>
-    ///// Tests the AddStepFromType method to ensure it adds a step correctly.
-    ///// </summary>
-    //[Fact]
-    //public void AddStepFromTypeAddsStep()
-    //{
-    //    // Arrange
-    //    var processBuilder = new ProcessBuilder(ProcessName);
+    /// <summary>
+    /// Verify initialization based on <see cref="ProcessStepBuilder"/>.
+    /// </summary>
+    [Fact]
+    public void ProcessMapBuilderIsValidTarget()
+    {
+        // Arrange
+        ProcessStepBuilder<SimpleTestStep> step = new($"One{nameof(SimpleTestStep)}");
+        ProcessMapBuilder map = new(step);
 
-    //    // Act
-    //    var stepBuilder = processBuilder.AddStepFromType<TestStep>(StepName);
+        // Act
+        ProcessStepBuilder<SimpleTestStep> step2 = new($"Two{nameof(SimpleTestStep)}");
+        step2.OnEvent("Any").SendEventTo(map);
 
-    //    // Assert
-    //    Assert.Single(processBuilder.Steps);
-    //    Assert.Equal(StepName, stepBuilder.Name);
-    //}
+        // Assert
+        Assert.Single(step2.Edges);
+        Assert.Single(step2.Edges.Single().Value);
+        Assert.NotNull(step2.Edges.Single().Value[0].Target);
+        Assert.Equal(map, step2.Edges.Single().Value[0].Target!.Step);
+    }
 
-    ///// <summary>
-    ///// Tests the AddStepFromProcess method to ensure it adds a sub-process correctly.
-    ///// </summary>
-    //[Fact]
-    //public void AddStepFromProcessAddsSubProcess()
-    //{
-    //    // Arrange
-    //    var processBuilder = new ProcessBuilder(ProcessName);
-    //    var subProcessBuilder = new ProcessBuilder(SubProcessName);
+    /// <summary>
+    /// Verify initialization based on <see cref="ProcessStepBuilder"/>.
+    /// </summary>
+    [Fact]
+    public void ProcessMapBuilderCanTargetStep()
+    {
+        // Arrange
+        ProcessStepBuilder<SimpleTestStep> step = new($"One{nameof(SimpleTestStep)}");
+        ProcessMapBuilder map = new(step);
 
-    //    // Act
-    //    var stepBuilder = processBuilder.AddStepFromProcess(subProcessBuilder);
+        // Act
+        ProcessStepBuilder<SimpleTestStep> step2 = new($"Two{nameof(SimpleTestStep)}");
+        map.OnEvent("Any").SendEventTo(new ProcessFunctionTargetBuilder(step2));
 
-    //    // Assert
-    //    Assert.Single(processBuilder.Steps);
-    //    Assert.Equal(SubProcessName, stepBuilder.Name);
-    //}
+        // Assert
+        Assert.Single(map.Edges);
+        Assert.Single(map.Edges.Single().Value);
+        Assert.NotNull(map.Edges.Single().Value[0].Target);
+        Assert.Equal(step2, map.Edges.Single().Value[0].Target!.Step);
 
-    ///// <summary>
-    ///// Tests the OnExternalEvent method to ensure it creates an edge builder correctly.
-    ///// </summary>
-    //[Fact]
-    //public void OnExternalEventCreatesEdgeBuilder()
-    //{
-    //    // Arrange
-    //    var processBuilder = new ProcessBuilder(ProcessName);
+        // Act
+        KernelProcessStepInfo processMap = map.BuildStep();
 
-    //    // Act
-    //    var edgeBuilder = processBuilder.OnInputEvent(EventId);
+        // Assert
+        Assert.NotNull(processMap);
+        Assert.Equal(processMap.Edges.Count, map.Edges.Count);
+        Assert.Equal(processMap.Edges.Single().Value.Count, map.Edges.First().Value.Count);
+        Assert.Equal(processMap.Edges.Single().Value.Single().OutputTarget!.StepId, map.Edges.Single().Value[0].Target!.Step.Id);
+    }
 
-    //    // Assert
-    //    Assert.NotNull(edgeBuilder);
-    //    Assert.Equal(EventId, edgeBuilder.EventId);
-    //}
+    /// <summary>
+    /// Verify <see cref="ProcessMapBuilder.GetFunctionMetadataMap"/> always throws.
+    /// </summary>
+    [Fact]
+    public void ProcessMapBuilderGetFunctionMetadataMapThrows()
+    {
+        // Arrange
+        ProcessStepBuilder<SimpleTestStep> step = new($"One{nameof(SimpleTestStep)}");
+        ProcessMapBuilder map = new(step);
 
-    ///// <summary>
-    ///// Tests the Build method to ensure it creates a KernelProcess correctly.
-    ///// </summary>
-    //[Fact]
-    //public void BuildCreatesKernelProcess()
-    //{
-    //    // Arrange
-    //    var processBuilder = new ProcessBuilder(ProcessName);
-    //    processBuilder.AddStepFromType<TestStep>(StepName);
+        // Act
+        Assert.Throws<NotImplementedException>(() => map.GetFunctionMetadataMap());
+    }
 
-    //    // Act
-    //    var kernelProcess = processBuilder.Build();
+    /// <summary>
+    /// Verify <see cref="ProcessMapBuilder.BuildStep"/> produces the
+    /// expected <see cref="KernelProcessMap"/>.
+    /// </summary>
+    [Fact]
+    public void ProcessMapBuilderWillBuild()
+    {
+        // Arrange
+        ProcessStepBuilder<SimpleTestStep> step = new($"One{nameof(SimpleTestStep)}");
+        ProcessMapBuilder map = new(step);
 
-    //    // Assert
-    //    Assert.NotNull(kernelProcess);
-    //    Assert.Equal(ProcessName, kernelProcess.State.Name);
-    //    Assert.Single(kernelProcess.Steps);
-    //}
+        // Act
+        KernelProcessStepInfo processMap = map.BuildStep();
 
-    private sealed class TestStep : KernelProcessStep<TestState>
+        // Assert
+        Assert.NotNull(processMap);
+        Assert.IsType<KernelProcessMap>(processMap);
+        Assert.Equal(map.Name, processMap.State.Name);
+        Assert.Equal(map.Id, processMap.State.Id);
+    }
+
+    /// <summary>
+    /// Verify <see cref="ProcessMapBuilder.BuildStep"/> throws an exception
+    /// if the target does not accept any parameters.
+    /// </summary>
+    [Fact]
+    public void ProcessMapBuilderFailsBuildForInvalidTarget()
+    {
+        // Arrange
+        ProcessStepBuilder<InvalidTestStep> step = new($"One{nameof(InvalidTestStep)}");
+        ProcessMapBuilder map = new(step);
+
+        // Act & Assert
+        Assert.Throws<KernelException>(() => map.BuildStep());
+    }
+
+    /// <summary>
+    /// Verify <see cref="ProcessMapBuilder.BuildStep"/> throws an exception
+    /// if the target does not accept any parameters.
+    /// </summary>
+    [Fact]
+    public void ProcessMapBuilderRequiresFunctionTarget()
+    {
+        // Arrange
+        ProcessStepBuilder<ComplexTestStep> step = new($"One{nameof(ComplexTestStep)}");
+        ProcessMapBuilder map = new(step);
+
+        // Act & Assert
+        Assert.Throws<KernelException>(() => map.BuildStep());
+
+        // Arrange (add target)
+        map.ForTarget(nameof(ComplexTestStep.TestFunctionA));
+
+        // Act
+        KernelProcessStepInfo processMap = map.BuildStep();
+
+        // Assert
+        Assert.NotNull(processMap);
+    }
+
+    private sealed class SimpleTestStep : KernelProcessStep<TestState>
     {
         private TestState? _state;
 
@@ -138,6 +193,48 @@ public class ProcessMapBuilderTests
 
         [KernelFunction]
         public void TestFunction(Guid value)
+        {
+            Assert.NotNull(this._state);
+        }
+    }
+
+    private sealed class InvalidTestStep : KernelProcessStep<TestState>
+    {
+        private TestState? _state;
+
+        public override ValueTask ActivateAsync(KernelProcessStepState<TestState> state)
+        {
+            this._state = state.State;
+
+            return ValueTask.CompletedTask;
+        }
+
+        [KernelFunction]
+        public void TestFunction()
+        {
+            Assert.NotNull(this._state);
+        }
+    }
+
+    private sealed class ComplexTestStep : KernelProcessStep<TestState>
+    {
+        private TestState? _state;
+
+        public override ValueTask ActivateAsync(KernelProcessStepState<TestState> state)
+        {
+            this._state = state.State;
+
+            return ValueTask.CompletedTask;
+        }
+
+        [KernelFunction]
+        public void TestFunctionA(Guid value)
+        {
+            Assert.NotNull(this._state);
+        }
+
+        [KernelFunction]
+        public void TestFunctionB(Guid value)
         {
             Assert.NotNull(this._state);
         }
