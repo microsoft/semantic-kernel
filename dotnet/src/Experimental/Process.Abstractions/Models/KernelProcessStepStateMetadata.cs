d2696a91cbc8e6198372a87df3f0d16b197a3cee
Estefanía Tenorio <8483207+esttenorio@users.noreply.github.com>
2024-10-28 09:53:28 -0700

    .Net Processes: State Management in Processes Part 1  (#9424)
    ### Description
    This is the first one of a few PR that address the State Management in
    SK Processes. Dividing in smaller PRs since feature contain multiple big
    components.
    
    #### In this PR
    - **SAVE:** After running a Process, adding capability to extract
    process state for stateful processes - works also with nested processes
    - **LOAD:** from a Process State data structure, loading state to
    ProcessBuilder before `Build()` to apply it to Process and use it on
    runtime. For now only focusing on Step State only, not focusing on exact
    replica of processes (using same ids)
    - **Samples:** Adding more samples in Step02_FoodPrep that load existing
    State from file and use it to run specific food pre processes, also
    samples on how to save the state
    
    #### Out of Scope but coming up in future PRs
    - How to deal with processes running that have the same id (processid,
    stepid)
    - In the future, ProcessStep State will have versions, so if loading an
    old state to a process with a new state version there is:
        - validation
        - migration strategy placeholder to add custom state migration
    - A new way to create ProcessBuilders so that on save, processTypeIds
    can be interpreted instead of needing to pass all steps, and edges when
    loading a ProcessBuilder from file - current DAPR implementation
    - More samples showcasing more complex scenarios
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [x] The code builds clean without any errors or warnings
    - [x] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [x] All unit tests pass, and I have added new tests where possible
    - [x] I didn't break anyone :smile:

---
+++ dotnet/src/Experimental/Process.Abstractions/Models/KernelProcessStepStateMetadata.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace Microsoft.SemanticKernel.Process.Models;
+
+/// <summary>
+/// Step state used for State Persistence serialization
+/// </summary>
+public record class KernelProcessStepStateMetadata
+{
+    /// <summary>
+    /// The identifier of the Step which is required to be unique within an instance of a Process.
+    /// This may be null until a process containing this step has been invoked.
+    /// </summary>
+    [DataMember]
+    [JsonPropertyName("id")]
+    public string? Id { get; init; }
+
+    /// <summary>
+    /// The name of the Step. This is intended to be human readable and is not required to be unique. If
+    /// not provided, the name will be derived from the steps .NET type.
+    /// </summary>
+    [DataMember]
+    [JsonPropertyName("name")]
+    public string? Name { get; init; }
+
+    /// <summary>
+    /// Version of the state that is stored. Used for validation and versioning
+    /// purposes when reading a state and applying it to a ProcessStepBuilder/ProcessBuilder
+    /// </summary>
+    [DataMember]
+    [JsonPropertyName("versionInfo")]
+    public string? VersionInfo { get; init; } = null;
+}
+
+/// <summary>
+/// Step state used for State Persistence serialization for stateful steps
+/// </summary>
+public record class KernelProcessStepStateMetadata<TState> : KernelProcessStepStateMetadata where TState : class, new()
+{
+    /// <summary>
+    /// The user-defined state object associated with the Step.
+    /// </summary>
+    [DataMember]
+    [JsonPropertyName("state")]
+    public TState? State { get; set; } = null;
+}
