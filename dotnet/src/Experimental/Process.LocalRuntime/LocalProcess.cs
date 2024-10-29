366d1cd5925116e23826471a6d2f3212c29ceab9
Chris Rickman <crickman@microsoft.com>
2024-10-29 14:28:53 -0700

    Typo

--- dotnet/src/Experimental/Process.LocalRuntime/LocalProcess.cs
+++ dotnet/src/Experimental/Process.LocalRuntime/LocalProcess.cs
@@ -76,7 +76,7 @@ internal sealed class LocalProcess : LocalStep, IDisposable
     internal async Task RunOnceAsync(KernelProcessEvent? processEvent, Kernel? kernel = null)
     {
         Verify.NotNull(processEvent);
-        await Task.Yield(); // Ensure that the process has an opportunity to run in a different syncronization context.
+        await Task.Yield(); // Ensure that the process has an opportunity to run in a different synchronization context.
         await this._externalEventChannel.Writer.WriteAsync(processEvent).ConfigureAwait(false);
         await this.StartAsync(kernel, keepAlive: false).ConfigureAwait(false);
         await this._processTask!.JoinAsync().ConfigureAwait(false);
