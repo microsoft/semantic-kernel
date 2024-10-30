afa387215540c2e3e65a5986b34c4784f173fe75
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2024-10-30 12:05:38 +0000

    .Net: Update Azure AI Inference to beta.2 (#9467)
    ### Motivation and Context
    
    - Resolve #9466

--- dotnet/src/Connectors/Connectors.AzureAIInference/Settings/AzureAIInferencePromptExecutionSettings.cs
+++ dotnet/src/Connectors/Connectors.AzureAIInference/Settings/AzureAIInferencePromptExecutionSettings.cs
@@ -166,7 +166,7 @@ public sealed class AzureAIInferencePromptExecutionSettings : PromptExecutionSet
     /// <summary>
     /// The available tool definitions that the chat completions request can use, including caller-defined functions.
     /// Please note <see cref="ChatCompletionsToolDefinition"/> is the base class. According to the scenario, a derived class of the base class might need to be assigned here, or this property needs to be casted to one of the possible derived classes.
-    /// The available derived classes include <see cref="ChatCompletionsFunctionToolDefinition"/>.
+    /// The available derived classes include <see cref="ChatCompletionsToolDefinition"/>.
     /// </summary>
     [JsonPropertyName("tools")]
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
