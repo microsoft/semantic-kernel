88d88aa464d8f3cfc4e5a4bcf12d9a27db5d125b
edvan_microsoft <edvan@microsoft.com>
2024-11-01 13:43:44 +0100

    updated inits and lock

--- python/semantic_kernel/data/__init__.py
+++ python/semantic_kernel/data/__init__.py
@@ -5,22 +5,18 @@ from semantic_kernel.data.const import (
     DistanceFunction,
     IndexKind,
 )
-from semantic_kernel.data.filter_clauses.any_tags_equal_to_filter_clause import AnyTagsEqualTo
-from semantic_kernel.data.filter_clauses.equal_to_filter_clause import EqualTo
+from semantic_kernel.data.filter_clauses import AnyTagsEqualTo, EqualTo
 from semantic_kernel.data.kernel_search_results import KernelSearchResults
-from semantic_kernel.data.record_definition.vector_store_model_decorator import vectorstoremodel
-from semantic_kernel.data.record_definition.vector_store_model_definition import VectorStoreRecordDefinition
-from semantic_kernel.data.record_definition.vector_store_record_fields import (
+from semantic_kernel.data.record_definition import (
     VectorStoreRecordDataField,
+    VectorStoreRecordDefinition,
     VectorStoreRecordKeyField,
+    VectorStoreRecordUtils,
     VectorStoreRecordVectorField,
+    vectorstoremodel,
 )
-from semantic_kernel.data.record_definition.vector_store_record_utils import VectorStoreRecordUtils
 from semantic_kernel.data.search_options import SearchOptions
-from semantic_kernel.data.text_search.text_search import TextSearch
-from semantic_kernel.data.text_search.text_search_filter import TextSearchFilter
-from semantic_kernel.data.text_search.text_search_options import TextSearchOptions
-from semantic_kernel.data.text_search.text_search_result import TextSearchResult
+from semantic_kernel.data.text_search import TextSearch, TextSearchFilter, TextSearchOptions, TextSearchResult
 from semantic_kernel.data.vector_search import (
     VectorizableTextSearchMixin,
     VectorizedSearchMixin,
@@ -28,10 +24,8 @@ from semantic_kernel.data.vector_search import (
     VectorSearchFilter,
     VectorSearchOptions,
     VectorSearchResult,
-    VectorSearchType,
 )
-from semantic_kernel.data.vector_storage.vector_store import VectorStore
-from semantic_kernel.data.vector_storage.vector_store_record_collection import VectorStoreRecordCollection
+from semantic_kernel.data.vector_storage import VectorStore, VectorStoreRecordCollection
 
 __all__ = [
     "DEFAULT_DESCRIPTION",
@@ -55,7 +49,6 @@ __all__ = [
     "VectorSearchOptions",
     "VectorSearchResult",
     "VectorSearchResult",
-    "VectorSearchType",
     "VectorStore",
     "VectorStoreRecordCollection",
     "VectorStoreRecordDataField",
--- python/semantic_kernel/data/vector_search/const.py
+++ python/semantic_kernel/data/vector_search/const.py
@@ -4,24 +4,6 @@
 from enum import Enum
 
 
-class VectorSearchType(str, Enum):
-    """Types of vector search queries.
-
-    Attributes:
-        SEARCH_STR_RESULT: Search using a query, with result as strings.
-        SEARCH_TEXT_SEARCH_RESULT: Search using a query, with result as text search results.
-        SEARCH_VECTOR_RESULT: Search using a query, with result as vector search results.
-        VECTORIZED_SEARCH: Search with a vector, with result as vector search results.
-        VECTORIZABLE_TEXT_SEARCH: Search for a vectorizable text, with result as vector search results.
-    """
-
-    SEARCH_STR_RESULT = "search_str_result"
-    SEARCH_TEXT_SEARCH_RESULT = "search_text_search_result"
-    SEARCH_VECTOR_RESULT = "search_vector_result"
-    VECTORIZED_SEARCH = "vectorized_search"
-    VECTORIZABLE_TEXT_SEARCH = "vectorizable_text_search"
-
-
 class TextSearchFunctions(str, Enum):
     """Text search functions.
 
