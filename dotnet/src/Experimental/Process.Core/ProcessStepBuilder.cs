1087d4bf64a4a370a7a9d64a425266fde09440b9
Estefania Tenorio <8483207+esttenorio@users.noreply.github.com>
2024-11-01 18:41:31 -0700

    adding missing namespace

--- dotnet/src/Experimental/Process.Core/ProcessStepBuilder.cs
+++ dotnet/src/Experimental/Process.Core/ProcessStepBuilder.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json;
+using Microsoft.SemanticKernel.Process;
 using Microsoft.SemanticKernel.Process.Internal;
 using Microsoft.SemanticKernel.Process.Models;
 
