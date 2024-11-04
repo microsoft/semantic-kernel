aafebb3a79d04d337f3d41c7b22e63b55917efc9
Chris Rickman <crickman@microsoft.com>
2024-11-04 10:55:24 -0800

    Remove cruft

--- dotnet/src/Experimental/Process.Utilities.UnitTests/ProcessEventTests.cs
+++ dotnet/src/Experimental/Process.Utilities.UnitTests/ProcessEventTests.cs
@@ -40,93 +40,6 @@ public class ProcessEventTests
         this.VerifySerializeEvent(source);
     }
 
-    ///// <summary>
-    ///// Validates that the <see cref="ProcessEvent"/> can be serialized and deserialized correctly.
-    ///// </summary>
-    //[Fact]
-    //public void VerifySerializeEventComplexTest()
-    //{
-    //    // Arrange
-    //    ComplexData data = new() { Id = "test", Value = 33 };
-    //    ProcessEvent source = new KernelProcessEvent { Id = "1", Data = data, Visibility = KernelProcessEventVisibility.Public }.ToProcessEvent("test");
-
-    //    // Act & Assert
-    //    this.VerifySerializeEvent(source);
-    //}
-
-    ///// <summary>
-    ///// Validates that the <see cref="ProcessEvent"/> can be serialized and deserialized correctly.
-    ///// </summary>
-    //[Fact]
-    //public void VerifySerializeEventCollectionTest()
-    //{
-    //    // Arrange
-    //    int[] data = [1, 2, 3, 4, 5];
-    //    ProcessEvent source = new KernelProcessEvent { Id = "1", Data = data, Visibility = KernelProcessEventVisibility.Public }.ToProcessEvent("test");
-
-    //    // Act & Assert
-    //    this.VerifySerializeEvent(source);
-    //}
-
-    //private static readonly JsonSerializerOptions s_options = new() { WriteIndented = true }; // %%% REMOVE
-
-    ///// <summary>
-    ///// Validates that the <see cref="ProcessEvent"/> can be serialized and deserialized correctly.
-    ///// </summary>
-    //[Fact]
-    //public void VerifySerializeCollectionOfEventsTest()
-    //{
-    //    // Arrange
-    //    ComplexData data = new() { Id = "test", Value = 33 };
-    //    ProcessEvent[] processEvents = [
-    //        new KernelProcessEvent { Id = "1", Data = 1 }.ToProcessEvent("test"),
-    //        new KernelProcessEvent { Id = "1", Data = "test"}.ToProcessEvent("test"),
-    //        new KernelProcessEvent { Id = "1", Data = data}.ToProcessEvent("test"),
-    //    ];
-
-    //    // Act
-    //    string json = JsonSerializer.Serialize(processEvents, s_options);
-    //    // Assert
-    //}
-
-    ///// <summary>
-    ///// Validates that the <see cref="ProcessEvent"/> can be serialized and deserialized correctly.
-    ///// </summary>
-    //[Fact]
-    //public void VerifySerializeCollectionOfKernelEventsTest()
-    //{
-    //    // Arrange
-    //    ComplexData data = new() { Id = "test", Value = 33 };
-    //    KernelProcessEvent[] processEvents = [
-    //        new KernelProcessEvent { Id = "1", Data = 1 },
-    //        new KernelProcessEvent { Id = "1", Data = "test"},
-    //        new KernelProcessEvent { Id = "1", Data = data},
-    //    ];
-
-    //    // Act
-    //    string json = JsonSerializer.Serialize(processEvents, s_options);
-    //    // Assert
-    //}
-
-    ///// <summary>
-    ///// Validates that the <see cref="ProcessEvent"/> can be serialized and deserialized correctly.
-    ///// </summary>
-    //[Fact]
-    //public void VerifySerializeCollectionOfMessagesTest()
-    //{
-    //    // Arrange
-    //    ComplexData data = new() { Id = "test", Value = 33 };
-    //    ProcessMessage[] processEvents = [
-    //        new ProcessMessage("testsource", "testdestination", "testfunction", new Dictionary<string, object?>() { { "data", 123 } }),
-    //        new ProcessMessage("testsource", "testdestination", "testfunction", new Dictionary<string, object?>() { { "text", "testext" } }),
-    //        new ProcessMessage("testsource", "testdestination", "testfunction", new Dictionary<string, object?>() { { "data", data } }),
-    //    ];
-
-    //    // Act
-    //    string json = JsonSerializer.Serialize(processEvents, s_options);
-    //    // Assert
-    //}
-
     private void VerifySerializeEvent(ProcessEvent source)
     {
         // Act
@@ -138,18 +51,6 @@ public class ProcessEventTests
         Assert.NotNull(copy1);
         Assert.Equivalent(source, copy1); // record type evaluates logical equality
     }
-
-    //private void VerifySerializeEvent<TData>(ProcessEvent<TData> source)
-    //{
-    //    // Act
-    //    using MemoryStream stream = new();
-    //    source.Serialize(stream);
-    //    ProcessEvent<TData>? copy1 = stream.Deserialize<ProcessEvent<TData>>();
-
-    //    // Assert
-    //    Assert.NotNull(copy1);
-    //    Assert.Equivalent(source, copy1); // record type evaluates logical equality
-    //}
 
     [DataContract]
     internal sealed class ComplexData
