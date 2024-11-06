1a49b74040ecb3098c9b262c4bc7fb063967a445
edvan_microsoft <edvan@microsoft.com>
2024-11-06 17:04:50 +0100

    added search to in-memory connector

--- python/tests/unit/connectors/memory/in_memory/test_in_memory.py
+++ python/tests/unit/connectors/memory/in_memory/test_in_memory.py
@@ -4,6 +4,8 @@ from pytest import fixture, mark
 
 from semantic_kernel.connectors.memory.in_memory.in_memory_collection import InMemoryVectorCollection
 from semantic_kernel.connectors.memory.in_memory.in_memory_store import InMemoryVectorStore
+from semantic_kernel.data.const import DistanceFunction
+from semantic_kernel.data.vector_search.vector_search_options import VectorSearchOptions
 
 
 @fixture
@@ -74,3 +76,44 @@ async def test_delete_collection(collection):
 @mark.asyncio
 async def test_create_collection(collection):
     await collection.create_collection()
+
+
+@mark.asyncio
+async def test_text_search(collection):
+    record = {"id": "testid", "content": "test content", "vector": [0.1, 0.2, 0.3, 0.4, 0.5]}
+    await collection.upsert(record)
+    results = await collection.text_search(search_text="content")
+    assert len([res async for res in results.results]) == 1
+
+
+@mark.asyncio
+@mark.parametrize(
+    "distance_function, expected_similar_score, expected_dissimilar_score",
+    [
+        (DistanceFunction.COSINE_DISTANCE, 0.0, 2.0),
+        (DistanceFunction.COSINE_SIMILARITY, 1.0, -1.0),
+        (DistanceFunction.EUCLIDEAN_DISTANCE, 0.0, 4.47213595499958),
+        (DistanceFunction.MANHATTAN, 0.0, 10.0),
+        (DistanceFunction.EUCLIDEAN_SQUARED_DISTANCE, 0.0, 20.0),
+        (DistanceFunction.DOT_PROD, 5.0, -5.0),
+        (DistanceFunction.HAMMING, 0.0, 1.0),
+    ],
+)
+async def test_vectorized_search_similar(
+    collection, distance_function, expected_similar_score, expected_dissimilar_score
+):
+    collection.data_model_definition.fields["vector"].distance_function = distance_function
+    record = {"id": "testid", "content": "test content", "vector": [1.0, 1.0, 1.0, 1.0, 1.0]}
+    await collection.upsert(record)
+    results = await collection.vectorized_search(
+        vector=[1.0, 1.0, 1.0, 1.0, 1.0], options=VectorSearchOptions(vector_field_name="vector")
+    )
+    async for res in results.results:
+        assert res.score == expected_similar_score
+        assert res.record == record
+    results = await collection.vectorized_search(
+        vector=[-1.0, -1.0, -1.0, -1.0, -1.0], options=VectorSearchOptions(vector_field_name="vector")
+    )
+    async for res in results.results:
+        assert res.score == expected_dissimilar_score
+        assert res.record == record
