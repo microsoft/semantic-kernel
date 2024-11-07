d8c6d9bc58badfefd7b8a2e66cf2cc04836eb194
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2024-11-07 17:53:27 +0000

    Add OllamaFunctionCalling Demonstration

---
+++ dotnet/samples/Demos/OllamaFunctionCalling/Plugins/MyAlarmPlugin.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.ComponentModel;
+using Microsoft.SemanticKernel;
+
+/// <summary>
+/// Class that represents a controllable alarm.
+/// </summary>
+public class MyAlarmPlugin
+{
+    private string hour;
+
+    public MyAlarmPlugin(string providedHour)
+    {
+        this.hour = providedHour;
+    }
+
+    [KernelFunction, Description("Sets an alarm at the provided time")]
+    public string SetAlarm(string time)
+    {
+        this.hour = time;
+        return GetCurrentAlarm();
+    }
+
+    [KernelFunction, Description("Get current alarm set")]
+    public string GetCurrentAlarm()
+    {
+        return $"Alarm set for {hour}";
+    }
+}
