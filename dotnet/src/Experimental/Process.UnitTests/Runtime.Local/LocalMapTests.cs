813a842d837c9824f41d72f69a03d61fc7abdf19
Chris Rickman <crickman@microsoft.com>
2024-11-07 23:40:20 -0800

    Namespace

--- dotnet/src/Experimental/Process.UnitTests/Runtime.Local/LocalMapTests.cs
+++ dotnet/src/Experimental/Process.UnitTests/Runtime.Local/LocalMapTests.cs
@@ -5,7 +5,6 @@ using System.IO;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
 using Xunit;
 
 namespace Microsoft.SemanticKernel.Process.Runtime.Local.UnitTests;
