556aa1e919bc27e9966ea9615165fd51e998892a
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2024-11-13 13:02:33 +0000

    Adding env variable + chatModelId to the constructor.

--- dotnet/src/IntegrationTests/TestSettings/AzureAIInferenceConfiguration.cs
+++ dotnet/src/IntegrationTests/TestSettings/AzureAIInferenceConfiguration.cs
@@ -7,10 +7,10 @@ namespace SemanticKernel.IntegrationTests.TestSettings;
 
 [SuppressMessage("Performance", "CA1812:Internal class that is apparently never instantiated",
     Justification = "Configuration classes are instantiated through IConfiguration.")]
-internal sealed class AzureAIInferenceConfiguration(Uri endpoint, string apiKey, string? serviceId = null)
+internal sealed class AzureAIInferenceConfiguration(Uri endpoint, string apiKey, string? serviceId = null, string? chatModelId = null)
 {
     public Uri Endpoint { get; set; } = endpoint;
     public string? ApiKey { get; set; } = apiKey;
     public string? ServiceId { get; set; } = serviceId;
-    public string? ChatModelId { get; set; }
+    public string? ChatModelId { get; set; } = chatModelId;
 }
