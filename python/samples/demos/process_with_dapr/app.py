13b60a2c853b2ca3447f70b39965e96e73d3e76c
Evan Mattson <evan.mattson@microsoft.com>
2024-11-14 14:41:17 -0500

    Clean up demo sample.

--- python/samples/demos/process_with_dapr/app.py
+++ python/samples/demos/process_with_dapr/app.py
@@ -1,10 +1,7 @@
 # Copyright (c) Microsoft. All rights reserved.
 
-import asyncio
 import logging
 from contextlib import asynccontextmanager
-from enum import Enum
-from typing import TYPE_CHECKING, ClassVar
 
 import uvicorn
 from dapr.actor import ActorId
@@ -12,12 +9,10 @@ from dapr.actor.runtime.context import ActorRuntimeContext
 from dapr.ext.fastapi import DaprActor, DaprApp
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-from pydantic import Field
 
+from samples.demos.process_with_dapr.process.process import get_process
+from samples.demos.process_with_dapr.process.steps import CommonEvents
 from semantic_kernel import Kernel
-from semantic_kernel.functions import kernel_function
-from semantic_kernel.kernel_pydantic import KernelBaseModel
-from semantic_kernel.processes import ProcessBuilder
 from semantic_kernel.processes.dapr_runtime import (
     EventBufferActor,
     ExternalEventBufferActor,
@@ -26,19 +21,23 @@ from semantic_kernel.processes.dapr_runtime import (
     StepActor,
     start,
 )
-from semantic_kernel.processes.kernel_process import (
-    KernelProcessStep,
-    KernelProcessStepContext,
-    KernelProcessStepState,
-)
-
-if TYPE_CHECKING:
-    from semantic_kernel.processes.kernel_process.kernel_process import KernelProcess
 
 logging.basicConfig(level=logging.ERROR)
 
 
 kernel = Kernel()
+
+#########################################################################
+# The following Process and Dapr runtime sample uses a FastAPI app      #
+# to start a process and run steps. The process is defined in the       #
+# process/process.py file and the steps are defined in the steps.py     #
+# file. The process is started by calling the /processes/{process_id}   #
+# endpoint. The actors are registered with the Dapr runtime using       #
+# the DaprActor class. The ProcessActor and the StepActor require a     #
+# kernel dependency to be injected during creation. This is done by     #
+# defining a factory function that takes the kernel as a parameter      #
+# and returns the actor instance with the kernel injected.              #
+#########################################################################
 
 
 # Define a Process Actor Factory that allows a dependency to be injected during Process Actor creation
@@ -57,10 +56,15 @@ def step_actor_factory(ctx: ActorRuntimeContext, actor_id: ActorId) -> StepActor
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     print("## actor startup ##")
+    # The process actor is used to manage the process: it starts, stops, and sends events to the process
     await actor.register_actor(ProcessActor, actor_factory=process_actor_factory)
+    # The step actor is used to run the process steps
     await actor.register_actor(StepActor, actor_factory=step_actor_factory)
+    # The event buffer actor is used to handle incoming events and is used by a step to bubble up events
     await actor.register_actor(EventBufferActor)
+    # The message buffer actor is used to handle incoming messages from edges
     await actor.register_actor(MessageBufferActor)
+    # The external event buffer actor handles incoming events to kick off a process
     await actor.register_actor(ExternalEventBufferActor)
     yield
 
@@ -74,109 +78,6 @@ actor = DaprActor(app)
 @app.get("/healthz")
 async def healthcheck():
     return "Healthy!"
-
-
-class CommonEvents(Enum):
-    """Common events for the sample process."""
-
-    UserInputReceived = "UserInputReceived"
-    CompletionResponseGenerated = "CompletionResponseGenerated"
-    WelcomeDone = "WelcomeDone"
-    AStepDone = "AStepDone"
-    BStepDone = "BStepDone"
-    CStepDone = "CStepDone"
-    StartARequested = "StartARequested"
-    StartBRequested = "StartBRequested"
-    ExitRequested = "ExitRequested"
-    StartProcess = "StartProcess"
-
-
-# Define a sample step that once the `on_input_event` is received,
-# it will emit two events to start the A and B steps.
-class KickOffStep(KernelProcessStep):
-    KICK_OFF_FUNCTION: ClassVar[str] = "kick_off"
-
-    @kernel_function(name=KICK_OFF_FUNCTION)
-    async def print_welcome_message(self, context: KernelProcessStepContext):
-        print("##### Kickoff ran.")
-        await context.emit_event(process_event=CommonEvents.StartARequested.value, data="Get Going A")
-        await context.emit_event(process_event=CommonEvents.StartBRequested.value, data="Get Going B")
-
-
-# Define a sample `AStep` step that will emit an event after 1 second.
-# The event will be sent to the `CStep` step with the data `I did A`.
-class AStep(KernelProcessStep):
-    @kernel_function()
-    async def do_it(self, context: KernelProcessStepContext):
-        print("##### AStep ran.")
-        await asyncio.sleep(1)
-        await context.emit_event(process_event=CommonEvents.AStepDone.value, data="I did A")
-
-
-# Define a sample `BStep` step that will emit an event after 2 seconds.
-# The event will be sent to the `CStep` step with the data `I did B`.
-class BStep(KernelProcessStep):
-    @kernel_function()
-    async def do_it(self, context: KernelProcessStepContext):
-        print("##### BStep ran.")
-        await asyncio.sleep(2)
-        await context.emit_event(process_event=CommonEvents.BStepDone.value, data="I did B")
-
-
-# Define a sample `CStepState` that will keep track of the current cycle.
-class CStepState(KernelBaseModel):
-    current_cycle: int = 1
-
-
-# Define a sample `CStep` step that will emit an `ExitRequested` event after 3 cycles.
-class CStep(KernelProcessStep[CStepState]):
-    state: CStepState = Field(default_factory=CStepState)
-
-    # The activate method overrides the base class method to set the state in the step.
-    async def activate(self, state: KernelProcessStepState[CStepState]):
-        """Activates the step and sets the state."""
-        step_state = CStepState.model_validate(state.state)
-        print(f"##### CStep activated with Cycle = '{step_state.current_cycle}'.")
-        self.state = step_state
-
-    @kernel_function()
-    async def do_it(self, context: KernelProcessStepContext, astepdata: str, bstepdata: str):
-        self.state.current_cycle += 1
-        if self.state.current_cycle >= 3:
-            print("##### CStep run cycle 3 - exiting.")
-            await context.emit_event(process_event=CommonEvents.ExitRequested.value)
-            return
-        print(f"##### CStep run cycle {self.state.current_cycle}")
-        await context.emit_event(process_event=CommonEvents.CStepDone.value)
-
-
-def get_process() -> "KernelProcess":
-    # Define the process builder
-    process = ProcessBuilder(name="ProcessWithDapr")
-
-    # Add the step types to the builder
-    kickoff_step = process.add_step(step_type=KickOffStep)
-    myAStep = process.add_step(step_type=AStep)
-    myBStep = process.add_step(step_type=BStep)
-
-    # Initialize the CStep with an initial state and the state's current cycle set to 1
-    myCStep = process.add_step(step_type=CStep, initial_state=CStepState(current_cycle=1))
-
-    # Define the input event and where to send it to
-    process.on_input_event(event_id=CommonEvents.StartProcess.value).send_event_to(target=kickoff_step)
-
-    # Define the process flow
-    kickoff_step.on_event(event_id=CommonEvents.StartARequested.value).send_event_to(target=myAStep)
-    kickoff_step.on_event(event_id=CommonEvents.StartBRequested.value).send_event_to(target=myBStep)
-    myAStep.on_event(event_id=CommonEvents.AStepDone.value).send_event_to(target=myCStep, parameter_name="astepdata")
-
-    # Define the fan in behavior once both AStep and BStep are done
-    myBStep.on_event(event_id=CommonEvents.BStepDone.value).send_event_to(target=myCStep, parameter_name="bstepdata")
-    myCStep.on_event(event_id=CommonEvents.CStepDone.value).send_event_to(target=kickoff_step)
-    myCStep.on_event(event_id=CommonEvents.ExitRequested.value).stop_process()
-
-    # Build the process
-    return process.build()
 
 
 @app.get("/processes/{process_id}")
