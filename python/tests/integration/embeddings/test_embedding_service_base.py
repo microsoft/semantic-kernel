b0a3184789651927e19ea65cd49d2c76a703720f
Tao Chen <taochen@microsoft.com>
2024-11-18 17:01:33 -0800

    Disable ollama on Windwos runners embedding

--- python/tests/integration/embeddings/test_embedding_service_base.py
+++ python/tests/integration/embeddings/test_embedding_service_base.py
@@ -37,7 +37,7 @@ from semantic_kernel.connectors.ai.open_ai.services.open_ai_text_embedding impor
 from semantic_kernel.connectors.ai.open_ai.settings.azure_open_ai_settings import AzureOpenAISettings
 from semantic_kernel.connectors.ai.prompt_execution_settings import PromptExecutionSettings
 from semantic_kernel.utils.authentication.entra_id_authentication import get_entra_auth_token
-from tests.integration.utils import is_service_setup_for_testing
+from tests.integration.utils import is_service_setup_for_testing, is_test_running_on_supported_platforms
 
 # Make sure all services are setup for before running the tests
 # The following exceptions apply:
@@ -50,7 +50,9 @@ mistral_ai_setup: bool = is_service_setup_for_testing(
 )  # We don't have a MistralAI deployment
 google_ai_setup: bool = is_service_setup_for_testing(["GOOGLE_AI_API_KEY", "GOOGLE_AI_EMBEDDING_MODEL_ID"])
 vertex_ai_setup: bool = is_service_setup_for_testing(["VERTEX_AI_PROJECT_ID", "VERTEX_AI_EMBEDDING_MODEL_ID"])
-ollama_setup: bool = is_service_setup_for_testing(["OLLAMA_EMBEDDING_MODEL_ID"])
+ollama_setup: bool = is_service_setup_for_testing([
+    "OLLAMA_EMBEDDING_MODEL_ID"
+]) and is_test_running_on_supported_platforms(["Linux"])
 
 
 class EmbeddingServiceTestBase:
