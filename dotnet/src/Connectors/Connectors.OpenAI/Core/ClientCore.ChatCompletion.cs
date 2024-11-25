d3d5edc8d7f2d1c74a152f09b6c0604b15586cb8
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2024-11-25 16:32:23 +0000

    Replace stj-schema-mapper source code with M.E.AI schema generation

--- dotnet/src/Connectors/Connectors.OpenAI/Core/ClientCore.ChatCompletion.cs
+++ dotnet/src/Connectors/Connectors.OpenAI/Core/ClientCore.ChatCompletion.cs
@@ -370,7 +370,7 @@ internal partial class ClientCore
 
             // Process function calls by invoking the functions and adding the results to the chat history.
             // Each function call will trigger auto-function-invocation filters, which can terminate the process.
-            // In such cases, we'll return the last message in the chat history.  
+            // In such cases, we'll return the last message in the chat history.
             var lastMessage = await this.FunctionCallsProcessor.ProcessFunctionCallsAsync(
                 chatMessageContent,
                 chatHistory,
@@ -679,8 +679,8 @@ internal partial class ClientCore
         {
             var toolCalls = new List<ChatToolCall>();
 
-            // Handling function calls supplied via either:  
-            // ChatCompletionsToolCall.ToolCalls collection items or  
+            // Handling function calls supplied via either:
+            // ChatCompletionsToolCall.ToolCalls collection items or
             // ChatMessageContent.Metadata collection item with 'ChatResponseMessage.FunctionToolCalls' key.
             IEnumerable<ChatToolCall>? tools = (message as OpenAIChatMessageContent)?.ToolCalls;
             if (tools is null && message.Metadata?.TryGetValue(OpenAIChatMessageContent.FunctionToolCallsProperty, out object? toolCallsObject) is true)
@@ -734,7 +734,7 @@ internal partial class ClientCore
             }
 
             // This check is necessary to prevent an exception that will be thrown if the toolCalls collection is empty.
-            // HTTP 400 (invalid_request_error:) [] should be non-empty - 'messages.3.tool_calls'  
+            // HTTP 400 (invalid_request_error:) [] should be non-empty - 'messages.3.tool_calls'
             if (toolCalls.Count == 0)
             {
                 return [new AssistantChatMessage(message.Content) { ParticipantName = message.AuthorName }];
