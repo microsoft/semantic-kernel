87b857d7b012c2515c33642e4642455da8851783
Estefania Tenorio <estenori@microsoft.com>
2024-11-25 16:30:35 -0800

    fixing input

--- dotnet/samples/GettingStartedWithProcesses/Step02/Steps/TestInputs/UserInputCreditScoreFailureInteractionStep.cs
+++ dotnet/samples/GettingStartedWithProcesses/Step02/Steps/TestInputs/UserInputCreditScoreFailureInteractionStep.cs
@@ -12,7 +12,7 @@ public sealed class UserInputCreditScoreFailureInteractionStep : ScriptedUserInp
     public override void PopulateUserInputs(UserInputState state)
     {
         state.UserInputs.Add("I would like to open an account");
-        state.UserInputs.Add("My name is John Contoso, dob 02/03/1990");
+        state.UserInputs.Add("My name is John Contoso, dob 01/01/1990");
         state.UserInputs.Add("I live in Washington and my phone number es 222-222-1234");
         state.UserInputs.Add("My userId is 987-654-3210");
         state.UserInputs.Add("My email is john.contoso@contoso.com, what else do you need?");
