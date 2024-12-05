dc57cda859cb20f7287b6b365ae91584dd631e14
markwallace-microsoft <127216156+markwallace-microsoft@users.noreply.github.com>
2024-12-05 11:00:54 +0000

    Update comment

--- dotnet/src/Functions/Functions.OpenApi/RestApiParameterFilter.cs
+++ dotnet/src/Functions/Functions.OpenApi/RestApiParameterFilter.cs
@@ -7,6 +7,11 @@ namespace Microsoft.SemanticKernel.Plugins.OpenApi;
 /// <summary>
 /// Represents a delegate for filtering <see cref="RestApiParameter"/> instances.
 /// </summary>
+/// <remarks>
+/// Implementations of this delegate can either return null which will cause the parameter
+/// to be removed from the REST API or return a new instance of <see cref="RestApiParameter"/>
+/// which will replace the original parameter.
+/// </remarks>
 /// <param name="parameter">The REST API parameter to filter.</param>
 /// <param name="operation">The REST API operation</param>
 /// <param name="parent">The parent object of the parameter, can be either an instance of <see cref="RestApiPayload"/> or <see cref="RestApiPayloadProperty"/> or null if the parameter belongs to the operation.</param>
