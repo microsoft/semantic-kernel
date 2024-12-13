fa4c84562be033080937ece12ec342ef3ba0f461
Vincent Biret <vibiret@microsoft.com>
2024-12-13 10:35:53 -0500

    fix: extraneous import
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>

--- dotnet/src/Functions/Functions.OpenApi/OpenApi/OpenApiDocumentParser.cs
+++ dotnet/src/Functions/Functions.OpenApi/OpenApi/OpenApiDocumentParser.cs
@@ -8,7 +8,6 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
-using System.Reflection.Metadata;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Nodes;
