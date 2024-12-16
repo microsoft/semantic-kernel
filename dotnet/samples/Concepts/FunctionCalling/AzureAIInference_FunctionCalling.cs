c40828c09973090f848ff5278f1fd767e392d56a
markwallace-microsoft <127216156+markwallace-microsoft@users.noreply.github.com>
2024-12-16 11:34:47 +0000

    Make tools and stop sequences optional

--- dotnet/samples/Concepts/FunctionCalling/AzureAIInference_FunctionCalling.cs
+++ dotnet/samples/Concepts/FunctionCalling/AzureAIInference_FunctionCalling.cs
@@ -1,10 +1,22 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
 using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.AzureAIInference;
 
 namespace FunctionCalling;
-public class AzureAIInference_FunctionCalling(ITestOutputHelper output) : BaseTest(output)
+public class AzureAIInference_FunctionCalling : BaseTest
 {
+    private readonly LoggingHandler _handler;
+    private readonly HttpClient _httpClient;
+    private bool _isDisposed;
+
+    public AzureAIInference_FunctionCalling(ITestOutputHelper output) : base(output)
+    {
+        // Create a logging handler to output HTTP requests and responses
+        this._handler = new LoggingHandler(new HttpClientHandler(), this.Output);
+        this._httpClient = new(this._handler);
+    }
+
     /// <summary>
     /// This example demonstrates usage of <see cref="FunctionChoiceBehavior.Auto"/> that advertises all kernel functions to the AI model.
     /// </summary>
@@ -12,18 +24,45 @@ public class AzureAIInference_FunctionCalling(ITestOutputHelper output) : BaseTe
     public async Task FunctionCallingAsync()
     {
         var kernel = CreateKernel();
+        AzureAIInferencePromptExecutionSettings settings = new() { FunctionChoiceBehavior = FunctionChoiceBehavior.Auto() };
+        Console.WriteLine(await kernel.InvokePromptAsync("Given the current time of day and weather, what is the likely color of the sky in Boston?", new(settings)));
+    }
+
+    /// <summary>
+    /// This example demonstrates usage of <see cref="FunctionChoiceBehavior.Auto"/> that advertises all kernel functions to the AI model.
+    /// </summary>
+    [Fact]
+    public async Task FunctionCallingWithPromptExecutionSettingsAsync()
+    {
+        var kernel = CreateKernel();
         PromptExecutionSettings settings = new() { FunctionChoiceBehavior = FunctionChoiceBehavior.Auto() };
         Console.WriteLine(await kernel.InvokePromptAsync("Given the current time of day and weather, what is the likely color of the sky in Boston?", new(settings)));
     }
 
-    private static Kernel CreateKernel()
+    protected override void Dispose(bool disposing)
+    {
+        if (!this._isDisposed)
+        {
+            if (disposing)
+            {
+                this._handler.Dispose();
+                this._httpClient.Dispose();
+            }
+
+            this._isDisposed = true;
+        }
+        base.Dispose(disposing);
+    }
+
+    private Kernel CreateKernel()
     {
         // Create kernel
         var kernel = Kernel.CreateBuilder()
             .AddAzureAIInferenceChatCompletion(
                 modelId: TestConfiguration.AzureAIInference.ChatModelId,
                 endpoint: new Uri(TestConfiguration.AzureAIInference.Endpoint),
-                apiKey: TestConfiguration.AzureAIInference.ApiKey)
+                apiKey: TestConfiguration.AzureAIInference.ApiKey,
+                httpClient: this._httpClient)
             .Build();
 
         // Add a plugin with some helper functions we want to allow the model to call.
