504c78e4e1dde94764de1aaeb51a1a257cdf5ff3
Rob Emanuele <rdemanuele@gmail.com>
2025-01-02 13:40:26 -0500

    Add __all__ to __init__.py

--- python/semantic_kernel/connectors/memory/azure_db_for_postgres/__init__.py
+++ python/semantic_kernel/connectors/memory/azure_db_for_postgres/__init__.py
@@ -1,1 +1,11 @@
 # Copyright (c) Microsoft. All rights reserved.
+
+from semantic_kernel.connectors.memory.azure_db_for_postgres.azure_db_for_postgres_collection import (
+    AzureDBForPostgresCollection,
+)
+from semantic_kernel.connectors.memory.azure_db_for_postgres.azure_db_for_postgres_settings import (
+    AzureDBForPostgresSettings,
+)
+from semantic_kernel.connectors.memory.azure_db_for_postgres.azure_db_for_postgres_store import AzureDBForPostgresStore
+
+__all__ = ["AzureDBForPostgresCollection", "AzureDBForPostgresSettings", "AzureDBForPostgresStore"]
