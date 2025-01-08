d22be4fa1f6c362b47c76f0a42db3d3478d15208
Chris Rickman <crickman@microsoft.com>
2025-01-08 15:47:23 -0800

    Namespace

--- dotnet/src/Agents/AzureAI/Internal/AgentMessageFactory.cs
+++ dotnet/src/Agents/AzureAI/Internal/AgentMessageFactory.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Azure.AI.Projects;
-using Microsoft.SemanticKernel.Connectors.FunctionCalling;
 
 namespace Microsoft.SemanticKernel.Agents.AzureAI.Internal;
 
