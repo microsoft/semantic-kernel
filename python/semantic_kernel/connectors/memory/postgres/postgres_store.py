39a855f0b88cfea149a33429e7849727d71138d0
Rob Emanuele <rdemanuele@gmail.com>
2025-01-23 13:41:40 -0500

    Shorten import package paths

--- python/semantic_kernel/connectors/memory/postgres/postgres_store.py
+++ python/semantic_kernel/connectors/memory/postgres/postgres_store.py
@@ -9,9 +9,7 @@ from psycopg_pool import AsyncConnectionPool
 
 from semantic_kernel.connectors.memory.postgres.postgres_collection import PostgresCollection
 from semantic_kernel.connectors.memory.postgres.postgres_memory_store import DEFAULT_SCHEMA
-from semantic_kernel.data.record_definition.vector_store_model_definition import VectorStoreRecordDefinition
-from semantic_kernel.data.vector_storage.vector_store import VectorStore
-from semantic_kernel.data.vector_storage.vector_store_record_collection import VectorStoreRecordCollection
+from semantic_kernel.data import VectorStore, VectorStoreRecordCollection, VectorStoreRecordDefinition
 from semantic_kernel.utils.experimental_decorator import experimental_class
 
 if sys.version_info >= (3, 12):
