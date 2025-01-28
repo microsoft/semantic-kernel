10818c5ac1ab8f272b33d5375988780a0d4e6cf8
Estefanía Tenorio <8483207+esttenorio@users.noreply.github.com>
2025-01-28 11:09:35 -0800

    .Net: SK Process Cloud Events - Publish Interface abstractions scaffolding (#10222)
    ### Motivation and Context
    
    Initial change need for Cloud Event support, in particular the
    publishing events scenario.
    
    - Initial changes need in abstractions layer
    - min changes for Dapr Runtime
    - min changes for Local Runtime
    - UTs in Local and Dapr Runtime to ensure parity
    
    Coming up changes in separate PRs:
    - New abstractions to be used when creating sk ProcessBuilder
    - Additional internal plumbing needed
    - Samples/Demos with usage
    
    Fixes #9721 
    
    ### Description
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [x] The code builds clean without any errors or warnings
    - [x] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [x] All unit tests pass, and I have added new tests where possible
    - [x] I didn't break anyone :smile:

--- dotnet/src/InternalUtilities/process/Abstractions/StepExtensions.cs
+++ dotnet/src/InternalUtilities/process/Abstractions/StepExtensions.cs
@@ -101,9 +101,14 @@ internal static class StepExtensions
     /// <param name="channel">The source channel to evaluate</param>
     /// <param name="functions">A dictionary of KernelFunction instances.</param>
     /// <param name="logger">An instance of <see cref="ILogger"/>.</param>
+    /// <param name="externalMessageChannel">An instance of <see cref="IExternalKernelProcessMessageChannel"/></param>
     /// <returns><see cref="Dictionary{TKey, TValue}"/></returns>
     /// <exception cref="InvalidOperationException"></exception>
-    public static Dictionary<string, Dictionary<string, object?>?> FindInputChannels(this IKernelProcessMessageChannel channel, Dictionary<string, KernelFunction> functions, ILogger? logger)
+    public static Dictionary<string, Dictionary<string, object?>?> FindInputChannels(
+        this IKernelProcessMessageChannel channel,
+        Dictionary<string, KernelFunction> functions,
+        ILogger? logger,
+        IExternalKernelProcessMessageChannel? externalMessageChannel = null)
     {
         if (functions is null)
         {
@@ -126,7 +131,7 @@ internal static class StepExtensions
                 // and are instantiated here.
                 if (param.ParameterType == typeof(KernelProcessStepContext))
                 {
-                    inputs[kvp.Key]![param.Name] = new KernelProcessStepContext(channel);
+                    inputs[kvp.Key]![param.Name] = new KernelProcessStepContext(channel, externalMessageChannel);
                 }
                 else
                 {
