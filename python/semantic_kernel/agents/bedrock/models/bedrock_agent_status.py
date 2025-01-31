59ce3b470ab5b9c95f1a6a65214a8c2f01c0db55
Tao Chen <taochen@microsoft.com>
2025-01-30 16:38:53 -0800

    Fix pipeline 2

--- python/semantic_kernel/agents/bedrock/models/bedrock_agent_status.py
+++ python/semantic_kernel/agents/bedrock/models/bedrock_agent_status.py
@@ -1,13 +1,13 @@
 # Copyright (c) Microsoft. All rights reserved.
 
 
-from enum import StrEnum
+from enum import Enum
 
 from semantic_kernel.utils.experimental_decorator import experimental_class
 
 
 @experimental_class
-class BedrockAgentStatus(StrEnum):
+class BedrockAgentStatus(str, Enum):
     """Bedrock Agent Status.
 
     https://docs.aws.amazon.com/bedrock/latest/APIReference/API_agent_PrepareAgent.html#API_agent_PrepareAgent_ResponseElements
