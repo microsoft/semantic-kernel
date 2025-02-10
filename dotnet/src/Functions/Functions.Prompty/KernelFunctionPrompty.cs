4a5b05f1f4cbc448889d777e4e395f9bead6dd52
markwallace-microsoft <127216156+markwallace-microsoft@users.noreply.github.com>
2025-02-10 10:19:57 +0000

    Update prompty support to use prompty.core

--- dotnet/src/Functions/Functions.Prompty/KernelFunctionPrompty.cs
+++ dotnet/src/Functions/Functions.Prompty/KernelFunctionPrompty.cs
@@ -9,6 +9,7 @@ using Microsoft.SemanticKernel.PromptTemplates.Handlebars;
 using Microsoft.SemanticKernel.PromptTemplates.Liquid;
 using Microsoft.SemanticKernel.Prompty.Core;
 using YamlDotNet.Serialization;
+using PromptyCore = Prompty.Core;
 
 namespace Microsoft.SemanticKernel.Prompty;
 
@@ -19,7 +20,7 @@ public static partial class KernelFunctionPrompty
 {
     /// <summary>Default template factory to use when none is provided.</summary>
     internal static readonly AggregatorPromptTemplateFactory s_defaultTemplateFactory =
-        new(new LiquidPromptTemplateFactory(), new HandlebarsPromptTemplateFactory());
+        new(new LiquidPromptTemplateFactory(), new HandlebarsPromptTemplateFactory(), new KernelPromptTemplateFactory());
 
     private const string PromptyPattern = /* lang=regex */ """
         ^---\s*$\n      # Start of YAML front matter, a line beginning with "---" followed by optional whitespace
@@ -67,7 +68,7 @@ public static partial class KernelFunctionPrompty
     /// <returns>The created <see cref="PromptTemplateConfig"/>.</returns>
     /// <exception cref="ArgumentNullException"><paramref name="promptyTemplate"/> is null.</exception>
     /// <exception cref="ArgumentException"><paramref name="promptyTemplate"/> is empty or composed entirely of whitespace.</exception>
-    public static PromptTemplateConfig ToPromptTemplateConfig(string promptyTemplate)
+    public static PromptTemplateConfig ToPromptTemplateConfigOld(string promptyTemplate)
     {
         Verify.NotNullOrWhiteSpace(promptyTemplate);
 
@@ -114,6 +115,8 @@ public static partial class KernelFunctionPrompty
 
         var prompty = new DeserializerBuilder().Build().Deserialize<PromptyYaml>(header) ??
             throw new ArgumentException("Invalid prompty template. Header could not be parsed.");
+
+        //var prompty = Prompty.Core.Prompty.Load();
 
         // Step 2:
         // Create a prompt template config from the prompty data.
@@ -211,6 +214,71 @@ public static partial class KernelFunctionPrompty
         // Step 4:
         // Update template format. If not provided, use Liquid as default.
         promptTemplateConfig.TemplateFormat = prompty.Template ?? LiquidPromptTemplateFactory.LiquidTemplateFormat;
+
+        return promptTemplateConfig;
+    }
+
+    /// <summary>
+    /// Create a <see cref="PromptTemplateConfig"/> from a prompty template.
+    /// </summary>
+    /// <param name="promptyTemplate">Prompty representation of a prompt-based <see cref="KernelFunction"/>.</param>
+    /// <returns>The created <see cref="PromptTemplateConfig"/>.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="promptyTemplate"/> is null.</exception>
+    /// <exception cref="ArgumentException"><paramref name="promptyTemplate"/> is empty or composed entirely of whitespace.</exception>
+    public static PromptTemplateConfig ToPromptTemplateConfig(string promptyTemplate)
+    {
+        Verify.NotNullOrWhiteSpace(promptyTemplate);
+
+        PromptyCore.Prompty prompty = PromptyCore.Prompty.Load(promptyTemplate, []);
+
+        var promptTemplateConfig = new PromptTemplateConfig
+        {
+            Name = prompty.Name,
+            Description = prompty.Description,
+            Template = prompty.Content.ToString() ?? string.Empty,
+        };
+
+        PromptExecutionSettings? defaultExecutionSetting = null;
+        if (prompty.Model?.Parameters?.Items is not null && prompty.Model.Parameters.Items.Count > 0)
+        {
+            defaultExecutionSetting = new PromptExecutionSettings()
+            {
+                ServiceId = prompty.Model.Parameters.Items.TryGetValue("service_id", out var serviceId) && serviceId is string serviceIdStr ? serviceIdStr : null,
+                ModelId = prompty.Model.Parameters.Items.TryGetValue("model_id", out var modelId) && modelId is string modelIdStr ? modelIdStr : null,
+                ExtensionData = prompty.Model.Parameters.Items
+            };
+            promptTemplateConfig.AddExecutionSettings(defaultExecutionSetting);
+        }
+
+        // Add input and output variables.
+        if (prompty.Inputs is not null)
+        {
+            foreach (var input in prompty.Inputs)
+            {
+                if (input.Items.TryGetValue("name", out var value) && value is string name)
+                {
+                    string description = input.Items.TryGetValue("description", out var desc) && desc is string descStr ? descStr : string.Empty;
+                    promptTemplateConfig.InputVariables.Add(new()
+                    {
+                        Name = name,
+                        Description = description,
+                    });
+                }
+            }
+        }
+        if (prompty.Outputs is not null)
+        {
+            // PromptTemplateConfig supports only a single output variable. If the prompty template
+            // contains one and only one, use it. Otherwise, ignore any outputs.
+            if (prompty.Outputs.Length == 1 &&
+                prompty.Outputs[0].Items.TryGetValue("description", out var value) && value is string description)
+            {
+                promptTemplateConfig.OutputVariable = new() { Description = description };
+            }
+        }
+
+        // Update template format. If not provided, use Liquid as default.
+        promptTemplateConfig.TemplateFormat = prompty.Template?.Type ?? LiquidPromptTemplateFactory.LiquidTemplateFormat;
 
         return promptTemplateConfig;
     }
