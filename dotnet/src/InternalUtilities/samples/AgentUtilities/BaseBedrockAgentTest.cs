5f1279d118a154f1f06259aaed758e76824d6f9c
Tao Chen <taochen@microsoft.com>
2025-02-11 14:53:14 -0800

    Formatting

--- dotnet/src/InternalUtilities/samples/AgentUtilities/BaseBedrockAgentTest.cs
+++ dotnet/src/InternalUtilities/samples/AgentUtilities/BaseBedrockAgentTest.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft. All rights reserved.
+﻿// Copyright (c) Microsoft. All rights reserved.
 
 using Amazon.BedrockAgent.Model;
 using Microsoft.SemanticKernel.Agents.Bedrock;
