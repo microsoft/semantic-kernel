d9f0ee0f324db7f10260aa621fabec28881d5c99
westey <164392973+westey-m@users.noreply.github.com>
2025-02-13 17:31:17 +0000

    .Net: Add Azure CosmosDB NoSQL Hybrid Keyword Search (#10524)
    ### Motivation and Context
    
    #9399 
    
    ### Description
    
    - Add an azure ComsosDB NoSQL hybrid search implementation using
    keywords
    - Add integration tests and update unit tests.
    - The required features to implement this is only available in preview
    versions of the SDK right now.
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [x] The code builds clean without any errors or warnings
    - [x] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [x] All unit tests pass, and I have added new tests where possible
    - [x] I didn't break anyone :smile:

---
+++ dotnet/src/IntegrationTests/Connectors/Memory/AzureCosmosDBNoSQL/AzureCosmosDBNoSQLConnectionStringSetConditionAttribute.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Threading.Tasks;
+using SemanticKernel.IntegrationTests.Connectors.Memory.Xunit;
+
+namespace SemanticKernel.IntegrationTests.Connectors.Memory.AzureCosmosDBNoSQL;
+
+/// <summary>
+/// Attribute to use to skip tests if the connection string for CosmosDB NoSQL is not set.
+/// </summary>
+[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+public sealed class AzureCosmosDBNoSQLConnectionStringSetConditionAttribute : Attribute, ITestCondition
+{
+    public ValueTask<bool> IsMetAsync()
+    {
+        var isMet = AzureCosmosDBNoSQLVectorStoreFixture.GetConnectionString() is not null;
+
+        return ValueTask.FromResult(isMet);
+    }
+
+    public string SkipReason
+        => $"CosmosDB NoSQL connection string was not specified in user secrets. Use the following command to set it: dotnet user-secrets set \"{AzureCosmosDBNoSQLVectorStoreFixture.ConnectionStringKey}\" \"your_connection_string\"";
+}
