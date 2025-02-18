11d3d51e5dd059c577c85f04b8e9d57a04311b1c
Tao Chen <taochen@microsoft.com>
2025-02-18 11:46:19 -0800

    .Net Bedrock agent with tests, integration tests init

---
+++ dotnet/src/IntegrationTests/Agents/BedrockAgentTests.cs
@@ -0,0 +1,87 @@
+// Copyright (c) Microsoft. All rights reserved.
+
+using System.Threading.Tasks;
+using Amazon.BedrockAgent.Model;
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel.Agents.Bedrock;
+using SemanticKernel.IntegrationTests.TestSettings;
+using Xunit;
+
+namespace SemanticKernel.IntegrationTests.Agents;
+
+#pragma warning disable xUnit1004 // Contains test methods used in manual verification. Disable warning for this file only.
+
+public sealed class BedrockAgentTests
+{
+    private readonly IConfigurationRoot _configuration = new ConfigurationBuilder()
+        .AddJsonFile(path: "testsettings.json", optional: true, reloadOnChange: true)
+        .AddJsonFile(path: "testsettings.development.json", optional: true, reloadOnChange: true)
+        .AddEnvironmentVariables()
+        .AddUserSecrets<BedrockAgentTests>()
+        .Build();
+
+    /// <summary>
+    /// Integration test for creating a <see cref="BedrockAgent"/>.
+    /// </summary>
+    // [Theory(Skip = "This test is for manual verification.")]
+    [Theory()]
+    [InlineData("Why is the sky blue in one sentence?")]
+    public async Task InvokeTestAsync(string input)
+    {
+        var agent = await BedrockAgent.CreateAsync(this.GetCreateAgentRequest());
+        try
+        {
+            await this.ExecuteAgentAsync(agent, input);
+        }
+        finally
+        {
+            await agent.DeleteAsync(default);
+        }
+    }
+
+    /// <summary>
+    /// Executes a <see cref="BedrockAgent"/> with the specified input and expected output.
+    /// The output of the agent will be verified against the expected output.
+    /// If the expected output is not provided, the verification will pass as long as the output is not null or empty.
+    /// </summary>
+    /// <param name="agent">The agent to execute.</param>
+    /// <param name="input">The input to provide to the agent.</param>
+    /// <param name="expected">The expected output from the agent.</param>
+    private async Task ExecuteAgentAsync(BedrockAgent agent, string input, string? expected = null)
+    {
+        var responses = agent.InvokeAsync(BedrockAgent.CreateSessionId(), input, null, default);
+        string responseContent = string.Empty;
+        await foreach (var response in responses)
+        {
+            // Non-streaming invoke will only return one response.
+            responseContent = response.Content ?? string.Empty;
+        }
+
+        if (expected != null)
+        {
+            Assert.Contains(expected, responseContent);
+        }
+        else
+        {
+            Assert.False(string.IsNullOrEmpty(responseContent));
+        }
+    }
+
+    private const string AgentName = "SKIntegrationTestAgent";
+    private const string AgentDescription = "A helpful assistant who helps users find information.";
+    private const string AgentInstruction = "You're a helpful assistant who helps users find information.";
+    private CreateAgentRequest GetCreateAgentRequest()
+    {
+        BedrockAgentConfiguration bedrockAgentSettings = this._configuration.GetSection("BedrockAgent").Get<BedrockAgentConfiguration>()!;
+        Assert.NotNull(bedrockAgentSettings);
+
+        return new()
+        {
+            AgentName = AgentName,
+            Description = AgentDescription,
+            Instruction = AgentInstruction,
+            AgentResourceRoleArn = bedrockAgentSettings.AgentResourceRoleArn,
+            FoundationModel = bedrockAgentSettings.FoundationModel,
+        };
+    }
+}
