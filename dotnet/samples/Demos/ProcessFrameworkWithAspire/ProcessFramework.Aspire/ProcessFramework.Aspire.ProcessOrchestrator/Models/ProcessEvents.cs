4c91cfdbfe4d97983156b032713556f07be210dd
Tommaso Stocchi <tstocchi@microsoft.com>
2025-02-21 21:57:33 +0100

    .Net: Adds Process Framework with Aspire demo (#10614)
    ### Motivation and Context
    
    This demo aims to showcase how the [Semantic Kernel Process
    Framework](https://learn.microsoft.com/en-us/semantic-kernel/overview/)
    can be used with [.NET
    Aspire](https://learn.microsoft.com/en-us/dotnet/aspire/get-started/aspire-overview).
    The Process Framework allows developers to create business processes
    based on events. Each step of the process can be an agent or native
    code.
    
    ### Description
    
    In this demo, I have defined the agents as external services, and each
    step will call these agents using HTTP requests. This setup allows .NET
    Aspire to add value by tracing the process using OpenTelemetry.
    Additionally, since each agent is a service, they can be restarted as
    needed using the .NET Aspire developer dashboard.
    
    
    ![architecture](https://github.com/user-attachments/assets/e8a570f9-6887-4fa2-8e3d-7a7ec9225c41)
    
    ### Why .NET Aspire?
    
    Thanks to .NET Aspire, developers can leverage the developer dashboard
    to debug in real time the Process and all the agents running. Since the
    agents are single APIs, each can scale independently from each other.
    
    
    ![aspire-dashboard](https://github.com/user-attachments/assets/93a6b043-2b87-4d18-a5e9-929690146443)
    
    ![aspire-metrics](https://github.com/user-attachments/assets/1ae308dd-3be0-465d-a4cb-411efd77fd23)
    
    ![aspire-traces](https://github.com/user-attachments/assets/86fd9da1-d289-48d9-83ad-c90cdfeaf740)
    
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [x] The code builds clean without any errors or warnings
    - [x] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [x] All unit tests pass, and I have added new tests where possible
    - [x] I didn't break anyone :smile:
    
    ---------
    
    Co-authored-by: Chris Rickman <crickman@microsoft.com>
    Co-authored-by: Chris <66376200+crickman@users.noreply.github.com>
    Co-authored-by: Devis Lucato <dluc@users.noreply.github.com>

---
+++ dotnet/samples/Demos/ProcessFrameworkWithAspire/ProcessFramework.Aspire/ProcessFramework.Aspire.ProcessOrchestrator/Models/ProcessEvents.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+namespace ProcessFramework.Aspire.ProcessOrchestrator.Models;
+
+public static class ProcessEvents
+{
+    public static readonly string TranslateDocument = nameof(TranslateDocument);
+    public static readonly string DocumentTranslated = nameof(DocumentTranslated);
+    public static readonly string SummarizeDocument = nameof(SummarizeDocument);
+    public static readonly string DocumentSummarized = nameof(DocumentSummarized);
+}
---
+++ dotnet/samples/Demos/ProcessFrameworkWithAspire/ProcessFramework.Aspire/ProcessFramework.Aspire.ProcessOrchestrator/Program.cs
@@ -0,0 +1,90 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.SemanticKernel;
+using OpenTelemetry;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
+using ProcessFramework.Aspire.ProcessOrchestrator;
+using ProcessFramework.Aspire.ProcessOrchestrator.Models;
+using ProcessFramework.Aspire.ProcessOrchestrator.Steps;
+
+var builder = WebApplication.CreateBuilder(args);
+
+string otelExporterEndpoint = builder.GetConfiguration("OTEL_EXPORTER_OTLP_ENDPOINT");
+string otelExporterHeaders = builder.GetConfiguration("OTEL_EXPORTER_OTLP_HEADERS");
+
+AppContext.SetSwitch("Microsoft.SemanticKernel.Experimental.GenAI.EnableOTelDiagnosticsSensitive", true);
+
+var loggerFactory = LoggerFactory.Create(builder =>
+{
+    // Add OpenTelemetry as a logging provider
+    builder.AddOpenTelemetry(options =>
+    {
+        options.AddOtlpExporter(exporter => { exporter.Endpoint = new Uri(otelExporterEndpoint); exporter.Headers = otelExporterHeaders; exporter.Protocol = OtlpExportProtocol.Grpc; });
+        // Format log messages. This defaults to false.
+        options.IncludeFormattedMessage = true;
+    });
+
+    builder.AddTraceSource("Microsoft.SemanticKernel");
+    builder.SetMinimumLevel(LogLevel.Information);
+});
+
+using var traceProvider = Sdk.CreateTracerProviderBuilder()
+    .AddSource("Microsoft.SemanticKernel*")
+    .AddOtlpExporter(exporter => { exporter.Endpoint = new Uri(otelExporterEndpoint); exporter.Headers = otelExporterHeaders; exporter.Protocol = OtlpExportProtocol.Grpc; })
+    .Build();
+
+using var meterProvider = Sdk.CreateMeterProviderBuilder()
+    .AddMeter("Microsoft.SemanticKernel*")
+    .AddOtlpExporter(exporter => { exporter.Endpoint = new Uri(otelExporterEndpoint); exporter.Headers = otelExporterHeaders; exporter.Protocol = OtlpExportProtocol.Grpc; })
+    .Build();
+
+builder.AddServiceDefaults();
+builder.Services.AddHttpClient<TranslatorAgentHttpClient>(client => { client.BaseAddress = new("https+http://translatoragent"); });
+builder.Services.AddHttpClient<SummaryAgentHttpClient>(client => { client.BaseAddress = new("https+http://summaryagent"); });
+builder.Services.AddSingleton(builder =>
+{
+    var kernelBuilder = Kernel.CreateBuilder();
+
+    kernelBuilder.Services.AddSingleton(builder.GetRequiredService<TranslatorAgentHttpClient>());
+    kernelBuilder.Services.AddSingleton(builder.GetRequiredService<SummaryAgentHttpClient>());
+
+    return kernelBuilder.Build();
+});
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.MapGet("/api/processdoc", async (Kernel kernel) =>
+{
+    var processBuilder = new ProcessBuilder("ProcessDocument");
+    var translateDocumentStep = processBuilder.AddStepFromType<TranslateStep>();
+    var summarizeDocumentStep = processBuilder.AddStepFromType<SummarizeStep>();
+
+    processBuilder
+        .OnInputEvent(ProcessEvents.TranslateDocument)
+        .SendEventTo(new(translateDocumentStep, TranslateStep.Functions.Translate, parameterName: "textToTranslate"));
+
+    translateDocumentStep
+        .OnEvent(ProcessEvents.DocumentTranslated)
+        .SendEventTo(new(summarizeDocumentStep, SummarizeStep.Functions.Summarize, parameterName: "textToSummarize"));
+
+    summarizeDocumentStep
+        .OnEvent(ProcessEvents.DocumentSummarized)
+        .StopProcess();
+
+    var process = processBuilder.Build();
+    using var runningProcess = await process.StartAsync(
+        kernel,
+        new KernelProcessEvent { Id = ProcessEvents.TranslateDocument, Data = "COME I FORNITORI INFLUENZANO I TUOI COSTI Quando scegli un piano di assicurazione sanitaria, uno dei fattori più importanti da considerare è la rete di fornitori in convenzione disponibili con il piano. Northwind Standard offre un'ampia varietà di fornitori in convenzione, tra cui medici di base, specialisti, ospedali e farmacie. Questo ti permette di scegliere un fornitore comodo per te e la tua famiglia, contribuendo al contempo a mantenere bassi i tuoi costi. Se scegli un fornitore in convenzione con il tuo piano, pagherai generalmente copay e franchigie più basse rispetto a un fornitore fuori rete. Inoltre, molti servizi, come l'assistenza preventiva, possono essere coperti senza alcun costo aggiuntivo se ricevuti da un fornitore in convenzione. È importante notare, tuttavia, che Northwind Standard non copre i servizi di emergenza, l'assistenza per la salute mentale e l'abuso di sostanze, né i servizi fuori rete. Questo significa che potresti dover pagare di tasca tua per questi servizi se ricevuti da un fornitore fuori rete. Quando scegli un fornitore in convenzione, ci sono alcuni suggerimenti da tenere a mente. Verifica che il fornitore sia in convenzione con il tuo piano. Puoi confermarlo chiamando l'ufficio del fornitore e chiedendo se è in rete con Northwind Standard. Puoi anche utilizzare lo strumento di ricerca fornitori sul sito web di Northwind Health per verificare la copertura. Assicurati che il fornitore stia accettando nuovi pazienti. Alcuni fornitori potrebbero essere in convenzione ma non accettare nuovi pazienti. Considera la posizione del fornitore. Se il fornitore è troppo lontano, potrebbe essere difficile raggiungere gli appuntamenti. Valuta gli orari dell'ufficio del fornitore. Se lavori durante il giorno, potresti aver bisogno di trovare un fornitore con orari serali o nel fine settimana. Scegliere un fornitore in convenzione può aiutarti a risparmiare sui costi sanitari. Seguendo i suggerimenti sopra e facendo ricerche sulle opzioni disponibili, puoi trovare un fornitore conveniente, accessibile e in rete con il tuo piano Northwind Standard." }
+    );
+
+    return Results.Ok("Process completed successfully");
+});
+
+app.MapDefaultEndpoints();
+
+app.Run();
