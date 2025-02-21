adbe914313e5b7735684d3d7005e28b9cec6522e
markwallace-microsoft <127216156+markwallace-microsoft@users.noreply.github.com>
2025-02-21 20:46:51 +0000

    Fix warning

--- dotnet/src/Agents/Yaml/KernelAgentFactoryYamlExtensions.cs
+++ dotnet/src/Agents/Yaml/KernelAgentFactoryYamlExtensions.cs
@@ -1,6 +1,5 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -14,14 +13,14 @@ public static class KernelAgentFactoryYamlExtensions
     /// <summary>
     /// Create a <see cref="KernelAgent"/> from the given YAML text.
     /// </summary>
-    /// <param name="kernelAgentFactory"></param>
-    /// <param name="kernel"></param>
-    /// <param name="text"></param>
-    /// <param name="cancellationToken"></param>
+    /// <param name="kernelAgentFactory">Kernel agent factory which will be used to create the agent</param>
+    /// <param name="kernel">Kernel instance</param>
+    /// <param name="text">YAML in text format</param>
+    /// <param name="cancellationToken">Optional cancellation token</param>
     public static async Task<KernelAgent?> CreateAgentFromYamlAsync(this KernelAgentFactory kernelAgentFactory, Kernel kernel, string text, CancellationToken cancellationToken = default)
     {
         var agentDefinition = AgentDefinitionYaml.FromYaml(text);
-        agentDefinition.Type = agentDefinition.Type ?? kernelAgentFactory.Types.FirstOrDefault();
+        agentDefinition.Type = agentDefinition.Type ?? (kernelAgentFactory.Types.Count > 0 ? kernelAgentFactory.Types[0] : null);
 
         return await kernelAgentFactory.CreateAsync(
             kernel,
