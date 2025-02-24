bd15c546471eabb484d87d7d5730d2b9a3431271
markwallace-microsoft <127216156+markwallace-microsoft@users.noreply.github.com>
2025-02-24 11:49:30 +0000

    Add getting started sample

---
+++ dotnet/samples/GettingStartedWithAgents/AzureAIAgent/Step08_AzureAIAgent_Declarative.cs
@@ -0,0 +1,81 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+using Azure.AI.Projects;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Agents;
+using Microsoft.SemanticKernel.Agents.AzureAI;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+namespace GettingStarted.AzureAgents;
+
+/// <summary>
+/// This example demonstrates how to declaratively create instances of <see cref="AzureAIAgent"/>.
+/// </summary>
+public class Step08_AzureAIAgent_Declarative : BaseAzureAgentTest
+{
+    public Step08_AzureAIAgent_Declarative(ITestOutputHelper output) : base(output)
+    {
+        var builder = Kernel.CreateBuilder();
+        builder.Services.AddSingleton<AIProjectClient>(this.Client);
+        this._kernel = builder.Build();
+    }
+
+    [Fact]
+    public async Task AzureAIAgentWithCodeInterpreterAsync()
+    {
+        var text =
+            """
+            type: azureai_agent
+            name: AzureAIAgent
+            description: AzureAIAgent Description
+            instructions: AzureAIAgent Instructions
+            model:
+              id: gpt-4o-mini
+            tools:
+                - name: tool1
+                  type: code_interpreter
+            """;
+        AzureAIAgentFactory factory = new();
+
+        var agent = await factory.CreateAgentFromYamlAsync(this._kernel, text) as AzureAIAgent;
+
+        await InvokeAgentAsync(agent, "Use code to determine the values in the Fibonacci sequence that that are less then the value of 101?");
+    }
+
+    #region private
+    private readonly Kernel _kernel;
+
+    /// <summary>
+    /// Invoke the agent with the user input.
+    /// </summary>
+    private async Task InvokeAgentAsync(AzureAIAgent? agent, string input)
+    {
+        // Create a thread for the agent conversation.
+        AgentThread thread = await this.AgentsClient.CreateThreadAsync(metadata: SampleMetadata);
+
+        // Respond to user input
+        try
+        {
+            await InvokeAsync(input);
+        }
+        finally
+        {
+            await this.AgentsClient.DeleteThreadAsync(thread.Id);
+            await this.AgentsClient.DeleteAgentAsync(agent!.Id);
+        }
+
+        // Local function to invoke agent and display the conversation messages.
+        async Task InvokeAsync(string input)
+        {
+            ChatMessageContent message = new(AuthorRole.User, input);
+            await agent!.AddChatMessageAsync(thread.Id, message);
+            this.WriteAgentChatMessage(message);
+
+            await foreach (ChatMessageContent response in agent.InvokeAsync(thread.Id))
+            {
+                this.WriteAgentChatMessage(response);
+            }
+        }
+    }
+    #endregion
+}
