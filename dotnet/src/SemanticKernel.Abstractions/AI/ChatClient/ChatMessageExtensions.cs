d82f7ea40411088a2d15c1e9e6d102a95eae2cdd
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2025-02-26 19:55:38 +0000

    Add suppressions

--- dotnet/src/SemanticKernel.Abstractions/AI/ChatClient/ChatMessageExtensions.cs
+++ dotnet/src/SemanticKernel.Abstractions/AI/ChatClient/ChatMessageExtensions.cs
@@ -1,8 +1,8 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
+using System;
 using Microsoft.Extensions.AI;
 using Microsoft.SemanticKernel.ChatCompletion;
-using System;
 
 namespace Microsoft.SemanticKernel.AI.ChatCompletion;
 
--- dotnet/src/SemanticKernel.Abstractions/AI/PromptExecutionSettingsExtensions.cs
+++ dotnet/src/SemanticKernel.Abstractions/AI/PromptExecutionSettingsExtensions.cs
@@ -1,13 +1,13 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
-using Microsoft.Extensions.AI;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.Linq;
-using System.Text.Json.Serialization.Metadata;
 using System.Text.Json;
-using System;
+using System.Text.Json.Serialization.Metadata;
+using Microsoft.Extensions.AI;
 
 namespace Microsoft.SemanticKernel;
 
