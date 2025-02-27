5efad9dbffab20985df3031111128426c742957b
markwallace-microsoft <127216156+markwallace-microsoft@users.noreply.github.com>
2025-02-27 14:55:41 +0000

    Add support for all Azure AI tool types

--- dotnet/src/Agents/AzureAI/Extensions/AgentDefinitionExtensions.cs
+++ dotnet/src/Agents/AzureAI/Extensions/AgentDefinitionExtensions.cs
@@ -11,6 +11,29 @@ namespace Microsoft.SemanticKernel.Agents.AzureAI;
 /// </summary>
 internal static class AgentDefinitionExtensions
 {
+    private const string AzureAISearchType = "azure_ai_search";
+    private const string AzureFunctionType = "azure_function";
+    private const string BingGroundingType = "bing_grounding";
+    private const string CodeInterpreterType = "code_interpreter";
+    private const string FileSearchType = "file_search";
+    private const string FunctionType = "function";
+    private const string MicrosoftFabricType = "fabric_aiskill";
+    private const string OpenApiType = "openapi";
+    private const string SharepointGroundingType = "sharepoint_grounding";
+
+    private static readonly string[] s_validToolTypes = new string[]
+    {
+        AzureAISearchType,
+        AzureFunctionType,
+        BingGroundingType,
+        CodeInterpreterType,
+        FileSearchType,
+        FunctionType,
+        MicrosoftFabricType,
+        OpenApiType,
+        SharepointGroundingType
+    };
+
     /// <summary>
     /// Return the Azure AI tool definitions which corresponds with the provided <see cref="AgentDefinition"/>.
     /// </summary>
@@ -18,15 +41,22 @@ internal static class AgentDefinitionExtensions
     /// <exception cref="InvalidOperationException"></exception>
     public static IEnumerable<ToolDefinition> GetAzureToolDefinitions(this AgentDefinition agentDefinition)
     {
-        return agentDefinition.Tools?.Select<AgentToolDefinition, ToolDefinition>(tool =>
+        return agentDefinition.Tools.Select<AgentToolDefinition, ToolDefinition>(tool =>
         {
             return tool.Type switch
             {
-                "code_interpreter" => new CodeInterpreterToolDefinition(),
-                "file_search" => new FileSearchToolDefinition(),
-                _ => throw new NotSupportedException($"Unable to create Azure AI tool definition because of unsupported tool type: {tool.Type}"),
+                AzureAISearchType => CreateAzureAISearchToolDefinition(tool),
+                AzureFunctionType => CreateAzureFunctionToolDefinition(tool),
+                BingGroundingType => CreateBingGroundingToolDefinition(tool),
+                CodeInterpreterType => CreateCodeInterpreterToolDefinition(tool),
+                FileSearchType => CreateFileSearchToolDefinition(tool),
+                FunctionType => CreateFunctionToolDefinition(tool),
+                MicrosoftFabricType => CreateMicrosoftFabricToolDefinition(tool),
+                OpenApiType => CreateOpenApiToolDefinition(tool),
+                SharepointGroundingType => CreateSharepointGroundingToolDefinition(tool),
+                _ => throw new NotSupportedException($"Unable to create Azure AI tool definition because of unsupported tool type: {tool.Type}, supported tool types are: {string.Join(",", s_validToolTypes)}"),
             };
-        }) ?? [];
+        });
     }
 
     /// <summary>
@@ -40,4 +70,97 @@ internal static class AgentDefinitionExtensions
         // TODO: Implement
         return null;
     }
+
+    #region private
+    private static AzureAISearchToolDefinition CreateAzureAISearchToolDefinition(AgentToolDefinition tool)
+    {
+        Verify.NotNull(tool);
+
+        return new AzureAISearchToolDefinition();
+    }
+
+    private static AzureFunctionToolDefinition CreateAzureFunctionToolDefinition(AgentToolDefinition tool)
+    {
+        Verify.NotNull(tool);
+        Verify.NotNull(tool.Name);
+        Verify.NotNull(tool.Description);
+
+        string name = tool.Name;
+        string description = tool.Description;
+        AzureFunctionBinding inputBinding = tool.GetInputBinding();
+        AzureFunctionBinding outputBinding = tool.GetOutputBinding();
+        BinaryData parameters = tool.GetParameters();
+
+        return new AzureFunctionToolDefinition(name, description, inputBinding, outputBinding, parameters);
+    }
+
+    private static BingGroundingToolDefinition CreateBingGroundingToolDefinition(AgentToolDefinition tool)
+    {
+        Verify.NotNull(tool);
+
+        ToolConnectionList bingGrounding = tool.GetToolConnectionList();
+
+        return new BingGroundingToolDefinition(bingGrounding);
+    }
+
+    private static CodeInterpreterToolDefinition CreateCodeInterpreterToolDefinition(AgentToolDefinition tool)
+    {
+        return new CodeInterpreterToolDefinition();
+    }
+
+    private static FileSearchToolDefinition CreateFileSearchToolDefinition(AgentToolDefinition tool)
+    {
+        Verify.NotNull(tool);
+
+        return new FileSearchToolDefinition()
+        {
+            FileSearch = tool.GetFileSearchToolDefinitionDetails()
+        };
+    }
+
+    private static FunctionToolDefinition CreateFunctionToolDefinition(AgentToolDefinition tool)
+    {
+        Verify.NotNull(tool);
+        Verify.NotNull(tool.Name);
+        Verify.NotNull(tool.Description);
+
+        string name = tool.Name;
+        string description = tool.Description;
+        BinaryData parameters = tool.GetParameters();
+
+        return new FunctionToolDefinition(name, description, parameters);
+    }
+
+    private static MicrosoftFabricToolDefinition CreateMicrosoftFabricToolDefinition(AgentToolDefinition tool)
+    {
+        Verify.NotNull(tool);
+
+        ToolConnectionList fabricAiskill = tool.GetToolConnectionList();
+
+        return new MicrosoftFabricToolDefinition(fabricAiskill);
+    }
+
+    private static OpenApiToolDefinition CreateOpenApiToolDefinition(AgentToolDefinition tool)
+    {
+        Verify.NotNull(tool);
+        Verify.NotNull(tool.Name);
+        Verify.NotNull(tool.Description);
+
+        string name = tool.Name;
+        string description = tool.Description;
+        BinaryData spec = tool.GetSpecification();
+        OpenApiAuthDetails auth = tool.GetOpenApiAuthDetails();
+
+        return new OpenApiToolDefinition(name, description, spec, auth);
+    }
+
+    private static SharepointToolDefinition CreateSharepointGroundingToolDefinition(AgentToolDefinition tool)
+    {
+        Verify.NotNull(tool);
+
+        ToolConnectionList sharepointGrounding = tool.GetToolConnectionList();
+
+        return new SharepointToolDefinition(sharepointGrounding);
+    }
+    #endregion
 }
