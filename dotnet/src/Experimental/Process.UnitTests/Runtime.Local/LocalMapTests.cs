9c67dfc06ba06d276d27bb3f9480ad4fd842af05
Estefania Tenorio <8483207+esttenorio@users.noreply.github.com>
2025-03-04 14:18:19 -0800

    fixing formatting

--- dotnet/src/Experimental/Process.UnitTests/Runtime.Local/LocalMapTests.cs
+++ dotnet/src/Experimental/Process.UnitTests/Runtime.Local/LocalMapTests.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 using SemanticKernel.Process.TestsShared.Steps;
 using Xunit;
