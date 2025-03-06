75b49010499d0fef77215855b94c26b8585ee41b
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2025-03-06 11:08:08 +0000

    DI simplification

--- dotnet/samples/Demos/StructuredDataConnector/Abstractions/StructuredDataService.cs
+++ dotnet/samples/Demos/StructuredDataConnector/Abstractions/StructuredDataService.cs
@@ -8,17 +8,12 @@ namespace StructuredDataConnector.Abstractions;
 /// <summary>
 /// Represents a connection to a data source.
 /// </summary>
-public class StructuredDataService : IDisposable
+public class StructuredDataService<TContext> : IDisposable where TContext : DbContext
 {
     /// <summary>
     /// Gets the database context.
     /// </summary>
-    public DbContext Context { get; }
-
-    /// <summary>
-    /// Gets the list of allowed tables.
-    /// </summary>
-    public List<string> AllowedTables { get; set; } = [];
+    public TContext Context { get; }
 
     private readonly bool _internalContext;
 
@@ -28,7 +23,7 @@ public class StructuredDataService : IDisposable
     /// <param name="connectionString">The connection string.</param>
     public StructuredDataService(string connectionString)
     {
-        this.Context = new DbContext(connectionString);
+        this.Context = (TContext)Activator.CreateInstance(typeof(TContext), connectionString);
         this._internalContext = true;
     }
 
@@ -36,7 +31,7 @@ public class StructuredDataService : IDisposable
     /// Initializes a new instance of the <see cref="StructuredDataService"/> class.
     /// </summary>
     /// <param name="dbContext">The database context.</param>
-    public StructuredDataService(DbContext dbContext)
+    public StructuredDataService(TContext dbContext)
     {
         this.Context = dbContext;
     }
@@ -71,59 +66,4 @@ public class StructuredDataService : IDisposable
         return this.Context.Set<T>()
             .Where(expression);
     }
-
-    /// <summary>
-    /// Searches for entities that match the specified expression.
-    /// </summary>
-    /// <param name="tableName">Table name to search.</param>
-    /// <returns>A sequence of <see cref="Dictionary{TKey, TValue}"/> objects representing the search results.</returns>
-    /// <exception cref="InvalidOperationException">Thrown when the table is not allowed.</exception>
-    public virtual async IAsyncEnumerable<Dictionary<string, object>> SearchByTableAsync(string tableName)
-    {
-        this.EnsureTableIsAllowed(tableName);
-
-        this.Context.Database.Connection.Open();
-        using (var command = this.Context.Database.Connection.CreateCommand())
-        {
-#pragma warning disable CA2100 // Review SQL queries for security vulnerabilities
-            var sql = $"SELECT * FROM {tableName}";
-            command.CommandText = sql;
-#pragma warning restore CA2100 // Review SQL queries for security vulnerabilities
-
-            using (var reader = await command.ExecuteReaderAsync().ConfigureAwait(false))
-            {
-                while (reader.Read())
-                {
-                    var record = new Dictionary<string, object>();
-                    for (int i = 0; i < reader.FieldCount; i++)
-                    {
-                        record[reader.GetName(i)] = reader.GetValue(i);
-                    }
-
-                    yield return record;
-                }
-            }
-        }
-    }
-
-    private void EnsureTableIsAllowed(string tableName)
-    {
-        if (!this.AllowedTables.Contains(tableName))
-        {
-            throw new InvalidOperationException($"Table '{tableName}' is not allowed.");
-        }
-    }
-
-    /*// <summary>
-    /// Perform a search for content related to the specified query and return <see cref="object"/> values representing the search results.
-    /// </summary>
-    /// <param name="query">What to search for.</param>
-    /// <param name="searchOptions">Options used when executing a text search.</param>
-    /// <param name="cancellationToken">The <see cref="CancellationToken"/> to monitor for cancellation requests. The default is <see cref="CancellationToken.None"/>.</param>
-    Task<KernelSearchResults<object>> GetSearchResultsAsync(
-        string query,
-        TextSearchOptions? searchOptions = null,
-        CancellationToken cancellationToken = default);
-
-    */
 }
--- dotnet/samples/Demos/StructuredDataConnector/Program.cs
+++ dotnet/samples/Demos/StructuredDataConnector/Program.cs
@@ -2,6 +2,7 @@
 
 using System.Data.Entity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using StructuredDataConnector.Abstractions;
 
 namespace StructuredDataConnector;
@@ -10,40 +11,35 @@ internal sealed class Program
 {
     internal static async Task Main(string[] args)
     {
-        var _config = new ConfigurationBuilder()
-            .AddUserSecrets<Program>()
-            .Build();
+        var serviceCollection = new ServiceCollection()
+            .AddSingleton<IConfiguration>((sp) => new ConfigurationBuilder().AddUserSecrets<Program>().Build())
+            .AddTransient<MyDbContext>()
+            .AddTransient<StructuredDataService<MyDbContext>>();
 
-        var connectionString = _config["ConnectionStrings:MyDbContext"]!;
+        var serviceProvider = serviceCollection.BuildServiceProvider();
 
-        using var myDbContext = new MyDbContext(connectionString);
-        using var structuredConnector = new StructuredDataService(myDbContext)
-        {
-            AllowedTables = ["Test1"]
-        };
+        using var structuredDataService = serviceProvider.GetRequiredService<StructuredDataService<MyDbContext>>();
 
-        var result = structuredConnector.SearchByEntity<MyEntity>(e => e.Description == "Test");
+        var result = structuredDataService.SearchByEntity<MyEntity>(e => e.Description == "Test");
 
         foreach (var entity in result)
         {
             Console.WriteLine($"Id: {entity.Id}, Description: {entity.Description}");
-        }
-
-        await foreach (var record in structuredConnector.SearchByTableAsync(structuredConnector.AllowedTables[0]).ConfigureAwait(false))
-        {
-            Console.WriteLine($"Id: {record[nameof(MyEntity.Id)]}, Description: {record[nameof(MyEntity.Description)]}");
         }
     }
 }
 
 internal sealed class MyDbContext : DbContext
 {
+    public MyDbContext(IConfiguration config) :
+        base(config[$"ConnectionStrings:{nameof(MyDbContext)}"]!)
+    {
+    }
+
     public MyDbContext(string connectionString)
         : base(connectionString)
     {
     }
-
-    public DbSet<MyEntity>? MyEntities { get; set; }
 
     protected override void OnModelCreating(DbModelBuilder modelBuilder)
     {
