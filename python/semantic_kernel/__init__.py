f3af4931312cdcfaad248e4f32b13db60951f4f6
eavanvalkenburg <github@vanvalkenburg.eu>
2025-03-06 20:30:05 +0100

    rc2

--- python/semantic_kernel/__init__.py
+++ python/semantic_kernel/__init__.py
@@ -4,6 +4,6 @@ from semantic_kernel.kernel import Kernel
 
 __version__ = "1.23.0"
 
-DEFAULT_RC_VERSION = f"{__version__}-rc3"
+DEFAULT_RC_VERSION = f"{__version__}-rc2"
 
 __all__ = ["DEFAULT_RC_VERSION", "Kernel", "__version__"]
