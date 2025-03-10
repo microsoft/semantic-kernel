5d15596ed208199cfe550506028ed259e6387ac8
eavanvalkenburg <github@vanvalkenburg.eu>
2025-03-10 12:03:04 +0100

    fixed tests

--- python/semantic_kernel/connectors/memory/faiss.py
+++ python/semantic_kernel/connectors/memory/faiss.py
@@ -26,15 +26,15 @@ from semantic_kernel.data.record_definition.vector_store_record_fields import Ve
 from semantic_kernel.data.vector_search.vector_search_options import VectorSearchOptions
 from semantic_kernel.data.vector_search.vector_search_result import VectorSearchResult
 from semantic_kernel.data.vector_storage.vector_store import VectorStore
-from semantic_kernel.exceptions.vector_store_exceptions import VectorStoreInitializationException
+from semantic_kernel.exceptions.vector_store_exceptions import (
+    VectorStoreInitializationException,
+    VectorStoreOperationException,
+)
 
 if TYPE_CHECKING:
     from semantic_kernel.data.vector_storage.vector_store_record_collection import VectorStoreRecordCollection
 
 logger = logging.getLogger(__name__)
-
-DEFAULT_IVF_NLIST = 100
-DEFAULT_HNSW_M = 32
 
 
 class FaissCollection(InMemoryVectorCollection[TKey, TModel], Generic[TKey, TModel]):
@@ -44,7 +44,6 @@ class FaissCollection(InMemoryVectorCollection[TKey, TModel], Generic[TKey, TMod
     It adds Faiss Indexes for each of the vector fields.
     """
 
-    enabled_gpu: bool = False
     indexes: MutableMapping[str, faiss.Index] = Field(default_factory=dict)
     indexes_key_map: MutableMapping[str, MutableMapping[TKey, int]] = Field(default_factory=dict)
 
@@ -53,7 +52,6 @@ class FaissCollection(InMemoryVectorCollection[TKey, TModel], Generic[TKey, TMod
         collection_name: str,
         data_model_type: type[TModel],
         data_model_definition: VectorStoreRecordDefinition | None = None,
-        enable_gpu: bool = False,
         **kwargs: Any,
     ):
         """Create a Faiss Collection.
@@ -72,89 +70,81 @@ class FaissCollection(InMemoryVectorCollection[TKey, TModel], Generic[TKey, TMod
             collection_name: The name of the collection.
             data_model_type: The type of the data model.
             data_model_definition: The definition of the data model.
-            enable_gpu: Enable GPU indexes and searches.
             kwargs: Additional arguments.
         """
-        if enable_gpu:
-            try:
-                assert faiss.StandardGpuResources  # nosec
-            except AttributeError:
-                logger.warning("Faiss GPU is not enabled on this system, ignoring enable_gpu.")
-                enable_gpu = False
         super().__init__(
             data_model_type=data_model_type,
             data_model_definition=data_model_definition,
             collection_name=collection_name,
-            enabled_gpu=enable_gpu,
             **kwargs,
         )
 
-    def _create_index(self, field: VectorStoreRecordVectorField, **kwargs: Any) -> faiss.Index:
+    def _create_indexes(self, index: faiss.Index | None = None, indexes: dict[str, faiss.Index] | None = None) -> None:
+        """Create Faiss indexes for each vector field.
+
+        Args:
+            index: The index to use, this can be used when there is only one vector field.
+            indexes: A dictionary of indexes, the key is the name of the vector field.
+        """
+        if len(self.data_model_definition.vector_fields) == 1 and index is not None:
+            if not isinstance(index, faiss.Index):
+                raise VectorStoreInitializationException("Index must be a subtype of faiss.Index")
+            if not index.is_trained:
+                raise VectorStoreInitializationException("Index must be trained before using.")
+            self.indexes[self.data_model_definition.vector_fields[0].name] = index
+            return
+        for vector_field in self.data_model_definition.vector_fields:
+            if indexes and vector_field.name in indexes:
+                if not isinstance(indexes[vector_field.name], faiss.Index):
+                    raise VectorStoreInitializationException(
+                        f"Index for {vector_field.name} must be a subtype of faiss.Index"
+                    )
+                if not indexes[vector_field.name].is_trained:
+                    raise VectorStoreInitializationException(
+                        f"Index for {vector_field.name} must be trained before using."
+                    )
+                self.indexes[vector_field.name] = indexes[vector_field.name]
+                if vector_field.name not in self.indexes_key_map:
+                    self.indexes_key_map.setdefault(vector_field.name, {})
+                continue
+            if vector_field.name not in self.indexes:
+                index = self._create_index(vector_field)
+                self.indexes[vector_field.name] = index
+            if vector_field.name not in self.indexes_key_map:
+                self.indexes_key_map.setdefault(vector_field.name, {})
+
+    def _create_index(self, field: VectorStoreRecordVectorField) -> faiss.Index:
         """Create a Faiss index."""
-        new_index: faiss.Index | None = None
         index_kind = field.index_kind or IndexKind.FLAT
         distance_function = field.distance_function or DistanceFunction.EUCLIDEAN_SQUARED_DISTANCE
         match index_kind:
             case IndexKind.FLAT:
                 match distance_function:
                     case DistanceFunction.EUCLIDEAN_SQUARED_DISTANCE:
-                        new_index = faiss.IndexFlatL2(field.dimensions)
+                        return faiss.IndexFlatL2(field.dimensions)
                     case DistanceFunction.DOT_PROD:
-                        new_index = faiss.IndexFlatIP(field.dimensions)
-                    case DistanceFunction.MANHATTAN:
-                        new_index = faiss.IndexFlat(field.dimensions, faiss.METRIC_L1)
-            case IndexKind.IVF_FLAT:
-                n_list = kwargs.get("n_list", DEFAULT_IVF_NLIST)
-                match distance_function:
-                    case DistanceFunction.EUCLIDEAN_SQUARED_DISTANCE:
-                        new_index = faiss.IndexIVFFlat(
-                            faiss.IndexFlatL2(field.dimensions), field.dimensions, n_list, faiss.METRIC_L2
+                        return faiss.IndexFlatIP(field.dimensions)
+                    case _:
+                        raise VectorStoreInitializationException(
+                            f"Distance function {distance_function} is not supported for index kind {index_kind}."
                         )
-                    case DistanceFunction.DOT_PROD:
-                        new_index = faiss.IndexIVFFlat(
-                            faiss.IndexFlatIP(field.dimensions), field.dimensions, n_list, faiss.METRIC_INNER_PRODUCT
-                        )
-                    case DistanceFunction.MANHATTAN:
-                        new_index = faiss.IndexIVFFlat(
-                            faiss.IndexFlat(field.dimensions, faiss.METRIC_L1),
-                            field.dimensions,
-                            n_list,
-                            faiss.METRIC_L1,
-                        )
-            case IndexKind.HNSW:
-                hnsw_m = kwargs.get("hnsw_m", DEFAULT_HNSW_M)
-                match distance_function:
-                    case DistanceFunction.EUCLIDEAN_SQUARED_DISTANCE:
-                        new_index = faiss.IndexHNSWFlat(field.dimensions, hnsw_m, faiss.METRIC_L2)
-                    case DistanceFunction.DOT_PROD:
-                        new_index = faiss.IndexHNSWFlat(field.dimensions, hnsw_m, faiss.METRIC_INNER_PRODUCT)
-                    case DistanceFunction.MANHATTAN:
-                        new_index = faiss.IndexHNSWFlat(field.dimensions, hnsw_m, faiss.METRIC_L1)
-        if not new_index:
-            raise VectorStoreInitializationException(
-                f"Index with {field.index_kind} and {field.distance_function} is not supported."
-            )
-        if self.enabled_gpu:
-            return faiss.index_cpu_to_gpu(faiss.StandardGpuResources(), 0, new_index)
-        return new_index
+            case _:
+                raise VectorStoreInitializationException(f"Index with {index_kind} is not supported.")
 
     @override
-    async def create_collection(self, **kwargs: Any) -> None:
+    async def create_collection(
+        self, index: faiss.Index | None = None, indexes: dict[str, faiss.Index] | None = None, **kwargs: Any
+    ) -> None:
         """Create a collection.
 
         Create a Faiss index for each vector field.
 
         Args:
-            kwargs: Additional arguments, used for different setups:
-                n_list: The number of lists for the IVF index.
-                hnsw_m: The number of neighbors for the HNSW index.
+            index: The index to use, this can be used when there is only one vector field.
+            indexes: A dictionary of indexes, the key is the name of the vector field.
+            kwargs: Additional arguments.
         """
-        for vector_field in self.data_model_definition.vector_fields:
-            if vector_field.name not in self.indexes:
-                index = self._create_index(vector_field, **kwargs)
-                self.indexes[vector_field.name] = index
-            if vector_field.name not in self.indexes_key_map:
-                self.indexes_key_map.setdefault(vector_field.name, {})
+        self._create_indexes(index=index, indexes=indexes)
 
     @override
     async def _inner_upsert(self, records: Sequence[Any], **kwargs: Any) -> Sequence[TKey]:
@@ -163,8 +153,12 @@ class FaissCollection(InMemoryVectorCollection[TKey, TModel], Generic[TKey, TMod
             vectors_to_add = [record.get(vector_field) for record in records]
             vectors = np.array(vectors_to_add, dtype=np.float32)
             if not self.indexes[vector_field].is_trained:
-                logger.info(f"This index for {vector_field} is not trained, training now.")
-                self.indexes[vector_field].train(vectors)
+                raise VectorStoreOperationException(
+                    f"This index (of type {type(self.indexes[vector_field])}) requires training, "
+                    "which is not supported. To train the index, "
+                    f"use <collection>.indexes[{vector_field}].train, "
+                    "see faiss docs for more details."
+                )
             self.indexes[vector_field].add(vectors)
             start = len(self.indexes_key_map[vector_field])
             for i, record in enumerate(records):
@@ -184,8 +178,11 @@ class FaissCollection(InMemoryVectorCollection[TKey, TModel], Generic[TKey, TMod
 
     @override
     async def delete_collection(self, **kwargs: Any) -> None:
-        self.indexes = {}
-        self.indexes_key_map = {}
+        for vector_field in self.data_model_definition.vector_field_names:
+            if vector_field in self.indexes:
+                del self.indexes[vector_field]
+            if vector_field in self.indexes_key_map:
+                del self.indexes_key_map[vector_field]
         await super().delete_collection(**kwargs)
 
     @override
@@ -200,7 +197,7 @@ class FaissCollection(InMemoryVectorCollection[TKey, TModel], Generic[TKey, TMod
     ) -> KernelSearchResults[VectorSearchResult[TModel]]:
         field = options.vector_field_name or self.data_model_definition.vector_field_names[0]
         assert isinstance(self.data_model_definition.fields.get(field), VectorStoreRecordVectorField)  # nosec
-        if vector:
+        if vector and field:
             return_list = []
             filtered_records = self._get_filtered_records(options)
             np_vector = np.array(vector, dtype=np.float32).reshape(1, -1)
@@ -211,7 +208,6 @@ class FaissCollection(InMemoryVectorCollection[TKey, TModel], Generic[TKey, TMod
                     continue
                 filtered_records[key][IN_MEMORY_SCORE_KEY] = distances[0][i]
                 return_list.append(filtered_records[key])
-
         return KernelSearchResults(
             results=self._get_vector_search_results_from_results(return_list, options),
             total_count=len(return_list) if options and options.include_total_count else None,
