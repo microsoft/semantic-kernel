2ef523345a4c9a7e7b7ac874b491c09951e92d84
westey <164392973+westey-m@users.noreply.github.com>
2025-03-11 18:12:55 +0000

    .Net: March 2025 VectorData updates (#10804)
    ### Motivation and Context
    
    Adding 
    
    ### Description
    
    Adding various features and design improvements for VectorData and the
    associated implementations:
    
    - linq based query filtering
    - linq based property selection
    - hybrid search
    - deprecation of collection factories
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [x] The code builds clean without any errors or warnings
    - [x] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [x] All unit tests pass, and I have added new tests where possible
    - [x] I didn't break anyone :smile:
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Signed-off-by: Vincent Biret <vibiret@microsoft.com>
    Co-authored-by: Shay Rojansky <roji@roji.org>
    Co-authored-by: Evan Mattson <35585003+moonbox3@users.noreply.github.com>
    Co-authored-by: Dmytro Struk <13853051+dmytrostruk@users.noreply.github.com>
    Co-authored-by: Chris <66376200+crickman@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Tao Chen <taochen@microsoft.com>
    Co-authored-by: Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
    Co-authored-by: Mark Wallace <127216156+markwallace-microsoft@users.noreply.github.com>
    Co-authored-by: Estefanía Tenorio <8483207+esttenorio@users.noreply.github.com>
    Co-authored-by: davidpene <d.c.pene@gmail.com>
    Co-authored-by: ThDuquennoy <tduquennoy@chapsvision.com>
    Co-authored-by: Thomas DUQUENNOY <Thomas.DUQUENNOY@sinequa.com>
    Co-authored-by: Eduard van Valkenburg <eavanvalkenburg@users.noreply.github.com>
    Co-authored-by: Evan Mattson <evan.mattson@microsoft.com>
    Co-authored-by: Rob Emanuele <rdemanuele@gmail.com>
    Co-authored-by: K. Andrew Parker <andrew+github@rationarium.org>
    Co-authored-by: SergeyMenshykh <68852919+SergeyMenshykh@users.noreply.github.com>
    Co-authored-by: jenfoxbot <jen.fox@microsoft.com>
    Co-authored-by: Ben Thomas <ben.thomas@microsoft.com>
    Co-authored-by: Ben Thomas <bentho@microsoft.com>
    Co-authored-by: Adit Sheth <shethaadit@gmail.com>
    Co-authored-by: Adit Sheth <adsheth@microsoft.com>
    Co-authored-by: ふぁー <47295014+ymuichiro@users.noreply.github.com>
    Co-authored-by: Vincent Biret <vibiret@microsoft.com>
    Co-authored-by: David A. Torres <10944960+davidatorres@users.noreply.github.com>
    Co-authored-by: Genevieve Warren <24882762+gewarren@users.noreply.github.com>
    Co-authored-by: Atiqur Rahman Foyshal <113086917+atiq-bs23@users.noreply.github.com>
    Co-authored-by: Md. Atiqur Rahman Foyshal <atiqur.foyshal@brainstation-23.com>
    Co-authored-by: Fabian Williams <92543063+fabianwilliams@users.noreply.github.com>
    Co-authored-by: fabian.williams@microsoft.com <fabianwilliams@users.noreply.github.com>
    Co-authored-by: Ram.Type-0 <39725073+RamType0@users.noreply.github.com>
    Co-authored-by: Tommy Falgout <tommy@lastcoolnameleft.com>
    Co-authored-by: Gary Tang <113477176+gtang31-te@users.noreply.github.com>
    Co-authored-by: Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
    Co-authored-by: Tommaso Stocchi <tstocchi@microsoft.com>
    Co-authored-by: Chris Rickman <crickman@microsoft.com>
    Co-authored-by: Devis Lucato <dluc@users.noreply.github.com>
    Co-authored-by: K. Andrew Parker <kparker@citytech.cuny.edu>
    Co-authored-by: Jose Luis Latorre Millas <joslat@gmail.com>
    Co-authored-by: Carsten Lemm <clemm@microsoft.com>
    Co-authored-by: Stephen Toub <stoub@microsoft.com>
    Co-authored-by: Musale Martin <martinmshale@gmail.com>
    Co-authored-by: Ross Smith <ross-p-smith@users.noreply.github.com>
    Co-authored-by: Adam Sitnik <adam.sitnik@gmail.com>

---
+++ dotnet/src/VectorDataIntegrationTests/CosmosNoSQLIntegrationTests/Support/CosmosConnectionStringRequiredAttribute.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using VectorDataSpecificationTests.Xunit;
+
+namespace CosmosNoSQLIntegrationTests.Support;
+
+/// <summary>
+/// Checks whether the sqlite_vec extension is properly installed, and skips the test(s) otherwise.
+/// </summary>
+[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Assembly)]
+public sealed class CosmosConnectionStringRequiredAttribute : Attribute, ITestCondition
+{
+    public ValueTask<bool> IsMetAsync() => new(CosmosNoSQLTestEnvironment.IsConnectionStringDefined);
+
+    public string Skip { get; set; } = "The Cosmos connection string hasn't been configured (AzureCosmosDBNoSQL:ConnectionString).";
+
+    public string SkipReason
+        => this.Skip;
+}
