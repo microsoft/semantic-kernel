d2da97f3ee5fdc9fa014fb6c1393b22e94ad3ddd
westey <164392973+westey-m@users.noreply.github.com>
2025-03-14 11:03:34 +0000

    Switch posgres and sqlserver to preview, and move experimental to only memory store artifacts.

--- dotnet/src/Connectors/Connectors.Memory.Postgres/PostgresMemoryStore.cs
+++ dotnet/src/Connectors/Connectors.Memory.Postgres/PostgresMemoryStore.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -20,6 +21,7 @@ namespace Microsoft.SemanticKernel.Connectors.Postgres;
 /// The embedded data is saved to the Postgres database specified in the constructor.
 /// Similarity search capability is provided through the pgvector extension. Use Postgres's "Table" to implement "Collection".
 /// </remarks>
+[Experimental("SKEXP0020")]
 public class PostgresMemoryStore : IMemoryStore, IDisposable
 {
     internal const string DefaultSchema = "public";
