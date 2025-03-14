17deedb99ee8683d5e4dc06decfa7c5f878cede4
SergeyMenshykh <sergemenshikh@gmail.com>
2025-03-14 15:44:26 +0000

    use depclarative mapping from vector store records to text search results

--- dotnet/samples/Demos/Hosting/Agent/ChatWithAgent.ApiService/Program.cs
+++ dotnet/samples/Demos/Hosting/Agent/ChatWithAgent.ApiService/Program.cs
@@ -166,16 +166,7 @@ public static class Program
         static void AddRagServicesa<TKey>(WebApplicationBuilder builder, RagConfig ragConfig) where TKey : notnull
         {
             // Add a text search implementation that uses the registered vector store record collection for search.
-            builder.Services.AddVectorStoreTextSearch<TextSnippet<TKey>>(
-                new TextSearchStringMapper((result) => (result as TextSnippet<TKey>)!.Text!),
-                new TextSearchResultMapper((result) =>
-                {
-                    // Create a mapping from the Vector Store data type to the data type returned by the Text Search.
-                    // This text search will ultimately be used in a plugin and this TextSearchResult will be returned to the prompt template
-                    // when the plugin is invoked from the prompt template.
-                    var castResult = result as TextSnippet<TKey>;
-                    return new TextSearchResult(value: castResult!.Text!) { Name = castResult.ReferenceDescription };
-                }));
+            builder.Services.AddVectorStoreTextSearch<TextSnippet<TKey>>();
 
             builder.Services.AddSingleton<UniqueKeyGenerator<string>>(new UniqueKeyGenerator<string>(() => Guid.NewGuid().ToString()));
             builder.Services.AddSingleton<IDataLoader, PdfLoader<TKey>>((sp) =>
