fbba9ed134dafa1b546e37ad78dd1db996f7d83b
Estefania Tenorio <8483207+esttenorio@users.noreply.github.com>
2025-03-17 15:22:50 -0700

    fixing format issues

--- dotnet/src/InternalUtilities/process/TestsShared/Services/CounterService.cs
+++ dotnet/src/InternalUtilities/process/TestsShared/Services/CounterService.cs
@@ -2,7 +2,7 @@
 
 namespace SemanticKernel.Process.TestsShared.Services;
 
-internal class CounterService : ICounterService
+internal sealed class CounterService : ICounterService
 {
     internal int _counter = 0;
     public int GetCount()
