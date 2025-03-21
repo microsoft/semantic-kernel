7b41735ffb50c1aa1a535c07102ef0970b8ad9a0
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2025-03-21 16:14:57 +0000

    Added Vision sample with default BaseAddress definition if not provided in the client

---
+++ dotnet/samples/Concepts/ChatCompletion/Ollama_ChatCompletionWithVision.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Resources;
+
+namespace ChatCompletion;
+
+// This example shows how to use GPT Vision model with different content types (text and image).
+public class Ollama_ChatCompletionWithVision(ITestOutputHelper output) : BaseTest(output)
+{
+    [Fact]
+    public async Task LocalImageAsync()
+    {
+        var imageBytes = await EmbeddedResource.ReadAllAsync("sample_image.jpg");
+
+        var kernel = Kernel.CreateBuilder()
+            .AddOllamaChatCompletion("llama3.2-vision")
+            .Build();
+
+        var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+        var chatHistory = new ChatHistory("You are a friendly assistant.");
+
+        chatHistory.AddUserMessage(
+        [
+            new TextContent("What’s in this image?"),
+            new ImageContent(imageBytes, "image/jpg")
+        ]);
+
+        var reply = await chatCompletionService.GetChatMessageContentAsync(chatHistory);
+
+        Console.WriteLine(reply.Content);
+    }
+}
