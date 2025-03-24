90b23397a6aed8f02e4cd4cf1c4ebc37077ff788
SergeyMenshykh <68852919+SergeyMenshykh@users.noreply.github.com>
2025-03-24 19:44:30 +0000

    .Net: Agent Framework With Aspire (#11151)
    ### Motivation, Context and Description
    This PR merges the `feature-agents-hosting` feature branch into the main
    branch.
    
    **Closes** to: https://github.com/microsoft/semantic-kernel/issues/10149
    
    ---------
    
    Co-authored-by: Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>

---
+++ dotnet/samples/Demos/AgentFrameworkWithAspire/ChatWithAgent.Configuration/AzureOpenAIEmbeddingsConfig.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.ComponentModel.DataAnnotations;
+
+namespace ChatWithAgent.Configuration;
+
+/// <summary>
+/// Azure OpenAI embeddings configuration.
+/// </summary>
+public sealed class AzureOpenAIEmbeddingsConfig
+{
+    /// <summary>
+    /// Configuration section name.
+    /// </summary>
+    public const string ConfigSectionName = "AzureOpenAIEmbeddings";
+
+    /// <summary>
+    /// The name of the embeddings deployment.
+    /// </summary>
+    [Required]
+    public string DeploymentName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The name of the embeddings model.
+    /// </summary>
+    public string ModelName { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The embeddings model version.
+    /// </summary>
+    public string ModelVersion { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The SKU name.
+    /// </summary>
+    public string? SkuName { get; set; }
+
+    /// <summary>
+    /// The SKU capacity
+    /// </summary>
+    public int? SkuCapacity { get; set; }
+}
