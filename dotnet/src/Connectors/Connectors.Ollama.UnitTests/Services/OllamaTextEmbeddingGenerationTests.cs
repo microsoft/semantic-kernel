3da76fb3d1aa02fedc5101b78d84b13ed890aa69
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2025-03-24 09:12:57 +0000

    remove using

--- dotnet/src/Connectors/Connectors.Ollama.UnitTests/Services/OllamaTextEmbeddingGenerationTests.cs
+++ dotnet/src/Connectors/Connectors.Ollama.UnitTests/Services/OllamaTextEmbeddingGenerationTests.cs
@@ -5,7 +5,6 @@ using System.IO;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
-using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Embeddings;
 using OllamaSharp;
 using OllamaSharp.Models;
