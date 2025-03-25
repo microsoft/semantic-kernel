ecf94e081635e9205cca9d44f7830ddcbf01e9f4
SergeyMenshykh <sergemenshikh@gmail.com>
2025-03-25 14:12:08 +0000

    sort usings

--- dotnet/src/SemanticKernel.UnitTests/Functions/AIFunctionExtensionsTests.cs
+++ dotnet/src/SemanticKernel.UnitTests/Functions/AIFunctionExtensionsTests.cs
@@ -1,11 +1,11 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
 using System.Collections.Generic;
-using System.Threading.Tasks;
 using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.Extensions.AI;
-using Xunit;
 using Microsoft.SemanticKernel;
+using Xunit;
 
 namespace SemanticKernel.UnitTests.Functions;
 
