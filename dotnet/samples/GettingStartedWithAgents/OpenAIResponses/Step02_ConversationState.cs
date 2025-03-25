cfa1d36150777d45317f1344f918e34f521fd3fd
markwallace-microsoft <127216156+markwallace-microsoft@users.noreply.github.com>
2025-03-25 13:27:02 +0000

    Initial check-in for OpenAIResponsesAgent

---
+++ dotnet/samples/GettingStartedWithAgents/OpenAIResponses/Step02_ConversationState.cs
@@ -0,0 +1,95 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Agents;
+using Microsoft.SemanticKernel.Agents.OpenAI;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+namespace GettingStarted.OpenAIResponses;
+
+/// <summary>
+/// This example demonstrates how to manage conversation state during a model interaction using <see cref="OpenAIResponsesAgent"/>.
+/// OpenAI provides a few ways to manage conversation state, which is important for preserving information across multiple messages or turns in a conversation.
+/// </summary>
+public class Step02_ConversationState(ITestOutputHelper output) : BaseResponsesAgentTest(output)
+{
+    [Fact]
+    public async Task ManuallyConstructPastConversationAsync()
+    {
+        // Define the agent
+        OpenAIResponsesAgent agent = new(this.Client)
+        {
+            StoreEnabled = false,
+        };
+
+        ICollection<ChatMessageContent> messages =
+            [
+            new ChatMessageContent(AuthorRole.User, "knock knock."),
+            new ChatMessageContent(AuthorRole.Assistant, "Who's there?"),
+            new ChatMessageContent(AuthorRole.User, "Orange.")
+            ];
+
+        // Invoke the agent and output the response
+        var responseItems = agent.InvokeAsync(messages);
+        await foreach (ChatMessageContent responseItem in responseItems)
+        {
+            WriteAgentChatMessage(responseItem);
+        }
+    }
+
+    [Fact]
+    public async Task ManuallyManageConversationStateWithResponsesApiAsync()
+    {
+        // Define the agent
+        OpenAIResponsesAgent agent = new(this.Client)
+        {
+            StoreEnabled = false,
+        };
+
+        string[] messages =
+            [
+            "Tell me a joke?",
+            "Tell me another?",
+            ];
+
+        // Invoke the agent and output the response
+        AgentThread? agentThread = null;
+        foreach (string message in messages)
+        {
+            var responseItems = agent.InvokeAsync(new ChatMessageContent(AuthorRole.User, message), agentThread);
+            await foreach (AgentResponseItem<ChatMessageContent> responseItem in responseItems)
+            {
+                agentThread = responseItem.Thread;
+                WriteAgentChatMessage(responseItem.Message);
+            }
+        }
+    }
+
+    [Fact]
+    public async Task ManuallyManageConversationStateWithChatCompletionsApiAsync()
+    {
+        // Define the agent
+        OpenAIResponsesAgent agent = new(this.Client)
+        {
+            StoreEnabled = true,
+        };
+
+        string[] messages =
+            [
+            "Tell me a joke?",
+            "Explain why this is funny.",
+            ];
+
+        // Invoke the agent and output the response
+        AgentThread? agentThread = null;
+        foreach (string message in messages)
+        {
+            var responseItems = agent.InvokeAsync(new ChatMessageContent(AuthorRole.User, message), agentThread);
+            await foreach (AgentResponseItem<ChatMessageContent> responseItem in responseItems)
+            {
+                agentThread = responseItem.Thread;
+                WriteAgentChatMessage(responseItem.Message);
+            }
+        }
+    }
+}
---
+++ dotnet/src/Agents/OpenAI/Extensions/ResponseItemExtensions.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using Microsoft.SemanticKernel.ChatCompletion;
+using OpenAI.Responses;
+
+namespace Microsoft.SemanticKernel.Agents.OpenAI;
+
+internal static class ResponseItemExtensions
+{
+    /// <summary>
+    /// Converts a <see cref="ResponseItem"/> instance to a <see cref="ChatMessageContent"/>.
+    /// </summary>
+    /// <param name="item">The response item to convert.</param>
+    /// <returns>A <see cref="ChatMessageContent"/> instance.</returns>
+    public static ChatMessageContent ToChatMessageContent(this ResponseItem item)
+    {
+        if (item is MessageResponseItem messageResponseItem)
+        {
+            var role = messageResponseItem.Role.ToAuthorRole();
+            var collection = messageResponseItem.Content.ToChatMessageContentItemCollection();
+
+            return new ChatMessageContent(role, collection, innerContent: messageResponseItem);
+        }
+        throw new InvalidOperationException();
+    }
+
+    #region private
+    private static ChatMessageContentItemCollection ToChatMessageContentItemCollection(this IList<ResponseContentPart> content)
+    {
+        var collection = new ChatMessageContentItemCollection();
+        foreach (var part in content)
+        {
+            if (part.Kind == ResponseContentPartKind.OutputText)
+            {
+                collection.Add(new TextContent(part.Text));
+            }
+        }
+        return collection;
+    }
+
+    private static AuthorRole ToAuthorRole(this MessageRole messageRole)
+    {
+        return messageRole switch
+        {
+            MessageRole.Assistant => AuthorRole.Assistant,
+            MessageRole.Developer => AuthorRole.Developer,
+            MessageRole.System => AuthorRole.System,
+            MessageRole.User => AuthorRole.User,
+            _ => new AuthorRole("unknown"),
+        };
+    }
+    #endregion
+}
