c669f74099629db40c281397886ae5d81856e9e4
Stephen Toub <stoub@microsoft.com>
2025-03-25 11:25:36 -0400

    .Net: Update MCP sample with ModelContextProtocol (#11121)
    cc: @markwallace-microsoft, @RogerBarret0 
    
    Closes #10966
    
    ---------
    
    Co-authored-by: SergeyMenshykh <sergemenshikh@gmail.com>

---
+++ dotnet/samples/Demos/ModelContextProtocolPlugin/Program.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using ModelContextProtocol.Client;
+
+var config = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .AddEnvironmentVariables()
+    .Build();
+
+if (config["OpenAI:ApiKey"] is not { } apiKey)
+{
+    Console.Error.WriteLine("Please provide a valid OpenAI:ApiKey to run this sample. See the associated README.md for more details.");
+    return;
+}
+
+// Create an MCPClient for the GitHub server
+await using var mcpClient = await McpClientFactory.CreateAsync(
+    new()
+    {
+        Id = "github",
+        Name = "GitHub",
+        TransportType = "stdio",
+        TransportOptions = new Dictionary<string, string>
+        {
+            ["command"] = "npx",
+            ["arguments"] = "-y @modelcontextprotocol/server-github",
+        }
+    },
+    new() { ClientInfo = new() { Name = "GitHub", Version = "1.0.0" } }).ConfigureAwait(false);
+
+// Retrieve the list of tools available on the GitHub server
+var tools = await mcpClient.GetAIFunctionsAsync().ConfigureAwait(false);
+foreach (var tool in tools)
+{
+    Console.WriteLine($"{tool.Name}: {tool.Description}");
+}
+
+// Prepare and build kernel with the MCP tools as Kernel functions
+var builder = Kernel.CreateBuilder();
+builder.Services
+    .AddLogging(c => c.AddDebug().SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace))
+    .AddOpenAIChatCompletion(
+        serviceId: "openai",
+        modelId: config["OpenAI:ChatModelId"] ?? "gpt-4o-mini",
+        apiKey: apiKey);
+Kernel kernel = builder.Build();
+kernel.Plugins.AddFromFunctions("GitHub", tools.Select(aiFunction => aiFunction.AsKernelFunction()));
+
+// Enable automatic function calling
+OpenAIPromptExecutionSettings executionSettings = new()
+{
+    Temperature = 0,
+    FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+};
+
+// Test using GitHub tools
+var prompt = "Summarize the last four commits to the microsoft/semantic-kernel repository?";
+var result = await kernel.InvokePromptAsync(prompt, new(executionSettings)).ConfigureAwait(false);
+Console.WriteLine($"\n\n{prompt}\n{result}");
