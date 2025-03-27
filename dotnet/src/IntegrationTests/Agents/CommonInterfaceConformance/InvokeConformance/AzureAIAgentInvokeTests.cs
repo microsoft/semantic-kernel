038dddd19a1c1c2267f091185e754378e1dfb649
westey <164392973+westey-m@users.noreply.github.com>
2025-03-27 20:00:49 +0000

    .Net: Add fix to ChatCompletionAgent for function call termination bug (#11235)
    ### Motivation and Context
    
    See code comments for further detail.
    
    ### Description
    
    Add fix to ChatCompletionAgent for function call termination bug
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [ ] The code builds clean without any errors or warnings
    - [ ] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [ ] All unit tests pass, and I have added new tests where possible
    - [ ] I didn't break anyone :smile:

--- dotnet/src/IntegrationTests/Agents/CommonInterfaceConformance/InvokeConformance/AzureAIAgentInvokeTests.cs
+++ dotnet/src/IntegrationTests/Agents/CommonInterfaceConformance/InvokeConformance/AzureAIAgentInvokeTests.cs
@@ -1,16 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
-using System.Threading.Tasks;
-using Xunit;
-
 namespace SemanticKernel.IntegrationTests.Agents.CommonInterfaceConformance.InvokeConformance;
 
 public class AzureAIAgentInvokeTests() : InvokeTests(() => new AzureAIAgentFixture())
 {
-    [Fact]
-    public override Task InvokeWithPluginAndManualInvokeAsync()
-    {
-        // Manual invocation is not currently supported with the Azure AI Agent.
-        return Task.CompletedTask;
-    }
 }
