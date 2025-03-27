b493196ac4ee09a2c4590e18d89bc7031fa38e46
westey <164392973+westey-m@users.noreply.github.com>
2025-03-27 15:06:51 +0000

    Fix formatting.

--- dotnet/src/Agents/Core/ChatCompletionAgent.cs
+++ dotnet/src/Agents/Core/ChatCompletionAgent.cs
@@ -3,7 +3,6 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.Json;
