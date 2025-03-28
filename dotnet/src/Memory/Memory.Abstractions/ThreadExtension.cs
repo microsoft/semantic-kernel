fc6883998a9719f4a0a4ff06c4ade589200a7560
westey <164392973+westey-m@users.noreply.github.com>
2025-03-28 11:29:45 +0000

    Add memory projects with common abstractions and mem zero implementation

---
+++ dotnet/src/Memory/Memory.Abstractions/ThreadExtension.cs
@@ -0,0 +1,75 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.SemanticKernel.Memory;
+
+/// <summary>
+/// Base class for all thread extensions.
+/// </summary>
+public abstract class ThreadExtension
+{
+    /// <summary>
+    /// Called just after a new thread is created.
+    /// </summary>
+    /// <remarks>
+    /// Implementers can use this method to do any operations required at the creation of a new thread.
+    /// For exmple, checking long term storage for any data that is relevant to the current session based on the input text.
+    /// </remarks>
+    /// <param name="threadId">The ID of the new thread.</param>
+    /// <param name="inputText">The input text, typically a user ask.</param>
+    /// <param name="cancellationToken">The <see cref="CancellationToken"/> to monitor for cancellation requests. The default is <see cref="CancellationToken.None"/>.</param>
+    /// <returns>A task that completes when the context has been loaded.</returns>
+    public virtual Task OnThreadCreateAsync(string threadId, string? inputText = default, CancellationToken cancellationToken = default)
+    {
+        return Task.CompletedTask;
+    }
+
+    /// <summary>
+    /// This method is called when a new message has been contributed to the chat by any participant.
+    /// </summary>
+    /// <remarks>
+    /// Inheritors can use this method to update their context based on the new message.
+    /// </remarks>
+    /// <param name="newMessage">The new message.</param>
+    /// <param name="cancellationToken">The <see cref="CancellationToken"/> to monitor for cancellation requests. The default is <see cref="CancellationToken.None"/>.</param>
+    /// <returns>A task that completes when the context has been updated.</returns>
+    public virtual Task OnNewMessageAsync(ChatMessageContent newMessage, CancellationToken cancellationToken = default)
+    {
+        return Task.CompletedTask;
+    }
+
+    /// <summary>
+    /// Called just before a thread is deleted.
+    /// </summary>
+    /// <remarks>
+    /// Implementers can use this method to do any operations required before a thread is deleted.
+    /// For exmple, storing the context to long term storage.
+    /// </remarks>
+    /// <param name="threadId">The id of the thread that will be deleted.</param>
+    /// <param name="cancellationToken">The <see cref="CancellationToken"/> to monitor for cancellation requests. The default is <see cref="CancellationToken.None"/>.</param>
+    /// <returns>A task that completes when the context has been saved.</returns>
+    public virtual Task OnThreadDeleteAsync(string threadId, CancellationToken cancellationToken = default)
+    {
+        return Task.CompletedTask;
+    }
+
+    /// <summary>
+    /// Called just before the AI is invoked
+    /// Implementers can load any additional context required at this time,
+    /// but they should also return any context that should be passed to the AI.
+    /// </summary>
+    /// <param name="newMessage">The most recent message that the AI is being invoked with.</param>
+    /// <param name="cancellationToken">The <see cref="CancellationToken"/> to monitor for cancellation requests. The default is <see cref="CancellationToken.None"/>.</param>
+    /// <returns>A task that completes when the context has been rendered and returned.</returns>
+    public abstract Task<string> OnAIInvocationAsync(ChatMessageContent newMessage, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Register plugins required by this extension component on the provided <see cref="Kernel"/>.
+    /// </summary>
+    /// <param name="kernel">The kernel to register the plugins on.</param>
+    public virtual void RegisterPlugins(Kernel kernel)
+    {
+    }
+}
---
+++ dotnet/src/Memory/Memory.Abstractions/ThreadExtensionsManager.cs
@@ -0,0 +1,88 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.SemanticKernel.Memory;
+
+/// <summary>
+/// A container class for thread extension components that manages their lifecycle and interactions.
+/// </summary>
+public class ThreadExtensionsManager
+{
+    private readonly List<ThreadExtension> _threadExtensions = new();
+
+    /// <summary>
+    /// Gets the list of registered thread extensions.
+    /// </summary>
+    public virtual IReadOnlyList<ThreadExtension> ThreadExtensions => this._threadExtensions;
+
+    /// <summary>
+    /// Registers a new thread extensions.
+    /// </summary>
+    /// <param name="threadExtension">The thread extensions to register.</param>
+    public virtual void RegisterThreadExtension(ThreadExtension threadExtension)
+    {
+        this._threadExtensions.Add(threadExtension);
+    }
+
+    /// <summary>
+    /// Called when a new thread is created.
+    /// </summary>
+    /// <param name="threadId">The ID of the new thread.</param>
+    /// <param name="inputText">The input text, typically a user ask.</param>
+    /// <param name="cancellationToken">The <see cref="CancellationToken"/> to monitor for cancellation requests. The default is <see cref="CancellationToken.None"/>.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    public virtual async Task OnThreadCreateAsync(string threadId, string inputText, CancellationToken cancellationToken = default)
+    {
+        await Task.WhenAll(this.ThreadExtensions.Select(x => x.OnThreadCreateAsync(threadId, inputText, cancellationToken)).ToList()).ConfigureAwait(false);
+    }
+
+    /// <summary>
+    /// Called just before a thread is deleted.
+    /// </summary>
+    /// <param name="threadId">The id of the thread that will be deleted.</param>
+    /// <param name="cancellationToken">The <see cref="CancellationToken"/> to monitor for cancellation requests. The default is <see cref="CancellationToken.None"/>.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    public virtual async Task OnThreadDeleteAsync(string threadId, CancellationToken cancellationToken = default)
+    {
+        await Task.WhenAll(this.ThreadExtensions.Select(x => x.OnThreadDeleteAsync(threadId, cancellationToken)).ToList()).ConfigureAwait(false);
+    }
+
+    /// <summary>
+    /// This method is called when a new message has been contributed to the chat by any participant.
+    /// </summary>
+    /// <param name="newMessage">The new message.</param>
+    /// <param name="cancellationToken">The <see cref="CancellationToken"/> to monitor for cancellation requests. The default is <see cref="CancellationToken.None"/>.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    public virtual async Task OnNewMessageAsync(ChatMessageContent newMessage, CancellationToken cancellationToken = default)
+    {
+        await Task.WhenAll(this.ThreadExtensions.Select(x => x.OnNewMessageAsync(newMessage, cancellationToken)).ToList()).ConfigureAwait(false);
+    }
+
+    /// <summary>
+    /// Called just before the AI is invoked
+    /// </summary>
+    /// <param name="newMessage">The most recent message that the AI is being invoked with.</param>
+    /// <param name="cancellationToken">The <see cref="CancellationToken"/> to monitor for cancellation requests. The default is <see cref="CancellationToken.None"/>.</param>
+    /// <returns>A task that represents the asynchronous operation, containing the combined context from all thread extensions.</returns>
+    public virtual async Task<string> OnAIInvocationAsync(ChatMessageContent newMessage, CancellationToken cancellationToken = default)
+    {
+        var subContexts = await Task.WhenAll(this.ThreadExtensions.Select(x => x.OnAIInvocationAsync(newMessage, cancellationToken)).ToList()).ConfigureAwait(false);
+        return string.Join("\n", subContexts);
+    }
+
+    /// <summary>
+    /// Registers plugins required by all thread extensions contained by this manager on the provided <see cref="Kernel"/>.
+    /// </summary>
+    /// <param name="kernel">The kernel to register the plugins on.</param>
+    public virtual void RegisterPlugins(Kernel kernel)
+    {
+        foreach (var threadExtension in this.ThreadExtensions)
+        {
+            threadExtension.RegisterPlugins(kernel);
+        }
+    }
+}
