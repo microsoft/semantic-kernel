f0d6dbf9ee64698d08c9008f5215f12e5653e88a
westey <164392973+westey-m@users.noreply.github.com>
2025-03-28 11:48:20 +0000

    Add user preferences component

---
+++ dotnet/src/Memory/Memory/OptionalTextMemoryStore.cs
@@ -0,0 +1,77 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.SemanticKernel.Memory;
+
+namespace Microsoft.SemanticKernel.Agents.Memory;
+
+/// <summary>
+/// Helper class to get a <see cref="TextMemoryStore"/> from the DI container if a name is provided
+/// and if the memory store is registered.
+/// Class implements no-op methods if no name is provided or the store is not registered.
+/// </summary>
+internal sealed class OptionalTextMemoryStore : TextMemoryStore
+{
+    private readonly TextMemoryStore? _textMemoryStore;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="OptionalTextMemoryStore"/> class.
+    /// </summary>
+    /// <param name="kernel">The kernel to try and get the named store from.</param>
+    /// <param name="storeName">The name of the store to get from the DI container.</param>
+    public OptionalTextMemoryStore(Kernel kernel, string? storeName)
+    {
+        if (storeName is not null)
+        {
+            this._textMemoryStore = kernel.Services.GetKeyedService<TextMemoryStore>(storeName);
+        }
+    }
+
+    /// <inheritdoc/>
+    public override Task<string?> GetMemoryAsync(string documentName, CancellationToken cancellationToken = default)
+    {
+        if (this._textMemoryStore is not null)
+        {
+            return this._textMemoryStore.GetMemoryAsync(documentName, cancellationToken);
+        }
+
+        return Task.FromResult<string?>(null);
+    }
+
+    /// <inheritdoc/>
+    public override Task SaveMemoryAsync(string documentName, string memoryText, CancellationToken cancellationToken = default)
+    {
+        if (this._textMemoryStore is not null)
+        {
+            return this._textMemoryStore.SaveMemoryAsync(documentName, memoryText, cancellationToken);
+        }
+
+        return Task.CompletedTask;
+    }
+
+    /// <inheritdoc/>
+    public override Task SaveMemoryAsync(string memoryText, CancellationToken cancellationToken = default)
+    {
+        if (this._textMemoryStore is not null)
+        {
+            return this._textMemoryStore.SaveMemoryAsync(memoryText, cancellationToken);
+        }
+
+        return Task.CompletedTask;
+    }
+
+    /// <inheritdoc/>
+    public override IAsyncEnumerable<string> SimilaritySearch(string query, CancellationToken cancellationToken = default)
+    {
+        if (this._textMemoryStore is not null)
+        {
+            return this._textMemoryStore.SimilaritySearch(query, cancellationToken);
+        }
+
+        return AsyncEnumerable.Empty<string>();
+    }
+}
---
+++ dotnet/src/Memory/Memory/UserPreferencesMemoryComponent.cs
@@ -0,0 +1,157 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Memory;
+
+namespace Microsoft.SemanticKernel.Agents.Memory;
+
+/// <summary>
+/// A memory component that can retrieve, maintain and store user preferences that
+/// are learned from the user's interactions with the agent.
+/// </summary>
+public class UserPreferencesMemoryComponent : ThreadExtension
+{
+    private readonly Kernel _kernel;
+    private readonly TextMemoryStore _textMemoryStore;
+    private string _userPreferences = string.Empty;
+    private bool _contextLoaded = false;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="UserPreferencesMemoryComponent"/> class.
+    /// </summary>
+    /// <param name="kernel">A kernel to use for making chat completion calls.</param>
+    /// <param name="textMemoryStore">The memory store to retrieve and save memories from and to.</param>
+    public UserPreferencesMemoryComponent(Kernel kernel, TextMemoryStore textMemoryStore)
+    {
+        this._kernel = kernel;
+        this._textMemoryStore = textMemoryStore;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="UserPreferencesMemoryComponent"/> class.
+    /// </summary>
+    /// <param name="kernel">A kernel to use for making chat completion calls.</param>
+    /// <param name="userPreferencesStoreName">The service key that the <see cref="TextMemoryStore"/> for user preferences is registered under in DI.</param>
+    public UserPreferencesMemoryComponent(Kernel kernel, string? userPreferencesStoreName = "UserPreferencesStore")
+    {
+        this._kernel = kernel;
+        this._textMemoryStore = new OptionalTextMemoryStore(kernel, userPreferencesStoreName);
+    }
+
+    /// <summary>
+    /// Gets or sets the name of the document to use for storing user preferences.
+    /// </summary>
+    public string UserPreferencesDocumentName { get; init; } = "UserPreferences";
+
+    /// <summary>
+    /// Gets or sets the prompt template to use for extracting user preferences and merging them with existing preferences.
+    /// </summary>
+    public string MaintainencePromptTemplate { get; init; } =
+        """
+        You are an expert in extracting facts about a user from text and combining these facts with existing facts to output a new list of facts.
+        Facts are short statements that each contain a single piece of information.
+        Facts should always be about the user and should always be in the present tense.
+        Facts should focus on the user's long term preferences and characteristics, not on their short term actions.
+
+        Here are 5 few shot examples:
+
+        EXAMPLES START
+
+        Input text: My name is John. I love dogs and cats, but unfortunately I am allergic to cats. I'm not alergic to dogs though. I have a dog called Fido.
+        Input facts: User name is John. User is alergic to cats.
+        Output: User name is John. User loves dogs. User loves cats. User is alergic to cats. User is not alergic to dogs. User has a dog. User dog's name is Fido.
+
+        Input text: My name is Mary. I like active holidays. I enjoy cycling and hiking.
+        Input facts: User name is Mary. User dislikes cycling.
+        Output: User name is Mary. User likes cycling. User likes hiking. User likes active holidays.
+
+        Input text: How do I calculate the area of a circle?
+        Input facts:
+        Output:
+
+        Input text: What is today's date?
+        Input facts: User name is Peter.
+        Output: User name is Peter.
+
+        EXAMPLES END
+
+        Return output for the following inputs like shown in the examples above:
+
+        Input text: {{$inputText}}
+        Input facts: {{$existingPreferences}}
+        """;
+
+    /// <inheritdoc/>
+    public override async Task OnThreadCreateAsync(string threadId, string? inputText = default, CancellationToken cancellationToken = default)
+    {
+        if (!this._contextLoaded)
+        {
+            this._userPreferences = string.Empty;
+
+            var memoryText = await this._textMemoryStore.GetMemoryAsync("UserPreferences", cancellationToken).ConfigureAwait(false);
+            if (memoryText is not null)
+            {
+                this._userPreferences = memoryText;
+            }
+
+            this._contextLoaded = true;
+        }
+    }
+
+    /// <inheritdoc/>
+    public override async Task OnThreadDeleteAsync(string threadId, CancellationToken cancellationToken = default)
+    {
+        await this._textMemoryStore.SaveMemoryAsync("UserPreferences", this._userPreferences, cancellationToken).ConfigureAwait(false);
+    }
+
+    /// <inheritdoc/>
+    public override Task OnNewMessageAsync(ChatMessageContent newMessage, CancellationToken cancellationToken = default)
+    {
+        if (newMessage.Role == AuthorRole.User && !string.IsNullOrWhiteSpace(newMessage.Content))
+        {
+            // Don't wait for task to complete. Just run in the background.
+            var task = this.ExtractAndSaveMemoriesAsync(newMessage.Content, cancellationToken);
+        }
+
+        return Task.CompletedTask;
+    }
+
+    /// <inheritdoc/>
+    public override Task<string> OnAIInvocationAsync(ChatMessageContent newMessage, CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult("The following list contains facts about the user:\n" + this._userPreferences);
+    }
+
+    /// <inheritdoc/>
+    public override void RegisterPlugins(Kernel kernel)
+    {
+        base.RegisterPlugins(kernel);
+        kernel.Plugins.AddFromObject(this, "UserPreferencesMemory");
+    }
+
+    /// <summary>
+    /// Plugin method to clear user preferences stored in memory.
+    /// </summary>
+    [KernelFunction]
+    [Description("Deletes any user preferences stored about the user.")]
+    public async Task ClearUserPreferencesAsync(CancellationToken cancellationToken = default)
+    {
+        this._userPreferences = string.Empty;
+        await this._textMemoryStore.SaveMemoryAsync("UserPreferences", this._userPreferences, cancellationToken).ConfigureAwait(false);
+    }
+
+    private async Task ExtractAndSaveMemoriesAsync(string inputText, CancellationToken cancellationToken = default)
+    {
+        var result = await this._kernel.InvokePromptAsync(
+            this.MaintainencePromptTemplate,
+            new KernelArguments() { ["inputText"] = inputText, ["existingPreferences"] = this._userPreferences },
+            cancellationToken: cancellationToken).ConfigureAwait(false);
+
+        this._userPreferences = result.ToString();
+
+        await this._textMemoryStore.SaveMemoryAsync("UserPreferences", this._userPreferences, cancellationToken).ConfigureAwait(false);
+    }
+}
