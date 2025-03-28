155452e500d6c0e0a3ae5b6f6083e7e7d2efae15
SergeyMenshykh <sergemenshikh@gmail.com>
2025-03-28 18:12:47 +0000

    refactor mcp sample to use auto function invocation filter, telemetry and openapi funcitons

--- dotnet/samples/Demos/ModelContextProtocolClientServer/MCPServer/Program.cs
+++ dotnet/samples/Demos/ModelContextProtocolClientServer/MCPServer/Program.cs
@@ -1,21 +1,74 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
-using MCPServer.Tools;
+using Azure.Monitor.OpenTelemetry.Exporter;
+using MCPServer;
 using Microsoft.SemanticKernel;
 using ModelContextProtocol;
+using OpenTelemetry.Resources;
+using OpenTelemetry;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Metrics;
+using MCPServer.Resources;
 
-// Create a kernel builder and add plugins
-IKernelBuilder kernelBuilder = Kernel.CreateBuilder();
-kernelBuilder.Plugins.AddFromType<DateTimeUtils>();
-kernelBuilder.Plugins.AddFromType<WeatherUtils>();
+// Enable Application Insights telemetry
+string connectionString = GetAppInsightsConnectionString();
+
+// Enable diagnostics with sensitive data.
+AppContext.SetSwitch("Microsoft.SemanticKernel.Experimental.GenAI.EnableOTelDiagnosticsSensitive", true);
+
+var resourceBuilder = ResourceBuilder
+    .CreateDefault()
+    .AddService("SKTelemetry");
+
+using var traceProvider = Sdk.CreateTracerProviderBuilder()
+    .SetResourceBuilder(resourceBuilder)
+    .AddSource("Microsoft.SemanticKernel*")
+    .AddAzureMonitorTraceExporter(options => options.ConnectionString = connectionString)
+    .Build();
+
+using var meterProvider = Sdk.CreateMeterProviderBuilder()
+    .SetResourceBuilder(resourceBuilder)
+    .AddMeter("Microsoft.SemanticKernel*")
+    .AddAzureMonitorMetricExporter(options => options.ConnectionString = connectionString)
+    .Build();
+
+using var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder.AddOpenTelemetry(options =>
+    {
+        options.SetResourceBuilder(resourceBuilder);
+        options.AddAzureMonitorLogExporter(options => options.ConnectionString = connectionString);
+        options.IncludeFormattedMessage = true;
+        options.IncludeScopes = true;
+    });
+    builder.AddFilter("Microsoft.SemanticKernel*", LogLevel.Trace);
+});
 
 // Build the kernel
-Kernel kernel = kernelBuilder.Build();
+Kernel kernel = new();
+
+// Import a OpenAPI plugin defined weather.json OpenAPI/Swagger spec
+using Stream stream = EmbeddedResource.ReadAsStream("weather.json");
+await kernel.ImportPluginFromOpenApiAsync("Weather", stream);
+
+// Register a function invocation filter to validate function calls
+kernel.AutoFunctionInvocationFilters.Add(new ContentSafetyAutoFunctionInvocationFilter());
 
 var builder = Host.CreateEmptyApplicationBuilder(settings: null);
 builder.Services
+    .AddSingleton<ILoggerFactory>(loggerFactory)
     .AddMcpServer()
     .WithStdioServerTransport()
     // Add kernel functions to the MCP server as MCP tools
     .WithTools(kernel.Plugins.SelectMany(p => p.Select(f => f.AsAIFunction())));
 await builder.Build().RunAsync();
+
+static string GetAppInsightsConnectionString()
+{
+    var config = new ConfigurationBuilder()
+        .AddUserSecrets<Program>()
+        .AddEnvironmentVariables()
+        .Build();
+
+    return config["ApplicationInsights:ConnectionString"]!;
+}
---
+++ dotnet/samples/Demos/ModelContextProtocolClientServer/MCPServer/Resources/EmbeddedResource.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Reflection;
+
+namespace MCPServer.Resources;
+
+/// <summary>
+/// Reads embedded resources from the assembly.
+/// </summary>
+public static class EmbeddedResource
+{
+    private static readonly string? s_namespace = typeof(EmbeddedResource).Namespace;
+
+    internal static Stream ReadAsStream(string fileName)
+    {
+        // Get the current assembly. Note: this class is in the same assembly where the embedded resources are stored.
+        Assembly assembly =
+            typeof(EmbeddedResource).GetTypeInfo().Assembly ??
+            throw new InvalidOperationException($"[{s_namespace}] {fileName} assembly not found");
+
+        // Resources are mapped like types, using the namespace and appending "." (dot) and the file name
+        var resourceName = $"{s_namespace}." + fileName;
+        return
+            assembly.GetManifestResourceStream(resourceName) ??
+            throw new InvalidOperationException($"{resourceName} resource not found");
+    }
+}
