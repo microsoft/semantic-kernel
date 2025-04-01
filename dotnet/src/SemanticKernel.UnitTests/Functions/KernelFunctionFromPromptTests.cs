f685ac40e9e6e58c4b4ba3deacc315adb0b08afd
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2025-04-01 14:48:19 +0100

    Fix format

--- dotnet/src/SemanticKernel.UnitTests/Functions/KernelFunctionFromPromptTests.cs
+++ dotnet/src/SemanticKernel.UnitTests/Functions/KernelFunctionFromPromptTests.cs
@@ -1220,7 +1220,7 @@ public class KernelFunctionFromPromptTests
         {
             GetStreamingResponseResult = [
                 new MEAI.ChatResponseUpdate(MEAI.ChatRole.Assistant, "Hi! How can ") { RawRepresentation = rawRepresentation },
-                new MEAI.ChatResponseUpdate(role: null, content: "I assist you today?")  { RawRepresentation = rawRepresentation }
+                new MEAI.ChatResponseUpdate(role: null, content: "I assist you today?") { RawRepresentation = rawRepresentation }
             ]
         };
 
