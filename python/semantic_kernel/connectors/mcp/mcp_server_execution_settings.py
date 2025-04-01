967fa6885a026babf649e0d6e34c34fef5b92b81
Nico Möller <nkm.moeller@gmail.com>
2025-04-01 14:43:14 +0200

    Python: Add initial MCP Connector Version (#10778)
    With MCP getting more and more popular, i thought it's a cool idea to
    have plugins loaded and executed remotely with Semantic Kernel.
    
    What is included ?
    - Sample on how to use a MCP Server with Semantic Kernel
    - New Connector that can read Tools from the MCP Server
    - The Connector creates a Plugin with the corresponding MCP Tools
    - Connection can be done via stdio & sse
    
    What is **not** included ?
    - Loading of Resources, Prompts etc.
    - Handling Authentication to the MCP Server
    
    What needs to be done ?
    - [x] Load MCP Tools as Plugins
    - [x] Enhance Parameter Parsing between KernelParameters and MCP Tool
    Parameters
    - [x] Add Unit Tests
    - [x] Add Integration Tests
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [x] The code builds clean without any errors or warnings
    - [x] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [x] All unit tests pass, and I have added new tests where possible
    - [x] I didn't break anyone :smile:
    
    ---------
    
    Co-authored-by: Eduard van Valkenburg <eavanvalkenburg@users.noreply.github.com>

---
+++ python/semantic_kernel/connectors/mcp/mcp_server_execution_settings.py
@@ -0,0 +1,74 @@
+# Copyright (c) Microsoft. All rights reserved.
+from contextlib import asynccontextmanager
+from typing import Any
+
+from mcp import ClientSession
+from mcp.client.sse import sse_client
+from mcp.client.stdio import StdioServerParameters, stdio_client
+from pydantic import Field
+
+from semantic_kernel.exceptions import KernelPluginInvalidConfigurationError
+from semantic_kernel.kernel_pydantic import KernelBaseModel
+
+
+class MCPServerExecutionSettings(KernelBaseModel):
+    """MCP server settings."""
+
+    session: ClientSession | None = None
+
+    @asynccontextmanager
+    async def get_session(self):
+        """Get or Open an MCP session."""
+        try:
+            if self.session is None:
+                # If the session is not open, create always new one
+                async with self.get_mcp_client() as (read, write), ClientSession(read, write) as session:
+                    await session.initialize()
+                    yield session
+            else:
+                # If the session is set by the user, just yield it
+                yield self.session
+        except Exception as ex:
+            raise KernelPluginInvalidConfigurationError("Failed establish MCP session.") from ex
+
+    def get_mcp_client(self):
+        """Get an MCP client."""
+        raise NotImplementedError("This method is only needed for subclasses.")
+
+
+class MCPStdioServerExecutionSettings(MCPServerExecutionSettings):
+    """MCP stdio server settings."""
+
+    command: str
+    args: list[str] = Field(default_factory=list)
+    env: dict[str, str] | None = None
+    encoding: str = "utf-8"
+
+    def get_mcp_client(self):
+        """Get an MCP stdio client."""
+        return stdio_client(
+            server=StdioServerParameters(
+                command=self.command,
+                args=self.args,
+                env=self.env,
+                encoding=self.encoding,
+            )
+        )
+
+
+class MCPSseServerExecutionSettings(MCPServerExecutionSettings):
+    """MCP sse server settings."""
+
+    url: str
+    headers: dict[str, Any] | None = None
+    timeout: float = 5
+    sse_read_timeout: float = 60 * 5
+
+    def get_mcp_client(self):
+        """Get an MCP SSE client."""
+        return sse_client(
+            url=self.url,
+            headers=self.headers,
+            timeout=self.timeout,
+            sse_read_timeout=self.sse_read_timeout,
+        )
