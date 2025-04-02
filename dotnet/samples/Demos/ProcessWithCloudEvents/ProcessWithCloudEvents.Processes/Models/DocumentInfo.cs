540b5dcfdb53656ccbcc1c7362dd1c6a34bce720
Estefanía Tenorio <8483207+esttenorio@users.noreply.github.com>
2025-04-02 09:48:30 -0700

    removing commented code

--- dotnet/samples/Demos/ProcessWithCloudEvents/ProcessWithCloudEvents.Processes/Models/DocumentInfo.cs
+++ dotnet/samples/Demos/ProcessWithCloudEvents/ProcessWithCloudEvents.Processes/Models/DocumentInfo.cs
@@ -1,8 +1,5 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
-using System.Runtime.Serialization;
-using System.Text.Json;
-
 namespace ProcessWithCloudEvents.Processes.Models;
 
 /// <summary>
@@ -16,27 +13,13 @@ public class DocumentInfo
     /// <summary>
     /// Id of the document
     /// </summary>
-    //[DataMember]
     public string Id { get; set; } = string.Empty;
     /// <summary>
     /// Title of the document
     /// </summary>
-    //[DataMember]
     public string Title { get; set; } = string.Empty;
     /// <summary>
     /// Content of the document
     /// </summary>
-    //[DataMember]
     public string Content { get; set; } = string.Empty;
-
-    /// <summary>
-    /// For properly supporting serialization for state management saving and restoring,
-    /// objects used in state and parameter types must be serializable.<br/>
-    /// Overriding the ToString method allows injecting custom serialization logic if needed.
-    /// </summary>
-    /// <returns></returns>
-    //public override string ToString()
-    //{
-    //    return JsonSerializer.Serialize(this);
-    //}
 }
