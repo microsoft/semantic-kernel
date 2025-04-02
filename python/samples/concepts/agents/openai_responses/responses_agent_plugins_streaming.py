2ba89a79fb1e6eda432ca3ddca964b2be769b827
Evan Mattson <35585003+moonbox3@users.noreply.github.com>
2025-04-03 08:18:57 +0900

    Python: Introduce the OpenAI Responses Agent (#11240)
    ### Motivation and Context
    
    OpenAI recently released their Responses API, which is their newest core
    API and an agentic API primitive, combining the simplicity of Chat
    Completions with the ability to do more agentic tasks. Azure OpenAI also
    recently released the Responses API, with close-to feature parity with
    OpenAI's SDK. It gives us enough to introduce an `AzureResponsesAgent`
    and an `OpenAIResponsesAgent` to Semantic Kernel.
    
    <!-- Thank you for your contribution to the semantic-kernel repo!
    Please help reviewers and future users, providing the following
    information:
      1. Why is this change required?
      2. What problem does it solve?
      3. What scenario does it contribute to?
      4. If it fixes an open issue, please link to the issue here.
    -->
    
    ### Description
    
    This PR introduces:
    - The `AzureResponsesAgent` and `OpenAIResponsesAgent`
    - Allows use of the web search tool with `OpenAI` (`Azure OpenAI`
    currently doesn't support this).
    - Allows use of the file search tool with both `OpenAI` and `Azure
    OpenAI`.
    - Provides getting started samples for both agent types.
    
    **TODO:**
    - Add unit test coverage for the `ResponsesAgentThreadActions` class.
    - Add support for the computer user agent tool.
    - Improve typing in the `ResponsesAgentThreadActions` class.
    - Closes #11026
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [X] The code builds clean without any errors or warnings
    - [X] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [X] All unit tests pass, and I have added new tests where possible
    - [X] I didn't break anyone :smile:

---
+++ python/samples/concepts/agents/openai_responses/responses_agent_plugins_streaming.py
@@ -0,0 +1,93 @@
+# Copyright (c) Microsoft. All rights reserved.
+import asyncio
+from typing import Annotated
+
+from semantic_kernel.agents import AzureResponsesAgent
+from semantic_kernel.functions import kernel_function
+
+"""
+The following sample demonstrates how to create an OpenAI Responses Agent.
+The sample shows how to have the agent answer questions about the sample menu
+with streaming responses.
+
+The interaction with the agent is via the `get_response` method, which sends a
+user input to the agent and receives a response from the agent. The conversation
+history is maintained by the agent service, i.e. the responses are automatically
+associated with the thread. Therefore, client code does not need to maintain the
+conversation history.
+"""
+
+
+# Define a sample plugin for the sample
+class MenuPlugin:
+    """A sample Menu Plugin used for the concept sample."""
+
+    @kernel_function(description="Provides a list of specials from the menu.")
+    def get_specials(self, menu_item: str) -> Annotated[str, "Returns the specials from the menu."]:
+        return """
+        Special Soup: Clam Chowder
+        Special Salad: Cobb Salad
+        Special Drink: Chai Tea
+        """
+
+    @kernel_function(description="Provides the price of the requested menu item.")
+    def get_item_price(
+        self, menu_item: Annotated[str, "The name of the menu item."]
+    ) -> Annotated[str, "Returns the price of the menu item."]:
+        return "$9.99"
+
+
+# Simulate a conversation with the agent
+USER_INPUTS = [
+    "Hello",
+    "What is the special soup?",
+    "What is the special drink?",
+    "How much is it?",
+    "Thank you",
+]
+
+
+async def main():
+    # 1. Create the client using OpenAI resources and configuration
+    client, model = AzureResponsesAgent.setup_resources()
+
+    # 2. Create a Semantic Kernel agent for the OpenAI Responses API
+    agent = AzureResponsesAgent(
+        ai_model_id=model,
+        client=client,
+        instructions="Answer questions about the menu.",
+        name="Host",
+        plugins=[MenuPlugin()],
+    )
+
+    # 3. Create a thread for the agent
+    # If no thread is provided, a new thread will be
+    # created and returned with the initial response
+    thread = None
+
+    for user_input in USER_INPUTS:
+        print(f"# User: '{user_input}'")
+        first_chunk = True
+        # 4. Invoke the agent for the current message and print the response
+        async for response in agent.invoke_stream(messages=user_input, thread=thread):
+            thread = response.thread
+            if first_chunk:
+                print(f"# {response.name}: ", end="", flush=True)
+                first_chunk = False
+            print(response.content, end="", flush=True)
+        print()
+
+    """
+    You should see output similar to the following:
+
+    # User: 'Why is the sky blue?'
+    # Agent: The sky appears blue because molecules in the atmosphere scatter sunlight in all directions, and blue 
+        light is scattered more than other colors because it travels in shorter, smaller waves.
+    # User: 'What is the speed of light?'
+    # Agent: The speed of light in a vacuum is approximately 299,792,458 meters per second 
+        (about 186,282 miles per second).
+     """
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
