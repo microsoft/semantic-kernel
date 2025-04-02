16251923a52f0803899fb63e9a8fab45214f6d34
Mark Wallace <127216156+markwallace-microsoft@users.noreply.github.com>
2025-04-02 13:24:16 +0100

    .Net: Merge Tavily feature branch to main (#11227)
    ### Motivation and Context
    
    Add a TextSearch implementation using the Tavily Search API
    
    - [x] Initial implementation with search
    - [x] Support for image search
    - [x] Integration tests
    
    Closes #11060 
    
    ### Description
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [ ] The code builds clean without any errors or warnings
    - [ ] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [ ] All unit tests pass, and I have added new tests where possible
    - [ ] I didn't break anyone :smile:
    
    ---------
    
    Co-authored-by: Dmytro Struk <13853051+dmytrostruk@users.noreply.github.com>
    Co-authored-by: Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>

---
+++ dotnet/src/Plugins/Plugins.Web/Tavily/TavilySearchResult.cs
@@ -0,0 +1,73 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Microsoft.SemanticKernel.Plugins.Web.Tavily;
+
+/// <summary>
+/// Represents a search result from Tavily.
+/// </summary>
+[System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1056:URI-like properties should not be strings", Justification = "API definition")]
+public sealed class TavilySearchResult
+{
+    /// <summary>
+    /// The title of the search result.
+    /// </summary>
+    [JsonPropertyName("title")]
+    [JsonRequired]
+    public string Title { get; set; }
+
+    /// <summary>
+    ///The URL of the search result.
+    /// </summary>
+    [JsonPropertyName("url")]
+    [JsonRequired]
+    public string Url { get; set; }
+
+    /// <summary>
+    /// A short description of the search result.
+    /// </summary>
+    [JsonPropertyName("content")]
+    [JsonRequired]
+    public string Content { get; set; }
+
+    /// <summary>
+    /// The cleaned and parsed HTML content of the search result. Only if include_raw_content is true.
+    /// </summary>
+    [JsonPropertyName("raw_content")]
+    public string? RawContent { get; set; }
+
+    /// <summary>
+    /// The relevance score of the search result.
+    /// </summary>
+    [JsonPropertyName("score")]
+    [JsonRequired]
+    public double Score { get; set; }
+
+    /// <summary>
+    /// Additional properties that are not explicitly defined in the schema
+    /// </summary>
+    [JsonExtensionData]
+    public IDictionary<string, object> AdditionalProperties { get; set; } = new Dictionary<string, object>();
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TavilySearchResult" /> class.
+    /// </summary>
+    public TavilySearchResult(
+        string title,
+#pragma warning disable CA1054 // URI-like parameters should not be strings
+        string url,
+#pragma warning restore CA1054 // URI-like parameters should not be strings
+        string content,
+        double score,
+        string? rawContent)
+    {
+        this.Title = title ?? throw new ArgumentNullException(nameof(title));
+        this.Url = url ?? throw new ArgumentNullException(nameof(url));
+        this.Content = content ?? throw new ArgumentNullException(nameof(content));
+        this.Score = score;
+        this.RawContent = rawContent;
+    }
+}
