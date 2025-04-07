04e03b3b36ad4d6fe5cf8bc5c4fdb4559e6f23b0
eavanvalkenburg <github@vanvalkenburg.eu>
2025-04-07 14:33:16 +0200

    allow prompts to return images or audio

--- python/semantic_kernel/connectors/ai/open_ai/services/open_ai_text_to_image_base.py
+++ python/semantic_kernel/connectors/ai/open_ai/services/open_ai_text_to_image_base.py
@@ -5,36 +5,36 @@ from typing import Any
 from openai.types.images_response import ImagesResponse
 
 from semantic_kernel.connectors.ai.open_ai.prompt_execution_settings.open_ai_text_to_image_execution_settings import (
-    ImageSize,
     OpenAITextToImageExecutionSettings,
 )
 from semantic_kernel.connectors.ai.open_ai.services.open_ai_handler import OpenAIHandler
 from semantic_kernel.connectors.ai.prompt_execution_settings import PromptExecutionSettings
 from semantic_kernel.connectors.ai.text_to_image_client_base import TextToImageClientBase
-from semantic_kernel.exceptions.service_exceptions import ServiceResponseException
+from semantic_kernel.exceptions.service_exceptions import ServiceInvalidRequestError, ServiceResponseException
 
 
 class OpenAITextToImageBase(OpenAIHandler, TextToImageClientBase):
     """OpenAI text to image client."""
 
-    async def generate_image(self, description: str, width: int, height: int, **kwargs: Any) -> bytes | str:
+    async def generate_image(self, prompt: str, settings: PromptExecutionSettings, **kwargs: Any) -> bytes | str:
         """Generate image from text.
 
         Args:
-            description: Description of the image.
-            width: Width of the image, check the openai documentation for the supported sizes.
-            height: Height of the image, check the openai documentation for the supported sizes.
+            prompt: Description of the image.
+            settings: Execution settings for the prompt.
             kwargs: Additional arguments, check the openai images.generate documentation for the supported arguments.
 
         Returns:
             bytes | str: Image bytes or image URL.
         """
-        settings = OpenAITextToImageExecutionSettings(
-            prompt=description,
-            size=ImageSize(width=width, height=height),
-            ai_model_id=self.ai_model_id,
-            **kwargs,
-        )
+        if not isinstance(settings, OpenAITextToImageExecutionSettings):
+            settings = OpenAITextToImageExecutionSettings.from_prompt_execution_settings(settings)
+
+        if not settings.prompt:
+            settings.prompt = prompt
+
+        if not settings.prompt:
+            raise ServiceInvalidRequestError("Prompt is required.")
 
         response = await self._send_request(settings)
 
--- python/semantic_kernel/connectors/ai/text_to_image_client_base.py
+++ python/semantic_kernel/connectors/ai/text_to_image_client_base.py
@@ -3,6 +3,8 @@
 from abc import ABC, abstractmethod
 from typing import Any
 
+from semantic_kernel.connectors.ai.prompt_execution_settings import PromptExecutionSettings
+from semantic_kernel.contents.image_content import ImageContent
 from semantic_kernel.services.ai_service_client_base import AIServiceClientBase
 
 
@@ -10,16 +12,31 @@ class TextToImageClientBase(AIServiceClientBase, ABC):
     """Base class for text to image client."""
 
     @abstractmethod
-    async def generate_image(self, description: str, width: int, height: int, **kwargs: Any) -> bytes | str:
+    async def generate_image(self, prompt: str, settings: PromptExecutionSettings, **kwargs: Any) -> bytes | str:
         """Generate image from text.
 
         Args:
-            description: Description of the image.
-            width: Width of the image.
-            height: Height of the image.
+            prompt: Description of the image.
+            settings: Execution settings for the prompt.
             kwargs: Additional arguments.
 
         Returns:
             bytes | str: Image bytes or image URL.
         """
         raise NotImplementedError
+
+    async def get_image_content(self, prompt: str, settings: PromptExecutionSettings, **kwargs: Any) -> ImageContent:
+        """Get image content.
+
+        Args:
+            prompt: Description of the image.
+            settings: Execution settings for the prompt.
+            kwargs: Additional arguments.
+
+        Returns:
+            ImageContent: Image content.
+        """
+        image = await self.generate_image(prompt, settings, **kwargs)
+        if isinstance(image, str):
+            return ImageContent(uri=image)
+        return ImageContent(data=image)
