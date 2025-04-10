1c14eeb048e2618db5ac85aa52b37ff8cb0974fd
Dmytro Struk <13853051+dmytrostruk@users.noreply.github.com>
2025-04-10 08:44:08 -0700

    Small fixes after merge

--- dotnet/src/VectorDataIntegrationTests/WeaviateIntegrationTests/Support/WeaviateSimpleModelFixture.cs
+++ dotnet/src/VectorDataIntegrationTests/WeaviateIntegrationTests/Support/WeaviateSimpleModelFixture.cs
@@ -6,7 +6,7 @@ namespace WeaviateIntegrationTests.Support;
 
 public class WeaviateSimpleModelFixture : SimpleModelFixture<Guid>
 {
-    public override TestStore TestStore => WeaviateTestStore.Instance;
+    public override TestStore TestStore => WeaviateTestStore.NamedVectorsInstance;
 
     // Weaviate requires the name to start with a capital letter and not contain any chars other than a-Z and 0-9.
     // Source: https://weaviate.io/developers/weaviate/starter-guides/managing-collections#collection--property-names
