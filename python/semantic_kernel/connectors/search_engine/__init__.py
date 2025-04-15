b5296f02e86e298e1b46d062d9e5a981c402cfc1
N-E-W-T-O-N <new99mal@gmail.com>
2025-04-15 13:29:21 +0530

    Remove BraveConnector and related settings from search engine connectors

--- python/semantic_kernel/connectors/search_engine/__init__.py
+++ python/semantic_kernel/connectors/search_engine/__init__.py
@@ -1,7 +1,6 @@
 # Copyright (c) Microsoft. All rights reserved.
 
 from semantic_kernel.connectors.search_engine.bing_connector import BingConnector
-from semantic_kernel.connectors.search_engine.brave_connector import BraveConnector
 from semantic_kernel.connectors.search_engine.google_connector import GoogleConnector
 
-__all__ = ["BingConnector", "BraveConnector", "GoogleConnector"]
+__all__ = ["BingConnector", "GoogleConnector"]
