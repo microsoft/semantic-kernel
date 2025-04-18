f2c83c6cced2344e8a7b083b0d7a4a296a50ccaa
westey <164392973+westey-m@users.noreply.github.com>
2025-04-18 13:09:30 +0100

    Fix encoding

--- dotnet/src/VectorDataIntegrationTests/CosmosNoSQLIntegrationTests/CRUD/CosmosNoSQLNoDataConformanceTests.cs
+++ dotnet/src/VectorDataIntegrationTests/CosmosNoSQLIntegrationTests/CRUD/CosmosNoSQLNoDataConformanceTests.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Microsoft. All rights reserved.
+﻿// Copyright (c) Microsoft. All rights reserved.
 
 using CosmosNoSQLIntegrationTests.Support;
 using VectorDataSpecificationTests.CRUD;
