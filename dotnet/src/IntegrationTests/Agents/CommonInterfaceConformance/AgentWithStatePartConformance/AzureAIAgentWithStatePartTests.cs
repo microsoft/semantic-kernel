dc8fb482cd8e9a534cbbbbeac02653b1c8fe4fd4
westey <164392973+westey-m@users.noreply.github.com>
2025-04-24 14:52:14 +0100

    Update AzureAI and OpenAIAssistant agents to support conversation state

---
+++ dotnet/src/IntegrationTests/Agents/CommonInterfaceConformance/AgentWithStatePartConformance/AzureAIAgentWithStatePartTests.cs
@@ -0,0 +1,7 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+namespace SemanticKernel.IntegrationTests.Agents.CommonInterfaceConformance.AgentWithStatePartConformance;
+
+public class AzureAIAgentWithStatePartTests() : AgentWithStatePartTests<AzureAIAgentFixture>(() => new AzureAIAgentFixture())
+{
+}
