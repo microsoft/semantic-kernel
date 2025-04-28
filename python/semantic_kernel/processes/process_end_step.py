30d2b0802a3fe213acdf648a98f0e4464f2b3072
Evan Mattson <35585003+moonbox3@users.noreply.github.com>
2025-04-29 07:51:11 +0900

    Python: Support Process State Management (#11637)
    ### Motivation and Context
    
    SK Python processes have been missing support to be able to serialize
    and deserialize JSON state for a process and its steps. This PR brings
    in the functionality to allow the developer to do so. The
    `getting_started_with_processes` step03 has been update to reflect this
    latest functionality. It is possible to dump a JSON state to a file, and
    reload the state to continue running the process.
    
    State metadata that handles the version for steps is managed via a
    decorator:
    
    ```python
    @kernel_process_step_metadata("CutFoodStep.V1")
    class CutFoodStep(KernelProcessStep):
        class Functions(Enum):
            ChopFood = "ChopFood"
            SliceFood = "SliceFood"
    ```
    
    If no decorator/state is supplied the step will be built with a default
    state version of "v1" which aligns with .Net.
    
    <!-- Thank you for your contribution to the semantic-kernel repo!
    Please help reviewers and future users, providing the following
    information:
      1. Why is this change required?
      2. What problem does it solve?
      3. What scenario does it contribute to?
      4. If it fixes an open issue, please link to the issue here.
    -->
    
    ### Description
    
    Support state and versioning management for Python processes.
    - Update samples to reflect changes.
    - Closes #9584
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [X] The code builds clean without any errors or warnings
    - [X] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [X] All unit tests pass, and I have added new tests where possible
    - [ ] I didn't break anyone :smile:

--- python/semantic_kernel/processes/process_end_step.py
+++ python/semantic_kernel/processes/process_end_step.py
@@ -1,10 +1,17 @@
 # Copyright (c) Microsoft. All rights reserved.
+import sys
 from typing import TYPE_CHECKING, ClassVar
 
 from semantic_kernel.processes.kernel_process.kernel_process_step_info import KernelProcessStepInfo
 from semantic_kernel.processes.kernel_process.kernel_process_step_state import KernelProcessStepState
+from semantic_kernel.processes.kernel_process.kernel_process_step_state_metadata import KernelProcessStepStateMetadata
 from semantic_kernel.processes.process_step_builder import ProcessStepBuilder
 from semantic_kernel.utils.feature_stage_decorator import experimental
+
+if sys.version_info >= (3, 12):
+    from typing import override  # pragma: no cover
+else:
+    from typing_extensions import override  # pragma: no cover
 
 if TYPE_CHECKING:
     from semantic_kernel.functions import KernelFunctionMetadata
@@ -18,6 +25,7 @@ class EndStep(ProcessStepBuilder):
     END_STEP_VALUE: ClassVar[str] = "Microsoft.SemanticKernel.Process.EndStep"
     END_STEP_NAME: ClassVar[str] = END_STEP_VALUE
     END_STEP_ID: ClassVar[str] = END_STEP_VALUE
+    END_STEP_VERSION: ClassVar[str] = "v0"
 
     @staticmethod
     def get_instance() -> "EndStep":
@@ -36,10 +44,11 @@ class EndStep(ProcessStepBuilder):
         """Gets the function metadata map."""
         return {}
 
-    def build_step(self) -> KernelProcessStepInfo:
+    @override
+    def build_step(self, state_metadata: KernelProcessStepStateMetadata | None = None) -> KernelProcessStepInfo:
         """Build the step."""
         return KernelProcessStepInfo(
             inner_step_type=KernelProcessStepState,
-            state=KernelProcessStepState(name=self.END_STEP_NAME),
+            state=KernelProcessStepState(name=self.END_STEP_NAME, version=self.END_STEP_VERSION),
             output_edges={},
         )
