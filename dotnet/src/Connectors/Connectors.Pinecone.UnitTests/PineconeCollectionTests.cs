3a80c24cc725afd0aa2c77052e9150e11b901837
Adam Sitnik <adam.sitnik@gmail.com>
2025-05-05 14:48:41 +0200

    fix namespace ordering issue

--- dotnet/src/Connectors/Connectors.Pinecone.UnitTests/PineconeCollectionTests.cs
+++ dotnet/src/Connectors/Connectors.Pinecone.UnitTests/PineconeCollectionTests.cs
@@ -4,8 +4,8 @@ using System;
 using System.Collections.Generic;
 using Microsoft.Extensions.VectorData;
 using Microsoft.SemanticKernel.Connectors.Pinecone;
-using Xunit;
 using Pinecone;
+using Xunit;
 
 namespace SemanticKernel.Connectors.Pinecone.UnitTests;
 
