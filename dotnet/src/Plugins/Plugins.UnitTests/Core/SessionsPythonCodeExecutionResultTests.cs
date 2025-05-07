c8a679e1a1fc368785d8dd8de7be2f2323f89c33
SergeyMenshykh <sergemenshikh@gmail.com>
2025-05-07 13:29:43 +0100

    use type to represent python code execution result instead of string

---
+++ dotnet/src/Plugins/Plugins.UnitTests/Core/SessionsPythonCodeExecutionResultTests.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SemanticKernel.Plugins.Core.CodeInterpreter;
+using Xunit;
+
+namespace SemanticKernel.Plugins.UnitTests.Core;
+
+public class SessionsPythonCodeExecutionResultTests
+{
+    [Fact]
+    public void ItShouldConvertResultToString()
+    {
+        // Arrange
+        var result = new SessionsPythonCodeExecutionResult
+        {
+            Status = "Succeeded",
+            Result = new SessionsPythonCodeExecutionResult.ExecutionDetails
+            {
+                StdOut = "Hello World",
+                StdErr = "Error",
+                ExecutionResult = "42"
+            }
+        };
+
+        // Act
+        string resultString = result.ToString();
+
+        // Assert
+        Assert.Contains("Status: Succeeded", resultString);
+        Assert.Contains("Result: 42", resultString);
+        Assert.Contains("Stdout: Hello World", resultString);
+        Assert.Contains("Stderr: Error", resultString);
+    }
+}
