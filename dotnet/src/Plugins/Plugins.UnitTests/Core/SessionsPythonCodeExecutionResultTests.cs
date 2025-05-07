fa2d52f6cc53629098db9f6ce92452e03cbf950e
SergeyMenshykh <68852919+SergeyMenshykh@users.noreply.github.com>
2025-05-07 22:25:45 +0100

    .Net: Use type to represent python code execution result (#11931)
    ### Motivation, Context and Description
    
    This PR introduces a new type, `SessionsPythonCodeExecutionResult`, to
    represent the Python code execution result and refactors the
    `SessionsPythonPlugin.ExecuteCodeAsync` method to return this type
    instead of a string. This should allow access to the result in a
    structured way for scenarios where the result needs to be processed
    further, for example, in the auto function invocation filters. At the
    same time, the type overloads the `ToString()` method to be used in
    scenarios where the result needs to be a string, for example when the
    function is invoked as part of rendering a prompt template.
    
    Contributes to:
    https://github.com/microsoft/semantic-kernel/issues/10070

---
+++ dotnet/src/Plugins/Plugins.UnitTests/Core/SessionsPythonCodeExecutionResultTests.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.SemanticKernel.Plugins.Core.CodeInterpreter;
+using Xunit;
+
+namespace SemanticKernel.Plugins.UnitTests.Core;
+
+public class SessionsPythonCodeExecutionResultTests
+{
+    [Fact]
+    public void ItShouldConvertResultToString()
+    {
+        // Arrange
+        var result = new SessionsPythonCodeExecutionResult
+        {
+            Status = "Succeeded",
+            Result = new SessionsPythonCodeExecutionResult.ExecutionDetails
+            {
+                StdOut = "Hello World",
+                StdErr = "Error",
+                ExecutionResult = "42"
+            }
+        };
+
+        // Act
+        string resultString = result.ToString();
+
+        // Assert
+        Assert.Contains("Status: Succeeded", resultString);
+        Assert.Contains("Result: 42", resultString);
+        Assert.Contains("Stdout: Hello World", resultString);
+        Assert.Contains("Stderr: Error", resultString);
+    }
+}
