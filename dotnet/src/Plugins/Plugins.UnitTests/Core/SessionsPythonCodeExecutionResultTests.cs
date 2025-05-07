d570d035e7a6efb223013aa6d8bbca20c29e7f01
SergeyMenshykh <sergemenshikh@gmail.com>
2025-05-07 14:42:00 +0100

    remove unnecessary usings

--- dotnet/src/Plugins/Plugins.UnitTests/Core/SessionsPythonCodeExecutionResultTests.cs
+++ dotnet/src/Plugins/Plugins.UnitTests/Core/SessionsPythonCodeExecutionResultTests.cs
@@ -1,10 +1,5 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.SemanticKernel.Plugins.Core.CodeInterpreter;
 using Xunit;
 
