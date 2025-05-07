ea8a21dabb4ba7d87961b20e2c464df11b2009e8
Adam Sitnik <adam.sitnik@gmail.com>
2025-05-07 15:55:40 +0200

    address code review feedback:
    - reduce code duplication
    - reduce XML docs duplication
    - create a copy of the option bag to avoid modifying user provided input

--- dotnet/src/Connectors/Connectors.Memory.SqlServer/SqlServerServiceCollectionExtensions.cs
+++ dotnet/src/Connectors/Connectors.Memory.SqlServer/SqlServerServiceCollectionExtensions.cs
@@ -16,30 +16,25 @@ public static class SqlServerServiceCollectionExtensions
     /// <summary>
     /// Registers a <see cref="SqlServerVectorStore"/> as <see cref="VectorStore"/>, with the specified connection string and service lifetime.
     /// </summary>
-    /// <param name="services">The <see cref="IServiceCollection"/> to register the <see cref="VectorStore"/> on.</param>
-    /// <param name="connectionStringProvider">The connection string provider.</param>
-    /// <param name="optionsProvider">Options provider to further configure the vector store.</param>
-    /// <param name="lifetime">The service lifetime for the store. Defaults to <see cref="ServiceLifetime.Singleton"/>.</param>
-    /// <returns>The service collection.</returns>
-    public static IServiceCollection AddSqlServerVectorStore(this IServiceCollection services,
+    /// <inheritdoc cref="AddVectorStore"/>
+    public static IServiceCollection AddSqlServerVectorStore(
+        this IServiceCollection services,
+        Func<IServiceProvider, string> connectionStringProvider,
+        Func<IServiceProvider, SqlServerVectorStoreOptions>? optionsProvider = null,
+        ServiceLifetime lifetime = ServiceLifetime.Singleton)
+        => AddVectorStore(services, serviceKey: null, connectionStringProvider, optionsProvider, lifetime);
+
+    /// <inheritdoc cref="AddVectorStore"/>
+    public static IServiceCollection AddKeyedSqlServerVectorStore(
+        this IServiceCollection services,
+        object serviceKey,
         Func<IServiceProvider, string> connectionStringProvider,
         Func<IServiceProvider, SqlServerVectorStoreOptions>? optionsProvider = null,
         ServiceLifetime lifetime = ServiceLifetime.Singleton)
     {
-        Verify.NotNull(services);
-        Verify.NotNull(connectionStringProvider);
+        Verify.NotNull(serviceKey);
 
-        services.Add(new ServiceDescriptor(typeof(SqlServerVectorStore), sp =>
-        {
-            var connectionString = connectionStringProvider(sp);
-            var options = GetStoreOptions(sp, optionsProvider);
-            return new SqlServerVectorStore(connectionString, options);
-        }, lifetime));
-
-        services.Add(new ServiceDescriptor(typeof(VectorStore),
-            static sp => sp.GetRequiredService<SqlServerVectorStore>(), lifetime));
-
-        return services;
+        return AddVectorStore(services, serviceKey, connectionStringProvider, optionsProvider, lifetime);
     }
 
     /// <summary>
@@ -51,14 +46,14 @@ public static class SqlServerServiceCollectionExtensions
     /// <param name="optionsProvider">Options provider to further configure the vector store.</param>
     /// <param name="lifetime">The service lifetime for the store. Defaults to <see cref="ServiceLifetime.Singleton"/>.</param>
     /// <returns>The service collection.</returns>
-    public static IServiceCollection AddKeyedSqlServerVectorStore(this IServiceCollection services,
-        object serviceKey,
+    private static IServiceCollection AddVectorStore(
+        IServiceCollection services,
+        object? serviceKey,
         Func<IServiceProvider, string> connectionStringProvider,
-        Func<IServiceProvider, SqlServerVectorStoreOptions>? optionsProvider = null,
-        ServiceLifetime lifetime = ServiceLifetime.Singleton)
+        Func<IServiceProvider, SqlServerVectorStoreOptions>? optionsProvider,
+        ServiceLifetime lifetime)
     {
         Verify.NotNull(services);
-        Verify.NotNull(serviceKey);
         Verify.NotNull(connectionStringProvider);
 
         services.Add(new ServiceDescriptor(typeof(SqlServerVectorStore), serviceKey, (sp, _) =>
@@ -77,13 +72,21 @@ public static class SqlServerServiceCollectionExtensions
     /// <summary>
     /// Registers a <see cref="SqlServerCollection{TKey, TRecord}"/> as <see cref="VectorStoreCollection{TKey, TRecord}"/>, with the specified connection string and service lifetime.
     /// </summary>
-    /// <param name="services">The <see cref="IServiceCollection"/> to register the <see cref="VectorStoreCollection{TKey, TRecord}"/> on.</param>
-    /// <param name="collectionName">The name of the collection.</param>
-    /// <param name="connectionStringProvider">The connection string provider.</param>
-    /// <param name="optionsProvider">Options provider to further configure the collection.</param>
-    /// <param name="lifetime">The service lifetime for the store. Defaults to <see cref="ServiceLifetime.Singleton"/>.</param>
-    /// <returns>The service collection.</returns>
-    public static IServiceCollection AddSqlServerCollection<TKey, TRecord>(this IServiceCollection services,
+    /// <inheritdoc cref="AddCollection{TKey, TRecord}(IServiceCollection, object?, string, Func{IServiceProvider, string}, Func{IServiceProvider, SqlServerCollectionOptions}?, ServiceLifetime)"/>
+    public static IServiceCollection AddSqlServerCollection<TKey, TRecord>(
+        this IServiceCollection services,
+        string collectionName,
+        Func<IServiceProvider, string> connectionStringProvider,
+        Func<IServiceProvider, SqlServerCollectionOptions>? optionsProvider = null,
+        ServiceLifetime lifetime = ServiceLifetime.Singleton)
+        where TKey : notnull
+        where TRecord : class
+        => AddCollection<TKey, TRecord>(services, serviceKey: null, collectionName, connectionStringProvider, optionsProvider, lifetime);
+
+    /// <inheritdoc cref="AddCollection{TKey, TRecord}(IServiceCollection, object?, string, Func{IServiceProvider, string}, Func{IServiceProvider, SqlServerCollectionOptions}?, ServiceLifetime)"/>
+    public static IServiceCollection AddKeyedSqlServerCollection<TKey, TRecord>(
+        this IServiceCollection services,
+        object serviceKey,
         string collectionName,
         Func<IServiceProvider, string> connectionStringProvider,
         Func<IServiceProvider, SqlServerCollectionOptions>? optionsProvider = null,
@@ -91,20 +94,9 @@ public static class SqlServerServiceCollectionExtensions
         where TKey : notnull
         where TRecord : class
     {
-        Verify.NotNull(services);
-        Verify.NotNullOrWhiteSpace(collectionName);
-        Verify.NotNull(connectionStringProvider);
+        Verify.NotNull(serviceKey);
 
-        services.Add(new ServiceDescriptor(typeof(SqlServerCollection<TKey, TRecord>), sp =>
-        {
-            var connectionString = connectionStringProvider(sp);
-            var options = GetCollectionOptions(sp, optionsProvider);
-            return new SqlServerCollection<TKey, TRecord>(connectionString, collectionName, options);
-        }, lifetime));
-
-        RegisterAbstractions<TKey, TRecord>(services, lifetime, serviceKey: null);
-
-        return services;
+        return AddCollection<TKey, TRecord>(services, serviceKey, collectionName, connectionStringProvider, optionsProvider, lifetime);
     }
 
     /// <summary>
@@ -117,17 +109,17 @@ public static class SqlServerServiceCollectionExtensions
     /// <param name="optionsProvider">Options provider to further configure the collection.</param>
     /// <param name="lifetime">The service lifetime for the store. Defaults to <see cref="ServiceLifetime.Singleton"/>.</param>
     /// <returns>The service collection.</returns>
-    public static IServiceCollection AddKeyedSqlServerCollection<TKey, TRecord>(this IServiceCollection services,
-        object serviceKey,
+    private static IServiceCollection AddCollection<TKey, TRecord>(
+        this IServiceCollection services,
+        object? serviceKey,
         string collectionName,
         Func<IServiceProvider, string> connectionStringProvider,
-        Func<IServiceProvider, SqlServerCollectionOptions>? optionsProvider = null,
+        Func<IServiceProvider, SqlServerCollectionOptions?>? optionsProvider = null,
         ServiceLifetime lifetime = ServiceLifetime.Singleton)
         where TKey : notnull
         where TRecord : class
     {
         Verify.NotNull(services);
-        Verify.NotNull(serviceKey);
         Verify.NotNullOrWhiteSpace(collectionName);
         Verify.NotNull(connectionStringProvider);
 
@@ -138,7 +130,14 @@ public static class SqlServerServiceCollectionExtensions
             return new SqlServerCollection<TKey, TRecord>(connectionString, collectionName, options);
         }, lifetime));
 
-        RegisterAbstractions<TKey, TRecord>(services, lifetime, serviceKey);
+        services.Add(new ServiceDescriptor(typeof(VectorStoreCollection<TKey, TRecord>), serviceKey,
+            static (sp, key) => sp.GetRequiredKeyedService<SqlServerCollection<TKey, TRecord>>(key), lifetime));
+
+        services.Add(new ServiceDescriptor(typeof(IVectorSearchable<TRecord>), serviceKey,
+            static (sp, key) => sp.GetRequiredKeyedService<SqlServerCollection<TKey, TRecord>>(key), lifetime));
+
+        // Once HybridSearch supports get implemented (https://github.com/microsoft/semantic-kernel/issues/11080)
+        // we need to add IKeywordHybridSearchable abstraction here as well.
 
         return services;
     }
@@ -146,13 +145,21 @@ public static class SqlServerServiceCollectionExtensions
     /// <summary>
     /// Registers a <see cref="SqlServerCollection{TKey, TRecord}"/> as <see cref="VectorStoreCollection{TKey, TRecord}"/>, with the specified connection string and service lifetime.
     /// </summary>
-    /// <param name="services">The <see cref="IServiceCollection"/> to register the <see cref="VectorStoreCollection{TKey, TRecord}"/> on.</param>
-    /// <param name="collectionName">The name of the collection.</param>
-    /// <param name="connectionString">The connection string.</param>
-    /// <param name="options">Options to further configure the collection.</param>
-    /// <param name="lifetime">The service lifetime for the store. Defaults to <see cref="ServiceLifetime.Singleton"/>.</param>
-    /// <returns>The service collection.</returns>
-    public static IServiceCollection AddSqlServerCollection<TKey, TRecord>(this IServiceCollection services,
+    /// <inheritdoc cref="AddCollection{TKey, TRecord}(IServiceCollection, object?, string, string, SqlServerCollectionOptions?, ServiceLifetime)"/>/>
+    public static IServiceCollection AddSqlServerCollection<TKey, TRecord>(
+        this IServiceCollection services,
+        string collectionName,
+        string connectionString,
+        SqlServerCollectionOptions? options = null,
+        ServiceLifetime lifetime = ServiceLifetime.Singleton)
+        where TKey : notnull
+        where TRecord : class
+        => AddCollection<TKey, TRecord>(services, serviceKey: null, collectionName, connectionString, options, lifetime);
+
+    /// <inheritdoc cref="AddCollection{TKey, TRecord}(IServiceCollection, object?, string, string, SqlServerCollectionOptions?, ServiceLifetime)"/>/>
+    public static IServiceCollection AddKeyedSqlServerCollection<TKey, TRecord>(
+        this IServiceCollection services,
+        object serviceKey,
         string collectionName,
         string connectionString,
         SqlServerCollectionOptions? options = null,
@@ -160,20 +167,9 @@ public static class SqlServerServiceCollectionExtensions
         where TKey : notnull
         where TRecord : class
     {
-        Verify.NotNull(services);
-        Verify.NotNullOrWhiteSpace(collectionName);
-        Verify.NotNullOrWhiteSpace(connectionString);
+        Verify.NotNull(serviceKey);
 
-        services.Add(new ServiceDescriptor(typeof(SqlServerCollection<TKey, TRecord>),
-            sp =>
-            {
-                options = GetCollectionOptions(sp, _ => options);
-                return new SqlServerCollection<TKey, TRecord>(connectionString, collectionName, options);
-            }, lifetime));
-
-        RegisterAbstractions<TKey, TRecord>(services, lifetime, serviceKey: null);
-
-        return services;
+        return AddCollection<TKey, TRecord>(services, serviceKey, collectionName, connectionString, options, lifetime);
     }
 
     /// <summary>
@@ -186,8 +182,9 @@ public static class SqlServerServiceCollectionExtensions
     /// <param name="options">Options to further configure the collection.</param>
     /// <param name="lifetime">The service lifetime for the store. Defaults to <see cref="ServiceLifetime.Singleton"/>.</param>
     /// <returns>The service collection.</returns>
-    public static IServiceCollection AddKeyedSqlServerCollection<TKey, TRecord>(this IServiceCollection services,
-        object serviceKey,
+    private static IServiceCollection AddCollection<TKey, TRecord>(
+        this IServiceCollection services,
+        object? serviceKey,
         string collectionName,
         string connectionString,
         SqlServerCollectionOptions? options = null,
@@ -195,54 +192,30 @@ public static class SqlServerServiceCollectionExtensions
         where TKey : notnull
         where TRecord : class
     {
-        Verify.NotNull(services);
-        Verify.NotNull(serviceKey);
-        Verify.NotNullOrWhiteSpace(collectionName);
         Verify.NotNullOrWhiteSpace(connectionString);
 
-        services.Add(new ServiceDescriptor(typeof(SqlServerCollection<TKey, TRecord>), serviceKey,
-            (sp, _) =>
-            {
-                options = GetCollectionOptions(sp, _ => options);
-                return new SqlServerCollection<TKey, TRecord>(connectionString, collectionName, options);
-            }, lifetime));
-
-        RegisterAbstractions<TKey, TRecord>(services, lifetime, serviceKey);
-
-        return services;
+        return AddCollection<TKey, TRecord>(services, serviceKey, collectionName, _ => connectionString, _ => options, lifetime);
     }
 
-    private static void RegisterAbstractions<TKey, TRecord>(IServiceCollection services, ServiceLifetime lifetime, object? serviceKey)
-        where TKey : notnull
-        where TRecord : class
+    private static SqlServerVectorStoreOptions? GetStoreOptions(IServiceProvider sp, Func<IServiceProvider, SqlServerVectorStoreOptions?>? optionsProvider)
     {
-        // Once HybridSearch supports get implemented (https://github.com/microsoft/semantic-kernel/issues/11080)
-        // we need to add IKeywordHybridSearchable abstraction here as well.
-
-        services.Add(new ServiceDescriptor(typeof(VectorStoreCollection<TKey, TRecord>), serviceKey,
-            static (sp, key) => sp.GetRequiredKeyedService<SqlServerCollection<TKey, TRecord>>(key), lifetime));
-
-        services.Add(new ServiceDescriptor(typeof(IVectorSearchable<TRecord>), serviceKey,
-            static (sp, key) => sp.GetRequiredKeyedService<SqlServerCollection<TKey, TRecord>>(key), lifetime));
+        var options = optionsProvider?.Invoke(sp);
+        return new() // Create a new instance to avoid modifying the original options.
+        {
+            // Don't override the embedding generator if the user has provided one.
+            EmbeddingGenerator = options?.EmbeddingGenerator ?? sp.GetService<IEmbeddingGenerator>(),
+            Schema = options?.Schema,
+        };
     }
 
-    private static SqlServerVectorStoreOptions GetStoreOptions(IServiceProvider sp, Func<IServiceProvider, SqlServerVectorStoreOptions?>? optionsProvider)
+    private static SqlServerCollectionOptions? GetCollectionOptions(IServiceProvider sp, Func<IServiceProvider, SqlServerCollectionOptions?>? optionsProvider)
     {
-        var options = optionsProvider?.Invoke(sp) ?? new();
-
-        // We resolve the embedding generator only in case the user didn't provide one.
-        options.EmbeddingGenerator ??= sp.GetService<IEmbeddingGenerator>();
-
-        return options;
-    }
-
-    private static SqlServerCollectionOptions GetCollectionOptions(IServiceProvider sp, Func<IServiceProvider, SqlServerCollectionOptions?>? optionsProvider)
-    {
-        var options = optionsProvider?.Invoke(sp) ?? new();
-
-        // We resolve the embedding generator only in case the user didn't provide one.
-        options.EmbeddingGenerator ??= sp.GetService<IEmbeddingGenerator>();
-
-        return options;
+        var options = optionsProvider?.Invoke(sp);
+        return new() // Create a new instance to avoid modifying the original options.
+        {
+            // Don't override the embedding generator if the user has provided one.
+            EmbeddingGenerator = options?.EmbeddingGenerator ?? sp.GetService<IEmbeddingGenerator>(),
+            Schema = options?.Schema,
+        };
     }
 }
