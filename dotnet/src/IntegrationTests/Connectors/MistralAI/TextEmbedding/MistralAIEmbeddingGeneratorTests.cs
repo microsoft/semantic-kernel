6679b91d9a75437b7fb871081ca2c716f63794a1
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2025-05-09 14:50:18 +0100

    Ensuring Mistral and Amazon

--- dotnet/src/IntegrationTests/Connectors/MistralAI/TextEmbedding/MistralAIEmbeddingGeneratorTests.cs
+++ dotnet/src/IntegrationTests/Connectors/MistralAI/TextEmbedding/MistralAIEmbeddingGeneratorTests.cs
@@ -1,6 +1,5 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
-using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
@@ -10,40 +9,39 @@ using Xunit;
 namespace SemanticKernel.IntegrationTests.Connectors.MistralAI;
 
 /// <summary>
-/// Integration tests for <see cref="MistralAITextEmbeddingGenerationService"/>.
+/// Integration tests for <see cref="MistralAIEmbeddingGenerator"/>.
 /// </summary>
-[Obsolete("Temporary Tests for Obsolete MistralAITextEmbeddingGenerationService")]
-public sealed class MistralAITextEmbeddingGenerationServiceTests
+public sealed class MistralAIEmbeddingGeneratorTests
 {
     private readonly IConfigurationRoot _configuration;
 
-    public MistralAITextEmbeddingGenerationServiceTests()
+    public MistralAIEmbeddingGeneratorTests()
     {
         // Load configuration
         this._configuration = new ConfigurationBuilder()
             .AddJsonFile(path: "testsettings.json", optional: false, reloadOnChange: true)
             .AddJsonFile(path: "testsettings.development.json", optional: true, reloadOnChange: true)
             .AddEnvironmentVariables()
-            .AddUserSecrets<MistralAITextEmbeddingGenerationServiceTests>()
+            .AddUserSecrets<MistralAIEmbeddingGeneratorTests>()
             .Build();
     }
 
     [Fact(Skip = "This test is for manual verification.")]
-    public async Task MistralAITextGenerateEmbeddingsAsync()
+    public async Task MistralAIGenerateEmbeddingsAsync()
     {
         // Arrange
-        var model = this._configuration["MistralAI:EmbeddingModel"];
+        var model = this._configuration["MistralAI:EmbeddingModelId"];
         var apiKey = this._configuration["MistralAI:ApiKey"];
-        var service = new MistralAITextEmbeddingGenerationService(model!, apiKey!);
+        using var service = new MistralAIEmbeddingGenerator(model!, apiKey!);
 
         // Act
         List<string> data = ["Hello", "world"];
-        var response = await service.GenerateEmbeddingsAsync(data);
+        var response = await service.GenerateAsync(data);
 
         // Assert
         Assert.NotNull(response);
         Assert.Equal(2, response.Count);
-        Assert.Equal(1024, response[0].Length);
-        Assert.Equal(1024, response[1].Length);
+        Assert.Equal(1024, response[0].Vector.Length);
+        Assert.Equal(1024, response[1].Vector.Length);
     }
 }
