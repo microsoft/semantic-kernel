b8fd7a0e3b8399966e3e4972a5ba0b52239d7870
westey <164392973+westey-m@users.noreply.github.com>
2025-05-09 14:00:09 +0100

    Fix formatting

--- dotnet/src/SemanticKernel.UnitTests/Memory/WhiteboardBehaviorTests.cs
+++ dotnet/src/SemanticKernel.UnitTests/Memory/WhiteboardBehaviorTests.cs
@@ -1,9 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.AI;
