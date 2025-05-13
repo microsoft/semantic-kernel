01e1c6164f9aa32f6cafc7b6df4001ea354cde1e
Chris Rickman <crickman@microsoft.com>
2025-05-12 21:16:35 -0700

    Namespace

--- dotnet/src/SemanticKernel.Abstractions/Contents/StreamingAnnotationContent.cs
+++ dotnet/src/SemanticKernel.Abstractions/Contents/StreamingAnnotationContent.cs
@@ -1,6 +1,5 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 using System;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Text;
 using System.Text.Json.Serialization;
