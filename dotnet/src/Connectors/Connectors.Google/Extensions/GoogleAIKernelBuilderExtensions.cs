f8d905fd41930b0ccf7641670e79ab86881e2134
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2025-05-13 19:58:11 +0100

    Fix warnings

--- dotnet/src/Connectors/Connectors.Google/Extensions/GoogleAIKernelBuilderExtensions.cs
+++ dotnet/src/Connectors/Connectors.Google/Extensions/GoogleAIKernelBuilderExtensions.cs
@@ -2,7 +2,6 @@
 
 using System;
 using System.Net.Http;
-using System.Runtime.InteropServices;
 using Microsoft.Extensions.AI;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
