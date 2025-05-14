0390b0968fe045ef9acdfb56cee8502c8d6aee11
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2025-05-14 10:36:57 +0100

    .Net: Google - Add `IEmbeddingGenerator`  and obsolete  `ITextEmbeddingsGenerationService` - Phase 08 (#12042)
    ### Motivation and Context
    
    - Resolves Partically #10811

--- dotnet/samples/Concepts/Search/MyAzureAISearchPlugin.cs
+++ dotnet/samples/Concepts/Search/MyAzureAISearchPlugin.cs
@@ -6,6 +6,7 @@ using Azure;
 using Azure.Search.Documents;
 using Azure.Search.Documents.Indexes;
 using Azure.Search.Documents.Models;
+using Microsoft.Extensions.AI;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Embeddings;
@@ -160,10 +161,10 @@ public class AzureAISearchPlugin(ITestOutputHelper output) : BaseTest(output)
     /// It uses <see cref="IAzureAISearchService"/> to perform a request to Azure AI Search.
     /// </summary>
     private sealed class MyAzureAISearchPlugin(
-        ITextEmbeddingGenerationService textEmbeddingGenerationService,
+        IEmbeddingGenerator<string, Embedding<float>> embeddingGenerator,
         AzureAISearchPlugin.IAzureAISearchService searchService)
     {
-        private readonly ITextEmbeddingGenerationService _textEmbeddingGenerationService = textEmbeddingGenerationService;
+        private readonly IEmbeddingGenerator<string, Embedding<float>> _embeddingGenerator = embeddingGenerator;
         private readonly IAzureAISearchService _searchService = searchService;
 
         [KernelFunction("Search")]
@@ -174,7 +175,7 @@ public class AzureAISearchPlugin(ITestOutputHelper output) : BaseTest(output)
             CancellationToken cancellationToken = default)
         {
             // Convert string query to vector
-            ReadOnlyMemory<float> embedding = await this._textEmbeddingGenerationService.GenerateEmbeddingAsync(query, cancellationToken: cancellationToken);
+            ReadOnlyMemory<float> embedding = (await this._embeddingGenerator.GenerateAsync(query, cancellationToken: cancellationToken)).Vector;
 
             // Perform search
             return await this._searchService.SearchAsync(collection, embedding, searchFields, cancellationToken) ?? string.Empty;
--- dotnet/src/Connectors/Connectors.Google/Extensions/VertexAIServiceCollectionExtensions.DependencyInjection.cs
+++ dotnet/src/Connectors/Connectors.Google/Extensions/VertexAIServiceCollectionExtensions.DependencyInjection.cs
@@ -3,95 +3,99 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Connectors.Google;
 using Microsoft.SemanticKernel.Http;
-using Microsoft.SemanticKernel.Memory;
 
-namespace Microsoft.SemanticKernel;
+namespace Microsoft.Extensions.DependencyInjection;
 
 /// <summary>
-/// Provides extension methods for the <see cref="MemoryBuilder"/> class to configure VertexAI connector.
+/// Extensions for adding VertexAI generation services to the application.
 /// </summary>
-public static class VertexAIMemoryBuilderExtensions
+public static class VertexAIServiceCollectionExtensions
 {
     /// <summary>
-    /// Add VertexAI embeddings generation service to the memory builder.
+    /// Add Vertex AI <see cref="IEmbeddingGenerator{String, Embedding}"/> to the specified service collection.
     /// </summary>
-    /// <param name="builder">The <see cref="MemoryBuilder"/> instance</param>
-    /// <param name="modelId">The model for text generation.</param>
+    /// <param name="services">The service collection to add the Gemini Embeddings Generation service to.</param>
+    /// <param name="modelId">The model for embeddings generation.</param>
     /// <param name="bearerTokenProvider">The Bearer Key provider for authentication.</param>
     /// <param name="location">The location to process the request</param>
     /// <param name="projectId">Your project ID</param>
     /// <param name="apiVersion">The version of the Vertex API.</param>
+    /// <param name="serviceId">Optional service ID.</param>
     /// <param name="httpClient">The optional custom HttpClient.</param>
-    /// <returns>The updated memory builder.</returns>
+    /// <returns>The updated service collection.</returns>
     /// <remarks>
     /// This <paramref name="bearerTokenProvider"/> will be called on every request,
     /// when providing the token consider using caching strategy and refresh token logic
     /// when it is expired or close to expiration.
     /// </remarks>
-    [Obsolete("This method is now obsolete and will be removed in future. Use an EmbeddingGenerator with your VectorStore instead.")]
-    public static MemoryBuilder WithVertexAITextEmbeddingGeneration(
-        this MemoryBuilder builder,
+    public static IServiceCollection AddVertexAIEmbeddingGenerator(
+        this IServiceCollection services,
         string modelId,
         Func<ValueTask<string>> bearerTokenProvider,
         string location,
         string projectId,
         VertexAIVersion apiVersion = VertexAIVersion.V1,
+        string? serviceId = null,
         HttpClient? httpClient = null)
     {
-        Verify.NotNull(builder);
+        Verify.NotNull(services);
         Verify.NotNull(modelId);
         Verify.NotNull(bearerTokenProvider);
         Verify.NotNull(location);
         Verify.NotNull(projectId);
 
-        return builder.WithTextEmbeddingGeneration((loggerFactory, builderHttpClient) =>
-            new VertexAITextEmbeddingGenerationService(
+        return services.AddKeyedSingleton<IEmbeddingGenerator<string, Embedding<float>>>(serviceId, (serviceProvider, _) =>
+            new VertexAIEmbeddingGenerator(
                 modelId: modelId,
                 bearerTokenProvider: bearerTokenProvider,
                 location: location,
                 projectId: projectId,
                 apiVersion: apiVersion,
-                httpClient: HttpClientProvider.GetHttpClient(httpClient ?? builderHttpClient),
-                loggerFactory: loggerFactory));
+                httpClient: HttpClientProvider.GetHttpClient(httpClient, serviceProvider),
+                loggerFactory: serviceProvider.GetService<ILoggerFactory>()));
     }
 
     /// <summary>
-    /// Add VertexAI embeddings generation service to the memory builder.
+    /// Add Vertex AI <see cref="IEmbeddingGenerator{String, Embedding}"/> to the specified service collection.
     /// </summary>
-    /// <param name="builder">The <see cref="MemoryBuilder"/> instance</param>
-    /// <param name="modelId">The model for text generation.</param>
+    /// <param name="services">The service collection to add the Gemini Embeddings Generation service to.</param>
+    /// <param name="modelId">The model for embeddings generation.</param>
     /// <param name="bearerKey">The Bearer Key for authentication.</param>
     /// <param name="location">The location to process the request</param>
     /// <param name="projectId">Your project ID</param>
     /// <param name="apiVersion">The version of the Vertex API.</param>
+    /// <param name="serviceId">Optional service ID.</param>
     /// <param name="httpClient">The optional custom HttpClient.</param>
-    /// <returns>The updated memory builder.</returns>
-    [Obsolete("This method is now obsolete and will be removed in future. Use an EmbeddingGenerator with your VectorStore instead.")]
-    public static MemoryBuilder WithVertexAITextEmbeddingGeneration(
-        this MemoryBuilder builder,
+    /// <returns>The updated service collection.</returns>
+    public static IServiceCollection AddVertexAIEmbeddingGenerator(
+        this IServiceCollection services,
         string modelId,
         string bearerKey,
         string location,
         string projectId,
         VertexAIVersion apiVersion = VertexAIVersion.V1,
+        string? serviceId = null,
         HttpClient? httpClient = null)
     {
-        Verify.NotNull(builder);
+        Verify.NotNull(services);
         Verify.NotNull(modelId);
         Verify.NotNull(bearerKey);
         Verify.NotNull(location);
         Verify.NotNull(projectId);
 
-        return builder.WithTextEmbeddingGeneration((loggerFactory, builderHttpClient) =>
-            new VertexAITextEmbeddingGenerationService(
+        return services.AddKeyedSingleton<IEmbeddingGenerator<string, Embedding<float>>>(serviceId, (serviceProvider, _) =>
+            new VertexAIEmbeddingGenerator(
                 modelId: modelId,
                 bearerKey: bearerKey,
                 location: location,
                 projectId: projectId,
                 apiVersion: apiVersion,
-                httpClient: HttpClientProvider.GetHttpClient(httpClient ?? builderHttpClient),
-                loggerFactory: loggerFactory));
+                httpClient: HttpClientProvider.GetHttpClient(httpClient, serviceProvider),
+                loggerFactory: serviceProvider.GetService<ILoggerFactory>()));
     }
 }
