173443ed3b7352dce135209fc55470000871b44c
Ben Thomas <ben.thomas@microsoft.com>
2025-05-15 08:07:40 -0700

    Removing old projects.

--- dotnet/samples/Demos/ProcessWithCloudEvents.old/ProcessWithCloudEvents.Client/src/components/Icons.ts
+++
@@ -1,9 +0,0 @@
-/*
- *   Copyright (c) 2025 Microsoft
- *   All rights reserved.
- */
-import { CheckmarkRegular, DismissRegular } from "@fluentui/react-icons";
-
-export const CheckIcon = CheckmarkRegular;
-export const RejectIcon = DismissRegular;
-export const ExitIcon = DismissRegular;
--- dotnet/samples/Demos/ProcessWithCloudEvents.old/ProcessWithCloudEvents.Client/src/vite-env.d.ts
+++
@@ -1,5 +0,0 @@
-/*
- *   Copyright (c) 2025 Microsoft
- *   All rights reserved.
- */
-/// <reference types="vite/client" />
