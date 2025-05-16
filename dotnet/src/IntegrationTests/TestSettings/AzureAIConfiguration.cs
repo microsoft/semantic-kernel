a984b419727285abad4c7a750149777a059d092c
Chris <66376200+crickman@users.noreply.github.com>
2025-05-16 16:16:24 -0700

    .Net Agents - Fix assistant settings for integration test fixture (#12138)
    ### Motivation and Context
    <!-- Thank you for your contribution to the semantic-kernel repo!
    Please help reviewers and future users, providing the following
    information:
      1. Why is this change required?
      2. What problem does it solve?
      3. What scenario does it contribute to?
      4. If it fixes an open issue, please link to the issue here.
    -->
    
    `OpenAIAssistantAgentFixture` mistakenly referencing
    `AzureAIConfiguration` instead of `AzureOpenAIConfiguration`
    
    ### Description
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    `AzureAIConfiguration.Endpoint` needs to be reserved for GA Foundry
    projects.
    
    ### Contribution Checklist
    <!-- Before submitting this PR, please make sure: -->
    
    - [X] The code builds clean without any errors or warnings
    - [X] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [X] All unit tests pass, and I have added new tests where possible
    - [X] I didn't break anyone :smile:

--- dotnet/src/IntegrationTests/TestSettings/AzureAIConfiguration.cs
+++ dotnet/src/IntegrationTests/TestSettings/AzureAIConfiguration.cs
@@ -6,10 +6,8 @@ namespace SemanticKernel.IntegrationTests.TestSettings;
 
 [SuppressMessage("Performance", "CA1812:Internal class that is apparently never instantiated",
     Justification = "Configuration classes are instantiated through IConfiguration.")]
-internal sealed class AzureAIConfiguration(string endpoint, string connectionString, string chatModelId)
+internal sealed class AzureAIConfiguration(string connectionString, string chatModelId)
 {
-    public string Endpoint { get; set; } = endpoint;
-
     public string ConnectionString { get; set; } = connectionString;
 
     public string ChatModelId { get; set; } = chatModelId;
