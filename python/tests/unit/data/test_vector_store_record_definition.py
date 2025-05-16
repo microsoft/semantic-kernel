3dd455ec704bd72c3a1e68d10125a6467539166e
eavanvalkenburg <github@vanvalkenburg.eu>
2025-05-16 09:41:25 +0200

    2nd update AZS

--- python/tests/unit/data/test_vector_store_record_definition.py
+++ python/tests/unit/data/test_vector_store_record_definition.py
@@ -3,19 +3,15 @@
 from pydantic import ValidationError
 from pytest import raises
 
-from semantic_kernel.data import (
-    VectorStoreRecordDataField,
-    VectorStoreRecordDefinition,
-    VectorStoreRecordKeyField,
-)
+from semantic_kernel.data import VectorStoreRecordDataField, VectorStoreRecordDefinition, VectorStoreRecordKeyField
 from semantic_kernel.data.record_definition import VectorStoreRecordVectorField
 from semantic_kernel.exceptions import VectorStoreModelException
 
 
 def test_vector_store_record_definition():
-    id_field = VectorStoreRecordKeyField()
-    vsrd = VectorStoreRecordDefinition(fields={"id": id_field})
-    assert vsrd.fields == {"id": VectorStoreRecordKeyField(name="id")}
+    id_field = VectorStoreRecordKeyField(name="id")
+    vsrd = VectorStoreRecordDefinition(fields=[id_field])
+    assert vsrd.fields == [VectorStoreRecordKeyField(name="id")]
     assert vsrd.key_field_name == "id"
     assert vsrd.key_field == id_field
     assert vsrd.field_names == ["id"]
@@ -29,56 +25,58 @@ def test_vector_store_record_definition():
 
 def test_no_fields_fail():
     with raises(VectorStoreModelException):
-        VectorStoreRecordDefinition(fields={})
+        VectorStoreRecordDefinition(fields=[])
 
 
 def test_no_name_fields_fail():
     with raises(ValidationError):
-        VectorStoreRecordDefinition(fields={None: VectorStoreRecordKeyField()})  # type: ignore
+        VectorStoreRecordDefinition(fields=[VectorStoreRecordKeyField(name=None)])  # type: ignore
     with raises(VectorStoreModelException):
-        VectorStoreRecordDefinition(fields={"": VectorStoreRecordKeyField()})
+        VectorStoreRecordDefinition(fields=[VectorStoreRecordKeyField(name="")])
 
 
 def test_no_key_field_fail():
     with raises(VectorStoreModelException):
-        VectorStoreRecordDefinition(fields={"content": VectorStoreRecordDataField()})
+        VectorStoreRecordDefinition(fields=[VectorStoreRecordDataField(name="content")])
 
 
 def test_multiple_key_field_fail():
     with raises(VectorStoreModelException):
-        VectorStoreRecordDefinition(fields={"key1": VectorStoreRecordKeyField(), "key2": VectorStoreRecordKeyField()})
+        VectorStoreRecordDefinition(
+            fields=[VectorStoreRecordKeyField(name="key1"), VectorStoreRecordKeyField(name="key2")]
+        )
 
 
 def test_vector_and_non_vector_field_names():
     definition = VectorStoreRecordDefinition(
-        fields={
-            "id": VectorStoreRecordKeyField(),
-            "content": VectorStoreRecordDataField(),
-            "vector": VectorStoreRecordVectorField(),
-        }
+        fields=[
+            VectorStoreRecordKeyField(name="id"),
+            VectorStoreRecordDataField(name="content"),
+            VectorStoreRecordVectorField(name="vector", dimensions=5),
+        ]
     )
     assert definition.vector_field_names == ["vector"]
-    assert definition.non_vector_field_names == ["id", "content"]
+    assert definition.data_field_names == ["content"]
 
 
 def test_try_get_vector_field():
     definition = VectorStoreRecordDefinition(
-        fields={
-            "id": VectorStoreRecordKeyField(),
-            "content": VectorStoreRecordDataField(),
-            "vector": VectorStoreRecordVectorField(),
-        }
+        fields=[
+            VectorStoreRecordKeyField(name="id"),
+            VectorStoreRecordDataField(name="content"),
+            VectorStoreRecordVectorField(name="vector", dimensions=5),
+        ]
     )
-    assert definition.try_get_vector_field() == definition.fields["vector"]
-    assert definition.try_get_vector_field("vector") == definition.fields["vector"]
+    assert definition.try_get_vector_field() == definition.fields[2]
+    assert definition.try_get_vector_field("vector") == definition.fields[2]
 
 
 def test_try_get_vector_field_none():
     definition = VectorStoreRecordDefinition(
-        fields={
-            "id": VectorStoreRecordKeyField(),
-            "content": VectorStoreRecordDataField(),
-        }
+        fields=[
+            VectorStoreRecordKeyField(name="id"),
+            VectorStoreRecordDataField(name="content"),
+        ]
     )
     assert definition.try_get_vector_field() is None
     with raises(VectorStoreModelException, match="Field vector not found."):
@@ -87,10 +85,10 @@ def test_try_get_vector_field_none():
 
 def test_try_get_vector_field_wrong_name_fail():
     definition = VectorStoreRecordDefinition(
-        fields={
-            "id": VectorStoreRecordKeyField(),
-            "content": VectorStoreRecordDataField(),
-        }
+        fields=[
+            VectorStoreRecordKeyField(name="id"),
+            VectorStoreRecordDataField(name="content"),
+        ]
     )
     with raises(VectorStoreModelException, match="Field content is not a vector field."):
         definition.try_get_vector_field("content")
@@ -98,11 +96,11 @@ def test_try_get_vector_field_wrong_name_fail():
 
 def test_get_field_names():
     definition = VectorStoreRecordDefinition(
-        fields={
-            "id": VectorStoreRecordKeyField(),
-            "content": VectorStoreRecordDataField(),
-            "vector": VectorStoreRecordVectorField(),
-        }
+        fields=[
+            VectorStoreRecordKeyField(name="id"),
+            VectorStoreRecordDataField(name="content"),
+            VectorStoreRecordVectorField(name="vector", dimensions=5),
+        ]
     )
     assert definition.get_field_names() == ["id", "content", "vector"]
     assert definition.get_field_names(include_vector_fields=False) == ["id", "content"]
