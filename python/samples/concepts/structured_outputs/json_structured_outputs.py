01c04cc0f86be62e697d7705ffcadc924340c554
Evan Mattson <evan.mattson@microsoft.com>
2025-05-20 17:43:57 +0900

    Support structured outputs with Azure AI inference chat completion.

--- python/samples/concepts/structured_outputs/json_structured_outputs.py
+++ python/samples/concepts/structured_outputs/json_structured_outputs.py
@@ -2,106 +2,91 @@
 
 import asyncio
 
+from pydantic import BaseModel
+
+from samples.concepts.setup.chat_completion_services import Services, get_chat_completion_service_and_request_settings
 from semantic_kernel import Kernel
-from semantic_kernel.connectors.ai.function_choice_behavior import FunctionChoiceBehavior
-from semantic_kernel.connectors.ai.open_ai.services.azure_chat_completion import AzureChatCompletion
-from semantic_kernel.connectors.ai.open_ai.services.open_ai_chat_completion import OpenAIChatCompletion
-from semantic_kernel.contents import ChatHistory
-from semantic_kernel.contents.streaming_chat_message_content import StreamingChatMessageContent
+from semantic_kernel.connectors.ai import FunctionChoiceBehavior
+from semantic_kernel.contents import ChatHistory, StreamingChatMessageContent
 
-###################################################################
-# The following sample demonstrates how to create a chat          #
-# completion call that assists users in solving math problems.    #
-# The bot guides the user step-by-step through the solution       #
-# process using a structured output format based on either a      #
-# Pydantic model or a non-Pydantic model.                         #
-###################################################################
+"""
+The following sample demonstrates how to create a chat
+completion call that assists users in solving math problems.
+The bot guides the user step-by-step through the solution
+process using a structured output format based on either a
+Pydantic model or a non-Pydantic model.
 
 
-###################################################################
-# NOTE: If using Azure OpenAI the the following is required:
-# - access to gpt-4o-2024-08-06
-# - the 2024-08-01-preview API version
-# - if using a token instead of an API KEY, you must have the
-#    `Cognitive Services OpenAI Contributor` role assigned to your
-#    Azure AD user.
-# - flip the `use_azure_openai` flag to `True`
-###################################################################
-use_azure_openai = False
+NOTE: If using Azure OpenAI the the following is required:
+- access to gpt-4o-2024-08-06
+- the 2024-08-01-preview API version
+- if using a token instead of an API KEY, you must have the
+   `Cognitive Services OpenAI Contributor` role assigned to your
+   Azure AD user.
+- flip the `use_azure_openai` flag to `True`
+"""
 
 system_message = """
 You are a helpful math tutor. Guide the user through the solution step by step.
 """
 
+"""
+Define the Pydantic model that represents the
+structured output from the OpenAI service. This model will be
+used to parse the structured output from the OpenAI service,
+and ensure that the model correctly outputs the schema based
+on the Pydantic model.
+"""
 
-###################################################################
-# OPTION 1: Define the Pydantic model that represents the
-# structured output from the OpenAI service. This model will be
-# used to parse the structured output from the OpenAI service,
-# and ensure that the model correctly outputs the schema based
-# on the Pydantic model.
-from semantic_kernel.kernel_pydantic import KernelBaseModel  # noqa: E402
+# Note: The `extra=forbid` means to forbid extra fields during model initialization
+# It is required to ensure that the model is strict and does not
+# accept any extra fields that are not defined in the model.
 
 
-class Step(KernelBaseModel):
+class Step(BaseModel):
     explanation: str
     output: str
 
 
-class Reasoning(KernelBaseModel):
+class Reasoning(BaseModel):
     steps: list[Step]
     final_answer: str
 
 
-###################################################################
-
-
-# OPTION 2: Define a non-Pydantic model that should represent the
-# structured output from the OpenAI service. This model will be
-# converted to the proper JSON Schema and sent to the LLM.
-# Uncomment the follow lines and comment out the Pydantic model
-# above to use this option.
-# class Step:
-#     explanation: str
-#     output: str
-
-
-# class Reasoning:
-#     steps: list[Step]
-#     final_answer: str
-
-
-###################################################################
-
 kernel = Kernel()
 
-service_id = "structured-output"
-if use_azure_openai:
-    chat_service = AzureChatCompletion(
-        service_id=service_id,
-    )
-else:
-    chat_service = OpenAIChatCompletion(
-        service_id=service_id,
-    )
-kernel.add_service(chat_service)
+# You can select from the following chat completion services:
+# Note: the model must allow for structured outputs.
+# - Services.OPENAI
+# - Services.AZURE_OPENAI
+# - Services.AZURE_AI_INFERENCE
+# - Services.ANTHROPIC
+# - Services.BEDROCK
+# - Services.GOOGLE_AI
+# - Services.MISTRAL_AI
+# - Services.OLLAMA
+# - Services.ONNX
+# - Services.VERTEX_AI
+# - Services.DEEPSEEK
+# Please make sure you have configured your environment correctly for the selected chat completion service.
+chat_completion_service, request_settings = get_chat_completion_service_and_request_settings(Services.AZURE_OPENAI)
+kernel.add_service(chat_completion_service)
 
-req_settings = kernel.get_prompt_execution_settings_from_service_id(service_id=service_id)
-req_settings.max_tokens = 2000
-req_settings.temperature = 0.7
-req_settings.top_p = 0.8
-req_settings.function_choice_behavior = FunctionChoiceBehavior.Auto(filters={"excluded_plugins": ["chat"]})
+request_settings.max_tokens = 2000
+request_settings.temperature = 0.7
+request_settings.top_p = 0.8
+request_settings.function_choice_behavior = FunctionChoiceBehavior.Auto(filters={"excluded_plugins": ["chat"]})
 
 # NOTE: This is the key setting in this example that tells the OpenAI service
 # to return structured output based on the Pydantic model Reasoning.
-req_settings.response_format = Reasoning
+request_settings.response_format = Reasoning
 
 
 chat_function = kernel.add_function(
     prompt=system_message + """{{$chat_history}}""",
     function_name="chat",
     plugin_name="chat",
-    prompt_execution_settings=req_settings,
+    prompt_execution_settings=request_settings,
 )
 
 history = ChatHistory()
