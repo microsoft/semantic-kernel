425b8449942eee6b9230c319c8a894f2d3e4a0be
Eduard van Valkenburg <eavanvalkenburg@users.noreply.github.com>
2025-05-21 21:27:31 +0200

    Python: Move old and deprecate (#12121)
    ### Motivation and Context
    
    <!-- Thank you for your contribution to the semantic-kernel repo!
    Please help reviewers and future users, providing the following
    information:
      1. Why is this change required?
      2. What problem does it solve?
      3. What scenario does it contribute to?
      4. If it fixes an open issue, please link to the issue here.
    -->
    Move all the old connectors to memory_store and deprecated old memory
    classes
    
    ### Description
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [ ] The code builds clean without any errors or warnings
    - [ ] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [ ] All unit tests pass, and I have added new tests where possible
    - [ ] I didn't break anyone :smile:

--- python/semantic_kernel/connectors/memory/azure_cosmosdb/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) Microsoft. All rights reserved.
-
-from semantic_kernel.connectors.memory.azure_cosmosdb.azure_cosmos_db_memory_store import (
-    AzureCosmosDBMemoryStore,
-)
-from semantic_kernel.connectors.memory.azure_cosmosdb.azure_cosmosdb_settings import AzureCosmosDBSettings
-
-__all__ = ["AzureCosmosDBMemoryStore", "AzureCosmosDBSettings"]
