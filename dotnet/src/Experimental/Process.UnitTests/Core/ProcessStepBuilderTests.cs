52b0f6e3d8013bffd72498b74a9401b9fd20b33d
Estefanía Tenorio <8483207+esttenorio@users.noreply.github.com>
2025-05-28 14:36:11 -0700

    unused directives

--- dotnet/src/Experimental/Process.UnitTests/Core/ProcessStepBuilderTests.cs
+++ dotnet/src/Experimental/Process.UnitTests/Core/ProcessStepBuilderTests.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
 using System.Collections.Generic;
-using Microsoft.SemanticKernel.Process.Models;
 using Xunit;
 
 namespace Microsoft.SemanticKernel.Process.Core.UnitTests;
