27ad65a4ef7a14942b32ed110e6df91c6d84a8d1
SergeyMenshykh <sergemenshikh@gmail.com>
2025-05-29 18:54:19 +0100

    change access modifier

--- dotnet/src/SemanticKernel.Core/Functions/ContextualSelection/FunctionStoreLoggingExtensions.cs
+++ dotnet/src/SemanticKernel.Core/Functions/ContextualSelection/FunctionStoreLoggingExtensions.cs
@@ -22,7 +22,7 @@ internal static class FunctionStoreLoggingExtensions
         }
     }
 
-    public static void LogFunctionsSearchResults(this ILogger logger, string context, int maxNumberOfFunctionsToReturn, IList<VectorSearchResult<Dictionary<string, object?>>> results)
+    internal static void LogFunctionsSearchResults(this ILogger logger, string context, int maxNumberOfFunctionsToReturn, IList<VectorSearchResult<Dictionary<string, object?>>> results)
     {
         logger.LogInformation("ContextualFunctionProvider: Search returned {Count} functions, with a maximum limit of {MaxCount}", results.Count, maxNumberOfFunctionsToReturn);
 
