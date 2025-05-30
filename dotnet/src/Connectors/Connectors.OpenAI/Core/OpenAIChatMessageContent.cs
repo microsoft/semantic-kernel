1519170f723441abfd536ef0d236c42eae304f07
Roger Barreto <19890735+RogerBarreto@users.noreply.github.com>
2025-05-30 17:48:28 +0100

    Fixing serialization / deserialization OpenAIMessageContent

--- dotnet/src/Connectors/Connectors.OpenAI/Core/OpenAIChatMessageContent.cs
+++ dotnet/src/Connectors/Connectors.OpenAI/Core/OpenAIChatMessageContent.cs
@@ -1,7 +1,10 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using Microsoft.SemanticKernel.ChatCompletion;
 using OpenAI.Chat;
 using OpenAIChatCompletion = OpenAI.Chat.ChatCompletion;
@@ -22,6 +25,17 @@ public sealed class OpenAIChatMessageContent : ChatMessageContent
     /// Gets the metadata key for the list of <see cref="ChatToolCall"/>.
     /// </summary>
     internal static string FunctionToolCallsProperty => "ChatResponseMessage.FunctionToolCalls";
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="OpenAIChatMessageContent"/> class.
+    /// This constructor is for internal use and JSON deserialization.
+    /// </summary>
+    [JsonConstructor]
+    internal OpenAIChatMessageContent()
+    {
+        this.Role = AuthorRole.User; // Default role
+        this.ToolCalls = [];
+    }
 
     /// <summary>
     /// Initializes a new instance of the <see cref="OpenAIChatMessageContent"/> class.
@@ -69,7 +83,8 @@ public sealed class OpenAIChatMessageContent : ChatMessageContent
     /// <summary>
     /// A list of the tools called by the model.
     /// </summary>
-    public IReadOnlyList<ChatToolCall> ToolCalls { get; }
+    [JsonConverter(typeof(ChatToolCallListJsonConverter))]
+    public IReadOnlyList<ChatToolCall> ToolCalls { get; set; }
 
     /// <summary>
     /// Retrieve the resulting function from the chat result.
@@ -130,5 +145,108 @@ public sealed class OpenAIChatMessageContent : ChatMessageContent
         }
 
         return original;
+    }
+}
+
+/// <summary>
+/// JSON converter for IReadOnlyList&lt;ChatToolCall&gt; that handles serialization and deserialization
+/// of ChatToolCall objects using their basic properties.
+/// </summary>
+internal sealed class ChatToolCallListJsonConverter : JsonConverter<IReadOnlyList<ChatToolCall>>
+{
+    public override IReadOnlyList<ChatToolCall> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType == JsonTokenType.Null)
+        {
+            return [];
+        }
+
+        if (reader.TokenType != JsonTokenType.StartArray)
+        {
+            throw new JsonException("Expected array for ChatToolCall list");
+        }
+
+        var toolCalls = new List<ChatToolCall>();
+
+        while (reader.Read())
+        {
+            if (reader.TokenType == JsonTokenType.EndArray)
+            {
+                break;
+            }
+
+            if (reader.TokenType == JsonTokenType.StartObject)
+            {
+                var toolCall = ReadChatToolCall(ref reader);
+                if (toolCall != null)
+                {
+                    toolCalls.Add(toolCall);
+                }
+            }
+        }
+
+        return toolCalls;
+    }
+
+    public override void Write(Utf8JsonWriter writer, IReadOnlyList<ChatToolCall> value, JsonSerializerOptions options)
+    {
+        writer.WriteStartArray();
+
+        foreach (var toolCall in value)
+        {
+            WriteChatToolCall(writer, toolCall);
+        }
+
+        writer.WriteEndArray();
+    }
+
+    private static ChatToolCall? ReadChatToolCall(ref Utf8JsonReader reader)
+    {
+        string? id = null;
+        string? functionName = null;
+        string? arguments = null;
+
+        while (reader.Read())
+        {
+            if (reader.TokenType == JsonTokenType.EndObject)
+            {
+                break;
+            }
+
+            if (reader.TokenType == JsonTokenType.PropertyName)
+            {
+                var propertyName = reader.GetString();
+                reader.Read();
+
+                switch (propertyName)
+                {
+                    case "Id":
+                        id = reader.GetString();
+                        break;
+                    case "FunctionName":
+                        functionName = reader.GetString();
+                        break;
+                    case "FunctionArguments":
+                        arguments = reader.GetString();
+                        break;
+                }
+            }
+        }
+
+        if (id != null && functionName != null && arguments != null)
+        {
+            return ChatToolCall.CreateFunctionToolCall(id, functionName, BinaryData.FromString(arguments));
+        }
+
+        return null;
+    }
+
+    private static void WriteChatToolCall(Utf8JsonWriter writer, ChatToolCall toolCall)
+    {
+        writer.WriteStartObject();
+        writer.WriteString("Id", toolCall.Id);
+        writer.WriteString("FunctionName", toolCall.FunctionName);
+        writer.WriteString("FunctionArguments", toolCall.FunctionArguments.ToString());
+        writer.WriteEndObject();
     }
 }
