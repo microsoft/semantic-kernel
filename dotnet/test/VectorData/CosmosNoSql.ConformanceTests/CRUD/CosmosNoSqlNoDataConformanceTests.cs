b98a556681f2bdbf7b8630eabd98d6cc38b0e5e6
Shay Rojansky <roji@roji.org>
2025-05-31 10:00:49 +0200

    Reorganize MEVD projects

---
+++ dotnet/test/VectorData/CosmosNoSql.ConformanceTests/CRUD/CosmosNoSqlNoDataConformanceTests.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using CosmosNoSql.ConformanceTests.Support;
+using VectorData.ConformanceTests.CRUD;
+using VectorData.ConformanceTests.Support;
+using Xunit;
+
+namespace CosmosNoSql.ConformanceTests.CRUD;
+
+public class CosmosNoSqlNoDataConformanceTests(CosmosNoSqlNoDataConformanceTests.Fixture fixture)
+    : NoDataConformanceTests<string>(fixture), IClassFixture<CosmosNoSqlNoDataConformanceTests.Fixture>
+{
+    public new class Fixture : NoDataConformanceTests<string>.Fixture
+    {
+        public override TestStore TestStore => CosmosNoSqlTestStore.Instance;
+    }
+}
---
+++ dotnet/test/VectorData/VectorData.ConformanceTests/Xunit/ConditionalTheoryAttribute.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Xunit;
+using Xunit.Sdk;
+
+namespace VectorData.ConformanceTests.Xunit;
+
+[AttributeUsage(AttributeTargets.Method)]
+[XunitTestCaseDiscoverer("VectorDataSpecificationTests.Xunit.ConditionalTheoryDiscoverer", "VectorDataIntegrationTests")]
+public sealed class ConditionalTheoryAttribute : TheoryAttribute;
