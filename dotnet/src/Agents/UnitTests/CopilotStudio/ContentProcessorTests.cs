f7ef37b9c5acd17c7215318c2158e4a66c438a54
Chris Rickman <crickman@microsoft.com>
2025-06-04 15:00:24 -0700

    Namespace

--- dotnet/src/Agents/UnitTests/CopilotStudio/ContentProcessorTests.cs
+++ dotnet/src/Agents/UnitTests/CopilotStudio/ContentProcessorTests.cs
@@ -2,7 +2,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.SemanticKernel;
