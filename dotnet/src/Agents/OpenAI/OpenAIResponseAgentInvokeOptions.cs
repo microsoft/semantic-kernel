aabfc9c0c26583d8de1a097f52c2aaf73c78a9b1
Mark Wallace <127216156+markwallace-microsoft@users.noreply.github.com>
2025-06-10 15:26:17 +0100

    .Net: Feature OpenAI Response Agent (#11498)
    ### Motivation and Context
    
    Semantic Kernel `Agent` implementation backed by the Open AI Response
    API.
    
    ### Description
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    Add agent, samples, and tests for `OpenAIResponseAgent`
    
    ### Contribution Checklist
    <!-- Before submitting this PR, please make sure: -->
    
    - [X] The code builds clean without any errors or warnings
    - [X] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [X] All unit tests pass, and I have added new tests where possible
    - [X] I didn't break anyone :smile:
    
    ---------
    
    Co-authored-by: Chris <66376200+crickman@users.noreply.github.com>
    Co-authored-by: Chris Rickman <crickman@microsoft.com>
    Co-authored-by: SergeyMenshykh <68852919+SergeyMenshykh@users.noreply.github.com>

---
+++ dotnet/src/Agents/OpenAI/OpenAIResponseAgentInvokeOptions.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using OpenAI.Responses;
+
+namespace Microsoft.SemanticKernel.Agents.OpenAI;
+
+/// <summary>
+/// Optional parameters for <see cref="OpenAIResponseAgent"/> invocation.
+/// </summary>
+public sealed class OpenAIResponseAgentInvokeOptions : AgentInvokeOptions
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="OpenAIResponseAgentInvokeOptions"/> class.
+    /// </summary>
+    public OpenAIResponseAgentInvokeOptions()
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="OpenAIResponseAgentInvokeOptions"/> class by cloning the provided options.
+    /// </summary>
+    /// <param name="options">The options to clone.</param>
+    public OpenAIResponseAgentInvokeOptions(AgentInvokeOptions options)
+        : base(options)
+    {
+        Verify.NotNull(options);
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="OpenAIResponseAgentInvokeOptions"/> class by cloning the provided options.
+    /// </summary>
+    /// <param name="options">The options to clone.</param>
+    public OpenAIResponseAgentInvokeOptions(OpenAIResponseAgentInvokeOptions options)
+        : base(options)
+    {
+        Verify.NotNull(options);
+    }
+
+    /// <summary>
+    /// Gets or initializes the options used for creating a response.
+    /// </summary>
+    public ResponseCreationOptions? ResponseCreationOptions { get; set; }
+}
