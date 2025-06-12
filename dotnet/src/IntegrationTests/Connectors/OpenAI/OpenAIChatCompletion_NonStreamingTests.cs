4ab3d570dee62b5d3c43a134323a3eeb7621422f
westey <164392973+westey-m@users.noreply.github.com>
2025-06-12 08:43:26 +0100

    .Net: Adding retry to flakey test and switch to a different model (#12439)
    ### Description
    
    Adding retry to flakey test and switch to a different model
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [ ] The code builds clean without any errors or warnings
    - [ ] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [ ] All unit tests pass, and I have added new tests where possible
    - [ ] I didn't break anyone :smile:

--- dotnet/src/IntegrationTests/Connectors/OpenAI/OpenAIChatCompletion_NonStreamingTests.cs
+++ dotnet/src/IntegrationTests/Connectors/OpenAI/OpenAIChatCompletion_NonStreamingTests.cs
@@ -13,6 +13,7 @@ using Microsoft.SemanticKernel.Connectors.OpenAI;
 using Microsoft.SemanticKernel.TextGeneration;
 using OpenAI.Chat;
 using SemanticKernel.IntegrationTests.TestSettings;
+using xRetry;
 using Xunit;
 
 namespace SemanticKernel.IntegrationTests.Connectors.OpenAI;
@@ -142,11 +143,11 @@ public sealed class OpenAIChatCompletionNonStreamingTests : BaseIntegrationTest
         Assert.Empty((logProbabilityInfo as IReadOnlyList<ChatTokenLogProbabilityDetails>)!);
     }
 
-    [Fact]
+    [RetryFact]
     public async Task ChatCompletionWithWebSearchAsync()
     {
         // Arrange
-        var kernel = this.CreateAndInitializeKernel(modelIdOverride: "gpt-4o-mini-search-preview");
+        var kernel = this.CreateAndInitializeKernel(modelIdOverride: "gpt-4o-search-preview");
         var chatService = kernel.Services.GetRequiredService<IChatCompletionService>();
         var settings = new OpenAIPromptExecutionSettings
         {
