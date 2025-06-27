b78307cf64ae34af7e1cba5a87cbc922fafaa1af
Roger Barreto <19890735+rogerbarreto@users.noreply.github.com>
2025-06-27 15:04:23 -0700

    .Net: Revert RetryFacts for AzureOpenAI Integration Tests (#12576)
    ### Motivation and Context
    
    - Added timeout to up to 100 seconds for potential failing long running
    tests
    - Removed recently added RetryFact due to a potential disruption in the
    current integration infrastructure when multiple integration tests are
    running in parallel from merge queue.
    
    ---------
    
    Co-authored-by: Mark Wallace <127216156+markwallace-microsoft@users.noreply.github.com>

--- dotnet/src/IntegrationTests/Connectors/AzureOpenAI/AzureOpenAIChatClient_RequiredFunctionChoiceBehaviorTests.cs
+++ dotnet/src/IntegrationTests/Connectors/AzureOpenAI/AzureOpenAIChatClient_RequiredFunctionChoiceBehaviorTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Globalization;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Azure.Identity;
 using Microsoft.Extensions.AI;
@@ -12,13 +13,13 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Connectors.AzureOpenAI;
 using SemanticKernel.IntegrationTests.TestSettings;
-using xRetry;
 using Xunit;
 
 namespace SemanticKernel.IntegrationTests.Connectors.AzureOpenAI;
 
-public sealed class AzureOpenAIChatClientRequiredFunctionChoiceBehaviorTests : BaseIntegrationTest
+public sealed class AzureOpenAIChatClientRequiredFunctionChoiceBehaviorTests : BaseIntegrationTest, IDisposable
 {
+    private HttpClient? _httpClient;
     private readonly Kernel _kernel;
     private readonly FakeFunctionFilter _autoFunctionInvocationFilter;
     private readonly IChatClient _chatClient;
@@ -32,7 +33,7 @@ public sealed class AzureOpenAIChatClientRequiredFunctionChoiceBehaviorTests : B
         this._chatClient = this._kernel.GetRequiredService<IChatClient>();
     }
 
-    [RetryFact]
+    [Fact]
     public async Task SpecifiedInCodeInstructsConnectorToInvokeKernelFunctionAutomaticallyAsync()
     {
         // Arrange
@@ -63,7 +64,7 @@ public sealed class AzureOpenAIChatClientRequiredFunctionChoiceBehaviorTests : B
         Assert.Contains("GetCurrentDate", invokedFunctions);
     }
 
-    [RetryFact(Skip = "For manual verification only")]
+    [Fact]
     public async Task SpecifiedInPromptInstructsConnectorToInvokeKernelFunctionAutomaticallyAsync()
     {
         // Arrange
@@ -352,8 +353,15 @@ public sealed class AzureOpenAIChatClientRequiredFunctionChoiceBehaviorTests : B
         Assert.Empty(invokedFunctions);
     }
 
+    public void Dispose()
+    {
+        this._httpClient?.Dispose();
+        this._chatClient?.Dispose();
+    }
+
     private Kernel InitializeKernel()
     {
+        this._httpClient ??= new() { Timeout = TimeSpan.FromSeconds(100) };
         var azureOpenAIConfiguration = this._configuration.GetSection("AzureOpenAI").Get<AzureOpenAIConfiguration>();
         Assert.NotNull(azureOpenAIConfiguration);
         Assert.NotNull(azureOpenAIConfiguration.ChatDeploymentName);
@@ -365,7 +373,8 @@ public sealed class AzureOpenAIChatClientRequiredFunctionChoiceBehaviorTests : B
             deploymentName: azureOpenAIConfiguration.ChatDeploymentName,
             modelId: azureOpenAIConfiguration.ChatModelId,
             endpoint: azureOpenAIConfiguration.Endpoint,
-            credentials: new AzureCliCredential());
+            credentials: new AzureCliCredential(),
+            httpClient: this._httpClient);
 
         return kernelBuilder.Build();
     }
