cd34dcd59132a840e6e441df7526d5611d44abec
Shay Rojansky <roji@roji.org>
2025-07-24 12:52:50 +0200

    .Net: Add MEVD TestSuiteImplementationTests (#12574)
    I noticed that actual test suite implementation is a bit spotty across
    providers; so this adds a test suite which tests that all other test
    suites are implemented. We have this in the EF tests as well, also as a
    way of letting provider maintainers know when a new test suite is
    introduced which they probably want to implement. Of course, providers
    can explicitly ignore specific test suites which don't make sense for
    them.
    
    Part of #10194

---
+++ dotnet/test/VectorData/Weaviate.ConformanceTests/WeaviateTestSuiteImplementationTests.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using VectorData.ConformanceTests;
+using VectorData.ConformanceTests.VectorSearch;
+
+namespace Weaviate.ConformanceTests;
+
+public class WeaviateTestSuiteImplementationTests : TestSuiteImplementationTests
+{
+    protected override ICollection<Type> IgnoredTestBases { get; } =
+    [
+        typeof(VectorSearchWithFilterConformanceTests<>)
+    ];
+}
