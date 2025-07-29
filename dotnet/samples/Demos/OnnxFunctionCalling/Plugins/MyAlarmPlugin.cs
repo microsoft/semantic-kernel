2f425bb1cc1e411df2786c7d3576eddb5182df9c
Loïc BERTRAND <loic.bert.marcel@gmail.com>
2025-07-29 12:48:22 +0200

    .Net: add new demo projects

---
+++ dotnet/samples/Demos/OnnxFunctionCalling/Plugins/MyAlarmPlugin.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.ComponentModel;
+using Microsoft.SemanticKernel;
+
+namespace OnnxFunctionCalling.Plugins;
+
+/// <summary>
+/// Class that represents a controllable alarm.
+/// </summary>
+public class MyAlarmPlugin
+{
+    private string _hour;
+
+    public MyAlarmPlugin(string providedHour)
+    {
+        this._hour = providedHour;
+    }
+
+    [KernelFunction]
+    [Description("Sets an alarm at the provided time")]
+    public string SetAlarm(string time)
+    {
+        this._hour = time;
+        return GetCurrentAlarm();
+    }
+
+    [KernelFunction]
+    [Description("Get current alarm set")]
+    public string GetCurrentAlarm()
+    {
+        return $"Alarm set for {_hour}";
+    }
+}
---
+++ dotnet/samples/Demos/OpenAIFunctionCalling/Plugins/MyLightPlugin.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.ComponentModel;
+using Microsoft.SemanticKernel;
+
+namespace OpenAIFunctionCalling.Plugins;
+
+/// <summary>
+/// Class that represents a controllable light bulb.
+/// </summary>
+[Description("Represents a light bulb")]
+public class MyLightPlugin(bool turnedOn = false)
+{
+    private bool _turnedOn = turnedOn;
+
+    [KernelFunction]
+    [Description("Returns whether this light is on")]
+    public bool IsTurnedOn() => _turnedOn;
+
+    [KernelFunction]
+    [Description("Turn on this light")]
+    public void TurnOn() => _turnedOn = true;
+
+    [KernelFunction]
+    [Description("Turn off this light")]
+    public void TurnOff() => _turnedOn = false;
+}
