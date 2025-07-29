7caa5e674a069141c0d225f7bec11d9e944ea307
Chris Rickman <crickman@microsoft.com>
2025-07-28 22:13:36 -0700

    Demo

---
+++ dotnet/samples/Demos/DeclarativeWorkflow/Program.cs
@@ -0,0 +1,75 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Diagnostics;
+using System.Reflection;
+using Azure.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+
+namespace Demo.DeclarativeWorkflow;
+
+internal static class Program
+{
+    private const string InputEventId = "start-workflow";
+
+    public static async Task Main(string[] args)
+    {
+        // Load configuration and create kernel with Azure OpenAI Chat Completion service
+        IConfiguration config = InitializeConfig();
+        Kernel kernel = CreateKernel(config["AzureOpenAI:Endpoint"]!, config["AzureOpenAI:ChatDeploymentName"]!);
+
+        Notify("PROCESS INIT\n");
+
+        Stopwatch timer = Stopwatch.StartNew();
+
+        // Interpret the workflow YAML into a KernelProcess
+        using StreamReader yamlReader = File.OpenText("demo250729.yaml");
+        KernelProcess process = ObjectModelBuilder.Build(yamlReader, InputEventId);
+
+        Notify($"\nPROCESS DEFINED: {timer.Elapsed}\n");
+
+        Notify("\nPROCESS INVOKE\n");
+
+        // Run the process, just like any other KernelProcess
+        await using LocalKernelProcessContext context =
+            await process.StartAsync(
+                kernel,
+                new KernelProcessEvent()
+                {
+                    Id = InputEventId,
+                    // Pass the first argument as the input data for the process, if present.
+                    Data = args.FirstOrDefault() ?? string.Empty
+                });
+
+        Notify("\nPROCESS DONE");
+    }
+
+    // Load configuration from user-secrets
+    private static IConfigurationRoot InitializeConfig() =>
+        new ConfigurationBuilder()
+            .AddUserSecrets(Assembly.GetExecutingAssembly())
+            .Build();
+
+    // Create kernel with Azure OpenAI Chat Completion service
+    private static Kernel CreateKernel(string endpoint, string model)
+    {
+        IKernelBuilder kernelBuilder = Kernel.CreateBuilder();
+
+        kernelBuilder.AddAzureOpenAIChatCompletion(model, endpoint, new AzureCliCredential());
+
+        return kernelBuilder.Build();
+    }
+
+    private static void Notify(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        try
+        {
+            Console.WriteLine(message);
+        }
+        finally
+        {
+            Console.ResetColor();
+        }
+    }
+}
---
+++ dotnet/samples/Demos/DeclarativeWorkflow/readme.md
@@ -0,0 +1,24 @@
+﻿# Summary
+
+This demo showcases the ability to parse a YAML workflow based on Copilo Studio actions
+and produce a `KernelProcess` that can be executed in the same fashion as any other `KernelProcess`.
+
+## Key Features
+
+This demo illustrates the following capabilities:
+
+- Parse YAML workflow actions using `Microsoft.Bot.ObjectModel`
+- Store and retrieve variable state
+- Evaluate expressions using `Microsoft.PowerFx.Interpreter`
+- Support control flow (foreach, goto, etc...)
+- Generate response from LLM using _Semantic Kernel_
+
+## Status Details
+
+- This is using a POC based on the _Process Framework_ from the _Semantic Kernel_ repo.
+  - When the redesigned _Process Framework_ is available in the _Agent Framework_ repo it must 
+    be re-implemented using the new API patterns.
+  - Capturing and restoring workflow state is not yet available in either version of the _Process Framework_.
+  - The ability to emit events from the _KernelProcess_ to the host API is not yet supported.
+- `Microsoft.Bot.ObjectModel` is not (yet) available as a dependency that may be referenced by a _GitHub_ repository.
+- The full set of CPSDL actions to be supported is not fully defined, nor are the "Pri-0" samples.
