84f14ec3acf4fc0f4bfd93ec3cc77e37aa8d03ae
Damien Guard <damieng@gmail.com>
2025-07-30 11:40:21 +0100

    Add support for Guid and ObjectID keys in the MongoDB Connector.

--- dotnet/test/VectorData/MongoDB.ConformanceTests/Support/MongoTestStore.cs
+++ dotnet/test/VectorData/MongoDB.ConformanceTests/Support/MongoTestStore.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 
 using Microsoft.SemanticKernel.Connectors.MongoDB;
+using MongoDB.Bson;
 using MongoDB.Driver;
 using Testcontainers.MongoDb;
 using VectorData.ConformanceTests.Support;
@@ -49,4 +50,16 @@ internal sealed class MongoTestStore : TestStore
 
     protected override Task StopAsync()
         => this._container.StopAsync();
+
+    private static readonly string? s_baseObjectId = ObjectId.GenerateNewId().ToString().Substring(0, 14);
+
+    public override TKey GenerateKey<TKey>(int value)
+    {
+        if (typeof(TKey) == typeof(ObjectId))
+        {
+            return (TKey)(object)ObjectId.Parse(s_baseObjectId + value.ToString("0000000000"));
+        }
+
+        return base.GenerateKey<TKey>(value);
+    }
 }
