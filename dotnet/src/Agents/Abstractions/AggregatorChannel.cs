dc4ccd0f3c93007892f7926838b63533e0a2b15e
SergeyMenshykh <sergemenshikh@gmail.com>
2025-08-01 12:52:50 +0100

    remove unnecessury namespace

--- dotnet/src/Agents/Abstractions/AggregatorChannel.cs
+++ dotnet/src/Agents/Abstractions/AggregatorChannel.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text.Json;
 using System.Threading;
