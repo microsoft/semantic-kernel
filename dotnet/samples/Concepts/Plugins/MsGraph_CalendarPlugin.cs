ca3c7d9d38f768a912526c3a54996e51497cf27f
Roger Barreto <19890735+rogerbarreto@users.noreply.github.com>
2025-08-19 20:12:00 +0100

    .Net: Address vulnerable packages (+Update MsGraph for latest Major V5)  (#12962)
    ### Motivation and Context
    
    This PR addresses a recent vulnerability warning received while working
    in the Semantic Kernel solution, limited to specific packages for
    Prompty, Milvus and Microsoft Graph Plugin.
    
    As part of addressing the vulnerabilities was also necessary to upgrade
    the logic to use the newest packages from Microsoft.Graph library.

---
+++ dotnet/samples/Concepts/Plugins/MsGraph_CalendarPlugin.cs
@@ -0,0 +1,61 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Text.Json;
+using Azure.Identity;
+using Microsoft.Graph;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Plugins.MsGraph;
+using Microsoft.SemanticKernel.Plugins.MsGraph.Connectors;
+
+namespace Plugins;
+
+/// <summary>
+/// This example shows how to use Microsoft Graph Plugin
+/// These examples require a valid Microsoft account and delegated/application access for the Microsoft Graph used resources.
+/// </summary>
+public class MsGraph_CalendarPlugin(ITestOutputHelper output) : BaseTest(output)
+{
+    private static readonly JsonSerializerOptions s_options = new() { WriteIndented = true };
+
+    /// <summary>Shows how to use Microsoft Graph Calendar Plugin with AI Models.</summary>
+    [Fact]
+    public async Task UsingWithAIModel()
+    {
+        // Setup the Kernel
+        Kernel kernel = Kernel.CreateBuilder()
+            .AddOpenAIChatClient(TestConfiguration.OpenAI.ChatModelId, TestConfiguration.OpenAI.ApiKey)
+            .Build();
+
+        using var graphClient = GetGraphClient();
+
+        var calendarConnector = new OutlookCalendarConnector(graphClient);
+
+        // Add the plugin to the Kernel
+        var graphPlugin = kernel.Plugins.AddFromObject(new CalendarPlugin(calendarConnector, jsonSerializerOptions: s_options));
+
+        var settings = new PromptExecutionSettings { FunctionChoiceBehavior = FunctionChoiceBehavior.Auto() };
+
+        string Prompt = $"""
+            1. Show me the next 10 calendar events I have
+            2. If I don't have any event named "Semantic Kernel", please create a new event named "Semantic Kernel"
+            starting at {DateTimeOffset.Now.AddHours(1)} with 1 hour of duration.
+            """;
+
+        // Invoke the OneDrive plugin multiple times
+        var result = await kernel.InvokePromptAsync(Prompt, new(settings));
+
+        Console.WriteLine(result);
+    }
+
+    private static GraphServiceClient GetGraphClient()
+    {
+        var credential = new InteractiveBrowserCredential(new InteractiveBrowserCredentialOptions()
+        {
+            ClientId = TestConfiguration.MSGraph.ClientId,
+            TenantId = TestConfiguration.MSGraph.TenantId,
+            RedirectUri = TestConfiguration.MSGraph.RedirectUri,
+        });
+
+        return new GraphServiceClient(credential);
+    }
+}
---
+++ dotnet/samples/Concepts/Plugins/MsGraph_EmailPlugin.cs
@@ -0,0 +1,58 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Azure.Identity;
+using Microsoft.Graph;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Plugins.MsGraph.Connectors;
+
+namespace Plugins;
+
+/// <summary>
+/// This example shows how to use Microsoft Graph Plugin
+/// These examples require a valid Microsoft account and delegated/application access for the used resources.
+/// </summary>
+public class MsGraph_EmailPlugin(ITestOutputHelper output) : BaseTest(output)
+{
+    /// <summary>Shows how to use Microsoft Graph Email Plugin with AI Models.</summary>
+    [Fact]
+    public async Task EmailPlugin_SendEmailToMyself()
+    {
+        // Setup the Kernel
+        Kernel kernel = Kernel.CreateBuilder()
+            .AddOpenAIChatClient(TestConfiguration.OpenAI.ChatModelId, TestConfiguration.OpenAI.ApiKey)
+            .Build();
+
+        using var graphClient = GetGraphClient();
+
+        var emailConnector = new OutlookMailConnector(graphClient);
+
+        // Add the plugin to the Kernel
+        var graphPlugin = kernel.Plugins.AddFromObject(new Microsoft.SemanticKernel.Plugins.MsGraph.EmailPlugin(emailConnector));
+
+        var settings = new PromptExecutionSettings { FunctionChoiceBehavior = FunctionChoiceBehavior.Auto() };
+
+        const string Prompt = """
+            Using the tools available, please do the following:
+            1. Get my email address
+            2. Send an email to myself with the subject "FYI" and content "This is a very important email"
+            3. List 10 of my email messages
+            """;
+
+        // Invoke the Graph plugin with a prompt
+        var result = await kernel.InvokePromptAsync(Prompt, new(settings));
+
+        Console.WriteLine(result);
+    }
+
+    private static GraphServiceClient GetGraphClient()
+    {
+        var credential = new InteractiveBrowserCredential(new InteractiveBrowserCredentialOptions()
+        {
+            ClientId = TestConfiguration.MSGraph.ClientId,
+            TenantId = TestConfiguration.MSGraph.TenantId,
+            RedirectUri = TestConfiguration.MSGraph.RedirectUri,
+        });
+
+        return new GraphServiceClient(credential);
+    }
+}
